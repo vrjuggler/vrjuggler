e8e9439f44a8f36beadbeb2575dbe9e6345e0e9f
Allen Bierbaum <abierbaum@gmail.com>
1998-09-04 18:20:04 +0000

    Accept config file on command line.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@98 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/test/Draw/OGL/cubes/cubes.cpp
+++ modules/vrjuggler/test/Draw/OGL/cubes/cubes.cpp
@@ -8,12 +8,16 @@
       // --- Lib Stuff --- //
 #include <Kernel/vjKernel.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
    vjKernel* kernel = vjKernel::instance();        // Get the kernel
    cubesApp* application = new cubesApp(kernel);   // Declare an instance of the app
 
    kernel->setApplication(application);         // Set application
+   
+   if(argc > 1)
+      kernel->setProgramSpecifiedConfigFile(argv[1]);
+
    kernel->start();
       //- Kernel load global config  -- Environment variable
       //- App KernelConfig function
