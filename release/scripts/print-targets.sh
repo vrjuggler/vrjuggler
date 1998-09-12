396da97e540a12d087e9b21400d3322eae92fd9d
Patrick Hartling <patrick.hartling@gmail.com>
1998-09-12 01:20:44 +0000

    Add new scripts used primarily for installation/release of the library.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@125 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ release/scripts/print-targets.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+cat <<EOF
+
+Build targets are:
+    all             - Build everything.
+    dbg             - Build a debugging, static copy of the library.
+    ddso            - Build a debugging, dynamic shared object version of the
+                      library.
+    opt             - Build an optimized, static copy of the library.
+    dso             - Build a dynamic shared object version of the library.
+    obj             - Build the object files using the default value for
+                      \${OBJDIR}.
+
+Library type targets are:
+    lib-static      - Build the static version of the library.  This target
+                      depends on a correct value for \${OBJDIR} (whose default
+                      value is set in Makefile.base).
+    lib-dynamic     - Build the dynamic shared version of the library.  This
+                      target depends on a correct value for \${OBJDIR} (whose
+                      default value is set in Makefile.base).
+
+Dependency targets are:
+    depend          - Recursively build the dependencies for the source files.
+    newdepend       - Recursively (and forcibly) rebuild the dependencies for
+                      the source files.
+
+Install targets are:
+    install-all     - Install all versions of the library, all header files,
+                      the Data directory and the test code.
+    install         - Install the library, all header files, the Data
+                      directory and the test code.
+    install-dbg     - Install the debugging, static version of the library.
+    install-ddso    - Install the debugging, dynamic shared object version of
+                      the library.
+    install-opt     - Install the optimized, static version of the library.
+    install-dso     - Install the dynamic shared object version of the library.
+    install-headers - Install only the header files.
+    install-test    - Install only the test code.
+    install-data    - Install only the Data directory.
+
+Miscellaneous targets are:
+    links           - Define links (for developer use only) that provide
+                      functionality allowing the use of the \$VJ_BASE_DIR
+                      environment variable.  This target depends on a correct
+                      value for \${OBJDIR} (whose default value is set in
+                      Makefile.base).
+    clean-links     - Remove the symlinks created by 'links'.
+
+Clean-up targets are:
+    clean           - Clean up everything (uses common 'clean' target).
+    clobber         - Clean up everything and remove the directory containing
+                      the compiled library.
+
+The default target is 'dbg'.  'install' will install the debugging, static
+version of the library.
+
+EOF
