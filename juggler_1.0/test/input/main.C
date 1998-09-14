a1c081c01ae98aae9f32a6c8bf23ee25e066a857
Allen Bierbaum <abierbaum@gmail.com>
1998-09-14 18:45:52 +0000

    Now uses vjInterface's
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@151 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/test/input/main.C
+++ juggler_1.0/test/input/main.C
@@ -1,5 +1,7 @@
 #include <SharedMem/vjMemPool.h>
 #include <Input/InputManager/vjInputManager.h>
+#include <Input/InputManager/vjPosInterface.h>
+#include <Math/vjCoord.h>
 
 
 #define CHUNK_DESC_LOCATION "/home/users/allenb/Source/juggler/Data/chunksDesc"
@@ -7,7 +9,7 @@
 
 int main()
 {
-   // --- Load chunk database -- //    
+   // --- Load chunk database -- //
    vjMemPool* shared_pool = new vjSharedPool(1024*1024);
    vjChunkDescDB desc;
    bool load_worked = desc.load(CHUNK_DESC_LOCATION);
@@ -50,12 +52,21 @@ int main()
       input_manager->UpdateAllData();
 
 
-      vjMatrix* pd_head = input_manager->GetPosData(head_index);
-      vjMatrix* pd_wand = input_manager->GetPosData(wand_index);    
-      
+      vjMatrix* pd_head = input_manager->GetPosProxy(head_index)->GetData();
+      vjMatrix* pd_wand = input_manager->GetPosProxy(wand_index)->GetData();
+
+      cout << "-------------------------------------\n";
       cout << "head:\n" << *pd_head << endl;
+      vjCoord head_coord(*pd_head);
+      cout << "\tpos:" << head_coord.pos << endl;
+      cout << "\tor:" << head_coord.orient << endl;
+
       cout << "wand:\n" << *pd_wand << endl << flush;
-   } 
+      vjCoord wand_coord(*pd_wand);
+      cout << "\tpos:" << wand_coord.pos << endl;
+      cout << "\tor:" << wand_coord.orient << endl;
+      cout << "-------------------------------------\n\n";
+   }
 
 
    // -- Delete the garabage --- //
