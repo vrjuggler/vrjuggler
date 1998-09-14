93da9a01191e282d4e99d70c9b17e5e89c48896c
Allen Bierbaum <abierbaum@gmail.com>
1998-09-14 18:43:54 +0000

    Nothing.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@150 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/Input/vjPosition/vjPosition.C
+++ juggler_1.0/Input/vjPosition/vjPosition.C
@@ -8,43 +8,39 @@ vjPosition::vjPosition(vjConfigChunk *c) : vjInput(c) , xformMat()
   port_id = -1;
   active = 0;
   theData = NULL;
-  
+
   if ((c->getNum("translate") == 3) && (c->getNum("rotate") == 3))
-  {  
+  {
     // These are the transforms from the base tracker coord system
-     
+
     float xt, yt, zt;
-    xt = c->getProperty("translate",0); 
+    xt = c->getProperty("translate",0);
     yt = c->getProperty("translate",1);
     zt = c->getProperty("translate",2);
-  
-  
+
+
     float xr, yr, zr;
     xr = c->getProperty("rotate",0);
     yr = c->getProperty("rotate",1);
     zr = c->getProperty("rotate",2);
-      
+
       // This makes a rotation matrix that moves the device's coord system
       // to the vj coord system.
     rotMat.makeXYZEuler(xr, yr, zr);
-    
-    /*
-    vjMatrix temp_rot = rotMat;
-    rotMat.invert(temp_rot);     // We need to use the inverse to xform pts
-    */
-    
+
+
     xformMat.makeTrans(xt, yt, zt);
     xformMat.postMult(rotMat);         // xformMat = T*R
   }
-  
-  deviceAbilities = deviceAbilities | DEVICE_POSITION; 
-   
+
+  deviceAbilities = deviceAbilities | DEVICE_POSITION;
+
 }
 
 vjPosition::vjPosition()
 {
   cerr << "    vjPosition::vjPosition()" << endl;
-  deviceAbilities = deviceAbilities | DEVICE_POSITION; 
+  deviceAbilities = deviceAbilities | DEVICE_POSITION;
   theData = NULL;
 }
 
