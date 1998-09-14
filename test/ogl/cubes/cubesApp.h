bbd083dcd481a43517e05894bc7c5a9a92022428
Allen Bierbaum <abierbaum@gmail.com>
1998-09-14 18:46:41 +0000

    Uses vjDEBUG now to suppress output.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@152 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- test/ogl/cubes/cubesApp.h
+++ test/ogl/cubes/cubesApp.h
@@ -24,7 +24,7 @@ public:
       firstTime = true;
       cubeDLIndex = -1;
    }
-   
+
 public:
    bool  firstTime;
    int   cubeDLIndex;
@@ -43,7 +43,7 @@ public:
    {
       navMatrix.makeIdent();
    }
-   
+
    // Execute any initialization needed before the API is started
    virtual void init()
    {
@@ -66,20 +66,20 @@ public:
     */
    virtual void draw()
    {
-      
+
       if(contextData().firstTime == true)  // If not inited
       {
          contextData().firstTime = false;
          contextData().cubeDLIndex = glGenLists(1);
-         
+
          cerr << "Creating DL:" << contextData().cubeDLIndex << endl;
-      
+
          glNewList(contextData().cubeDLIndex, GL_COMPILE);
             drawbox(-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, GL_QUADS);
          glEndList();
       }
-      
-      
+
+
       initGLState();    // This should really be in another function
       myDraw();
    }
@@ -95,18 +95,18 @@ public:
     *	       postDraw();      // Drawing is happening while here
     *       sync();
     *        postSync();      // Drawing is now done
-    *      
+    *
     *	      UpdateTrackers();
     *  }
     * </pre>
     */
-   
+
    /// Function called before updating trackers but after the frame is drawn
    virtual void postSync()
    {
       vjDEBUG(2) << "cubesApp::postSync" << endl << vjDEBUG_FLUSH;
    }
-   
+
    /// Function called after tracker update but before start of drawing
    virtual void preDraw()
    {
@@ -114,7 +114,7 @@ public:
 
        updateNavigation();       // Update the navigation matrix
    }
-   
+
    /// Function called after drawing has been triggered but BEFORE it completes
    virtual void postDraw()
    {
@@ -128,55 +128,55 @@ private:
       // Cur*Transform = New Location
       vjMatrix    transform, transformIdent;
       vjQuat      source_rot, goal_rot, slerp_rot;
-      
+
       transformIdent.makeIdent();
       source_rot.makeQuat(transformIdent);
 
       vjMatrix* wand_matrix;
       wand_matrix = mWand->GetData();
-      
-      cout << "===================================\n";
-      cout << "Wand:\n" << *wand_matrix << endl;
+
+      vjDEBUG(1) << "===================================\n" << vjDEBUG_FLUSH;
+      vjDEBUG(1) << "Wand:\n" << *wand_matrix << endl << vjDEBUG_FLUSH;
 
       transform = *wand_matrix;
       goal_rot.makeQuat(transform);
 
       if(transformIdent != transform)
       {
-         slerp_rot.slerp(0.0005, source_rot, goal_rot);
+         slerp_rot.slerp(0.05, source_rot, goal_rot);    // Transform 5% of the way there
          transform.makeQuaternion(slerp_rot);
       }
       else
          transform.makeIdent();
 
-      cout << "Transform:\n" << transform << endl;
+      vjDEBUG(1) << "Transform:\n" << transform << endl << vjDEBUG_FLUSH;
 
       navMatrix.postMult(transform);
 
-      cout << "Nav:\n" << navMatrix << endl << endl << flush;
+      vjDEBUG(1) << "Nav:\n" << navMatrix << endl << endl << vjDEBUG_FLUSH;
    }
 
    //----------------------------------------------
    //  Draw the scene.  A bunch of boxes of differing color and stuff
    //----------------------------------------------
-   
+
    void myDraw()
    {
       vjDEBUG(2) << "\n--- myDraw() ---\n" << vjDEBUG_FLUSH;
-      
+
       static const float SCALE = 100;
       //static const float SCALE = 10;
       static const float INCR = 0.1;
-   
+
       glClearColor(0.0, 0.0, 0.0, 0.0);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   
+
       glPushMatrix();
             // --- Push on Navigation matrix --- //
          glMultMatrixf(navMatrix.getFloatPtr());
-         
-         
-      
+
+
+
          //---- Main box loop -----///
          for (float x=0;x<1;x += INCR)
             for (float y=0;y<1; y += INCR)
@@ -189,9 +189,9 @@ private:
    		            drawCube();
                   glPopMatrix();
                }
-         
 
-       
+
+
         /*
             // --- Draw corner boxes --- //
          for(float x=0;x<=1.0;x++)
@@ -211,10 +211,10 @@ private:
         */
 
       glPopMatrix();
-      
-      
+
+
    }
-   
+
    void initGLState()
    {
       GLfloat light0_ambient[] = { .2,  .2,  .2,  1.0};
@@ -222,53 +222,43 @@ private:
       GLfloat light0_specular[] = { 1.0,  1.0,  1.0,  1.0};
       GLfloat light0_position[] = {2000.0, 1000.0, 100.0, 1.0};
       GLfloat light0_direction[] = {-100, -100.0, -100.0};
-   
+
       GLfloat mat_ambient[] = { 0.7, 0.7,  0.7,  1.0};
       GLfloat mat_diffuse[] = { 1.0,  0.5,  0.8,  1.0};
       GLfloat mat_specular[] = { 1.0,  1.0,  1.0,  1.0};
       GLfloat mat_shininess[] = { 50.0};
       GLfloat mat_emission[] = { 1.0,  1.0,  1.0,  1.0};
       GLfloat no_mat[] = { 0.0,  0.0,  0.0,  1.0};
-   
+
       glLightfv(GL_LIGHT0, GL_AMBIENT,  light0_ambient);
       glLightfv(GL_LIGHT0, GL_DIFFUSE,  light0_diffuse);
       glLightfv(GL_LIGHT0, GL_SPECULAR,  light0_specular);
       glLightfv(GL_LIGHT0, GL_POSITION,  light0_position);
       glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light0_direction);
-   
+
       glMaterialfv( GL_FRONT, GL_AMBIENT, mat_ambient );
-      glMaterialfv( GL_FRONT,  GL_DIFFUSE, mat_diffuse ); 
+      glMaterialfv( GL_FRONT,  GL_DIFFUSE, mat_diffuse );
       glMaterialfv( GL_FRONT, GL_SPECULAR, mat_specular );
-      glMaterialfv( GL_FRONT,  GL_SHININESS, mat_shininess ); 
-      glMaterialfv( GL_FRONT,  GL_EMISSION, no_mat); 
-   
+      glMaterialfv( GL_FRONT,  GL_SHININESS, mat_shininess );
+      glMaterialfv( GL_FRONT,  GL_EMISSION, no_mat);
+
       glEnable(GL_DEPTH_TEST);
       glEnable(GL_NORMALIZE);
       glEnable(GL_LIGHTING);
       glEnable(GL_LIGHT0);
       glEnable(GL_COLOR_MATERIAL);
-      glShadeModel(GL_SMOOTH); 
-   
+      glShadeModel(GL_SMOOTH);
+
       glMatrixMode(GL_MODELVIEW);
    }
 
    void drawCube()
    {
-      
-      /*
-      if(GL_TRUE == glIsList(contextData().cubeDLIndex))
-         cerr << "Valid display List" << endl;
-      else
-         cerr << "In-Valid display list" << endl;
-      */
-
       glCallList(contextData().cubeDLIndex);
-      
-      //drawbox(-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, GL_QUADS);
    }
-   
-   
-   
+
+
+
    void drawbox(GLdouble x0, GLdouble x1, GLdouble y0, GLdouble y1,
                 GLdouble z0, GLdouble z1, GLenum type)
    {
@@ -282,18 +272,18 @@ private:
       };
       GLdouble v[8][3], tmp;
       GLint i;
-   
+
       if (x0 > x1)
       {
          tmp = x0; x0 = x1; x1 = tmp;
       }
       if (y0 > y1)
       {
-         tmp = y0; y0 = y1; y1 = tmp; 
+         tmp = y0; y0 = y1; y1 = tmp;
       }
       if (z0 > z1)
       {
-         tmp = z0; z0 = z1; z1 = tmp; 
+         tmp = z0; z0 = z1; z1 = tmp;
       }
       v[0][0] = v[1][0] = v[2][0] = v[3][0] = x0;
       v[4][0] = v[5][0] = v[6][0] = v[7][0] = x1;
@@ -301,7 +291,7 @@ private:
       v[2][1] = v[3][1] = v[6][1] = v[7][1] = y1;
       v[0][2] = v[3][2] = v[4][2] = v[7][2] = z0;
       v[1][2] = v[2][2] = v[5][2] = v[6][2] = z1;
-   
+
       for (i = 0; i < 6; i++)
       {
          glBegin(type);
@@ -324,5 +314,5 @@ public:
    vjMatrix    navMatrix;     // Matrix for navigation in the application
 
 };
- 
+
 #endif
