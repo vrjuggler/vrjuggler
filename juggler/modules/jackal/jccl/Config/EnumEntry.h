0d941d51debb9e490acd81fe7b056792a19036cf
Christopher Just <coronax@users.sourceforge.net>
1998-09-15 00:50:53 +0000

    *** empty log message ***
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@154 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/jackal/jccl/Config/EnumEntry.h
@@ -0,0 +1,30 @@
+/* Tiny little data structure used to store an entry of enumeration
+ * or valuelabel data 
+ */
+
+#include <strings.h>
+
+class vjEnumEntry {
+private:
+  char *entry;
+  int val;
+
+public:
+  vjEnumEntry (char *e, int v) {
+    entry = new char[strlen(e)+1];
+    strcpy (entry, e);
+    val = v;
+  }
+
+  ~vjEnumEntry () {
+    delete entry;
+  }
+
+  char* getName () {
+    return entry;
+  }
+
+  int getVal () {
+    return val;
+  }
+};
