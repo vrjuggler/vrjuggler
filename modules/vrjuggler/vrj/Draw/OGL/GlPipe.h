6da841b5e66f07f407af27e8ff0b352eb6a87df7
Allen Bierbaum <abierbaum@gmail.com>
1998-10-06 17:39:54 +0000

    Thread manager added. Context specific data added.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@195 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/OGL/GlPipe.h
+++ modules/vrjuggler/vrj/Draw/OGL/GlPipe.h
@@ -20,7 +20,7 @@ class vjGlDrawManager;
 #include <Sync/vjCond.h>
 #include <Sync/vjSemaphore.h>
 
-   
+
 //--------------------------------------------------------------------------------------
 //: Handles the rendering on multiple GLWindows in a single process.
 //
@@ -37,7 +37,7 @@ public:
       //!ARGS: _num - The id number of the pipe.
       //!NOTE: All contained windows SHOULD have the same pipe number
    vjGlPipe(int _num, vjGlDrawManager* glMgr)
-      : controlExit(0), glManager(glMgr), renderTriggerSema(0), renderCompleteSema(0)
+      : controlExit(0), glManager(glMgr), renderTriggerSema(0), renderCompleteSema(0), mActiveThread(NULL)
    {
       mThreadRunning = false;
    }
@@ -49,7 +49,7 @@ public:
       //! NOTE: The pipe does NOT have to have any windows in order to run
       //+        that way we can add windows to pipes at run-time
    int start();
-  
+
       //: Stop the pipe
       //!POST: Flag is set to tell pipe to stop rendering
    void stop()
@@ -66,7 +66,7 @@ public:
       //! PRE: The pipe must have a thread of control
       //!POST: The pipe has completed its rendering
    void completeRender();
-    
+
       //: Add a GLWindow to the new window list
       // Control loop must now open the window on the next frame
    void addWindow(vjGlWindow* win);
@@ -100,10 +100,10 @@ private:
    {
       ;
    }
-   
+
       //:   Renders the window using OpenGL
       //!POST: win is rendered (In stereo if it is a stereo window)
-   void renderWindow(vjGlWindow* win);   
+   void renderWindow(vjGlWindow* win);
 
 public:
    enum RenderState {RENDERING, WAITING};    // Type to delineate the current rendering state
@@ -124,10 +124,11 @@ private:
    //RenderState renderState;         //: The current render state.
    vjSemaphore    renderCompleteSema;  //: signals render completed
    vjSemaphore    renderTriggerSema;   //: Signals render trigger
-   
+
    int         controlExit;         //: Flag for when to exit the control loop
 
-   bool        mThreadRunning;      //: Do we have a running thread? 
+   vjThread*   mActiveThread;      //: The thread running this object
+   bool        mThreadRunning;      //: Do we have a running thread?
 };
 
 #endif
