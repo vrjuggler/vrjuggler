7b2c23865ed569549ec9e4f1b7723a73bff94cf6
Allen Bierbaum <abierbaum@gmail.com>
1998-10-06 17:47:16 +0000

    Updated to use thread manager.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@202 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- test/threads/test.cpp
+++ test/threads/test.cpp
@@ -1,103 +1,52 @@
-/// TOP OF TEST_C
 #include <iostream.h>
 #include <stdio.h>
-
-#ifdef C2_OS_SGI
-#   include <ulocks.h>
-#endif
-
-#include <unistd.h>
 #include <math.h>
-#include <time.h>
-#include <sys/time.h>
 
-#include <Sync/C2Mutex.h>
-#include <Sync/C2Semaphore.h>
-#include <Sync/C2Barrier.h>
-#include <Sync/NullMutex.h>
-#include <Threads/C2Thread.h>
+#include <Threads/vjThread.h>
+#include <Threads/vjTSObjectProxy.h>
+#include <Threads/vjTSObject.h>
+#include <Kernel/vjDebug.h>
 
-void doIt(void*);
-void printArray(void);
+int num_threads_to_create = 6;
 
-long counter;
-C2Mutex myMutex;
-C2Semaphore mySema;
+void doFunc(void*);
 
-const int NUMTHREADS = 8;
-C2Barrier myBarrier(1);
-
-///---//  Beginning of main
-int main()
+int main(void)
 {
-    cout << "Hello there\n\n" << flush;
-    cout << "Start:\n\n";
+   vjTSObjectProxy<int>    tsCounter;     // Allocate a thread specific counter
 
-    counter = 0;
-    cout << "Counter: " << counter << endl;
-    
-    
-//-------------------------------------------------
-	    // Timing variables
-    struct timeval startTime, endTime;		    // Used to calculate time
-    double startSecond, endSecond, totalSeconds;    // performance
-    
-    gettimeofday(&startTime, NULL);	    // Get the starting time
-//--------------------------------------------------
-    
-    for (float i=0;i<NUMTHREADS;i++) {
-	//sprocFunc(doIt,NULL,NULL);
-	C2Thread::spawn(doIt);      
-    }
-    myBarrier.wait();
-//--------------------------------------------------------
-    gettimeofday(&endTime, NULL);	    // Get ending time
-    startSecond = (startTime.tv_sec + (startTime.tv_usec * 0.000001));
-    endSecond = endTime.tv_sec + (endTime.tv_usec * 0.000001);
-    totalSeconds = (endSecond - startSecond);
-    
-    cout << "\nDone: It took... " << totalSeconds << " seconds" << flush;
-//---------------------------------------------------------
-    
-    usleep(33333300);
-    cout << "\n\nCounter: " << counter << endl << flush;
-    
-    cout << "End:\n\n";
-    cout << "\n" << flush;
-    
-     float x = 123.45;
-     
-//     while(1)
-//	x = x*-1;
-    
-    // -- Clean up --- //
-    //    delete mySemaphore;
-//    delete myMutex;    
+   vjThread* new_thread = new vjThread(doFunc, (void*)(&tsCounter));
 
-    return 0;
+
+   while(1)
+   {
+      ;
+   }
 }
 
-void doIt(void* param)
+
+void doFunc(void* TSCounterPtr)
 {
-///---//  Beginning of doIt
-    float x = 1000.0034;
-//    for(long k=0;k<10000;k++)
-//	x = (k*x)/(3.14*k);
-    myBarrier.addProcess();
-    //mySemaphore->acquire();
-    //myMutex.acquire();
-    mySema.acquire();
-	cerr << "\n-------------------------------------------\n"
-	     << "\tMy Id: " << C2Thread::self() << endl;
-	counter++;
-	cerr << "Counter now: " << counter << endl << flush;
-    mySema.release();
-    //myMutex.release();
-    //mySemaphore->release();
-    myBarrier.wait();
-    
-//    while(1)
-//	x = x*-1;
-	
+   vjTSObjectProxy<int>&  ts_counter = *(static_cast<vjTSObjectProxy<int>*>(TSCounterPtr));
+
+   num_threads_to_create -= 1;
+   if(num_threads_to_create > 0)
+      vjThread* new_thread = new vjThread(doFunc, TSCounterPtr);
+   if(num_threads_to_create > 0)
+      vjThread* new_thread2 = new vjThread(doFunc, TSCounterPtr);
+   if(num_threads_to_create > 0)
+      vjThread* new_thread3 = new vjThread(doFunc, TSCounterPtr);
+
+   (*ts_counter) = 0;
+   vjDEBUG(0) << "Starting with: " << *ts_counter << endl << flush << vjDEBUG_FLUSH;
+
+   while((*ts_counter) < 1000)
+   {
+      for(int i=0;i<1000;i++)
+         float x = sin(i);
+      (*ts_counter)++;
+   }
+
+   vjDEBUG(0) << "Finished counting: Final val:" << *ts_counter << endl << vjDEBUG_FLUSH;
 }
 
