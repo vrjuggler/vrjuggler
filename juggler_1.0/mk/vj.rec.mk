d278d922ce275a090c925fe58dc70862c0d0fd1e
Patrick Hartling <patrick.hartling@gmail.com>
1998-10-14 03:45:40 +0000

    Build the Performer and OpenGL API code in separate libraries.  They are named libJuggler_pf and libJuggler_ogl respectively.
    The recursive build process has changed slightly to accomodate this.
    ${OBJDIR} is now overridden by a ${BASE_OBJDIR} variable that gives the
    top-level directory where the object files are built.  This allows a
    subdirectory to be appended to provide for object files to be built in
    separate directories.  This is how the GL and Performer APIs are
    separated from the rest of the VR Juggler library.
    
    Libraries are now built using common 'static-lib' and 'shared-lib' targets.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@253 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/mk/vj.rec.mk
+++ juggler_1.0/mk/vj.rec.mk
@@ -6,17 +6,18 @@
 # -----------------------------------------------------------------------------
 # The Makefile including this file should define the following variables:
 #
-# OBJDIR    - The directory to which the object file(s) will be written.
-# OPTIMIZER - Optimizer flag(s) for the compiler.
-# RECTARGET - The name of the recursive target to build.
-# SUBDIRS   - The subdirectories over which the recursion will be done.
+# BASE_OBJDIR - The directory to which the object file(s) will be written.
+# OPTIMIZER   - Optimizer flag(s) for the compiler.
+# RECTARGET   - The name of the recursive target to build.
+# SUBDIRS     - The subdirectories over which the recursion will be done.
 # -----------------------------------------------------------------------------
 
 recursive:
 	@amf=$$2; list='${SUBDIRS}';					\
             for subdir in $$list; do					\
                 (cd $$subdir && ${MAKE} OPTIMIZER="${OPTIMIZER}"	\
-                                    OBJDIR=${OBJDIR} ${RECTARGET})	\
+                                    BASE_OBJDIR="${BASE_OBJDIR}"	\
+                                    ${RECTARGET})                        \
                     || case "$$amf" in					\
                          *=*) exit 1;;					\
                          *k*) fail=yes;;				\
