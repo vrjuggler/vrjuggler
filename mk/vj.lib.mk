f10bc680dbc87845cf15198a4a68d2b759334ab2
Patrick Hartling <patrick.hartling@gmail.com>
1998-10-14 03:45:40 +0000

    Build the Performer and OpenGL API code in separate libraries.  They are named libJuggler_pf and libJuggler_ogl respectively.
    The recursive build process has changed slightly to accomodate this.
    ${OBJDIR} is now overridden by a ${BASE_OBJDIR} variable that gives the
    top-level directory where the object files are built.  This allows a
    subdirectory to be appended to provide for object files to be built in
    separate directories.  This is how the GL and Performer APIs are
    separated from the rest of the VR Juggler library.
    
    Libraries are now built using common 'static-lib' and 'shared-lib' targets.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@253 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ mk/vj.lib.mk
@@ -0,0 +1,21 @@
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# This include file <vj.lib.mk> handles compiling of libraries (both static
+# and shared) from compiled object files.
+# -----------------------------------------------------------------------------
+# The Makefile including this file should define the following variables:
+#
+# AR      - Path to ar(1) (or equivalent program).
+# LD      - Path to ld(1) (or equivalent program).
+# LDOPTS  - Extra, often platform-specific, options for the linker.
+# LIBDIR  - The directory where the library will be made.
+# LIBNAME - The name of the library to generate.
+# OBJS    - The object files to be compiled into the named library.
+# -----------------------------------------------------------------------------
+
+static-lib:
+	cd ${LIBDIR} && ${AR} ${LIBNAME} ${OBJS}
+
+shared-lib:
+	cd ${LIBDIR} && ${LD} ${LDOPTS} -o ${LIBNAME} ${OBJS}
