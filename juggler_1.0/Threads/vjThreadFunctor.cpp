36af6a81d2528d86f5b86345b1cc05de2b64109e
Patrick Hartling <patrick.hartling@gmail.com>
1998-11-04 22:04:45 +0000

    Clean up a bunch of little formatting inconsistencies, typos and other bits that should have been removed from this file a long time ago.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@272 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/Threads/vjThreadFunctor.cpp
+++ juggler_1.0/Threads/vjThreadFunctor.cpp
@@ -2,7 +2,7 @@
 
 /*
  * ---------------------------------------------------------------------------
- * ThreadFunctor.C
+ * vjThreadFunctor.C
  * $Revision$
  * $Date$
  * ---------------------------------------------------------------------------
@@ -14,40 +14,39 @@
 
 //--------------------------------------------
 // This is the actual function that is called.
-// It must be extern "C"
 //--------------------------------------------
 #ifdef VJ_SGI_IPC   // ---- SGI IPC Barrier ------ //
-    extern "C" void 
+    void 
     ThreadFunctorFunction (void* args) {
-         vjBaseThreadFunctor& func = *( vjBaseThreadFunctor*)args;
+        vjBaseThreadFunctor& func = *(vjBaseThreadFunctor*)args;
         func();	// Call the functor's operator ()
     }
 #else
 #ifdef WIN32
     unsigned int __stdcall 
     ThreadFunctorFunction (void* args) {
-          vjBaseThreadFunctor& func = *( vjBaseThreadFunctor*)args;
+         vjBaseThreadFunctor& func = *(vjBaseThreadFunctor*)args;
          func();
          return 0;
     }
 #else
 #ifdef VJ_USE_PTHREADS
-        extern "C" void
 #   ifdef _PTHREADS_DRAFT_4
-        ThreadFunctorFunction (void* args) {
+        void
 #   else
-        *ThreadFunctorFunction (void* args) {
+        void*
 #   endif
-             vjBaseThreadFunctor& func = *( vjBaseThreadFunctor*)args;
+        ThreadFunctorFunction (void* args) {
+            vjBaseThreadFunctor& func = *(vjBaseThreadFunctor*)args;
             func();
         }
 
 #else
-    extern "C" void 
+    void 
     ThreadFunctorFunction (void* args) {
-         vjBaseThreadFunctor& func = *( vjBaseThreadFunctor*)args;
+        vjBaseThreadFunctor& func = *(vjBaseThreadFunctor*)args;
         func();
     }
-#endif	/* CS_USE_PTHREADS */
+#endif	/* VJ_USE_PTHREADS */
 #endif	/* WIN32 */
 #endif	/* VJ_SGI_IPC */
