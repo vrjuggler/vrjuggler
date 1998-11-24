9eac99b1e420fb666cd7c4c87a319955ccca9da8
Patrick Hartling <patrick.hartling@gmail.com>
1998-11-24 16:43:31 +0000

    This is a generic file that includes vjDefines.h if HAVE_CONFIG_H is defined.  It can be used for including other files common to most, if not all, source files in the VR Juggler source tree.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@317 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/vrj/vrjConfig.h
@@ -0,0 +1,21 @@
+#ifndef _VJ_CONFIG_H_
+#define _VJ_CONFIG_H_
+
+/*
+ * ----------------------------------------------------------------------------
+ * This file (vjConfig.h) includes header files common to most, if not all,
+ * files in the VR Juggler source tree.  It must be included at the top of
+ * every .h and .cpp file before any other headers because it includes system
+ * headers.
+ *
+ * $Id$
+ * ----------------------------------------------------------------------------
+ */
+
+/* This should always be included first. */
+#ifdef HAVE_CONFIG_H
+#include <vjDefines.h>
+#endif
+
+
+#endif	/* _VJ_CONFIG_H_ */
