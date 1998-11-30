a35358bd4e35e4cc4211c3dd63eca26b9de3e457
Allen Bierbaum <abierbaum@gmail.com>
1998-11-30 19:12:20 +0000

    renamed
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@349 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- Performance/vjPerfDataBuffer.C
+++
@@ -1,2 +0,0 @@
-
-#include <Performance/vjPerfDataBuffer.h>
--- Sync/vjSemaphoreSGI.C
+++
@@ -1,7 +0,0 @@
-#include <vjConfig.h>
-
-#include <Sync/vjSemaphore.h>
-#include <Sync/vjSemaphoreSGI.h>
-
-vjMemPoolSGI* vjSemaphoreSGI::semaphorePool = NULL;
-int* vjSemaphoreSGI::attachedCounter = NULL;
