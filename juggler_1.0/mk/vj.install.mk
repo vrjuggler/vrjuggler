21c48fa10d3d0bf56e10e6439db166327671fcea
Patrick Hartling <patrick.hartling@gmail.com>
1999-01-29 20:54:06 +0000

    Made the 'install' target more flexible.  Users can now optionally define ${INSTALL_FILES} to list the files that are to be installed and ${INSTALL_PATH} to give the path where the files are installed.  If these variables are not defined, they default to the values ${srcdir}/*.h and ${includedir} respectively.  This provides backwards compatability with the previous behavior and prevents the need to update all the Makefiles that include this file.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@414 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/mk/vj.install.mk
+++ juggler_1.0/mk/vj.install.mk
@@ -6,16 +6,22 @@
 # -----------------------------------------------------------------------------
 # The Makefile including this file must define the following variables:
 #
-# includedir - The installation directory for the header files.
-# srcdir     - The directory where the source file(s) is/are located.
+# includedir - The installation directory for the header files.  This is
+#              only needed if the default value for ${INSTALL_PATH} is to be
+#              used.
+# srcdir     - The directory where the source file(s) is/are located.  This is
+#              only needed if the default value for ${INSTALL_FILES} is to be
+#              used.
 # INSTALL    - The path to a BSD-compatible install program.
 # -----------------------------------------------------------------------------
 # Optionally, the including file can add to the following existing variables
 # to provide additional local 'install':
 #
-# INSTALL_DEPS - Dependencies for the 'install' target.
-# FILE_PERMS   - Permissions on normal (non-executable) files.
-# GROUP_NAME   - The name of the group that will own the files.
+# INSTALL_DEPS  - Dependencies for the 'install' target.
+# INSTALL_FILES - The file(s) to be installed.
+# INSTALL_PATH  - The path to which the file(s) will be installed.
+# FILE_PERMS    - Permissions on normal (non-executable) files.
+# GROUP_NAME    - The name of the group that will own the files.
 # -----------------------------------------------------------------------------
 
 ifndef FILE_PERMS
@@ -28,5 +34,14 @@ endif
 
 INSTALL_DEPS	?=
 
+ifndef INSTALL_FILES
+    INSTALL_FILES	= ${srcdir}/*.h
+endif
+
+ifndef INSTALL_PATH
+    INSTALL_PATH	= ${includedir}
+endif
+
 install: ${INSTALL_DEPS}
-	${INSTALL} -m ${FILE_PERMS} -g ${GROUP_NAME} ${srcdir}/*.h ${includedir}
+	${INSTALL} -m ${FILE_PERMS} -g ${GROUP_NAME} ${INSTALL_FILES}	\
+            ${INSTALL_PATH}
