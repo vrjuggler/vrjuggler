af1574b36663037385e96f21b07d5c648df4864c
Christopher Just <coronax@users.sourceforge.net>
1999-02-03 21:28:03 +0000

    *** empty log message ***
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@433 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler_1.0/GUI/VjGUI/configchunk/configchunk.mk
@@ -0,0 +1,18 @@
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# This include file <configchunk.mk> defines the list of .java files that are built
+# into Java bytecode with javac.
+# -----------------------------------------------------------------------------
+# Variables defined here are:
+#
+# VJGUICONFIGCHUNK_SOURCES - The list of .java files to build
+# VJGUICONFIGCHUNK_OBJS    - The list of .class files that will be created.
+# -----------------------------------------------------------------------------
+
+VJGUICONFIGCHUNK_SOURCES = VjGUI/configchunk/ConfigChunkFrame.java 	\
+			   VjGUI/configchunk/ListBoxModel.java          \
+			   VjGUI/configchunk/PropertyPanel.java		\
+			   VjGUI/configchunk/VarValuePanel.java
+
+VJGUICONFIGCHUNK_OBJS = ${VJGUICONFIGCHUNK_SOURCES:.java=.class}
