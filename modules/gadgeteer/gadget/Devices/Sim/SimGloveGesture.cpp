85099bc550be6b0086a35954746099203dd29105
Allen Bierbaum <abierbaum@gmail.com>
1999-02-04 05:40:14 +0000

    Added simInput and Gesture stuff
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@443 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Devices/Sim/SimGloveGesture.cpp
@@ -0,0 +1,75 @@
+#include <Input/vjSim/vjSimGloveGesture.h>
+#include <iostream.h>
+#include <fstream.h>
+
+//: Construct the vjSimGloveGesture
+// -Set the keyboard key pairs <b>
+// -Load the sample file
+// -Trim the smallest so they are same length
+vjSimGloveGesture::vjSimGloveGesture(vjConfigChunk* chunk)
+   : vjGesture(chunk), vjGlove(chunk), vjSimInput(chunk)
+{
+   mCurGesture = 0;     // We are in no gesture yet
+
+   // Get keyboard pairs
+   int num_keys = chunk->getNum("keys");
+   int num_mods = chunk->getNum("modKeys");
+   int num_pairs = ( (num_keys < num_mods) ? num_keys : num_mods);   // Trim to smaller value
+
+   // Fill the key pairs
+   for(int i=0;i<num_pairs;i++)
+   {
+      vjKeyModPair key_pair;
+      key_pair.mKey      = (int)chunk->getProperty("keys",i);
+      key_pair.mModifier = (int)chunk->getProperty("modKeys",i);
+      mSimKeys.push_back(key_pair);
+   }
+
+   // Get sample filename
+   string sample_file = chunk->getProperty("trainedFilename");
+   loadTrainedFile(sample_file);
+
+   // Trim the lengths
+   int num_gestures = getNumGestures();
+   while(num_gestures < mSimKeys.size())     // If we have to many keys
+   {
+      mSimKeys.pop_back();
+      vjDEBUG(0) << "vjSimGloveGesture: Not enough gestures. Trimming" << endl << vjDEBUG_FLUSH;
+   }
+}
+
+//: Get the current gesture.
+//! RETURNS: id of current gesture
+int vjSimGloveGesture::getGesture()
+{ return mCurGesture; }
+
+//: Update the device data
+// -Get the gesture id
+// -Set the glove params
+void vjSimGloveGesture::UpdateData()
+{
+   // Get the current gesture
+   for(int i=0;i<mSimKeys.size();i++)
+   {
+      if(checkKeyPair(mSimKeys[i]) > 0)
+      {
+         mCurGesture = i;
+         vjDEBUG(0) << "vjSimGloveGesture: Got gesture: " << getGestureString(mGestureId) << endl << vjDEBUG_FLUSH;
+      }
+   }
+
+   // Set the glove to the sample
+   mTheData[0][current] = mGestureExamples[mCurGesture];    // Copy over the example
+   mTheData[0][current].calcXforms();                       // Update the xform data
+}
+
+
+//: Load trained data for the gesture object
+// Loads the file for trained data
+void vjSimGloveGesture::loadTrainedFile(string fileName)
+{
+   ifstream inFile(fileName);
+
+   this->loadFileHeader(inFile);
+}
+
--- modules/gadgeteer/gadget/Devices/Sim/SimGloveGesture.h
+++ modules/gadgeteer/gadget/Devices/Sim/SimGloveGesture.h
@@ -1,73 +1,46 @@
+#ifndef _VJ_SIM_GLOVE_GESTURE_H_
+#define _VJ_SIM_GLOVE_GESTURE_H_
 
-class vjSimGloveGesture : public vjGloveGesture, public vjGlove
+
+//: Simulated glove class
+//
+// This class simulates a gesture input device
+//
+// By default the glove is in gesture 0
+class vjSimGloveGesture : public vjGesture, public vjGlove, public vjSimInput
 {
 public:
-
+   //: Construct a vjSimGloveGesture
    vjSimGloveGesture(vjConfigChunk* chunk);
 
-   //: This actually starts the training on the given data.
-   virtual void train() {
-   }
    //: Get the current gesture.
    //! RETURNS: id of current gesture
-   virtual int getGesture() {
-   }
-   //: Adds a new sample of the given gesture to the training data.
-   virtual void addSample(int gestureId) {
-   }
-   //: Loads the sample training data specified.
-   // This file contains previous samples for the gesture recognizer to train from.
-   virtual void loadSamplesFile(string filename) {
-   }
-   //: Clear the samples that we have taken so far.
-   //! ARGS: gestureId = -1 (default) then we clear all gestures else we clear only
-   // the gesture that is specified.
-   virtual void clearSamples(int gestureId=-1) {
-   }
+   virtual int getGesture();
+
    //: Load trained data for the gesture object
-   virtual void loadTrainedFile(string fileName) {
-   }
-   //: Save a trained data file for the gesture object
-   virtual void saveTrainedFile(string fileName) {
-   }
-   //: Saves the sample training data specified.
-   // This data can be loaded at a later time to do more sample training.
-   virtual void saveSamplesFile(string filename) {
-   }
-   //: StopSampling.
-   //
-   //  Reverse the effects of StartSampling()
-   virtual int StopSampling() {
-   }
-   //: Sample the device
-   //
-   //  Every input device should have a sample function, after which the
-   //  device has been sampled to have new data.  (This new data is not
-   //  accessable until UpdateData is called, however)
-   virtual int Sample() {
-   }
-   //: Start a device sampling.
-   //
-   //  Start the device sampling, normally this will spawn a thread which will
-   //  just repeatedly call Sample().
-   //  This function should return true when it sucessfully starts,
-   //      false otherwise.
-   virtual int StartSampling() {
-   }
-   //: UpdateData()
-   //
-   //  After this function is called subsequent calls to GetData(d) will
-   //  return the most recent data at the time of THIS function call.  Data is
-   //  guaranteed to be valid and static until the next call to UpdateData.
-   virtual void UpdateData() {
-   }
-   //: GetDevicename()
-   //
-   //  Returns the name identifying the TYPE of Input Device
-   virtual char* GetDeviceName() {
-   }
+   // Loads the file for trained data
+   void loadTrainedFile(string fileName);
+
+   int StartSampling() { return 1; }
+   int StopSampling() { return 1; }
+   int Sample() { return 1; }
+
+   //: Update the device data
+   void UpdateData ();
+
+   //: Return our name
+   char* GetDeviceName() { return "vjSimGloveGesture"; }
+
+   /*** These are not supported in sim ***/
+   void saveTrainedFile(string fileName) { ;}
+   void loadSamplesFile(string filename) {;}
+   void saveSamplesFile(string filename) {;}
+   void clearSamples(int gestureId=-1) {;}
+   void addSample(int gestureId) {;}
+   void train() { ; }
 
 private:
-
+   vector<vjKeyModPair>    mSimKeys;      //: The keys to press for the gestures
+   int                     mCurGesture;   //: The current gesture id
 };
 
