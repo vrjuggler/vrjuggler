d0c59574d9879ba4fe7a8240f15dfaf2dbb24588
Allen Bierbaum <abierbaum@gmail.com>
1999-02-08 20:44:29 +0000

    Initial version of interface.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@462 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/gadgeteer/gadget/Type/GloveInterface.h
@@ -0,0 +1,22 @@
+#ifndef _VJ_GLOVE_INTERFACE_H_
+#define _VJ_GLOVE_INTERFACE_H_
+
+#include <Kernel/vjKernel.h>
+#include <Input/InputManager/vjInputManager.h>
+#include <Input/InputManager/vjGloveProxy.h>
+#include <Input/InputManager/vjDeviceInterface.h>
+
+//: Provides a simplified user interface to an glove device
+//
+// See also: vjDeviceInterface
+class vjGloveInterface : public vjDeviceInterface
+{
+public:
+   vjGloveProxy* operator->()
+   { return vjKernel::instance()->getInputManager()->GetGloveProxy(mProxyIndex); }
+
+   vjGloveProxy& operator*()
+   { return *(vjKernel::instance()->getInputManager()->GetGloveProxy(mProxyIndex)); }
+};
+
+#endif
