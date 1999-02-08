d1be5f89dc3f6cabf1ff76f2b293fcd68699199a
Allen Bierbaum <abierbaum@gmail.com>
1999-02-08 20:44:29 +0000

    Initial version of interface.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@462 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Type/GestureInterface.h
@@ -0,0 +1,22 @@
+#ifndef _VJ_GESTURE_INTERFACE_H_
+#define _VJ_GESTURE_INTERFACE_H_
+
+#include <Kernel/vjKernel.h>
+#include <Input/InputManager/vjInputManager.h>
+#include <Input/InputManager/vjGestureProxy.h>
+#include <Input/InputManager/vjDeviceInterface.h>
+
+//: Provides a simplified user interface to an gesture device
+//
+// See also: vjDeviceInterface
+class vjGestureInterface : public vjDeviceInterface
+{
+public:
+   vjGestureProxy* operator->()
+   { return vjKernel::instance()->getInputManager()->GetGestureProxy(mProxyIndex); }
+
+   vjGestureProxy& operator*()
+   { return *(vjKernel::instance()->getInputManager()->GetGestureProxy(mProxyIndex)); }
+};
+
+#endif
