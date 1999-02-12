bdc4edd83f8664b55ec611b475129d98b8b3695e
Allen Bierbaum <abierbaum@gmail.com>
1999-02-12 06:46:42 +0000

    Moved  common  includes to vjConfig.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@496 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- vjConfig.h
+++ vjConfig.h
@@ -26,4 +26,16 @@
 #include <fstream.h>
 #endif
 
+/* --- Macros ---- */
+#define VJ_DEG2RAD(x) ((x)*M_PI/180.0)
+#define VJ_RAD2DEG(x) ((x)*180.0/M_PI)
+#define VJ_ZERO_CLAMP(x) ((fabs(x) < 1e-15)? 0.0f : x)
+#define VJ_CLAMP(x,y) ((x>y)? y : x)
+#define VJ_MIN2(x,y) ((x>y)? y : x)
+#define VJ_MIN3(x,y,z) VJ_MIN2(VJ_MIN2(x,y),z)
+#define VJ_MIN4(w,x,y,z) VJ_MIN2(VJ_MIN2(w,x), VJ_MIN2(y,z))
+#define VJ_MAX2(x,y) ((x>y)? x : y)
+#define VJ_MAX3(x,y,z) VJ_MAX2(VJ_MAX2(x,y),z)
+#define VJ_MAX4(w,x,y,z) VJ_MAX2(VJ_MAX2(w,x),VJ_MAX2(y,z))
+
 #endif	/* _VJ_CONFIG_H_ */
