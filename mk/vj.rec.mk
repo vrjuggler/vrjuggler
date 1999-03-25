9b3f29b964f61e30c4b773db05a53e521746dba3
Patrick Hartling <patrick.hartling@gmail.com>
1999-03-25 22:06:22 +0000

    Changed the 'recursive' target so that the directories over which the recursion is done are entered in parallel.  This represents a big change into how the 'recursive' target is handled because it now uses only built-in GNU make features to implement recursion.  There is no manual looping of any kind done now, thus making the code for the recursion a lot shorter and easier to look at.
    Comments have also been added and updated to reflect the changes made in
    this revision.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@602 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- mk/vj.rec.mk
+++ mk/vj.rec.mk
@@ -4,7 +4,16 @@
 # This include file <vj.rec.mk> handles recursive builds using a target called
 # 'recursive'.
 # -----------------------------------------------------------------------------
-# The Makefile including this file should define the following variables:
+# The Makefile including this file must define the following variable before
+# including it:
+#
+# SUBDIRS     - The subdirectories over which the recursion will be done.
+#               Note that these directories will be built in parallel if the
+#               -j flag is passed on the command line.
+# -----------------------------------------------------------------------------
+# The Makefile including this file should define the following variables
+# before including it, or they should be passed as arguments to the recursive
+# call to ${MAKE}:
 #
 # BASE_OBJDIR - The directory to which the object file(s) will be written.
 # DIRPRFX     - The prefix directory (if any) of the directories listed in
@@ -12,20 +21,18 @@
 #               entered.
 # OPTIMIZER   - Optimizer flag(s) for the compiler.
 # RECTARGET   - The name of the recursive target to build.
-# SUBDIRS     - The subdirectories over which the recursion will be done.
 # -----------------------------------------------------------------------------
 
-recursive:
-	@amf=$$2; list='${SUBDIRS}';					\
-            for subdir in $$list; do					\
-                echo "===> ${DIRPRFX}$$subdir" ;			\
-                (cd $$subdir && ${MAKE} OPTIMIZER="${OPTIMIZER}"	\
-                                    BASE_OBJDIR="${BASE_OBJDIR}"	\
-                                    ${RECTARGET})			\
-                    || case "$$amf" in					\
-                         *=*) exit 1;;					\
-                         *k*) fail=yes;;				\
-                         *) exit 1;;					\
-                       esac;						\
-                echo "<=== ${DIRPRFX}$$subdir" ;			\
-            done && test -z "$$fail"
+# This is defined to be a phony target since everything listed in ${SUBDIRS}
+# is a directory that is supposed to exist in the current directory.
+.PHONY: ${SUBDIRS}
+
+${SUBDIRS}:
+	@echo "===> ${DIRPRFX}$@"
+	@${MAKE} -C $@ BASE_OBJDIR="${BASE_OBJDIR}"			\
+            OPTIMIZER="${OPTIMIZER}" ${RECTARGET}
+	@echo "<=== ${DIRPRFX}$@"
+
+# Using ${SUBDIRS} as the dependencies to 'recursive' allows GNU make to
+# build them all in parallel.
+recursive: ${SUBDIRS}
