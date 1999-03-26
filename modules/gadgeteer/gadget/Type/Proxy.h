7c31d51c253f5bcf2723e7438cc7804cb01285a3
Allen Bierbaum <abierbaum@gmail.com>
1999-03-26 06:58:11 +0000

    Added a proxy base class.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@612 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Type/Proxy.h
@@ -0,0 +1,26 @@
+#ifndef _VJ_PROXY_H_
+#define _VJ_PROXY_H_
+
+#include <vjConfig.h>
+#include <Config/vjConfigChunk.h>
+
+//: Base class for all proxies
+//!PUBLIC_API
+class vjProxy
+{
+public:
+   vjProxy() {;}
+
+   //: Configure the proxy
+   //! POST:
+   //+  Proxy is configured
+   //+  Proxy is registered with input manager
+   //! RETURNS: success
+   virtual bool config(vjConfigChunk* chunk) = 0;
+
+   //: Returns the string rep of the chunk type used to config this device
+   // Used by input manager to find chunks that construct devices
+   static string getChunkType() { return "Undefined"; }
+};
+
+#endif
