38e1f75f0a65551fcd55dc8eabfdc5a2749efe5a
Allen Bierbaum <abierbaum@gmail.com>
1999-03-29 17:10:28 +0000

    Added run-time config setup for display manager.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@629 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- Kernel/vjDisplayManager.h
+++ Kernel/vjDisplayManager.h
@@ -22,6 +22,7 @@ class vjDisplay;
 #include <Input/vjPosition/vjPosition.h>
 #include <Kernel/vjDebug.h>
 #include <Input/InputManager/vjPosInterface.h>
+#include <Kernel/vjConfigChunkHandler.h>
 
 //-----------------------------------------------------------------------------
 //: Singleton Container class for all vjDisplays.
@@ -36,26 +37,56 @@ class vjDisplay;
 // @author Allen Bierbaum
 //  Date: 9-7-97
 //-----------------------------------------------------------------------------
-class vjDisplayManager
+class vjDisplayManager : public vjConfigChunkHandler
 {
 public:
-   //
-   virtual int addDisplay(vjDisplay* disp, int notifyDrawMgr = 0);
-
-   virtual int closeDisplay(int dispId);
-
-   virtual vjDisplay* getDisplay(int dispId);
-
    //: This function updates the projections for all contained displays.
    virtual void updateProjections();
 
+   //: Set the draw manager that the system is running
+   // We need to know this in order to notify the draw
+   // manager of any display changes
    void setDrawManager(vjDrawManager* drawMgr);
 
+   //: Return a list of the current displays
+   //! NOTE: DO NOT EDIT THE DISPLAYS
+   vector<vjDisplay*> getDisplays()
+   { return mDisplays;}
+
+public:     // --- Config stuff -- //
+   //: Add the chunk to the configuration
+   //! PRE: configCanHandle(chunk) == true
+   //! RETURNS: success
+   virtual bool configAdd(vjConfigChunk* chunk);
+
+   //: Remove the chunk from the current configuration
+   //! PRE: configCanHandle(chunk) == true
+   //!RETURNS: success
+   virtual bool configRemove(vjConfigChunk* chunk);
+
+   //: Can the handler handle the given chunk?
+   //! RETURNS: true - Can handle it
+   //+          false - Can't handle it
+   virtual bool configCanHandle(vjConfigChunk* chunk);
+
+private:
+   //: Add a display to the current system
+   //! PRE: Draw manager is known
+   //! POST: disp has been added to the list of displays
+   //+  (notifyDrawMgr == true) ==> Draw manager now has been given new window to display
+   virtual int addDisplay(vjDisplay* disp, bool notifyDrawMgr = true);
+
+   //: Close the given display
+   virtual int closeDisplay(int dispId);
+
+   //: Get the display information for the display
+   virtual vjDisplay* getDisplay(int dispId);
+
 public:
-   vector<vjDisplay*>  displays;    //: List of displays currently operating
+   vector<vjDisplay*>  mDisplays;    //: List of displays currently operating
 
 protected:
-   vjDrawManager*  drawManager;     //: The current drawManager to communicate with
+   vjDrawManager*  mDrawManager;     //: The current drawManager to communicate with
 
 
    // ---- Singleton stuff ---- //
