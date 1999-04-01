c1544f52c7e793346d0ab3ade628c2444f5314b9
Kevin Meinert <subatomic@users.sourceforge.net>
1999-04-01 21:37:25 +0000

    added speech stuff, and socket stuff to run the speech stuff
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@661 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Type/Speech.h
@@ -0,0 +1,97 @@
+class vjSpeech
+{
+public:
+	//: Construct using chunk
+	vjSpeech();
+
+	//: Destructor
+	~vjSpeech();
+	
+	virtual std::string		getWord();
+	virtual int			getId();
+	virtual void			setMode( Mode mode );
+	virtual std::list<asdfjlk>&	getList();
+	virtual void			config();
+
+	enum Mode
+	{
+		DICTATE,COMMAND,ON,OFF
+	};
+
+
+	
+	class asdfjlk
+	{
+		std::string word;
+		int id;
+	};
+	
+	virtual char* GetDeviceName() { return "vjSpeech";}
+
+       virtual int StartSampling() = 0;
+       virtual int StopSampling() = 0;
+       virtual int Sample() = 0;
+       virtual void UpdateData () = 0;
+    
+protected:
+	std::list<asdfjlk>	_wordQueue;
+	Mode				_currentMode;
+
+	// Config chunck sets these.
+	std::string			_filename;
+	std::string			_dragonIP;
+	int					_dragonPort;
+	std::string			_user;
+
+protected:
+	void				startThread();
+	static void			controlLoop(vjSpeech& currentInstance);
+};
+
+
+inline std::string vjSpeech::getWord()
+{
+	std::string word = "";
+
+	if (_wordQueue.size > 0)
+	{
+		word = (*_wordQueue.begin()).word;
+		_wordQueue.pop_front();
+	}
+
+	return word;
+}
+
+inline int vjSpeech::getId()
+{
+	int id = -1;
+
+	if (_wordQueue.size > 0)
+	{
+		id = (*_wordQueue.begin()).id;
+		_wordQueue.pop_front();
+	}
+
+	return id;
+}
+
+inline void vjSpeech::setMode( Mode mode )
+{
+	_mode = mode;
+	// TODO: tell dragon about the mode change.
+}
+
+inline std::list<asdfjlk>& vjSpeech::getList()
+{
+	return _wordQueue;
+}
+
+//TODO: take a config chunck here.
+inline void vjSpeech::config()
+{
+	_filename = "somefile.vjs";
+	_dragonIP = "129.186.232.70";
+	_dragonPort = 5600;
+	_user = "kevin";
+}
+
