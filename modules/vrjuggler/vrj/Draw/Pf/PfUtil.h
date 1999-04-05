c4c1feca2ed241f5ce103202934560db2df868da
Patrick Hartling <patrick.hartling@gmail.com>
1999-04-05 20:20:27 +0000

    Added file vjPfUtil.cpp to hold the definitions of vjGetVJMatrix() and vjGetPfMatrix().  This prevents multiply defined symbols.  vjConfig.h, Performer/pr/pfLinMath.h and Math/vjMatrix.h are now included by vjPfUtil.h to ensure that what the functions need is defined.
    -DPF_CPLUSPLUS_API has been added to ${DEPENDFLAGS} in the Makefile so
    that no warnings will be spit out by makedepend(1).
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@665 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/Pf/PfUtil.h
+++ modules/vrjuggler/vrj/Draw/Pf/PfUtil.h
@@ -5,33 +5,17 @@
 * Performer helper routines                       *
 ***************************************************/
 
-vjMatrix vjGetVJMatrix(pfMatrix& perfMat);
-pfMatrix vjGetPfMatrix(vjMatrix& mat);
+#include <vjConfig.h>
+
+#include <Performer/pr/pfLinMath.h>
+#include <Math/vjMatrix.h>
 
 
 //: Convert Performer matrix to Juggler matrix
-vjMatrix vjGetVJMatrix(pfMatrix& perfMat)
-{
-   vjMatrix mat;
-   vjVec3 x_axis(1,0,0);
-   mat.set(&(perfMat.mat[0][0]));
-   mat.preRot(90, x_axis, mat);
-   mat.postRot(mat, -90, x_axis);
-
-   return mat;
-}
+vjMatrix vjGetVJMatrix(pfMatrix& perfMat);
 
 //: Convert Juggler Matrix to Pf Matrix
-pfMatrix vjGetPfMatrix(vjMatrix& mat)
-{
-   pfMatrix perf_mat;
-
-   perf_mat.set(mat.getFloatPtr());
-   perf_mat.preRot(-90, 1, 0, 0, perf_mat);
-   perf_mat.postRot(perf_mat, 90, 1, 0, 0);
-
-   return perf_mat;
-}
+pfMatrix vjGetPfMatrix(vjMatrix& mat);
 
 
 #endif
