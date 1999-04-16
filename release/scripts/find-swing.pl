9da28a643b015c24dd69b0f2afa7fcf98db022d6
Patrick Hartling <patrick.hartling@gmail.com>
1999-04-16 16:37:45 +0000

    Initial version of a script that searches the user's $CLASSPATH environment variable looking for a copy of swingall.jar.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@721 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ release/scripts/find-swing.pl
@@ -0,0 +1,37 @@
+#!/usr/local/bin/perl
+
+# -----------------------------------------------------------------------------
+# $Id$
+# -----------------------------------------------------------------------------
+# This is a really simple script that searches the users $CLASSPATH
+# environment variable looking for swingall.jar.  Once it finds it, it prints
+# out the full path of the file.  If it is not found in $CLASSPATH, it falls
+# back on a known location (Chris' Swing directory in his home directory).  If
+# that file is not readable, the script bombs out with error status.
+#
+# Usage:
+#     find-swing.pl
+# -----------------------------------------------------------------------------
+
+use Env('CLASSPATH');
+
+my $fallback = "/home/users/cjust/Swing/swingall.jar";
+my(@dirs) = split(/:/, "$CLASSPATH");
+
+my $dir = '';
+
+foreach $dir ( @dirs ) {
+    if ( $dir =~ /swingall\.jar$/ && -r "$dir" ) {
+	print "$dir\n";
+	exit 0;
+    }
+}
+
+if ( -r "$fallback" ) {
+    print "$fallback\n";
+    exit 0;
+}
+
+print STDERR "$0: swingall.jar not found in $CLASSPATH or in $fallback\n";
+
+exit 1;
