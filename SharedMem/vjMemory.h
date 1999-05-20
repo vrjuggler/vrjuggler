94a4e10db967030a47e17766743e23110ea5e16a
Patrick Hartling <patrick.hartling@gmail.com>
1999-05-20 01:29:13 +0000

    Converted comments over to PERCEPS style.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@831 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- SharedMem/vjMemory.h
+++ SharedMem/vjMemory.h
@@ -14,20 +14,18 @@ class vjMemory;
 // -- Just allocate to much memory
 // -- Then move pointer over by siezeof(memPool)
 
-/**
- * vjMemory
- *
- * Purpose:
- *	Base Class for all objects that want memory abilities
- *	This class allows object to be allocated/deallocated
- *      from memory "pools".  This class takes care of the new
- *	and delete as well as tracking the pool.
- *
- * Author:
- *	Allen Bierbaum
- *
- * Date: 1-9-97
- */
+// vjMemory
+//
+// Purpose:
+//:	Base Class for all objects that want memory abilities.
+//	This class allows object to be allocated/deallocated
+//      from memory "pools".  It takes care of the new and delete
+//	as well as tracking the pool.
+//
+// Author:
+//	Allen Bierbaum
+//
+// Date: 1-9-97
 class vjMemory {
 public:
     vjMemory()
@@ -35,24 +33,19 @@ public:
 	;
     }
 public:
-    /** operator new(size)
-     *
-     *  allows access to be allocate on default heap
-     *  rather than the memPool
-     */
+    // -----------------------------------------------------------------------
+    //: Allows access to be allocate on default heap rather than the memPool.
+    // -----------------------------------------------------------------------
     void* operator new(size_t sz);
     
-    /** operator new(size, memPool)
-     *
-     *  allocates memory of size from the given memPool
-     *
-     */
+    // -----------------------------------------------------------------------
+    //: Allocates memory of size from the given memPool.
+    // -----------------------------------------------------------------------
     void* operator new(size_t sz, vjMemPool* mp);
     
-    /** localDelete(ptr)
-     *
-     *   same as operator delete, reverses effects of new
-     */
+    // -----------------------------------------------------------------------
+    //: Same as operator delete, reverses effects of new.
+    // -----------------------------------------------------------------------
     void localDelete(void* ptr)
     {    	    
 	if (memPool != NULL)
@@ -65,47 +58,41 @@ public:
 #endif
     }
     
-    /** delete(ptr)
-     *
-     *  reverses the effects of either new operator
-     */
+    // -----------------------------------------------------------------------
+    //: Reverses the effects of either new operator.
+    // -----------------------------------------------------------------------
     void operator delete(void* ptr)
     { 
         static_cast<vjMemory*>(ptr)->localDelete(ptr);
     }
 
-    /** getMyMemPool()
-     *
-     *    Return the memPool that the derived object is living,
-     *  this MUST be used for vjMemory derived classes to dynamically
-     *  create other vjMemory derivced classes in the form:
-     *
-     *     myClass *mc = new ( getMyMemPool() ) myClass;
-     */	
+    // -----------------------------------------------------------------------
+    //: Return the memPool that the derived object is living.
+    //  This MUST be used for vjMemory derived classes to dynamically create
+    //  other vjMemory derivced classes in the form:
+    //
+    //     myClass *mc = new ( getMyMemPool() ) myClass;
+    // -----------------------------------------------------------------------
     vjMemPool* getMyMemPool()
     {
 	return memPool;
     }
 
-    /** allocate(size)--> to be used in classes when new (getMyMemPool())
-     *                --> is wierd or you're converting from malloc
-     *
-     *  An object should use allocate to get more memory from its
-     *  memPool.
-     */
+    // -----------------------------------------------------------------------
+    //: An object should use allocate to get more memory from its memPool.
+    // -----------------------------------------------------------------------
     void* allocate(size_t size)
     {
 	return getMyMemPool()->allocate(size);
     }
     
-    /** deallocate(ptr)
-     *  pre: ptr was allocated with allocate(size) by the same
-     *       object that making this call
-     *  modifies: memPool
-     *
-     *  Memory allocated using allocate should be deallocated with
-     *  this function.
-     */
+    // -----------------------------------------------------------------------
+    //: Memory allocated using allocate should be deallocated with this
+    //+ function.
+    //
+    //! PRE: ptr was allocated with allocate(size) by the same object making
+    //+      this call.
+    // -----------------------------------------------------------------------
     void deallocate(void* ptr)
     {
 	getMyMemPool()->deallocate(ptr);
