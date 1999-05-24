85622c2f401ed001c0d81c979395e92b2ae7c7a6
Christopher Just <coronax@users.sourceforge.net>
1999-05-24 19:19:30 +0000

    major update to perfanalyzer gui. most of this is still partly broken though.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@843 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/GUI/VjPerf/GraphFrame.java
+++ juggler_1.0/GUI/VjPerf/GraphFrame.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.util.Vector;
 import VjPerf.DataLine;
 import VjPerf.PerfDataCollector;
+import VjGUI.Core;
 
 public class GraphFrame extends JFrame {
 
@@ -17,48 +18,97 @@ public class GraphFrame extends JFrame {
     float heightscale;
     JScrollPane display_pane;
     GraphCanvas canvas;
+    HorizBarCanvas horizbarcanvas;
+    int preskip, postskip;
 
-    public class GraphCanvas extends Canvas {
+    public class GraphCanvas extends JPanel {
 	public GraphFrame gf;
+	int width, height;
 	public GraphCanvas (GraphFrame _gf) {
 	    super();
 	    gf = _gf;
-	    setSize (gf.width, gf.height);
-	    //setMinimumSize (new Dimension (gf.width, gf.height));
+	    height = gf.height;
+	    width = gf.width;
+	    //setSize (width, height);
+	    //setMinimumSize (new Dimension (width, height));
+	    setPreferredSize (new Dimension (width, height));
+	    //System.out.println ("set size: " + width + ", " + height);
 	}
 	public void paint (Graphics g) {
+	    System.out.println ("paint called");
 	    g.setColor (Color.white);
-	    g.fillRect (0, 0, gf.width, gf.height);
+	    g.fillRect (0, 0, width, height);
 	    g.setColor (Color.black);
-	    int i = 0, j = 0;
-	    for (; i < gf.width; i++) {
+	    int i = preskip, j = 0;
+	    for (; i < width + preskip; i++) {
 		double f = ((DataLine)gf.dc.datalines.elementAt(i)).diffs[gf.numtouse];
 		if (!Double.isNaN(f)) {
-		    g.drawLine (j, 200, j, (int)(gf.heightscale * ((DataLine)gf.dc.datalines.elementAt(i)).diffs[gf.numtouse]));
+		    g.setColor (Color.black);
+		    g.drawLine (j, height, j, height - (int)(gf.heightscale * ((DataLine)gf.dc.datalines.elementAt(i)).diffs[gf.numtouse]));
+		    j++;
+		}
+		else {
+		    g.setColor (Color.red);
+		    g.drawLine (j, height, j, 0);
 		    j++;
 		}
 	    }
 	}
     }
 
-    public GraphFrame (PerfDataCollector _dc, int _num) {
-	super(_dc.name+_num);
 
+
+    public class HorizBarCanvas extends JPanel {
+	public GraphFrame gf;
+	int width, height;
+
+	public HorizBarCanvas (GraphFrame _gf) {
+	    super();
+	    gf = _gf;
+	    height = 25;
+	    width = gf.width;
+	    setPreferredSize (new Dimension (width, height));
+	    setMinimumSize (new Dimension (width, height));
+	}
+
+	public void paint (Graphics g) {
+	    System.out.println ("paint called");
+	    g.setColor (Color.red);
+	    g.fillRect (0, 0, width, height);
+	}
+    }
+
+
+
+    public GraphFrame (PerfDataCollector _dc, int _num, int _preskip, int _postskip) {
+	super(_dc.name+ " phase " + _num + " " + _dc.getLabelForPhase (_num));
+
+	preskip = _preskip;
+	postskip = _postskip;
 	numtouse = _num;
 	dc = _dc;
 	numsamples = dc.numsamps[numtouse];
 	max = dc.maxvals[numtouse];
 
-	setSize (500,220);
+	int winwidth = Math.min (numsamples + 40, Core.screenWidth);
+	int winheight = Math.min (370, Core.screenHeight);
 
-	width = numsamples;
-	height = 200;
-	heightscale = (float)(height/max);
+	setSize (winwidth, winheight);
 
-	getContentPane().add(display_pane = new JScrollPane (canvas = new GraphCanvas (this), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
+	width = numsamples - preskip - postskip;
+	height = winheight - 40;
+	heightscale = (float)((height-5)/max);
+
+	canvas = new GraphCanvas (this);
+	horizbarcanvas = new HorizBarCanvas (this);
+
+	getContentPane().add(display_pane = new JScrollPane (JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS));
+	display_pane.setViewportView (canvas);
+	display_pane.setRowHeaderView (horizbarcanvas);
     }
 
 }
 
 	
+
 
--- juggler_1.0/GUI/VjPerf/PerfAnalyzerPanel.java
+++ juggler_1.0/GUI/VjPerf/PerfAnalyzerPanel.java
@@ -15,10 +15,91 @@ package VjPerf;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import VjPerf.*;
 import VjGUI.FileControl;
 
 public class PerfAnalyzerPanel extends JPanel implements ActionListener {
+
+
+    private class AnomaliesButton extends JButton {
+	public PerfDataCollector collector;
+	public int phase;
+
+	public AnomaliesButton (PerfDataCollector _collector, int _phase) {
+	    super ("Anomalies");
+	    collector = _collector;
+	    phase = _phase;
+	}
+    }
+
+
+    private class GraphButton extends JButton {
+	public PerfDataCollector collector;
+	public int phase;
+
+	public GraphButton (PerfDataCollector _collector, int _phase) {
+	    super ("Graph");
+	    collector = _collector;
+	    phase = _phase;
+	}
+    }
+
+
+    private String padFloat (double f) {
+	// reformats f to a string w/ 3 places after decimal
+	String s = Double.toString(f);
+	int i = s.lastIndexOf('.');
+	if (i >= 0)
+	    s = s.substring (0, i + 3);
+	return s;
+    }
+
+    private void buildDataPanel () {
+	int i,j;
+	PerfDataCollector col;
+	JLabel l;
+	JButton b;
+	double avg;
+	data_panel.removeAll();
+	Insets insets = new Insets (1,1,1,1);
+
+	for (i = 0; i < collection.getSize(); i++) {
+	    col = collection.getCollector(i);
+	    col.generateAverages(preskip, postskip);
+	    gbc.gridwidth = gbc.REMAINDER;
+	    l = new JLabel ("----------------------------------------------");
+	    gblayout.setConstraints (l, gbc);
+	    data_panel.add (l);
+	    l = new JLabel ("Averages report for " + col.getName());
+	    gblayout.setConstraints (l, gbc);
+	    data_panel.add (l);
+	    for (j = 0; j < col.getNumPhases(); j++) {
+		avg = col.getAverageForPhase(j);
+		if (avg == 0.0)
+		    continue;
+		gbc.gridwidth = 1;
+		l = new JLabel (j + ": " + col.getLabelForPhase(j));
+		gblayout.setConstraints (l, gbc);
+		data_panel.add(l);
+		l = new JLabel (padFloat(avg), JLabel.RIGHT);
+		gblayout.setConstraints (l, gbc);
+		data_panel.add(l);
+		b = new GraphButton (col, j);
+		b.addActionListener (this);
+		b.setMargin(insets);
+		gblayout.setConstraints (b, gbc);
+		data_panel.add(b);
+		b = new AnomaliesButton (col, j);
+		b.addActionListener (this);
+		b.setMargin(insets);
+		gbc.gridwidth = gbc.REMAINDER;
+		gblayout.setConstraints (b, gbc);
+		data_panel.add(b);
+	    }
+	}
+    }
+
 
     JComboBox  collection_choice;
     JComboBox  display_choice;
@@ -28,6 +109,9 @@ public class PerfAnalyzerPanel extends JPanel implements ActionListener {
     JButton load_button;
     JButton print_button;
     JButton print_all_button;
+    JPanel data_panel;
+    GridBagLayout gblayout;
+    GridBagConstraints gbc;
 
     JScrollPane display_pane;
     JTextArea text_area;
@@ -52,6 +136,14 @@ public class PerfAnalyzerPanel extends JPanel implements ActionListener {
 	anomalycutoff = 1.0f;
 
 	setLayout (new BorderLayout (5, 5));
+	setBorder (new EmptyBorder (5,5,5,5));
+
+	data_panel = new JPanel();
+	data_panel.setLayout (gblayout = new GridBagLayout());
+	gbc = new GridBagConstraints();
+	gbc.insets = new Insets (0, 4, 0, 4);
+	gbc.fill = gbc.HORIZONTAL;
+
 
 	JPanel epanel = new JPanel ();
 	epanel.setLayout (new BoxLayout (epanel, BoxLayout.Y_AXIS));
@@ -60,6 +152,9 @@ public class PerfAnalyzerPanel extends JPanel implements ActionListener {
 	load_button = new JButton ("Load");
 	epanel.add (load_button);
 	load_button.addActionListener (this);
+
+	epanel.add (savecontents_button = new JButton ("Save Contents"));
+	savecontents_button.addActionListener (this);
 
 	print_button = new JButton ("Print");
 	print_button.addActionListener (this);
@@ -90,7 +185,7 @@ public class PerfAnalyzerPanel extends JPanel implements ActionListener {
 
 
 	JPanel nargpanel = new JPanel();
-	nargpanel.setLayout (new GridLayout (6,6,5,5));
+	nargpanel.setLayout (new GridLayout (4,6,5,5));
 	npanel.add (nargpanel);
 
 	nargpanel.add (new JLabel ("Preskip"));
@@ -101,24 +196,50 @@ public class PerfAnalyzerPanel extends JPanel implements ActionListener {
 	nargpanel.add (postskip_box = new JTextField ("   " + postskip));
 	postskip_box.addActionListener (this);
 
-	nargpanel.add (savecontents_button = new JButton ("Save Contents"));
-	savecontents_button.addActionListener (this);
-
-	display_pane = new JScrollPane (JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+	display_pane = new JScrollPane (data_panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
 	setCollectionList();
+	setCurrentCollector (null);
 
 	add (display_pane, "Center");
 
     }
 
 
+    public void setCurrentCollector (PerfDataCollector c) {
+	if (c == null) {
+	    current_collector = null;
+	    savecontents_button.setEnabled (false);
+	    print_button.setEnabled (false);
+	    print_all_button.setEnabled (false);
+	}
+	else {
+	    current_collector = c;
+	    savecontents_button.setEnabled (true);
+	    print_button.setEnabled (true);
+	    print_all_button.setEnabled (true);
+	}
+    }
+
     public void actionPerformed (ActionEvent e) {
+	if (e.getSource() instanceof AnomaliesButton) {
+	    System.out.println ("not implemented");
+	}
+	else if (e.getSource() instanceof GraphButton) {
+	    GraphButton b = (GraphButton)e.getSource();
+	    GraphFrame gf = new GraphFrame (b.collector, b.phase, preskip, postskip);
+	    gf.show();
+	}
 	if (e.getSource() == display_choice)
 	    refreshDisplay();
 	else if (e.getSource() == collection_choice) {
-	    current_collector = (PerfDataCollector)collection_choice.getSelectedItem();
-	    refreshDisplay();
+	    Object o = collection_choice.getSelectedItem();
+	    if (o instanceof String)
+		setCurrentCollector (null);
+	    else 
+		setCurrentCollector ((PerfDataCollector)collection_choice.getSelectedItem());
+	    //current_collector = (PerfDataCollector)collection_choice.getSelectedItem();
+	    //refreshDisplay();
 	}
 	else if (e.getSource() == preskip_box) {
 	    preskip = Integer.parseInt (preskip_box.getText().trim());
@@ -154,37 +275,14 @@ public class PerfAnalyzerPanel extends JPanel implements ActionListener {
 
 
     public void refreshDisplay() {
-	String s;
-	text_area = null;
-	if (display_choice.getSelectedItem().equals ("Averages with discrepencies")) {
-	    if (current_collector != null)
-		s = current_collector.dumpAverages (preskip, postskip, true, anomalycutoff);
-	    else
-		s = "";
-	    text_area = new JTextArea(s);
-	    display_pane.getViewport().remove (display_pane.getViewport().getView());
-	    display_pane.getViewport().add(text_area);
-	}
-	else if (display_choice.getSelectedItem().equals ("Averages")) {
-	    if (current_collector != null)
-		s = current_collector.dumpAverages (preskip, postskip, false, anomalycutoff);
-	    else
-		s = "";
-	    text_area = new JTextArea(s);
-	    display_pane.getViewport().add(text_area);
-	}
-	display_pane.validate();
-	display_pane.repaint();
-//  	for (int j = 0; j < current_collector.num; j++) {
-//  	    JFrame f = (new GraphFrame (current_collector, j));
-//  	    f.show();
-//  	}
+	buildDataPanel();
     }
 
     public void setCollectionList() {
 	while (collection_choice.getItemCount() != 0) 
 	    collection_choice.removeItemAt(0);
 	//collection_choice.removeAllItems();
+	collection_choice.addItem ("None Selected");
 	for (int i = 0; i < collection.collectors.size(); i++) {
 	    collection_choice.addItem (collection.collectors.elementAt(i));
 	}
