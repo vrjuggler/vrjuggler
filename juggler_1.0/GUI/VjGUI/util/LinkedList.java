ef487d8e59566d0a310a71dc7313d83ce19a82bb
Christopher Just <coronax@users.sourceforge.net>
1999-06-07 19:12:32 +0000

    Linked-list class.  I don't want to break compatibility with jdk1.1 yet, so I needed to do this myself.  It's a subset of the jdk1.2 linkelist interface, so it should be drop-in replaceable when we move to jdk1.2-only development.
    The sooner, the better ;)
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@923 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler_1.0/GUI/VjGUI/util/LinkedList.java
@@ -0,0 +1,99 @@
+package VjGUI.util;
+
+import VjGUI.util.ListElem;
+import VjGUI.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    ListElem first;
+    ListElem last;
+    int num;
+
+    
+    public LinkedList() {
+	first = new ListElem();
+	last = new ListElem();
+	first.next = last;
+	last.prev = first;
+	num = 0;
+    }
+
+    
+    public void add (Object o) {
+	// adds to end;
+	ListElem tmp = new ListElem();
+	tmp.data = o;
+	tmp.next = last;
+	tmp.prev = last.prev;
+	last.prev.next = tmp;
+	last.prev = tmp;
+	num++;
+    }
+
+    public void addFirst (Object o) {
+	// adds to start
+	ListElem tmp = new ListElem();
+	tmp.data = o;
+	tmp.next = first.next;
+	tmp.prev = first;
+	first.next.prev = tmp;
+	first.next = tmp;
+	num++;
+    }
+
+    public Object getFirst () throws NoSuchElementException {
+	if (first.next == last)
+	    throw new NoSuchElementException();
+	else
+	    return first.next.data;
+    }
+
+    public Object getLast () throws NoSuchElementException {
+	if (last.prev == first)
+	    throw new NoSuchElementException();
+	else
+	    return last.prev.data;
+    }
+
+    public Object removeFirst() throws NoSuchElementException {
+	ListElem tmp;
+
+	if (first.next == last)
+	    throw new NoSuchElementException();
+	else {
+	    tmp = first.next;
+	    tmp.next.prev = first;
+	    first.next = tmp.next;
+	    num--;
+	    return tmp.data;
+	}
+    }
+
+    public Object removeLast() throws NoSuchElementException {
+	ListElem tmp;
+
+	if (last.prev == first)
+	    throw new NoSuchElementException();
+	else {
+	    tmp = last.prev;
+	    tmp.prev.next = last;
+	    last.prev = tmp.prev;
+	    num--;
+	    return tmp.data;
+	}
+    }
+
+
+    public int size() {
+	return num;
+    }
+
+    public ListIterator listIterator (int index) {
+	// bug; assumes index is 0
+	return new ListIterator (first, last, first);
+    }
+}
+
+
+
