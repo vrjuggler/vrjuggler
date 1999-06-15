e03130fc247cee5e63db0bad07d08100e020a017
Christopher Just <coronax@users.sourceforge.net>
1999-06-15 18:12:14 +0000

    added default chunk support
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@986 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- GUI/VjConfig/ChunkFactory.java
+++ GUI/VjConfig/ChunkFactory.java
@@ -1,6 +1,8 @@
 
 package VjConfig;
 
+import java.util.Vector;
+import VjConfig.ConfigChunkDB;
 import VjConfig.ChunkDescDB;
 import VjConfig.ChunkDesc;
 import VjConfig.ConfigChunk;
@@ -8,17 +10,32 @@ import VjConfig.ConfigChunk;
 public class ChunkFactory {
 
     static private ChunkDescDB descdb = null;
+    static private ConfigChunkDB default_chunkdb = null;
 
     static public void setChunkDescDB (ChunkDescDB _db) {
 	descdb = _db;
     }
 
+    static public void setDefaultChunkDB (ConfigChunkDB _db) {
+	default_chunkdb = _db;
+    }
+
     static public ConfigChunk createChunkWithDescName (String s) {
-	ChunkDesc cd = descdb.getByName(s);
-	if (cd != null)
-	    return new ConfigChunk (cd, descdb);
-	else
-	    return null;
+	ConfigChunk newchunk = null;
+
+	if (default_chunkdb != null) {
+	    Vector v = default_chunkdb.getOfDescName (s);
+	    if (!v.isEmpty()) {
+		//System.out.println ("creating chunk from default");
+		return new ConfigChunk ((ConfigChunk)v.elementAt(0));
+	    }
+	}
+	if (descdb != null) {
+	    ChunkDesc cd = descdb.getByName(s);
+	    if (cd != null)
+		return new ConfigChunk (cd, descdb);
+	}
+	return null;
     }
 
     static public ConfigChunk createChunk (ChunkDesc cd) {
