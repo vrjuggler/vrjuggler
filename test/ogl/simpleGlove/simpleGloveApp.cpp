acc2fa4dcafe392f55306559decba9a3a7ce669c
Patrick Hartling <patrick.hartling@gmail.com>
1999-07-25 17:06:09 +0000

    Moved simpleGloveApp::myDraw() and simpleGloveApp::initGLState() to the new file simpleGloveApp.cpp.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@1111 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ test/ogl/simpleGlove/simpleGloveApp.cpp
@@ -0,0 +1,86 @@
+#include <simpleGloveApp.h>
+
+
+void simpleGloveApp::myDraw()
+{
+	// Clear the scene
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glMatrixMode(GL_MODELVIEW);
+
+   int finger;
+
+      // -- Draw box on wand --- //
+   vjMatrix finger_matrix;
+
+   // Draw green balls on finger tips
+   glColor3f(0.0f, 1.0f, 0.0f);
+   glPushMatrix();
+   {
+      for(finger=vjGloveData::INDEX;finger<=vjGloveData::PINKY;finger++)
+      {
+      glPushMatrix();
+         finger_matrix =
+            mGlove->getPos((vjGloveData::vjGloveComponent)finger);
+         glMultMatrixf(finger_matrix.getFloatPtr());
+         drawSphere((0.1f*(1.0f/12.0f)), 4, 4);
+      glPopMatrix();
+      }
+   }
+   glPopMatrix();
+
+   // Draw red rays out of fingers
+   // I am not doing this in the most efficient way.
+   // I am just trying to test the vector functions
+   glColor3f(1.0f, 0.0f, 0.0f);
+   glPushMatrix();
+   {
+      for(finger=vjGloveData::INDEX;finger<=vjGloveData::PINKY;finger++)
+      {
+      glPushMatrix();
+         vjVec3   origin(0,0,0);    // Base of the vector
+         finger_matrix =
+            mGlove->getPos((vjGloveData::vjGloveComponent)finger);
+         origin.xformFull(finger_matrix, origin);     // Go to new coord system
+         drawLine(origin,
+                  origin + (0.25 * mGlove->getVector((vjGloveData::vjGloveComponent)finger)));
+      glPopMatrix();
+      }
+   }
+   glPopMatrix();
+}
+
+void simpleGloveApp::initGLState()
+{
+   GLfloat light0_ambient[] = { .2,  .2,  .2,  1.0};
+   GLfloat light0_diffuse[] = { 1.0,  1.0,  1.0,  1.0};
+   GLfloat light0_specular[] = { 1.0,  1.0,  1.0,  1.0};
+   GLfloat light0_position[] = {2000.0, 1000.0, 100.0, 1.0};
+   GLfloat light0_direction[] = {-100, -100.0, -100.0};
+
+   GLfloat mat_ambient[] = { 0.7, 0.7,  0.7,  1.0};
+   GLfloat mat_diffuse[] = { 1.0,  0.5,  0.8,  1.0};
+   GLfloat mat_specular[] = { 1.0,  1.0,  1.0,  1.0};
+   GLfloat mat_shininess[] = { 50.0};
+   GLfloat mat_emission[] = { 1.0,  1.0,  1.0,  1.0};
+   GLfloat no_mat[] = { 0.0,  0.0,  0.0,  1.0};
+
+   glLightfv(GL_LIGHT0, GL_AMBIENT,  light0_ambient);
+   glLightfv(GL_LIGHT0, GL_DIFFUSE,  light0_diffuse);
+   glLightfv(GL_LIGHT0, GL_SPECULAR,  light0_specular);
+   glLightfv(GL_LIGHT0, GL_POSITION,  light0_position);
+   glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light0_direction);
+
+   glMaterialfv( GL_FRONT, GL_AMBIENT, mat_ambient );
+   glMaterialfv( GL_FRONT,  GL_DIFFUSE, mat_diffuse );
+   glMaterialfv( GL_FRONT, GL_SPECULAR, mat_specular );
+   glMaterialfv( GL_FRONT,  GL_SHININESS, mat_shininess );
+   glMaterialfv( GL_FRONT,  GL_EMISSION, no_mat);
+
+   glEnable(GL_DEPTH_TEST);
+   glEnable(GL_NORMALIZE);
+   glEnable(GL_LIGHTING);
+   glEnable(GL_LIGHT0);
+   glEnable(GL_COLOR_MATERIAL);
+   glShadeModel(GL_SMOOTH);
+}
