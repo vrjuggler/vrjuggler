f6ece858e5e391eb8fb2c8885df5e029087a0768
Patrick Hartling <patrick.hartling@gmail.com>
1999-10-30 19:38:37 +0000

    gcc (more specifically, GNU ld) does not allow multiply defined symbols, so the definitions of vjTrackedWallProjection::updateWallParams() and vjTrackedWallProjection::outStream() have been moved to vjTrackedWallProjection.cpp.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1184 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler_1.0/Kernel/vjTrackedWallProjection.cpp
@@ -0,0 +1,40 @@
+/*
+ *  File:	    $RCSfile$
+ *  Date modified:  $Date$
+ *  Version:	    $Revision$
+ *
+ *
+ *                                VR Juggler
+ *                                    by
+ *                              Allen Bierbaum
+ *                             Christopher Just
+ *                             Patrick Hartling
+ *                            Carolina Cruz-Neira
+ *                               Albert Baker
+ *
+ *                         Copyright  - 1997,1998,1999
+ *                Iowa State University Research Foundation, Inc.
+ *                            All Rights Reserved
+ */
+
+#include <vjConfig.h>
+
+#include <Kernel/vjTrackedWallProjection.h>
+
+
+void vjTrackedWallProjection::updateWallParams()
+{
+   vjMatrix tracker_mat = *(mTracker->getData());
+
+   // Compute the correct rotation matrix
+   mWallRotationMatrix.mult(tracker_mat,mWallRotationMatrix_bak);
+
+   // Compute the correct rotation parameters
+}
+
+ostream& vjTrackedWallProjection::outStream(ostream& out)
+{
+   out << "vjTrackedWallProjection:\n";
+   out << "Pos Proxy: " << mTracker.getProxyName() << endl;
+   return vjWallProjection::outStream(out);
+}
