6923383a71a1021c7de9ceb217806afee2962235
Patrick Hartling <patrick.hartling@gmail.com>
1999-12-07 22:17:21 +0000

    Added the VR Juggler copyright notice.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@1230 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/drivers/Ascension/MotionStar/MotionStar.cpp
+++ modules/gadgeteer/drivers/Ascension/MotionStar/MotionStar.cpp
@@ -1,4 +1,23 @@
+/*
+ *  File:	    $RCSfile$
+ *  Date modified:  $Date$
+ *  Version:	    $Revision$
+ *
+ *
+ *                                VR Juggler
+ *                                    by
+ *                              Allen Bierbaum
+ *                             Christopher Just
+ *                             Patrick Hartling
+ *                            Carolina Cruz-Neira
+ *                               Albert Baker
+ *
+ *                         Copyright - 1997,1998,1999
+ *                Iowa State University Research Foundation, Inc.
+ *                            All Rights Reserved
+ */
 
+ 
 #include <vjConfig.h>
 #include <sys/file.h>
 #include <sys/ioctl.h>
--- modules/gadgeteer/drivers/Ascension/MotionStar/MotionStarStandalone.cpp
+++ modules/gadgeteer/drivers/Ascension/MotionStar/MotionStarStandalone.cpp
@@ -1,4 +1,22 @@
-// aMotionStar.cpp
+/*
+ *  File:	    $RCSfile$
+ *  Date modified:  $Date$
+ *  Version:	    $Revision$
+ *
+ *
+ *                                VR Juggler
+ *                                    by
+ *                              Allen Bierbaum
+ *                             Christopher Just
+ *                             Patrick Hartling
+ *                            Carolina Cruz-Neira
+ *                               Albert Baker
+ *
+ *                         Copyright - 1997,1998,1999
+ *                Iowa State University Research Foundation, Inc.
+ *                            All Rights Reserved
+ */
+
  
 #include <Input/vjPosition/aMotionStar.h>
 #include <strings.h>
