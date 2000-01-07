1d4a9fe1b27862afec6ac141d9a7b4f21948fa63
Patrick Hartling <patrick.hartling@gmail.com>
2000-01-07 17:08:40 +0000

    Added a substitution variable $OBJ_NAME_FLAG used for providing the compiler with a name for an object file being compiled.  This is necessary because Visual C++ uses a really weird option for specifying an alternate name.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1288 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/mk/vj.compile.mk
+++ juggler_1.0/mk/vj.compile.mk
@@ -5,18 +5,20 @@
 # -----------------------------------------------------------------------------
 # The Makefile including this file should define the following variables:
 #
-# OBJDIR      - The directory where the object file will be created.
-# C_COMPILE   - The C compiler command line (without -c or -o <outfile>
-#               options).
-# CXX_COMPILE - The C++ compiler command line (without -c or -o <outfile>
-#               options).
+# OBJDIR        - The directory where the object file will be created.
+# C_COMPILE     - The C compiler command line (without -c or -o <outfile>
+#                 options).
+# CXX_COMPILE   - The C++ compiler command line (without -c or -o <outfile>
+#                 options).
+# OBJ_NAME_FLAG - Flag (with any extra options) used for naming the object
+#                 file being compiled here.
 # -----------------------------------------------------------------------------
 
 ${OBJDIR}/%.o : %.c
-	${C_COMPILE} -c -o $@ $<
+	${C_COMPILE} -c ${OBJ_NAME_FLAG} $<
 
 ${OBJDIR}/%.o : %.cpp
-	${CXX_COMPILE} -c -o $@ $<
+	${CXX_COMPILE} -c ${OBJ_NAME_FLAG} $<
 
 ${OBJDIR}/%.o : %.C
-	${CXX_COMPILE} -c -o $@ $<
+	${CXX_COMPILE} -c ${OBJ_NAME_FLAG} $<
