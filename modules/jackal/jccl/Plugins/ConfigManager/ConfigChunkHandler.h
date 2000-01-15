800094d24461a5660b3ddfda89543b96d444562c
Allen Bierbaum <abierbaum@gmail.com>
2000-01-15 20:50:48 +0000

    Partial updates to allow for configManager and dependencyManager.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@1461 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/jccl/Plugins/ConfigManager/ConfigChunkHandler.h
+++ modules/jackal/jccl/Plugins/ConfigManager/ConfigChunkHandler.h
@@ -1,7 +1,7 @@
 /*
- *  File:	    $RCSfile$
+ *  File:       $RCSfile$
  *  Date modified:  $Date$
- *  Version:	    $Revision$
+ *  Version:       $Revision$
  *
  *
  *                                VR Juggler
@@ -23,6 +23,8 @@
 
 #include <vjConfig.h>
 #include <Config/vjConfigChunk.h>
+#include <Kernel/vjConfigManager.h>
+#include <Kernel/vjDependencyManager.h>
 
 //-----------------------------------------
 //: Base class for all classes that can handle config chunks
@@ -52,6 +54,66 @@ public:
    //+          false - Can't handle it
    virtual bool configCanHandle(vjConfigChunk* chunk)
    { return false; }
+
+   //: Process any pending reconfiguration that we can deal with
+   // This function processes each pending reconfiguration in configuration manager
+   // The default implementation does the following. <br>
+   // if(this->configCanHandle && passesDepCheck)
+   //    configAdd/configRemove(chunk)
+   // if(success)
+   //   add/remove from pending
+   //   remove/add from current
+   //
+   //! ARGS: lockIt - Should we lock the cfg_mgr pending list
+   //+       defaults to true.  The only time this should be false is 
+   //+       if you override this function and still want to make use of it's abilities
+   //+         (ex.  The kernel needs to do this because if has to call processPending on other managers in addition to itself)
+   virtual void processPending(bool lockIt = true)
+   {
+      vjConfigManager*     cfg_mgr = vjConfigManager::instance();
+      vjDependencyManager* dep_mgr = vjDependencyManager::instance();
+      cfg_mgr->lockPending();     // We need to lock the pending first
+      {
+         std::list<vjConfigManager::vjPendingChunk>::iterator current, end, remove_me;
+         current = cfg_mgr->getPendingBegin();
+         end = cfg_mgr->getPendingEnd();
+
+         while(current != end)
+         {
+            vjConfigChunk* cur_chunk = (*current).mChunk;
+
+            // CONFIG ADD
+            if((*current).mType == vjConfigManager::vjPendingChunk::ADD)
+            {           
+               if(this->configCanHandle(cur_chunk) &&
+                  dep_mgr->depSatisfied(cur_chunk))
+               {
+                  bool added = this->configAdd(cur_chunk);
+                  if(added)      // Was there success adding
+                  {
+                     remove_me = current;
+                     current++;                          // Goto next item
+                     cfg_mgr->removePending(remove_me);  // Delete previous item
+                     cfg_mgr->lockActive();
+                        cfg_mgr->addActive(cur_chunk);     // Add it to the current config
+                     cfg_mgr->unlockActive();
+                  }
+                  else // Failed to add
+                  {
+                     current++;
+                  }
+               }
+            }
+            // CONFIG REMOVE
+            else if((*current).mType == vjConfigManager::vjPendingChunk::REMOVE)
+            {
+               current++;
+            }
+         }        // END: while(current != end)
+
+      }
+      cfg_mgr->unlockPending();   // Unlock it
+   }
 };
 
 #endif
---
+++ modules/vrjuggler/vrj/Kernel/ConfigManager.cpp
@@ -0,0 +1,5 @@
+#include <vjConfig.h>
+#include <Kernel/vjConfigManager.h>
+
+vjConfigManager* vjConfigManager::_instance = NULL;
+
