5204a9b92d6aefca366e603f862fc1e5521e35e9
Patrick Hartling <patrick.hartling@gmail.com>
2000-01-18 03:46:46 +0000

    Added VR Juggler copyright notice.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@1485 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- SharedMem/vjMemPoolWin32.cpp
+++ SharedMem/vjMemPoolWin32.cpp
@@ -1,3 +1,22 @@
+/*
+ *  File:          $RCSfile$
+ *  Date modified: $Date$
+ *  Version:       $Revision$
+ *
+ *
+ *                                VR Juggler
+ *                                    by
+ *                              Allen Bierbaum
+ *                             Christopher Just
+ *                             Patrick Hartling
+ *                            Carolina Cruz-Neira
+ *                               Albert Baker
+ *
+ *                  Copyright (C) - 1997, 1998, 1999, 2000
+ *              Iowa State University Research Foundation, Inc.
+ *                            All Rights Reserved
+ */
+
 #include <vjConfig.h>
 #include <SharedMem/vjMemPoolWin32.h>
 
