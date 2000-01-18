dd9d5fbc0916248796db795b9ab3f24e8566ec18
Patrick Hartling <patrick.hartling@gmail.com>
2000-01-18 03:27:54 +0000

    Added VR Juggler copyright notice.  I have no idea how these two were missed when it was initially added.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1482 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrj/Draw/OGL/GlWindowWin32.h
+++ juggler/modules/vrjuggler/vrj/Draw/OGL/GlWindowWin32.h
@@ -1,3 +1,22 @@
+/*
+ *  File:          $RCSfile$
+ *  Date modified: $Date$
+ *  Version:       $Revision$
+ *
+ *
+ *                                VR Juggler
+ *                                    by
+ *                              Allen Bierbaum
+ *                             Christopher Just
+ *                             Patrick Hartling
+ *                            Carolina Cruz-Neira
+ *                               Albert Baker
+ *
+ *                  Copyright (C) - 1997, 1998, 1999, 2000
+ *              Iowa State University Research Foundation, Inc.
+ *                            All Rights Reserved
+ */
+
 #ifndef _GL_WIN_WIN32_
 #define _GL_WIN_WIN32_
 
