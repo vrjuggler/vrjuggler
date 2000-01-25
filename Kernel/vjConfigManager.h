ed1c287d23358a9bb5c661157990cbfd7cd6040a
Patrick Hartling <patrick.hartling@gmail.com>
2000-01-25 16:53:53 +0000

    Use the c_str() method to convert std::string's to C strings for use with cout on Windows.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@1578 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- Kernel/vjConfigManager.h
+++ Kernel/vjConfigManager.h
@@ -165,7 +165,9 @@ public:   // ----- PENDING LIST ----- //
          vjConfigChunk* cur_chunk = (*current).mChunk;
 
          vjDEBUGlg(vjDBG_ALL,0,false,true) << cur_chunk->getProperty("name")
-                                           << " type: " << (std::string)cur_chunk->getType() << endl << vjDEBUG_FLUSH;
+                                           << " type: "
+                                           << ((std::string)cur_chunk->getType()).c_str()
+                                           << endl << vjDEBUG_FLUSH;
          current++;
       }
       vjDEBUG_ENDlg(vjDBG_ALL,0,false,true) << "-------------------------------\n" << vjDEBUG_FLUSH      ;
