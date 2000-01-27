9444adeb842753cc071c4d38732b143c74d8cc6c
Christopher Just <coronax@users.sourceforge.net>
2000-01-27 04:33:39 +0000

    tweaked debug output
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1641 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/Threads/vjThreadManager.cpp
+++ juggler_1.0/Threads/vjThreadManager.cpp
@@ -41,25 +41,25 @@ vjThreadManager* vjThreadManager::_instance = NULL;
 //: Dump the state of the manager to debug
 void vjThreadManager::debugDump()
 {
-   vjDEBUG(vjDBG_ALL, vjDBG_CRITICAL_LVL)
+   vjDEBUG(vjDBG_ALL, vjDBG_CONFIG_LVL)
       << "------- Thread Manager DUMP -------\n" << vjDEBUG_FLUSH;
-   vjDEBUG_BEGIN(vjDBG_ALL, vjDBG_CRITICAL_LVL) << "--- Thread List ----\n"
+   vjDEBUG_BEGIN(vjDBG_ALL, vjDBG_CONFIG_LVL) << "--- Thread List ----\n"
                                                 << vjDEBUG_FLUSH;
    for (int i=0;i<mThreads.size();i++)
    {
       if (mThreads[i] != NULL)
-         vjDEBUG(vjDBG_ALL, vjDBG_CRITICAL_LVL) << i << ": ["
+         vjDEBUG(vjDBG_ALL, vjDBG_CONFIG_LVL) << i << ": ["
                                                 << (void*)mThreads[i] << "] "
                                                 << mThreads[i] << endl
                                                 << vjDEBUG_FLUSH;
       else
-         vjDEBUG(vjDBG_ALL, vjDBG_CRITICAL_LVL) << i << ": ["
+         vjDEBUG(vjDBG_ALL, vjDBG_CONFIG_LVL) << i << ": ["
                                                 << (void*)mThreads[i]
                                                 << "] No thread\n"
                                                 << vjDEBUG_FLUSH;
    }
 
-   vjDEBUG_END(vjDBG_ALL, vjDBG_CRITICAL_LVL) << "---------------------\n"
+   vjDEBUG_END(vjDBG_ALL, vjDBG_CONFIG_LVL) << "---------------------\n"
                                               << vjDEBUG_FLUSH;
 }
 
