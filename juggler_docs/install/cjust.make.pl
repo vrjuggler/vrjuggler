42fee3392c380f09972ac6373c9fe783df36db15
Christopher Just <coronax@users.sourceforge.net>
2000-01-29 01:45:39 +0000

    These scripts are so cool everybody needs to have one.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1720 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler_docs/install/cjust.make.pl
@@ -0,0 +1,37 @@
+#!/home/freeware/bin/perl
+require 5.004;
+
+
+use Getopt::Std;
+
+getopt('');
+my $src_dir = "../.";
+my $dest_dir = "/home/users/cjust/public_html/vrj";
+my $css_filename = "html/css_header.html";
+my $html_header_filename = "html/header.html";
+my $html_footer_filename = "html/footer.html";
+my $html_install_prefix = "http://www.vrac.iastate.edu/~cjust/vrj/";
+my $subst_file = "subst.pl";
+
+
+
+my $command = "install-web.pl ";
+$command .= " -s $src_dir";
+$command .= " -t $dest_dir";
+$command .= " -i __deleteme,JExplorer,install,CVS";
+$command .= " -c $css_filename";
+$command .= " -f $html_header_filename";
+$command .= " -f $html_footer_filename";
+$command .= " -h $html_install_prefix";
+$command .= " -w $subst_file";
+$command .= " -z use.last.build.pl";
+if($opt_a == 1)
+{
+$command .= " -a";
+}
+
+print "$command\n\n\n\n----------------\n\n";
+system("$command");
+my $netscape_command = "netscape " . $html_install_prefix . "index.html &";
+print "$netscape_command\n";
+system("$netscape_command");
