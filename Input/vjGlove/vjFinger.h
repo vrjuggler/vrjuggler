3f4401fac507cb8050e08c5f27f86bf062b88379
Kevin Meinert <subatomic@users.sourceforge.net>
2000-03-27 17:17:21 +0000

    added finger and hand definitions.  useful for pinch
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@1855 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ Input/vjGlove/vjFinger.h
@@ -0,0 +1,71 @@
+/*
+ * VRJuggler
+ *   Copyright (C) 1997,1998,1999,2000
+ *   Iowa State University Research Foundation, Inc.
+ *   All Rights Reserved
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ */
+ 
+#ifndef VJ_FINGER___
+#define VJ_FINGER___
+
+//  DIJ = Distal Interphalangeal Joint  --- vjFinger tip
+//  PIJ = Proximal "              "     --- Middle joint
+//  MPJ = Metacarpo "             "     --- closest to vjHand
+//  ABDUCT = spread of vjFingers
+class vjFinger
+{
+public:
+   vjFinger( const float& dij = -15, const float& pij = -45, const float& mpj = -45, const float& abduct = 0 ) : mDij( dij ), mPij( pij ), mMpj( mpj ), mAbduct( abduct )
+   {
+   }
+   
+   inline const float& dij() const { return mDij; }
+   inline const float& pij() const { return mPij; }
+   inline const float& mpj() const { return mMpj; }
+   inline const float& abduct() const { return mAbduct; }
+   
+   inline void setDij( const float& dij ) { mDij = dij; }
+   inline void setPij( const float& pij ) { mPij = pij; }
+   inline void setMpj( const float& mpj ) { mMpj = mpj; }
+   inline void setAbduct( const float& abduct ) { mAbduct = abduct; }
+   
+   inline void set( const float& dij = -15, const float& pij = -45, const float& mpj = -45, const float& abduct = 0 )
+   {
+       mDij = dij;
+       mPij = pij;
+       mMpj = mpj;
+       mAbduct = abduct;
+   }   
+   
+private: 
+   float mDij, mPij, mMpj, mAbduct;
+};
+
+
+#endif
