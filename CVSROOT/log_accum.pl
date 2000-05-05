b681f7ab1c5b97e709a31f0f96e1eab6300d394d
Kevin Meinert <subatomic@users.sourceforge.net>
2000-05-05 05:22:15 +0000

    well, i got that message, maybe it's fixed now that it "rebuilt the administrative database"
    i'll do one more commit somewhere else to see whats up.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1895 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- CVSROOT/log_accum.pl
+++ CVSROOT/log_accum.pl
@@ -270,7 +270,7 @@ sub build_header {
 sub mlist_map {
     local($dir) = @_;		# perl warns about this....
    
-    return 'patrick@vrac.iastate.edu,kevn@vrjuggler.org,allenb@vrac.iastate.edu';
+    return 'kevn@vrjuggler.org,patrick@vrac.iastate.edu,allenb@vrac.iastate.edu';
 }    
 
 sub do_changes_file {
