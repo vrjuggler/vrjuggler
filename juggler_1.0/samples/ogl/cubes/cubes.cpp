df8a7f8043954ed925d1274cbf3a620dfdb9e0d1
Kevin Meinert <subatomic@users.sourceforge.net>
2000-05-12 22:28:45 +0000

    added some code to check if user supplied config files to the executable. if not, then it outputs usage info.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1915 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/samples/ogl/cubes/cubes.cpp
+++ juggler_1.0/samples/ogl/cubes/cubes.cpp
@@ -50,6 +50,19 @@ int main(int argc, char* argv[])
    vjKernel* kernel = vjKernel::instance();        // Get the kernel
    cubesApp* application = new cubesApp(kernel);   // Declare an instance of the app
 
+   if (argc <= 1)
+   {
+      // display some usage info (holding the user by the hand stuff)
+      //  this will probably go away once the kernel becomes separate 
+      //  and can load application plugins.
+      cout<<"\n"<<flush;
+      cout<<"\n"<<flush;
+      cout<<"Usage: "<<argv[0]<<" vjconfigfile[0] vjconfigfile[1] ... vjconfigfile[n]\n"<<flush;
+      cout<<"\n"<<flush;
+      cout<<"\n"<<flush;
+      exit(1);
+   }
+   
    // Load any config files specified on the command line
    for(int i=1;i<argc;i++)
       kernel->loadConfigFile(argv[i]);
