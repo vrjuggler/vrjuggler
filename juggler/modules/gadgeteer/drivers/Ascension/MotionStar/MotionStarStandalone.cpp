a051faff93107de207b93c1ef122e59fae0bb6ea
Patrick Hartling <patrick.hartling@gmail.com>
2000-05-27 01:35:00 +0000

    Removed trailing whitespace on many, many lines.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@1940 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/drivers/Ascension/MotionStar/MotionStarStandalone.cpp
+++ juggler/modules/gadgeteer/drivers/Ascension/MotionStar/MotionStarStandalone.cpp
@@ -31,7 +31,7 @@
  * -----------------------------------------------------------------
  */
 
- 
+
 #ifdef __sun__
 #define _REENTRANT
 #endif
@@ -41,7 +41,7 @@
 #include <assert.h>
 #include <time.h>
 #include <Input/vjPosition/aMotionStar.h>
- 
+
 aMotionStar::aMotionStar(int _hemisphere,
 			 unsigned int _birdFormat,
 			 unsigned int _birdsRequired,
@@ -55,14 +55,14 @@ aMotionStar::aMotionStar(int _hemisphere,
 				runMode(_runMode),
 				birdRate(_birdRate),
 				reportRate(_reportRate)
-{  
+{
 
 } // end aMotionStar::aMotionStar()
 
 
 
 aMotionStar::~aMotionStar() {
-  
+
   this->stop();
 
 } // end aMotionStar::~aMotionStar()
@@ -70,13 +70,13 @@ aMotionStar::~aMotionStar() {
 
 void aMotionStar::start () {
 
-  cout << "In start" << endl; 
+  cout << "In start" << endl;
   assert(!active);
 
   /*    strcpy(ip_address, "192.1.2.193");      */
    strcpy(ip_address, "129.186.232.39");
   // strcpy(ip_address, "129.186.232.21");
-  
+
    rate[0] = 57; // 48
   rate[1] = 48; // 50
   rate[2] = 48;
@@ -89,31 +89,31 @@ void aMotionStar::start () {
     case 1:
       format = birdFormat | 0x30;
       break;
- 
+
     case 2:
       format = birdFormat | 0x30;
       break;
- 
+
     case 3:
       format = birdFormat | 0x90;
       break;
- 
+
     case 4:
       format = birdFormat | 0x60;
       break;
- 
+
     case 5:
       format = birdFormat | 0xC0;
       break;
- 
+
     case 7:
       format = birdFormat | 0x40;
       break;
- 
+
     case 8:
       format = birdFormat | 0x70;
       break;
- 
+
     default:
       format = 0x64;
       break;
@@ -122,14 +122,14 @@ void aMotionStar::start () {
   /*
    * Open a TCP socket.
    */
- 
+
   if (( s = socket(AF_INET,SOCK_STREAM,0))<0)
     printf("client: can't open stream socket");
- 
+
   /*
    * Bind any local address for us.
    */
- 
+
   bzero((char *)&client, sizeof(client));
   client.sin_family=AF_INET;
   client.sin_port=htons(MYPORT);
@@ -138,48 +138,48 @@ void aMotionStar::start () {
     rtn = bind(s,(struct sockaddr *) &client, sizeof (client));
     printf("bind = %4d, socket =  %4d, bind error =  %4d\n",rtn,s,errno);
   */
- 
+
   /* Fill in the structure with the address of the
    * server that we want to connect to.
    */
- 
+
   bzero((char *)&server, sizeof(server));
   server.sin_family=AF_INET;
   server.sin_port=htons(TCP_PORT);                              /* Server port number */
   server.sin_addr.s_addr=inet_addr(ip_address);         /* Server address */
- 
+
   cout << "connecting..." << endl;
-  rtn = connect(s, (struct sockaddr *) &server, sizeof(server)); 
+  rtn = connect(s, (struct sockaddr *) &server, sizeof(server));
   printf("connect = %4d, connect error = %4d, ", rtn, errno);
   perror(NULL);
   printf("\n");
- 
+
   newptr =(char*)(&response);
   lpCommand = &command;
   lpRspGetStatAll=(struct RSP_GET_STATUS_all*)(newptr);
   lpResponse = &response;
   Size = sizeof(server);
   lpSize = &Size;
- 
+
   /* send the wake up call */
   send_wakeup();
- 
+
   /* get the system status */
   get_status_all();
- 
+
   /* send the system setup */
   set_status_all();
- 
+
    /* for 1 to n flock boards                    */
   /* get the individual bird status             */
   /* modify the appropriate contents            */
   /* send the individual bird status packet back        */
- 
+
   for(int flock=1;flock<=chassisDevices;flock++)
     {
       /* get the status of an individual bird */
       get_status_fbb(flock);
- 
+
                 /* change the data format to something new for all birds */
       if(flock<=birdsRequired)
         {
@@ -189,7 +189,7 @@ void aMotionStar::start () {
         {
           response.buffer[6] = 0;
         }
- 
+
       /* set new report rate for all birds */
       response.buffer[7] = reportRate;
       response.buffer[26] = hemisphere;
@@ -203,12 +203,12 @@ void aMotionStar::start () {
   else {
       singleShot();
   }
-  active = true; 
- 
+  active = true;
+
 } // end void aMotionStar::start()
 
- 
-void aMotionStar::stop() { 
+
+void aMotionStar::stop() {
     /* put in a STOP COMMAND here */
 
     command.sequence      = sequenceNumber;
@@ -226,7 +226,7 @@ void aMotionStar::send_wakeup()
 {
   /***** send a command to the server wakeup *****/
   printf("WAKEUP:");
- 
+
   command.sequence              = sequenceNumber++;
   command.type                  = 10;
   command.xtype                 = 0;
@@ -234,28 +234,28 @@ void aMotionStar::send_wakeup()
   command.number_bytes          = 0;
   command.error_code            = 0;
   command.error_code_extension  = 0;
- 
+
   /*n++;*/
   numberBytes = send(s,(void*)lpCommand, sizeof(command), 0);
- 
+
   printf(" - number of bytes sent = %3d errorcode = %d", numberBytes,errno);
- 
+
   bytesReceived = recv( s,
                         (void*)lpResponse,
                         sizeof(response),
                         0);
- 
+
   printf("  | WAKEUP ACKNOWLEDGE: - number bytes received = %5d\n", bytesReceived);
 
 } // end void aMotionStar::send_wakeup ()
- 
- 
+
+
 void aMotionStar::runContinuous() {
   cout << "runContinous" << endl;
-  
+
   command.type=104;
-// MSG_RUN CONTINIOUS - request server to send packets continuously 
- 
+// MSG_RUN CONTINIOUS - request server to send packets continuously
+
   command.xtype = 0;
   command.sequence = sequenceNumber++;
   n++;
@@ -263,19 +263,19 @@ void aMotionStar::runContinuous() {
                        (void*)lpCommand,
                        sizeof(command),
                        0);
- 
+
     printf("\n\nSTREAM: - number of bytes sent = %3d errorcode = %d", numberBytes,errno);
- 
+
   bytesReceived = recv( s,
                         (void*)lpResponse,
                         sizeof(response),
                         0);
- 
+
     printf("  | STREAM ACKNOWLEDGE: - number bytes received = %5d\n", bytesReceived);
 
-  
+
       /* receive the header */
-/*   
+/*
    int           headerBytesReceived;
    char          *lpBuffer;
 
@@ -290,12 +290,12 @@ void aMotionStar::runContinuous() {
                                lpSize);
       if (bytesReceived < 0) {
           perror("recv1"), exit(1);
-      } 
- 
+      }
+
       headerBytesReceived = headerBytesReceived + bytesReceived;
       lpBuffer = (char *)lpBuffer + bytesReceived;
     }
-*/ 
+*/
 } // end aMotionStar::runContinuous()
 
 
@@ -307,16 +307,16 @@ void aMotionStar::singleShot()
   command.sequence = sequenceNumber++;
   numberBytes = sendto(s,(void*)lpCommand, sizeof(command), 0,
                        (struct sockaddr *)&server, sizeof(server));
- 
+
     //printf("bytes sent = %d errno %d\n", numberBytes,errno);
- 
+
   /* wait for the packet to come back */
- 
+
   bytesReceived = recvfrom(s, (void*)lpResponse, sizeof(response), 0,
                            (struct sockaddr *)&server, lpSize);
     //printf("  >>> %3d; sequence=%4d; type=%4d; bytes received=%4d\n",n,response.header.sequence
-	//	,response.header.type, bytesReceived); 
-    
+	//	,response.header.type, bytesReceived);
+
 } // end void aMotionStar::singleShot()
 
 
@@ -324,9 +324,9 @@ void aMotionStar::sample() {
 //    cout << "Sampling..." << endl;
     if (runMode == 1) {singleShot();}
 //    cout << "After singleShot() " << endl;
-/*    
+/*
     double        x;
-    long          p;   
+    long          p;
     unsigned int  xraw, yraw, zraw;
     int           xint, yint, zint;
     double                xreal, yreal, zreal;
@@ -336,11 +336,11 @@ void aMotionStar::sample() {
     int           totalBytesNeeded, totalBytesReceived;
     char          *lpBuffer;
     int           headerBytesReceived;
- 
+
        /* receive the header */
- 
+
     if (runMode == 0) {
-   
+
       headerBytesReceived = 0;
       lpBuffer = (char*)lpResponse;
       while(headerBytesReceived != 16) {
@@ -353,26 +353,26 @@ void aMotionStar::sample() {
       /*  if (bytesReceived < 0){
           perror("recv1"), exit(1);
           } */
- 
+
         headerBytesReceived = headerBytesReceived + bytesReceived;
         lpBuffer = (char *)lpBuffer + bytesReceived;
       }
     }
     sequenceNumber = response.header.sequence;
- 
+
     //        printf("\n#%6d bytes received:%3d",n ,bytesReceived);
-    /*        printf("\ntype:%3d seq:%6d #bytes:%4d",response.header.type,  
+    /*        printf("\ntype:%3d seq:%6d #bytes:%4d",response.header.type,
 			response.header.sequence,response.header.number_bytes); */
- 
+
     /*  printf("\n#%6d:",n);
      */
- 
+
     /*      for (i=0;i<16;++i)
             {
             printf("%3x",*(newptr+i));
             }
     */
- 
+
     timeSeconds = (unsigned int)(response.header.time[0]) << 24;
     timeSeconds = timeSeconds | (unsigned int)(response.header.time[1]) << 16;
     timeSeconds = timeSeconds | (unsigned int)(response.header.time[2]) <<  8;
@@ -383,7 +383,7 @@ void aMotionStar::sample() {
 #else
     asctime_r(&newtime, timeChar);
 #endif
- 
+
     /*  printf(" TIME: %10d ", timeSeconds);     */
     if(response.header.milliseconds>999)
       {
@@ -393,11 +393,11 @@ void aMotionStar::sample() {
      */
     /* printf(" %3.3d ",  response.header.milliseconds);
      */
- 
+
     totalBytesReceived = 0;
     totalBytesNeeded = response.header.number_bytes;
     lpBuffer = (char*)lpResponse + 16;
-    
+
     if (runMode == 0) {
       while(totalBytesReceived != totalBytesNeeded){
         bytesReceived = recvfrom(s,
@@ -408,16 +408,16 @@ void aMotionStar::sample() {
                                lpSize);
         if (bytesReceived < 0)
           perror("recv2"), exit(1);
- 
+
         totalBytesReceived = totalBytesReceived + bytesReceived;
         lpBuffer = (char*)lpBuffer + bytesReceived;
- 
+
       }
     }
     //    printf(" .....received data #bytes = %d", totalBytesReceived);
- 
+
     //    printf("\n");
- 
+
     /*    for(cpos= 0;cpos< 42;cpos=cpos+2) {
       if (cpos % 14 == 0){
         printf("\n");
@@ -428,18 +428,18 @@ void aMotionStar::sample() {
         printf(" %2x%2x=", response.buffer[cpos], response.buffer[cpos+1]);
         printf(" %2f",  180.0*  rawToFloat(response.buffer[cpos], response.buffer[cpos+1]));
       }
- 
+
       for(cpos=50;cpos<100;cpos=cpos+2) {
         printf(" %2x%2x", response.buffer[cpos], response.buffer[cpos+1]);
       }
       printf("\n");
- 
+
     }
     */
     int o; // Offset -- 14 * bird number (bnum)
     for (int bnum = 0; bnum < birdsRequired; bnum++)
     {
-      o = 14*bnum; 
+      o = 14*bnum;
       posinfo[bnum][0] = 144*rawToFloat(response.buffer[2+o],
                                      response.buffer[3+o]);//XPos
       posinfo[bnum][1] = 144*rawToFloat(response.buffer[4+o],
@@ -454,13 +454,13 @@ void aMotionStar::sample() {
                                      response.buffer[13+o]);//XRot
     } // end for loop
 
- 
+
 } // end void aMotionStar::sample()
 
- 
+
 void aMotionStar::printInfo(){
   int i;
- 
+
   for (i = 0; i < 3; i++){
     printf("P = %f, %f, %f  A = %f, %f, %f \n", posinfo[i][0], posinfo[i][1],
            posinfo[i][2], posinfo[i][3], posinfo[i][4],posinfo[i][5]);
@@ -468,22 +468,22 @@ void aMotionStar::printInfo(){
   printf ("\n");
 
 } // end void aMotionStar::printinfo()
- 
+
 void aMotionStar::get_status_all()
 {
   void                  *lpBuffer;
   int                   headerBytes, dataBytes;
   int                   bytesReceived;
- 
+
   command.type=101;
   command.xtype=0;
   numberBytes = sendto(s,(void*)lpCommand, sizeof(command), 0,
                        (struct sockaddr *)&server, sizeof(server));
   printf("\nGET STATUS - number bytes sent = %5d errorno %d", numberBytes,errno)
 ;
- 
+
   /***** Receive packet from the server *****/
- 
+
   headerBytes = 0;
   lpBuffer = lpResponse;
   while(headerBytes<16)
@@ -492,35 +492,35 @@ void aMotionStar::get_status_all()
       headerBytes = headerBytes + bytesReceived;
       lpBuffer = (char *)lpBuffer + bytesReceived;
     }
- 
+
   dataBytes = 0;
   while(dataBytes<(response.header.number_bytes))
     {
       bytesReceived = recvfrom(s, (void*)lpBuffer, response.header.number_bytes,
- 
+
                                0, (struct sockaddr *)&server, lpSize);
       dataBytes = dataBytes + bytesReceived;
       lpBuffer = (char *)lpBuffer + bytesReceived;
     }
- 
+
   statusSize = headerBytes + dataBytes;
   printf("\nSYSTEM STATUS RECEIVED - number bytes received = %5d,", bytesReceived);
   printf("  type %d\n",response.header.type);
- 
+
   printf("\n\n============================================================\n\n");
- 
+
   /* now print out the details of the status packet */
- 
+
   all = response.buffer[0];
- 
+
   FBBerror = response.buffer[1];
- 
+
   flockNumber = response.buffer[2];
   printf("Number of FBB devices in system = %d,  ", flockNumber);
- 
+
   transmitterNumber = (response.buffer[4]>>4) & 0x0F;
   printf("Transmitter is at FBB address %d,  ", transmitterNumber);
- 
+
   szRate[0] = response.buffer[5];
   szRate[1] = response.buffer[6];
   szRate[2] = response.buffer[7];
@@ -528,19 +528,19 @@ void aMotionStar::get_status_all()
   szRate[4] = response.buffer[9];
   szRate[5] = response.buffer[10];
   szRate[6] = 0;
- 
+
  serverNumber = response.buffer[3];
   chassisNumber = response.buffer[11];
   chassisDevices = response.buffer[12];
   firstAddress = response.buffer[13];
   softwareRevision = (((unsigned short)(response.buffer[14])<<8) & 0xFF00)
     | ((unsigned short)(response.buffer[15]));
- 
- 
+
+
   mRate = atoi(szRate);
   realRate = (double)(mRate)/1000;
   printf("Measurement rate = %6.1f\n", realRate);
- 
+
   printf("Number of chassis in system = %d, \n", serverNumber);
   printf("Chassis# = %d, ", chassisNumber);
   printf("Number of FBB devices in this chassis = %d, ", chassisDevices);
@@ -548,15 +548,15 @@ void aMotionStar::get_status_all()
   printf("SERVER Software Version = %d\n\n============================================================\n\n",
          softwareRevision);
   printf("\n\n");
- 
- 
+
+
   for(i= 0;i<16;i++) printf(" %2x", response.buffer[i]);
   printf("  |  ");
   for(i=16;i<48;i++) printf(" %2x", response.buffer[i]);
- 
+
   printf("\n\n");
- 
- 
+
+
   for(i=0;i<flockNumber;i++)
     {
       Bird[i].status.status             = response.buffer[16+i*8];
@@ -571,9 +571,9 @@ void aMotionStar::get_status_all()
       Bird[i].status.hemisphere = response.buffer[26+i*8];
       printf("Bird hemisphere = %2x \n", Bird[i].status.hemisphere);
     }
- 
+
   printf("");
- 
+
   /*
     for (i=0;i<bytesReceived;++i)
     {
@@ -581,55 +581,55 @@ void aMotionStar::get_status_all()
     }
     printf("\n");
 */
- 
+
 } // end void aMotionStar::get_status_all()
 
 
- 
+
 void aMotionStar::set_status_all()
 {
      int i;
      response.header.type=102;
      response.header.xtype=0;
      response.header.number_bytes = statusSize-16;
- 
+
      for(i=0;i<6;i++)
         {
         response.buffer[5+i] = rate[i];
         }
- 
+
      numberBytes = sendto(s,(void*)lpResponse, statusSize, 0,
                  (struct sockaddr *)&server, sizeof(server));
- 
+
      printf("\nSEND STATUS - number bytes sent = %5d errorNumber %d", numberBytes,errno);
- 
+
      /***** Receive packet from the server *****/
- 
+
      bytesReceived = recvfrom(s, (void*)lpResponse, sizeof(response), 0,
                               (struct sockaddr *)&server, lpSize);
      printf("\nSEND STATUS ACK - number bytes received = %5d,", bytesReceived);
      printf("  type %d\n",response.header.type);
 
 } // end void aMotionStar::set_status_all()
- 
- 
- 
+
+
+
 void aMotionStar::get_status_fbb(unsigned char fbb_addr)
 {
   int headerBytes, dataBytes;
   void * lpBuffer;
- 
- 
+
+
   command.type=101;
   command.xtype=fbb_addr;
   command.number_bytes = 0;
- 
+
   numberBytes = sendto(s,(void*)lpCommand, sizeof(command), 0,
                        (struct sockaddr *)&server, sizeof(server));
   printf("\n\nGET STATUS #%d; number bytes sent = %5d errorno %d ",fbb_addr, numberBytes,errno);
- 
+
   /***** Receive packet from the server *****/
- 
+
   headerBytes = 0;
   lpBuffer = lpResponse;
   while(headerBytes<16)
@@ -638,19 +638,19 @@ void aMotionStar::get_status_fbb(unsigned char fbb_addr)
       headerBytes = headerBytes + bytesReceived;
       lpBuffer = (char *)lpBuffer + bytesReceived;
     }
- 
+
   dataBytes = 0;
   while(dataBytes<(response.header.number_bytes))
     {
       bytesReceived = recvfrom(s, (void*)lpBuffer, response.header.number_bytes,
- 
+
                                0, (struct sockaddr *)&server, lpSize);
       dataBytes = dataBytes + bytesReceived;
       lpBuffer = (char *)lpBuffer + bytesReceived;
     }
- 
+
   bytesReceived = dataBytes + headerBytes;
- 
+
   printf("\nSTATUS RECEIVED - number bytes received = %5d ", bytesReceived);
   printf("type %d ",response.header.type);
   for (i=0;i<bytesReceived;++i)
@@ -659,40 +659,40 @@ void aMotionStar::get_status_fbb(unsigned char fbb_addr)
 } // end void aMotionStar::get_status_fbb()
 
 
- 
+
 void aMotionStar::display_hdr()
 {
- 
- 
+
+
   /*printf("sequence=%6d time= %8d milliseconds=%6d type= %4d "
 	,response.header.sequence);*/
 
 } // end void aMotionStar::display_hdr()
- 
 
- 
+
+
 void aMotionStar::set_status_fbb(unsigned char fbb_addr)
 {
   response.header.type = 102;
   response.header.xtype = fbb_addr;
   response.header.number_bytes = 70;
- 
+
   numberBytes = sendto(s,(void*)lpResponse, 86, 0,
                        (struct sockaddr *)&server, sizeof(server));
   printf("\nSEND SETUP #%d; number bytes sent = %5d errorno %d ",fbb_addr, numberBytes,errno);
- 
+
   /***** Receive packet from the server *****/
- 
+
   bytesReceived = recvfrom(s, (void*)lpResponse, sizeof(response), 0,
                            (struct sockaddr *)&server, lpSize);
   printf("\nSETUP ACKNOWLEDGED - number bytes received = %5d ", bytesReceived);
   printf("type %d ",response.header.type);
   for (i=0;i<bytesReceived;++i)
     printf("%4d",*(newptr+i));
- 
+
 } // end void aMotionStar::set_status_fbb()
- 
- 
+
+
 float aMotionStar::rawToFloat(char r1,  char r2){
   short int ival1, ival2, val;
   ival1 = r1 & 0xff;
@@ -708,7 +708,7 @@ void aMotionStar::setBirdFormat (unsigned int n) { birdFormat = n; }
 void aMotionStar::setNumBirds (unsigned int n) { birdsRequired = n; }
 void aMotionStar::setBirdRate (double n) { birdRate = n; }
 void aMotionStar::setRunMode ( int n ) { runMode = n; }
-void aMotionStar::setReportRate (unsigned char n) { reportRate = n; } 
+void aMotionStar::setReportRate (unsigned char n) { reportRate = n; }
 
 float aMotionStar::xPos( int i) {return posinfo[i][0];}
 float aMotionStar::yPos( int i) {return posinfo[i][1];}
