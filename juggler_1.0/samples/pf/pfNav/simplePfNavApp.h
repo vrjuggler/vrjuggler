9769505aef9d34b85b3d6c020bd4a8a0869dbdfc
Allen Bierbaum <abierbaum@gmail.com>
2000-06-07 04:55:03 +0000

    -Fisrt version of configurable pfNav application The configurable application derives from the simplePfNavApp, so there are some changes in both classes There is also now a chunks desc file (pfNavChunks.dsc) in the directory.  It defines the chunks for pfNav applications Currently, the only chunk used in the application is pf_basic_nav_app
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2091 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/samples/pf/pfNav/simplePfNavApp.h
+++ juggler_1.0/samples/pf/pfNav/simplePfNavApp.h
@@ -52,7 +52,12 @@ class simplePfNavApp : public vjPfApp
 public:
    simplePfNavApp( vjKernel* kern ) : mStatusMessageEmitCount(0), vjPfApp( kern )
    {
-      // get a valid time on the stopwatch...
+      mSun = NULL;
+      mRootNode = NULL;
+      mWorldModel = NULL;
+      mWorldDCS = NULL;
+      mCollidableModelGroup = NULL;
+      enableStats();
    }
 
    virtual void init()
@@ -65,36 +70,45 @@ public:
 
    virtual void apiInit()
    {
-      vjDEBUG(vjDBG_ALL,1) << "pfNavJugglerApplication::apiInit\n" << vjDEBUG_FLUSH;
+      vjDEBUG(vjDBG_ALL,1) << "simplePfNavApp::apiInit\n" << vjDEBUG_FLUSH;
    }
 
    virtual void preForkInit()
    {
       // Initialize type system
-      vjDEBUG(vjDBG_ALL,1) << "pfNavJugglerApplication::preForkInit: Initializing new types.\n" << vjDEBUG_FLUSH;
+      vjDEBUG(vjDBG_ALL,1) << "simplePfNavApp::preForkInit: Initializing new types.\n" << vjDEBUG_FLUSH;
 
       // Initialize loaders
-      //pfdInitConverter("terrain.flt");
+      if(!mModelFileName.empty())
+         pfdInitConverter(mModelFileName.c_str());
 
       pfNavDCS::init();
-      mStats.preForkInit();
+      if(mUseStats)
+         mStats.preForkInit();
    }
 
    /// Initialize the scene graph
    virtual void initScene();
 
+   // load the model into the scene graph
+   // If another model is already in the scene graph, we destroy it and load the newly configured one.
+   // The model loaded is based on the configuration information that we currently have
+   // This may be called multiple times
+   virtual void initializeModel();
+
    /// Return the current scene graph
    virtual pfGroup* getScene()
    {
-      vjDEBUG(vjDBG_ALL, 0) << "pfNavJugglerApplication::getScene\n" << vjDEBUG_FLUSH;
-      return rootNode;
+      vjDEBUG(vjDBG_ALL, 0) << "simplePfNavApp::getScene\n" << vjDEBUG_FLUSH;
+      return mRootNode;
    }
 
    //: Function called in application process for each active channel each frame
    // Called immediately before draw (pfFrame())
    virtual void appChanFunc(pfChannel* chan)
    {
-      mStats.appChanFunc(chan);
+      if(mUseStats)
+         mStats.appChanFunc(chan);
    }
 
    // Function called by the DEFAULT drawChan function before clearing the channel
@@ -103,7 +117,8 @@ public:
    {
       //pfDisable( PFEN_TEXTURE );
       //pfOverride(PFSTATE_ENTEXTURE, PF_OFF);
-      mStats.preDrawChan(chan,chandata);
+      if(mUseStats)
+         mStats.preDrawChan(chan,chandata);
    }
 
    /// Function called after pfSync and before pfDraw
@@ -117,7 +132,8 @@ public:
       }
 
       // Update stats stuff
-      mStats.preFrame();
+      if(mUseStats)
+         mStats.preFrame();
    }
 
    /// Function called after pfDraw
@@ -132,6 +148,9 @@ public:  // Configure the application
    void setWorldDcsTrans(const vjVec3 trans)       {mWorldDcsTrans = trans;}
    void setInitialNavPos(const vjVec3 initialPos)  { mInitialNavPos = initialPos; }
 
+   void enableStats() { mUseStats = true;}
+   void disableStats() {mUseStats = false;}
+
 public:
    // CONFIG PARAMS
    std::string    mModelFileName;
@@ -140,18 +159,62 @@ public:
    vjVec3         mWorldDcsTrans;
    vjVec3         mInitialNavPos;
 
-
    int mStatusMessageEmitCount;
+
+   bool           mUseStats;
+   vjPfAppStats   mStats;
 
    // navigation objects.
    velocityNav*   mVelNavDrive;
    pfNavDCS*      mNavigationDCS;
 
-   // scene's root (as far as we're concerned here)
-   pfGroup*       rootNode;
-
-   vjPfAppStats   mStats;
+   // SCENE GRAPH NODES
+   pfLightSource* mSun;                      // Sun to light the environment
+   pfGroup*       mRootNode;                 // The root of the scene graph
+   pfNode*        mWorldModel;               // The model of the world
+   pfDCS*         mWorldDCS;                 // DCS to transform the world
+   pfGroup*       mCollidableModelGroup;     // Part of the world that is collidable
 };
+
+// ------- SCENE GRAPH ----
+//
+//                           /-- sun1
+// rootNode -- mNavigationDCS -- mWorldDCS -- collidable_modelGroup -- world_model
+//
+void simplePfNavApp::initializeModel()
+{
+   if(NULL == mRootNode)      // If we haven't had initScene called yet
+   {
+      return;
+   }
+
+   // ----------- DESTROY OLD -------- //
+   // REMOVE old Model
+   if(mWorldModel != NULL)    // If we already have one world model
+   {
+      mCollidableModelGroup->removeChild(mWorldModel);
+   }
+
+   // Destory any old data
+   // XXX: Delete old stuff
+
+   // ------------- LOAD NEW --------- //
+   // add some common file paths.
+   pfFileIO::setFilePath( ".:./data:/usr/share/Performer/data:/usr/share/Performer/data/town:");
+   pfFileIO::addFilePath( mFilePath );
+
+   // LOAD file
+   mWorldModel = pfFileIO::autoloadFile( mModelFileName, pfFileIO::FEET );
+
+   // --- CONSTRUCT SCENE GRAPH --- //
+   pfVec3 pf_worldDcsTrans;
+   pf_worldDcsTrans = vjGetPfVec( mWorldDcsTrans );
+
+   mWorldDCS->setScale( mWorldDcsScale );
+   mWorldDCS->setTrans( pf_worldDcsTrans[0], pf_worldDcsTrans[1], pf_worldDcsTrans[2]);
+
+   mCollidableModelGroup->addChild(mWorldModel);
+}
 
 void simplePfNavApp::initScene()
 {
@@ -159,41 +222,28 @@ void simplePfNavApp::initScene()
    vjDEBUG(vjDBG_ALL, 0) << "pfNavJugglerApplication::initScene\n" << vjDEBUG_FLUSH;
 
    // Allocate all the nodes needed
-   rootNode                = new pfGroup;          // Root of our graph
-   mNavigationDCS          = new pfNavDCS;      // DCS to navigate with
-   pfNode* world_model;
-   pfDCS*   world_model_dcs = new pfDCS;
-   pfGroup* collidable_modelGroup = new pfGroup;
+   mRootNode             = new pfGroup;       // Root of our graph
+   mNavigationDCS        = new pfNavDCS;      // DCS to navigate with
+   mCollidableModelGroup = new pfGroup;
+   mWorldDCS             = new pfDCS;
 
    // Create the SUN
-   pfLightSource* sun1 = new pfLightSource;
-   sun1->setPos( 0.3f, 0.0f, 0.3f, 0.0f );
-   sun1->setColor( PFLT_DIFFUSE,0.3f,0.0f,0.95f );
-   sun1->setColor( PFLT_AMBIENT,0.4f,0.4f,0.4f );
-   sun1->setColor( PFLT_SPECULAR, 1.0f, 1.0f, 1.0f );
-   sun1->on();
+   mSun = new pfLightSource;
+   mSun->setPos( 0.3f, 0.0f, 0.3f, 0.0f );
+   mSun->setColor( PFLT_DIFFUSE,0.3f,0.0f,0.95f );
+   mSun->setColor( PFLT_AMBIENT,0.4f,0.4f,0.4f );
+   mSun->setColor( PFLT_SPECULAR, 1.0f, 1.0f, 1.0f );
+   mSun->on();
 
-   /// Load SIMPLE geometry
-   // add some common file paths.
-   pfFileIO::addFilePath( ".:./data:/usr/share/Performer/data:/usr/share/Performer/data/town");
-   pfFileIO::addFilePath( mFilePath );
+   // --- Load the model (if it is configured) --- //
+   initializeModel();
 
-   // LOAD file
-   world_model = pfFileIO::autoloadFile( mModelFileName, pfFileIO::FEET );
+   // --- CONSTRUCT STATIC Structure of SCENE GRAPH -- //
+   mRootNode->addChild( mNavigationDCS );
+   mNavigationDCS->addChild(mWorldDCS);
+   mWorldDCS->addChild(mCollidableModelGroup);
 
-   // --- CONSTRUCT SCENE GRAPH --- //
-   //                           /-- sun1
-   // rootNode -- mNavigationDCS -- collidable_modelGroup -- world_model
-   //                           \-- noncollidable_modelGroup -- other_models
-   pfVec3 pf_worldDcsTrans;
-   pf_worldDcsTrans = vjGetPfVec( mWorldDcsTrans );
-   
-   rootNode->addChild( mNavigationDCS );
-   mNavigationDCS->setScale( mWorldDcsScale );
-   mNavigationDCS->setTrans( pf_worldDcsTrans[0], pf_worldDcsTrans[1], pf_worldDcsTrans[2]);
-   mNavigationDCS->addChild( sun1 );
-   mNavigationDCS->addChild( collidable_modelGroup );
-   collidable_modelGroup->addChild(world_model);
+   mRootNode->addChild(mSun);
 
    // Configure the Navigator DCS node:
    vjMatrix initial_nav;              // Initial navigation position
@@ -204,30 +254,23 @@ void simplePfNavApp::initScene()
    mVelNavDrive->setCurPos(initial_nav);
    mVelNavDrive->setMode(velocityNav::DRIVE);
 
-      //mVelNavDrive->setMode(velocityNav::FLY);
-
    // --- COLLISION DETECTORS --- //
    // Terrain collider
    //planeCollider* collide = new planeCollider;
-   pfPogoCollider*  ride_collide = new pfPogoCollider(collidable_modelGroup);
+   pfPogoCollider*  ride_collide = new pfPogoCollider(mCollidableModelGroup);
    mVelNavDrive->addCollider(ride_collide);
 
    // Set the navigator's collider.
-   pfBoxCollider* correction_collide = new pfBoxCollider( collidable_modelGroup );
+   pfBoxCollider* correction_collide = new pfBoxCollider( mCollidableModelGroup );
    mVelNavDrive->addCollider( correction_collide );
 
    mNavigationDCS->setNavigator(mVelNavDrive);
 
-   // replace all nodes with _Sound_ with pjSoundNodes...
-/*
-   pjSoundReplaceTrav::traverse( collidable_modelGroup, gSoundEngine, "_Sound_" );
-   assert( NULL);
-*/
    // load these files into perfly to see just what your scenegraph
    // looked like. . . . .useful for debugging.
-   cout<<"Saving entire scene into lastscene.pfb, COULD TAKE A WHILE!\n"<<flush;
-   pfuTravPrintNodes( rootNode, "lastscene.out" );
-   pfdStoreFile( rootNode, "lastscene.pfb" );
+   cout << "Saving entire scene into lastscene.pfb, COULD TAKE A WHILE!\n" << flush;
+   pfuTravPrintNodes( mRootNode, "lastscene.out" );
+   pfdStoreFile( mRootNode, "lastscene.pfb" );
 }
 
 #endif
