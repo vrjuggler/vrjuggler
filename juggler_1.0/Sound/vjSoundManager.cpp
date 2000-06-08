18671bd920d5b14deac2e33db55d0227833939cf
Patrick Hartling <patrick.hartling@gmail.com>
2000-06-08 17:59:51 +0000

    Added a simple protected constructor that initializes mSoundEngine to NULL.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2143 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/Sound/vjSoundManager.cpp
+++ juggler_1.0/Sound/vjSoundManager.cpp
@@ -37,6 +37,10 @@
 
 vjSoundManager* vjSoundManager::_instance = NULL;   //: The instance ptr
 
+vjSoundManager::vjSoundManager () {
+    mSoundEngine = NULL;
+}
+
 //: Add the chunk to the configuration
 //! PRE: configCanHandle(chunk) == true
 bool vjSoundManager::configAdd(vjConfigChunk* chunk)
