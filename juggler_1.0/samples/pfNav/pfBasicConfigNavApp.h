68544c3b2d4eb439114233370fa88c1f6f77c489
Kevin Meinert <subatomic@users.sourceforge.net>
2000-06-09 09:29:52 +0000

    new config chunkdescs for pfNav app. and configApp function supports it too...
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2172 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/samples/pfNav/pfBasicConfigNavApp.h
+++ juggler_1.0/samples/pfNav/pfBasicConfigNavApp.h
@@ -60,7 +60,7 @@ public:     // RECONFIG STUFF
    //    retval == true, application will be allowed to enter the system
    virtual bool depSatisfied()
    { return true; }
-
+        
 protected:
    //! NOTE: Inherited from vjConfigChunkHandler
    virtual bool configAdd(vjConfigChunk* chunk)
@@ -77,7 +77,6 @@ protected:
    vjConfigChunk* mCurrentConfig;      // config chunk that is current configuring this thingie
 };
 
-
 // Get the properties from the chunks
 // XXX: Smart update
 // Set the properties
@@ -87,30 +86,57 @@ void pfBasicConfigNavApp::configApp(vjConfigChunk* chunk)
    vjASSERT(std::string("pf_basic_nav_app") == (std::string)chunk->getType());
 
    // Set the properties based on the chunk values
-   std::string file_name = chunk->getProperty("model_name");
+   std::string app_name = chunk->getProperty("Name");
    std::string file_path = chunk->getProperty("file_path");
-   float world_scale = chunk->getProperty("scale");
-   vjVec3 dcs_trans;
-   //dcs_trans.set(chunk->getProperty("
-   //setWorldDcsTrans(dcs_trans);
    vjVec3 initial_pos;
    initial_pos.set(chunk->getProperty("start_location",0),
                    chunk->getProperty("start_location",1),
                    chunk->getProperty("start_location",2));
 
-   setFileName(file_name);
+   // sounds
+   for (int x = 0; x < chunk->getNum( "Sounds" ); ++x)
+   {
+      vjConfigChunk* sound_chunk = chunk->getProperty( "Sound", x );
+      sound s;
+      s.name = sound_chunk->getProperty( "Name" );
+      s.alias = sound_chunk->getProperty( "soundAlias" );
+      s.positional = sound_chunk->getProperty( "positional" );
+      s.pos.set( sound_chunk->getProperty( "x" ),
+            sound_chunk->getProperty( "y" ),
+            sound_chunk->getProperty( "z" ) );
+      mSoundList.push_back( s );
+   }
+   
+   // models
+   for (int x = 0; x < chunk->getNum( "Models" ); ++x)
+   {
+      vjConfigChunk* model_chunk = chunk->getProperty( "Model", x );
+      model m;
+      m.description = model_chunk->getProperty( "Name" );
+      m.filename = model_chunk->getProperty( "filename" );
+      m.scale = model_chunk->getProperty( "Scale" );
+      m.pos.set( model_chunk->getProperty( "x" ),
+            model_chunk->getProperty( "y" ),
+            model_chunk->getProperty( "z" ) );
+      m.rot.set( model_chunk->getProperty( "rotx" ),
+            model_chunk->getProperty( "roty" ),
+            model_chunk->getProperty( "rotz" ) );
+      mModelList.push_back( s );
+   }
+   
    setFilePath(file_path);
-   setWorldDcsScale(world_scale);
    setInitialNavPos(initial_pos);
 
+   vjDEBUG(vjDBG_ALL,0) << "\_\_\_\_\_\_\_\_\_\_\_ " << app_name << " \_\_\_\_\_\_\_\_\n" << vjDEBUG_FLUSH;
    vjDEBUG(vjDBG_ALL,0) << "pfBasicConfigNav::configApp:  ========= configuration application =========\n" << vjDEBUG_FLUSH;
    vjDEBUG_CONT(vjDBG_ALL,0) << "filename: " << file_name << endl << vjDEBUG_FLUSH;
    vjDEBUG_CONT(vjDBG_ALL,0) << "filepath: " << file_path << endl << vjDEBUG_FLUSH;
    vjDEBUG_CONT(vjDBG_ALL,0) << "world scale: " << world_scale << endl << vjDEBUG_FLUSH;
    vjDEBUG_CONT(vjDBG_ALL,0) << "initial pos: " << initial_pos << endl << vjDEBUG_FLUSH;
 
-   // Initialize the model
-   initializeModel();
+   // Initialize the models and sounds
+   initializeModels();
+   initializeSounds();
 }
 
 
