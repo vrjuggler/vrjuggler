f7bf3d5a679d48d0968f1a680d96d2695e909646
Kevin Meinert <subatomic@users.sourceforge.net>
2000-06-14 03:56:28 +0000

    future features... commented out...
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2243 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/Sound/vjSoundHandle.h
+++ juggler_1.0/Sound/vjSoundHandle.h
@@ -3,14 +3,24 @@
 class vjSoundHandle
 {
 public:
-	vjSoundHandle() : mIdentifier( 0 )
+   // creates stale by default...
+	vjSoundHandle() : mIdentifier( 0 ), mStale( true ), mName( "no name" )
 	{
 	}
-
-	void setId( const int& id )
-	{
-		mIdentifier = id;
-	}
-
+   
+   const std::string& name() const { return mName; }
+   const int&         id() const { return mIdentifier; }
+   const bool&        isStale() const { return mStale; }
+   
+protected: 
+   void               setName( const std::string& name ) { mName = name; }
+	void               setId( const int& id ) { mIdentifier = id; }
+   void               setStale( const bool& stale ) { mStale = stale; }
+   
+   friend class vjSoundManager;
+   
+private:
+   bool mStale;
 	int mIdentifier;
+   std::string mName;
 };
