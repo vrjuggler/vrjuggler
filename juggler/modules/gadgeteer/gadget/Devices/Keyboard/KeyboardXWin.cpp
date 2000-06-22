8c5195a9cd5e56704b528e0669ce6aa166df86fd
Kevin Meinert <subatomic@users.sourceforge.net>
2000-06-22 21:38:23 +0000

    made the key board sample rate default to 16hz if 0 is returned for the sleep_time property.
    read my comments in the file for more info...
    
    This is a different change than the one I did before... I may remove that
    one... It just ensures that each sample frame that usleep is at least
    1us... which is a lot different than saying 1ms (1000us)
    
    basically 1 is like 0, a yield..  of course it's unattainable now that I
    set the config function to reject 0.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2313 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/gadget/Devices/Keyboard/KeyboardXWin.cpp
+++ juggler/modules/gadgeteer/gadget/Devices/Keyboard/KeyboardXWin.cpp
@@ -87,9 +87,29 @@ bool vjXWinKeyboard::config(vjConfigChunk *c)
                << m_mouse_sensitivity << endl << vjDEBUG_FLUSH;
     vjDEBUG_END(vjDBG_INPUT_MGR, vjDBG_STATE_LVL) << endl << vjDEBUG_FLUSH;
 
+    // KEVIN: 
+    // Note:      that this function returns 0 if the property is not found.
+    // Also Note: that in IRIX, usleep of 0 does not yield.
     mSleepTimeMS = c->getProperty("sleep_time");
-    if(mSleepTimeMS <  0)
-       mSleepTimeMS = 0;
+    
+    // KEVIN: 
+    // If sleep_time property is 0, then set it to some "reasonable" sample rate.
+    // Default value for a "reasonable" sample rate is 62.5ms (16hz)
+    //
+    // NOTES:
+    // 0ms is an excessive default for keyboard sampling.  
+    //      Never allow 0 since:
+    //      a.) 0 could be a default or user entered, and  
+    //      b.) usleep of 0 within a while(1) thread could starve all other processes
+    // 1ms is also excessive, (1000hz sample rate for a lowly keyboard input device?!?!)
+    //      But we'll allow 1ms since:
+    //      a.) 1+ is definately user entered, and
+    //      b.) at least the IRIX usleep will yield
+    // We really only need about 8-16hz (in my opinion)...
+    //      note that:  8hz == 125ms  and  16hz == 62.5ms  
+    //
+    if(mSleepTimeMS <=  0)
+       mSleepTimeMS = 62.5f; 
 
     return true;
 }
