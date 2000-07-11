12c278e06ebff39cb6c0cb62fe4de904b66728bd
Allen Bierbaum <abierbaum@gmail.com>
2000-07-11 15:42:07 +0000

    -Removed thread specific data from manager   Moved it to the vjBaseThread class instead -Added fucntion for generating thread specifics keys.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2383 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/Threads/vjThreadManager.cpp
+++ juggler_1.0/Threads/vjThreadManager.cpp
@@ -46,7 +46,7 @@ vjThreadManager* vjThreadManager::_instance = NULL;
 //-----------------------------------------------------
 void vjThreadManager::addThread(vjBaseThread* thread)
 {
-   vjASSERT((mThreadVectorMutex.test()==1) && (mTSMutex.test()==1)); // Assert manager locked
+   vjASSERT(mThreadVectorMutex.test()==1); // Assert manager locked
    vjASSERT(thread->getTID() >= 0);
 
    // Insert thread into local table
@@ -54,7 +54,6 @@ void vjThreadManager::addThread(vjBaseThread* thread)
       mThreads.push_back(NULL);
    mThreads[thread->getTID()] = thread;
 
-   tsThreadAdded(thread);     // Tell TS routines that thread added
    debugDump();               // Dump current state
 }
 
@@ -66,10 +65,9 @@ void vjThreadManager::addThread(vjBaseThread* thread)
 //-----------------------------------------------------
 void vjThreadManager::removeThread(vjBaseThread* thread)
 {
-   vjASSERT((mThreadVectorMutex.test()==1) && (mTSMutex.test()==1)); // Assert manager locked
+   vjASSERT(mThreadVectorMutex.test()==1); // Assert manager locked
    vjASSERT((thread->getTID() >= 0) && (thread->getTID() < (int)mThreads.size()));
-   mThreads[(unsigned int)thread->getTID()] = NULL;
-   tsThreadRemoved(thread);   // Tell TS routines that thread removed
+   mThreads[(unsigned int)thread->getTID()] = NULL;   
 }
 
 
@@ -99,86 +97,6 @@ void vjThreadManager::debugDump()
 }
 
 
-//: Add an object to all the tables
-// Creates a new key
-// Adds object to all tables using that key
-long vjThreadManager::addTSObject(vjTSBaseObject* object)
-{
-vjGuard<vjMutex> guard(mTSMutex);      // MUTEX Protection
-   long new_key = generateNewTSKey();
-
-   mBaseTSTable.setObject(object, new_key);     // Set it in the base table
-   for (unsigned int i=0;i<mTSTables.size();i++) // For all thread tables
-      mTSTables[i]->setObject(object, new_key); // Add the object (table is mutex protected)
-
-   return new_key;
-}
 
 
-//: Remove objects of given key from the tables
-// Requests that each table release the object
-void vjThreadManager::removeTSObject(long key)
-{
-vjGuard<vjMutex> guard(mTSMutex);      // MUTEX Protection
-   mBaseTSTable.releaseObject(key);
-   for (unsigned int i=0;i<mTSTables.size();i++)   // For all thread tables
-      mTSTables[i]->releaseObject(key);   // relase - (table is mutex protected)
-}
 
-//: Return a ptr to the thread table of the current process
-// Queries the TID of self.
-// Looks up the element in the local TS Table based on this id
-vjTSTable* vjThreadManager::getCurrentTSTable()
-{
-   vjBaseThread* cur_thread;
-   int32_t thread_id;
-
-   cur_thread = vjThread::self();               // Get current thread
-   thread_id = cur_thread->getTID();            // Get thread id
-
-   vjTSTable* ret_val = cur_thread->getTSTable();     // Get the cached copy
-
-   if(ret_val == NULL)     // If it's null, then it needs set
-   {
-   vjGuard<vjMutex> guard(mTSMutex);            // MUTEX Protection
-      vjASSERT((thread_id >= 0) && (thread_id < mTSTables.size()));
-      ret_val =  mTSTables[thread_id];          // Get the table for that id
-      cur_thread->setTSTable(ret_val);
-   }
-   return ret_val;
-}
-
-//: Called when a thread has been added to the system
-// Creates a new table for the thread
-// Puts table in the vector
-//! PRE: Manager must be locked
-void vjThreadManager::tsThreadAdded(vjBaseThread* thread)
-{
-   vjASSERT((mThreadVectorMutex.test()) && (mTSMutex.test())); // Assert manager locked
-   vjASSERT(thread->valid());
-
-   // Create the new table
-   vjTSTable* new_table = mBaseTSTable.createNew();
-
-   // Make sure there is room for new table
-   while (mTSTables.size() <= thread->getTID())
-      mTSTables.push_back(NULL);
-
-   mTSTables[thread->getTID()] = new_table;     // Add the new table to the local list
-   thread->setTSTable(new_table);               // Set the cached copy in the thread
-}
-
-
-//: Called when a thread has been removed from the system
-// Deletes the table assigned to that thread
-// Remove the reference to that table too
-//! PRE: Manager must be locked
-void vjThreadManager::tsThreadRemoved(vjBaseThread* thread)
-{
-   vjASSERT((mThreadVectorMutex.test()) && (mTSMutex.test())); // Assert manager locked
-    // Find this threads table
-   if (mTSTables[thread->getTID()] != NULL)
-      delete mTSTables[thread->getTID()];   // Delete it
-
-   mTSTables[thread->getTID()] = NULL;
-}
