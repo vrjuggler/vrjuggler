811c94120baa414c216601ae9e2c02de6e03aec0
Patrick Hartling <patrick.hartling@gmail.com>
2000-08-17 20:07:41 +0000

    Share the directory list used by PERCEPS in the new file subdirs.mk.  The configure-generated makefile now works when the user directly calls its targets rather than only working when invoked via the top-level makefile. Chalk that up as a bug fix.  :)
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@2488 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ Doc/subdirs.mk
@@ -0,0 +1,19 @@
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# This include file <subdirs.mk> lists the subdirectories over which PERCEPS
+# recurses when it does its thing.  These directories are listed in the
+# $(SUBDIRS) variable which can be used by the includer.
+# -----------------------------------------------------------------------------
+# The including makefile must define the following variable:
+#
+#     top_srcdir - The top directory where the directories containing source
+#                  code to be parsed can be found.
+# -----------------------------------------------------------------------------
+
+SUBDIRS		= $(top_srcdir)/Config $(top_srcdir)/Environment	\
+                  $(top_srcdir)/Input $(top_srcdir)/Kernel		\
+                  $(top_srcdir)/Math $(top_srcdir)/Performance		\
+                  $(top_srcdir)/SharedMem $(top_srcdir)/Sound		\
+                  $(top_srcdir)/Syn $(top_srcdir)/Threads		\
+                  $(top_srcdir)/Utils
