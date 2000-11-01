6d783b14c6ab61d6d6ba783d69b828cfd3ea8bf4
Allen Bierbaum <abierbaum@gmail.com>
2000-11-01 20:00:41 +0000

    - Commented out the helpful sytlesheet include until such a time as the install web supports the ability to tell it what not to copy over in a file. - Updated TOC to fix a few spelling changes. - Changed wording in index.html to not say introduction.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2919 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_docs/install/make.allen.pl
+++ juggler_docs/install/make.allen.pl
@@ -1,19 +1,19 @@
-#! perl
+#!/usr/freeware/bin/perl
 # Options:  -a     recompile everything
 #           -n     don't run netscape after compile
 
-
 require 5.004;
-use Getopt::Std;
 
+
+use Getopt::Std;
 
 getopt('');
 my $src_dir = "../.";
-my $dest_dir = "/home/users/allenb/public_html/temp_vrj/";
+my $dest_dir = "/home/users/allenb/public_html/vrj_test/";
 my $css_filename = "html/css_header.html";
-my $html_header_filename = "";
+my $html_header_filename = "html/header.html";
 my $html_footer_filename = "html/footer.html";
-my $html_install_prefix = "http://www.vrac.iastate.edu/~allenb/temp_vrj";
+my $html_install_prefix = "http://www.vrac.iastate.edu/~allenb/vrj_test/";
 my $subst_file = "subst.pl";
 
 
@@ -21,23 +21,24 @@ my $subst_file = "subst.pl";
 my $command = "install-web.pl ";
 $command .= " -s $src_dir";
 $command .= " -t $dest_dir";
-$command .= " -i ";
+$command .= " -i cgi-bin,__deleteme,install,CVS,ver1.05,ver1.08,bak,programmer.reference";
 $command .= " -c $css_filename";
-$command .= " -f $html_header_filename";
+$command .= " -e $html_header_filename";
 $command .= " -f $html_footer_filename";
 $command .= " -h $html_install_prefix";
 $command .= " -w $subst_file";
-$command .= " -z use.last.build.pl";
-if ($opt_a == 1)
+$command .= " -z most.recent.pl";
+if($opt_a == 1)
 {
-    $command .= " -a";
+$command .= " -a";
 }
 
-print "$command\n\n\n\n----------------\n\n";
+print "$command\n\n\n\n-------------\n\n";
 system("$command");
-if ($opt_n == 0)
+
+if($opt_n == 0)
 {
-    my $netscape_command = "netscape " . $html_install_prefix . "index.html &";
-    print "$netscape_command\n";
-    system("$netscape_command");
+  my $netscape_command = "netscape " . $html_install_prefix . "index.html &";
+  print "$netscape_command\n";
+  system("$netscape_command");
 }
