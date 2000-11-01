a4b96597f7355ba2bd5d5db66ca6d4c5611d7684
Allen Bierbaum <abierbaum@gmail.com>
2000-11-01 16:32:55 +0000

    Updated.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@2915 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_docs/install/make.allen.pl
+++ juggler_docs/install/make.allen.pl
@@ -1,18 +1,19 @@
-#!/usr/freeware/bin/perl
+#! perl
 # Options:  -a     recompile everything
 #           -n     don't run netscape after compile
+
 
 require 5.004;
 use Getopt::Std;
 
-getopt('');
 
+getopt('');
 my $src_dir = "../.";
 my $dest_dir = "/home/users/allenb/public_html/temp_vrj/";
-#my $dest_dir = "/var/tmp/web_copy";
 my $css_filename = "html/css_header.html";
+my $html_header_filename = "";
 my $html_footer_filename = "html/footer.html";
-my $html_install_prefix = "http://www.vrac.iastate.edu/~allenb/temp_vrj/";
+my $html_install_prefix = "http://www.vrac.iastate.edu/~allenb/temp_vrj";
 my $subst_file = "subst.pl";
 
 
@@ -20,19 +21,21 @@ my $subst_file = "subst.pl";
 my $command = "install-web.pl ";
 $command .= " -s $src_dir";
 $command .= " -t $dest_dir";
-$command .= " -i __deleteme,install,CVS,ver1.05,ver1.08,bak";
+$command .= " -i ";
 $command .= " -c $css_filename";
+$command .= " -f $html_header_filename";
 $command .= " -f $html_footer_filename";
 $command .= " -h $html_install_prefix";
 $command .= " -w $subst_file";
-if($opt_a == 1)
+$command .= " -z use.last.build.pl";
+if ($opt_a == 1)
 {
-$command .= " -a";
+    $command .= " -a";
 }
 
 print "$command\n\n\n\n----------------\n\n";
 system("$command");
-if($opt_n == 0)
+if ($opt_n == 0)
 {
     my $netscape_command = "netscape " . $html_install_prefix . "index.html &";
     print "$netscape_command\n";
