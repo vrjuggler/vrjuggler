5cde956b481091a8f7eaa47d71b3c87a781bd225
Patrick Hartling <patrick.hartling@gmail.com>
2000-11-02 07:00:52 +0000

    Updated to reflect changes in the driver.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler_1.0@2935 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- test/input/aMotionStarTest.cpp
+++ test/input/aMotionStarTest.cpp
@@ -1,17 +1,3 @@
-#include <iostream.h>
-#include <iomanip.h>
-#include <SharedMem/vjMemPool.h>
-//#include <SharedMem/vjSharedType.h>
-#include <Input/vjPosition/aMotionStar.h>
-#include <Math/vjCoord.h>
-
-int main()
-{
-  vjMemPool* aMemPool = new vjSharedPool(1024*1024);
-
-  // Get configuration information \
-
-  cerr << "-------- Bird Test program -------\n";
 /*************** <auto-copyright.pl BEGIN do not edit this line> **************
  *
  * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
@@ -43,27 +29,143 @@ int main()
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
-                  //cout << " Bird: " << bird;
-                  cout << " " << setprecision(8)  << motionstar->xPos(bird)
-                       << " " << setprecision(8)  << motionstar->yPos(bird)
-                       << " " << setprecision(8)  << motionstar->zPos(bird)
-                       << " " << setprecision(8)  << motionstar->xRot(bird)
-                       << " " << setprecision(8) << motionstar->yRot(bird)
-                       << " " << setprecision(8) << motionstar->zRot(bird);
+
+#include <iostream>
+#include <iomanip>
+#include <SharedMem/vjMemPool.h>
+//#include <SharedMem/vjSharedType.h>
+#include <Input/vjPosition/aMotionStar.h>
+
+
+int
+main () {
+  vjMemPool* aMemPool = new vjSharedPool(1024*1024);
+
+  // Get configuration information \
+
+  std::cerr << "-------- MotionStar Test program -------\n";
+/*            << "First we need some config info:\n"
+            << "NOTE: Sample values are in ()'s\n" << std::flush;
+
+  std::cout << "\nEnter port (/dev/ttyd3): " << std::flush;
+  std::cin >> port;
+  std::cout << "\nEnter baud rate (38400): " << std::flush;
+  std::cin >> baud;
+*/
+//  std::cout << "\nHow many birds (3): " << std::flush;
+//  std::cin >> birdsRequired;
+/*
+  std::cout << "\nNot using calibration file." << std::flush;
+  std::cout << "\nUsing LOWER_HEM" << std::flush;
+  std::cout << "\nUsing AC_NARROW filter" << std::flush;
+  std::cout << "\nEnter Sync (0 or 1): " << std::flush;
+  std::cin >> sync;
+  std::cout << "\nBlocking port? (0 or 1): " << std::flush;
+  std::cin >> blocking;
+  std::cout << "\nWhat report rate (Q-every cycle, R-every other, S-every 8, T-every 32): " << std::flush;
+  std::cin >> report;
+  std::cout << "\nWhat is the transmitter id (1-based numbering): " << std::flush;
+  std::cin >> transmitter;
+*/
+  unsigned int birdsRequired = 2;
+
+  unsigned short port = 6000;
+
+
+  aMotionStar* motionstar = new(aMemPool) aMotionStar("mstar.vrac.iastate.edu",
+                                                      port, BIRDNET::TCP, true,
+						      FLOCK::FRONT_HEMISPHERE,
+						      FLOCK::POSITION_QUATERNION,
+						      BIRDNET::CONTINUOUS, 1,
+						      birdsRequired);
+
+  char achar;
+  bool birds_running(false);      // Are the birds currently running
+
+   do
+   {
+      std::cerr << "\n----- MotionStar Test Program ----\n"
+                << "----- Main Menu ----\n"
+                << "Cur State:\n"
+                << "   running:" << birds_running
+                << "\n\nCommands:\n"
+                << "   U - Update\n"
+                << "   S - Start\n"
+                << "   X - Stop\n"
+                << "   Q - Quit\n"
+                << "   O - Output\n"
+                << "\nCommand:" << std::flush;
+      std::cin >> achar;        // Get next command
+
+      std::cerr << "\nGot command: " << achar << std::endl << std::flush;
+      switch (achar)
+      {
+      case 'u':
+      case 'U':
+         std::cerr << "---- Updating (not implemented) ---\n" << std::flush;
+         break;
+      case 's':
+      case 'S':
+         std::cerr << "---- Starting MotionStar ---\n" << std::flush;
+         motionstar->start();
+         birds_running = true;
+         break;
+      case 'x':
+      case 'X':
+         std::cerr << "---- Stoping motionstar ---\n" << std::flush;
+         motionstar->stop();
+         birds_running = false;
+         break;
+      case 'o':
+      case 'O':
+         long num_samples;
+         std::cerr << "Number of samples?";
+         std::cin >> num_samples;
+
+         for (long z = 0; z < num_samples; z++)
+         {
+            motionstar->sample();
+            //std::cout << "-------- aMotionStar: Sampling (" << z << " of " << num_samples << ") -------" << std::endl;
+            std::cout << z << ":\n";
+	    std::cout << "\t      X\t\t  Y\t     Z\t\tA\t    E\t\tR\n";
+            for ( int bird=0; bird < motionstar->getNumBirds(); bird++ ) {
+            //   vjVec3 pos0 = vjCoord( *motionstar->getPosData(bird) ).pos;
+            //   vjVec3 ori0 = vjCoord( *motionstar->getPosData(bird) ).orient;
+               /*
+               std::cout << "Bird " << bird << ":\n"
+                 << setiosflags(ios::right | ios::fixed | ios::showpoint)
+                 << "Data: x:" << setprecision(2) << setw(10) << motionstar->getXPos(bird) << std::endl
+                 << "      y:" << setprecision(2) << setw(10) << motionstar->getYPos(bird) << std::endl
+                 << "      z:" << setprecision(2) << setw(10) << motionstar->getZPos(bird) << std::endl
+                 << "    azi:" << setprecision(2) << setw(10) << motionstar->getXRot(bird) << std::endl
+                 << "   elev:" << setprecision(2) << setw(10) << motionstar->getYRot(bird) << std::endl
+                 << "   roll:" << setprecision(2) << setw(10) << motionstar->getZRot(bird) << std::endl << std::endl;
+                 */
+                  std::cout << " Bird " << bird << ":";
+                  std::cout << "  " << std::setprecision(8) << motionstar->getXPos(bird)
+                            << "  " << std::setprecision(8) << motionstar->getYPos(bird)
+                            << "  " << std::setprecision(8) << motionstar->getZPos(bird)
+                            << "  " << std::setprecision(8) << motionstar->getZRot(bird)
+                            << "  " << std::setprecision(8) << motionstar->getYRot(bird)
+                            << "  " << std::setprecision(8) << motionstar->getXRot(bird)
+		            << std::endl;
             }
-            cout << endl;
+            std::cout << std::endl;
             //sleep(2);
          }
          break;
       }
    } while ((achar != 'q') && (achar != 'Q'));
 
-   cerr << "---- Quiting -----\n"
-        << "Stoping birds.....\n" << flush;
+   std::cerr << "---- Quiting -----\n";
 
-  motionstar->stop();
+   if ( motionstar->isActive() ) {
+      std::cerr << "Stoping MotionStar ...\n" << std::flush;
 
-  delete motionstar;
-  delete aMemPool;
-  return 0;
+      motionstar->stop();
+   }
+
+   delete motionstar;
+//   delete aMemPool;
+   return 0;
 }
