597305a7a1e507a0a95b93bd1dcc9967a9ac36ea
Allen Bierbaum <abierbaum@gmail.com>
2000-12-13 18:37:39 +0000

    - Addition of simplePfApp to build - Moved comment header in dummyTrackd program
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@3274 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/test/dummyTrackd.cpp
+++ juggler/modules/gadgeteer/test/dummyTrackd.cpp
@@ -1,3 +1,34 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -140,37 +171,7 @@ void setNewTrackerData(int numSensors)
  }
  gettimeofday(&curtime,NULL);
  t = curtime.tv_sec-starttime.tv_sec + (curtime.tv_usec-starttime.tv_usec)/1000000.0f;
- if (nextSensor==0)  /*************** <auto-copyright.pl BEGIN do not edit this line> **************
- *
- * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
- *
- * Original Authors:
- *   Allen Bierbaum, Christopher Just,
- *   Patrick Hartling, Kevin Meinert,
- *   Carolina Cruz-Neira, Albert Baker
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- * -----------------------------------------------------------------
- * File:          $RCSfile$
- * Date modified: $Date$
- * Version:       $Revision$
- * -----------------------------------------------------------------
- *
- *************** <auto-copyright.pl END do not edit this line> ***************/
+ if (nextSensor==0)  
  {
    tracker->sensor[0].x = fsin(t/2.0f) * 2.0f;
    tracker->sensor[0].y = 6;
