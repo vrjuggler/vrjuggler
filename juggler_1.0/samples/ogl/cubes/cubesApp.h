42ac5dceb3c6ff2d3a4dde5236fa80e504c97e48
Patrick Hartling <patrick.hartling@gmail.com>
2001-03-08 20:11:06 +0000

    Added contextClose() so that we can clean up after ourselves when creating display lists.  The ContextData class has been modified to contain the maximum number of display lists created so that we can continue to use the randomizing code for testing.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@4041 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/samples/ogl/cubes/cubesApp.h
+++ juggler_1.0/samples/ogl/cubes/cubesApp.h
@@ -62,12 +62,13 @@ public:
    ContextData()
    {
       firstTime = true;
-      cubeDLIndex = -1;
+      dlIndex   = -1;
    }
 
 public:
    bool  firstTime;
-   int   cubeDLIndex;
+   int   dlIndex;
+   int   maxIndex;     // For debugging purposes only!
 };
 
 // Class to hold all data for a specific user
@@ -141,6 +142,11 @@ public:
    // once for every display window that is opened.  Put OpenGL resource
    // allocation here.
    virtual void contextInit();
+
+   // Called immediately upon closing an OpenGL context 
+   // (called for every window that is closed)
+   // put your opengl deallocation here...
+   virtual void contextClose();
 
    /**   name Drawing Loop Functions
     *
@@ -216,7 +222,7 @@ private:
 
    void drawCube()
    {
-       glCallList(mDlData->cubeDLIndex);
+       glCallList(mDlData->dlIndex);
        //drawbox(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f, GL_QUADS);
    }
 
@@ -225,6 +231,7 @@ private:
 
 public:
    vjGlContextData<ContextData>  mDlData;       // Data for display lists
+   vjGlContextData<ContextData>  mDlDebugData;  // Data for debugging display lists
    std::vector<UserData*>        mUserData;     // All the users in the program
 };
 
