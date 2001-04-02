382754036939eabd1966936ab190f8083e26fc96
Patrick Hartling <patrick.hartling@gmail.com>
2001-04-02 16:07:13 +0000

    Fixed to compile on Win32.  The functions min() and max() had to be renamed to Min() and Max() to avoid problems with min() and max() defined as macros.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@4151 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/test/Draw/OGL/texture/defines.h
+++ juggler/modules/vrjuggler/test/Draw/OGL/texture/defines.h
@@ -5,13 +5,13 @@ namespace kev
 {
    // returns the maximum of a and b.
    template<class T> 
-   inline static const T& max( const T& a, const T& b )
+   inline static const T& Max( const T& a, const T& b )
    { 
       return (a>=b) ? a : b; 
    }
    // returns the minimum of a and b.
    template<class T> 
-   inline static const T& min( const T& a, const T& b )
+   inline static const T& Min( const T& a, const T& b )
    { 
       return (a<b) ? a : b; 
    }
