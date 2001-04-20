b5d9f4ffff223c6565e376c6470f5e80d5b2d270
Patrick Hartling <patrick.hartling@gmail.com>
2001-04-20 16:44:21 +0000

    Fixed namespace problems.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@4346 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/IO/IOSys.h
+++ modules/vapor/vpr/IO/IOSys.h
@@ -35,20 +35,25 @@
 
 #include <vprConfig.h>
 
-namespace vpr {
-
 // determine which implementation of vprSystem to include.
 #if defined(VRP_USE_NSPR)
 #   include <md/NSPR/IOSysNSPR.h>
+
+namespace vpr {
     typedef IOSysNSPR IOSys;
+};
 #elif defined(VPR_USE_IRIX_SPROC) || defined(VPR_USE_PTHREADS)
 #   include <md/POSIX/IOSysUnix.h>
+
+namespace vpr {
     typedef IOSysUnix IOSys;
+};
 #elif defined(VPR_USE_WIN32)
 #   include <md/WIN32/IOSysWin32.h>
-    typedef IOSysWin32 IOSys;
-#endif
 
+namespace vpr {
+    typedef IOSysWin32 IOSys;
 };
+#endif
 
 #endif   /* _VPR_IOSYS_FAKE_H_ */
