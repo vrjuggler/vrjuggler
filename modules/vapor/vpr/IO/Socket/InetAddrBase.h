eeb7d6e10a3fab4347a64221b2c23db8c1ec6e8d
Allen Bierbaum <abierbaum@gmail.com>
2001-04-20 02:45:08 +0000

    Added inet addr base
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@4327 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/IO/Socket/InetAddrBase.h
@@ -0,0 +1,216 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VPR_INET_ADDR_BASE_H_
+#define _VPR_INET_ADDR_BASE_H_
+
+#include <IO/Socket/SocketTypes.h>
+#include <Utils/Assert.h>
+
+namespace vpr {
+
+// ----------------------------------------------------------------------------
+//: Cross-platform abstraction to Internet address structures.
+// ----------------------------------------------------------------------------
+class InetAddrBase {
+public:
+    //static const InetAddr AnyAddr;  -- NEED An AnyAddr defined
+
+    // ------------------------------------------------------------------------
+    //: Default constructor.  This initializes the memory for the encapsulated
+    //+ address structure.
+    //
+    //! PRE: None.
+    //! POST: Zero out the address and set everything to ANY
+    // ------------------------------------------------------------------------
+    InetAddr (void)
+    { vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    // Set address to a string like "hostname.domain.com:<port_num>"
+    // ------------------------------------------------------------------------
+    InetAddr (const std::string& address)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    // Initialize to address and port
+    // ------------------------------------------------------------------------
+    InetAddr (const std::string& address, const Uint16 port)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    // Initialize to an ANY address and a given port
+    // ------------------------------------------------------------------------
+    InetAddr (const Uint16 port)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Copy constructor.
+    //
+    //! PRE: None.
+    //! POST: A copy of the given vpr::InetAddr object is made in this object.
+    //
+    //! ARGS: addr - The vpr::InetAddr object to be copied into this object.
+    // ------------------------------------------------------------------------
+    InetAddr (const InetAddr& addr)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Get the protocol family of this address structure.
+    //
+    //! PRE: The protocol family has been properly initialized.
+    //! POST: The protocol family is returned as a vpr::SocketTypes::Domain
+    //+       value.
+    //
+    //! RETURNS: A vpr::SocketTypes::Domain value representing this object's
+    //+          protocol family.
+    // ------------------------------------------------------------------------
+    SocketTypes::Domain getFamily(void) const
+    { vprASSERT(false && "Implement me"); }
+
+    // ------------------------------------------------------------------------
+    //: Set the protocol family of this address structure.
+    //
+    //! PRE: None.
+    //! POST: The given protocol family (a vpr::SocketTypes::Domain value) is
+    //+       mapped to the appropriate platform-specific protocol family
+    //+       value and stored.
+    //
+    //! ARGS: family - The protocol family value.
+    // ------------------------------------------------------------------------
+    void setFamily(const SocketTypes::Domain family)
+    { vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Get this address' port in host byte order.
+    //
+    //! PRE: The port has been initialized properly in network byte order.
+    //! POST: The port associated with this address structure is returned to
+    //+       the caller in host byte order.
+    //
+    //! RETURNS: A Uint16 giving the port for this address structure in host byte order.
+    // ------------------------------------------------------------------------
+    Uint16
+    getPort (void) const
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Set this address' port.  The given port must be in host byte order.
+    //
+    //! PRE: The given port number is in host byte order.
+    //! POST: The given port number is stored in the address structure in
+    //+       network byte order.
+    //
+    //! ARGS: port - A Uint16 port number for this
+    //+              address structure in host byte order.
+    // ------------------------------------------------------------------------
+    inline void
+    setPort (const Uint16 port)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Get this address structure's Internet address in host byte order.
+    //
+    //! PRE: The IP address has been initialized properly in network byte
+    //+      order.
+    //! POST: The IP address associated with this address structure is
+    //+       returned to the caller in host byte order.
+    //
+    //! RETURNS: An unsigned int (32-bit value) giving the IP address for this
+    //+          address structure in host byte order.
+    // ------------------------------------------------------------------------
+    inline Uint32
+    getAddressValue (void) const
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Set this structure's IP address.  The given address must be in host
+    //+ byte order.
+    //
+    //! PRE: The given IP address is in host byte order.
+    //! POST: The given IP address is stored in the address structure in
+    //+       network byte order.
+    //
+    //! ARGS: port - An unsigned int IP address for this
+    //+              address structure in host byte order.
+    // ------------------------------------------------------------------------
+    inline void
+    setAddressValue (const Uint32 addr_value)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Get the IP address associated with this structure as a human-readable
+    //+ string.
+    //
+    //! PRE: The structure contains a valid IP address.
+    //! POST: The integer IP address is converted to dotted-decmial notation
+    //+       and returned as a string.
+    //
+    //! RETURNS: A std::string object containing this structure's IP address
+    //+          in the human-readable "dotted-decimal" notation.
+    // ------------------------------------------------------------------------
+    std::string getAddressString(void) const
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //
+    // ARGS: addr - an address string in IP format or hostname formant
+    //
+    // Returns:
+    //     true  - The address lookup was successful.
+    //     false - The address could not be looked up.  An error message is
+    //             printed to stderr explaining what went wrong.
+    // ------------------------------------------------------------------------
+    bool setAddress (const std::string& addr)
+    {vprASSERT(false && "Implement me");}
+
+    // ------------------------------------------------------------------------
+    //: Overloaded assignment operator to ensure that assignments work
+    //+ correctly.
+    //
+    //! PRE: None.
+    //! POST: A copy of the given vpr::InetAddr object is made in this object
+    //+       which is then returned to the caller.
+    //
+    //! ARGS: addr - The vpr::InetAddr object to be copied into this object.
+    //
+    //! RETURNS: A reference to this object.
+    // ------------------------------------------------------------------------
+    inline InetAddr&
+    operator= (const InetAddr& addr)
+    {vprASSERT(false && "Implement me");}
+};
+
+}; // End of vpr namespace
+
+
+#endif   /* _VPR_INET_ADDR_H_ */
