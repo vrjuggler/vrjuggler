3cec91841e50e91b4b4da7d367d0e41496d4bdf2
Allen Bierbaum <abierbaum@gmail.com>
2001-04-28 20:39:29 +0000

    Commented out the SocketTest suite because it is broken. :(
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@4534 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/test/TestSuite/main.cpp
+++ juggler/modules/vapor/test/TestSuite/main.cpp
@@ -3,6 +3,7 @@
 #include <TestCases/Socket/SocketTest.h>
 #include <TestCases/Thread/ThreadTest.h>
 #include <TestCases/IO/Socket/InetAddrTest.h>
+#include <TestCases/Socket/SocketConnectorAcceptorTest.h>
 
 
 //using namespace vpr;
@@ -21,21 +22,19 @@ int main (int ac, char **av)
    //suite_1->addTest( /* put your test here */ );
    noninteractive_suite->addTest(vprTest::InetAddrTest::suite());
    noninteractive_suite->addTest(vprTest::SocketTest::suite());
+   noninteractive_suite->addTest(vprTest::SocketConnectorAcceptorTest::suite());
 
    // Add the test suite to the runner
    runner.addTest( "noninteractive", noninteractive_suite );
 
    // create test suite #2
-   TestSuite* suite_2 = new TestSuite("suite_2");
+   TestSuite* suite_2 = new TestSuite("Interactive");
 
    // add tests to the suite
-   //suite_2->addTest(gmtlTest::Vec3Test::suite());
-   //suite_2->addTest(gmtlTest::MatrixTest::suite());
-   //suite_2->addTest(gmtlTest::Point3Test::suite());
    suite_2->addTest(ThreadTest::suite());
 
    // Add the test suite to the runner
-   runner.addTest("suite_2_tests", suite_2);
+   runner.addTest("interactive", suite_2);
 
    // run all test suites
    runner.run( ac, av );
