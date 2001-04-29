92f629f7eccefdb3331f15269f730ac6e74d976a
Allen Bierbaum <abierbaum@gmail.com>
2001-04-29 00:27:03 +0000

    Fixed const == problem
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@4543 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/md/POSIX/IO/Socket/InetAddrBSD.h
+++ modules/vapor/vpr/md/POSIX/IO/Socket/InetAddrBSD.h
@@ -283,7 +283,7 @@ public:
     //: Overloaded equality operator.
     // ------------------------------------------------------------------------
     bool
-    operator== (const InetAddrBSD& addr) {
+    operator== (const InetAddrBSD& addr) const {
         return ((m_addr.sin_addr.s_addr == addr.m_addr.sin_addr.s_addr) &&
                 (m_addr.sin_port == addr.m_addr.sin_port) &&
                 (m_addr.sin_family == addr.m_addr.sin_family));
@@ -293,7 +293,7 @@ public:
     //: Overloaded inequality operator.
     // ------------------------------------------------------------------------
     bool
-    operator!= (const InetAddrBSD& addr) {
+    operator!= (const InetAddrBSD& addr) const {
         return ! (*this == addr);
     }
 
