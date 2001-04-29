d4f99aaafc6c8f4d57f1923d3c48588afd6d9f24
Allen Bierbaum <abierbaum@gmail.com>
2001-04-29 22:00:50 +0000

    Initial bridge and .h file for the Selector interface.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@4565 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/IO/Selector.h
@@ -0,0 +1,62 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VPR_SELECTOR_H_
+#define _VPR_SELECTOR_H_
+
+#include <string>
+
+// include bridge class
+#include <IO/Socket/Selector_t.h>
+
+// make the connection
+#if defined(VPR_USE_NSPR)
+#   include <md/NSPR/SelectorImpNSPR.h>
+namespace vpr {
+  typedef Selector_t<SelectorImpNSPR> Selector;
+};
+
+#elif defined(VPR_USE_WIN32)
+#   include <md/WIN32/SelectorImpWinSock.h>
+namespace vpr {
+  typedef Selector_t<SelectorImpWinSock> Selector;
+};
+
+#else
+#   include <md/POSIX/SelectorImpBSD.h>
+namespace vpr {
+  typedef Selector_t<SelectorImpBSD> Selector;
+};
+#endif
+
+
+#endif  /* _VPR_SELECTOR_H_ */
---
+++ modules/vapor/vpr/IO/Selector_t.h
@@ -0,0 +1,111 @@
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VPR_SELECTOR_BRIDGE_H_
+#define _VPR_SELECTOR_BRIDGE_H_
+// NOTE: this is the bridge class for use with Selector.h
+
+#include <string>
+#include <IO/IOSys.h>
+
+namespace vpr {
+
+// ----------------------------------------------------------------------------
+//: Cross-platform selection interface.
+//
+// A selector is used to wait on a set of Handles untils any of the 
+// events occur that the user is interested in.
+// ----------------------------------------------------------------------------
+//!PUBLIC_API:
+template<class RealSelectorImp>
+class Selector_t
+{
+public:
+   enum EventType
+   { READ = 1,           // Read - 
+     WRITE = 2,          // Write -
+     EXCEPT = 4,         // Exception -
+     ERR = 8,            // Error -
+     INVALID = 16         // Invalid - Invalid handle
+   };
+
+   //: Add the given handle to the selector
+   //! PRE: handle is a valid handle
+   //! POST: handle is added to the handle set, and initialized to a mask of no-events
+   bool addHandle(IOSys::Handle handle)
+   {
+      return mSelectorImp.addHandle(handle);
+   }
+
+   //: Remove a handle from the selector
+   //! PRE: handle is in the selector
+   //! POST: handle is removed from the set of valid handles
+   bool removeHandle(IOSys::Handle handle)
+   {
+      return mSelectorImp.removeHandle(handle);
+   }
+
+   //: Set the event flags going in to the select to mask
+   bool setIn(IOSys::Handle handle, vpr::Uint16 mask)
+   {
+      return mSelectorImp.setIn(handle, mask);
+   }
+
+   //: Get the current in flag mask
+   vpr::Unit16 getIn(IOSys::Handle handle)
+   {
+      return mSelectorImpl.getIn(handle);
+   }
+
+   //: Get the current out flag mask
+   vpr::Unit16 getOut(IOSys::Handle handle)
+   {
+      return mSelectorImpl.getOut(handle);
+   }
+
+   //: Select
+   //! ARGS: numWithEvents - Upon completion, this holds the number of items that have events
+   //! ARGS: timeout - The number of msecs to select for (0 - don't wait)
+   bool select(vpr::Uint16& numWithEvents, vpr::Uint16 timeout)
+   {
+      return mSelectorImpl.select(numWithEvents, timeout);
+   }
+
+protected:
+    RealSelectorImp mSelectorImp;     // Platform specific implementation                                 
+};
+
+}; // End of vpr namespace
+
+
+#endif  /* _VPR_SELECTOR_BRIDGE_H_ */
