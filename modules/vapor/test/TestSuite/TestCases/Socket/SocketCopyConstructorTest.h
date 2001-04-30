83f7d9df1d090066cedf94a7b4f24b60a3351c02
Zheng Min <zmin@vrac.iastate.edu>
2001-04-30 19:12:27 +0000

    Using condition varialble in copy constructor test, it works well now.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@4582 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/test/TestSuite/TestCases/Socket/SocketCopyConstructorTest.h
+++ modules/vapor/test/TestSuite/TestCases/Socket/SocketCopyConstructorTest.h
@@ -18,6 +18,7 @@
 #include <Threads/Thread.h>
 #include <Threads/ThreadFunctor.h>
 #include <Sync/Mutex.h>
+#include <Sync/Cond.h>
 
 #include <vector>
 
@@ -98,6 +99,9 @@ public:
    void testCopyConnectedSocket()
    {
       testAssertReset();
+      
+      mState = NOT_READY;                        // Initialize      
+      
       // Spawn acceptor thread
       vpr::ThreadMemberFunctor<SocketCopyConstructorTest> 
             acceptor_functor( this, &SocketCopyConstructorTest::testCopyConstructor_acceptor );
@@ -121,13 +125,20 @@ public:
        bool result = 0;       
        bool result1 = 0;
        char buffer[40];
-       
        remote_addr.setPort(port);
        vpr::SocketStream connector_socket(vpr::InetAddr::AnyAddr, remote_addr);
        vpr::SocketStream* copy_connector(NULL);       
        
        result = connector_socket.open();
        threadAssertTest((result != false), "Can not open connector socket");
+       
+       // WAIT for READY 
+       mCondVar.acquire();
+       {
+          while(mState != READY)
+             mCondVar.wait();
+       }
+       mCondVar.release();
        
        result1 = connector_socket.connect();
        threadAssertTest((result1 != false), "connect fails");
@@ -139,12 +150,22 @@ public:
          if (result>0)
             copy_connector->close();
        }
+       
        threadAssertTest(copy_connector->getType() == connector_socket.getType());       
        threadAssertTest(copy_connector->getRemoteAddr() == connector_socket.getRemoteAddr());
        threadAssertTest(copy_connector->getLocalAddr() == connector_socket.getLocalAddr());
        
        connector_socket.close();
        copy_connector->close();
+       
+       // Tell everyone that we closed
+       mCondVar.acquire();
+       {
+          mState = CONNECTOR_CLOSED;
+          mCondVar.signal();       // Tell any waiting threads
+       }
+       mCondVar.release();
+             
        delete copy_connector;
    }
 
@@ -164,7 +185,7 @@ public:
        
       result = acceptor_socket.open();
       threadAssertTest((result != false), "Can not open acceptor socket");
-       
+      
       result = acceptor_socket.bind();
       
       if (result == false)
@@ -172,7 +193,13 @@ public:
       else { 
          result = acceptor_socket.listen( backlog );
          threadAssertTest((result != false), "acceptor listening fails");
-       
+         // READY - Tell everyone that we are ready to accept
+            mCondVar.acquire();
+            {
+               mState = READY;
+               mCondVar.signal();       // Tell any waiting threads
+            }
+            mCondVar.release();       
          child_socket = acceptor_socket.accept();
          threadAssertTest((child_socket != NULL), "accept() fails");
       }
@@ -190,7 +217,13 @@ public:
          threadAssertTest(copy_child->getRemoteAddr() == child_socket->getRemoteAddr());
          threadAssertTest(copy_child->getLocalAddr() == child_socket->getLocalAddr());
       }
-       
+      
+      // WAIT for close
+      mCondVar.acquire();         
+         while(mState != CONNECTOR_CLOSED)
+            mCondVar.wait();             
+      mCondVar.release();
+      
       result = acceptor_socket.close();
       threadAssertTest((result != false), "acceptor close fails");
       if (child_socket != NULL) child_socket->close();
@@ -213,8 +246,12 @@ public:
 
 private:
    bool             mThreadAssertTest; // true for no error
-
    
+protected:
+    vpr::Cond       mCondVar;       // Condition variable
+    enum State { READY, NOT_READY, CONNECTOR_CLOSED
+    };
+    State           mState;         // State variable   
 };
 
 }       // namespace
