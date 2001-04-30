62007fc17ea834bcb6864d37696a5d47dc5e433b
Zheng Min <zmin@vrac.iastate.edu>
2001-04-30 04:48:40 +0000

    Add a test for copying connected socket Modify the first one
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@4576 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/test/TestSuite/TestCases/Socket/SocketCopyConstructorTest.h
+++ modules/vapor/test/TestSuite/TestCases/Socket/SocketCopyConstructorTest.h
@@ -62,12 +62,9 @@ public:
    // =========================================================================
    void simpleTest()
    {
-       const vpr::Uint16 local_port(12345);
        const vpr::Uint16 remote_port(13579);
        vpr::InetAddr remote_addr;
-       vpr::InetAddr local_addr;
-       local_addr.setPort(local_port);
-       local_addr.setPort(remote_port);
+       remote_addr.setPort(remote_port);
        
        vpr::SocketStream*	sock(NULL);    
        vpr::SocketStream*  newSock1(NULL);
@@ -81,19 +78,136 @@ public:
        assertTest(sock->getLocalAddr() == newSock1->getLocalAddr());
        
        delete sock;
-       assertTest(newSock1->open() && "Can not open copied socket");
-       assertTest(newSock2->open() && "Can not open copied socket");
+       assertTest(newSock1->open() && "Can not open copied socket1");
+       assertTest(newSock2->open() && "Can not open copied socket2");
        
        assertTest(newSock2->getType() == newSock1->getType());
        assertTest(newSock2->getRemoteAddr() == newSock1->getRemoteAddr());
        assertTest(newSock2->getLocalAddr() == newSock1->getLocalAddr());
+       
+       assertTest(newSock1->close() && "Can not close copied socket1");
+       assertTest(newSock2->close() && "Can not close copied socket2");
+       
+       delete newSock1;
+       delete newSock2;
+   }
+   
+   // =========================================================================
+   // testCopyConnectedSocket -- spawns two threads as connector and acceptor
+   // =========================================================================
+   void testCopyConnectedSocket()
+   {
+      testAssertReset();
+      // Spawn acceptor thread
+      vpr::ThreadMemberFunctor<SocketCopyConstructorTest> 
+            acceptor_functor( this, &SocketCopyConstructorTest::testCopyConstructor_acceptor );
+      vpr::Thread acceptor_thread( &acceptor_functor );
+      
+      // spawn a connector thread
+      vpr::ThreadMemberFunctor<SocketCopyConstructorTest> 
+            connector_functor( this, &SocketCopyConstructorTest::testCopyConstructor_connector );
+      vpr::Thread connector_thread( &connector_functor );
+      
+      connector_thread.join();
+      acceptor_thread.join();
+      
+      assertTest( mThreadAssertTest );
+   }
+
+   void testCopyConstructor_connector(void* arg)
+   {
+       const vpr::Uint16 port(13579);
+       vpr::InetAddr remote_addr;
+       bool result = 0;       
+       bool result1 = 0;
+       char buffer[40];
+       
+       remote_addr.setPort(port);
+       vpr::SocketStream connector_socket(vpr::InetAddr::AnyAddr, remote_addr);
+       vpr::SocketStream* copy_connector(NULL);       
+       
+       result = connector_socket.open();
+       threadAssertTest((result != false), "Can not open connector socket");
+       
+       result1 = connector_socket.connect();
+       threadAssertTest((result1 != false), "connect fails");
+       
+       copy_connector = new vpr::SocketStream (connector_socket);
+       
+       if (result1 != false) {
+         result = copy_connector->read (buffer, 40);
+         if (result>0)
+            copy_connector->close();
+       }
+       threadAssertTest(copy_connector->getType() == connector_socket.getType());       
+       threadAssertTest(copy_connector->getRemoteAddr() == connector_socket.getRemoteAddr());
+       threadAssertTest(copy_connector->getLocalAddr() == connector_socket.getLocalAddr());
+       
+       connector_socket.close();
+       copy_connector->close();
+       delete copy_connector;
+   }
+
+    void testCopyConstructor_acceptor(void* arg)
+   {
+      const vpr::Uint16 port(13579);
+      const int backlog = 5;
+      bool result = 0;
+      char buffer[]="Hello!";
+      vpr::InetAddr local_addr;
+      local_addr.setPort(port);
+      vpr::SocketStream acceptor_socket(local_addr, vpr::InetAddr::AnyAddr);
+       
+      vpr::SocketStream* child_socket(NULL);
+      vpr::SocketStream* copy_acceptor(NULL);
+      vpr::SocketStream* copy_child(NULL);
+       
+      result = acceptor_socket.open();
+      threadAssertTest((result != false), "Can not open acceptor socket");
+       
+      result = acceptor_socket.bind();
+      
+      if (result == false)
+         threadAssertTest((false), "Can not bind acceptor socket");
+      else { 
+         result = acceptor_socket.listen( backlog );
+         threadAssertTest((result != false), "acceptor listening fails");
+       
+         child_socket = acceptor_socket.accept();
+         threadAssertTest((child_socket != NULL), "accept() fails");
+      }
+       
+      copy_acceptor = new vpr::SocketStream (acceptor_socket);
+      if (child_socket != NULL) copy_child = new vpr::SocketStream (*child_socket);
+       
+      threadAssertTest(copy_acceptor->getType() == acceptor_socket.getType());
+      threadAssertTest(copy_acceptor->getRemoteAddr() == acceptor_socket.getRemoteAddr());
+      threadAssertTest(copy_acceptor->getLocalAddr() == acceptor_socket.getLocalAddr());
+       
+      if (copy_child != NULL) {
+         copy_child->write(buffer, sizeof(buffer));
+         threadAssertTest(copy_child->getType() == child_socket->getType());
+         threadAssertTest(copy_child->getRemoteAddr() == child_socket->getRemoteAddr());
+         threadAssertTest(copy_child->getLocalAddr() == child_socket->getLocalAddr());
+      }
+       
+      result = acceptor_socket.close();
+      threadAssertTest((result != false), "acceptor close fails");
+      if (child_socket != NULL) child_socket->close();
+       
+      copy_acceptor->close();
+      if (copy_child != NULL) copy_child->close();
+       
+      if (child_socket != NULL) delete child_socket;
+      delete copy_acceptor;
+      if (copy_child != NULL) delete copy_child;
    }
    
    static Test* suite()
    {
-      TestSuite *test_suite = new TestSuite ("SocketCopyConstructorTest");
-      
+      TestSuite *test_suite = new TestSuite ("SocketCopyConstructorTest");      
       test_suite->addTest( new TestCaller<SocketCopyConstructorTest>("simpleTest", &SocketCopyConstructorTest::simpleTest));
+      test_suite->addTest( new TestCaller<SocketCopyConstructorTest>("testCopyConnectedSocket", &SocketCopyConstructorTest::testCopyConnectedSocket));
       return test_suite;
    }
 
