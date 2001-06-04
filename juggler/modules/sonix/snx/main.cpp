5897a34b5938a4dd9ba774c149e2936f0222b74c
Kevin Meinert <subatomic@users.sourceforge.net>
2001-06-04 22:39:42 +0000

    main
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@4837 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/sonix/snx/main.cpp
@@ -0,0 +1,18 @@
+#include "vpr.h"    // interface
+#include "aj.h"    // interface
+
+void main()
+{
+}
+
+
+/*
+
+#include "ajSoundFactory.h"
+#include "ajSoundImplementation.h"
+#include "ajAudioWorksSoundImplementation.h"  
+#include "ajSoundInfo.h"
+#include "ajOpenALSoundImplementation.h"      
+#include "ajStubSoundImplementation.h"
+
+*/
