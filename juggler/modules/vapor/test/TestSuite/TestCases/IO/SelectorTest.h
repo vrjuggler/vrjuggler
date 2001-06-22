f2285e1b9c0cf604d324453193e1576a0f3a4f99
Patrick Hartling <patrick.hartling@gmail.com>
2001-06-22 18:27:29 +0000

    Removed trailing whitespace.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@5127 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/test/TestSuite/TestCases/IO/SelectorTest.h
+++ juggler/modules/vapor/test/TestSuite/TestCases/IO/SelectorTest.h
@@ -53,7 +53,7 @@ public:
    virtual void tearDown()
    {
    }
-   
+  
    // use this within your threads (CppUnit doesn't catch the assertTest there)
    // then test mThreadAssertTest with assertTest in your parent func.
    // then reset it to true.
@@ -61,7 +61,7 @@ public:
    {
        if(!testcase)
        {
-           mThreadAssertTest = testcase; 
+           mThreadAssertTest = testcase;
            std::cerr << "threadAssertTest: " << text << std::endl;
        }
        //else
@@ -69,7 +69,7 @@ public:
    }
    void testAssertReset()
    { mThreadAssertTest = true; }
-   
+  
 
    // Test an acceptor pool
    // This test is based upon the idea of having a pool of acceptors that
@@ -79,21 +79,21 @@ public:
    void testAcceptorPoolSelection()
    {
        testAssertReset();
-       mRendevousPort = 47000 + (random() % 71);     // Get a partially random port       
+       mRendevousPort = 47000 + (random() % 71);     // Get a partially random port      
        mNumRendevousPorts = 13;
        mNumIters = 50;
        mMessageValue = std::string("The Data");
        mMessageLen = mMessageValue.length();
-       
+      
        mState = NOT_READY;                        // Initialize
 
        // Spawn acceptor thread
-       vpr::ThreadMemberFunctor<SelectorTest> 
+       vpr::ThreadMemberFunctor<SelectorTest>
            acceptor_functor( this, &SelectorTest::testAcceptorPoolSelection_acceptor );
        vpr::Thread acceptor_thread( &acceptor_functor);
 
        // Spawn connector thread
-       vpr::ThreadMemberFunctor<SelectorTest> 
+       vpr::ThreadMemberFunctor<SelectorTest>
            connector_functor( this, &SelectorTest::testAcceptorPoolSelection_connector );
        vpr::Thread connector_thread( &connector_functor);
 
@@ -122,9 +122,9 @@ public:
          vpr::IOSys::Handle handle = new_acceptor->getHandle();                  // Get the Handle to register
          acceptorTable[handle] = new_acceptor;
          selector.addHandle(handle);                          // Add to selector
-         selector.setIn(handle, (vpr::Selector::VPR_READ | vpr::Selector::VPR_WRITE | vpr::Selector::VPR_EXCEPT));         
+         selector.setIn(handle, (vpr::Selector::VPR_READ | vpr::Selector::VPR_WRITE | vpr::Selector::VPR_EXCEPT));        
       }
-       
+      
       // READY - Tell everyone that we are ready to accept
       mCondVar.acquire();
       {
@@ -134,7 +134,7 @@ public:
       mCondVar.release();
 
       // Use selector to find the acceptor to accept on
-      // Then accept and send a message back                     
+      // Then accept and send a message back                    
        for(i=0;i<mNumIters;i++)
        {
           vpr::Uint16 num_events;
@@ -144,8 +144,8 @@ public:
                            "Selection did not return successfully");
           threadAssertTest((num_events == 1), "There was not only 1 event");
 
-          
-          // get an acceptor that has a connection request pending (ready_acceptor)
+          // get an acceptor that has a connection request pending
+          // (ready_acceptor)
           int num_found(0);
           vpr::SocketAcceptor* ready_acceptor = NULL;
           for(j=0;j<selector.getNumHandles();j++)
@@ -155,7 +155,7 @@ public:
             {
                threadAssertTest((acceptorTable.find(selector.getHandle(j)) != acceptorTable.end()),
                                 "Handle not found int acceptor table");
-               ready_acceptor = acceptorTable[selector.getHandle(j)];               
+               ready_acceptor = acceptorTable[selector.getHandle(j)];
                num_found += 1;
             }
           }
@@ -168,12 +168,12 @@ public:
           ret_val = ready_acceptor->accept(sock);
           threadAssertTest((ret_val.success()), "Accepted socket is null");
           threadAssertTest((sock.isOpen()), "Accepted socket should be open");
-           
+
           // use the sock to write (send)... then close it...
-          ret_val = sock.write(mMessageValue, mMessageLen, bytes_written);      // Send a message           
+          ret_val = sock.write(mMessageValue, mMessageLen, bytes_written);      // Send a message          
           threadAssertTest((ret_val.success()), "Problem writing in acceptor");
           ret_val = sock.close();         // Close the socket
-          threadAssertTest((ret_val.success()), "Problem closing accepted socket");          
+          threadAssertTest((ret_val.success()), "Problem closing accepted socket");         
        }
 
        // Delete acceptors
@@ -189,17 +189,17 @@ public:
       vpr::SocketConnector connector;           // Connect to acceptor
       ssize_t bytes_read;
 
-      // WAIT for READY 
+      // WAIT for READY
       mCondVar.acquire();
       {
          while(mState != ACCEPTOR_READY)
             mCondVar.wait();
       }
       mCondVar.release();
-      
+     
       // Connect randomly to the rendevous ports
       for(unsigned int i=0;i<mNumIters;i++)
-      {         
+      {        
          vpr::Uint16 port_num = mRendevousPort+(random() % mNumRendevousPorts);
          //std::cout << " p: " << port_num << std::flush;
          remote_addr.setAddress("localhost", port_num);
@@ -207,15 +207,15 @@ public:
          std::string       data;
          ret_val = connector.connect(con_sock, remote_addr, 100);
          threadAssertTest((ret_val.success()), "Connector can't connect");
-         
+        
          ret_val = con_sock.read(data, mMessageLen, bytes_read);   // Recieve data
          threadAssertTest((bytes_read == mMessageLen), "Connector recieved message of wrong size" );
          threadAssertTest((ret_val.success()), "Failure reading data");
-         
+        
          con_sock.close();                                   // Close socket
       }
    }
-   
+  
 
    // Test sending data to a group of recievers that are pooled
    // This test is based upon the idea of having a pool of
@@ -223,25 +223,25 @@ public:
    //
    // The connector side selects some subgroup of the sockets, marks them as selected, and then sends data on them
    // The acceptor side then selects on the sockets and hopefully it gets that same group back as the ones with pending data
-   //      
+   //     
    void testSendThenPoll()
    {
        testAssertReset();
-       mRendevousPort = 47100 + (random() % 71);     // Get a partially random port       
+       mRendevousPort = 47100 + (random() % 71);     // Get a partially random port      
        mNumRendevousPorts = 17;
        mNumIters = 40;
        mMessageValue = std::string("The Data");
        mMessageLen = mMessageValue.length();
-       
+      
        mState = NOT_READY;                        // Initialize
 
        // Spawn acceptor thread
-       vpr::ThreadMemberFunctor<SelectorTest> 
+       vpr::ThreadMemberFunctor<SelectorTest>
            acceptor_functor( this, &SelectorTest::testSendThenPoll_acceptor );
        vpr::Thread acceptor_thread( &acceptor_functor);
 
        // Spawn connector thread
-       vpr::ThreadMemberFunctor<SelectorTest> 
+       vpr::ThreadMemberFunctor<SelectorTest>
            connector_functor( this, &SelectorTest::testSendThenPoll_connector );
        vpr::Thread connector_thread( &connector_functor);
 
@@ -267,7 +267,7 @@ public:
          acceptors.push_back(new_acceptor);
          vpr::InetAddr local_acceptor_addr;
          local_acceptor_addr.setAddress("localhost", (mRendevousPort+i));        // Set local address
-         new_acceptor->open(local_acceptor_addr);                                // Open acceptor                  
+         new_acceptor->open(local_acceptor_addr);                                // Open acceptor                 
       }
 
       // READY - Tell everyone that we are ready to accept
@@ -276,7 +276,7 @@ public:
          mState = ACCEPTOR_READY;
          mCondVar.signal();       // Tell any waiting threads
       }
-      mCondVar.release();      
+      mCondVar.release();     
 
       // Accept all the connections
       for(i=0;i<mNumRendevousPorts;i++)
@@ -294,17 +294,17 @@ public:
       threadAssertTest((mNumRendevousPorts == selector.getNumHandles()), "We didn't add all ports correctly to selector");
 
       // Use selector to find the sock(s) to read on
-      // Then read message after checking to verify subgroup                     
+      // Then read message after checking to verify subgroup                    
        for(i=0;i<mNumIters;i++)
        {
           // WAIT for data to be sent
          mCondVar.acquire();
          {
             while(mState != DATA_SENT)
-               mCondVar.wait();         
+               mCondVar.wait();        
          }
          mCondVar.release();
-          
+         
           // Get the events
           vpr::Uint16 num_events;
           Status ret = selector.select(num_events, 50000);
@@ -314,13 +314,13 @@ public:
           threadAssertTest((num_events == mSelectedPorts.size()),
                            "There was an incorrect number of events");
 
-          // mSelectedPorts holds the index numbers of the sockets that data was sent on          
+          // mSelectedPorts holds the index numbers of the sockets that data was sent on         
           // Read from each selected socket
           unsigned num_found(0);
           for(j=0;j<selector.getNumHandles();j++)
           {
              // If have data to read
-             if(selector.getOut(selector.getHandle(j)) & (vpr::Selector::VPR_READ))      
+             if(selector.getOut(selector.getHandle(j)) & (vpr::Selector::VPR_READ))     
             {
                threadAssertTest((handleTable.find(selector.getHandle(j)) != handleTable.end()),
                                 "Handle not found int acceptor table");
@@ -336,7 +336,7 @@ public:
           }
           threadAssertTest((num_found == mSelectedPorts.size()),
                            "Wrong number of readers found");
-          
+         
           // Tell senders that we are DONE_READING
           mCondVar.acquire();
           {
@@ -349,7 +349,7 @@ public:
        // Close Sockets and delete acceptors
        for(i=0;i<socks.size();i++)
        {
-          socks[i].close();          
+          socks[i].close();         
        }
        for(i=0;i<mNumRendevousPorts;i++)
        {
@@ -367,7 +367,7 @@ public:
       std::vector<vpr::Uint16> port_indicies(mNumRendevousPorts);     // This vector holds a list of valid indices to send data to
       std::iota(port_indicies.begin(), port_indicies.end(), 0);    // Fill with (0...n)
 
-      // WAIT for READY 
+      // WAIT for READY
       mCondVar.acquire();
       {
          while(mState != ACCEPTOR_READY)
@@ -388,15 +388,15 @@ public:
       // - Send the data
       // - Wait for recievers to say that it was good
       for(i=0;i<mNumIters;i++)
-      { 
+      {
          vpr::Uint16 num_ports = (random() % mNumRendevousPorts);          // Select some number of ports to send to
          if(num_ports == 0)
             num_ports = 1;                                                 // Make sure we always send some
-         
+        
          mSelectedPorts = std::vector<vpr::Uint16>(num_ports, 0);          // Replace old data with new of given size
          std::random_sample(port_indicies.begin(), port_indicies.end(),      // Select the random subset
                             mSelectedPorts.begin(), mSelectedPorts.end());
-  
+ 
          // Send data to the random subset of ports
          for(j=0;j<mSelectedPorts.size();j++)
          {
@@ -404,7 +404,7 @@ public:
             ret_val = sockets[mSelectedPorts[j]].write(mMessageValue, mMessageLen, bytes_written);    // Write the data
             threadAssertTest((ret_val.success()), "Error writing data on socket");
          }
-         
+        
          // SIGNAL that we have SENT_DATA
          mCondVar.acquire();
          {
@@ -436,11 +436,11 @@ public:
 
       test_suite->addTest( new TestCaller<SelectorTest>("testAcceptorPoolSelection", &SelectorTest::testAcceptorPoolSelection));
       test_suite->addTest( new TestCaller<SelectorTest>("testSendThenPoll", &SelectorTest::testSendThenPoll));
-                                         
+                                        
       return test_suite;
    }
 
-private:   
+private:  
    bool             mThreadAssertTest; // true for no error
 
 protected:
@@ -448,7 +448,7 @@ protected:
 
     vpr::Uint16     mRendevousPort;       // The port the acceptor will be waiting on
     vpr::Uint16     mNumRendevousPorts;   // The number of ports to use
-    
+   
     vpr::Uint16     mMessageLen;        // Length of message to send
     std::string     mMessageValue;      // The value of the message that is supposed to be sent (and recieved)
 
@@ -457,9 +457,9 @@ protected:
     vpr::CondVar    mCondVar;       // Condition variable
 
     std::vector<vpr::Uint16>     mSelectedPorts;      // List of ports that were selected to send data
-    
+   
 };
 
 }       // namespace
 
-#endif          
+#endif         
