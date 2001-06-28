ad1443afe010ab7c3db6f5ee5aa7116284ba421e
Allen Bierbaum <abierbaum@gmail.com>
2001-06-28 21:16:15 +0000

    Test for: Changed Selector interface to allow mask to be send as parameter to addHandle(...)
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@5264 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/vpr/IO/Selector_t.h
+++ juggler/modules/vapor/vpr/IO/Selector_t.h
@@ -59,9 +59,9 @@ public:
    //: Add the given handle to the selector
    //! PRE: handle is a valid handle
    //! POST: handle is added to the handle set, and initialized to a mask of no-events
-   bool addHandle(IOSys::Handle handle)
+   bool addHandle(IOSys::Handle handle, vpr::Uint16 mask=0)
    {
-      return mSelectorImp.addHandle(handle);
+      return mSelectorImp.addHandle(handle, mask);
    }
 
    //: Remove a handle from the selector
