e8db9f17534cc7bf2f4ad3c969901fe3a20a6997
Patrick Hartling <patrick.hartling@gmail.com>
2001-07-05 03:42:36 +0000

    Added a Perl script that can generate three types of template Makefile.in's for use with a configure script:
       1. A Doozer++ makefile that can do one of the following three build
          processes:
              A. Build object files from an auto-detected list of sources
                 coming from one or more user-specified directories
              B. Recurse through a list of directories
              C. A and B together
       2. A makefile capable of compiling a single application from an
          arbitrary auto-detected list of sources from an arbitrary list of
          user-specified directories.
       3. A makefile capable of compiling multiple applications from an
          arbitrary list of user-specified sources (compared against an
          auto-detected list for error checking) from an arbitrary list of
          user-specified directories.
    
    Given the right information, a real Makefile can be generated automatically
    for any of the above types as part of the creation process.
    
    The makefiles generated can be used without modification, though in typical
    use, I expect they will serve as more of a starting point requiring only
    minor modifications.  In any case, writing standard makefiles for anything
    built in VR Juggler (aside from the two top-level glue makefiles) should be
    trivial now.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@5315 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/release/scripts/SourceList.pm
@@ -0,0 +1,245 @@
+# ************** <auto-copyright.pl BEGIN do not edit this line> **************
+#
+# VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+#
+# Original Authors:
+#   Allen Bierbaum, Christopher Just,
+#   Patrick Hartling, Kevin Meinert,
+#   Carolina Cruz-Neira, Albert Baker
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# -----------------------------------------------------------------
+# File:          $RCSfile$
+# Date modified: $Date$
+# Version:       $Revision$
+# -----------------------------------------------------------------
+#
+# *************** <auto-copyright.pl END do not edit this line> ***************
+
+package SourceList;
+
+require 5.004;
+
+use strict qw(vars);
+
+# -----------------------------------------------------------------------------
+# Constructor.
+# -----------------------------------------------------------------------------
+sub new ($;@) {
+    my $class = shift;
+
+    # Make the anonymous array reference that will contain the list of files.
+    # If a list of files was given as the optional arguments to the
+    # constructor, assign that list to the new array reference.
+    # XXX: I suspect that this could all be done in a single assignment to
+    # \@_, but I am wary of using a reference to something that I didn't
+    # create myself.
+    my $files = [];
+    @{$files} = @_ if @_;
+
+    # The structure used for this class is a little complicated.  It uses
+    # the following data members:
+    #
+    #     all_files - An array reference.
+    #     suffixes  - A hash with suffixes as keys but with no associated
+    #                 values.  A hash is used to prevent duplication of
+    #                 suffixes.
+    #     dirs      - A hash whose keys are the directories containing source
+    #                 files.  The values associated with the keys are in turn
+    #                 hashes whose keys are suffixes.  The values associated
+    #                 with those keys are the actual files in an aray
+    #                 reference.  Conceptually, it looks like this:
+    #
+    #                     dirs => {
+    #                         dir1 => {
+    #                             .c   => [ file1.c, file2.c ],
+    #                             .cxx => [ file3.cxx, file4.cxx ],
+    #                             :
+    #                             :
+    #                         },
+    #                         dir2 => {
+    #                             .c++ => [ file5.c++, file6.c++ ],
+    #                             .cpp => [ file7.cpp, file8.cpp ],
+    #                             :
+    #                             :
+    #                         },
+    #                         :
+    #                         :
+    #                     }
+    return bless {
+        'all_files' => $files,    # Full list of all known source files
+        'suffixes'  => {},        # List of suffixes used by those files
+        'dirs'      => {}         # Complete categorization of source files
+    }, $class;
+}
+
+# -----------------------------------------------------------------------------
+# Destructor.
+# -----------------------------------------------------------------------------
+sub DESTROY ($) {
+    my $this = shift;
+    return 1;
+}
+
+# -----------------------------------------------------------------------------
+# Read all the source files from the given directory.  This populates this
+# object with the actual source list.
+# -----------------------------------------------------------------------------
+sub readSources ($$) {
+    my $this = shift;
+    my $dir  = shift;
+
+    my $status;
+
+    if ( opendir(SRCDIR, "$dir") ) {
+        my @all_files = readdir(SRCDIR);
+        closedir(SRCDIR);
+
+        ${$this->{'dirs'}}{"$dir"} = {};
+
+        my $file;
+        foreach $file ( @all_files ) {
+            # Only match C/C++ source files.
+            if ( -f "$file" && $file =~ /(\.(c\+\+|cpp|cxx|cc|c))$/i ) {
+                my $ext = "$1";
+
+                # Add this suffix to the main suffix hash.
+                ${$this->{'suffixes'}}{"$ext"} = '';
+
+                # Create the anonymous array for the files if this extension
+                # has not already been registered.
+                unless ( exists(${${$this->{'dirs'}}{"$dir"}}{"$ext"}) ) {
+                    ${${$this->{'dirs'}}{"$dir"}}{"$ext"} = [];
+                } 
+
+                # Add this file to the list of files for the the current file
+                # extension in this directory, and add it to the main list of
+                # all source files.
+                push(@{${$this->{'dirs'}{"$dir"}}{"$ext"}}, "$file");
+                push(@{$this->{'all_files'}}, "$file");
+            }
+        }
+
+        $status = 1;
+    }
+    else {
+        warn "WARNING: Could not open directory $dir: $!\n";
+        $status = 0;
+    }
+
+    return $status;
+}
+
+# -----------------------------------------------------------------------------
+# Deterime if the given directory is known to this source file list.
+#
+# Returns:
+#     0 - The directory is not in this source list.
+#     1 - The directory is in this source list.
+# -----------------------------------------------------------------------------
+sub hasDirectory ($$) {
+    my $this = shift;
+
+    return exists(${$this->{'dirs'}}{"$_[0]"});
+}
+
+# -----------------------------------------------------------------------------
+# Get all the files registered with this object.
+# -----------------------------------------------------------------------------
+sub getAllFiles ($) {
+    my $this = shift;
+    return @{$this->{'all_files'}};
+}
+
+# -----------------------------------------------------------------------------
+# Get all the directories registered with this object.
+# -----------------------------------------------------------------------------
+sub getDirectories ($) {
+    my $this = shift;
+    return keys(%{$this->{'dirs'}});
+}
+
+# -----------------------------------------------------------------------------
+# Get all the suffixes registered with this object or all the suffixes in use
+# within a given directory.
+# -----------------------------------------------------------------------------
+sub getSuffixes ($;$) {
+    my $this = shift;
+    my $dir  = shift || '';
+
+    my @suffixes = ();
+
+    # If a specific directory is requested, return only its suffixes.
+    if ( $dir ) {
+        @suffixes = keys(%{${$this->{'dirs'}}{"$dir"}});
+    }
+    # Otherwise, return all the known suffixes.
+    else {
+        @suffixes = keys(%{$this->{'suffixes'}});
+    }
+
+    return @suffixes;
+}
+
+# -----------------------------------------------------------------------------
+# Get all the files in the given directory with the given suffix.
+# -----------------------------------------------------------------------------
+sub getFiles ($$$) {
+    my $this   = shift;
+    my $dir    = shift;
+    my $suffix = shift;
+
+    return @{${${$this->{'dirs'}}{"$dir"}}{"$suffix"}};
+}
+
+# -----------------------------------------------------------------------------
+# Find a file in this source list.  Its containing directory is returned to
+# the caller, or undef is returned if the file is not in this list.
+# -----------------------------------------------------------------------------
+sub findFile ($$) {
+    my $this = shift;
+    my $file = shift;
+
+    my($dir, $suffix);
+    foreach $dir ( $this->getDirectories() ) {
+        foreach $suffix ( $this->getSuffixes("$dir") ) {
+            if ( $file =~ /$suffix$/i ) {
+                foreach ( $this->getFiles("$dir", "$suffix") ) {
+                    return $dir if "$file" eq "$_";
+                }
+            }
+        }
+    }
+
+    return undef;
+}
+
+# -----------------------------------------------------------------------------
+# Insert a file into this source list within the given directory.
+# -----------------------------------------------------------------------------
+sub insertFile ($$$) {
+    my $this = shift;
+    my $file = shift;
+    my $dir  = shift;
+
+    $file =~ /(\.(c\+\+|cpp|cxx|cc|c))$/i;
+    my $ext = "$1";
+
+    push(@{${${$this->{'dirs'}}{"$dir"}}{"$ext"}}, "$file");
+}
+
+1;
---
+++ juggler_1.0/release/scripts/mkmakefile.pl
@@ -0,0 +1,577 @@
+#!/usr/bin/perl
+
+# ************** <auto-copyright.pl BEGIN do not edit this line> **************
+#
+# VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+#
+# Original Authors:
+#   Allen Bierbaum, Christopher Just,
+#   Patrick Hartling, Kevin Meinert,
+#   Carolina Cruz-Neira, Albert Baker
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# -----------------------------------------------------------------
+# File:          $RCSfile$
+# Date modified: $Date$
+# Version:       $Revision$
+# -----------------------------------------------------------------
+#
+# *************** <auto-copyright.pl END do not edit this line> ***************
+
+require 5.004;
+
+use strict 'vars';
+use vars qw($dir_prfx $dotin $exp_file $gmake $makefile_name $sub_objdir);
+use vars qw(%VARS);
+
+use File::Basename;
+use File::Copy;
+use Getopt::Long;
+
+# Do this to include the path to the script in @INC.
+my $path;
+
+BEGIN {
+    $path = (fileparse("$0"))[1];
+}
+
+use lib qw($path /home/mystify /home/mystify/juggler/release/scripts);
+use InstallOps;
+use SourceList;
+
+# Subroutine prototypes.
+sub printHelp();
+sub findSources(@);
+sub readSources($);
+sub printObjMakefile($@);
+sub printAppMakefile($$);
+sub printMultiAppMakefile($$);
+sub printAppMakefileStart_in($@);
+sub printAppObjs_in($$$);
+sub printAppSuffixRules_in($$);
+sub printAppMakefileEnd_in($;@);
+sub expandAll($);
+
+# Set default values for all variables used as storage by GetOptions().
+my $app     = '';
+my %apps    = ();
+my $gmake   = 0;
+my $help    = 0;
+my @srcdirs = ();
+my @subdirs = ();
+
+$dir_prfx   = '';
+$dotin      = 1;
+$sub_objdir = '';
+
+GetOptions('app=s' => \$app, 'apps=s' => \%apps, 'dirprefix=s' => \$dir_prfx,
+           'dotin!' => \$dotin, 'expfile=s' => \$exp_file, 'gmake' => \$gmake,
+           'help' => \$help, 'srcdir=s' => \@srcdirs, 'subdirs=s' => \@subdirs,
+           'subobjdir=s' => \$sub_objdir)
+    or printHelp() && exit(1);
+
+printHelp() && exit(0) if $help;
+
+# This allows source directories to be specified using multiple --srcdir
+# options and/or using the form --srcdir=dir1,dir2,...,dirN.
+@srcdirs = split(/,/, join(',', @srcdirs));
+@subdirs = split(/,/, join(',', @subdirs));
+
+# Set the generated file name once since all the subroutines will create a
+# file with the same name.
+$makefile_name = 'Makefile';
+$makefile_name .= ".in" if $dotin;
+
+my $all_src = findSources(@srcdirs);
+
+# Generate a single-application makefile.
+if ( $app ) {
+    printAppMakefile($all_src, "$app");
+}
+# Generate a multi-application makefile.
+elsif ( keys(%apps) ) {
+    printMultiAppMakefile($all_src, \%apps);
+}
+# Generate a Doozer++ object-building makefile.
+else {
+    printObjMakefile($all_src, @subdirs);
+}
+
+exit(0);
+
+# =============================================================================
+# Subroutines follow.
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+# Print a usage message explaining how to use this script.
+# -----------------------------------------------------------------------------
+sub printHelp () {
+    print <<EOF;
+Usage:
+    $0
+        [ --app=<name> | --apps <name1>=<srcs1> [ --apps <name2>=<srcs2> ... ]]
+        [ --dirprefix=<prefix> ] [ --dotin | --nodotin --expfile=<file> ]
+        [ --gmake ] [ --srcdir=<dirs> [...] ] [ --subdirs=<dirs> [...] ]
+        [ --subobjdir=<dir> ]
+
+For application makefiles:
+
+    --app=<name>
+        Specify the name of the application to be compiled.
+
+  or
+
+    --apps <name1>=<srcs1> --apps <name2>=<srcs2> ... --apps <nameN>=<srcsN>
+        Specify the names of multiple applications and the lists of source
+        files that will be compiled for each application.
+
+    --gmake
+        Use GNU make features.
+
+    --srcdir=<dirs1> --srcdir=<dirs2> ... --srcdir=<dirsN>
+        Give a list of directories besides the current directory where
+        source files will be found.  Each directory can be given as an
+        argument to a separate --srcdir option, or they can all be passed
+        to a single --srcdir option as a comma-separated list (no spaces).
+
+For object makefiles:
+
+    --dirprefix=<prefix>
+        Name a prefix for the directory containing the source files.
+
+    --subdirs=<dirs1> --subdirs=<dirs2> ... --subdirs=<dirsN>
+        Give a list of subdirectories of the current directory where the
+        build must recurse.  Each subdirectory can be given as an argument
+        to a separate --subdirs option, or they can all be passed to a
+        single --subdirs option as a comma-separated list (no spaces).
+
+    --subobjdir=<dir>
+        Name the subdirectory of \$(OBJDIR) where the object files will go.
+
+For both types of makefiles:
+
+    --dotin (default)
+        Generate Makefile.in
+
+  or
+
+    --nodotin --expfile=<Perl file>
+        Generate a fully expanded Makefile using the %VARS hash in the
+        file named as the argument to --expfile.
+EOF
+}
+
+# -----------------------------------------------------------------------------
+# Find all the source files in the given directories.
+# -----------------------------------------------------------------------------
+sub findSources (@) {
+    my @dirs = @_;
+
+    # Make sure that the current directory is in the list of directories to
+    # search.
+    my $found_dot = 0;
+
+    foreach ( @dirs ) {
+        if ( "$_" eq "." ) {
+            $found_dot = 1;
+            last;
+        }
+    }
+
+    push(@dirs, ".") unless $found_dot;
+
+    # Create the primary SourceList object.  It will be populated with
+    # information in the following loop.
+    my $srcs = new SourceList();
+    my $dir;
+
+    # Loop over the given source directories.
+    foreach $dir ( @dirs ) {
+        next if $srcs->hasDirectory("$dir");
+        $srcs->readSources("$dir") or warn "Failed to read sources in $dir\n";
+    }
+
+    return $srcs;
+}
+
+# -----------------------------------------------------------------------------
+# Generate a makefile for use with Doozer++ that can compile object files and
+# recurse into subdirectories if necessary.
+# -----------------------------------------------------------------------------
+sub printObjMakefile ($@) {
+    my $srcs    = shift;
+    my @subdirs = @_;
+
+    # Create the makefile.
+    open(MAKEFILE, "> $makefile_name")
+        or die "ERROR: Could not create $makefile_name: $!\n";
+
+    my $include_dir = '@includedir@';
+    $include_dir .= "/$dir_prfx" if $dir_prfx;
+
+    # Print out the basic stuff that is common to all types of Doozer++
+    # makefiles.
+    print MAKEFILE <<MK_START;
+default: all
+
+# Include common definitions here.
+#include ...
+
+includedir	= $include_dir
+srcdir		= \@srcdir\@
+top_srcdir	= \@top_srcdir\@
+INSTALL		= \@INSTALL\@
+MK_START
+
+    my($has_objs, $has_subdirs) = (0, 0);
+
+    # Get a list of all the files in the directory.
+    my @src_files = $srcs->getAllFiles();
+    $has_objs = 1 if $#src_files > -1;
+
+    if ( $sub_objdir && $has_objs ) {
+        print MAKEFILE "SUBOBJDIR\t= $sub_objdir\n";
+    }
+
+    print MAKEFILE "\n";
+
+    # If there are subdirectories, list them.
+    if ( $#subdirs > -1 ) {
+        $has_subdirs = 1;
+        print MAKEFILE "DIRPRFX\t= $dir_prfx/\n\n" if $dir_prfx;
+
+        print MAKEFILE "# Subdirectories to compile.\n";
+        print MAKEFILE "SUBDIR\t=";
+
+        foreach ( @subdirs ) {
+            print MAKEFILE " $_" unless "$_" eq "." ;
+        }
+
+        print MAKEFILE "\n\n";
+    }
+
+    # If there are object files to compile, list the source files.
+    if ( $has_objs ) {
+        print MAKEFILE "SRCS\t= ", join(' ', sort(@src_files)), "\n";
+
+        # If the directory list has more than one element (which would be
+        # the current directory), add an assignment for $(EXTRA_SRCS_PATH) so
+        # that make will know where to find all the source files.
+        my @other_dirs = $srcs->getDirectories();
+        if ( $#other_dirs > 0 ) {
+            print MAKEFILE "EXTRA_SRCS_PATH\t= ", join(' ', sort(@other_dirs)),
+                           "\n";
+        }
+
+        print MAKEFILE "\n";
+    }
+
+    warn "No object files found, no subdirectories listed!\n"
+        unless $has_objs || $has_subdirs;
+
+    # Include the mk file that build objects and recurses if both object files
+    # and subdirectories must be handled.
+    if ( $has_objs && $has_subdirs ) {
+        print MAKEFILE "include \$(MKPATH)/dpp.obj-subdir.mk\n\n";
+    }
+    # If we have only objects to build, include that mk file.
+    elsif ( $has_objs ) {
+        print MAKEFILE "include \$(MKPATH)/dpp.obj.mk\n\n";
+    }
+    # If we have only subdirectories, include the recursion file.
+    elsif ( $has_subdirs ) {
+        print MAKEFILE "\$(RECTARGET): recursive\n\n";
+        print MAKEFILE "include \$(MKPATH)/dpp.subdir.mk\n\n";
+    }
+
+    # If this directory has object files, generate the dependency including
+    # code too.
+    if ( $has_objs ) {
+        print MAKEFILE <<MK_END;
+# -----------------------------------------------------------------------------
+# Include dependencies generated automatically.
+# -----------------------------------------------------------------------------
+ifndef DO_CLEANDEPEND
+include \$(DEPEND_FILES)
+endif
+MK_END
+    }
+
+    close(MAKEFILE) or warn "WARNING: Could not close $makefile_name: $!\n";
+
+    # Now replace all the @...@ strings if this is not a Makefile.in.
+    unless ( $dotin ) {
+        expandAll($makefile_name);
+    }
+}
+
+# -----------------------------------------------------------------------------
+# Generate a makefile capable of compiling a single application.  This is
+# relatively simple.
+# -----------------------------------------------------------------------------
+sub printAppMakefile ($$) {
+    my $srcs = shift;
+    my $app  = shift;
+
+    my @dirs = $srcs->getDirectories();
+
+    open(MAKEFILE, "> $makefile_name")
+        or die "ERROR: Could not create $makefile_name: $!\n";
+
+    print MAKEFILE "default: $app\@EXEEXT\@\n\n";
+
+    printAppMakefileStart_in(MAKEFILE, @dirs);
+    printAppObjs_in(MAKEFILE, 'OBJS', $srcs);
+
+    # Print the target for the application.
+    print MAKEFILE <<MK_END;
+$app\@EXEEXT\@: \$(OBJS)
+	\$(LINK) \@EXE_NAME_FLAG\@ \$(OBJS) \$(BASIC_LIBS) \$(EXTRA_LIBS)
+
+MK_END
+
+    printAppSuffixRules_in(MAKEFILE, $srcs);
+    printAppMakefileEnd_in(MAKEFILE, "$app");
+
+    close(MAKEFILE) or warn "WARNING: Could not close $makefile_name: $!\n";
+
+    # Now replace all the @...@ strings if this is not a Makefile.in.
+    unless ( $dotin ) {
+        expandAll($makefile_name);
+    }
+}
+
+# -----------------------------------------------------------------------------
+# Print a makefile that can compile multiple applications.  This would be
+# simpler, but I put in some sanity checking that verifies that the source
+# files listed on the command line actually exist.
+# -----------------------------------------------------------------------------
+sub printMultiAppMakefile ($$) {
+    my $srcs = shift;
+    my %apps = %{$_[0]};
+
+    my @dirs = $srcs->getDirectories();
+
+    open(MAKEFILE, "> $makefile_name")
+        or die "ERROR: Could not create $makefile_name: $!\n";
+
+    print MAKEFILE "default: all\n\n";
+
+    printAppMakefileStart_in(MAKEFILE, @dirs);
+
+    my @cur_srcs = ();
+    my @all_apps = sort(keys(%apps));
+    my $app;
+
+    # Loop over all the applications and verify that their specific source
+    # files exist.
+    foreach $app ( @all_apps ) {
+        @cur_srcs = split(/,/, "$apps{$app}");
+
+        # Create a new SourceList object for this application's source list.
+        my $cur_app_src = new SourceList(@cur_srcs);
+
+        # This the actual sanity check loop.
+        my $file;
+        foreach $file ( @cur_srcs ) {
+            # Find the current file in the known list.
+            my $dir = $srcs->findFile("$file");
+
+            # If it was found, insert it into the correct directory block for
+            # the SourceList object.
+            if ( $dir ) {
+                $cur_app_src->insertFile("$file", "$dir");
+            }
+            # If it was not found, complain.
+            else {
+                warn "WARING: Could not find file $file!\n";
+            }
+        }
+
+        # Finally, generate the object list for this application.
+        printAppObjs_in(MAKEFILE, "${app}_OBJS", $cur_app_src);
+    }
+
+    # Loop over the applications again and print the targets that build the
+    # actual executables.
+    foreach ( @all_apps ) {
+        print MAKEFILE <<MK_END;
+$_\@EXEEXT\@: \$(${_}_OBJS)
+	\$(LINK) \@EXE_NAME_FLAG\@ \$(${_}_OBJS) \$(BASIC_LIBS) \$(EXTRA_LIBS)
+
+MK_END
+    }
+
+    printAppSuffixRules_in(MAKEFILE, $srcs);
+    printAppMakefileEnd_in(MAKEFILE, @all_apps);
+
+    close(MAKEFILE) or warn "WARNING: Could not close $makefile_name: $!\n";
+
+    # Now replace all the @...@ strings if this is not a Makefile.in.
+    unless ( $dotin ) {
+        expandAll($makefile_name);
+    }
+}
+
+# -----------------------------------------------------------------------------
+# Print the variable assignments and other "leading" information common to all
+# applications to be compiled using this system.
+# -----------------------------------------------------------------------------
+sub printAppMakefileStart_in ($@) {
+    my $handle = shift;
+    my @dirs   = @_;
+
+    # Construct the complete list of include paths.
+    my $inc_line = '@APP_INCLUDES@';
+    foreach ( @dirs ) {
+        if ( "$_" eq "." ) {
+            $inc_line .= ' -I$(srcdir)';
+        }
+        else {
+            $inc_line .= " -I\$(srcdir)/$_";
+        }
+    }
+
+    print $handle <<MK_START;
+# Basic options.
+srcdir		= \@srcdir\@
+CFLAGS		= \@APP_CFLAGS\@ \$(EXTRA_FLAGS) \$(INCLUDES) \$(DEFS)
+CXXFLAGS	= \@APP_CXXFLAGS\@ \$(EXTRA_FLAGS) \$(INCLUDES) \$(DEFS)
+DEFS		= \@APP_DEFS\@
+EXTRA_FLAGS	= \@APP_EXTRA_FLAGS\@ \$(DEBUG_FLAGS\)
+DEBUG_FLAGS	= \@APP_DEBUG_FLAGS\@
+OPTIM_FLAGS	= \@APP_OPTIM_FLAGS\@
+INCLUDES	= $inc_line
+LINK_FLAGS	= \@APP_LINK_FLAGS\@ \$(EXTRA_FLAGS)
+
+# Libraries needed for linking.
+BASIC_LIBS	= \@APP_BASIC_LIBS_BEGIN\@ \@APP_BASIC_LIBS\@ \@APP_BASIC_EXT_LIBS\@ \@APP_BASIC_LIBS_END\@
+EXTRA_LIBS	= \@APP_EXTRA_LIBS_BEGIN\@ \@APP_EXTRA_LIBS\@ \@APP_EXTRA_LIBS_END\@ 
+
+# Commands to execute.
+C_COMPILE	= \@APP_CC\@ \$(CFLAGS)
+CXX_COMPILE	= \@APP_CXX\@ \$(CXXFLAGS)
+LINK		= \@APP_LINK\@ \$(LINK_FLAGS)
+
+MK_START
+
+    # Fill in the VPATH info.  We'll take advantage of GNU make syntax where
+    # possible.
+    if ( $gmake ) {
+        print $handle "vpath \$(srcdir)";
+
+        foreach ( @dirs ) {
+            if ( "$_" ne "." ) {
+                print $handle " \$(srcdir)/$_";
+            }
+        }
+    }
+    else {
+        print $handle "VPATH = \@srcdir\@";
+
+        foreach ( @dirs ) {
+            if ( "$_" ne "." ) {
+                print $handle ":\@srcdir\@/$_";
+            }
+        }
+    }
+
+    # Finish the VPATH line.
+    print MAKEFILE "\n\n";
+}
+
+# -----------------------------------------------------------------------------
+# Print the assignment for the list of object files to be compiled.  This code
+# looks pretty simple, but the ugliness is being hidden by the SourceList
+# class.
+# -----------------------------------------------------------------------------
+sub printAppObjs_in ($$$) {
+    my($handle, $obj_var, $srcs) = @_;
+
+    # Get the list of files as a scalar and change the suffixes to @OBJEXT@.
+    my $files = join(' ', sort($srcs->getAllFiles()));
+    $files    =~ s/\.(cpp|cxx|c\+\+|cc|c)/.\@OBJEXT\@/gi;
+
+    # XXX: Wrap me!
+    print $handle "$obj_var\t= $files\n\n";
+}
+
+# -----------------------------------------------------------------------------
+# Print the suffix rules needed for this appliation makefile.  Only the rules
+# needed for the known sources are generated to keep the file simple.
+# -----------------------------------------------------------------------------
+sub printAppSuffixRules_in ($$) {
+    my($handle, $srcs) = @_;
+
+    my @suffixes = sort($srcs->getSuffixes());
+
+    print $handle "# Suffix rules for building object files.\n";
+    print $handle ".SUFFIXES: ", join(' ', @suffixes), " .\@OBJEXT\@\n\n";
+
+    # XXX: Could use GNU stuff too.
+    foreach ( @suffixes ) {
+        print $handle "$_.\@OBJEXT\@:\n";
+
+        # C source file.
+        if ( "$_" eq ".c" ) {
+            print $handle "\t\$(C_COMPILE) \@OBJ_NAME_FLAG\@ \@OBJ_BUILD_FLAG\@ \$<\n\n";
+        }
+        # C++ source file.
+        else {
+            print $handle "\t\$(CXX_COMPILE) \@OBJ_NAME_FLAG\@ \@OBJ_BUILD_FLAG\@ \$<\n\n";
+        }
+    }
+}
+
+# -----------------------------------------------------------------------------
+# Finish off the application makefile by adding 'clean' and 'clobber' targets.
+# -----------------------------------------------------------------------------
+sub printAppMakefileEnd_in ($;@) {
+    my $handle = shift;
+    my @apps   = @_;
+
+    my $dirt ='';
+    foreach ( @_ ) {
+        $dirt .= "$_\@EXEEXT\@ ";
+    }
+
+    print $handle <<MK_END;
+# Clean-up targets.
+clean:
+	rm -f *.\@OBJEXT\@ core*
+	rm -rf ii_files
+
+clobber:
+	\@\$(MAKE) clean
+	rm -f $dirt
+MK_END
+}
+
+# -----------------------------------------------------------------------------
+# Expand all the @...@ strings using the %VARS hash.
+# -----------------------------------------------------------------------------
+sub expandAll ($) {
+    my $filename = shift;
+
+    # Suck in the full %VARS hash.
+    require "$exp_file" if $exp_file;
+
+    warn "WARNING: No expansion values!\n" unless keys(%VARS) && $exp_file;
+
+    replaceTags("$filename", %VARS);
+}
