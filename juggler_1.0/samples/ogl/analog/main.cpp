083d11badffe06b6f486d8b1f8e67bf4f6cfc805
Patrick Hartling <patrick.hartling@gmail.com>
2001-07-05 17:00:34 +0000

    Added a missing header (VPR/vjSystem.h) and changed the use of vjSystemBase to vjSystem.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@5328 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler_1.0/samples/ogl/analog/main.cpp
+++ juggler_1.0/samples/ogl/analog/main.cpp
@@ -33,6 +33,7 @@
 // VR Juggler
 #include <Kernel/vjKernel.h>     // vr juggler kernel
 #include <Kernel/vjProjection.h> // for setNearFar
+#include <VPR/vjSystem.h>
 
 // the application
 #include <application.h>         // the application
@@ -81,6 +82,6 @@ int main( int argc, char* argv[] )
    std::cout << "Press CTRL-C to exit\n" << std::flush;
    while (1)
    {
-      vjSystemBase::usleep( 100000 );
+      vjSystem::usleep( 100000 );
    }
 }
