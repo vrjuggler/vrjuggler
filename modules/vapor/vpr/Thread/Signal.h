d21987afdebaf79350da5bcaabe9da7f20d0f75a
Patrick Hartling <patrick.hartling@gmail.com>
2001-07-09 16:03:10 +0000

    Added a first pass at an abstraction for signal handling by threads.  I was going to base this on the way that ACE does it, but as usual, ACE's implementation carried with it a lot of overhead and complexity.  This initial version is extremely rough and has received only partial testing in some VR Juggler code I wrote months ago.  I wanted to get this committed to VPR now so that we can refine it and put it to good use later.
    Writing a simple multi-threaded test for this code should be pretty easy,
    though.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@5403 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/Thread/Signal.h
@@ -0,0 +1,314 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VPR_SIGNAL_HANDLER_H_
+#define _VPR_SIGNAL_HANDLER_H_
+
+#include <vpr/vprConfig.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#else
+#error "I don't know what to do without signal.h!"
+#endif
+
+#include <vpr/Util/Status.h>
+
+
+namespace vpr {
+
+// XXX: Use a functor for the real handler function?
+#ifdef VPR_OS_Win32
+typedef RETSIGTYPE (__cdecl *SignalHandler_t)(int);
+#else
+typedef RETSIGTYPE (*SignalHandler_t)(int);
+#endif
+
+}
+
+#ifndef HAVE_SIGACTION
+struct sigaction {
+    int                  sa_flags;      /**< see signal options below */
+    vpr::SignalHandler_t sa_handler;    /**< signal handler */
+    sigset_t             sa_mask;       /**< signal mask to apply */
+};
+#endif
+
+namespace vpr {
+
+/**
+ * Wrapepr class for a signal set.  A signal set contains the set of system
+ * signals to be masked when used with a vpr::SignalAction.
+ *
+ * @author Patrick Hartling
+ */
+class VPR_CLASS_API SignalSet {
+public:
+    /**
+     * Initialize <m_sigset> with <sigset>.  If <sigset> == 0 then fill
+     * the set.
+     *
+     * @param sigset The signal set used for initializing this object.
+     */
+    SignalSet (const sigset_t* const sigset) {
+        if ( sigset == 0 ) {
+            fillSet();
+        }
+        else {
+            m_sigset = *sigset;
+        }
+    }
+
+    /**
+     * If <fill> == false then initialize the <m_sigset> to be empty, else
+     * full.
+     *
+     * @param fill A boolean flag stating whether to fill this object's
+     *        signal set or make it empty.
+     */
+    SignalSet (bool fill = false) {
+        if ( fill ) {
+            fillSet();
+        }
+        else {
+            emptySet();
+        }
+    }
+
+    /**
+     * Creates a set that includes no signals defined by the system.
+     *
+     * @return vpr::Status::Success is returned if this object's signal set is
+     *         emptied.  vpr::Status::Failure is returned otherwise.
+     */
+    vpr::Status emptySet(void);
+
+    /**
+     * Creates a set that includes all signals defined by the system.
+     *
+     * @return vpr::Status::Success is returned if this object's signal set is
+     *         filled.  vpr::Status::Failure is returned otherwise.
+     */
+    vpr::Status fillSet(void);
+
+    /**
+     * Adds the individual signal specified by <sig_num> to the set.
+     *
+     * @param sig_num The signal number to be added.
+     *
+     * @return vpr::Status::Success is returned if the given signal is added
+     *         to this object's signal set.  vpr::Status::Failure is returned
+     *         otherwise.
+     */
+    vpr::Status addSignal(const int sig_num);
+
+    /**
+     * Deletes the individual signal specified by <sig_num> from the set.
+     *
+     * @param sig_num The signal number to be removed.
+     *
+     * @return vpr::Status::Success is returned if the given signal is removed
+     *         from this object's signal set.  vpr::Status::Failure is returned
+     *         otherwise.
+     */
+    vpr::Status removeSignal(const int sig_num);
+
+    /**
+     * Checks whether the signal specified by <sig_num> is in the set.
+     *
+     * @param sig_num The signal number being checked.
+     *
+     * @return true is returned if <sig_num> is in this object's set.  false
+     *         is returned otherwise.
+     */
+    bool isMember(const int sig_num) const;
+
+    /**
+     * Returns a constant pointer to the underlying sigset_t variable.
+     *
+     * @return A constant pointer to this object's sigset_t variable.
+     */
+    inline const sigset_t* const
+    getMask (void) const {
+        return &m_sigset;
+    }
+
+private:
+    sigset_t m_sigset;
+};
+
+/**
+ * Wrapper class for a signal action.  A signal action is used to encapsulate
+ * the handler callback, the set of signals to be masked, and flags specifying
+ * optional behavior when handling the signal.
+ *
+ * @author Patrick Hartling
+ */
+class VPR_CLASS_API SignalAction {
+public:
+    /**
+     * Constructs an object that will hold the given handler, the given mask,
+     * and any optional flags set.
+     *
+     * @post The encapsulated m_sa variable is fully initialized.
+     *
+     * @param handler The actual handler callback.  Set to SIG_IGN to ignore
+     *                the signal with which this action object will be
+     *                associated.
+     * @param sig_set The signal set mask used to mask out signals we are not
+     *                interested in.  This is an optional parameter and
+     *                defaults to NULL (which implies that no signals will be
+     *                masked).
+     * @param flags   Options defining the behavior for handling the signal.
+     *                This is an optional parameter and defaults to 0.
+     */
+    SignalAction (vpr::SignalHandler_t handler,
+                  const vpr::SignalSet* sig_set = NULL, const int flags = 0);
+
+private:
+    /**
+     * Default constructor.  We do not want this to be called.
+     */
+    SignalAction (void) {
+        /* Do nothing. */ ;
+    }
+
+    /**
+     * Initializes this object.
+     *
+     * @post The encapsulated m_sa variable is fully initialized.
+     *
+     * @param handler The actual handler callback.  Set to SIG_IGN to ignore
+     *                the signal with which this action object will be
+     *                associated.
+     * @param sig_set The signal set mask used to mask out signals we are not
+     *                interested in.  If the mask is NULL, no mask is applied.
+     * @param flags   Options defining the behavior for handling the signal.
+     */
+    inline void
+    init (vpr::SignalHandler_t handler, const sigset_t* mask, const int flags)
+    {
+        memset(&m_sa, '\0', sizeof(m_sa));
+        m_sa.sa_handler = handler;
+        m_sa.sa_flags   = flags;
+
+        if ( mask != NULL ) {
+            memcpy(&m_sa.sa_mask, mask, sizeof(sigset_t));
+        }
+    }
+
+public:
+    struct sigaction m_sa;
+};
+
+/**
+ * This class wraps the calls needed to register signal handlers with the
+ * operating system.  It currently has only static member functions and thus
+ * is not really much of a class at all.
+ *
+ * @author Patrick Hartling
+ */
+class VPR_CLASS_API SigHandler {
+    /**
+     * Reigsters the given callback as a handler for the named signal.  No
+     * mask is applied, and no option flags are set.  To get a mask and/or
+     * option flags, use a vpr::SignalAction object.
+     *
+     * @post If <sig_num> is a valid signal number, <handler> is registered
+     *       as a handler for that signal.
+     *
+     * @param sig_num The signal to be handled.
+     * @param handler The callback that does the signal handling.
+     *
+     * @returns vpr::Status::Success is returned if the given handler is
+     *          registered successfully; vpr::Status::Failure otherwise.
+     */
+    static vpr::Status registerHandler(const int sig_num,
+                                       vpr::SignalHandler_t handler);
+
+    /**
+     * Reigsters the given callback as a handler for the named signal.  No
+     * mask is applied, and no option flags are set.  To get a mask and/or
+     * option flags, use a vpr::SignalAction object.
+     *
+     * @pre  <action> is a reference to a valid vpr::SignalAction object.
+     * @post If <sig_num> is a valid signal number, <action> is registered
+     *       as the action to be taken when that signal is raised.
+     *
+     * @param sig_num The signal to be handled.
+     * @param action  A vpr::SignalAction object encapsulating all the
+     *                information needed to handle the named signal.
+     *
+     * @returns vpr::Status::Success is returned if the given action is
+     *          registered successfully; vpr::Status::Failure otherwise.
+     */
+    static vpr::Status registerHandler(const int sig_num,
+                                       vpr::SignalAction& action);
+
+protected:
+    /**
+     * Default constructor.  It is protected because there is no need for this
+     * class to be instantiated at this point.
+     */
+    SigHandler (void) {
+        /* Do nothing. */ ;
+    }
+
+    /**
+     * Wrapper around the system call sigaction(2).
+     *
+     * @pre  <action> points to a valid sigaction struct or is NULL.
+     * @post If <sig_num> is a valid signal number, <action> is registered as
+     *       the action to be taken when that signal is raised.
+     *
+     * @param sig_num The signal to be handled.
+     * @param action  A vpr::SignalAction object encapsulating all the
+     *                information needed to handle the named signal.  If
+     *                <action> is NULL, the signal will be ignored.
+     * @param old_act Storage for the previous action registered for the given
+     *                signal number.  If it is NULL, the previous action is
+     *                discarded.
+     *
+     * @returns 0 is returned on successful action registration; -1 is returned
+     *          otherwise.
+     */
+    static int sigaction(const int sig_num, const struct sigaction* action,
+                         struct sigaction* old_act = NULL);
+};
+
+} // End of vpr namespace
+
+
+#endif /* _VPR_SIGNAL_HANDLER_H_ */
