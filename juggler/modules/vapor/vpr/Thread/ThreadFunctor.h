60e6c4df73a02d43fbf1476a0184d0d7e32186bd
Patrick Hartling <patrick.hartling@gmail.com>
2001-07-09 14:40:03 +0000

    Converted documentation style to JavaDoc.  This file takes advantage of Doxygen's documentation inheritance to reduce that actual amount of written documentation.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@5400 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/vpr/Thread/ThreadFunctor.h
+++ juggler/modules/vapor/vpr/Thread/ThreadFunctor.h
@@ -39,24 +39,45 @@
 
 namespace vpr {
 
-//---------------------------------------------------------------
-//: Converts a function into a functor that can be passed to a
-//  extern C type function to be called by a thread creation
-//  routine.
-//!PUBLIC_API:
-//---------------------------------------------------------------
+/**
+ * Converts a function into a functor that can be passed to a
+ * extern C type function to be called by a thread creation
+ * routine.
+ *
+ * @author Allen Bierbaum
+ */
 class VPR_CLASS_API BaseThreadFunctor
 {
 public:
+    /**
+     * Overloaded operator() used for invoking the function encapsulated by
+     * this object.  This version takes no argument and instead passes the
+     * argument given when this object was constructed.
+     */
     virtual void operator()() = 0;    // Pure virtual
-    virtual void operator()(void*) = 0;
+
+    /**
+     * Overloaded operator() used for invoking the function encapsulated by
+     * this object.  This version takes an argument that is passed on to the
+     * function.
+     *
+     * @param arg The argument to be passed to the encapsulated function.
+     */
+    virtual void operator()(void* arg) = 0;
+
+    /**
+     * Sets the argument passed to the encapsulated function.  This will be
+     * used when the function is invoked through the overloaded operator().
+     */
     virtual void setArg(void*) = 0;
 };
 
-//---------------------------------------------------------------
-//: Member functor class.
-//!PUBLIC_API:
-//---------------------------------------------------------------
+/**
+ * Member functor class.  This class allows non-static class member functions
+ * to be used as functors.
+ *
+ * @author Allen Bierbaum
+ */
 template<class T>
 class VPR_CLASS_API ThreadMemberFunctor : public BaseThreadFunctor
 {
@@ -92,20 +113,24 @@ private:
 };
 
 
-//---------------------------------------------------------------
-//: Nonmember functor class.  Converts a NonMember function into
-//+ a functor.
-//
-//!PUBLIC_API:
-//---------------------------------------------------------------
+/**
+ * Nonmember functor class.  Converts a non-member function or a static class
+ * member function into a functor.
+ *
+ * @author Allen Bierbaum
+ */
 class VPR_CLASS_API ThreadNonMemberFunctor : public BaseThreadFunctor
 {
 public:
     typedef void(* NonMemFunPtr)(void*);
 
-    //---------------------------------------------------------------
-    //: Constructor.
-    //---------------------------------------------------------------
+    /**
+     * Constructor.
+     *
+     * @param f A pointer to a function that is not a class member.
+     * @param a A pointer to an argument to be passed to the function.  This
+     *          argument is optional and defaults to NULL.
+     */
     ThreadNonMemberFunctor (NonMemFunPtr f, void* a = NULL)
        : func(f), argument(a)
     {;}
@@ -124,8 +149,8 @@ public:
 
     // private:
     // = Arguments to thread startup.
-    NonMemFunPtr func;	// Thread startup function (C++ linkage).
-    void* argument;		// Argument to thread startup function.
+    NonMemFunPtr func;  /**< Thread startup function (C++ linkage). */
+    void* argument;     /**< Argument to thread startup function. */
 };
 
 //---------------------------------------------
@@ -143,7 +168,7 @@ public:
         vprThreadFunctorFunction(void* args);
 #else
     extern "C" void vprThreadFunctorFunction(void* args);
-#endif	/* VPR_IRIX_SPROC */
+#endif	/* VPR_USE_IRIX_SPROC */
 
 }; // End of vpr namespace
 
