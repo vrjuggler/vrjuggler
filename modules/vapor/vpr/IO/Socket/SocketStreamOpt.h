10c424313dda63c39827701954c1111be81f1089
Patrick Hartling <patrick.hartling@gmail.com>
2001-07-18 02:34:38 +0000

    Silence GCC warning by making the destructor virtual.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@5544 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/IO/Socket/SocketStreamOpt.h
+++ modules/vapor/vpr/IO/Socket/SocketStreamOpt.h
@@ -60,7 +60,7 @@ public:
      * Default destructor.  This does nothing and exists only so that we can
      * build a DLL on Win32.
      */
-    ~SocketStreamOpt(void);
+    virtual ~SocketStreamOpt(void);
 
     /**
      *
