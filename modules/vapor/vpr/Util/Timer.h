24e73ace53d75d139b40088e3850c0cf3acc4681
Patrick Hartling <patrick.hartling@gmail.com>
2001-07-26 02:16:30 +0000

    ^I --> '   ' and other whitespace clean-up.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@5686 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/Util/Timer.h
+++ modules/vapor/vpr/Util/Timer.h
@@ -53,77 +53,77 @@ namespace vpr {
 class Timer
 {
 public:
-	Timer()
-	{
-		totalTime = 0;
-		timeCount = 0;
-		startTime = 0;
-		endTime = 0;
-	}
-	
-	//: Start a timing
-	void startTiming()
-	{ startTime = wallclock(); }
-	
-	//: Stop current timing
-	//! PRE: Timing has been started
-	void stopTiming()
-	{ 
-		endTime = wallclock();
-		if (startTime == 0)	// If we haven't called start then ignore
-		{ totalTime = 0; }
-		else
-		{
-		    lastTiming = (endTime - startTime);
-		    totalTime += lastTiming;
-		    timeCount++;
-		}
-	}
+   Timer()
+   {
+      totalTime = 0;
+      timeCount = 0;
+      startTime = 0;
+      endTime = 0;
+   }
 
-	// Get the average timing
-	double getTiming()
-	{ return (totalTime/(double)timeCount); }
+   //: Start a timing
+   void startTiming()
+   { startTime = wallclock(); }
 
-	double getLastTiming()
-	{ return lastTiming; }
-	
-	//: Returns the number of timings collected
-	long getTimeCount()
-	{ return timeCount; }
-	
-	void reset()
-	{
-	    totalTime = 0;
-	    timeCount = 0;
-	    startTime = 0;
-	    endTime = 0;
-	}   
-    
+   //: Stop current timing
+   //! PRE: Timing has been started
+   void stopTiming()
+   {
+      endTime = wallclock();
+      if (startTime == 0)   // If we haven't called start then ignore
+      { totalTime = 0; }
+      else
+      {
+         lastTiming = (endTime - startTime);
+         totalTime += lastTiming;
+         timeCount++;
+      }
+   }
+
+   // Get the average timing
+   double getTiming()
+   { return (totalTime/(double)timeCount); }
+
+   double getLastTiming()
+   { return lastTiming; }
+
+   //: Returns the number of timings collected
+   long getTimeCount()
+   { return timeCount; }
+
+   void reset()
+   {
+      totalTime = 0;
+      timeCount = 0;
+      startTime = 0;
+      endTime = 0;
+   }
+
 #ifdef WIN32
    double wallclock()
    {
-   	return ((double)GetTickCount())/1000.0f;
+      return ((double)GetTickCount())/1000.0f;
    }
 #else
    double wallclock()
    {
-       double v;
-       struct timeval tv;
-   
-       gettimeofday(&tv, 0);
-   
-       v = (double) tv.tv_sec + (double) tv.tv_usec / 1000000.0;
-       return v;
+      double v;
+      struct timeval tv;
+
+      gettimeofday(&tv, 0);
+
+      v = (double) tv.tv_sec + (double) tv.tv_usec / 1000000.0;
+      return v;
    }
 #endif
 
-private:					// Timing stuff
-	double  startTime;	    // The times
-	double  endTime;
-	double  lastTiming;		// The last timing recieved
-	double  totalTime;	   	// The total time
-	long    timeCount;	   	// The count on the number of times	
+private:               // Timing stuff
+   double  startTime;       // The times
+   double  endTime;
+   double  lastTiming;      // The last timing recieved
+   double  totalTime;       // The total time
+   long    timeCount;       // The count on the number of times
 };  // class Timer
 
-};	// namespace vpr
-#endif 
+};   // namespace vpr
+#endif
