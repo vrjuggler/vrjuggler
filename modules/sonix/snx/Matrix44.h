4ea229355484327af7b101a49a60f3e2537c01ca
Kevin Meinert <subatomic@users.sourceforge.net>
2001-08-28 20:46:12 +0000

    Added copyright header to AJ
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@5959 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/sonix/snx/Matrix44.h
+++ modules/sonix/snx/Matrix44.h
@@ -1,4 +1,48 @@
 
+/****************** <AJ heading BEGIN do not edit this line> *****************
+ *
+ * Audio Juggler
+ *
+ * Original Authors:
+ *   Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <AJ heading END do not edit this line> ******************/
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000, 2001 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+
+
+
+
 #ifndef AUDIOJUGGLER_32BIT_4X4_MATRIX
 #define AUDIOJUGGLER_32BIT_4X4_MATRIX
 
