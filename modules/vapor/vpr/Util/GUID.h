4350eaf980e58c51a21b8edfd28b3a234f1c83d3
Patrick Hartling <patrick.hartling@gmail.com>
2001-09-01 22:39:47 +0000

    New feature:	vpr::GUID objects
    Added an OO wrapper around UUIDs/GUIDs called vpr::GUID.  These are created
    using vpr::GUIDFactory.  The MD5 and UUID toolkits are being compiled into
    VPR to enable the use of these objects.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6014 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/Util/GUID.h
@@ -0,0 +1,92 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000, 2001 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VPR_GUID_H_
+#define _VPR_GUID_H_
+
+#include <vpr/vprConfig.h>
+
+#include <string>
+#include <uuid/sysdep.h>
+#include <uuid/uuid.h>
+
+
+namespace vpr
+{
+
+class GUIDFactory;
+
+class VPR_CLASS_API GUID
+{
+public:
+   ~GUID (void) {;}
+
+   bool operator==(const GUID& guid_obj) const;
+
+private:
+   /**
+    * Generates a random GUID.
+    */
+   GUID(void);
+
+   /**
+    * Generates a GUID for the given namespace.
+    */
+//   GUID(const std::string& name);
+
+   /**
+    * Generates a GUID based on the given name that is part of the namespace
+    * identified by the given namespace GUID.
+    */
+   GUID(const GUID& ns_guid, const std::string& name);
+
+   GUID (const GUID& obj) {;}
+   void operator= (const GUID& obj) {;}
+
+   uuid_t m_guid;
+
+   friend class vpr::GUIDFactory;
+};
+
+} // End of vpr namespace
+
+
+#endif /* _VPR_GUID_H_ */
