78b3c7268a04c05f42078a0a6ef3b2e030486646
Patrick Hartling <patrick.hartling@gmail.com>
2001-09-06 15:45:03 +0000

    New feature:	Wrapper functions for accessing version information.
    Added vpr::getVersionString(), vpr::getVersionNumber(), and
    vpr::getSubsystem() for querying VPR version information at runtime.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6073 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/vapor/vpr/Util/Version.cpp
@@ -0,0 +1,76 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000, 2001 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <vpr/vprConfig.h>
+
+#include <vpr/vprParam.h>
+#include <vpr/Util/Version.h>
+
+
+namespace vpr
+{
+
+VPR_IMPLEMENT(std::string) getVersionString ()
+{
+   return std::string(VPR_VERSION);
+}
+
+VPR_IMPLEMENT(vpr::Uint32) getVersionNumber ()
+{
+   return __VPR_version;
+}
+
+VPR_IMPLEMENT(std::string) getSubsystem ()
+{
+   std::string subsystem_string;
+
+#if defined(VPR_USE_NSPR)
+   subsystem_string = std::string("NSPR");)
+#elif defined(VPR_USE_PTHREADS)
+   subsystem_string = std::string("POSIX");
+#elif defined(VPR_USE_IRIX_SPROC)
+   subsystem_string = std::string("IRIX_SPROC");
+#endif
+
+   return subsystem_string;
+}
+
+}
