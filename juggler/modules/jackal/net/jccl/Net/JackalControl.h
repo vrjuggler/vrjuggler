f5c58d7adf236cac346b02d53e6227ee7203c66e
Christopher Just <coronax@users.sourceforge.net>
2001-09-13 17:09:26 +0000

    Added copyright message. Also, this commit starts to abandon trying to keep class declarations at the same indent level as namespace declarations.  Emacs doesn't like the indentation error, and i generally defer to its wisdom in these matters (plus I have better things to do this week than re-learn lisp).
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6135 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/net/jccl/Net/JackalControl.h
+++ juggler/modules/jackal/net/jccl/Net/JackalControl.h
@@ -1,27 +1,59 @@
-
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
 
 
 #ifndef _JCCL_JACKAL_CONTROL_H_
 #define _JCCL_JACKAL_CONTROL_H_
 
+// JackalControls are units that provide specific functionality to Jackal.
+// Frex, dynamic config tools, perf monitoring tools, etc.
 namespace jccl {
 
-class Connect;
-class JackalServer;
-
-class JackalControl {
-
-public:
-
-    virtual void setJackalServer (JackalServer* js) {;}
-
-    //: Notification from JackalServer for a new connection.
-    virtual void addConnect (Connect* c) {;}
-
-    //: Notification from JackalServer for a closed connection.
-    virtual void removeConnect (Connect* c) {;}
-
-};
+    class Connect;
+    class JackalServer;
+    
+    class JackalControl {
+        
+    public:
+        
+        virtual void setJackalServer (JackalServer* js) {;}
+        
+        //: Notification from JackalServer for a new connection.
+        virtual void addConnect (Connect* c) {;}
+        
+        //: Notification from JackalServer for a closed connection.
+        virtual void removeConnect (Connect* c) {;}
+        
+    };
 
 };
 
