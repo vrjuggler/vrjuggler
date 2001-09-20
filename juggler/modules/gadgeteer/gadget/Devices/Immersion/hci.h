54ca090aff9e1cba64167059a37167475646847a
Patrick Hartling <patrick.hartling@gmail.com>
2001-09-20 01:56:13 +0000

    Backed out the last revision.  These file should not have the VR Juggler copyright on them.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6173 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/gadget/Devices/Immersion/hci.h
+++ juggler/modules/gadgeteer/gadget/Devices/Immersion/hci.h
@@ -1,34 +1,16 @@
-/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+/***************************************************
+ * - - - - - - -   IMMERSION CORP.   - - - - - - - *
+ *                                                 *
+ *       Platform-independent software series      *
+ *                Copyright (c) 1993               *
+ ***************************************************
+ * HCI.H   |   Rev 2.0   |   March 1994
  *
- * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
- *
- * Original Authors:
- *   Allen Bierbaum, Christopher Just,
- *   Patrick Hartling, Kevin Meinert,
- *   Carolina Cruz-Neira, Albert Baker
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- * -----------------------------------------------------------------
- * File:          $RCSfile$
- * Date modified: $Date$
- * Version:       $Revision$
- * -----------------------------------------------------------------
- *
- *************** <auto-copyright.pl END do not edit this line> ***************/
+ * Immersion Corp. Developer's Programming Library Module
+ * 	Definitions and prototypes for direct communication with
+ *        the Immersion HCI
+ *      Requires HCI firmware version 2.0 or later
+ */
 
 #ifndef _HCI_H
 #define _HCI_H
--- juggler/modules/gadgeteer/gadget/Devices/Immersion/ifbox.h
+++ juggler/modules/gadgeteer/gadget/Devices/Immersion/ifbox.h
@@ -1,34 +1,16 @@
-/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+/***************************************************
+ * - - - - - - -   IMMERSION CORP.   - - - - - - - *
+ *                                                 *
+ *       Platform-independent software series      *
+ *                Copyright (c) 1993               *
+ ***************************************************
+ * IBOX.H   |   Rev 2.0   |   March 1994
  *
- * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
- *
- * Original Authors:
- *   Allen Bierbaum, Christopher Just,
- *   Patrick Hartling, Kevin Meinert,
- *   Carolina Cruz-Neira, Albert Baker
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- * -----------------------------------------------------------------
- * File:          $RCSfile$
- * Date modified: $Date$
- * Version:       $Revision$
- * -----------------------------------------------------------------
- *
- *************** <auto-copyright.pl END do not edit this line> ***************/
+ * Immersion Corp. Developer's Programming Library Module
+ * 	Definitions for abstraction of an Immersion Interface Box
+ *        This abstract module is built upon the lower-level HCI module
+ *      Requires HCI firmware version 2.0 or later
+ */
 
 #ifndef _IBOX_H
 #define _IBOX_H
