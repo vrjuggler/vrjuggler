d601dda9b98e202cd8efc6403301371afc40e589
Christopher Just <coronax@users.sourceforge.net>
2001-09-21 20:02:23 +0000

    fixed a variable name error.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6233 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/vjcontrol/vjcontrol.sh
+++ juggler/modules/jackal/vjcontrol/vjcontrol.sh
@@ -48,7 +48,7 @@ if test ! -x "$JDK_HOME/bin/java" ; then
 # Otherwise, start up VjControl and exit with status 0.
 else
     if test "x$VJ_SHARE_DIR" = "x" ; then
-        VJ_SHARE_DIR=$VJ_BASE_DIR/#VJ_SHARE_DIR#
+        VJ_SHARE_DIR=$VJ_BASE_DIR/#JCCL_SHARE_DIR#
     fi
     if test "x$JCCL_SHARE_DIR" = "x" ; then
         JCCL_SHARE_DIR=$JCCL_BASE_DIR/#JCCL_SHARE_DIR#
