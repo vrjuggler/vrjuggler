a9d9621ee25c0cc72295f0d5d2e4089e5ba7c885
Christopher Just <coronax@users.sourceforge.net>
2001-10-05 19:51:25 +0000

    Formatting cleanup.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6273 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/perfmonitor/org/vrjuggler/jccl/perfmonitor/DataLine.java
+++ modules/jackal/perfmonitor/org/vrjuggler/jccl/perfmonitor/DataLine.java
@@ -37,16 +37,13 @@ package VjComponents.PerfMonitor;
 public class DataLine {
     public double vals[];
     public double diffs[]; // diffs[i] is val[i]-val[i-1]
-    //public boolean valid[]; /* used to mark missing datapoints */
     public int  numlost; // # points missing after last point 
     public double linetotal; // time from end of last dataline toend
                             // of this one...
-// i'll assume for now there's a nan value that i can use.
 
     public DataLine(int num) {
         vals = new double[num];
         diffs = new double[num];
-        //valid = new boolean[num];
 	numlost = 0;
     }
 
