ef53e8cb8ad9ffa7604ae0458d23755afb8c2fc3
Christopher Just <coronax@users.sourceforge.net>
2001-10-18 19:48:57 +0000

    Fixed some persisting references to VJ stuff (mainly VJ_PERF* & VJ_OS* flags).
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6377 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/acconfig.h
+++ modules/jackal/acconfig.h
@@ -30,8 +30,8 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
-#ifndef _VJ_DEFINES_H_
-#define _VJ_DEFINES_H_
+#ifndef _JCCL_DEFINES_H_
+#define _JCCL_DEFINES_H_
 
 /*
  * --------------------------------------------------------------------------
@@ -42,126 +42,81 @@
 
 @TOP@
 
-/* Define all of the following to use POSIX threads. */
-#undef VJ_USE_PTHREADS
-#undef VJ_THREAD_SCOPE
-#undef VJ_POSIX_C_SOURCE
-#undef _THREAD_SAFE
-#undef RWSTD_MULTI_THREAD
-#undef RW_MULTI_THREAD
-#undef _REENTRANT
-
-/* Define if using POSIX threads Draft 4. */
-#undef _PTHREADS_DRAFT_4
-
-/* Define if using POSIX threads Draft 10 (the "final" draft). */
-#undef _PTHREADS_DRAFT_10
-
-/* Define to use POSIX semaphores. */
-#undef VJ_POSIX_SEMAPHORES
-
-/* Define to use the IRIX sproc(2) threading model instead of POSIX threads. */
-#undef VJ_IRIX_SPROC
-
-/* Define to use the NSPR subsystem. */
-#undef VJ_USE_NSPR
-
-/* Define to use the OpenGL API. */
-#undef VJ_API_OPENGL
-
-/* Define to use the Performer API. */
-#undef VJ_API_PERFORMER
-
-/* Define to use the OpenScenegraph API. */
-#undef VJ_API_OPENSCENEGRAPH
-
 /* Define all of the following if building on IRIX. */
-#undef VJ_OS_IRIX
+#undef JCCL_OS_IRIX
 #undef _BSD_TYPES
 
 /* Define to be either IRIX5 or IRIX6 depending on your system. */
 #undef IRIXREL
 
 /* Define all of the following if building on HP-UX. */
-#undef VJ_OS_HPUX
-#undef VJ_HPUX_VERSION
+#undef JCCL_OS_HPUX
+#undef JCCL_HPUX_VERSION
 #undef _INCLUDE_TERMIO
 #undef _CMA_NOWRAPPERS_
 
 /* Define if building on Digital UNIX. */
-#undef VJ_OS_DUX
+#undef JCCL_OS_DUX
 
 /* Define if building on FreeBSD. */
-#undef VJ_OS_FreeBSD
+#undef JCCL_OS_FreeBSD
 
 /* Define if building on Linux. */
-#undef VJ_OS_Linux
+#undef JCCL_OS_Linux
 
 /* Define if building on Solaris. */
-#undef VJ_OS_Solaris
+#undef JCCL_OS_Solaris
 
 /* Define if building on AIX. */
-#undef VJ_OS_AIX
+#undef JCCL_OS_AIX
 
 /* Define if building on a Win32 platform. */
-#undef VJ_OS_Win32
+#undef JCCL_OS_Win32
 #undef WIN32
 #undef _MBCS
 
 /* Define if building on Darwin (aka, Mac OS X). */
-#undef VJ_OS_Darwin
+#undef JCCL_OS_Darwin
 
 /* Define if building on a platform not specified above. */
-#undef VJ_OS_GENERIC
+#undef JCCL_OS_GENERIC
 
-/* Define to match the output of running 'uname -r. */
-#undef VJ_OS_RELEASE
-
-/*
- * Define to match the numeric version number of the target operating system.
- * For example, on HP-UX 10.20:
- *     #define VJ_OS_RELEASE_MAJOR 10
- *     #define VJ_OS_RELEASE_MINOR 20
- */
-#undef VJ_OS_RELEASE_MAJOR
-#undef VJ_OS_RELEASE_MINOR
-
-/* Define to unique values used for defining VJ_PERFORMANCE. */
-#undef VJ_PERF_NONE
-#undef VJ_PERF_SGI
-#undef VJ_PERF_POSIX
+/* Define to unique values used for defining JCCL_PERFORMANCE. */
+#undef JCCL_PERF_NONE
+#undef JCCL_PERF_SGI
+#undef JCCL_PERF_POSIX
 
 /* Define to one of the above values to enable performance testing. */
-#undef VJ_PERFORMANCE
+#undef JCCL_PERFORMANCE
 
-/* Definte if DTK is installed. */
-#undef VJ_HAVE_DTK
+// /* Definte if DTK is installed. */
+// #undef JCCL_HAVE_DTK
 
 /* Define to `int' if the socklen_t type is not available.  */
 #undef socklen_t
 
-/*
- * Define to `unsigned int' if the u_int and/or u_int32_t types are not
- * available.  On Windows when using Visual C++, define to UINT.
- */
-#undef u_int
-#undef uint32_t
+// /*
+//  * Define to `unsigned int' if the u_int and/or u_int32_t types are not
+//  * available.  On Windows when using Visual C++, define to UINT.
+//  */
+// #undef u_int
+// #undef uint32_t
 
-/*
- * Define to the sizeof a memory address if the type caddr_t is not available.
- * char* is most likely going to be a safe substitute.
- */
-#undef caddr_t
+// /*
+//  * Define to the sizeof a memory address if the type caddr_t is not available.
+//  * char* is most likely going to be a safe substitute.
+//  */
+// #undef caddr_t
 
-/* 
- * On Windows when using Visual C++, define to __int32 and __int64
- * respectively if these two are not available.
- */
-#undef int32_t
-#undef int64_t
+// /* 
+//  * On Windows when using Visual C++, define to __int32 and __int64
+//  * respectively if these two are not available.
+//  */
+// #undef int32_t
+// #undef int64_t
 
-/* Define if you have the sched_yield function.  */
-#undef HAVE_SCHED_YIELD
+// /* Define if you have the sched_yield function.  */
+// #undef HAVE_SCHED_YIELD
 
 /* Define if you have the sinf function.  */
 #undef HAVE_SINF
@@ -186,4 +141,4 @@
 
 @BOTTOM@
 
-#endif	/* _VJ_DEFINES_H_ */
+#endif	/* _JCCL_DEFINES_H_ */
--- modules/jackal/jccl/Plugins/ConfigManager/XMLConfigCommunicator.cpp
+++ modules/jackal/jccl/Plugins/ConfigManager/XMLConfigCommunicator.cpp
@@ -43,7 +43,7 @@
 // a totally cheezy kludge to avoid asserts becasue of some disagreement
 // between xerces & jackal/juggler/etc.  Doing this causes the loader code
 // to leak memory like a sieve.
-#ifdef VJ_OS_Win32
+#ifdef JCCL_OS_Win32
 
 #define delete[] //
 
