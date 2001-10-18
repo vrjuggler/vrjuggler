fd8157cf7e6d993ec4c6fa75a392e8246fd9d199
Kevin Meinert <subatomic@users.sourceforge.net>
2001-10-18 22:02:37 +0000

    bah.  test before commit, test before commit.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6389 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/sonix/snx/Matrix44.h
+++ modules/sonix/snx/Matrix44.h
@@ -159,10 +159,10 @@ private:
 };
 inline std::ostream& operator<<( std::ostream& out, const Matrix44& mat )
 {
-   out<<mat(0,0)<<" "<<mat(0,0)<<" "<<mat(0,0)<<" "<<mat(0,0)<<"\n"
+   out<<mat(0,0)<<" "<<mat(1,0)<<" "<<mat(2,0)<<" "<<mat(3,0)<<"\n"
       <<mat(0,1)<<" "<<mat(1,1)<<" "<<mat(2,1)<<" "<<mat(3,1)<<"\n"
-      <<mat(0,1)<<" "<<mat(1,1)<<" "<<mat(2,1)<<" "<<mat(3,1)<<"\n"
-      <<mat(0,1)<<" "<<mat(1,1)<<" "<<mat(2,1)<<" "<<mat(3,1)<<"\n"<<std::flush;
+      <<mat(0,2)<<" "<<mat(1,2)<<" "<<mat(2,2)<<" "<<mat(3,2)<<"\n"
+      <<mat(0,3)<<" "<<mat(1,3)<<" "<<mat(2,3)<<" "<<mat(3,3)<<"\n"<<std::flush;
    return out;
 }
 
