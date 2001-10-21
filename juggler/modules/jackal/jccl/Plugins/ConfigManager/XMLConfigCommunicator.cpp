a703d5fb129c8fe5dcc4a84d73e4e02a2a615c16
Christopher Just <coronax@users.sourceforge.net>
2001-10-21 17:23:16 +0000

    Replaced previous win32 kludge with what should be a more vc++ friendly win32 kludge.  some days you just can't win...
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6422 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/jccl/Plugins/ConfigManager/XMLConfigCommunicator.cpp
+++ juggler/modules/jackal/jccl/Plugins/ConfigManager/XMLConfigCommunicator.cpp
@@ -43,11 +43,11 @@
 // a totally cheezy kludge to avoid asserts becasue of some disagreement
 // between xerces & jackal/juggler/etc.  Doing this causes the loader code
 // to leak memory like a sieve.
-#ifdef VPR_OS_Win32
+//  #ifdef VPR_OS_Win32
 
-#define delete[] //
+//  #define delete[] //
 
-#endif
+//  #endif
 
 
 
