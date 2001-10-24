7eff09e3a539d05680c63f5de5788e1130a90153
Kevin Meinert <subatomic@users.sourceforge.net>
2001-10-24 00:21:30 +0000

    renamed the aj header pl files to snx.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6466 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/scripts/auto-copyright/config/snx-heading-replace.c.pl
@@ -0,0 +1,7 @@
+# This is what VR Juggler source code uses for the enclosed copyright block.
+
+$begintag = '/****************** <SNX heading BEGIN do not edit this line> *****************';
+$endtag = ' ****************** <SNX heading END do not edit this line> ******************/';
+
+$newbegintag = "$begintag";
+$newendtag = "$endtag";
