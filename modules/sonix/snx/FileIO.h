33d2d3a450e9e4d6974f0708eeec9e0db9823a83
Kevin Meinert <subatomic@users.sourceforge.net>
2001-11-05 14:44:45 +0000

    formatting change
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6563 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/sonix/snx/FileIO.h
+++ modules/sonix/snx/FileIO.h
@@ -84,24 +84,24 @@
 
 namespace snxFileIO
 {
-	//: true - 
-	inline bool fileExists( const char* const name )
-	{
+   //: true - 
+   inline bool fileExists( const char* const name )
+   {
       if (name == NULL) return false;
       
-		FILE* file = ::fopen( name, "r" );
-		if (file == NULL)
-		{
-			return false;
-		}
-		
-		else
-		{
-			::fclose( file );
-			return true;
-		}
-	}
-	
+      FILE* file = ::fopen( name, "r" );
+      if (file == NULL)
+      {
+         return false;
+      }
+      
+      else
+      {
+         ::fclose( file );
+         return true;
+      }
+   }
+   
    inline int fileSize( const char* const filename )
    {
       if (filename == NULL) return 0;
@@ -110,7 +110,7 @@ namespace snxFileIO
          return 0;
 
       FILE* fh = fopen( filename, "rb" );
-	   
+      
       const int one_kilobyte = 1024;
       const int arbitraliy_chosen_size = one_kilobyte * 1000;
       
@@ -123,7 +123,7 @@ namespace snxFileIO
          file_length = fread( &data, 1, arbitraliy_chosen_size, fh );
          total_size += file_length;
       }
-	   
+      
       fclose( fh );
 
       return total_size;
@@ -140,7 +140,7 @@ namespace snxFileIO
       data.resize( size );
       
       FILE* fh = fopen( filename, "rb" );
-	   int file_length = fread( &data[0], 1, data.size(), fh );
+      int file_length = fread( &data[0], 1, data.size(), fh );
       assert( file_length == data.size() );
       fclose( fh );
    }
@@ -151,89 +151,89 @@ namespace snxFileIO
       LITTLE, BIG
    };
 
-	//: Read
-	// this function is designed to work with little endian files, such as TGA and BMP.
+   //: Read
+   // this function is designed to work with little endian files, such as TGA and BMP.
    // this will return data that is correctly translated for your system architecture.
    // i.e. reading a little endian BMP on a big endian mips system, 
    //      returns (translated) bigendian data
-	// i.e. reading a little endian TGA on a little endian intel system, 
+   // i.e. reading a little endian TGA on a little endian intel system, 
    //      returns (untranslated) littleendian data
-	template<class typeT>
-	inline static int ReadData( Endianness fileByteOrdering, FILE* fp, typeT& data )
-	{
-		int size = ::fread( &data, sizeof(typeT), 1, fp );
+   template<class typeT>
+   inline static int ReadData( Endianness fileByteOrdering, FILE* fp, typeT& data )
+   {
+      int size = ::fread( &data, sizeof(typeT), 1, fp );
 
-		// if we're not on a little endian machine (intel is little endian) then reverse the bytes.
-		if (fileByteOrdering == snxFileIO::LITTLE && snxEndian::isBig() ||
+      // if we're not on a little endian machine (intel is little endian) then reverse the bytes.
+      if (fileByteOrdering == snxFileIO::LITTLE && snxEndian::isBig() ||
          fileByteOrdering == snxFileIO::BIG && snxEndian::isLittle())
       {
          snxEndian::byteReverse( data );
-		}
+      }
 
-		return size;
-	}
+      return size;
+   }
 
-	//: Write
-	// this function is designed to work with little endian files, such as TGA and BMP.
-	template<class typeT>
-	inline static int WriteData( Endianness fileByteOrdering, FILE* fp, const typeT& data )
-	{
-		register typeT tempData = data;
+   //: Write
+   // this function is designed to work with little endian files, such as TGA and BMP.
+   template<class typeT>
+   inline static int WriteData( Endianness fileByteOrdering, FILE* fp, const typeT& data )
+   {
+      register typeT tempData = data;
 
-		// if we're not on a little endian machine (i.e. intel is little endian, mips is big) 
+      // if we're not on a little endian machine (i.e. intel is little endian, mips is big) 
       // then reverse the bytes.
-		if (fileByteOrdering == LITTLE && snxEndian::isBig() ||
+      if (fileByteOrdering == LITTLE && snxEndian::isBig() ||
          fileByteOrdering == BIG && snxEndian::isLittle())
       {
-			snxEndian::byteReverse( tempData );
-		}
-			
-		int size = ::fwrite( &tempData, 1, sizeof(typeT), fp );
+         snxEndian::byteReverse( tempData );
+      }
+         
+      int size = ::fwrite( &tempData, 1, sizeof(typeT), fp );
 
-		return size;
-	}
+      return size;
+   }
 
 
 
-	// Read one byte of data from the file stream
-	inline static int ReadByte( FILE *fp, unsigned char& value )
-	{
-		return ::fread( &value, 1, sizeof(unsigned char), fp );
-	}
+   // Read one byte of data from the file stream
+   inline static int ReadByte( FILE *fp, unsigned char& value )
+   {
+      return ::fread( &value, 1, sizeof(unsigned char), fp );
+   }
 
-	// Read one short of data from the file stream
-	// NOTE: these enhance readability and type checking, try to use them over the generic ReadData function
-	inline static int ReadShort( Endianness fileByteOrdering, FILE* fp, unsigned short& value )
-	{
-		return ReadData( fileByteOrdering, fp, value );
-	}
+   // Read one short of data from the file stream
+   // NOTE: these enhance readability and type checking, try to use them over the generic ReadData function
+   inline static int ReadShort( Endianness fileByteOrdering, FILE* fp, unsigned short& value )
+   {
+      return ReadData( fileByteOrdering, fp, value );
+   }
 
-	// Read one long of data from the file stream
-	// NOTE: these enhance readability and type checking, try to use them over the generic ReadData function
-	inline static int ReadLong( Endianness fileByteOrdering, FILE *fp, unsigned long& value )
-	{
-		return ReadData( fileByteOrdering, fp, value );
-	}
+   // Read one long of data from the file stream
+   // NOTE: these enhance readability and type checking, try to use them over the generic ReadData function
+   inline static int ReadLong( Endianness fileByteOrdering, FILE *fp, unsigned long& value )
+   {
+      return ReadData( fileByteOrdering, fp, value );
+   }
 
-	// Write one byte of data to the file stream
-	inline static int WriteByte( FILE *fp, const unsigned char& value )
-	{
-		return ::fwrite( &value, 1, sizeof(unsigned char), fp );
-	}
+   // Write one byte of data to the file stream
+   inline static int WriteByte( FILE *fp, const unsigned char& value )
+   {
+      return ::fwrite( &value, 1, sizeof(unsigned char), fp );
+   }
 
-	// Write one short of data to the file stream
-	// NOTE: these enhance readability and type checking, try to use them over the generic WriteData function
-	inline static int WriteShort( Endianness fileByteOrdering, FILE* fp, const unsigned short& value )
-	{
-		return WriteData( fileByteOrdering, fp, value );
-	}
+   // Write one short of data to the file stream
+   // NOTE: these enhance readability and type checking, try to use them over the generic WriteData function
+   inline static int WriteShort( Endianness fileByteOrdering, FILE* fp, const unsigned short& value )
+   {
+      return WriteData( fileByteOrdering, fp, value );
+   }
 
-	// Write one long of data to the file stream
-	// NOTE: these enhance readability and type checking, try to use them over the generic WriteData function
-	inline static int WriteLong( Endianness fileByteOrdering, FILE *fp, const unsigned long& value )
-	{
-		return WriteData( fileByteOrdering, fp, value );
-	}
+   // Write one long of data to the file stream
+   // NOTE: these enhance readability and type checking, try to use them over the generic WriteData function
+   inline static int WriteLong( Endianness fileByteOrdering, FILE *fp, const unsigned long& value )
+   {
+      return WriteData( fileByteOrdering, fp, value );
+   }
 
    inline static void getLine( ifstream& f, std::string& text  )
    {
@@ -249,11 +249,11 @@ namespace snxFileIO
 
    inline static void getAll( ifstream& f, std::string& buffer )
    {
-	   //cout<<"Reading:["<<flush;
+      //cout<<"Reading:["<<flush;
       while ((f.eof() == false) && (f.fail() == 0) )
       //while (f.gcount() >= 2048)
       {
-	      //cout<<"."<<flush;
+         //cout<<"."<<flush;
          char buf[2049];
          f.read( buf, 2048 );
          buf[2048] = '\0';
@@ -263,7 +263,7 @@ namespace snxFileIO
          }
          buffer += buf;
       }
-	   //cout<<"]\n"<<flush;
+      //cout<<"]\n"<<flush;
       //cout << "Gcount == " << f.gcount() << "\n"<<flush;
    }
    
