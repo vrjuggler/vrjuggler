187e965539ac012e99216931a5fed11f77449ae6
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-08 23:23:02 +0000

    Added copyright and improved the readability of init() by moving the code for populating the service and viewer registries into separate methods.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6590 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/java/org/vrjuggler/tweek/TweekCore.java
+++ modules/tweek/java/org/vrjuggler/tweek/TweekCore.java
@@ -1,3 +1,39 @@
+/***************** <Tweek heading BEGIN do not edit this line> ****************
+ * Tweek
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ ***************** <Tweek heading END do not edit this line> *****************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000, 2001 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
 package org.vrjuggler.tweek;
 
 import java.io.File;
@@ -5,6 +41,7 @@ import java.util.Vector;
 import org.vrjuggler.tweek.beans.*;
 import org.vrjuggler.tweek.beans.loader.BeanInstantiationException;
 import org.vrjuggler.tweek.services.*;
+import org.vrjuggler.tweek.net.corba.*;
 
 
 /**
@@ -33,25 +70,8 @@ public class TweekCore
       findBeans(System.getProperty("TWEEK_BASE_DIR") + File.separator +
                 "bin" + File.separator + "beans");
 
-      Vector services = BeanCollectionBuilder.instance().getServices();
-
-      for ( int i = 0; i < services.size(); i++ )
-      {
-         ServiceBean service = (ServiceBean) services.elementAt(i);
-
-         try
-         {
-            service.instantiate();
-            ServiceRegistry.instance().registerService(service.getServiceName(),
-                                                       service.getService());
-         }
-         catch (BeanInstantiationException e)
-         {
-            System.err.println("WARNING: Failed to instantiate service '" +
-                               service.getServiceName() + "': " +
-                               e.getMessage());
-         }
-      }
+      // Load the service Beans into the services registry.
+      buildServiceRegistry();
 
       // Register the command-line arguments with the Environment Service (if
       // it is available).
@@ -70,43 +90,14 @@ public class TweekCore
          System.err.println("WARNING: Failed to register command-line arguments");
       }
 
-      GlobalPreferencesService prefs = GlobalPreferencesService.instance();
-
-      Vector viewers           = BeanCollectionBuilder.instance().getViewers();
-      BeanTreeModel tree_model = BeanCollectionBuilder.instance().getPanelTree();
-
-      for ( int i = 0; i < viewers.size(); i++ )
-      {
-         ViewerBean viewer = (ViewerBean) viewers.elementAt(i);
-
-         try
-         {
-            // The process of loading a new BeanViewer object is a little
-            // involved.  First, we need an instance (duh).  After that, we
-            // need to pass it the tree model, initialize its GUI, and add it
-            // to the available choices in the global preferences.  Once all
-            // that is done, the viewer can be registered for use by other
-            // code.
-            viewer.instantiate();
-            BeanModelViewer bv = (BeanModelViewer) viewer.getViewer();
-            bv.initDataModel(tree_model);
-            bv.initGUI();
-            prefs.addBeanViewer(viewer.getViewerName());
-            ViewerRegistry.instance().registerViewer(viewer.getViewerName(),
-                                                     viewer.getViewer());
-         }
-         catch (BeanInstantiationException e)
-         {
-            System.err.println("WARNING: Failed to instantiate Bean viewer '" +
-                               viewer.getViewerName() + "': " + e.getMessage());
-         }
-      }
+      // Load the viewer Beans into the viewers registry.
+      buildViewerRegistry();
 
       // Load the user's global preferences file.
-      prefs.load();
+      GlobalPreferencesService.instance().load();
 
       m_gui = new TweekFrame(org.vrjuggler.tweek.net.PlexusNodeInterfaceFactory.create(),
-                             prefs);
+                             GlobalPreferencesService.instance());
 
       // Now we need to register the TweekFrame instance as a listener for
       // BeanFocusChangeEvents.
@@ -142,6 +133,64 @@ public class TweekCore
       catch (org.vrjuggler.tweek.beans.BeanPathException e)
       {
          System.out.println("WARNING: Invalid path " + path);
+      }
+   }
+
+   protected void buildServiceRegistry ()
+   {
+      Vector services = BeanCollectionBuilder.instance().getServices();
+
+      for ( int i = 0; i < services.size(); i++ )
+      {
+         ServiceBean service = (ServiceBean) services.elementAt(i);
+
+         try
+         {
+            service.instantiate();
+            ServiceRegistry.instance().registerService(service.getServiceName(),
+                                                       service.getService());
+         }
+         catch (BeanInstantiationException e)
+         {
+            System.err.println("WARNING: Failed to instantiate service '" +
+                               service.getServiceName() + "': " +
+                               e.getMessage());
+         }
+      }
+   }
+
+   protected void buildViewerRegistry ()
+   {
+      Vector viewers           = BeanCollectionBuilder.instance().getViewers();
+      BeanTreeModel tree_model = BeanCollectionBuilder.instance().getPanelTree();
+
+      GlobalPreferencesService prefs = GlobalPreferencesService.instance();
+
+      for ( int i = 0; i < viewers.size(); i++ )
+      {
+         ViewerBean viewer = (ViewerBean) viewers.elementAt(i);
+
+         try
+         {
+            // The process of loading a new BeanViewer object is a little
+            // involved.  First, we need an instance (duh).  After that, we
+            // need to pass it the tree model, initialize its GUI, and add it
+            // to the available choices in the global preferences.  Once all
+            // that is done, the viewer can be registered for use by other
+            // code.
+            viewer.instantiate();
+            BeanModelViewer bv = (BeanModelViewer) viewer.getViewer();
+            bv.initDataModel(tree_model);
+            bv.initGUI();
+            prefs.addBeanViewer(viewer.getViewerName());
+            ViewerRegistry.instance().registerViewer(viewer.getViewerName(),
+                                                     viewer.getViewer());
+         }
+         catch (BeanInstantiationException e)
+         {
+            System.err.println("WARNING: Failed to instantiate Bean viewer '" +
+                               viewer.getViewerName() + "': " + e.getMessage());
+         }
       }
    }
 
