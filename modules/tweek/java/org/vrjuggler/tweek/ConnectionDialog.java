226c7633c6ab074795af5eb85dbf3ae657bbaaa7
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-09 02:23:57 +0000

    Cleaned things up a little bit after my hurried creation of this file. Addedc copyright.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6596 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/java/org/vrjuggler/tweek/ConnectionDialog.java
+++ modules/tweek/java/org/vrjuggler/tweek/ConnectionDialog.java
@@ -1,3 +1,39 @@
+/***************** <Tweek heading BEGIN do not edit this line> ****************
+ * Tweek
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ ***************** <Tweek heading END do not edit this line> *****************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000, 2001 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
 package org.vrjuggler.tweek;
 
 import java.awt.*;
@@ -34,47 +70,122 @@ public class ConnectionDialog extends JDialog
       this.setVisible(true);
    }
 
-   private void jbInit() throws Exception
+   public int getStatus ()
    {
-      titledBorder1 = new TitledBorder("");
-      m_main_panel.setLayout(m_main_layout);
-      m_orb_addr_label.setText("Address");
-      m_addr_panel.setLayout(m_addr_layout);
-      m_orb_addr_field.setMinimumSize(new Dimension(80, 17));
-      m_orb_addr_field.setPreferredSize(new Dimension(180, 17));
-      m_main_layout.setColumns(1);
-      m_main_layout.setRows(-1);
-      m_orb_panel.setLayout(m_orb_layout);
-      m_orb_port_label.setText("Port Number");
-      m_addr_panel.setBorder(BorderFactory.createLineBorder(Color.black));
-      m_orb_panel.setBorder(BorderFactory.createLineBorder(Color.black));
-      m_main_panel.setPreferredSize(new Dimension(240, 38));
-      m_connected_orbs_panel.setLayout(m_connected_orbs_layout);
-      m_connected_orbs.setToolTipText("Current connected ORBs");
-      m_orb_panel.add(m_orb_port_label, BorderLayout.WEST);
-      m_orb_panel.add(m_orb_port_field, BorderLayout.CENTER);
-      this.getContentPane().add(m_main_panel,  BorderLayout.CENTER);
-      m_addr_panel.add(m_orb_addr_label,  BorderLayout.WEST);
-      m_addr_panel.add(m_orb_addr_field, BorderLayout.CENTER);
-      m_main_panel.add(m_addr_panel, null);
-      m_main_panel.add(m_orb_panel, null);
-      m_main_panel.add(m_connected_orbs_panel, null);
-      m_connected_orbs_panel.add(m_connected_orbs, BorderLayout.CENTER);
+      return status;
    }
 
-   private JPanel m_main_panel = new JPanel();
-   private JPanel m_addr_panel = new JPanel();
-   private JLabel m_orb_addr_label = new JLabel();
-   private JTextField m_orb_addr_field = new JTextField();
-   private BorderLayout borderLayout1 = new BorderLayout();
-   private BorderLayout m_addr_layout = new BorderLayout();
-   private JPanel m_orb_panel = new JPanel();
+   public static final int OK_OPTION     = JOptionPane.OK_OPTION;
+   public static final int CANCEL_OPTION = JOptionPane.CANCEL_OPTION;
+   public static final int CLOSED_OPTION = JOptionPane.CLOSED_OPTION;
+
+   protected void processWindowEvent (WindowEvent e)
+   {
+      super.processWindowEvent(e);
+
+      if (e.getID() == WindowEvent.WINDOW_CLOSING)
+      {
+         status = CLOSED_OPTION;
+      }
+   }
+
+   private void jbInit() throws Exception
+   {
+      m_main_panel.setLayout(m_main_layout);
+      m_main_panel.setMinimumSize(new Dimension(375, 96));
+      m_main_panel.setPreferredSize(new Dimension(375, 96));
+      m_main_layout.setRows(-1);
+      m_main_layout.setColumns(1);
+
+      m_addr_label.setText("Address");
+      m_addr_field.setMinimumSize(new Dimension(80, 17));
+      m_addr_field.setPreferredSize(new Dimension(250, 17));
+      m_addr_panel.setLayout(m_addr_layout);
+      m_addr_panel.add(m_addr_label, null);
+      m_addr_panel.add(m_addr_field, null);
+
+      m_port_label.setText("Port Number");
+      m_port_field.setMinimumSize(new Dimension(80, 17));
+      m_port_field.setPreferredSize(new Dimension(80, 17));
+      m_port_panel.setLayout(m_port_layout);
+      m_port_panel.add(m_port_label, null);
+      m_port_panel.add(m_port_field, null);
+
+      m_connected_orbs.setToolTipText("Current connected ORBs");
+      m_connected_orbs_panel.setLayout(m_connected_orbs_layout);
+      m_connected_orbs_label.setText("Currently Connected ORBs");
+      m_connected_orbs_panel.add(m_connected_orbs_label, null);
+      m_connected_orbs_panel.add(m_connected_orbs, null);
+
+      m_ok_button.setText("OK");
+      m_ok_button.setMnemonic('O');
+      m_ok_button.addActionListener(new ActionListener()
+      {
+         public void actionPerformed (ActionEvent e)
+         {
+            okButtonAction(e);
+         }
+      });
+
+      m_cancel_button.setText("Cancel");
+      m_cancel_button.setMnemonic('C');
+      m_cancel_button.addActionListener(new ActionListener()
+      {
+         public void actionPerformed (ActionEvent e)
+         {
+            cancelButtonAction(e);
+         }
+      });
+
+      this.getContentPane().add(m_main_panel, BorderLayout.CENTER);
+
+      m_main_panel.add(m_addr_panel, null);
+      m_main_panel.add(m_port_panel, null);
+      m_main_panel.add(m_connected_orbs_panel, null);
+
+      this.getContentPane().add(m_button_panel, BorderLayout.SOUTH);
+      m_button_panel.add(m_ok_button, null);
+      m_button_panel.add(m_cancel_button, null);
+   }
+
+   private void okButtonAction (ActionEvent e)
+   {
+      status = OK_OPTION;
+      commit();
+      setVisible(false);
+   }
+
+   private void cancelButtonAction (ActionEvent e)
+   {
+      status = CANCEL_OPTION;
+      setVisible(false);
+   }
+
+   private void commit ()
+   {
+   }
+
+   private int status;
+
+   private JPanel     m_main_panel  = new JPanel();
    private GridLayout m_main_layout = new GridLayout();
-   private BorderLayout m_orb_layout = new BorderLayout();
-   private JLabel m_orb_port_label = new JLabel();
-   private JTextField m_orb_port_field = new JTextField();
-   private TitledBorder titledBorder1;
-   private JPanel m_connected_orbs_panel = new JPanel();
-   private BorderLayout m_connected_orbs_layout = new BorderLayout();
-   private JComboBox m_connected_orbs = new JComboBox();
+
+   private JPanel     m_addr_panel  = new JPanel();
+   private FlowLayout m_addr_layout = new FlowLayout();
+   private JLabel     m_addr_label  = new JLabel();
+   private JTextField m_addr_field  = new JTextField();
+
+   private JPanel     m_port_panel  = new JPanel();
+   private FlowLayout m_port_layout = new FlowLayout();
+   private JLabel     m_port_label  = new JLabel();
+   private JTextField m_port_field  = new JTextField();
+
+   private JPanel     m_connected_orbs_panel  = new JPanel();
+   private FlowLayout m_connected_orbs_layout = new FlowLayout();
+   private JLabel     m_connected_orbs_label  = new JLabel();
+   private JComboBox  m_connected_orbs        = new JComboBox();
+
+   private JPanel  m_button_panel  = new JPanel();
+   private JButton m_ok_button     = new JButton();
+   private JButton m_cancel_button = new JButton();
 }
