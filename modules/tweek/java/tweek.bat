26518e10900440aa2decc185f76e032337423b26
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-10 18:36:49 +0000

    Added scripts for running this thing.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6621 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/tweek/java/tweek.bat
@@ -0,0 +1,1 @@
+java -DTWEEK_BASE_DIR=%TWEEK_BASE_DIR% -Djava.security.policy=%TWEEK_BASE_DIR%/bin/java.security.policy.txt -cp %TWEEK_BASE_DIR%/bin/jdom.jar;%TWEEK_BASE_DIR%/bin/xerces.jar;%TWEEK_BASE_DIR%/bin/Tweek.jar;%TWEEK_BASE_DIR%/bin/TweekBeans.jar;%TWEEK_BASE_DIR%/bin/TweekComm.jar org.vrjuggler.tweek.Tweek %1 %2 %3 %4 %5 %6 %7 %8 %9
