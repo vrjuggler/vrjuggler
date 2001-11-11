de25b66ea15f8fd44c0ca5a070312df9a38d33b0
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-11 21:03:32 +0000

    Attempt to register a SubjectManagerImpl instance with CORBA.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6640 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/test/corba/main.cpp
+++ juggler/modules/tweek/test/corba/main.cpp
@@ -1,4 +1,5 @@
 #include <tweek/CORBA/CorbaManager.h>
+#include <tweek/CORBA/SubjectManagerImpl.h>
 #include <vpr/Thread/Thread.h>
 #include <vpr/Util/Debug.h>
 
@@ -11,6 +12,27 @@ int main (int argc, char* argv[])
    {
       if ( mgr.init(argc, argv).success() )
       {
+         tweek::SubjectManagerImpl* subj_mgr = new tweek::SubjectManagerImpl();
+         vpr::ReturnStatus status;
+
+         try
+         {
+            status = mgr.registerSubjectManager(subj_mgr);
+         }
+         catch (CORBA::Exception& ex)
+         {
+            vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+               << "Caught an unknown CORBA exception when trying to register!\n"
+               << vprDEBUG_FLUSH;
+         }
+
+         if ( ! status.success() )
+         {
+            vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+               << "Failed to register Subject Manager instance\n"
+               << vprDEBUG_FLUSH;
+         }
+
          // Loop forever so that we can act sort of like a server.
          while ( 1 )
          {
