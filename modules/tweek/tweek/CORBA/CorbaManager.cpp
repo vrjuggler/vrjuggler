94a18bdc46ab2858b3e3bf2da6846e48b9eda9b6
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-11 17:46:04 +0000

    Robustify based on the omniORB User Guide (more extensive exception catching, mostly).  I have added an isValid() method so that the user can determine if the ORB and POA references were initialized successfully.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6637 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/tweek/CORBA/CorbaManager.cpp
+++ modules/tweek/tweek/CORBA/CorbaManager.cpp
@@ -46,69 +46,136 @@
 namespace tweek
 {
 
-void CorbaManager::init (int argc, char** argv)
+vpr::ReturnStatus CorbaManager::init (int argc, char** argv)
 {
-   CORBA::Object_var obj;
-
-   m_orb = CORBA::ORB_init(argc, argv, "omniORB3");
-   obj   = m_orb->resolve_initial_references("RootPOA");
-   m_poa = PortableServer::POA::_narrow(obj);
+   vpr::ReturnStatus status;
 
    try
    {
-      CORBA::Object_var name_obj;
+      CORBA::Object_var obj;
 
-      name_obj       = m_orb->resolve_initial_references("NameService");
-      m_root_context = CosNaming::NamingContext::_narrow(name_obj);
+      // Initialize the ORB.
+      vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Initializing ORB\n"
+                                             << vprDEBUG_FLUSH;
+      m_orb = CORBA::ORB_init(argc, argv, "omniORB3");
 
-      if ( CORBA::is_nil(m_root_context) )
+      // Obtain a reference to the root POA.
+      vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Requesting Root POA\n"
+                                             << vprDEBUG_FLUSH;
+      obj   = m_orb->resolve_initial_references("RootPOA");
+      m_poa = PortableServer::POA::_narrow(obj);
+
+      try
       {
-         vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
-            << "Failed to narrow. CorbaManager::init\n" << vprDEBUG_FLUSH;
+         CORBA::Object_var name_obj;
+
+         vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Requesting Name Service\n"
+                                                << vprDEBUG_FLUSH;
+         name_obj       = m_orb->resolve_initial_references("NameService");
+         m_root_context = CosNaming::NamingContext::_narrow(name_obj);
+
+         if ( CORBA::is_nil(m_root_context) )
+         {
+            vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+               << "Failed to narrow. CorbaManager::init\n" << vprDEBUG_FLUSH;
+         }
       }
+      catch (CORBA::ORB::InvalidName& ex)
+      {
+         // This should not happen!
+         vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+            << "NameService name invalid in CorbaManager::init!\n"
+            << vprDEBUG_FLUSH;
+      }
+
+      vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Starting ORB thread\n"
+                                             << vprDEBUG_FLUSH;
+      vpr::ThreadMemberFunctor<CorbaManager>* corba_run =
+         new vpr::ThreadMemberFunctor<CorbaManager>(this, &CorbaManager::run);
+
+      m_my_thread = new vpr::Thread(corba_run);
    }
-   catch (CORBA::ORB::InvalidName& ex)
+   catch (CORBA::SystemException&)
    {
+      status.setCode(vpr::ReturnStatus::Fail);
       vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
-         << "Service required invalid.  CorbaManager::init\n" << vprDEBUG_FLUSH;
+         << "Caught CORBA::SystemException\n" << vprDEBUG_FLUSH;
+   }
+   catch (CORBA::Exception&)
+   {
+      status.setCode(vpr::ReturnStatus::Fail);
+      vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+         << "Caught CORBA::Exception.\n" << vprDEBUG_FLUSH;
+   }
+   catch (omniORB::fatalException& fe)
+   {
+      status.setCode(vpr::ReturnStatus::Fail);
+      vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+         << "Caught omniORB::fatalException:\n" << vprDEBUG_FLUSH;
+      vprDEBUG_NEXT(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+         << "  file: " << fe.file() << endl << vprDEBUG_FLUSH;
+      vprDEBUG_NEXT(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+         << "  line: " << fe.line() << endl << vprDEBUG_FLUSH;
+      vprDEBUG_NEXT(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+         << "  mesg: " << fe.errmsg() << endl << vprDEBUG_FLUSH;
+   }
+   catch(...)
+   {
+      cerr << "Caught unknown exception." << endl;
    }
 
-   vpr::ThreadMemberFunctor<CorbaManager>* corba_run =
-      new vpr::ThreadMemberFunctor<CorbaManager>(this, &CorbaManager::run);
-
-   m_my_thread = new vpr::Thread(corba_run);
+   return status;
 }
 
-void CorbaManager::registerSubjectManager (tweek::SubjectManagerImpl* mgr)
+vpr::ReturnStatus CorbaManager::registerSubjectManager (tweek::SubjectManagerImpl* mgr)
 {
    vprASSERT(! CORBA::is_nil(m_root_context) && "No naming service available");
+   vpr::ReturnStatus status;
 
-   tweek::SubjectManager_ptr mgr_ptr;
-   const char* id   = "SubjectManager";
-   const char* kind = "Object";
-   CosNaming::Name context_name;
-
-   mgr_ptr = mgr->_this();
-
-   context_name.length(1);
-   context_name[0].id   = CORBA::string_dup(id);
-   context_name[0].kind = CORBA::string_dup(kind);
-
-   // Bind the Subject Manager reference and activate the object within the
-   // POA.  If a Subject Manager is already bound, the exceptoin thrown
-   // prevents either operation from happening.  This is correct since we
-   // only want one Subject Manager per address space.
    try
    {
-      m_root_context->bind(context_name, mgr_ptr);
-      m_subj_mgr_id = m_poa->activate_object(mgr);
+      tweek::SubjectManager_ptr mgr_ptr;
+      const char* id   = "SubjectManager";
+      const char* kind = "Object";
+      CosNaming::Name context_name;
+
+      mgr_ptr = mgr->_this();
+
+      context_name.length(1);
+      context_name[0].id   = CORBA::string_dup(id);
+      context_name[0].kind = CORBA::string_dup(kind);
+
+      // Bind the Subject Manager reference and activate the object within the
+      // POA.  If a Subject Manager is already bound, the exceptoin thrown
+      // prevents either operation from happening.  This is correct since we
+      // only want one Subject Manager per address space.
+      try
+      {
+         m_root_context->bind(context_name, mgr_ptr);
+         m_subj_mgr_id = m_poa->activate_object(mgr);
+      }
+      catch (CosNaming::NamingContext::AlreadyBound& ex)
+      {
+         vprDEBUG(vprDBG_ALL, vprDBG_WARNING_LVL)
+            << "WARNING: Subject manager reference already bound!\n"
+            << vprDEBUG_FLUSH;
+      }
    }
-   catch (CosNaming::NamingContext::AlreadyBound& ex)
+   catch (CORBA::COMM_FAILURE& ex)
    {
+      status.setCode(vpr::ReturnStatus::Fail);
       vprDEBUG(vprDBG_ALL, vprDBG_WARNING_LVL)
-         << "WARNING: Subject manager reference already bound!\n"
+         << "Unable to contact the naming service\n" << vprDEBUG_FLUSH;
+   }
+   catch (CORBA::SystemException&)
+   {
+      status.setCode(vpr::ReturnStatus::Fail);
+      vprDEBUG(vprDBG_ALL, vprDBG_WARNING_LVL)
+         << "Caught a CORBA::SystemException while using the naming service\n"
          << vprDEBUG_FLUSH;
    }
+
+   return status;
 }
 
 // ============================================================================
