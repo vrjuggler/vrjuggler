8af214d4f287d508eaa13c1c33ede25b65ca11e2
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-11 17:47:41 +0000

    Increased paranoia level.  This prevents crashes previously seen due to uncaught exceptions.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6638 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/test/corba/main.cpp
+++ modules/tweek/test/corba/main.cpp
@@ -1,17 +1,35 @@
 #include <tweek/CORBA/CorbaManager.h>
 #include <vpr/Thread/Thread.h>
+#include <vpr/Util/Debug.h>
 
 
 int main (int argc, char* argv[])
 {
    tweek::CorbaManager mgr;
 
-   mgr.init();
-
-   while ( 1 )
+   try
    {
-      vpr::Thread::msleep(100);
+      if ( mgr.init(argc, argv).success() )
+      {
+         // Loop forever so that we can act sort of like a server.
+         while ( 1 )
+         {
+            vpr::Thread::msleep(100);
+         }
+      }
+      else
+      {
+         vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+            << "CORBA failed to initialize\n" << vprDEBUG_FLUSH;
+      }
    }
+   catch (...)
+   {
+      vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+         << "Caught an unknown exception!\n" << vprDEBUG_FLUSH;
+   }
+
+   vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL) << "Exiting\n" << vprDEBUG_FLUSH;
 
    return 0;
 }
