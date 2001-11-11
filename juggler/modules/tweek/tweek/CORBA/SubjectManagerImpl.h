9ed111507eca671a3c5b06076aa9beb475224f06
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-11 23:54:44 +0000

    Flesh this out a little more.  Since we only want one of these per memory space, this class is now a singleton (by way of vpr::Singleton). The beginnings of making this a thread-safe class have been put into place as well.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6644 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/tweek/CORBA/SubjectManagerImpl.h
+++ juggler/modules/tweek/tweek/CORBA/SubjectManagerImpl.h
@@ -3,16 +3,42 @@
 
 #include <tweek/tweekConfig.h>
 
+#include <string>
+#include <map>
+#include <vpr/Sync/Mutex.h>
+#include <vpr/Util/Singleton.h>
+
 #include <tweek/CORBA/SubjectManager.h>
 
 
 namespace tweek
 {
 
-class SubjectManagerImpl : public POA_tweek::SubjectManager
+class SubjectManagerImpl : public POA_tweek::SubjectManager,
+                           public PortableServer::RefCountServantBase,
+                           public vpr::Singleton<SubjectManagerImpl>
 {
 public:
-   virtual Subject_ptr getSubject (const char* name);
+   virtual Subject_ptr getSubject(const char* name);
+
+protected:
+   friend class vpr::Singleton<SubjectManagerImpl>;
+
+   /// Default constructor.
+   SubjectManagerImpl (void)
+   {
+      /* Do nothing. */ ;
+   }
+
+   // These two have to be here because Visual C++ will try to make them
+   // exported public symbols.  This causes problems because copying
+   // vpr::Mutex objects is not allowed.
+   SubjectManagerImpl (const SubjectManagerImpl& sm) {;}
+   void operator= (const SubjectManagerImpl& sm) {;}
+
+private:
+   std::map<std::string, Subject_var> m_subjects;
+   vpr::Mutex m_subjects_mutex;
 };
 
 } // End of tweek namespace
