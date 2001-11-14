63b5d826b795f22d00ccfe410c9d87bcfb7027b8
Allen Bierbaum <abierbaum@gmail.com>
2001-11-14 07:31:12 +0000

    Bug fixed:	OpenSceneGraph multi-window/multi-pipe
    The change allows OpenSceneGraph applications to work correctly across multiple windows and (most likely) across multiple pipes.
    This change also includes the addition of a new method to allow applications to configure the osgUtil::SceneViewer's that are being created.
    
    There is also some slight code cleanup and documentation changes.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6684 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/OSG/simple/osgNav/OsgNav.h
+++ modules/vrjuggler/samples/OSG/simple/osgNav/OsgNav.h
@@ -74,25 +74,25 @@ using namespace std;
 class OsgNav : public vrj::OsgApp
 {
 public:
-    OsgNav(vrj::Kernel* kern)
-        : vrj::OsgApp(kern)         // Initialize base class
-    {
+   OsgNav(vrj::Kernel* kern)
+   : vrj::OsgApp(kern)         // Initialize base class
+   {
       mFileToLoad = std::string("");
    }
 
-    virtual ~OsgNav (void) {
-        /* Do nothing. */ ;
-    }
+   virtual ~OsgNav (void) {
+      /* Do nothing. */;
+   }
 
-    // Execute any initialization needed before the API is started<BR><BR>
-    //
-    // This is called once before OGL is initialized
-    virtual void initScene()
-    {
-        //DeBugOut = Debug::getStream(0, 3, true, true, 1, true);
+   // Execute any initialization needed before the API is started<BR><BR>
+   //
+   // This is called once before OGL is initialized
+   virtual void initScene()
+   {
+      //DeBugOut = Debug::getStream(0, 3, true, true, 1, true);
 
-        //cout << "---------- App:init() ---------------" << endl;
-        // Initialize devices
+      //cout << "---------- App:init() ---------------" << endl;
+      // Initialize devices
       std::string wand("VJWand");
       std::string vjhead("VJHead");
       std::string but0("VJButton0");
@@ -102,72 +102,82 @@ public:
       std::string but4("VJButton4");
       std::string but5("VJButton5");
 
-        mWand.init(wand);
-        mHead.init(vjhead);
-        mButton0.init(but0);
-        mButton1.init(but1);
-        mButton2.init(but2);
+      mWand.init(wand);
+      mHead.init(vjhead);
+      mButton0.init(but0);
+      mButton1.init(but1);
+      mButton2.init(but2);
       mButton3.init(but3);
       mButton4.init(but4);
       mButton5.init(but5);
 
-        myInit();
-    }
+      myInit();
+   }
 
-    void myInit();
+   void myInit();
 
    virtual osg::Group* getScene()
    {
       return mRootNode;
    }
 
-    //: Function that is called immediately after a new OGL context is created
-    // Initialize GL state here. Also used to create context specific information<BR>
-    //
-    // This is called once for each display (twice for each stereo display)
-    virtual void contextInit()
-    {
-        initGLState();       // Initialize the GL state information. (lights, shading, etc)
-    }
+   virtual void configSceneView(osgUtil::SceneView* newSceneViewer)
+   {
+      newSceneViewer->setDefaults();
+      newSceneViewer->setBackgroundColor( osg::Vec4(0.0f, 0.0f, 0.0f, 0.0f) );
+      newSceneViewer->getLight()->setAmbient(osg::Vec4(0.3f,0.3f,0.3f,1.0f));
+      newSceneViewer->getLight()->setDiffuse(osg::Vec4(0.9f,0.9f,0.9f,1.0f));
+      newSceneViewer->getLight()->setSpecular(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
+   }
 
-    void bufferPreDraw();
+   //: Function that is called immediately after a new OGL context is created
+   // Initialize GL state here. Also used to create context specific information<BR>
+   //
+   // This is called once for each display (twice for each stereo display)
+   virtual void contextInit()
+   {
+      OsgApp::contextInit();    // MUST call base class version
+      initGLState();       // Initialize the GL state information. (lights, shading, etc)
+   }
+
+   void bufferPreDraw();
 
    // ----- Drawing Loop Functions ------
-    //
-    //  The drawing loop will look similar to this:
-    //
-    //  while (drawing)
-    //  {
-    //        preFrame();
-    //       draw();
-    //        intraFrame();     // Drawing is happening while here
-    //       sync();
-    //        postFrame();      // Drawing is now done
-    //
-    //       UpdateTrackers();
-    //  }
-    //------------------------------------
+   //
+   //  The drawing loop will look similar to this:
+   //
+   //  while (drawing)
+   //  {
+   //        preFrame();
+   //       draw();
+   //        intraFrame();     // Drawing is happening while here
+   //       sync();
+   //        postFrame();      // Drawing is now done
+   //
+   //       UpdateTrackers();
+   //  }
+   //------------------------------------
 
-    //: Function called after tracker update but before start of drawing<BR><BR>
-    //
-    // called once before every frame.
-    virtual void preFrame();
+   //: Function called after tracker update but before start of drawing<BR><BR>
+   //
+   // called once before every frame.
+   virtual void preFrame();
 
-    //: Function called after drawing has been triggered but BEFORE it completes<BR><BR>
-    //
-    // called once during each frame
-    virtual void intraFrame()
-    {
-        // Put your intra frame computations here.
-    }
+   //: Function called after drawing has been triggered but BEFORE it completes<BR><BR>
+   //
+   // called once during each frame
+   virtual void intraFrame()
+   {
+      // Put your intra frame computations here.
+   }
 
-    //: Function called before updating trackers but after the frame is drawn<BR><BR>
-    //
-    // called once after every frame
-    virtual void postFrame()
-    {
-        // Put your post frame computations here.
-    }
+   //: Function called before updating trackers but after the frame is drawn<BR><BR>
+   //
+   // called once after every frame
+   virtual void postFrame()
+   {
+      // Put your post frame computations here.
+   }
 
    void setModelFileName(std::string filename)
    {
@@ -175,33 +185,33 @@ public:
    }
 
 private:
-    void initGLState();
+   void initGLState();
 
 private:
    osg::Vec3 mPos;
-    double posInc;
+   double posInc;
 
-    osg::Group* mRootNode;
-    osg::Group* mNoNav;
-    osg::Transform* mNavTrans;
+   osg::Group* mRootNode;
+   osg::Group* mNoNav;
+   osg::Transform* mNavTrans;
    osg::Transform* mModelTrans;
    osg::Transform* mModelBaseXform;
 
    osg::Node* mModel;
 
-    float speed;
+   float speed;
 
    std::string mFileToLoad;
 
 public:
-    gadget::PositionInterface    mWand;     // the Wand
-    gadget::PositionInterface    mHead;     // the head
-    gadget::DigitalInterface   mButton0;
-    gadget::DigitalInterface   mButton1;
-    gadget::DigitalInterface   mButton2;
-    gadget::DigitalInterface   mButton3;
-    gadget::DigitalInterface   mButton4;
-    gadget::DigitalInterface   mButton5;
+   gadget::PositionInterface    mWand;     // the Wand
+   gadget::PositionInterface    mHead;     // the head
+   gadget::DigitalInterface   mButton0;
+   gadget::DigitalInterface   mButton1;
+   gadget::DigitalInterface   mButton2;
+   gadget::DigitalInterface   mButton3;
+   gadget::DigitalInterface   mButton4;
+   gadget::DigitalInterface   mButton5;
 };
 
 
