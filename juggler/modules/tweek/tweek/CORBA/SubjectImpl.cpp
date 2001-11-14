71871428b63556a8e77125c0e78e596bcf014327
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-14 03:49:44 +0000

    Thread safety.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6682 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/tweek/CORBA/SubjectImpl.cpp
+++ juggler/modules/tweek/tweek/CORBA/SubjectImpl.cpp
@@ -1,5 +1,6 @@
 #include <tweek/tweekConfig.h>
 
+#include <vpr/Sync/Mutex.h>
 #include <vpr/Util/Debug.h>
 
 #include <tweek/CORBA/SubjectImpl.h>
@@ -10,6 +11,7 @@ namespace tweek
 
 void SubjectImpl::attach (Observer_ptr o)
 {
+   vpr::Guard<vpr::Mutex> guard(m_observers_mutex);
    vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Attaching observer\n"
                                           << vprDEBUG_FLUSH;
    m_observers.push_back(o);
@@ -17,6 +19,7 @@ void SubjectImpl::attach (Observer_ptr o)
 
 void SubjectImpl::detach (Observer_ptr o)
 {
+   vpr::Guard<vpr::Mutex> guard(m_observers_mutex);
    std::vector<Observer_var>::iterator i;
 
    for ( i = m_observers.begin(); i != m_observers.end(); i++ )
@@ -35,6 +38,7 @@ void SubjectImpl::detach (Observer_ptr o)
 
 void SubjectImpl::notify ()
 {
+   vpr::Guard<vpr::Mutex> guard(m_observers_mutex);
    std::vector<Observer_var>::iterator i;
 
    vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Notifying observers\n"
@@ -44,6 +48,9 @@ void SubjectImpl::notify ()
    {
       (*i)->update();
    }
+
+   vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Done notifying observers\n"
+                                          << vprDEBUG_FLUSH;
 }
 
 } // End of tweek namespace
