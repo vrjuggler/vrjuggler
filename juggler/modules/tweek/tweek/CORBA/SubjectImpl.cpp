c343e2ffd8879dd0e0366fa94f5247070b1fdcba
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-14 16:14:08 +0000

    Bug fixed:	Segmentation faults in the CORBA reference counting code 		when trying to notify observers.
    To store our observers, use a std::vector<> of Observer_ptr's instead of
    Observer_var's.  We do the duplicate/release manually.  This seems to
    satisfy the reference counting better than the other way.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6692 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/tweek/CORBA/SubjectImpl.cpp
+++ juggler/modules/tweek/tweek/CORBA/SubjectImpl.cpp
@@ -14,23 +14,24 @@ void SubjectImpl::attach (Observer_ptr o)
    vpr::Guard<vpr::Mutex> guard(m_observers_mutex);
    vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Attaching observer\n"
                                           << vprDEBUG_FLUSH;
-   m_observers.push_back(o);
+   m_observers.push_back(Observer::_duplicate(o));
 }
 
 void SubjectImpl::detach (Observer_ptr o)
 {
    vpr::Guard<vpr::Mutex> guard(m_observers_mutex);
-   std::vector<Observer_var>::iterator i;
+   std::vector<Observer_ptr>::iterator i;
 
    for ( i = m_observers.begin(); i != m_observers.end(); i++ )
    {
       if ( o->_is_equivalent(*i) )
       {
-         std::vector<Observer_var>::iterator j = i;
+         std::vector<Observer_ptr>::iterator j = i;
          i++;
 
          vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Detaching observer\n"
                                                 << vprDEBUG_FLUSH;
+         CORBA::release(*j);
          m_observers.erase(j);
       }
    }
@@ -39,7 +40,7 @@ void SubjectImpl::detach (Observer_ptr o)
 void SubjectImpl::notify ()
 {
    vpr::Guard<vpr::Mutex> guard(m_observers_mutex);
-   std::vector<Observer_var>::iterator i;
+   std::vector<Observer_ptr>::iterator i;
 
    vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL) << "Notifying observers\n"
                                           << vprDEBUG_FLUSH;
