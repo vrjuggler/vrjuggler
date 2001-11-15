4507728525bad01de4234066d6abb42c7ee9e713
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-15 22:16:03 +0000

    I've attempted to get the collaborative whiteboard working, but I think it is an ill-fated idea.  The Swing Document interface does not appear to allow modifications without event firing, and this would lead to inifinte loops (GUI handles data change event and informs subject, subject updates observers, observer gets update and modifies its data model, data model fires data change event that is handled by the GUI).  In addition, the editor of the whiteboard must be exempt from receiving updates from the subject lest all the entered keystrokes be duplicated.  If this were solved, the event firing issue could be avoided entirely.  Perhaps I will look at this some other time when deadlines are not looming so closely.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6719 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/test/NetworkTestBean/WhiteboardSubjectImpl.cpp
+++ modules/tweek/test/NetworkTestBean/WhiteboardSubjectImpl.cpp
@@ -4,10 +4,23 @@
 namespace networktest
 {
 
-void WhiteboardSubjectImpl::addText (const char* text)
+void WhiteboardSubjectImpl::insertText (CORBA::Long offset, CORBA::Long length,
+                                        const char* text)
 {
-   m_text.append(text);
+   m_text.insert(offset, text);
+   m_last_offset = offset;
+   m_last_length = length;
+   m_last_change = text;
    notify();
+}
+
+void WhiteboardSubjectImpl::getLastChange (CORBA::Long& offset,
+                                           CORBA::Long& length,
+                                           CORBA::String_out text)
+{
+   offset = m_last_offset;
+   length = m_last_length;
+   text   = CORBA::string_dup(m_last_change.c_str());
 }
 
 char* WhiteboardSubjectImpl::getAllText ()
--- modules/tweek/test/NetworkTestBean/networktest/NetworkTest.java
+++ modules/tweek/test/NetworkTestBean/networktest/NetworkTest.java
@@ -2,6 +2,8 @@ package networktest;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
 import org.vrjuggler.tweek.net.*;
 import org.vrjuggler.tweek.net.corba.*;
 import tweek.*;
@@ -24,51 +26,67 @@ public class NetworkTest extends JPanel implements CommunicationListener
       }
    }
 
-   public void communication (CommunicationEvent e)
+   public void connectionStateChanged (CommunicationEvent e)
    {
       CorbaService corba_service = e.getCorbaService();
       SubjectManager mgr         = corba_service.getSubjectManager();
 
       Subject subject = mgr.getSubject("SliderSubject");
       SliderSubject slider_subject = SliderSubjectHelper.narrow(subject);
-      m_slider_observer = new SliderObserverImpl(m_data_slider, slider_subject);
-      corba_service.registerObject(m_slider_observer, "SliderObserver");
-      slider_subject.attach(m_slider_observer._this());
-      m_data_slider.setValue(slider_subject.getValue());
-      m_data_slider.addChangeListener(new SliderChangeListener(slider_subject));
-
       subject = mgr.getSubject("WhiteboardSubject");
       WhiteboardSubject whiteboard_subject = WhiteboardSubjectHelper.narrow(subject);
-      m_whiteboard_observer = new WhiteboardObserverImpl(m_whiteboard,
-                                                         whiteboard_subject);
-      corba_service.registerObject(m_whiteboard_observer, "WhiteboardObserver");
-      whiteboard_subject.attach(m_whiteboard_observer._this());
+
+      if ( CommunicationEvent.CONNECT == e.getType() )
+      {
+         m_slider_observer = new SliderObserverImpl(m_data_slider, slider_subject);
+         corba_service.registerObject(m_slider_observer, "SliderObserver");
+         slider_subject.attach(m_slider_observer._this());
+         m_data_slider.setValue(slider_subject.getValue());
+         m_data_slider.addChangeListener(new SliderChangeListener(slider_subject));
+
+         m_whiteboard_observer = new WhiteboardObserverImpl(m_whiteboard,
+                                                            whiteboard_subject);
+         corba_service.registerObject(m_whiteboard_observer, "WhiteboardObserver");
+         whiteboard_subject.attach(m_whiteboard_observer._this());
+         m_whiteboard.setText(whiteboard_subject.getAllText());
+         m_whiteboard.getDocument().addDocumentListener(new DocumentChangeListener(whiteboard_subject));
+      }
+      else if ( CommunicationEvent.DISCONNECT == e.getType() )
+      {
+         slider_subject.detach(m_slider_observer._this());
+         whiteboard_subject.detach(m_whiteboard_observer._this());
+      }
    }
 
    private void jbInit() throws Exception
    {
       this.setLayout(m_bean_layout);
-      m_whiteboard.setBorder(BorderFactory.createLoweredBevelBorder());
-      m_whiteboard.setPreferredSize(new Dimension(300, 200));
-      m_whiteboard_panel.setLayout(m_whitboard_layout);
+      m_whiteboard_panel.setLayout(m_whiteboard_layout);
       m_data_slider.setMajorTickSpacing(20);
       m_data_slider.setMinorTickSpacing(5);
       m_data_slider.setPaintTicks(true);
       m_whiteboard_label.setHorizontalAlignment(SwingConstants.CENTER);
       m_whiteboard_label.setText("Whiteboard");
-      this.add(m_slider_panel, BorderLayout.NORTH);
+      m_whiteboard.setBorder(BorderFactory.createLoweredBevelBorder());
+      m_whiteboard.setMinimumSize(new Dimension(200, 25));
+      m_whiteboard.setPreferredSize(new Dimension(300, 200));
+      m_whiteboard.setLineWrap(true);
+      m_whiteboard.setWrapStyleWord(true);
       m_slider_panel.add(m_data_slider, null);
-      this.add(m_whiteboard_panel, BorderLayout.CENTER);
-      m_whiteboard_panel.add(m_whiteboard, BorderLayout.CENTER);
+      m_whiteboard_scroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+      m_whiteboard_scroller.getViewport().add(m_whiteboard, null);
+      m_whiteboard_panel.add(m_whiteboard_scroller, BorderLayout.CENTER);
       m_whiteboard_panel.add(m_whiteboard_label, BorderLayout.NORTH);
+
+      this.add(m_slider_panel, BorderLayout.NORTH);
+      this.add(m_whiteboard_panel, BorderLayout.CENTER);
    }
 
-   private class SliderChangeListener
-      implements javax.swing.event.ChangeListener
+   private class SliderChangeListener implements ChangeListener
    {
       public SliderChangeListener (SliderSubject subject)
       {
-         slider_subject = subject;
+         m_slider_subject = subject;
       }
 
       public void stateChanged (javax.swing.event.ChangeEvent e)
@@ -78,20 +96,58 @@ public class NetworkTest extends JPanel implements CommunicationListener
          if ( ! source.getValueIsAdjusting() )
          {
             System.out.println("Informing subject of new value ...");
-            slider_subject.setValue(source.getValue());
+            m_slider_subject.setValue(source.getValue());
             System.out.println("Done informing subject");
          }
       }
 
-      private SliderSubject slider_subject = null;
+      private SliderSubject m_slider_subject = null;
    }
+
+   private class DocumentChangeListener implements DocumentListener
+   {
+      public DocumentChangeListener (WhiteboardSubject subject)
+      {
+         m_whiteboard_subject = subject;
+      }
+
+      public void insertUpdate (DocumentEvent e)
+      {
+         Document doc = e.getDocument();
+
+         try
+         {
+            int offset = e.getOffset();
+            int length = e.getLength();
+            String new_text = doc.getText(offset, length);
+            System.out.println("New text: " + new_text);
+            m_whiteboard_subject.insertText(offset, length, new_text);
+         }
+         catch (BadLocationException loc_ex)
+         {
+            /* This probably won't happen. */ ;
+         }
+      }
+
+      public void removeUpdate (DocumentEvent e)
+      {
+      }
+
+      public void changedUpdate (DocumentEvent e)
+      {
+      }
+
+      private WhiteboardSubject m_whiteboard_subject = null;
+   }
+
+   private BorderLayout m_bean_layout = new BorderLayout();
 
    private JPanel m_slider_panel = new JPanel();
    private JPanel m_whiteboard_panel = new JPanel();
    private JSlider m_data_slider = new JSlider();
+   private JScrollPane m_whiteboard_scroller = new JScrollPane();
+   private BorderLayout m_whiteboard_layout = new BorderLayout();
    private JTextArea m_whiteboard = new JTextArea();
-   private BorderLayout m_whitboard_layout = new BorderLayout();
-   private BorderLayout m_bean_layout = new BorderLayout();
    private JLabel m_whiteboard_label = new JLabel();
 
    private SliderObserverImpl m_slider_observer = null;
