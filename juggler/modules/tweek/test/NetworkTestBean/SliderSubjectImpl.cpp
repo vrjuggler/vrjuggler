b130c2767ffaa62c702711c442fbc66d36cd46d7
Patrick Hartling <patrick.hartling@gmail.com>
2001-11-18 17:01:02 +0000

    Added some debugging output at vprDBG_STATE_LVL.  This was helpful in debugging the distributed deadlock I saw a few days ago, and it demonstrates what is happening on the C++ side.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6749 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/test/NetworkTestBean/SliderSubjectImpl.cpp
+++ juggler/modules/tweek/test/NetworkTestBean/SliderSubjectImpl.cpp
@@ -1,3 +1,4 @@
+#include <vpr/Util/Debug.h>
 #include <SliderSubjectImpl.h>
 
 
@@ -6,12 +7,16 @@ namespace networktest
 
 void SliderSubjectImpl::setValue (long value)
 {
+   vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL)
+      << "Setting m_value to " << value << std::endl << vprDEBUG_FLUSH;
    m_value = value;
-   notify();
+   SubjectImpl::notify();
 }
 
 long SliderSubjectImpl::getValue ()
 {
+   vprDEBUG(vprDBG_ALL, vprDBG_STATE_LVL)
+      << "Returning " << m_value << " to caller\n" << vprDEBUG_FLUSH;
    return m_value;
 }
 
