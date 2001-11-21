a90db629486542487a2a69009b82c742fce7136f
Kevin Meinert <subatomic@users.sourceforge.net>
2001-11-21 20:47:10 +0000

    updated to newest cppunit in juggler/external/cppunit.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@6788 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/sonix/TestSuite/main.cpp
+++ juggler/modules/sonix/TestSuite/main.cpp
@@ -42,46 +42,44 @@
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
 
-#include <framework/TestSuite.h>
-#include <textui/TestRunner.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TextTestRunner.h>
 
-#include <TestCases/snxMathTest.h>
+//#include <TestCases/snxMathTest.h>
 #include <TestCases/exampleTest.h>
 //#include <Utils/Debug.h>
 
 
-//using namespace vpr;
-
-int main (int ac, char **av)
+int main( int ac, char **av )
 {
 //    vprDEBUG(0,0) << "Starting test\n" << vprDEBUG_FLUSH;       
 
-    TestRunner runner;
+   CppUnit::TextTestRunner runner;
 
    //------------------------------------
    //  noninteractive
    //------------------------------------	
    // create non-interactive test suite
-   TestSuite* noninteractive_suite = new TestSuite("NonInteractive");
+   CppUnit::TestSuite* noninteractive_suite = new CppUnit::TestSuite("NonInteractive");
 
    // add tests to the suite
    noninteractive_suite->addTest( snxTest::exampleTest::suite());
-   noninteractive_suite->addTest( snxTest::snxMathTest::suite());
+  // noninteractive_suite->addTest( snxTest::snxMathTest::suite());
 
    // Add the test suite to the runner
-   runner.addTest( "noninteractive", noninteractive_suite );
+   runner.addTest( noninteractive_suite );
 
    // create test suite #2
-   TestSuite* interactive_suite = new TestSuite("Interactive");
+   //CppUnit::TestSuite* interactive_suite = new CppUnit::TestSuite("Interactive");
 
    // add tests to the suite
    //interactive_suite->addTest(ThreadTest::suite());
 
    // Add the test suite to the runner
-   runner.addTest("interactive", interactive_suite);
+   //runner.addTest("interactive", interactive_suite);
 
    // run all test suites
-   runner.run( ac, av );
+   runner.run();
 
 
    return 0;
