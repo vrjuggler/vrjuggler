3850abdaa26f15875727bcc883e9d60977f66a6d
Patrick Hartling <patrick.hartling@gmail.com>
2001-12-12 03:21:20 +0000

    Given a dotted-decimal IP address, the simulated DNS lookup now returns the corresponding 32-bit integer address rather than a fake number. This helps the test suite a little, but there seems to be something I am missing related to byte ordering.  Perhaps I shouldn't even bother considering that the simulator will never actually be reading anything from the network.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6864 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/md/SIM/DNS.cpp
@@ -0,0 +1,54 @@
+#include <vpr/vprConfig.h>
+
+#include <stdio.h>
+
+#include <vpr/Util/Debug.h>
+#include <vpr/md/SIM/DNS.h>
+
+
+namespace vpr
+{
+
+namespace sim
+{
+
+vpr::Uint32 DNS::lookupAddress (const std::string& addr_str)
+{
+   vpr::Uint32 found_addr;
+
+   // if name is found in lookup, return the address
+   if ( mDnsAddressLookup.count(addr_str) > 0 )
+   {
+      found_addr = mDnsAddressLookup[addr_str];
+   }
+   // if not found, then make an entry for the new address.
+   else
+   {
+      union
+      {
+         vpr::Uint8 bytes[0];
+         vpr::Uint32 value;
+      } addr;
+      int retval;
+
+      retval = sscanf(addr_str.c_str(), "%3u.%3u.%3u.%3u", &addr.bytes[0],
+                      &addr.bytes[1], &addr.bytes[2], &addr.bytes[3]);
+
+      if ( retval == 4 )
+      {
+         found_addr = addr.value;
+      }
+      else
+      {
+         found_addr = vpr::System::Htonl(mUniqueGenerator++);
+      }
+
+      mDnsAddressLookup[addr_str] = found_addr;
+   }
+
+   return found_addr;
+}
+
+}
+
+}
--- modules/vapor/vpr/md/SIM/IO/Socket/InetAddrSIM.h
+++ modules/vapor/vpr/md/SIM/IO/Socket/InetAddrSIM.h
@@ -47,6 +47,7 @@
 #include <string>
 #include <vpr/IO/Socket/SocketTypes.h>
 #include <vpr/IO/Socket/InetAddrBase.h> // my base class...
+#include <vpr/System.h>
 
 
 // XXX: Extend to work with IPv6.
@@ -198,7 +199,7 @@ public:
     */
    vpr::Uint32 getAddressValue( void ) const
    {
-      return mAddress;
+      return vpr::System::Ntohl(mAddress);
    }
 
    /**
@@ -262,7 +263,7 @@ protected:
     */
    void setAddressValue( const vpr::Uint32 addr_value )
    {
-      mAddress = addr_value;
+      mAddress = vpr::System::Htonl(addr_value);
    }
 
    /**
