46a03f6ff3da0b3f1519ca555e0b51258570df74
Patrick Hartling <patrick.hartling@gmail.com>
2001-12-20 19:18:37 +0000

    Beautify according to the coding standard.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@6935 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/md/POSIX/IO/Socket/InetAddrBSD.cpp
+++ modules/vapor/vpr/md/POSIX/IO/Socket/InetAddrBSD.cpp
@@ -52,7 +52,7 @@
 #include <vpr/md/POSIX/IO/Socket/InetAddrBSD.h>
 
 #ifndef INADDR_NONE
-#define INADDR_NONE 0xffffffff   /* -1 return */
+   #define INADDR_NONE 0xffffffff   /* -1 return */
 #endif
 
 
@@ -62,7 +62,8 @@
 extern int errno;
 
 
-namespace vpr {
+namespace vpr
+{
 
 const InetAddrBSD InetAddrBSD::AnyAddr;      // Default constructor defaults to ANY addr
 
@@ -71,48 +72,49 @@ const InetAddrBSD InetAddrBSD::AnyAddr;      // Default constructor defaults to 
 // form <address>:<port> where <address> can be a hostname or a
 // dotted-decimal IP address.
 // ----------------------------------------------------------------------------
-ReturnStatus
-InetAddrBSD::setAddress (const std::string& address) {
-    std::string::size_type pos;
-    std::string host_addr, host_port;
-    Uint16 port;
-    ReturnStatus retval;
+vpr::ReturnStatus InetAddrBSD::setAddress (const std::string& address)
+{
+   std::string::size_type pos;
+   std::string host_addr, host_port;
+   vpr::Uint16 port;
+   vpr::ReturnStatus retval;
 
-    // Extract the address and the port number from the given string.
-    pos       = address.find(":");
-    host_addr = address.substr(0, pos);
-    host_port = address.substr(pos + 1);
-    port      = (Uint16) atoi(host_port.c_str());
+   // Extract the address and the port number from the given string.
+   pos       = address.find(":");
+   host_addr = address.substr(0, pos);
+   host_port = address.substr(pos + 1);
+   port      = (Uint16) atoi(host_port.c_str());
 
-    retval = lookupAddress(host_addr);
-    setPort(port);
-    setFamily(SocketTypes::INET);
+   retval = lookupAddress(host_addr);
+   setPort(port);
+   setFamily(vpr::SocketTypes::INET);
 
-    return retval;
+   return retval;
 }
 
 // ----------------------------------------------------------------------------
 // Get the protocol family of this address structure.
 // ----------------------------------------------------------------------------
-SocketTypes::Domain
-InetAddrBSD::getFamily (void) const {
-    SocketTypes::Domain family;
+vpr::SocketTypes::Domain InetAddrBSD::getFamily (void) const
+{
+   vpr::SocketTypes::Domain family;
 
-    switch (m_addr.sin_family) {
+   switch ( m_addr.sin_family )
+   {
 #ifdef PF_LOCAL
       case PF_LOCAL:
 #else
       case PF_UNIX:
 #endif
-        family = SocketTypes::LOCAL;
-        break;
+         family = SocketTypes::LOCAL;
+         break;
       case PF_INET:
-        family = SocketTypes::INET;
-        break;
+         family = SocketTypes::INET;
+         break;
 #ifdef PF_INET6
       case PF_INET6:
-        family = SocketTypes::INET6;
-        break;
+         family = SocketTypes::INET6;
+         break;
 #endif
 #if defined(PF_LINK) || defined(PF_RAW)
 #ifdef PF_LINK
@@ -120,114 +122,120 @@ InetAddrBSD::getFamily (void) const {
 #else
       case PF_RAW:
 #endif
-        family = SocketTypes::LINK;
-        break;
+         family = SocketTypes::LINK;
+         break;
 #endif
-    }
+   }
 
-    return family;
+   return family;
 }
 
 // ----------------------------------------------------------------------------
 // Set the protocol family of this address structure.
 // ----------------------------------------------------------------------------
-void
-InetAddrBSD::setFamily (const SocketTypes::Domain family) {
-    switch (family) {
+void InetAddrBSD::setFamily (const vpr::SocketTypes::Domain family)
+{
+   switch ( family )
+   {
       case SocketTypes::LOCAL:
 #ifdef PF_LOCAL
-        m_addr.sin_family = PF_LOCAL;
+         m_addr.sin_family = PF_LOCAL;
 #else
-        m_addr.sin_family = PF_UNIX;
+         m_addr.sin_family = PF_UNIX;
 #endif
-        break;
+         break;
       case SocketTypes::INET:
-        m_addr.sin_family = PF_INET;
-        break;
+         m_addr.sin_family = PF_INET;
+         break;
       case SocketTypes::INET6:
 #ifdef PF_INET6
-        m_addr.sin_family = PF_INET6;
+         m_addr.sin_family = PF_INET6;
 #else
-        fprintf(stderr,
-                "[vpr::InetAddrBSD] WARNING: IPv6 not supported on this host!\n");
+         fprintf(stderr,
+                 "[vpr::InetAddrBSD] WARNING: IPv6 not supported on this host!\n");
 #endif
-        break;
+         break;
 #if defined(PF_LINK) || defined(PF_RAW)
       case SocketTypes::LINK:
 #ifdef PF_LINK
-        m_addr.sin_family = PF_LINK;
+         m_addr.sin_family = PF_LINK;
 #else
-        m_addr.sin_family = PF_RAW;
+         m_addr.sin_family = PF_RAW;
 #endif
-        break;
+         break;
 #endif
       default:
-        fprintf(stderr,
-                "[vpr::InetAddrBSD] ERROR: Unknown socket family value %d\n",
-                family);
-        break;
-    }
+         fprintf(stderr,
+                 "[vpr::InetAddrBSD] ERROR: Unknown socket family value %d\n",
+                 family);
+         break;
+   }
 }
 
 // ----------------------------------------------------------------------------
 // Get the IP address associated with this structure as a human-readable
 // string.
 // ----------------------------------------------------------------------------
-std::string
-InetAddrBSD::getAddressString (void) const {
-    char ip_addr[sizeof("255.255.255.255")];
-    std::string ip_str;
-    union {
-        char c[sizeof(Uint32)];
-        Uint32 value;
-    } addr;
+std::string InetAddrBSD::getAddressString (void) const
+{
+   char ip_addr[sizeof("255.255.255.255")];
+   std::string ip_str;
+   union
+   {
+      char c[sizeof(vpr::Uint32)];
+      vpr::Uint32 value;
+   } addr;
 
-    addr.value = m_addr.sin_addr.s_addr;
+   addr.value = m_addr.sin_addr.s_addr;
 
-    snprintf(ip_addr, sizeof(ip_addr), "%u.%u.%u.%u", (Uint8) addr.c[0],
-             (Uint8) addr.c[1], (Uint8) addr.c[2], (Uint8) addr.c[3]);
-    ip_str = ip_addr;
+   snprintf(ip_addr, sizeof(ip_addr), "%u.%u.%u.%u", (Uint8) addr.c[0],
+            (Uint8) addr.c[1], (Uint8) addr.c[2], (Uint8) addr.c[3]);
+   ip_str = ip_addr;
 
-    return ip_str;
+   return ip_str;
 }
 
 // ----------------------------------------------------------------------------
 // Look up the given address and store the address in m_addr.
 // ----------------------------------------------------------------------------
-ReturnStatus
-InetAddrBSD::lookupAddress (const std::string& address) {
-    ReturnStatus retval;
-    struct hostent* host_entry;
+vpr::ReturnStatus InetAddrBSD::lookupAddress (const std::string& address)
+{
+   vpr::ReturnStatus retval;
+   struct hostent* host_entry;
 
-    // First, try looking the host up by name.
-    host_entry = gethostbyname(address.c_str());
+   // First, try looking the host up by name.
+   host_entry = gethostbyname(address.c_str());
 
-    // If that succeeded, put the result in m_remote_addr.
-    if ( host_entry != NULL ) {
-        copyAddressValue(host_entry->h_addr);
-    }
-    // If gethostbyname(3) failed, the address string may be an IP address.
-    else {
-        in_addr_t addr;
+   // If that succeeded, put the result in m_remote_addr.
+   if ( host_entry != NULL )
+   {
+      copyAddressValue(host_entry->h_addr);
+   }
+   // If gethostbyname(3) failed, the address string may be an IP address.
+   else
+   {
+      in_addr_t addr;
 
-        // Try looking it up with inet_addr(3).
-        addr = inet_addr(address.c_str());
+      // Try looking it up with inet_addr(3).
+      addr = inet_addr(address.c_str());
 
-        // If the address string could not be found using inet_addr(3), then
-        // return error status.
-        if ( addr == INADDR_NONE ) {
-            fprintf(stderr,
-                    "[vpr::InetAddrBSD] Could not find address for '%s': %s\n",
-                    address.c_str(), strerror(errno));
-            retval.setCode(ReturnStatus::Fail);
-        }
-        // Otherwise, we found the integer address successfully.
-        else {
-            setAddressValue(addr);
-        }
-    }
+      // If the address string could not be found using inet_addr(3), then
+      // return error status.
+      if ( addr == INADDR_NONE )
+      {
+         fprintf(stderr,
+                 "[vpr::InetAddrBSD] Could not find address for '%s': %s\n",
+                 address.c_str(), strerror(errno));
+         retval.setCode(ReturnStatus::Fail);
+      }
+      // Otherwise, we found the integer address successfully.
+      else
+      {
+         setAddressValue(addr);
+      }
+   }
 
-    return retval;
+   return retval;
 }
 
-}; // End of vpr namespace
+} // End of vpr namespace
