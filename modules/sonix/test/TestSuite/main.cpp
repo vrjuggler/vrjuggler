dc0fd3cd2c60e702ab0cf09de5a0b073420a374b
Kevin Meinert <subatomic@users.sourceforge.net>
2001-12-28 21:56:43 +0000

    Initial revision
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7017 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/sonix/test/TestSuite/main.cpp
@@ -0,0 +1,66 @@
+
+#include <cppunit/TestSuite.h>
+#include <cppunit/TextTestRunner.h>
+
+#include <TestCases/exampleTest.h> // dummy test...
+
+
+void addNon( CppUnit::TextTestRunner& runner )
+{
+   CppUnit::TestSuite* suite = new CppUnit::TestSuite( "NonInteractive" );
+
+   // add tests to the suite
+   suite->addTest( synTest::exampleTest::suite() );
+  
+   // Add the test suite to the runner
+   runner.addTest( suite );
+}
+void addInt( CppUnit::TextTestRunner& runner )
+{
+   CppUnit::TestSuite* suite = new CppUnit::TestSuite( "Interactive" );
+
+   // add tests to the suite
+   suite->addTest( synTest::exampleTest::suite() );
+
+   // Add the test suite to the runner
+   runner.addTest( suite );
+}
+
+void addAll( CppUnit::TextTestRunner& runner )
+{
+   addNon( runner );
+   addInt( runner );
+}
+
+
+
+
+int main( int ac, char **av )
+{
+   // display usage if needed
+   if (ac == 1 || ac > 2)
+   {
+      std::cout<<"Usage: "<<av[0]<<" [non|int|all]\n"<<std::flush;
+      std::cout<<" - run with 'non' for non-interactive tests\n"<<std::flush;
+      std::cout<<" - run with 'int' for interactive tests\n"<<std::flush;
+      std::cout<<" - default is 'non'... (broken)\n\n"<<std::flush;
+   }   
+   
+   // add the tests
+   CppUnit::TextTestRunner runner;
+   for (int x = 1; x < ac; ++x)
+   {
+      std::string arg = av[x];
+      
+      if (arg == "non" || arg == "noninteractive")
+         addNon( runner );
+      else if (arg == "int" || arg == "interactive")
+         addInt( runner );
+      else if (arg == "all")
+         addAll( runner );
+   }
+
+   // run the tests
+   runner.run();
+   return 0;
+}
