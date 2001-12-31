cb265aeab31dc192e3691dc6e9e822a266f06cc6
Patrick Hartling <patrick.hartling@gmail.com>
2001-12-31 19:12:39 +0000

    Added a test that forces messages to go out on the network rather than using only loopback.  Miracle of miracles, it even passes!
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@7063 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/test/TestSuite/TestCases/Simulator/SocketSimulatorTest.h
+++ juggler/modules/vapor/test/TestSuite/TestCases/Simulator/SocketSimulatorTest.h
@@ -47,14 +47,10 @@ public:
 
    void graphConstructionTest (void)
    {
-      std::string path_base;
       vpr::ReturnStatus status;
       vpr::sim::NetworkGraph::AddressList addrs;
 
-      status = vpr::System::getenv("VPR_TEST_DIR", path_base);
-      CPPUNIT_ASSERT(status.success() && "Could not find VPR_TEST_DIR environment variable");
-
-      status = vpr::sim::Controller::instance()->constructNetwork(path_base.append("/test_network.tiers"));
+      status = constructGraph();
       CPPUNIT_ASSERT(status.success() && "Could not construct network");
 
       status = vpr::sim::Controller::instance()->getNetworkGraph().getAllAddresses(addrs);
@@ -124,6 +120,118 @@ public:
 
       status = acceptor.close();
       CPPUNIT_ASSERT(status.success() && "Failed to close acceptor");
+   }
+
+   void networkCommTest (void)
+   {
+      vpr::ReturnStatus status;
+      vpr::sim::NetworkGraph::AddressList addrs;
+
+//      status = constructGraph();
+//      CPPUNIT_ASSERT(status.success() && "Could not construct network");
+
+      status =
+         vpr::sim::Controller::instance()->getNetworkGraph().getAllAddresses(addrs);
+      CPPUNIT_ASSERT(status.success() && "Could not request all addresses");
+
+      const unsigned int node_count =
+         vpr::sim::Controller::instance()->getNetworkGraph().getNodeCount();
+      CPPUNIT_ASSERT(node_count > 0 && "No nodes in the graph");
+
+      std::vector<vpr::Uint32> addr_vec;
+      addr_vec.resize(node_count);
+      std::iota(addr_vec.begin(), addr_vec.end(), 0);
+      std::random_shuffle(addr_vec.begin(), addr_vec.end());
+
+      // Create a vector with min(20, node_count) sockets, all with default
+      // addresses,
+      const unsigned int socket_count = (20 < node_count ? 20 : node_count);
+      std::vector<vpr::SocketStream> sockets;
+
+      vpr::InetAddr local_addr;
+
+      // Create a bunch of sockets with different addresses to ensure that they
+      // are bound to different nodes on the network.
+      for ( unsigned int i = 0; i < socket_count; i++ )
+      {
+         vpr::sim::NetworkGraph::net_vertex_t node =
+            vpr::sim::Controller::instance()->getNetworkGraph().getNode(addr_vec[i]);
+         vpr::sim::NetworkNodePtr node_prop =
+            vpr::sim::Controller::instance()->getNetworkGraph().getNodeProperty(node);
+
+         // All the sockets can have the same port number because they will be
+         // on different nodes in the network.
+         local_addr.setAddress(node_prop->getIpAddress(), 40000);
+
+         sockets.push_back(vpr::SocketStream(local_addr,
+                                             vpr::InetAddr::AnyAddr));
+         CPPUNIT_ASSERT(sockets[i].getLocalAddr() == local_addr && "Address assignment failed");
+
+         status = sockets[i].open();
+         CPPUNIT_ASSERT(status.success() && "Failed to open new socket");
+
+         status = sockets[i].bind();
+         CPPUNIT_ASSERT(status.success() && "Failed to bind socket to local address");
+      }
+
+      std::vector<vpr::SocketStream> source_sockets, dest_sockets;
+
+      // Put the even-numbered sockets in a listening state, and connect the
+      // odd-numbered sockets to them.  That is, socket(n) is an acceptor,
+      // and socket(n + 1) connects to it (where n is even).
+      for ( unsigned int i = 0; i + 1 < socket_count; i += 2 )
+      {
+         const unsigned int acceptor = i, connector = i + 1;
+
+         status = sockets[acceptor].listen();
+         CPPUNIT_ASSERT(status.success() && "Failed to go into listening state");
+
+         sockets[connector].setRemoteAddr(sockets[acceptor].getLocalAddr());
+         status = sockets[connector].connect();
+         CPPUNIT_ASSERT(status == vpr::ReturnStatus::InProgress &&
+                        "Connection to acceptor should be in progress");
+
+         vpr::SocketStream new_sock;
+         status = sockets[acceptor].accept(new_sock);
+         CPPUNIT_ASSERT(status.success() && "Failed to accept waiting connection");
+
+         source_sockets.push_back(sockets[connector]);
+         dest_sockets.push_back(new_sock);
+      }
+
+      CPPUNIT_ASSERT(source_sockets.size() == dest_sockets.size() &&
+                     "Different number of source and destination sockets");
+
+      std::string msg("Here is a packet");
+
+      // Now we send a message from each source to its connected destination.
+      for ( unsigned int i = 0; i < source_sockets.size(); i++ )
+      {
+         vpr::Uint32 bytes_written;
+         status = source_sockets[i].send(msg, msg.length(), bytes_written);
+         CPPUNIT_ASSERT(status.success() && "Failed to send buffer to receiver");
+
+         char buffer[20];
+         vpr::Uint32 bytes_read;
+
+         do
+         {
+            status = dest_sockets[i].recv(buffer, sizeof(buffer), bytes_read);
+         } while (status == vpr::ReturnStatus::WouldBlock);
+
+         buffer[bytes_read] = '\0';
+
+         CPPUNIT_ASSERT(status.success() && "Failed to receive buffer");
+
+         CPPUNIT_ASSERT(buffer == msg && "Got the wrong message");
+      }
+
+      for ( std::vector<vpr::SocketStream>::iterator i = sockets.begin();
+            i != sockets.end();
+            i++ )
+      {
+         (*i).close();
+      }
    }
 
    void
@@ -314,12 +422,27 @@ public:
       CppUnit::TestSuite* test_suite = new CppUnit::TestSuite ("SocketSimulatorTest");
       test_suite->addTest( new CppUnit::TestCaller<SocketSimulatorTest>("graphConstructionTest", &SocketSimulatorTest::graphConstructionTest));
       test_suite->addTest( new CppUnit::TestCaller<SocketSimulatorTest>("singleThreadTest", &SocketSimulatorTest::singleThreadTest));
+      test_suite->addTest( new CppUnit::TestCaller<SocketSimulatorTest>("networkCommTest", &SocketSimulatorTest::networkCommTest));
 //      test_suite->addTest(new CppUnit::TestCaller<SocketSimulatorTest>("multiThreadTest", &SocketSimulatorTest::multiThreadTest));
 
       return test_suite;
    }
 
 protected:
+   vpr::ReturnStatus constructGraph (void)
+   {
+      std::string path_base;
+      vpr::ReturnStatus status;
+
+      status = vpr::System::getenv("VPR_TEST_DIR", path_base);
+      CPPUNIT_ASSERT(status.success() && "Could not find VPR_TEST_DIR environment variable");
+
+      status = vpr::sim::Controller::instance()->constructNetwork(path_base.append("/test_network.tiers"));
+      CPPUNIT_ASSERT(status.success() && "Could not construct network");
+
+      return status;
+   }
+
    enum State {
       ACCEPTOR_READY,
       NOT_READY,
