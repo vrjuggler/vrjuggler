a602d2a99923c529c31e2bfaaac01ad49116eccb
Allen Bierbaum <abierbaum@gmail.com>
2002-01-02 20:32:14 +0000

    vpr::Interval now uses a vpr::Uint64 internally.
    This eliminates the wrapping effect of the old method and allows for strict sorting of values.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7086 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/Util/Interval.cpp
+++ modules/vapor/vpr/Util/Interval.cpp
@@ -46,16 +46,68 @@
 #endif
 
 #include <vpr/Util/Interval.h>
+#include <vpr/System.h>
 
 const vpr::Interval vpr::Interval::NoWait(0,vpr::Interval::Base);
 const vpr::Interval vpr::Interval::NoTimeout(0xffffffffUL, vpr::Interval::Base);
 const vpr::Interval vpr::Interval::HalfPeriod((0xffffffffUL/2), vpr::Interval::Base);
 
-// Simulator-only version of vpr::Interval::setNow().
-#ifdef VPR_SIMULATOR
-void vpr::Interval::setNow ()
+namespace vpr
 {
-   mTensOfUsecs = vpr::sim::Controller::instance()->getClock().getCurrentTime().getBaseVal();
+
+   // Simulator-only version of vpr::Interval::setNow().
+#ifdef VPR_SIMULATOR
+void Interval::setNow()
+{
+   mMicroSeconds = vpr::sim::Controller::instance()->getClock().getCurrentTime().getBaseVal();
+}
+#else
+void Interval::setNow()
+{
+   setNowReal();
 }
 #endif /* ifdef VPR_SIMULATOR */
+
+//
+// Real implementation of setNow that uses the real clock time from the system
+//
+void Interval::setNowReal()
+{
+#if defined(VPR_OS_Win32)
+   LARGE_INTEGER count;
+
+   // XXX: Implement this
+   /* Sadly; nspr requires the interval to range from 1000 ticks per second
+    * to only 100000 ticks per second; QueryPerformanceCounter is too high
+    * resolution...
+    */
+   if (QueryPerformanceCounter(&count))
+   {
+      vpr::Int32 top = count.HighPart & _nt_highMask;
+      top = top << (32 - _nt_bitShift);
+      count.LowPart = count.LowPart >> _nt_bitShift;   
+      count.LowPart = count.LowPart + top; 
+      return(PRUint32)count.LowPart;
+   } //else
+   
+    //#if defined(__MINGW32__)
+    //        return time();
+    //#elif defined(WIN16)
+    //        return clock();        /* milliseconds since application start */
+    //#else
+    // return GetTickCount();  /* milliseconds since system start */
+    //#endif
+#else    // Default to POSIX time setting
+   
+   timeval cur_time;
+   vpr::System::gettimeofday(&cur_time);
+   mMicroSeconds = (cur_time.tv_usec + (1000000 * cur_time.tv_sec));
+
+#endif
+}
+
+}; // namespace vpr
+
+
+
 
