b8d879487ee527f25d8f4467c5bb25926f30c1da
Christopher Just <coronax@users.sourceforge.net>
2002-01-11 07:07:30 +0000

    Documentation updates.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7202 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/config/org/vrjuggler/jccl/config/ConfigIOStatus.java
+++ modules/jackal/config/org/vrjuggler/jccl/config/ConfigIOStatus.java
@@ -1,9 +1,52 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+
 
 package VjConfig;
 
 import java.util.List;
 import java.util.ArrayList;
 
+/** Status object for ConfigIO & its parser objects.
+ *  This is used to store various warning & error messages while 
+ *  parsing ConfigChunk or ChunkDesc files.  They can then be 
+ *  conveniently output to the user after parsing completes.
+ *  This allows parsing to continue uninterrupted when minor warnings
+ *  occur, and allows a GUI to display the messages in a user-friendly
+ *  manner.
+ *
+ *  @version $Revision$
+ */
 public class ConfigIOStatus {
 
     List messages;  // either Strings or Throwables.
@@ -22,6 +65,7 @@ public class ConfigIOStatus {
         clear();
     }
 
+    /** Clears messages and resets status to "success". */
     public void clear () {
         messages.clear();
         status = SUCCESS;
@@ -29,6 +73,10 @@ public class ConfigIOStatus {
         num_errors = 0;
     }
 
+    /** Convenience method to set the status.  When adding messages,
+     *  the overall severity can only get worse - ie we progress
+     *  from SUCCESS -> WARNINGS -> ERRORS -> FAILURE.
+     */
     private void setStatus (int new_status) {
         // wish i had an assert
 
@@ -41,10 +89,6 @@ public class ConfigIOStatus {
         return status;
     }
     
-//     public void addMessage (String msg) {
-//         messages.add (msg);
-//     }
-
     public void addWarning (String msg) {
         setStatus (WARNINGS);
         messages.add (msg);
@@ -79,14 +123,19 @@ public class ConfigIOStatus {
         messages.add (msg);
     }
 
+    /** Returns number of messages. */
     public int size() {
         return messages.size();
     }
 
+    /** Returns ith message object.  Messages are either Strings or
+     *  Throwables.
+     */
     public Object get (int i) {
         return messages.get(i);
     }
 
+    /** Provides a one-line summary of # of warnings, errors, etc. */
     public String getSummary () {
         switch (status) {
         case SUCCESS:
@@ -111,6 +160,7 @@ public class ConfigIOStatus {
         }
     }
 
+    /** Creates a string containing the summary and all messages. */
     public String toString () {
         StringBuffer s = new StringBuffer();
         s.append (getSummary());
