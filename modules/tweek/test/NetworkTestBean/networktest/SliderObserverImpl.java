068cdff4011540fdad7acf2bc1dac303280a5270
Patrick Hartling <patrick.hartling@gmail.com>
2002-01-18 17:18:53 +0000

    Added documentation.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7317 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/test/NetworkTestBean/networktest/SliderObserverImpl.java
+++ modules/tweek/test/NetworkTestBean/networktest/SliderObserverImpl.java
@@ -5,6 +5,12 @@ import javax.swing.JSlider;
 import tweek.*;
 
 
+/**
+ * Implementation of the Observer side of the Tweek Subject/Observer pattern.
+ * It must extend tweek.ObserverPOA so that instances of this class can be
+ * registered as CORBA servants.  In addition, CORBA references to the
+ * servants must be capable of being attached to remote subjects.
+ */
 public class SliderObserverImpl extends ObserverPOA
 {
    public SliderObserverImpl (JSlider _slider, SliderSubject _subject)
@@ -13,10 +19,16 @@ public class SliderObserverImpl extends ObserverPOA
       slider_subject = _subject;
    }
 
+   /**
+    * Implements the required method in tweek.ObserverPOA.  The remote subject
+    * will invoke this method whenever it is notified of a change.
+    */
    public void update ()
    {
       System.out.println("Updated from our subject!");
 
+      // If we have a valid slider object, we need to update its value to
+      // whatever our subject has.
       if ( slider != null )
       {
          DefaultBoundedRangeModel model = (DefaultBoundedRangeModel) slider.getModel();
@@ -29,6 +41,10 @@ public class SliderObserverImpl extends ObserverPOA
       System.out.println("Update processed");
    }
 
+   /**
+    * Detaches this observer from our subject.  This is needed when shutting
+    * down a CORBA connection.
+    */
    public void detach ()
    {
       slider_subject.detach(this._this());
