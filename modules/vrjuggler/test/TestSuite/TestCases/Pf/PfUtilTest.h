f1832e036e34fafe7a5147792daba0062fd86063
Patrick Hartling <patrick.hartling@gmail.com>
2002-01-27 00:09:03 +0000

    Updated the test suite to a new build system.  I did not update the vrj::Matrix test cases since it is on the verge of being obsoleted by GMTL's extensive test suite.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7414 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/test/TestSuite/TestCases/Pf/PfUtilTest.h
+++ modules/vrjuggler/test/TestSuite/TestCases/Pf/PfUtilTest.h
@@ -1,17 +1,9 @@
-
 #ifndef VRJ_PFUTILS_TESTS
 #define VRJ_PFUTILS_TESTS
-
-
-#include <iostream>
 
 #include <cppunit/TestCase.h>
 #include <cppunit/TestSuite.h>
 #include <cppunit/TestCaller.h>
-
-#include <vrj/Math/Math.h>
-#include <vrj/Math/Quat.h>
-#include <vrj/Draw/Pf/PfUtil.h>
 
 
 namespace vrjTest
@@ -29,30 +21,7 @@ public:
    virtual ~PfUtilTest()
    {;}
 
-   void testMatrixConversion()
-   {
-         vrj::Matrix mat;
-         mat.postTrans( mat, 1, 2, 3 );
-         mat.postRot( mat, 90, vrj::Vec3( 0, 1, 0 ) );
-
-         pfMatrix pf_mat = vrj::GetPfMatrix( mat );
-         vrj::Matrix vj_mat = vrj::GetVjMatrix( pf_mat );
-
-         /*
-         std::cout << pf_mat << "\n\n" << std::flush;
-         std::cout << vj_mat << "\n\n" << std::flush;
-         std::cout << mat << "\n===============\n" << std::flush;
-         if (!vj_mat.isEqual( mat ))
-         {
-            std::cout << "BAD!!!\n" << std::flush;
-         }  
-         else
-         {
-            std::cout << "GOOD!!!\n" << std::flush;
-         }                
-         */
-         assertTest( vj_mat.isEqual( mat ) && "test pf --> vj matrix conversion failed" );
-   }     
+   void testMatrixConversion();
 
    static CppUnit::Test* suite()
    {
