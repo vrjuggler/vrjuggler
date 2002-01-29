2db4a3d42244bf3ca21aca96b718f52b8888cb54
Patrick Hartling <patrick.hartling@gmail.com>
2002-01-29 00:11:07 +0000

    Beautify according to the coding standard.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@7435 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/vpr/Sync/RWMutex.cpp
+++ juggler/modules/vapor/vpr/Sync/RWMutex.cpp
@@ -47,129 +47,130 @@
 namespace vpr {
 
 /// Acquires a read mutex.
-vpr::ReturnStatus
-RWMutex::acquireRead () {
-    vpr::ReturnStatus status;
+vpr::ReturnStatus RWMutex::acquireRead ()
+{
+   vpr::ReturnStatus status;
 
-    if ( stateLock.acquire().failure() )
-    {
-       status.setCode(vpr::ReturnStatus::Fail);  // Didn't get the lock
-    }
-    else
-    {
-        // let those writers who are waiting have first shot
-        while(refCount < 0 || numWaitingWriters > 0)
-        {
-            numWaitingReaders++;        // Another one waiting
-            waitingReaders.wait();      // So wait until something changes
-            numWaitingReaders--;        //
-        }
-    }
+   if ( stateLock.acquire().failure() )
+   {
+      status.setCode(vpr::ReturnStatus::Fail);  // Didn't get the lock
+   }
+   else
+   {
+      // let those writers who are waiting have first shot
+      while ( refCount < 0 || numWaitingWriters > 0 )
+      {
+         numWaitingReaders++;        // Another one waiting
+         waitingReaders.wait();      // So wait until something changes
+         numWaitingReaders--;        //
+      }
+   }
 
-    if ( status.success() ) {
-        refCount++;
-        stateLock.release();
-    }
+   if ( status.success() )
+   {
+      refCount++;
+      stateLock.release();
+   }
 
-    return status;
+   return status;
 }
 
 /// Acquire a write mutex.
-vpr::ReturnStatus
-RWMutex::acquireWrite() {
-    vpr::ReturnStatus status;
+vpr::ReturnStatus RWMutex::acquireWrite()
+{
+   vpr::ReturnStatus status;
 
-    if ( stateLock.acquire().failure() )
-    {
-       status.setCode(vpr::ReturnStatus::Fail);    // Didn't get the lock
-    }
-    else
-    {
-        while(refCount != 0)        // While there are readers
-        {
-            numWaitingWriters++;    // One more waiting
-            waitingWriters.wait();  // Wait for soemthing to change
-            numWaitingWriters--;    // Not waiting any more
-        }
-    }
+   if ( stateLock.acquire().failure() )
+   {
+      status.setCode(vpr::ReturnStatus::Fail);    // Didn't get the lock
+   }
+   else
+   {
+      while ( refCount != 0 )        // While there are readers
+      {
+         numWaitingWriters++;    // One more waiting
+         waitingWriters.wait();  // Wait for soemthing to change
+         numWaitingWriters--;    // Not waiting any more
+      }
+   }
 
-    if( status.success() )
-    {
-        refCount = -1;      // Tell everyone that there is a writer
-        stateLock.release();
-    }
+   if ( status.success() )
+   {
+      refCount = -1;      // Tell everyone that there is a writer
+      stateLock.release();
+   }
 
-    return status;
+   return status;
 }
 
 /// Tries to acquire a read mutex.
-vpr::ReturnStatus
-RWMutex::tryAcquireRead () {
-    vpr::ReturnStatus status(vpr::ReturnStatus::Fail);
+vpr::ReturnStatus RWMutex::tryAcquireRead ()
+{
+   vpr::ReturnStatus status(vpr::ReturnStatus::Fail);
 
-    if ( stateLock.acquire().success() )
-    {
-        if ( ! (refCount == -1 || numWaitingWriters > 0) )
-        {
-            refCount++;
-            status.setCode(vpr::ReturnStatus::Succeed);
-        }
-        stateLock.release();
-    }
-    return status;
+   if ( stateLock.acquire().success() )
+   {
+      if ( ! (refCount == -1 || numWaitingWriters > 0) )
+      {
+         refCount++;
+         status.setCode(vpr::ReturnStatus::Succeed);
+      }
+      stateLock.release();
+   }
+   return status;
 }
 
 /// Tries to acquire a write mutex.
-vpr::ReturnStatus
-RWMutex::tryAcquireWrite () {
-    vpr::ReturnStatus status(vpr::ReturnStatus::Fail);
+vpr::ReturnStatus RWMutex::tryAcquireWrite ()
+{
+   vpr::ReturnStatus status(vpr::ReturnStatus::Fail);
 
-    if ( stateLock.acquire().success() )
-    {
-        if ( refCount == 0 )
-        {
-            refCount = -1;
-            status.setCode(vpr::ReturnStatus::Fail);
-        }
-        stateLock.release();
-    }
+   if ( stateLock.acquire().success() )
+   {
+      if ( refCount == 0 )
+      {
+         refCount = -1;
+         status.setCode(vpr::ReturnStatus::Fail);
+      }
+      stateLock.release();
+   }
 
-    return status;
+   return status;
 }
 
 /// Releases the mutex.
-vpr::ReturnStatus
-RWMutex::release () {
-    vpr::ReturnStatus status;
+vpr::ReturnStatus RWMutex::release ()
+{
+   vpr::ReturnStatus status;
 
-    if ( stateLock.acquire().failure() )
-        return vpr::ReturnStatus(vpr::ReturnStatus::Fail);
+   if ( stateLock.acquire().failure() )
+      return vpr::ReturnStatus(vpr::ReturnStatus::Fail);
 
-    if(refCount > 0)        // We have a reader to release
-        refCount--;
-    else if (refCount == -1)    // We have writer
-        refCount = 0;
-    else            // We have an error
-        std::cerr << "vpr::RWMutex::release: Should not have refCount of 0!!!"
-                  << std::endl;
+   if ( refCount > 0 )        // We have a reader to release
+      refCount--;
+   else if ( refCount == -1 )    // We have writer
+      refCount = 0;
+   else            // We have an error
+      std::cerr << "vpr::RWMutex::release: Should not have refCount of 0!!!"
+      << std::endl;
 
-    // Preference to writers
-    if (numWaitingWriters > 0)
-    {
-        status = waitingWriters.signal();
-    }
-    else if (numWaitingReaders > 0)
-    {
-        status = waitingReaders.broadcast();
-    }
-    else
-    {
-        status.setCode(vpr::ReturnStatus::Succeed);
-    }
+   // Preference to writers
+   if ( numWaitingWriters > 0 )
+   {
+      status = waitingWriters.signal();
+   }
+   else if ( numWaitingReaders > 0 )
+   {
+      status = waitingReaders.broadcast();
+   }
+   else
+   {
+      status.setCode(vpr::ReturnStatus::Succeed);
+   }
 
-    stateLock.release();
+   stateLock.release();
 
-    return status;
+   return status;
 }
 
-}; // End of vpr namespace
+} // End of vpr namespace
