3c9e93396754a0387dee37e354b3ead162d1a806
Christopher Just <coronax@users.sourceforge.net>
2002-01-29 20:45:16 +0000

    New feature:	Added a static 'resync' method which could theoretically                 be used to synchronize the TimeStamps on two computers.                 The idea is that one side generates a timestamp ts, calls                 ts.usecs() and passes that value to the other side.  The                 other side magically compensates for latency somehow and                 then calls TimeStamp::resync(value).                 This may be useful for Eric's perf monitoring with the                 remote input manager.  Currently only implemented on linux;                 irix version to follow.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7439 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/jccl/Plugins/PerformanceMonitor/TimeStampPosix.h
+++ modules/jackal/jccl/Plugins/PerformanceMonitor/TimeStampPosix.h
@@ -67,6 +67,23 @@ public:
     }
 
 
+    /* ok, we need to resync the timer so that 'now' is 
+     * as close as possible to t.  The argument t is
+     * a value returned by ts.usecs(), probably on some
+     * remote host, and probably modified a little to 
+     * account for latency etc.
+     * Only this method and initialize modify initval. 
+     * Anything calling utime() while this is rewriting
+     * initval could get a bogus value - but since it 
+     * was out of sync the values it was returning
+     * would be bogus anyway.
+     */
+    static void resync(float t) {
+        TimeStampPosix ts; // constructor sets...
+        float diff = t - ts.usecs();
+        initval -= diff; 
+    }
+
 
     TimeStampPosix();
 
