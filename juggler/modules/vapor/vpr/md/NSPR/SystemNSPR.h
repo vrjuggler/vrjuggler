b42de6c3a0b0e64158169dc8680383c8a35e6b9f
Patrick Hartling <patrick.hartling@gmail.com>
2002-02-21 20:36:41 +0000

    Reformatted according to the coding standard.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@7742 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/vpr/md/NSPR/SystemNSPR.h
+++ juggler/modules/vapor/vpr/md/NSPR/SystemNSPR.h
@@ -53,7 +53,7 @@
 #include <vpr/vprConfig.h>
 
 #ifdef VPR_OS_Win32
-#  include <winsock2.h>    /* For struct timeval */
+   #include <winsock2.h>    /* For struct timeval */
 #endif
 
 #include <stdlib.h>
@@ -68,124 +68,134 @@
 #include <vpr/SystemBase.h>
 
 #ifndef HAVE_GETTIMEOFDAY
-#ifndef VPR_OS_Win32
-struct timeval {
-    long tv_sec;           /* seconds since Jan. 1, 1970 */
-    long tv_usec;          /* and microseconds */
+   #ifndef VPR_OS_Win32
+struct timeval
+{
+   long tv_sec;           /* seconds since Jan. 1, 1970 */
+   long tv_usec;          /* and microseconds */
 };
-#endif
+   #endif
 
-struct timezone {
-    int tv_minuteswest;    /* minutes west of Greenwich */
-    int tv_dsttime;        /* type of dst correction */
+struct timezone
+{
+   int tv_minuteswest;    /* minutes west of Greenwich */
+   int tv_dsttime;        /* type of dst correction */
 };
 #else
-#include <sys/time.h>
+   #include <sys/time.h>
 #endif
 
 
 namespace vpr {
 
 class VPR_CLASS_API SystemNSPR
-     : public SystemBase
+: public SystemBase
 {
 public:
-    inline static int
-    gettimeofday (struct timeval* tp, struct timezone* tzp = NULL) {
-        PRTime now = PR_Now();
+   static int gettimeofday (struct timeval* tp, struct timezone* tzp = NULL)
+   {
+      PRTime now = PR_Now();
 
-        tp->tv_sec  = now / 1000000;
-        tp->tv_usec = now % 1000000;
+      tp->tv_sec  = now / 1000000;
+      tp->tv_usec = now % 1000000;
 
-        return 0;
-    }
+      return 0;
+   }
 
-    static vpr::Uint16 Ntohs(vpr::Uint16 conversion)
-    { return PR_ntohs(conversion); }
-    static vpr::Uint32 Ntohl(vpr::Uint32 conversion)
-    { return PR_ntohl(conversion); }
-    static vpr::Uint64 Ntohll(vpr::Uint64 conversion)
-    {
-       vpr::Uint64 ret_val;
-       *((vpr::Uint32*)(&ret_val)) = SystemNSPR::Ntohl(*((vpr::Uint32*)(&conversion)));
-       *( ((vpr::Uint32*)(&ret_val)) + 1) = SystemNSPR::Ntohl( *( ((vpr::Uint32*)(&conversion))+1) );
-       return ret_val;
-    }
-    static vpr::Uint16 Htons(vpr::Uint16 conversion)
-    { return PR_htons(conversion); }
-    static vpr::Uint32 Htonl(vpr::Uint32 conversion)
-    { return PR_htonl(conversion); }
-    static vpr::Uint64 Htonll(vpr::Uint64 conversion)
-    {
+   static vpr::Uint16 Ntohs(vpr::Uint16 conversion)
+   {
+      return PR_ntohs(conversion);
+   }
+   static vpr::Uint32 Ntohl(vpr::Uint32 conversion)
+   {
+      return PR_ntohl(conversion);
+   }
+   static vpr::Uint64 Ntohll(vpr::Uint64 conversion)
+   {
       vpr::Uint64 ret_val;
-       *((vpr::Uint32*)(&ret_val)) = SystemNSPR::Htonl(*((vpr::Uint32*)(&conversion)));
-       *( ((vpr::Uint32*)(&ret_val)) + 1) = SystemNSPR::Htonl( *( ((vpr::Uint32*)(&conversion))+1) );
-       return ret_val;
-    }
+      *((vpr::Uint32*)(&ret_val)) = SystemNSPR::Ntohl(*((vpr::Uint32*)(&conversion)));
+      *( ((vpr::Uint32*)(&ret_val)) + 1) = SystemNSPR::Ntohl( *( ((vpr::Uint32*)(&conversion))+1) );
+      return ret_val;
+   }
+   static vpr::Uint16 Htons(vpr::Uint16 conversion)
+   {
+      return PR_htons(conversion);
+   }
+   static vpr::Uint32 Htonl(vpr::Uint32 conversion)
+   {
+      return PR_htonl(conversion);
+   }
+   static vpr::Uint64 Htonll(vpr::Uint64 conversion)
+   {
+      vpr::Uint64 ret_val;
+      *((vpr::Uint32*)(&ret_val)) = SystemNSPR::Htonl(*((vpr::Uint32*)(&conversion)));
+      *( ((vpr::Uint32*)(&ret_val)) + 1) = SystemNSPR::Htonl( *( ((vpr::Uint32*)(&conversion))+1) );
+      return ret_val;
+   }
 
-    inline static ReturnStatus
-    getenv (const std::string& name, std::string& result) {
-        char* val;
-        ReturnStatus status;
+   static ReturnStatus getenv (const std::string& name, std::string& result)
+   {
+      char* val;
+      ReturnStatus status;
 
-        val = PR_GetEnv(name.c_str());
+      val = PR_GetEnv(name.c_str());
 
-        if ( val != NULL )
-        {
-            result = val;
-            status.setCode(ReturnStatus::Succeed);
-        }
-        else 
-        {
-            status.setCode(ReturnStatus::Fail);
-        }
+      if ( val != NULL )
+      {
+         result = val;
+         status.setCode(ReturnStatus::Succeed);
+      }
+      else
+      {
+         status.setCode(ReturnStatus::Fail);
+      }
 
-        return status;
-    }
+      return status;
+   }
 
-    /* 
-    * If value is "" then it either unsets the variable or clears it
+   /*
+   * If value is "" then it either unsets the variable or clears it
+   */
+   static ReturnStatus setenv (const std::string& name,
+                               const std::string& value)
+   {
+      // NSPR requires form of "name=value"
+      // NSPR takes possesion of the string memory, so we just leak here
+      std::string* set_value = new std::string(name);
+      (*set_value) += "=";
+      (*set_value) += value;
+
+      ReturnStatus status;
+
+      PRStatus ret_val = PR_SetEnv(set_value->c_str());
+
+      if ( ret_val != PR_SUCCESS )
+      {
+         status.setCode(ReturnStatus::Succeed);
+      }
+      else
+      {
+         status.setCode(ReturnStatus::Fail);
+      }
+
+      return status;
+   }
+
+   /** Returns the name of the host
+    * For example the hostname of: vapor.vrjuggler.org is "vapor"
     */
-    inline static ReturnStatus
-    setenv (const std::string& name, const std::string& value)
-    {
-       // NSPR requires form of "name=value"
-       // NSPR takes possesion of the string memory, so we just leak here
-       std::string* set_value = new std::string(name);
-       (*set_value) += "=";
-       (*set_value) += value;
+   static std::string getHostname()
+   {
+      char  hn_buf[SYS_INFO_BUFFER_LENGTH];
 
-        ReturnStatus status;
-
-        PRStatus ret_val = PR_SetEnv(set_value->c_str());
-
-        if ( ret_val != PR_SUCCESS )
-        {
-            status.setCode(ReturnStatus::Succeed);
-        }
-        else 
-        {
-            status.setCode(ReturnStatus::Fail);
-        }
-
-        return status;
-    }
-
-    /** Returns the name of the host
-     * For example the hostname of: vapor.vrjuggler.org is "vapor"
-     */
-    static std::string getHostname()
-    {
-       char  hn_buf[SYS_INFO_BUFFER_LENGTH];
-
-       //PR_SI_HOSTNAME 
-       //PR_SI_SYSNAME
-       PRStatus ret_val = PR_GetSystemInfo( PR_SI_HOSTNAME, hn_buf, SYS_INFO_BUFFER_LENGTH );
-       if(ret_val == PR_SUCCESS)
-          return std::string(hn_buf);
-       else
-          return std::string("<hostname-lookup failed>");               
-    }    
+      //PR_SI_HOSTNAME
+      //PR_SI_SYSNAME
+      PRStatus ret_val = PR_GetSystemInfo( PR_SI_HOSTNAME, hn_buf, SYS_INFO_BUFFER_LENGTH );
+      if ( ret_val == PR_SUCCESS )
+         return std::string(hn_buf);
+      else
+         return std::string("<hostname-lookup failed>");
+   }
 };
 
 }; // End of vpr namespace
