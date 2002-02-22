c3e0ceb1c0d1bb99e215e41594c35cb38b604870
Josh Brown <browner10@users.sourceforge.net>
2002-02-22 10:30:37 +0000

    New feature: Refactor: Bug fixed: SF Bug#:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    Changed the write call to write the correct number of bytes, not just the
    strings length.  Hopefully this saves future testers time.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7770 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/test/SerialPort/writer.cpp
+++ modules/vapor/test/SerialPort/writer.cpp
@@ -49,21 +49,24 @@ int main (int argc, char* argv[])
 
    if ( write_port->open().success() )
    {
-      char* buffer = "This is a test...\n";
+      char* buffer = "IT WORKS";
       vpr::Uint32 bytes;
       vpr::Uint16 size;
 
-      std::cout << "Port opened\n";
 
-      if ( write_port->getBufferSize(size).success() )
-      {
-         std::cout << "Min buffer size: " << size << std::endl;
+      if(write_port->getBufferSize(size).success()){
+        std::cout << "Min buffer size: " << size << std::endl;
       }
+
+      std::cout << sizeof(buffer)*strlen(buffer) << std::endl;
+
+      std::cout << "Port opened\n ";
 
       write_port->disableCanonicalInput();
       write_port->setUpdateAction(vpr::SerialTypes::NOW);
+
       write_port->setCharacterSize(vpr::SerialTypes::CS_BITS_8);
-      write_port->write(buffer, strlen(buffer) + 1, bytes);
+      write_port->write(buffer, sizeof(buffer)*strlen(buffer), bytes);
       write_port->flushQueue(vpr::SerialTypes::IO_QUEUES);
       std::cout << "Wrote " << bytes << " bytes to " << argv[1] << std::endl;
    }
