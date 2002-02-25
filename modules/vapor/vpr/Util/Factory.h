db2a3828647822e923f30c33e76dbb01b23db732
Allen Bierbaum <abierbaum@gmail.com>
2002-02-25 01:50:49 +0000

    New feature:	Added Factory Pattern template class
    Added Factory pattern template class similar to the design of the one in "Modern C++ Design".
    Added test cases for the Factory pattern.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@7785 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/Util/Factory.h
@@ -0,0 +1,82 @@
+#ifndef _VPR_FACTORY_H
+#define _VPR_FACTORY_H
+
+
+namespace vpr
+{
+
+template <class IdentifierType, class AbstractProduct>
+struct NullFactoryError
+{
+   static AbstractProduct* onUnknownType(IdentifierType)
+   {
+      return NULL;
+   }
+};
+
+/** Implements a useful little template function usable as a Creator in factory */
+template<class AbstractProduct, class ConcreteProduct>
+AbstractProduct* CreateProduct()
+{ return (new ConcreteProduct); }
+
+/** Implements generic Factory pattern
+*
+* AbstractProduct: The base class for the hierarchy for the object factory
+* IndentifierType: The id for indexing the creators (must be sortable)
+* ProductCreator: The callable entity that creates objects.
+*        Must support:
+*           AbstractProduct* operator();
+*        (ex: functions, functors, classes)
+*        default: Simple function of type: AbstractProduct* func() { }
+* FactoryErrorPolicy: The handler for failed lookups.
+*        Must support:
+*           FactoryErrorImpl<IdentifierType, AbstractProduct> fErrorImpl;
+*           AbstractProduct* p = fErrorImpl.onUnknownType(id)
+*/
+template<
+   class AbstractProduct,
+   class IdentifierType,
+   typename ProductCreator = AbstractProduct* (*)(),
+   template<typename, class>
+      class FactoryErrorPolicy = NullFactoryError
+>
+class Factory : public FactoryErrorPolicy<IdentifierType, AbstractProduct>
+{
+public:
+   bool registerCreator(const IdentifierType& id, ProductCreator creator)
+   {
+      return mCreatorMap.insert( CreatorMap::value_type(id,creator)).second;
+   }
+
+   bool unregisterCreator(const IdentifierType& id)
+   {
+      return (mCreatorMap.erase(id) == 1);   // return (num_erased == 1)
+   }
+   bool isRegistered(const IdentifierType& id)
+   {
+      return ( mCreatorMap.find(id) != mCreatorMap.end());
+   }
+
+   AbstractProduct* createObject(const IdentifierType& id)
+   {
+      typename CreatorMap::const_iterator i = mCreatorMap.find(id);
+      if(i != mCreatorMap.end())
+      {
+         return (i->second)();
+      }
+
+      return onUnknownType(id);     // Calls template method from FactoryErrorPolicy<>
+   }
+
+
+
+protected:
+   typedef std::map<IdentifierType, ProductCreator> CreatorMap;
+   CreatorMap mCreatorMap;
+};
+
+}; // namespace vpr
+
+
+#endif
+
