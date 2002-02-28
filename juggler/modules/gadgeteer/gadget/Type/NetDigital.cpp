15a05c27119ff7eae201a1da979ab87266c176aa
Eric Olson <eolson@users.sourceforge.net>
2002-02-28 16:28:35 +0000

    Removed remote input code that is no longer necessary after new SampleBuffer updates.  Also continued to move common code into the NetInput class, and made some minor code clean-up changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@7823 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/gadget/Type/NetDigital.cpp
+++ juggler/modules/gadgeteer/gadget/Type/NetDigital.cpp
@@ -5,13 +5,9 @@
 namespace gadget{
 
 // constructor for a transmitting network device
-NetDigital::NetDigital(const std::string& src_device_name, Input* input_ptr, VJ_NETID_TYPE local_device_id, VJ_NETID_TYPE rmt_device_id) : NetInput(src_device_name, input_ptr){
+NetDigital::NetDigital(const std::string& src_device_name, Input* input_ptr, VJ_NETID_TYPE local_device_id, VJ_NETID_TYPE rmt_device_id) : NetInput(src_device_name, input_ptr, local_device_id, rmt_device_id){
 
    vprDEBUG(vrjDBG_INPUT_MGR, vprDBG_CONFIG_LVL) << "NetDigital: local_device_id = " << local_device_id << std::endl << vprDEBUG_FLUSH;
-
-  mLocalId = local_device_id;
-  mRemoteId = rmt_device_id;
-  mDeviceName = src_device_name;
 
   if(src_device_name.length() > 0){   // pointing to another device/proxy for data source
      mLocalSource.init(mDeviceName);
@@ -33,25 +29,21 @@ NetDigital::NetDigital(const std::string& src_device_name, Input* input_ptr, VJ_
 }
 
 // constructor for a transmitting network proxy
-NetDigital::NetDigital(const std::string& src_device_name, Proxy* proxy_ptr, VJ_NETID_TYPE local_device_id, VJ_NETID_TYPE rmt_device_id) : NetInput(src_device_name, proxy_ptr){
-   mLocalId = local_device_id;
-   mRemoteId = rmt_device_id;
-   mDeviceName = src_device_name;
+NetDigital::NetDigital(const std::string& src_device_name, Proxy* proxy_ptr, VJ_NETID_TYPE local_device_id, VJ_NETID_TYPE rmt_device_id) : NetInput(src_device_name, proxy_ptr, local_device_id, rmt_device_id){
    mLocalSource.init(mDeviceName);
    mSendBuffer.resize(5);
    mNetworkShortDigitalValues.resize(1);  // a digital proxy only points to a single data item
 }
 
 // constructor for a receiving NetInput
-NetDigital::NetDigital(jccl::ConfigChunkPtr chunk, VJ_NETID_TYPE local_device_id) : NetInput(chunk) {
-   mLocalId = local_device_id;
-   mRemoteId = 0;
+NetDigital::NetDigital(jccl::ConfigChunkPtr chunk, VJ_NETID_TYPE local_device_id) : NetInput(chunk, local_device_id) {
    mSendBuffer.resize(1); // not used;
    mNetworkShortDigitalValues.resize(1);  // a digital proxy only points to a single data item
    mSendBuffer.resize(3 + sizeof(short) * mNetworkShortDigitalValues.size()); // buffer not used when receiving, but its size (5) is currently used to determine how much data to pull from the recv buffer
 }
 
 void NetDigital::updateFromLocalSource(){
+   vprASSERT(mNetworkShortDigitalValues.size() == 1);  // currently, only single digital can be transmitted at a time 
    std::vector<DigitalData>  digital_data_sample(mNetworkShortDigitalValues.size());
 
    // next 3 lines should only be executed when using mLocalSource (remote Proxies, not devices)
@@ -74,7 +66,7 @@ void NetDigital::updateFromLocalSource(){
       mNetworkShortDigitalValues[k] = htons( (short)digital_data_sample[k].getDigital());           // convert to network format
    }
 
-   shortTo2Bytes((char*)(&(mSendBuffer[0])), htons(mRemoteId));  // put the 2 byte id in the buffer
+   ushortTo2Bytes((char*)(&(mSendBuffer[0])), htons(mRemoteId));  // put the 2 byte id in the buffer
 
    unsigned int j = 0;
    for( ; j < mNetworkShortDigitalValues.size(); j++){
@@ -82,8 +74,7 @@ void NetDigital::updateFromLocalSource(){
          mSendBuffer[2 + sizeof(short)*j + i] = ((char*)(& (mNetworkShortDigitalValues[j]) ))[i];   // copy the number byte by byte
    }
 
-   // mSendBuffer[4] = ';';                                             // terminate the message
-   mSendBuffer[2 + sizeof(short) * j] = ';';
+   mSendBuffer[2 + sizeof(short) * j] = ';';                            // terminate the message
 
 }
 
--- juggler/modules/gadgeteer/gadget/Type/NetInput.h
+++ juggler/modules/gadgeteer/gadget/Type/NetInput.h
@@ -4,7 +4,7 @@
 #include <gadget/gadgetConfig.h>
 #include <jccl/Config/ConfigChunkPtr.h>
 #include <gadget/Type/Input.h>
-#include <gadget/Type/NetData.h>
+#include <gadget/RemoteInputManager/NetUtils.h>
 
 namespace gadget{
 
@@ -12,37 +12,36 @@ class Proxy;
 
 class GADGET_CLASS_API NetInput : public Input{
 protected:
-    std::string mSrcName;
-    int num_dependencies;
-    NetData* mData;    // can be removed when NetPosition is updated to SampleBuffers design
+    std::string mSrcName;  // name of the device/proxy on the remote computer (we're receiving its data)
+    int mNumDependencies;
     bool mAckInitialization;
     bool mWaitingForData;
     bool mNeedToResendRequest;
-    std::string mDeviceName;
+    std::string mDeviceName;  // name of device/proxy on local computer (we're xmitting its data) 
     VJ_NETID_TYPE mLocalId;  // unsigned short
     VJ_NETID_TYPE mRemoteId;
     std::vector<char> mSendBuffer;  
    
    // constructor used when a device has been requested from us (and remote id has been given)
-   NetInput(const std::string& device_name, Input* input_ptr);
+   NetInput(const std::string& src_device_name, Input* input_ptr, VJ_NETID_TYPE local_device_id, VJ_NETID_TYPE rmt_device_id);
 
    // constructor used when a device has been requested from us (and remote id has been given)
-   NetInput(const std::string& device_name, Proxy* proxy_ptr);
+   NetInput(const std::string& src_device_name, Proxy* proxy_ptr, VJ_NETID_TYPE local_device_id, VJ_NETID_TYPE rmt_device_id);
 
    // constructor used when we are going to request a remote device and receive data from it.
    //    a Proxy in the input manager will point to a NetInput in the Remote Input Manager.
    //    the NetInput will have "_NET_" appended to its name.
-   NetInput(jccl::ConfigChunkPtr chunk);
+   NetInput(jccl::ConfigChunkPtr chunk, VJ_NETID_TYPE local_device_id);
 
 public:
 
-    virtual ~NetInput(){;} // derived classes allocated mData, so they also delete mData
+    virtual ~NetInput(){;} 
 
     // The following two functions help us keep track if this net_input is being used.
     // We just count them so we'll know when it's not being used anymore -- there's  
        // currently no reason to store pointers to each dependency, 
-    void addDependency(){ num_dependencies++; }
-    void removeDependency(){ num_dependencies--; }
+    void addDependency(){ mNumDependencies++; }
+    void removeDependency(){ mNumDependencies--; }
 
     void setWasInitializedTrue(){ mAckInitialization = true; }
     bool getWasInitialized() const { return mAckInitialization; }
@@ -53,13 +52,13 @@ public:
 
     std::string getSourceName() const { return mSrcName; }
 
-    virtual void updateFromLocalSource(){ mData->updateFromLocalSource(); }
-    virtual void updateFromRemoteSource(char* recv_buffer, int recv_buff_len){ mData->updateFromRemoteSource(recv_buffer, recv_buff_len); }
-    virtual char* getDataPtr(){ return mData->getDataPtr(); }
-    virtual int getDataByteLength() const { return mData->getDataByteLength(); }
-    virtual VJ_NETID_TYPE getLocalId() const { return mData->getLocalId(); }
-    virtual VJ_NETID_TYPE getRemoteId() const { return mData->getRemoteId(); }
-    virtual void setRemoteId(VJ_NETID_TYPE remote_id){ mData->setRemoteId(remote_id); }
+    virtual void updateFromLocalSource(){;}
+    virtual void updateFromRemoteSource(char* recv_buffer, int recv_buff_len){;} 
+    virtual char* getDataPtr(){  return (char*) &(mSendBuffer[0]); }
+    virtual int getDataByteLength() const { return mSendBuffer.size(); }
+    virtual VJ_NETID_TYPE getLocalId() const { return mLocalId; }
+    virtual VJ_NETID_TYPE getRemoteId() const { return mRemoteId; }
+    virtual void setRemoteId(VJ_NETID_TYPE remote_id){ mRemoteId = remote_id; }
 
     int sample(){return 1;}
     int startSampling(){return 1;}
