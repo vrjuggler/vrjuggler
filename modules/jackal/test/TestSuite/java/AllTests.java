1562c2777e6963bf21925c7f5b08bc6101ed17c2
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-03-19 17:25:12 +0000

    Beginning to add tests for VjControl.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8037 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/jackal/test/TestSuite/java/AllTests.java
@@ -0,0 +1,26 @@
+import junit.framework.*;
+import test.*;
+
+/**
+ * This is the main class used to drive the VjControl Java test suite. Its
+ * static suite() method adds all the tests currently available.
+ */
+public class AllTests
+{
+   public static void main( String[] args )
+   {
+      junit.swingui.TestRunner.run( AllTests.class );
+   }
+
+   /**
+    * Adds all the tests currently available.
+    */
+   public static Test suite()
+   {
+      TestSuite suite = new TestSuite( "All VjControl JUnit Tests" );
+
+      suite.addTestSuite( ValTypeTest.class );
+
+      return suite;
+   }
+}
---
+++ modules/jackal/test/TestSuite/java/test/ValTypeTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import junit.framework.*;
+import VjConfig.*;
+
+/**
+ * Tests the ValType class.
+ */
+public class ValTypeTest
+   extends TestCase
+{
+   public ValTypeTest( String name )
+   {
+      super( name );
+   }
+
+   public void testToString()
+   {
+      assertEquals( ValType.STRING.toString(),           "String" );
+      assertEquals( ValType.FLOAT.toString(),            "Float" );
+      assertEquals( ValType.INT.toString(),              "Int" );
+      assertEquals( ValType.BOOL.toString(),             "Bool" );
+      assertEquals( ValType.CHUNK.toString(),            "Chunk" );
+      assertEquals( ValType.EMBEDDEDCHUNK.toString(),    "EmbeddedChunk" );
+      assertEquals( ValType.INVALID.toString(),          "Invalid" );
+   }
+
+   public void testGetValType()
+   {
+      assertEquals( ValType.getValType("string"),        ValType.STRING );
+      assertEquals( ValType.getValType("float"),         ValType.FLOAT );
+      assertEquals( ValType.getValType("int"),           ValType.INT );
+      assertEquals( ValType.getValType("bool"),          ValType.BOOL );
+      assertEquals( ValType.getValType("chunk"),         ValType.CHUNK );
+      assertEquals( ValType.getValType("embeddedchunk"), ValType.EMBEDDEDCHUNK );
+      assertEquals( ValType.getValType("invalid"),       ValType.INVALID );
+   }
+}
