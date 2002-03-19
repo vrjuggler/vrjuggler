a38e4ff03f65f19f78c7250c846dd10fdc09f65f
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-03-19 17:28:49 +0000

    Added tests for PropertyDesc.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8040 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/jackal/test/TestSuite/java/test/PropertyDescTest.java
@@ -0,0 +1,224 @@
+package test;
+
+import java.util.ArrayList;
+import junit.framework.*;
+import VjConfig.*;
+
+/**
+ * Test the PropertyDesc class.
+ */
+public class PropertyDescTest
+   extends TestCase
+{
+   public PropertyDescTest( String name )
+   {
+      super( name );
+   }
+
+   /**
+    * Tests get/setName() methods.
+    */
+   public void testName()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setName( "JimmyJohns" );
+      assertEquals( d.getName(), "JimmyJohns" );
+
+      d = new PropertyDesc();
+      d.setName( null );
+      assertNull( d.getName() );
+   }
+
+   /**
+    * Tests get/setToken() methods.
+    */
+   public void testToken()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setToken( "SomeToken" );
+      assertEquals( d.getToken(), "SomeToken" );
+
+      d = new PropertyDesc();
+      d.setToken( null );
+      assertNull( d.getToken() );
+   }
+
+   /**
+    * Tests get/setHelp() methods.
+    */
+   public void testHelp()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setHelp( "Help me test successfully!" );
+      assertEquals( d.getHelp(), "Help me test successfully!" );
+
+      d = new PropertyDesc();
+      d.setHelp( null );
+      assertNull( d.getHelp() );
+   }
+
+   /**
+    * Tests get/setValType() methods.
+    */
+   public void testValType()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setValType( ValType.STRING );
+      assertEquals( d.getValType(), ValType.STRING );
+
+      d = new PropertyDesc();
+      d.setValType( null );
+      assertNull( d.getValType() );
+   }
+
+   /**
+    * Tests get/setHasVariableNumberOfValues() methods.
+    */
+   public void testHasVariableNumberOfValues()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setHasVariableNumberOfValues( true );
+      assertEquals( d.getHasVariableNumberOfValues(), true );
+
+      d = new PropertyDesc();
+      d.setHasVariableNumberOfValues( false );
+      assertEquals( d.getHasVariableNumberOfValues(), false );
+   }
+
+   /**
+    * Tests get/setNumValues() methods.
+    */
+   public void testNumValues()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setNumValues( 20 );
+      assertEquals( d.getNumValues(), 20 );
+
+      d = new PropertyDesc();
+      d.setNumValues( -1 );
+      assertEquals( d.getNumValues(), -1 );
+   }
+
+   /**
+    * Tests get/setUserLevel() methods.
+    */
+   public void testUserLevel()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setUserLevel( 1 );
+      assertEquals( d.getUserLevel(), 1 );
+
+      d = new PropertyDesc();
+      d.setUserLevel( 100 );
+      assertEquals( d.getUserLevel(), 100 );
+   }
+
+   /**
+    * Tests value label operations.
+    */
+   public void testValueLabels()
+   {
+      PropertyDesc d = new PropertyDesc();
+      String[] labelArray = null;
+      
+      // should initially be empty
+      assertEquals( d.getValueLabelsSize(), 0 );
+      labelArray = d.getValueLabels();
+      assertEquals( labelArray.length, 0 );
+
+      // request a label outside our range
+      assertEquals( d.getValueLabel(0), "" );
+
+      // add a label
+      d.appendValueLabel( "SomeLabel" );
+      assertEquals( d.getValueLabelsSize(), 1 );
+      assertEquals( d.getValueLabel(0), "SomeLabel" );
+      labelArray = d.getValueLabels();
+      assertEquals( labelArray[0], "SomeLabel" );
+
+      // add another label
+      d.appendValueLabel( "AnotherLabel" );
+      assertEquals( d.getValueLabelsSize(), 2 );
+      assertEquals( d.getValueLabel(0), "SomeLabel" );
+      assertEquals( d.getValueLabel(1), "AnotherLabel" );
+      labelArray = d.getValueLabels();
+      assertEquals( labelArray[0], "SomeLabel" );
+      assertEquals( labelArray[1], "AnotherLabel" );
+
+      // replace the labels
+      ArrayList list = new ArrayList();
+      list.add( "Me" );
+      list.add( "Myself" );
+      list.add( "I" );
+      d.setValueLabels( list );
+      assertEquals( d.getValueLabelsSize(), 3 );
+      assertEquals( d.getValueLabel(0), "Me" );
+      assertEquals( d.getValueLabel(1), "Myself" );
+      assertEquals( d.getValueLabel(2), "I" );
+      labelArray = d.getValueLabels();
+      assertEquals( labelArray[0], "Me" );
+      assertEquals( labelArray[1], "Myself" );
+      assertEquals( labelArray[2], "I" );
+
+      // replace labels with empty list
+      list = new ArrayList();
+      d.setValueLabels( list );
+      assertEquals( d.getValueLabelsSize(), 0 );
+      assertEquals( d.getValueLabel(0), "" );
+      labelArray = d.getValueLabels();
+      assertEquals( labelArray.length, 0 );
+   }
+
+   /**
+    * Tests DescEnum operations.
+    */
+   public void testEnumerations()
+   {
+      PropertyDesc d = new PropertyDesc();
+      d.setValType( ValType.STRING );
+      DescEnum[] deArray = null;
+
+      // should initially be empty
+      assertEquals( d.getEnumerationsSize(), 0 );
+      deArray = d.getEnumerations();
+      assertEquals( deArray.length, 0 );
+
+      // add an enum
+      d.appendEnumeration( "Enum1", "Val1" );
+      assertEquals( d.getEnumerationsSize(), 1 );
+      assertTrue( d.getEnumAtIndex(0).equals(new DescEnum("Enum1", new VarValue("Val1"))) );
+      deArray = d.getEnumerations();
+      assertTrue( deArray[0].equals(new DescEnum("Enum1", new VarValue("Val1"))) );
+
+      // add another enum
+      d.appendEnumeration( "Enum2", "Val2" );
+      assertEquals( d.getEnumerationsSize(), 2 );
+      assertTrue( d.getEnumAtIndex(0).equals(new DescEnum("Enum1", new VarValue("Val1"))) );
+      assertTrue( d.getEnumAtIndex(1).equals(new DescEnum("Enum2", new VarValue("Val2"))) );
+      deArray = d.getEnumerations();
+      assertTrue( deArray[0].equals(new DescEnum("Enum1", new VarValue("Val1"))) );
+      assertTrue( deArray[1].equals(new DescEnum("Enum2", new VarValue("Val2"))) );
+
+      // replace the enums
+      ArrayList list = new ArrayList();
+      list.add( new DescEnum("Me", new VarValue("Me")) );
+      list.add( new DescEnum("Myself", new VarValue("Myself")) );
+      list.add( new DescEnum("I", new VarValue("I")) );
+      d.setEnumerations( list );
+      assertEquals( d.getEnumerationsSize(), 3 );
+      assertTrue( d.getEnumAtIndex(0).equals(new DescEnum("Me", new VarValue("Me"))) );
+      assertTrue( d.getEnumAtIndex(1).equals(new DescEnum("Myself", new VarValue("Myself"))) );
+      assertTrue( d.getEnumAtIndex(2).equals(new DescEnum("I", new VarValue("I"))) );
+      deArray = d.getEnumerations();
+      assertTrue( deArray[0].equals(new DescEnum("Me", new VarValue("Me"))) );
+      assertTrue( deArray[1].equals(new DescEnum("Myself", new VarValue("Myself"))) );
+      assertTrue( deArray[2].equals(new DescEnum("I", new VarValue("I"))) );
+
+      // replace with the empty list
+      list = new ArrayList();
+      d.setEnumerations( list );
+      assertEquals( d.getEnumerationsSize(), 0 );
+      deArray = d.getEnumerations();
+      assertEquals( deArray.length, 0 );
+   }
+}
