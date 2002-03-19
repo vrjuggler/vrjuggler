89732c032b434be796f46d9b02500c7d4a013e19
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-03-19 17:28:04 +0000

    Added tests for DescEnum.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8039 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/jackal/test/TestSuite/java/test/DescEnumTest.java
@@ -0,0 +1,80 @@
+package test;
+
+import junit.framework.*;
+import VjConfig.*;
+
+/**
+ * Test the DescEnum class.
+ */
+public class DescEnumTest
+   extends TestCase
+{
+   public DescEnumTest( String name )
+   {
+      super( name );
+   }
+
+   /**
+    * Test the toString representation.
+    */
+   public void testToString()
+   {
+      DescEnum de = new DescEnum( "ShoeSize", new VarValue(13) );
+      assertEquals( de.toString(), "ShoeSize=13" );
+
+      // null value case
+      de.val = null;
+      assertEquals( de.toString(), "ShoeSize" );
+   }
+
+   /**
+    * Tests the copy constructor.
+    */
+   public void testCopyConstructor()
+   {
+      DescEnum orig = new DescEnum( "PizzaTopping", new VarValue("Pepperoni") );
+      DescEnum copy = new DescEnum( orig );
+      assertEquals( orig.str, copy.str );
+//      assertTrue( orig.str != copy.str );
+      assertTrue( orig.val.equals(copy.val) );
+      assertTrue( orig.val != copy.val );
+   }
+
+   /**
+    * Tests the clone() method.
+    */
+   public void testClone()
+   {
+      DescEnum orig = new DescEnum( "Display", new VarValue(455) );
+      DescEnum clone = null;
+      try
+      {
+         clone = (DescEnum)orig.clone();
+      }
+      catch ( CloneNotSupportedException cnse )
+      {
+         assertTrue( cnse.getMessage(), false );
+      }
+      assertEquals( orig.str, clone.str );
+//      assertTrue( orig.str != clone.str );
+      assertTrue( orig.val.equals(clone.val) );
+      assertTrue( orig.val != clone.val );
+   }
+
+   /**
+    * Tests the equals() method.
+    */
+   public void testEquals()
+   {
+      DescEnum de1 = new DescEnum( "Pipe", new VarValue(1) );
+      DescEnum de2 = new DescEnum( "Pipe", new VarValue(1) );
+      assertTrue( de1.equals(de2) );
+      de2.str = "Display";
+      assertTrue( ! de1.equals(de2) );
+      de2.str = "Pipe";
+      de2.val.set(2);
+      assertTrue( ! de1.equals(de2) );
+      de2.val = new VarValue( 1.0f );
+      assertTrue( ! de1.equals(de2) );
+   }
+}
