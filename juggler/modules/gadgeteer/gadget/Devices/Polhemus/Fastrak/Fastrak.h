890d26adca2b5dcb4e9e637f549fc50ee68da8d4
Patrick Hartling <patrick.hartling@gmail.com>
2002-03-20 17:44:30 +0000

    Reformatted according to the coding standard (via Visual SlickEdit). This should not have changed any of the code, but I did add braces in many places where they were missing (as proscribed by the coding standard).  There were at least one or two places where some compilers would have complained about "ambiguous else" problems.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8059 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/gadget/Devices/Polhemus/Fastrak/Fastrak.h
+++ juggler/modules/gadgeteer/gadget/Devices/Polhemus/Fastrak/Fastrak.h
@@ -38,8 +38,8 @@
 
 // 4 stations: from 0 to 3
 
-#ifndef _VJ_FASTRACK_
-#define _VJ_FASTRACK_
+#ifndef _GADGET_FASTRACK_
+#define _GADGET_FASTRACK_
 
 #include <vjConfig.h>
 
@@ -59,31 +59,37 @@
 class vjFastrack : public vjPosition, public vjDigital
 {
 public:
-  vjFastrack();                                  // must call vjAnalog()? see vjAnalog.h
-  virtual ~vjFastrack();
-  static std::string getChunkType() { return std::string("Fastrack"); }// return what chunk type is associated with this class.
+   vjFastrack();                                  // must call vjAnalog()? see vjAnalog.h
+   virtual ~vjFastrack();
+   static std::string getChunkType()
+   {
+      return std::string("Fastrack");
+   }// return what chunk type is associated with this class.
 public:
-  //:vjInput pure virtual functions
-  virtual bool config( vjConfigChunk *fastrackChunk);
-  virtual int startSampling();                 // start a new thread
-  virtual int sample();                        // read data from the fastrack
-  virtual void updateData();                   // swap the data and vjInput::tri-buffered indicies
-  virtual int stopSampling();                  // kills off the sample thread
-  virtual char* getDeviceName() { return "vjFastrack"; };// device's configchunk name (match .desc file)
+   //:vjInput pure virtual functions
+   virtual bool config( vjConfigChunk *fastrackChunk);
+   virtual int startSampling();                 // start a new thread
+   virtual int sample();                        // read data from the fastrack
+   virtual void updateData();                   // swap the data and vjInput::tri-buffered indicies
+   virtual int stopSampling();                  // kills off the sample thread
+   virtual char* getDeviceName()
+   {
+      return "vjFastrack";
+   };// device's configchunk name (match .desc file)
 public:
-  //:vjDigital pure virtual function
-  virtual int getDigitalData(int station = 0);
-  //:vjPosition pure virtual function
-  virtual vjMatrix* getPosData(int station = 0);
+   //:vjDigital pure virtual function
+   virtual int getDigitalData(int station = 0);
+   //:vjPosition pure virtual function
+   virtual vjMatrix* getPosData(int station = 0);
 private:
-  static void threadedSampleFunction(void* classPointer);
-  int buttonState[3];                   // only one button on station 0
-  float trackersPosition[4][3][3];      // 4 stations, 3 coordinates, 3 buffers
-  float trackersOrientation[4][3][3];   // 4 stations, 3 orientations, 3 buffers
-  vjThread *sampleThread;
+   static void threadedSampleFunction(void* classPointer);
+   int buttonState[3];                   // only one button on station 0
+   float trackersPosition[4][3][3];      // 4 stations, 3 coordinates, 3 buffers
+   float trackersOrientation[4][3][3];   // 4 stations, 3 orientations, 3 buffers
+   vjThread *sampleThread;
 
-  char *configFile;
+   char *configFile;
 };
 
 
-#endif // _VJ_FASTRACK_
+#endif // _GADGET_FASTRACK_
