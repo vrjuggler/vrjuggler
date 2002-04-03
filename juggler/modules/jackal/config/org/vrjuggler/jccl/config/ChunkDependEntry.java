139d4fee7364f6d4c1c6e4ae033f776b57cfcae3
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-03 22:56:05 +0000

    Style conventions
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8120 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/config/org/vrjuggler/jccl/config/ChunkDependEntry.java
+++ juggler/modules/jackal/config/org/vrjuggler/jccl/config/ChunkDependEntry.java
@@ -29,31 +29,31 @@
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
-
-
-
 package VjConfig;
 
 import VjConfig.ConfigChunk;
 import VjConfig.Property;
 import java.util.Vector;
 
-public class ChunkDependEntry {
+public class ChunkDependEntry
+{
+   public ConfigChunk chunk;
+   public Vector propdepends; // vector of PropDependEntry
 
-    public ConfigChunk chunk;
-    public Vector propdepends; // vector of PropDependEntry
+   public ChunkDependEntry()
+   {
+      propdepends = new Vector();
+   }
 
-    public ChunkDependEntry() {
-	propdepends = new Vector();
-    }
-
-    public String toString() {
-	int i;
-	PropDependEntry pd;
-	String s = "Chunk \"" + chunk.getName() + "\"\n";
-	for (i = 0; i < propdepends.size(); i++)
-	    s += ((PropDependEntry)propdepends.elementAt(i)).toString();
-	return s;
-    }
-
+   public String toString()
+   {
+      int i;
+      PropDependEntry pd;
+      String s = "Chunk \"" + chunk.getName() + "\"\n";
+      for (i = 0; i < propdepends.size(); i++)
+      {
+         s += ((PropDependEntry)propdepends.elementAt(i)).toString();
+      }
+      return s;
+   }
 }
