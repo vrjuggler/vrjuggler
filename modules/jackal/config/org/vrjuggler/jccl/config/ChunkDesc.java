41a3c58b91a6768f0ee06834e1e49f4b9cff0a60
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-03 23:47:16 +0000

    Style conventions.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8126 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/config/org/vrjuggler/jccl/config/ChunkDesc.java
+++ modules/jackal/config/org/vrjuggler/jccl/config/ChunkDesc.java
@@ -43,239 +43,261 @@ import org.w3c.dom.*;
 
 import VjConfig.PropertyDesc;
 
-public class ChunkDesc implements Cloneable {
-    
-    private List props;
-    public String name;
-    public String token;
-    public String help;
+public class ChunkDesc
+   implements Cloneable
+{
+   private List props;
+   public String name;
+   public String token;
+   public String help;
 
-    private ConfigChunk default_chunk;
-    private Node default_node;
+   private ConfigChunk default_chunk;
+   private Node default_node;
 
-    public ChunkDesc (String n) {
-        props = new Vector();
-        addNameProperty();
-        name = n;
-        token = n;
-        help = "";
-        default_node = null;
-        default_chunk = null;
-    }
+   public ChunkDesc(String n)
+   {
+      props = new Vector();
+      addNameProperty();
+      name = n;
+      token = n;
+      help = "";
+      default_node = null;
+      default_chunk = null;
+   }
 
+   public ChunkDesc()
+   {
+      props = new Vector();
+      addNameProperty();
+      name = "";
+      token = "";
+      help = "";
+      default_node = null;
+      default_chunk = null;
+   }
 
-    public ChunkDesc () {
-        props = new Vector();
-        addNameProperty();
-        name = "";
-        token = "";
-        help = "";
-        default_node = null;
-        default_chunk = null;
-    }
+   public Object clone()
+   {
+      try
+      {
+         ChunkDesc d = (ChunkDesc)super.clone();
+         d.props = new Vector();
+         int n = props.size();
+         for (int i = 0; i < n; i++)
+         {
+            d.props.add(((PropertyDesc)props.get(i)).clone());
+         }
+         return d;
+      }
+      catch (CloneNotSupportedException e)
+      {
+         return null;
+      }
+   }
 
+   public final String getName()
+   {
+      return name;
+   }
 
-    public Object clone () {
-        try {
-            ChunkDesc d = (ChunkDesc)super.clone();
-            d.props = new Vector();
-            int n = props.size();
-            for (int i = 0; i < n; i++) {
-                d.props.add (((PropertyDesc)props.get(i)).clone());
+   public final void setName(String _name)
+   {
+      name = _name;
+   }
+
+   public final String getToken()
+   {
+      return token;
+   }
+
+   public final void setToken(String _token)
+   {
+      token = _token;
+   }
+
+   public final String getHelp()
+   {
+      return help;
+   }
+
+   public final void setHelp(String _help)
+   {
+      help = _help;
+   }
+
+   public void setDefaultChunk(ConfigChunk ch)
+   {
+      default_chunk = ch;
+   }
+
+   public void setDefaultChunk(Node n)
+   {
+      default_node = n;
+      default_chunk = null;
+   }
+
+   public ConfigChunk getDefaultChunk()
+   {
+      synchronized (this)
+      {
+         if ((default_chunk == null) && (default_node != null))
+         {
+            XMLConfigIOHandler h = 
+            (XMLConfigIOHandler)ConfigIO.getHandler ();
+            ConfigIOStatus iostatus = new ConfigIOStatus();
+            default_chunk = h.buildConfigChunk(default_node, false,
+                                               iostatus);
+            // slightly crude reporting here...
+            if (iostatus.getStatus() != iostatus.SUCCESS)
+            {
+               System.out.println(iostatus.toString());
             }
-            return d;
-        }
-        catch (CloneNotSupportedException e) {
-            return null;
-        }
-    }
+            ConfigIO.releaseHandler(h);
+         }
+         return default_chunk;
+      }
+   }
+
+   public void addPropertyDesc(PropertyDesc newp)
+   {
+      PropertyDesc oldp;
+      int n = props.size();
+      for (int i = 0; i < n; i++)
+      {
+         oldp = (PropertyDesc)props.get(i);
+         if (oldp.getToken().equalsIgnoreCase(newp.getToken()))
+         {
+            props.set(i, newp);
+            return;
+         }
+      }
+      props.add (newp);
+   }
 
 
+   public PropertyDesc[] getPropertyDescs()
+   {
+      PropertyDesc[] p = new PropertyDesc[props.size()];
+      return(PropertyDesc[])props.toArray(p);
+   }
 
-    public final String getName () {
-        return name;
-    }
+   public int propertyDescsSize()
+   {
+      return props.size();
+   }
 
+   public PropertyDesc getPropertyDesc(int i)
+   {
+      return(PropertyDesc)props.get(i);
+   }
 
-    public final void setName (String _name) {
-        name = _name;
-    }
+   public boolean equals (ChunkDesc c)
+   {
+      PropertyDesc p1, p2;
+      if (! name.equals(c.name))
+      {
+         return false;
+      }
+      if (! token.equals(c.token))
+      {
+         return false;
+      }
+      if (! help.equals(c.help))
+      {
+         return false;
+      }
 
+      if (props.size() != c.propertyDescsSize())
+      {
+         return false;
+      }
 
-    public final String getToken () {
-        return token;
-    }
-
-
-    public final void setToken (String _token) {
-        token = _token;
-    }
-
-    public final String getHelp () {
-        return help;
-    }
-
-    public final void setHelp (String _help) {
-        help = _help;
-    }
-
-
-    public void setDefaultChunk (ConfigChunk ch) {
-        default_chunk = ch;
-    }
-
-    public void setDefaultChunk (Node n) {
-        default_node = n;
-        default_chunk = null;
-    }
-
-    public ConfigChunk getDefaultChunk () {
-        synchronized (this) {
-            if ((default_chunk == null) && (default_node != null)) {
-                XMLConfigIOHandler h = 
-                    (XMLConfigIOHandler)ConfigIO.getHandler ();
-                ConfigIOStatus iostatus = new ConfigIOStatus();
-                default_chunk = h.buildConfigChunk (default_node, false,
-                                                    iostatus);
-                // slightly crude reporting here...
-                if (iostatus.getStatus() != iostatus.SUCCESS)
-                    System.out.println (iostatus.toString());
-                ConfigIO.releaseHandler(h);
-            }
-            return default_chunk;
-        }
-    }
-
-
-    public void addPropertyDesc (PropertyDesc newp) {
-        PropertyDesc oldp;
-        int n = props.size();
-        for (int i = 0; i < n; i++) {
-            oldp = (PropertyDesc)props.get(i);
-            if (oldp.getToken().equalsIgnoreCase (newp.getToken())) {
-                props.set (i, newp);
-                return;
-            }
-        }
-        props.add (newp);
-    }
-
-
-    public PropertyDesc[] getPropertyDescs () {
-        PropertyDesc[] p = new PropertyDesc[props.size()];
-        return (PropertyDesc[])props.toArray(p);
-    }
-
-    public int propertyDescsSize() {
-        return props.size();
-    }
-
-    public PropertyDesc getPropertyDesc(int i) {
-        return (PropertyDesc)props.get(i);
-    }
-
-    public boolean equals (ChunkDesc c) {
-        PropertyDesc p1, p2;
-        if (! name.equals(c.name))
-        {
+      /* This next part is O(n^2) <sigh> */
+      int n = props.size();
+      for (int i = 0; i < n; i++)
+      {
+         p1 = (PropertyDesc) props.get(i);
+         p2 = c.getPropertyDesc(p1.getToken());
+         if ((p2 == null) || (!p1.equals(p2)))
+         {
             return false;
-        }
-        if (! token.equals(c.token))
-        {
-            return false;
-        }
-        if (! help.equals(c.help))
-        {
-            return false;
-        }
+         }
+      }
+      return true;
+   }
 
-        if (props.size() != c.propertyDescsSize())
-        {
-            return false;
-        }
+   private void addNameProperty()
+   {
+      /* quick little function to add in a name property if it isn't already
+       * in the ChunkDesc.
+       */
+      PropertyDesc named;
+      named = getPropertyDesc("Name");
+      if (named == null)
+      {
+         named = new PropertyDesc();
+         named.setName("Name");
+         named.setToken("Name");
+         named.setHelp("Unique name of an instance of this chunk type");
+         named.setValType(ValType.STRING);
+         props.add(0, named);
+      }
+   }
 
-        /* This next part is O(n^2) <sigh> */
-        int n = props.size();
-        for (int i = 0; i < n; i++)
-        {
-            p1 = (PropertyDesc) props.get(i);
-            p2 = c.getPropertyDesc(p1.getToken());
-            if ((p2 == null) || (!p1.equals(p2)))
-                return false;
-        }
-        return true;
-    }
+   public PropertyDesc getPropertyDesc(String tok)
+   {
+      PropertyDesc p;
+      int n = props.size();
+      for (int i = 0; i < n; i++)
+      {
+         p = (PropertyDesc)props.get(i);
+         if (p.getToken().equalsIgnoreCase(tok))
+         {
+            return p;
+         }
+      }
+      return null;
+   }
 
+   public String toString()
+   {
+      return xmlRep("");
+   }
 
-
-    private void addNameProperty () {
-        /* quick little function to add in a name property if it isn't already
-         * in the ChunkDesc.
-         */
-        PropertyDesc named;
-        named = getPropertyDesc("Name");
-        if (named == null) {
-            named = new PropertyDesc();
-            named.setName ("Name");
-            named.setToken ("Name");
-            named.setHelp ("Unique name of an instance of this chunk type");
-            named.setValType (ValType.STRING);
-            props.add(0, named);
-        }
-    }
-
-
-
-    public PropertyDesc getPropertyDesc (String tok) {
-        PropertyDesc p;
-        int n = props.size();
-        for (int i = 0; i < n; i++) {
-            p = (PropertyDesc)props.get(i);
-            if (p.getToken().equalsIgnoreCase(tok))
-                return p;
-        }
-        return null;
-    }
-
-
-
-    public String toString() {
-        return xmlRep("");
-    }
-
-
-    public String xmlRep (String pad) {
-        String newpad = pad + pad;
-        StringBuffer retval = new StringBuffer(256);
-        retval.append(pad);
-        retval.append("<ChunkDesc token=\"");
-        retval.append(XMLConfigIOHandler.escapeString(token));
-        retval.append("\" name=\"");
-        retval.append(XMLConfigIOHandler.escapeString(name));
-        retval.append("\">\n");
-        if (!help.equals ("")) {
-            retval.append(newpad);
-            retval.append("<help>");
-            retval.append(XMLConfigIOHandler.escapeString(help));
-            retval.append("</help>\n");
-        }
-        int n = props.size();
-        for (int i = 0; i < n; i++) {
-            PropertyDesc p = (PropertyDesc)props.get(i);
-            retval.append(p.xmlRep(newpad));
-        }
-        ConfigChunk ch = getDefaultChunk();
-        if (ch != null) {
-            retval.append(newpad);
-            retval.append("<Defaults>\n");
-            retval.append(ch.xmlRep(newpad + "  "));
-            retval.append(newpad);
-            retval.append("</Defaults>\n");
-        }
-        retval.append(pad);
-        retval.append("</ChunkDesc>\n");
-        return retval.toString();
-    }
-  
+   public String xmlRep(String pad)
+   {
+      String newpad = pad + pad;
+      StringBuffer retval = new StringBuffer(256);
+      retval.append(pad);
+      retval.append("<ChunkDesc token=\"");
+      retval.append(XMLConfigIOHandler.escapeString(token));
+      retval.append("\" name=\"");
+      retval.append(XMLConfigIOHandler.escapeString(name));
+      retval.append("\">\n");
+      if (!help.equals (""))
+      {
+         retval.append(newpad);
+         retval.append("<help>");
+         retval.append(XMLConfigIOHandler.escapeString(help));
+         retval.append("</help>\n");
+      }
+      int n = props.size();
+      for (int i = 0; i < n; i++)
+      {
+         PropertyDesc p = (PropertyDesc)props.get(i);
+         retval.append(p.xmlRep(newpad));
+      }
+      ConfigChunk ch = getDefaultChunk();
+      if (ch != null)
+      {
+         retval.append(newpad);
+         retval.append("<Defaults>\n");
+         retval.append(ch.xmlRep(newpad + "  "));
+         retval.append(newpad);
+         retval.append("</Defaults>\n");
+      }
+      retval.append(pad);
+      retval.append("</ChunkDesc>\n");
+      return retval.toString();
+   }
 }
-
--- modules/jackal/config/org/vrjuggler/jccl/config/ChunkFactory.java
+++ modules/jackal/config/org/vrjuggler/jccl/config/ChunkFactory.java
@@ -40,92 +40,112 @@ import VjConfig.ChunkDescDB;
 import VjConfig.ChunkDesc;
 import VjConfig.ConfigChunk;
 
-public class ChunkFactory {
+public class ChunkFactory
+{
+   static private ChunkDescDB descdb = null;
+   static private ConfigChunkDB default_chunkdb = null;
 
-    static private ChunkDescDB descdb = null;
-    static private ConfigChunkDB default_chunkdb = null;
+   static public void setChunkDescDB(ChunkDescDB _db)
+   {
+      descdb = _db;
+   }
 
-    static public void setChunkDescDB (ChunkDescDB _db) {
-	descdb = _db;
-    }
+   static public void addChunkDescDB(ChunkDescDB _db)
+   {
+      descdb.addAll(_db);
+   }
 
-    static public void addChunkDescDB (ChunkDescDB _db) {
-        descdb.addAll (_db);
-    }
+   static public void setDefaultChunkDB(ConfigChunkDB _db)
+   {
+      default_chunkdb = _db;
+   }
 
-    static public void setDefaultChunkDB (ConfigChunkDB _db) {
-	default_chunkdb = _db;
-    }
+   static public ChunkDesc getChunkDescByToken(String s)
+   {
+      return descdb.getByToken(s);
+   }
 
-    static public ChunkDesc getChunkDescByToken (String s) {
-	return descdb.getByToken(s);
-    }
+   static public ConfigChunk createChunkWithDescName(String s)
+   {
+      return createChunkWithDescName(s, true);
+   }
 
-    static public ConfigChunk createChunkWithDescName (String s) {
-        return createChunkWithDescName (s, true);
-    }
+   static public ConfigChunk createChunkWithDescName(String s, boolean use_defaults)
+   {
+      ConfigChunk newchunk = null;
 
-    static public ConfigChunk createChunkWithDescName (String s, boolean use_defaults) {
-	ConfigChunk newchunk = null;
+      if (default_chunkdb != null)
+      {
+         List v = default_chunkdb.getOfDescName(s);
+         if (!v.isEmpty())
+         {
+            //System.out.println ("creating chunk from default");
+            return new ConfigChunk((ConfigChunk)v.get(0));
+         }
+      }
+      if (descdb != null)
+      {
+         ChunkDesc cd = descdb.getByName(s);
+         if (cd != null)
+         {
+            return new ConfigChunk(cd, use_defaults);
+         }
+      }
+      return null;
+   }
 
-	if (default_chunkdb != null) {
-	    List v = default_chunkdb.getOfDescName (s);
-	    if (!v.isEmpty()) {
-		//System.out.println ("creating chunk from default");
-		return new ConfigChunk ((ConfigChunk)v.get(0));
-	    }
-	}
-	if (descdb != null) {
-	    ChunkDesc cd = descdb.getByName(s);
-	    if (cd != null)
-		return new ConfigChunk (cd, use_defaults);
-	}
-	return null;
-    }
+   static public ConfigChunk createChunkWithDescToken(String s)
+   {
+      return createChunkWithDescToken(s, true);
+   }
 
+   static public ConfigChunk createChunkWithDescToken(String s, boolean use_defaults)
+   {
+      ConfigChunk newchunk = null;
 
-    static public ConfigChunk createChunkWithDescToken (String s) {
-        return createChunkWithDescToken (s, true);
-    }
+      if (default_chunkdb != null)
+      {
+         List v = default_chunkdb.getOfDescToken(s);
+         if (!v.isEmpty())
+         {
+            //System.out.println ("creating chunk from default");
+            return new ConfigChunk((ConfigChunk)v.get(0));
+         }
+      }
+      if (descdb != null)
+      {
+         ChunkDesc cd = descdb.getByToken(s);
+         if (cd != null)
+         {
+            return new ConfigChunk(cd, use_defaults);
+         }
+      }
+      return null;
+   }
 
-    static public ConfigChunk createChunkWithDescToken (String s, boolean use_defaults) {
-	ConfigChunk newchunk = null;
+   static public ConfigChunk createChunk(ChunkDesc cd)
+   {
+      return new ConfigChunk(cd);
+   }
 
-	if (default_chunkdb != null) {
-	    List v = default_chunkdb.getOfDescToken (s);
-	    if (!v.isEmpty()) {
-		//System.out.println ("creating chunk from default");
-		return new ConfigChunk ((ConfigChunk)v.get(0));
-	    }
-	}
-	if (descdb != null) {
-	    ChunkDesc cd = descdb.getByToken(s);
-	    if (cd != null)
-		return new ConfigChunk (cd, use_defaults);
-	}
-	return null;
-    }
+   // Utility functions dealing with the global DescDB 
+   /** Returns the names of all ChunkDescs defined in any of our DBs.
+    *  This is useful for GUI components that want to provide a list
+    *  or menu of desc names.
+    */
+   static public String[] getDescNames()
+   {
+      int n = descdb.size();
+      String[] names = new String[n];
+      for (int i = 0; i < n; i++)
+      {
+         names[i] = ((ChunkDesc)descdb.get(i)).getName();
+      }
+      return names;
+   }
 
-
-    static public ConfigChunk createChunk (ChunkDesc cd) {
-	return new ConfigChunk (cd);
-    }
-
-    // Utility functions dealing with the global DescDB 
-    /** Returns the names of all ChunkDescs defined in any of our DBs.
-     *  This is useful for GUI components that want to provide a list
-     *  or menu of desc names.
-     */
-    static public String[] getDescNames () {
-        int n = descdb.size();
-        String[] names = new String[n];
-        for (int i = 0; i < n; i++)
-            names[i] = ((ChunkDesc)descdb.get(i)).getName();
-        return names;
-    }
-
-    static public String getNameFromToken (String tok) {
-        return descdb.getNameFromToken (tok);
-    }
-
+   static public String getNameFromToken(String tok)
+   {
+      return descdb.getNameFromToken(tok);
+   }
 }
