76373ea901218cef66e4b7c2134dffd98ee0fe42
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-04 00:25:52 +0000

    Style guidelines.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8132 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/config/org/vrjuggler/jccl/config/PropertyDesc.java
+++ juggler/modules/jackal/config/org/vrjuggler/jccl/config/PropertyDesc.java
@@ -52,8 +52,9 @@ import java.io.*;
  *  @author Christopher Just
  *  @version $Revision$
  */
-public class PropertyDesc implements Cloneable {
-
+public class PropertyDesc
+   implements Cloneable
+{
    private String name;
    private String token;
    private String help;
@@ -72,8 +73,8 @@ public class PropertyDesc implements Cloneable {
    /** Assigns an individual label to each value of a property. */
    private ArrayList valuelabels;
 
-
-   public PropertyDesc () {
+   public PropertyDesc ()
+   {
       /* creates an "empty" PropertyDesc */
       name = "";
       token = "";
@@ -84,301 +85,363 @@ public class PropertyDesc implements Cloneable {
       valtype = ValType.INVALID;
       enumval = 0;
       user_level = 0;
-    }
+   }
 
+   public Object clone ()
+      throws CloneNotSupportedException
+   {
+      PropertyDesc p = (PropertyDesc)super.clone();
+      p.valuelabels = (ArrayList)valuelabels.clone(); // safe; it's a list of strings
+      p.enums = new ArrayList();
+      int i, n = enums.size();
+      for (i = 0; i < n; i++)
+      {
+         p.enums.add (((DescEnum)enums.get(i)).clone());
+      }
+      return p;
+   }
 
+   public void setName(String _name)
+   {
+      name = _name;
+   }
 
-    public Object clone () throws CloneNotSupportedException {
-        PropertyDesc p = (PropertyDesc)super.clone();
-        p.valuelabels = (ArrayList)valuelabels.clone(); // safe; it's a list of strings
-        p.enums = new ArrayList();
-        int i, n = enums.size();
-        for (i = 0; i < n; i++) {
-            p.enums.add (((DescEnum)enums.get(i)).clone());
-        }
-        return p;
-    }
+   public String getName()
+   {
+      return name;
+   }
 
+   public void setToken(String _token)
+   {
+      token = _token;
+   }
 
-    public void setName (String _name) {
-        name = _name;
-    }
+   public String getToken()
+   {
+      return token;
+   }
 
-    public String getName () {
-        return name;
-    }
+   public void setHelp(String _help)
+   {
+      help = _help;
+   }
 
-    public void setToken (String _token) {
-        token = _token;
-    }
+   public String getHelp()
+   {
+      return help;
+   }
 
-    public String getToken () {
-        return token;
-    }
+   public void setValType(ValType t)
+   {
+      valtype = t;
+   }
 
-    public void setHelp (String _help) {
-        help = _help;
-    }
+   public ValType getValType()
+   {
+      return valtype;
+   }
 
-    public String getHelp () {
-        return help;
-    }
+   public void setHasVariableNumberOfValues(boolean b)
+   {
+      if (b)
+      {
+         num = -1;
+      }
+      else
+      {
+         num = 1;
+      }
+   }
 
-    public void setValType (ValType t) {
-        valtype = t;
-    }
+   public boolean getHasVariableNumberOfValues()
+   {
+      return(num == -1);
+   }
 
-    public ValType getValType () {
-        return valtype;
-    }
+   public void setNumValues(int n)
+   {
+      num = n;
+   }
 
-    public void setHasVariableNumberOfValues (boolean b) {
-        if (b)
-            num = -1;
-        else
-            num = 1;
-    }
+   public int getNumValues()
+   {
+      return num;
+   }
 
-    public boolean getHasVariableNumberOfValues () {
-        return (num == -1);
-    }
+   public void setUserLevel(int level)
+   {
+      user_level = level;
+   }
 
-    public void setNumValues (int n) {
-        num = n;
-    }
+   public int getUserLevel()
+   {
+      return user_level;
+   }
 
-    public int getNumValues () {
-        return num;
-    }
+   public void appendValueLabel(String label)
+   {
+      valuelabels.add(label);
+   }
 
-    public void setUserLevel (int level) {
-        user_level = level;
-    }
+   public int getValueLabelsSize()
+   {
+      return valuelabels.size();
+   }
 
-    public int getUserLevel () {
-        return user_level;
-    }
+   public String getValueLabel(int i)
+   {
+      if (i < valuelabels.size())
+      {
+         return(String)valuelabels.get(i);
+      }
+      else
+      {
+         return "";
+      }
+   }
 
-    public void appendValueLabel (String label) {
-        valuelabels.add (label);
-    }
+   public String[] getValueLabels()
+   {
+      String[] e = new String[valuelabels.size()];
+      return(String[])valuelabels.toArray(e);
+   }
 
-    public int getValueLabelsSize () {
-        return valuelabels.size();
-    }
+   /* c had better be a collection of String, or things will get
+    * real ugly real fast.
+    */
+   public void setValueLabels(Collection c)
+   {
+      valuelabels.clear();
+      valuelabels.addAll(c);
+   }
 
-    public String getValueLabel (int i) {
-        if (i < valuelabels.size())
-            return (String)valuelabels.get(i);
-        else
-            return "";
-    }
-
-    public String[] getValueLabels () {
-        String[] e = new String[valuelabels.size()];
-        return (String[])valuelabels.toArray(e);
-    }
-
-    /* c had better be a collection of String, or things will get
-     * real ugly real fast.
-     */
-    public void setValueLabels (Collection c) {
-        valuelabels.clear();
-        valuelabels.addAll (c);
-    }
-
-    public void appendEnumeration (String label, String value) {
-        DescEnum d;
-        VarValue v;
-        if (value == "") {
-            /* no explicit value */
-            if (valtype == ValType.STRING ||
-                valtype == ValType.CHUNK ||
-                valtype == ValType.EMBEDDEDCHUNK)
-                v = new VarValue(label);
-            else
-                v = new VarValue(enumval++);
-        }
-        else {
-            v = new VarValue(valtype);
-            v.set (value);
-            /* explicit value */
+   public void appendEnumeration(String label, String value)
+   {
+      DescEnum d;
+      VarValue v;
+      if (value == "")
+      {
+         /* no explicit value */
+         if (valtype == ValType.STRING ||
+             valtype == ValType.CHUNK ||
+             valtype == ValType.EMBEDDEDCHUNK)
+         {
+            v = new VarValue(label);
+         }
+         else
+         {
+            v = new VarValue(enumval++);
+         }
+      }
+      else
+      {
+         v = new VarValue(valtype);
+         v.set (value);
+         /* explicit value */
 //              VarValue val = new VarValue(valtype);
 //              val.set (value);
 //              d = new DescEnum (label, val);
-        }
-        enums.add (new DescEnum (label, v));
-    }
+      }
+      enums.add(new DescEnum(label, v));
+   }
 
 
-    /* c had better be a collection of DescEnums, or things will get
-     * real ugly real fast.
-     */
-    public void setEnumerations (Collection c) {
-        enums.clear();
-        enums.addAll (c);
-    }
+   /* c had better be a collection of DescEnums, or things will get
+    * real ugly real fast.
+    */
+   public void setEnumerations(Collection c)
+   {
+      enums.clear();
+      enums.addAll (c);
+   }
 
-    public DescEnum[] getEnumerations () {
-        DescEnum[] e = new DescEnum[enums.size()];
-        return (DescEnum[])enums.toArray(e);
-    }
+   public DescEnum[] getEnumerations()
+   {
+      DescEnum[] e = new DescEnum[enums.size()];
+      return(DescEnum[])enums.toArray(e);
+   }
 
-    public int getEnumerationsSize() {
-        return enums.size();
-    }
+   public int getEnumerationsSize()
+   {
+      return enums.size();
+   }
 
-    public DescEnum getEnumAtIndex (int ind) {
-	return (DescEnum)enums.get(ind);
-    }
+   public DescEnum getEnumAtIndex(int ind)
+   {
+      return(DescEnum)enums.get(ind);
+   }
 
+   public boolean equals(PropertyDesc d)
+   {
+      DescEnum e1, e2;
+      if (d == null)
+      {
+         return false;
+      }
+      if (!name.equalsIgnoreCase(d.name))
+      {
+         return false;
+      }
+      if (!token.equalsIgnoreCase(d.token))
+      {
+         return false;
+      }
+      if (!help.equalsIgnoreCase(d.help))
+      {
+         return false;
+      }
+      if (num != d.num)
+      {
+         return false;
+      }
+      if (valtype != d.valtype)
+      {
+         return false;
+      }
 
+      /* KLUDGE: This next part returns false if both
+       * PropertyDescs have the same descenums in a
+       * different order.  I'm not sure if the enums
+       * code enforces any kind of order or not, or
+       * if this is reasonable behavior. This should
+       * be examined more carefully.
+       */
+      for (int i = 0; i <enums.size(); i++)
+      {
+         try
+         {
+            e1 = (DescEnum)enums.get(i);
+            e2 = (DescEnum)d.enums.get(i);
+            if (!e1.equals(e2))
+            {
+               return false;
+            }
+         }
+         catch (ArrayIndexOutOfBoundsException e)
+         {
+            return false;
+         }
+      }
+      return true;
+   }
 
-    public boolean equals(PropertyDesc d) {
-	DescEnum e1, e2;
-	if (d == null)
-	  return false;
-	if (!name.equalsIgnoreCase(d.name))
-	    return false;
-	if (!token.equalsIgnoreCase(d.token))
-	    return false;
-	if (!help.equalsIgnoreCase(d.help))
-	    return false;
-	if (num != d.num)
-	    return false;
-	if (valtype != d.valtype)
-	    return false;
+   public String toString()
+   {
+      return xmlRep ("");
+   }
 
-	/* KLUDGE: This next part returns false if both
-	 * PropertyDescs have the same descenums in a
-	 * different order.  I'm not sure if the enums
-	 * code enforces any kind of order or not, or
-	 * if this is reasonable behavior. This should
-	 * be examined more carefully.
-	 */
-	for (int i = 0; i <enums.size(); i++) {
-	    try {
-		e1 = (DescEnum)enums.get(i);
-		e2 = (DescEnum)d.enums.get(i);
-		if (!e1.equals(e2))
-		    return false;
-	    }
-	    catch (ArrayIndexOutOfBoundsException e) {
-		return false;
-	    }
-	}
-	return true;
-    }
-
-
-
-    public String toString() {
-        return xmlRep ("");
-    }
-
-
-
-    public String xmlRep (String pad) {
-        int i, n;
-        DescEnum e;
-        String newpad = pad + pad;
-        // string buffer is a lot more painful code-wise but noticeably 
-        // faster.
-        StringBuffer retval = new StringBuffer (256);
-        retval.append(pad);
-        retval.append("<PropertyDesc token=\"");
-        retval.append(XMLConfigIOHandler.escapeString(token));
-        retval.append("\" name=\"");
-        retval.append(XMLConfigIOHandler.escapeString(name));
-        if (user_level != 0) {
-            retval.append("\" userlevel=\"expert");
-        }
-        retval.append("\" type=\"");
-        retval.append(valtype.toString());
-        retval.append("\" num=\"");
-        if (num == -1)
-            retval.append ("variable\">\n");
-        else {
-            retval.append (num);
-            retval.append ("\">\n");
-        }
-        if (!help.equals ("")) {
+   public String xmlRep(String pad)
+   {
+      int i, n;
+      DescEnum e;
+      String newpad = pad + pad;
+      // string buffer is a lot more painful code-wise but noticeably 
+      // faster.
+      StringBuffer retval = new StringBuffer(256);
+      retval.append(pad);
+      retval.append("<PropertyDesc token=\"");
+      retval.append(XMLConfigIOHandler.escapeString(token));
+      retval.append("\" name=\"");
+      retval.append(XMLConfigIOHandler.escapeString(name));
+      if (user_level != 0)
+      {
+         retval.append("\" userlevel=\"expert");
+      }
+      retval.append("\" type=\"");
+      retval.append(valtype.toString());
+      retval.append("\" num=\"");
+      if (num == -1)
+      {
+         retval.append ("variable\">\n");
+      }
+      else
+      {
+         retval.append (num);
+         retval.append ("\">\n");
+      }
+      if (!help.equals (""))
+      {
+         retval.append(newpad);
+         retval.append("<help>");
+         retval.append(XMLConfigIOHandler.escapeString(help));
+         retval.append("</help>\n");
+      }
+      n = valuelabels.size();
+      if (n > 0)
+      {
+         for (i = 0; i < n; i++)
+         {
             retval.append(newpad);
-            retval.append("<help>");
-            retval.append(XMLConfigIOHandler.escapeString(help));
-            retval.append("</help>\n");
-        }
-        n = valuelabels.size();
-        if (n > 0) {
-            for (i = 0; i < n; i++) {
-                retval.append(newpad);
-                retval.append("<label name=\"");
-                retval.append(XMLConfigIOHandler.escapeString((String)valuelabels.get(i)));
-                retval.append("\"/>\n");
+            retval.append("<label name=\"");
+            retval.append(XMLConfigIOHandler.escapeString((String)valuelabels.get(i)));
+            retval.append("\"/>\n");
+         }
+      }
+      n = enums.size();
+      if (n > 0)
+      {
+         for (i = 0; i < n; i++)
+         {
+            e = (DescEnum)enums.get(i);
+            retval.append(newpad);
+            retval.append("<enumeration name=\"");
+            retval.append(XMLConfigIOHandler.escapeString(e.str));
+            if (valtype == ValType.STRING ||
+                valtype == ValType.CHUNK ||
+                valtype == ValType.EMBEDDEDCHUNK)
+            {
+               retval.append("\"/>\n");
             }
-        }
-        n = enums.size();
-        if (n > 0) {
-            for (i = 0; i < n; i++) {
-                e = (DescEnum)enums.get(i);
-                retval.append(newpad);
-                retval.append("<enumeration name=\"");
-                retval.append(XMLConfigIOHandler.escapeString(e.str));
-                if (valtype == ValType.STRING ||
-                    valtype == ValType.CHUNK ||
-                    valtype == ValType.EMBEDDEDCHUNK)
-                    retval.append("\"/>\n");
-                else {
-                    retval.append("\" value=\"");
-                    retval.append(XMLConfigIOHandler.escapeString(e.val.toString()));
-                    retval.append("\"/>\n");
-                }
+            else
+            {
+               retval.append("\" value=\"");
+               retval.append(XMLConfigIOHandler.escapeString(e.val.toString()));
+               retval.append("\"/>\n");
             }
-        }
-        retval.append(pad);
-        retval.append("</PropertyDesc>\n");
-        return retval.toString();
-    }
+         }
+      }
+      retval.append(pad);
+      retval.append("</PropertyDesc>\n");
+      return retval.toString();
+   }
 
+   /** Maps a string to an enumeration value.
+    *  @return The VarValue which is mapped by val, or null if no such
+    *          mapping exists.
+    */
+   public VarValue getEnumValue(String val)
+   {
+      /* returns the value associated with this enum el */
+      DescEnum t;
+      VarValue v;
 
+      for (int i = 0; i < enums.size(); i++)
+      {
+         t = (DescEnum)enums.get(i);
+         if (t.str.equalsIgnoreCase(val))
+         {
+            v = new VarValue(t.val);
+            return v;
+         }
+      }
+      return null;
+   }
 
-    /** Maps a string to an enumeration value.
-     *  @return The VarValue which is mapped by val, or null if no such
-     *          mapping exists.
-     */
-    public VarValue getEnumValue(String val) {
-	/* returns the value associated with this enum el */
-	DescEnum t;
- 	VarValue v;
-	
-	for (int i = 0; i < enums.size(); i++) {
-	    t = (DescEnum)enums.get(i);
-	    if (t.str.equalsIgnoreCase(val)) {
-		v = new VarValue(t.val);
-		return v;
-	    }
-	}
-        return null;
-    }
+   public String getEnumString(VarValue val)
+   {
+      /* does the reverse mapping of getEnumVal - maps a value 
+       * back to the name of the enum entry 
+       */
+      DescEnum t;
 
-
-
-    public String getEnumString(VarValue val) {
-	/* does the reverse mapping of getEnumVal - maps a value 
-	 * back to the name of the enum entry 
-	 */
-	DescEnum t;
-
-	for (int i = 0; i < enums.size(); i++) {
-	    t = (DescEnum)enums.get(i);
-	    if (t.val.equals(val)) {
-		return t.str;
-	    }
-	}
-	return val.toString();
-    }
-
-
+      for (int i = 0; i < enums.size(); i++)
+      {
+         t = (DescEnum)enums.get(i);
+         if (t.val.equals(val))
+         {
+            return t.str;
+         }
+      }
+      return val.toString();
+   }
 }
-
-
