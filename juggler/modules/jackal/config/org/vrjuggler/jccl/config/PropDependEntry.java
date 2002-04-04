27768f796d3722b6d8bf8330e7873864464a520d
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-04 00:21:17 +0000

    style conventions.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8131 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/config/org/vrjuggler/jccl/config/PropDependEntry.java
+++ juggler/modules/jackal/config/org/vrjuggler/jccl/config/PropDependEntry.java
@@ -29,38 +29,40 @@
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
-
-
-
 package VjConfig;
 
 import VjConfig.ConfigChunk;
 import VjConfig.Property;
 import java.util.Vector;
 
-public class PropDependEntry {
-    //public Property property;
-    public String propertyname;
-    public String dependency_name;
-    public Vector other_files;
-    
-    public PropDependEntry () {
-	other_files = new Vector();
-    }
+public class PropDependEntry
+{
+   //public Property property;
+   public String propertyname;
+   public String dependency_name;
+   public Vector other_files;
 
-    public String toString () {
-	String s = propertyname//property.getName()
-	    + " needs \"" + dependency_name + "\"\n";
-	if (other_files.size() > 0) {
-	    s += "    satisfied in:\n";
-	    for (int i = 0; i < other_files.size(); i++) {
-		s += "        " + (String)other_files.elementAt(i) + "\n";
-	    }
-	}
-	else {
-	    s += "    no match found in any loaded file.\n";
-	}
-	return s;
-    }
+   public PropDependEntry()
+   {
+      other_files = new Vector();
+   }
+
+   public String toString()
+   {
+      String s = propertyname//property.getName()
+                 + " needs \"" + dependency_name + "\"\n";
+      if (other_files.size() > 0)
+      {
+         s += "    satisfied in:\n";
+         for (int i = 0; i < other_files.size(); i++)
+         {
+            s += "        " + (String)other_files.elementAt(i) + "\n";
+         }
+      }
+      else
+      {
+         s += "    no match found in any loaded file.\n";
+      }
+      return s;
+   }
 }
-
