0b0e1c8700ad2156ec889e235fa19028bb10e591
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-04 00:34:46 +0000

    Style conventions.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8133 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/config/org/vrjuggler/jccl/config/ValType.java
+++ modules/jackal/config/org/vrjuggler/jccl/config/ValType.java
@@ -41,88 +41,99 @@ package VjConfig;
  *  created, it is safe to do direct object comparisons.  ie vt1 == vt2 can
  *  be treated as equivalent to vt1.equals(vt2).
  */
-public final class ValType {
+public final class ValType
+{
+   private final static int t_string        = 1;
+   private final static int t_float         = 2;
+   private final static int t_int           = 3;
+   private final static int t_bool          = 4;
+   private final static int t_chunk         = 5;
+   private final static int t_embeddedchunk = 6;
+   private final static int t_invalid       = 7;
 
-    private final static int t_string        = 1;
-    private final static int t_float         = 2;
-    private final static int t_int           = 3;
-    private final static int t_bool          = 4;
-    private final static int t_chunk         = 5;
-    private final static int t_embeddedchunk = 6;
-    private final static int t_invalid       = 7;
+   public final static ValType STRING           = new ValType(t_string);
+   public final static ValType FLOAT            = new ValType(t_float);
+   public final static ValType INT              = new ValType(t_int);
+   public final static ValType BOOL             = new ValType(t_bool);
+   public final static ValType CHUNK            = new ValType(t_chunk);
+   public final static ValType EMBEDDEDCHUNK    = new ValType(t_embeddedchunk);
+   public final static ValType INVALID          = new ValType(t_invalid);
 
+   private int val; // type identifier for this instance.
 
-    public final static ValType STRING = new ValType (t_string);
-    public final static ValType FLOAT = new ValType (t_float);
-    public final static ValType INT = new ValType (t_int);
-    public final static ValType BOOL = new ValType (t_bool);
-    public final static ValType CHUNK = new ValType (t_chunk);
-    public final static ValType EMBEDDEDCHUNK = new ValType (t_embeddedchunk);
-    public final static ValType INVALID = new ValType (t_invalid);
+   /** Constructor.  Private so that no other ValTypes can be created. */
+   private ValType(int i)
+   {
+      val = i;
+   }
 
-    private int val; // type identifier for this instance.
+   /** Returns the ValType associated with a String value.
+    *  This can be used in various parsing routines.
+    */
+   public static ValType getValType(String s)
+   {
+      String s2 = s.toLowerCase().trim();
+      if (s2.equalsIgnoreCase("string"))
+      {
+         return STRING;
+      }
+      else if (s2.equalsIgnoreCase("float"))
+      {
+         return FLOAT;
+      }
+      else if (s2.equalsIgnoreCase("int"))
+      {
+         return INT;
+      }
+      else if (s2.equalsIgnoreCase("bool"))
+      {
+         return BOOL;
+      }
+      else if (s2.equalsIgnoreCase("chunk"))
+      {
+         return CHUNK;
+      }
+      else if (s2.equalsIgnoreCase("embeddedchunk"))
+      {
+         return EMBEDDEDCHUNK;
+      }
+      else
+      {
+         return INVALID;
+      }
+   }
 
+   /** Converts the ValType to a String.
+    *  The string literals used are the same as those recognized by
+    *  ValTpe.getValType(String s).
+    */
+   public String toString()
+   {
+      switch (val)
+      {
+      case t_string:
+         return "String";
+      case t_float:
+         return "Float";
+      case t_int:
+         return "Int";
+      case t_bool:
+         return "Bool";
+      case t_chunk:
+         return "Chunk";
+      case t_embeddedchunk:
+         return "EmbeddedChunk";
+      default:
+         return "Invalid";
+      }
+   }
 
-    /** Constructor.  Private so that no other ValTypes can be created. */
-    private ValType (int i) {
-	val = i;
-    }
-
-
-    /** Returns the ValType associated with a String value.
-     *  This can be used in various parsing routines.
-     */
-    public static ValType getValType (String s) {
-	String s2 = s.toLowerCase().trim();
-	if (s2.equalsIgnoreCase ("string"))
-	    return STRING;
-	else if (s2.equalsIgnoreCase ("float"))
-	    return FLOAT;
-	else if (s2.equalsIgnoreCase ("int"))
-	    return INT;
-	else if (s2.equalsIgnoreCase ("bool"))
-	    return BOOL;
-	else if (s2.equalsIgnoreCase ("chunk"))
-	    return CHUNK;
-	else if (s2.equalsIgnoreCase ("embeddedchunk"))
-	    return EMBEDDEDCHUNK;
-	else 
-	    return INVALID;
-    }
-
-
-
-    /** Converts the ValType to a String.
-     *  The string literals used are the same as those recognized by
-     *  ValTpe.getValType(String s).
-     */
-    public String toString() {
-	switch (val) {
-	case t_string:
-	    return "String";
-	case t_float:
-	    return "Float";
-	case t_int:
-	    return "Int";
-	case t_bool:
-	    return "Bool";
-	case t_chunk:
-	    return "Chunk";
-	case t_embeddedchunk:
-	    return "EmbeddedChunk";
-	default:
-	    return "Invalid";
-	}
-    }
-
-
-
-    public boolean equals (ValType v) {
-        if (v == null)
-            return false;
-	return (val == v.val);
-    }
-
-
+   public boolean equals(ValType v)
+   {
+      if (v == null)
+      {
+         return false;
+      }
+      return (val == v.val);
+   }
 }
-
