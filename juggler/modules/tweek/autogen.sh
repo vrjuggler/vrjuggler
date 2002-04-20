5d5b7fc6f413976c2fea43ab0d5ff7abaa9b2953
Patrick Hartling <patrick.hartling@gmail.com>
2002-04-20 15:30:34 +0000

    Replaced this with the autogen.sh in use by Plexus.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8353 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/autogen.sh
+++ juggler/modules/tweek/autogen.sh
@@ -26,25 +26,34 @@
 
 # Run this to generate all the initial makefiles, etc.
 
-# dppgen.sh,v 1.5 2001/02/22 23:33:25 patrick Exp
+# dppgen.sh,v 1.4 2000/12/30 14:34:19 patrick Exp
 
 DIE=0
 
 : ${DPP_PATH=../../Doozer++}
+: ${MACRO_PATH=../../macros}
 
-if [ -n "$DPP_PATH" ]; then
-	ACLOCAL_FLAGS="-I $DPP_PATH/config -I ../../macros $ACLOCAL_FLAGS"
+if [ -n "$MACRO_PATH" ]; then
+   ACLOCAL_FLAGS="-I $MACRO_PATH $ACLOCAL_FLAGS"
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+if [ -n "$DPP_PATH" ]; then
+   ACLOCAL_FLAGS="-I $DPP_PATH/config $ACLOCAL_FLAGS"
+fi
+
+: ${AUTOCONF=autoconf}
+: ${AUTOHEADER=autoheader}
+: ${ACLOCAL=aclocal}
+
+($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`autoconf' installed to compile."
+  echo "**Error**: You must have \`autoconf' installed to compile VR Juggler."
   echo "Download the appropriate package for your distribution,"
   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
   DIE=1
 }
 
-(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+($ACLOCAL --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
   echo "installed doesn't appear recent enough."
@@ -57,29 +66,13 @@ if test "$DIE" -eq 1; then
   exit 1
 fi
 
-for coin in `find ${srcdir-.} -name configure.in -print`
-do 
-  dr=`dirname $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
-  else
-    echo processing $dr
-    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
-    ( cd $dr
-      macrosdir=`find . -name macros -print`
-#      for i in $macrodirs; do
-#      done
-
-      aclocalinclude="$ACLOCAL_FLAGS"
-      echo "Running aclocal $aclocalinclude ..."
-      aclocal $aclocalinclude
-      if grep "^AC_CONFIG_HEADER" configure.in >/dev/null
-      then
-	echo "Running autoheader..."
-	autoheader
-      fi
-      echo "Running autoconf ..."
-      autoconf
-    )
-  fi
-done
+aclocalinclude="$ACLOCAL_FLAGS"
+echo "Running $ACLOCAL $aclocalinclude ..."
+$ACLOCAL $aclocalinclude
+if grep "^AC_CONFIG_HEADER" configure.in >/dev/null
+then
+  echo "Running $AUTOHEADER..."
+  $AUTOHEADER
+fi
+echo "Running $AUTOCONF ..."
+$AUTOCONF
