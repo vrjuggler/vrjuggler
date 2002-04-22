b593a4e12ed39faf651823dc964d0deb30905ace
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-22 20:36:39 +0000

    Package fixes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8382 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/net/org/vrjuggler/jccl/net/NetworkModule.java
+++ modules/jackal/net/org/vrjuggler/jccl/net/NetworkModule.java
@@ -128,7 +128,7 @@ public class NetworkModule
         String classname = ch.getValueFromToken ("classname", 0).getString();
         if (classname == null)
             throw new VjComponentException (component_name + ": Not a component instance chunk: " + ch.getName());
-        if (Core.component_factory.classSupportsInterface (classname, VjComponents.Network.NetCommunicator.class)) {
+        if (Core.component_factory.classSupportsInterface (classname, NetCommunicator.class)) {
             NetCommunicator nc = (NetCommunicator) Core.component_factory.createComponent(classname);
             nc.setConfiguration (ch);
             nc.initialize ();
