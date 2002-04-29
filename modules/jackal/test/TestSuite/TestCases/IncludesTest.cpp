2842feeb2a61ad7c29434d22c8e38903d8f9aad2
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-04-29 23:28:21 +0000

    Test suite added to build and it compiles and runs.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8506 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/test/TestSuite/TestCases/IncludesTest.cpp
+++ modules/jackal/test/TestSuite/TestCases/IncludesTest.cpp
@@ -42,7 +42,7 @@ namespace jcclTest
         jccl::ConfigChunkPtr chunk;
         jccl::ConfigChunkDB chunkdb;
 
-        jccl::ChunkFactory::instance()->loadDescs ("${JCCL_BASE_DIR}/share/data/jccl-chunks.desc");
+        jccl::ChunkFactory::instance()->loadDescs ("${JCCL_BASE_DIR}/share/jccl/data/jccl-chunks.desc");
 
 
         std::string file_path(TESTFILES_PATH);
