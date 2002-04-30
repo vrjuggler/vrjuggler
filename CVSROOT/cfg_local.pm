a60918a3eed0c5a008da1e94afdb043a73ef9929
Patrick Hartling <patrick.hartling@gmail.com>
2002-04-30 02:12:34 +0000

    Disable email delivery while I do some debugging.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8512 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- CVSROOT/cfg_local.pm
+++ CVSROOT/cfg_local.pm
@@ -27,7 +27,7 @@ $MAILCMD = "/usr/lib/sendmail -odb -oem";
 $MAIL_BRANCH_HDR = "X-VRJuggler-CVS-Branch:";
 $ADD_TO_LINE = 1;
 
-$MAILADDRS = 'vrjuggler-checkins@lists.sourceforge.net';
+$MAILADDRS = 'patrickh@users.sourceforge.net';
 
 @COMMIT_HOSTS = ();
 
