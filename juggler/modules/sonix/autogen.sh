fcfedcfaea88eeb8805d03243251ba92ac8b7226
Kevin Meinert <subatomic@users.sourceforge.net>
2002-05-01 18:29:20 +0000

    make sonix-config executable.. make autogen.sh find gmtl
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8535 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/sonix/autogen.sh
+++ juggler/modules/sonix/autogen.sh
@@ -1,45 +1,3 @@
-
-# ***************** <SNX heading BEGIN do not edit this line> *****************
-#
-# sonix
-#
-# Original Authors:
-#   Kevin Meinert, Carolina Cruz-Neira
-#
-# -----------------------------------------------------------------
-# File:          $RCSfile$
-# Date modified: $Date$
-# Version:       $Revision$
-# -----------------------------------------------------------------
-#
-# ****************** <SNX heading END do not edit this line> ******************
-# ************** <auto-copyright.pl BEGIN do not edit this line> **************
-#
-# VR Juggler is (C) Copyright 1998, 1999, 2000, 2001 by Iowa State University
-#
-# Original Authors:
-#   Allen Bierbaum, Christopher Just,
-#   Patrick Hartling, Kevin Meinert,
-#   Carolina Cruz-Neira, Albert Baker
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-# Boston, MA 02111-1307, USA.
-#
-# *************** <auto-copyright.pl END do not edit this line> ***************
-
-
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
@@ -47,13 +5,27 @@
 
 DIE=0
 
-dpp_path=${DPP_PATH-../../Doozer++}
+: ${DPP_PATH=../../Doozer++}
+: ${MACRO_PATH=../../macros}
+: ${GGT_MACRO_PATH=../../macros.GGT}
 
-if [ -n "$dpp_path" ]; then
-	ACLOCAL_FLAGS="-I $dpp_path/config -I ../../macros $ACLOCAL_FLAGS"
+if [ -n "$DPP_PATH" ]; then
+   ACLOCAL_FLAGS="-I $DPP_PATH/config $ACLOCAL_FLAGS"
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+if [ -n "$MACRO_PATH" ]; then
+   ACLOCAL_FLAGS="-I $MACRO_PATH $ACLOCAL_FLAGS"
+fi
+
+if [ -n "$GGT_MACRO_PATH" ]; then
+   ACLOCAL_FLAGS="-I $GGT_MACRO_PATH $ACLOCAL_FLAGS"
+fi
+
+: ${AUTOCONF=autoconf}
+: ${AUTOHEADER=autoheader}
+: ${ACLOCAL=aclocal}
+
+($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: You must have \`autoconf' installed to compile VR Juggler."
   echo "Download the appropriate package for your distribution,"
@@ -61,7 +33,7 @@ fi
   DIE=1
 }
 
-(aclocal --version) < /dev/null > /dev/null 2>&1 || {
+($ACLOCAL --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
   echo "installed doesn't appear recent enough."
@@ -75,12 +47,12 @@ if test "$DIE" -eq 1; then
 fi
 
 aclocalinclude="$ACLOCAL_FLAGS"
-echo "Running aclocal $aclocalinclude ..."
-aclocal $aclocalinclude
+echo "Running $ACLOCAL $aclocalinclude ..."
+$ACLOCAL $aclocalinclude
 if grep "^AC_CONFIG_HEADER" configure.in >/dev/null
 then
-  echo "Running autoheader..."
-  autoheader
+  echo "Running $AUTOHEADER..."
+  $AUTOHEADER
 fi
-echo "Running autoconf ..."
-autoconf
+echo "Running $AUTOCONF ..."
+$AUTOCONF
