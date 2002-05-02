6ebd0bf36d9da0d6d1b14b1f68271e978f05c846
Allen Bierbaum <abierbaum@gmail.com>
2002-05-02 21:07:02 +0000

    Added test (that fails) for ConfigChunk's.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@8551 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/jackal/test/TestSuite/TestCases/ConfigChunkTest.h
@@ -0,0 +1,87 @@
+#ifndef _CONFIG_CHUNK_TEST_H
+#define _CONFIG_CHUNK_TEST_H
+
+#include <iostream>
+
+#include <cppunit/TestCase.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCaller.h>
+
+#include <jccl/Config/VarValue.h>
+#include <jccl/Config/ConfigChunk.h>
+#include <jccl/Config/ConfigChunkDB.h>
+#include <jccl/Config/ChunkFactory.h>
+
+/*****************************************************************
+ tests out the functionality expected of enumerations in ConfigChunks
+*******************************************************************/
+
+namespace jcclTest
+{
+
+class ConfigChunkTest : public CppUnit::TestCase
+{
+public:
+   ConfigChunkTest() : CppUnit::TestCase ()
+   {
+   }
+    
+   ConfigChunkTest(std::string name) : CppUnit::TestCase (name)
+   {
+   }
+   
+   virtual ~ConfigChunkTest()
+   {
+   }
+
+
+   void PropertyValueTests () 
+   {
+      std::string file_path(TESTFILES_PATH);
+
+      // Load the test configuration file
+      jccl::ConfigChunkDB chunkdb;
+      jccl::ChunkFactory::instance()->loadDescs (file_path + "ConfigChunkTest/ConfigChunkTest.desc");
+      chunkdb.load (file_path + "ConfigChunkTest/ConfigChunkTest.config");
+
+      // Check the chunks
+      jccl::ConfigChunkPtr chunk1 = chunkdb.getChunk("Chunk1");
+      CPPUNIT_ASSERT (chunk1.get() != NULL);
+
+      CPPUNIT_ASSERT( std::string("Chunk1") == chunk1->getName() );
+      CPPUNIT_ASSERT( int(chunk1->getProperty("single_int")) == 21 );
+      CPPUNIT_ASSERT( float(chunk1->getProperty("single_float")) == 21.75 );
+
+      CPPUNIT_ASSERT( chunk1->getNum("var_int") == 2 );
+      CPPUNIT_ASSERT( chunk1->getNum("var_float") == 2 );
+
+      CPPUNIT_ASSERT( float(chunk1->getProperty("var_float", 0)) == 12.21f );
+      CPPUNIT_ASSERT( float(chunk1->getProperty("var_float", 1)) == 19.75f );
+
+      CPPUNIT_ASSERT( int(chunk1->getProperty("var_int", 0)) == 12 );
+      CPPUNIT_ASSERT( int(chunk1->getProperty("var_int", 1)) == 21 );
+
+      CPPUNIT_ASSERT( std::string("test string") == std::string( chunk1->getProperty("single_string") ));
+
+      CPPUNIT_ASSERT( chunk1->getNum("single_chunk") == 1);
+      jccl::ConfigChunkPtr chunk2;
+      chunk2 = chunk1->getProperty("single_chunk",0);
+
+      CPPUNIT_ASSERT(chunk2.get() != NULL);
+      CPPUNIT_ASSERT( int(chunk2->getProperty("single_int")) == 21);
+      
+    }
+        
+
+   static CppUnit::Test* suite()
+   {
+      CppUnit::TestSuite* test_suite = new CppUnit::TestSuite("ConfigChunkTest");
+      test_suite->addTest( new CppUnit::TestCaller<ConfigChunkTest>("PropertyValueTests", &ConfigChunkTest::PropertyValueTests));
+      
+      return test_suite;
+   }
+};
+
+}
+
+#endif
