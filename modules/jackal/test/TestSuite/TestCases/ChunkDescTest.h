c02b018c8536e77d15fab09c1302dbf7e57b3aa5
Kevin Meinert <subatomic@users.sourceforge.net>
2002-05-04 20:00:27 +0000

    whitespace change
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@8578 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/test/TestSuite/TestCases/ChunkDescTest.h
+++ modules/jackal/test/TestSuite/TestCases/ChunkDescTest.h
@@ -11,71 +11,64 @@
 #include <jccl/Config/ConfigChunk.h>
 #include <jccl/Config/ChunkDesc.h>
 
-/*****************************************************************
- tests out the functionality expected of jccl::ChunkDesc
-*******************************************************************/
-
 namespace jcclTest
 {
+   class ChunkDescTest : public CppUnit::TestCase
+   {
+   public:
+      ChunkDescTest() : CppUnit::TestCase()
+      {
+      }
 
-class ChunkDescTest : public CppUnit::TestCase
-{
-public:
-    ChunkDescTest() : CppUnit::TestCase ()
-    {
-    }
-    
-    ChunkDescTest(std::string name) : CppUnit::TestCase (name)
-    {
-    }
+      ChunkDescTest( std::string name ) : CppUnit::TestCase( name )
+      {
+      }
 
-    virtual ~ChunkDescTest()
-    {
-    }
+      virtual ~ChunkDescTest()
+      {
+      }
 
+      void BasicFunctions() 
+      {
+         std::string name = "test desc 1";
+         std::string token = "test_desc_1";
+         std::string help = "test desc help info";
 
-    void BasicFunctions () {
-        std::string name = "test desc 1";
-        std::string token = "test_desc_1";
-        std::string help = "test desc help info";
+         jccl::ChunkDesc desc;
 
-        jccl::ChunkDesc desc;
+         desc.assertValid();
 
-        desc.assertValid();
+         desc.setName( name );
+         desc.setToken( token );
+         desc.setHelp( help );
+         CPPUNIT_ASSERT( desc.getName() == name );
+         CPPUNIT_ASSERT( desc.getToken() == token );
+         CPPUNIT_ASSERT( desc.getHelp() == help );
 
-        desc.setName (name);
-        desc.setToken (token);
-        desc.setHelp (help);
-        CPPUNIT_ASSERT (desc.getName() == name);
-        CPPUNIT_ASSERT (desc.getToken() == token);
-        CPPUNIT_ASSERT (desc.getHelp() == help);
+         jccl::ChunkDesc desc2( desc );
 
-        jccl::ChunkDesc desc2 (desc);
+         desc2.assertValid();
 
-        desc2.assertValid();
+         CPPUNIT_ASSERT( desc2.getName() == name );
+         CPPUNIT_ASSERT( desc2.getToken() == token );
+         CPPUNIT_ASSERT( desc2.getHelp() == help );
 
-        CPPUNIT_ASSERT (desc2.getName() == name);
-        CPPUNIT_ASSERT (desc2.getToken() == token);
-        CPPUNIT_ASSERT (desc2.getHelp() == help);
+         CPPUNIT_ASSERT( desc2 == desc );
+      }
 
-        CPPUNIT_ASSERT (desc2 == desc);
+      void PropertyDescTests()
+      {
+      }
 
-    }
+      static CppUnit::Test* suite()
+      {
+          CppUnit::TestSuite* test_suite = new CppUnit::TestSuite( "ChunkDescTest" );
+          test_suite->addTest( new CppUnit::TestCaller<ChunkDescTest>( "BasicFunctions", &ChunkDescTest::BasicFunctions ) );
+          test_suite->addTest( new CppUnit::TestCaller<ChunkDescTest>( "PropertyDescTests", &ChunkDescTest::PropertyDescTests ) );
 
-
-    void PropertyDescTests () {
-    }
-        
-
-    static CppUnit::Test* suite()
-    {
-        CppUnit::TestSuite* test_suite = new CppUnit::TestSuite("ChunkDescTest");
-        test_suite->addTest( new CppUnit::TestCaller<ChunkDescTest>("BasicFunctions", &ChunkDescTest::BasicFunctions));
-        test_suite->addTest( new CppUnit::TestCaller<ChunkDescTest>("PropertyDescTests", &ChunkDescTest::PropertyDescTests));
-        
-        return test_suite;
-    }
-};
+          return test_suite;
+      }
+   };
 
 }
 
