97b1fdfa0dcc64099a0edc7620032a22477d4e5d
Ben Scott <nonchocoboy@users.sourceforge.net>
2002-05-28 21:42:24 +0000

    Moved generic wizard framework from the JCCL module to Tweek.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9051 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/tweek/extensions/java/org/vrjuggler/tweek/wizard/Wizard.java
@@ -0,0 +1,135 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+package org.vrjuggler.jccl.wizard;
+
+import java.io.Serializable;
+
+/**
+ * Describes collection of WizardPanes in a particular sequence with the
+ * expectation that the user will walk through them from beginning to end. A
+ * particular wizard can be serialized at design time and brought back to life
+ * when the user requests it.
+ * <p>
+ * A wizard contains a sequence of wizard steps which are processed linearly.
+ * Any of these steps may be another sequence of wizard steps that offshoot from
+ * the root sequence.
+ * <p>
+ * While to the user a wizard appears to be a linear iteration through wizard
+ * panes, a wizard is really stored as a tree. Since the choice of what wizard
+ * pane to show next could rely on choices the user made in the previous pane,
+ * it seems logical that a wizard should be implemented as a tree of wizard
+ * panes connected by the logic conditions that decide which pane to show next.
+ */
+public class Wizard
+   extends RootWizardSequence
+   implements Serializable
+{
+   /**
+    * Creates a new wizard with no steps, no name and no title.
+    */
+   public Wizard()
+   {
+      name = "Wizard";
+      title = "";
+   }
+
+   /**
+    * Gets the name of this wizard.
+    *
+    * @return  the name
+    */
+   public String getName()
+   {
+      return name;
+   }
+
+   /**
+    * Sets the name of this wizard.
+    *
+    * @param   the name
+    */
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   /**
+    * Gets the title for this wizard.
+    *
+    * @return  the title
+    */
+   public String getTitle()
+   {
+      return title;
+   }
+
+   /**
+    * Sets the title of this wizard.
+    *
+    * @param   the title
+    */
+   public void setTitle(String title)
+   {
+      this.title = title;
+   }
+
+   /**
+    * Gets the sequence that makes up this wizard.
+    */
+   public WizardSequence getSequence()
+   {
+      return this;
+   }
+
+   /**
+    * Gets the iterator that may be used to walk through this wizard.
+    */
+   public WizardStepIterator iterator()
+   {
+      return new WizardIterator(this.begin());
+   }
+
+   /**
+    * The name of this wizard.
+    */
+   private String name;
+
+   /**
+    * The title of this wizard.
+    */
+   private String title;
+
+   /**
+    * The root sequence for this wizard.
+    */
+   private RootWizardSequence sequence;
+}
---
+++ modules/tweek/extensions/java/org/vrjuggler/tweek/wizard/builder/WizardStepChooserDialog.java
@@ -0,0 +1,193 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+package org.vrjuggler.jccl.wizard.builder;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import org.vrjuggler.jccl.wizard.*;
+
+/**
+ * Dialog that lets the user choose a WizardStep.
+ */
+public class WizardStepChooserDialog
+   extends JDialog
+{
+   JPanel basePnl = new JPanel();
+   BorderLayout baseLayout = new BorderLayout();
+   JPanel msgPanel = new JPanel();
+   BorderLayout msgPanelLayout = new BorderLayout();
+   JTextArea msgText = new JTextArea();
+   JTabbedPane tabPane = new JTabbedPane();
+   JScrollPane paneScrollPane = new JScrollPane();
+   JList panesList = new JList();
+   JScrollPane sequenceScrollPane = new JScrollPane();
+   JList sequencesList = new JList();
+   JPanel buttonsPanel = new JPanel();
+   JButton okBtn = new JButton();
+   FlowLayout buttonsLayout = new FlowLayout();
+   JButton cancelBtn = new JButton();
+
+   public WizardStepChooserDialog()
+   {
+      this(null);
+   }
+
+   public WizardStepChooserDialog(Frame owner)
+   {
+      this(owner, "Wizard Step Chooser", true);
+   }
+
+   public WizardStepChooserDialog(Frame owner, String title, boolean modal)
+   {
+      super(owner, title, modal);
+
+      try
+      {
+         jbInit();
+      }
+      catch(Exception e)
+      {
+         e.printStackTrace();
+      }
+
+      // Set the help text
+      msgText.setText("Please choose a Wizard Step to add to your Wizard.");
+
+      // Setup our lists
+      panesModel = new DefaultListModel();
+      panesList.setModel(panesModel);
+      panesList.setCellRenderer(new WizardStepListCellRenderer());
+      sequencesModel = new DefaultListModel();
+      sequencesList.setModel(sequencesModel);
+      sequencesList.setCellRenderer(new WizardStepListCellRenderer());
+
+      // Add in the default panes
+      panesModel.addElement("org.vrjuggler.jccl.wizard.panes.WelcomePane");
+      panesModel.addElement("org.vrjuggler.vrjconfig.wizard.panes.SimDisplayPane");
+   }
+
+   private void jbInit() throws Exception
+   {
+      setSize(400, 300);
+      basePnl.setLayout(baseLayout);
+      msgPanel.setLayout(msgPanelLayout);
+      msgText.setBackground(UIManager.getColor("Label.background"));
+      msgText.setFont(new java.awt.Font("Dialog", 1, 12));
+      msgText.setForeground(UIManager.getColor("Label.foreground"));
+      msgText.setBorder(null);
+      msgText.setOpaque(false);
+      msgText.setEditable(false);
+      msgText.setLineWrap(true);
+      okBtn.setText("OK");
+      okBtn.addActionListener(new java.awt.event.ActionListener()
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            okPressed();
+         }
+      });
+      buttonsPanel.setLayout(buttonsLayout);
+      buttonsLayout.setAlignment(FlowLayout.RIGHT);
+      cancelBtn.setText("Cancel");
+      cancelBtn.addActionListener(new java.awt.event.ActionListener()
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            cancelPressed();
+         }
+      });
+      buttonsPanel.setBorder(BorderFactory.createEtchedBorder());
+      msgPanel.setBorder(BorderFactory.createEtchedBorder());
+      buttonsPanel.add(cancelBtn, null);
+      this.getContentPane().add(basePnl, BorderLayout.CENTER);
+      basePnl.add(msgPanel, BorderLayout.NORTH);
+      msgPanel.add(msgText, BorderLayout.NORTH);
+      basePnl.add(tabPane,  BorderLayout.CENTER);
+      tabPane.add(paneScrollPane,    "Panes");
+      paneScrollPane.getViewport().add(panesList, null);
+      tabPane.add(sequenceScrollPane,  "Sequences");
+      sequenceScrollPane.getViewport().add(sequencesList, null);
+      basePnl.add(buttonsPanel,  BorderLayout.SOUTH);
+      buttonsPanel.add(okBtn, null);
+      tabPane.setSelectedIndex(0);
+   }
+
+   /**
+    * Handle cancel action.
+    */
+   void cancelPressed()
+   {
+      selectedItem = null;
+      this.setVisible(false);
+   }
+
+   /**
+    * Handle ok action.
+    */
+   void okPressed()
+   {
+      int tabIdx = tabPane.getSelectedIndex();
+      // get the selected item
+      if (tabIdx == 0)
+      {
+         selectedItem = (String)panesList.getSelectedValue();
+      }
+      else
+      {
+         selectedItem = (String)sequencesList.getSelectedValue();
+      }
+
+      this.setVisible(false);
+   }
+
+   /**
+    * The step that was chosen through this dialog.
+    */
+   String selectedItem;
+
+   DefaultListModel panesModel, sequencesModel;
+}
+
+class WizardStepListCellRenderer
+   extends DefaultListCellRenderer
+{
+   public Component getListCellRendererComponent(JList list, Object value,
+                                                 int index, boolean sel,
+                                                 boolean focus)
+   {
+      super.getListCellRendererComponent(list, value, index, sel, focus);
+      String cn = (String)value;
+      setText(cn.substring(cn.lastIndexOf('.')+1));
+      return this;
+   }
+}
