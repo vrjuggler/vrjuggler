5ed64b2f8efb785281611b3453df9807eafb7018
Patrick Hartling <patrick.hartling@gmail.com>
2002-06-05 13:24:24 +0000

    Changed the #include lines to point to the files Justin added.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@9156 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/deprecated/Math/vjMatrix.h
+++ juggler/modules/vrjuggler/deprecated/Math/vjMatrix.h
@@ -1,7 +1,7 @@
 #ifndef COMPATIBILITY_HEADER_VJ_MATRIX
 #define COMPATIBILITY_HEADER_VJ_MATRIX
 
-#include <vrj/Math/Matrix.h>
+#include <deprecated/Math/Matrix.h>
 
 typedef vrj::Matrix vjMatrix;
 
