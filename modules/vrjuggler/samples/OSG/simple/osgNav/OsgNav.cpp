0abeb27c295dbe3e6cecde9950170a0f7fb68397
Patrick Hartling <patrick.hartling@gmail.com>
2002-06-05 22:03:51 +0000

    Blanket copyright update.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9187 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/OSG/simple/osgNav/OsgNav.cpp
+++ modules/vrjuggler/samples/OSG/simple/osgNav/OsgNav.cpp
@@ -1,11 +1,11 @@
 /*************** <auto-copyright.pl BEGIN do not edit this line> **************
  *
- * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ * VR Juggler is (C) Copyright 1998-2002 by Iowa State University
  *
  * Original Authors:
- *  Allen Bierbaum, Christopher Just,
- *  Patrick Hartling, Kevin Meinert,
- *  Carolina Cruz-Neira, Albert Baker
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
