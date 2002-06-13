9f64ae744fe35bf235314e19a7a2186a5af501d3
Patrick Hartling <patrick.hartling@gmail.com>
2002-06-13 17:44:27 +0000

    Reformatted to conform to the coding standard.  This renames member variables to have more meaningful names.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@9278 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrj/Draw/OGL/GlWindowWin32.cpp
+++ juggler/modules/vrjuggler/vrj/Draw/OGL/GlWindowWin32.cpp
@@ -46,71 +46,77 @@ namespace vrj
 // - Registers new window with the window list
 int GlWindowWin32::open()
 {
-    if(false == GlWindowWin32::registerWindowClass())
-    {
-        return 0;
-    }
+   if ( false == GlWindowWin32::registerWindowClass() )
+   {
+      return 0;
+   }
 
-   if(window_is_open)
+   if ( window_is_open )
+   {
       return 1;
+   }
 
-    HMODULE hMod = GetModuleHandle(NULL);
-        DWORD style;
-        int root_height;
+   HMODULE hMod = GetModuleHandle(NULL);
+   DWORD style;
+   int root_height;
 
-        // OpenGL requires WS_CLIPCHILDREN and WS_CLIPSIBLINGS.
-        style = WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
+   // OpenGL requires WS_CLIPCHILDREN and WS_CLIPSIBLINGS.
+   style = WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
 
-        // If we want a border, create an overlapped window.  This will have
-        // a titlebar and a border.
-        if ( border ) {
-            vprDEBUG(vrjDBG_DRAW_MGR, 5) << "attempting to give window a border"
-                                       << std::endl << vprDEBUG_FLUSH;
-            style |= WS_OVERLAPPEDWINDOW;
-        }
-        // Otherwise, come as close as possible to having no border by using
-        // the thin-line border.
-        else {
-            vprDEBUG(vrjDBG_DRAW_MGR, 5) << "attempting to make window borderless"
-                                       << std::endl << vprDEBUG_FLUSH;
-            style |= WS_OVERLAPPED | WS_POPUP | WS_VISIBLE;
-        }
+   // If we want a border, create an overlapped window.  This will have
+   // a titlebar and a border.
+   if ( border )
+   {
+      vprDEBUG(vrjDBG_DRAW_MGR, 5) << "attempting to give window a border"
+         << std::endl << vprDEBUG_FLUSH;
+      style |= WS_OVERLAPPEDWINDOW;
+   }
+   // Otherwise, come as close as possible to having no border by using
+   // the thin-line border.
+   else
+   {
+      vprDEBUG(vrjDBG_DRAW_MGR, 5) << "attempting to make window borderless"
+         << std::endl << vprDEBUG_FLUSH;
+      style |= WS_OVERLAPPED | WS_POPUP | WS_VISIBLE;
+   }
 
-        root_height = GetSystemMetrics(SM_CYSCREEN);
+   root_height = GetSystemMetrics(SM_CYSCREEN);
 
-    // Create the main application window
-        hWnd = CreateWindow(GL_WINDOW_WIN32_CLASSNAME,
-                            GL_WINDOW_WIN32_CLASSNAME, style,
-                            origin_x, root_height - origin_y - window_height,
-                            window_width, window_height, NULL, NULL, hMod,
-                            NULL);
+   // Create the main application window
+   mWinHandle = CreateWindow(GL_WINDOW_WIN32_CLASSNAME,
+                             GL_WINDOW_WIN32_CLASSNAME, style,
+                             origin_x, root_height - origin_y - window_height,
+                             window_width, window_height, NULL, NULL, hMod,
+                             NULL);
 
-    // If window was not created, quit
-    if(NULL == hWnd)
-        return 0;
+   // If window was not created, quit
+   if ( NULL == mWinHandle )
+   {
+      return 0;
+   }
 
-    // We have a valid window, so... Create the context
-    //case WM_CREATE:
-    hDC = GetDC(hWnd);                   // Store the device context
-    if(false == setPixelFormat(hDC))        // Select the pixel format
-    {
-        return 0;
-    }
+   // We have a valid window, so... Create the context
+   //case WM_CREATE:
+   mDeviceContext = GetDC(mWinHandle);            // Store the device context
+   if ( false == setPixelFormat(mDeviceContext) ) // Select the pixel format
+   {
+      return 0;
+   }
 
-    // Create the rendering context and make it current
-    hRC = wglCreateContext(hDC);
-    vprASSERT(hRC != NULL);
-    wglMakeCurrent(hDC, hRC);
+   // Create the rendering context and make it current
+   mRenderContext = wglCreateContext(mDeviceContext);
+   vprASSERT(mRenderContext != NULL);
+   wglMakeCurrent(mDeviceContext, mRenderContext);
 
-    // Register the window with the window list
-    GlWindowWin32::addWindow(hWnd,this);
+   // Register the window with the window list
+   GlWindowWin32::addWindow(mWinHandle,this);
 
-    // Display the window
-    ShowWindow(hWnd,SW_SHOW);
-    UpdateWindow(hWnd);             // Tell the window to paint
+   // Display the window
+   ShowWindow(mWinHandle, SW_SHOW);
+   UpdateWindow(mWinHandle);             // Tell the window to paint
    window_is_open = true;
 
-    return 1;
+   return 1;
 }
 
 
@@ -120,16 +126,18 @@ int GlWindowWin32::open()
  */
 int GlWindowWin32::close()
 {
-    if(!window_is_open)
+   if ( !window_is_open )
+   {
       return false;
+   }
 
    // Remove window from window list
-    GlWindowWin32::removeWindow(hWnd);
+   GlWindowWin32::removeWindow(mWinHandle);
 
-    window_is_open = false;
+   window_is_open = false;
 
    // destroy the win32 window
-    return (1 == DestroyWindow(hWnd));
+   return(1 == DestroyWindow(mWinHandle));
 }
 
 /**
@@ -138,171 +146,177 @@ int GlWindowWin32::close()
  */
 bool GlWindowWin32::makeCurrent()
 {
-    vprASSERT((hDC != NULL) && (hRC != NULL));
-    wglMakeCurrent(hDC, hRC);       // Make our context current
-    return true;
+   vprASSERT((mDeviceContext != NULL) && (mRenderContext != NULL));
+   wglMakeCurrent(mDeviceContext, mRenderContext);  // Make our context current
+   return true;
 }
 
 // Swap the front and back buffers
 // Process events here
 void GlWindowWin32::swapBuffers()
 {
-    vprASSERT(hDC != NULL);
-    SwapBuffers(hDC);
+   vprASSERT(mDeviceContext != NULL);
+   SwapBuffers(mDeviceContext);
 }
 
 void GlWindowWin32::checkEvents()
 {
    MSG win_message;
 
-   while(PeekMessage(&win_message,NULL,0,0,PM_REMOVE))
-    {
-        // Test if quit
-        if(win_message.message == WM_QUIT)
-            break;
+   while ( PeekMessage(&win_message,NULL,0,0,PM_REMOVE) )
+   {
+      // Test if quit
+      if ( win_message.message == WM_QUIT )
+      {
+         break;
+      }
 
-        TranslateMessage(&win_message);     // Translate the accelerator keys
-        DispatchMessage(&win_message);      // Send to the WinProc
-    }
+      TranslateMessage(&win_message);     // Translate the accelerator keys
+      DispatchMessage(&win_message);      // Send to the WinProc
+   }
 }
 
 
 // WindowProcedure to deal with the events generated.
 // Called only for the window that we are controlling
-LRESULT GlWindowWin32::handleEvent(HWND     hWnd,
-                                          UINT  message,
-                                          WPARAM    wParam,
-                                          LPARAM    lParam)
+LRESULT GlWindowWin32::handleEvent(HWND hWnd, UINT message, WPARAM wParam,
+                                   LPARAM lParam)
 {
-    switch (message)
-    {
-        // ---- Window creation, setup for OpenGL ---- //
-    case WM_CREATE:
-        vprASSERT(false);                               // Should never get called because
-                                                        //we are not registered when this gets called
+   switch ( message )
+   {
+      // ---- Window creation, setup for OpenGL ---- //
+      case WM_CREATE:
+         vprASSERT(false);                               // Should never get called because
+                                                         //we are not registered when this gets called
 
-        hDC = GetDC(hWnd);                   // Store the device context
-        if(false == setPixelFormat(hDC))        // Select the pixel format
-        {
+         mDeviceContext = GetDC(hWnd);                   // Store the device context
+         if ( false == setPixelFormat(mDeviceContext) )  // Select the pixel format
+         {
             return 0;
-        }
+         }
 
-        // Create the rendering context and make it current
-        hRC = wglCreateContext(hDC);
-        wglMakeCurrent(hDC, hRC);
-        break;
+         // Create the rendering context and make it current
+         mRenderContext = wglCreateContext(mDeviceContext);
+         wglMakeCurrent(mDeviceContext, mRenderContext);
+         break;
 
-        // ---- Window is being destroyed, cleanup ---- //
-    case WM_DESTROY:
+         // ---- Window is being destroyed, cleanup ---- //
+      case WM_DESTROY:
 
-        // Deselect the current rendering context and delete it
-        wglMakeCurrent(hDC,NULL);
-        wglDeleteContext(hRC);
+         // Deselect the current rendering context and delete it
+         wglMakeCurrent(mDeviceContext, NULL);
+         wglDeleteContext(mRenderContext);
 
-        // Tell the application to terminate after the window
-        // is gone.
-        PostQuitMessage(0);
-        break;
+         // Tell the application to terminate after the window
+         // is gone.
+         PostQuitMessage(0);
+         break;
 
-        // --- Window is resized. --- //
-    case WM_SIZE:
-        // Call our function which modifies the clipping
-        // volume and viewport
-        sizeChanged(LOWORD(lParam), HIWORD(lParam));
-        break;
+         // --- Window is resized. --- //
+      case WM_SIZE:
+         // Call our function which modifies the clipping
+         // volume and viewport
+         sizeChanged(LOWORD(lParam), HIWORD(lParam));
+         break;
 
 
-        // The painting function.  This message sent by Windows
-        // whenever the screen needs updating.
-    case WM_PAINT:
-        {
+         // The painting function.  This message sent by Windows
+         // whenever the screen needs updating.
+      case WM_PAINT:
+         {
             PAINTSTRUCT   ps;           // Paint structure
             BeginPaint(hWnd, &ps);  // Validate the drawing of the window
             EndPaint(hWnd, &ps);
-        }
-        break;
+         }
+         break;
 
-    default:   // Passes it on if unproccessed
-        return (DefWindowProc(hWnd, message, wParam, lParam));
+      default:   // Passes it on if unproccessed
+         return(DefWindowProc(hWnd, message, wParam, lParam));
 
-    }
+   }
 
-    return (0L);
+   return(0L);
 }
 
 // Set the pixel format for the given window
 bool GlWindowWin32::setPixelFormat(HDC hDC)
 {
-    int pixel_format;
-    PIXELFORMATDESCRIPTOR pfd;
-    match = NULL;
+   int pixel_format;
+   PIXELFORMATDESCRIPTOR pfd;
+   mMatch = NULL;
 
-    memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
-    pfd.nSize = (sizeof(PIXELFORMATDESCRIPTOR));
-    pfd.nVersion = 1;
+   memset(&pfd, 0, sizeof(PIXELFORMATDESCRIPTOR));
+   pfd.nSize = (sizeof(PIXELFORMATDESCRIPTOR));
+   pfd.nVersion = 1;
 
-    /* Defaults. */
-    pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW | PFD_DOUBLEBUFFER;
+   /* Defaults. */
+   pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW | PFD_DOUBLEBUFFER;
 
-    if ( mDisplay->inStereo() )
-    {
-        in_stereo = true;
-        pfd.dwFlags |= PFD_STEREO;
-    }
-    else
-    {
-        in_stereo = false;
-    }
+   if ( mDisplay->inStereo() )
+   {
+      in_stereo = true;
+      pfd.dwFlags |= PFD_STEREO;
+   }
+   else
+   {
+      in_stereo = false;
+   }
 
-    pfd.iPixelType = PFD_TYPE_RGBA;
-    pfd.cColorBits = 32;
-    pfd.cRedBits = 8; /* Try to get the maximum. */
+   pfd.iPixelType = PFD_TYPE_RGBA;
+   pfd.cColorBits = 32;
+   pfd.cRedBits = 8; /* Try to get the maximum. */
    pfd.cGreenBits = 8;
    pfd.cBlueBits = 8;
    pfd.cAlphaBits = 8;
    pfd.cDepthBits = 32;
    pfd.cStencilBits = 0;
-    pfd.cAccumBits = 0;
-    pfd.cAuxBuffers = 0;
+   pfd.cAccumBits = 0;
+   pfd.cAuxBuffers = 0;
 
-    // Let Win32 choose one for us
-    pixel_format = ChoosePixelFormat(hDC, &pfd);
-    if (pixel_format > 0)
-    {
-        match = (PIXELFORMATDESCRIPTOR *) malloc(sizeof(PIXELFORMATDESCRIPTOR));
-        DescribePixelFormat(hDC, pixel_format, sizeof(PIXELFORMATDESCRIPTOR), match);
+   // Let Win32 choose one for us
+   pixel_format = ChoosePixelFormat(hDC, &pfd);
+   if ( pixel_format > 0 )
+   {
+      mMatch = (PIXELFORMATDESCRIPTOR *) malloc(sizeof(PIXELFORMATDESCRIPTOR));
+      DescribePixelFormat(hDC, pixel_format, sizeof(PIXELFORMATDESCRIPTOR),
+                          mMatch);
 
-    /* ChoosePixelFormat is dumb in that it will return a pixel
-       format that doesn't have stereo even if it was requested
-       so we need to make sure that if stereo was selected, we
-       got it. */
-        if (mDisplay->inStereo())
-        {
-            if (!(match->dwFlags & PFD_STEREO))
-            {
-                free(match);
-                return NULL;
-            }
-        }
-    }
+      /* ChoosePixelFormat is dumb in that it will return a pixel
+         format that doesn't have stereo even if it was requested
+         so we need to make sure that if stereo was selected, we
+         got it. */
+      if ( mDisplay->inStereo() )
+      {
+         if ( !(mMatch->dwFlags & PFD_STEREO) )
+         {
+            free(mMatch);
+            return NULL;
+         }
+      }
+   }
 
-    // Set the pixel format for the device context
-    SetPixelFormat(hDC, pixel_format, &pfd);
-    return true;
+   // Set the pixel format for the device context
+   SetPixelFormat(hDC, pixel_format, &pfd);
+   return true;
 }
 
 // the user has changed the size of the window
 void GlWindowWin32::sizeChanged(long width, long height)
 {
-    window_width = width;
-    window_height = height;
+   window_width = width;
+   window_height = height;
 
-    if(window_width == 0)        // Make sure we don't have window of 1 size (divide by zero would follow)
-        window_width = 1;
-    if(window_height == 0)
-        window_height = 1;
+   if ( window_width == 0 )        // Make sure we don't have window of 1 size (divide by zero would follow)
+   {
+      window_width = 1;
+   }
 
-    // XXX: Should reset viewport here
+   if ( window_height == 0 )
+   {
+      window_height = 1;
+   }
+
+   // XXX: Should reset viewport here
 }
 
 
@@ -310,92 +324,102 @@ void GlWindowWin32::sizeChanged(long width, long height)
 /**
  * Global Window event handler.
  */
-LRESULT CALLBACK GlWindowWin32::WndProc(    HWND    hWnd,
-                                                UINT    message,
-                                                WPARAM  wParam,
-                                                LPARAM  lParam)
+LRESULT CALLBACK GlWindowWin32::WndProc(HWND hWnd, UINT message,
+                                        WPARAM wParam, LPARAM lParam)
 {
-    GlWindowWin32* glWin = getGlWin(hWnd);
+   GlWindowWin32* glWin = getGlWin(hWnd);
 
-    if(glWin != NULL)       // Message for one of ours
-    {
-        return glWin->handleEvent(hWnd, message, wParam, lParam);
-    }
-    else
-    {
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
+   if ( glWin != NULL )       // Message for one of ours
+   {
+      return glWin->handleEvent(hWnd, message, wParam, lParam);
+   }
+   else
+   {
+      return DefWindowProc(hWnd, message, wParam, lParam);
+   }
 }
 
 /**
  * Window registration.
  */
 bool GlWindowWin32::mWinRegisteredClass = false;
-WNDCLASS GlWindowWin32::winClass;           // The window class to register
-std::map<HWND, GlWindowWin32*> GlWindowWin32::glWinMap;
+WNDCLASS GlWindowWin32::mWinClass;           // The window class to register
+std::map<HWND, GlWindowWin32*> GlWindowWin32::mGlWinMap;
 
 
 bool GlWindowWin32::registerWindowClass()
 {
-    if(mWinRegisteredClass)
-        return true;
+   if ( mWinRegisteredClass )
+   {
+      return true;
+   }
 
-    char lpszAppName[1024];
-    GetModuleFileName(NULL,lpszAppName,sizeof(lpszAppName));
+   char lpszAppName[1024];
+   GetModuleFileName(NULL,lpszAppName,sizeof(lpszAppName));
 
-    mWinRegisteredClass = true;     // We have registered now
+   mWinRegisteredClass = true;     // We have registered now
 
-    HINSTANCE hInstance = GetModuleHandle(NULL);
+   HINSTANCE hInstance = GetModuleHandle(NULL);
 
-    // Register Window style
-    winClass.style              = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-    winClass.lpfnWndProc        = (WNDPROC)GlWindowWin32::WndProc;
-    winClass.cbClsExtra     = 0;
-    winClass.cbWndExtra     = 0;
-    winClass.hInstance      = hInstance;            // Get handle to the module that created current process
-    winClass.hIcon              = LoadIcon(NULL, IDI_WINLOGO);;
-    winClass.hCursor            = LoadCursor(NULL, IDC_ARROW);
+   // Register Window style
+   mWinClass.style       = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+   mWinClass.lpfnWndProc = (WNDPROC)GlWindowWin32::WndProc;
+   mWinClass.cbClsExtra  = 0;
+   mWinClass.cbWndExtra  = 0;
+   mWinClass.hInstance   = hInstance;            // Get handle to the module that created current process
+   mWinClass.hIcon       = LoadIcon(NULL, IDI_WINLOGO);;
+   mWinClass.hCursor     = LoadCursor(NULL, IDC_ARROW);
 
-    // No need for background brush for OpenGL window
-    winClass.hbrBackground  = NULL;
+   // No need for background brush for OpenGL window
+   mWinClass.hbrBackground  = NULL;
 
-    winClass.lpszMenuName   = NULL;
-    winClass.lpszClassName  = GL_WINDOW_WIN32_CLASSNAME;
+   mWinClass.lpszMenuName   = NULL;
+   mWinClass.lpszClassName  = GL_WINDOW_WIN32_CLASSNAME;
 
-    // Register the window class
-    if(RegisterClass(&winClass) == 0)
-        return false;
-    else
-        return true;
-};
+   // Register the window class
+   if ( RegisterClass(&mWinClass) == 0 )
+   {
+      return false;
+   }
+   else
+   {
+      return true;
+   }
+}
 
 //----------------------//
 //      WinList         //
 //----------------------//
 void GlWindowWin32::addWindow(HWND handle, GlWindowWin32* glWin)
 {
-    vprASSERT(glWin != NULL);
+   vprASSERT(glWin != NULL);
 
-    if(glWinMap.find(handle) == glWinMap.end())     // Not already there
-        glWinMap[handle] = glWin;
-    //else
-    // vprASSERT(false);
+   if ( mGlWinMap.find(handle) == mGlWinMap.end() )     // Not already there
+   {
+      mGlWinMap[handle] = glWin;
+   }
+   //else
+   // vprASSERT(false);
 }
 
 void GlWindowWin32::removeWindow(HWND handle)
 {
-    glWinMap.erase(handle);     // Erase the entry in the list
+   mGlWinMap.erase(handle);     // Erase the entry in the list
 }
 
 GlWindowWin32* GlWindowWin32::getGlWin(HWND handle)
 {
    std::map<HWND, GlWindowWin32*>::iterator glWinIter;
 
-    glWinIter = glWinMap.find(handle);
-    if(glWinIter == glWinMap.end())     // Not found
-        return NULL;
-    else
-        return (*glWinIter).second;                 // Return the found window
+   glWinIter = mGlWinMap.find(handle);
+   if ( glWinIter == mGlWinMap.end() )     // Not found
+   {
+      return NULL;
+   }
+   else
+   {
+      return(*glWinIter).second;                 // Return the found window
+   }
 }
 
-};
+} // End of vrj namespace
