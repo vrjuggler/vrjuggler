c6e63bee1a2775e8baa59c380c32d3beac43d328
Justin Hare <jahare@users.sourceforge.net>
2002-06-24 19:28:04 +0000

    Added test for reconfiguration of a sim analog device
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9399 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/test/RTRC/reconfigApp.h
+++ modules/vrjuggler/test/RTRC/reconfigApp.h
@@ -26,6 +26,7 @@
 
 #include <gadget/Devices/Sim/SimPosition.h>
 #include <gadget/Devices/Sim/SimDigital.h>
+#include <gadget/Devices/Sim/SimAnalog.h>
 #include <gadget/Devices/Sim/SimInput.h>
 
 #include <jccl/Config/ConfigChunkPtr.h>
