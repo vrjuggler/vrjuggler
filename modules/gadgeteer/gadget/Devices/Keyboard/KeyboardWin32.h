162c0d0b475748b344c794919016071b92d672c2
Kevin Meinert <subatomic@users.sourceforge.net>
2002-07-05 19:06:46 +0000

    Implemented locking and cursor warping, and refactored to allow GlDisplayWin32 to be a keyboard device.
    This refactor also uses keyboard keys as buttons, effectively removing
    the key autorepeat effect seen in the graphics windows.
    
    This class holds some similarity to the KeyboardXwin code now - at least
    between their sequence diagrams...
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9597 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/gadget/Devices/Keyboard/KeyboardWin32.h
+++ modules/gadgeteer/gadget/Devices/Keyboard/KeyboardWin32.h
@@ -45,94 +45,123 @@
 #ifndef _GADGET_KEYBOARD_WIN32_H_
 #define _GADGET_KEYBOARD_WIN32_H_
 
-#include <gadget/gadgetConfig.h>
-
-
 // Get windows stuff
 #include <windows.h>
 #include <commctrl.h>
 
+#include <vpr/Thread/Thread.h>
+#include <vpr/System.h>
+#include <jccl/Config/ConfigChunk.h>
+
+#include <gadget/gadgetConfig.h>
 #include <gadget/Type/Input.h>
 #include <gadget/Type/Keyboard.h>
-#include <vpr/Thread/Thread.h>
+#include <gadget/Util/Debug.h>
+#include <gadget/InputManager.h>
+
+#include <gadget/Devices/Keyboard/KeyboardWin32.h> // my header
+
 
 namespace gadget
 {
 
-class KeyboardWin32 : public Input, public Keyboard
+class GADGET_CLASS_API KeyboardWin32 : public Input, public Keyboard
 {
 public:
+   // Enum to keep track of current lock state for state machine
+   // Unlocked - The mouse is free
+   // Lock_LockKey - The mouse is locked due to lock toggle key press
+   // Lock_LockKeyDown - The mouse is locked due to a key being held down
+   enum lockState { Unlocked, Lock_LockKey, Lock_KeyDown};
 
-   KeyboardWin32()
+   KeyboardWin32() : mControlLoopDone( false ), 
+                     mPrevX( 0 ), 
+                     mPrevY( 0 ), 
+                     mLockState( Unlocked ),
+                     mExitFlag( false ),
+                     mWeOwnTheWindow( true )
    {
-      for (int i =0; i < 256; i++)
-         m_realkeys[i] = m_keys[i] = m_framekeys[i] = 0;
-
-      m_realkeys[0] = m_keys[0] = m_framekeys[0] = 1;
    }
-   ~KeyboardWin32() { stopSampling();}
+   virtual ~KeyboardWin32() 
+   { 
+      stopSampling();
+   }
 
 
    virtual bool config(jccl::ConfigChunkPtr c);
 
+   // Main thread of control for this active object
+   void controlLoop( void* nullParam );
+
    /* Pure Virtuals required by Input */
    int startSampling();
    int stopSampling();
-   int sample() { return 1;}
-   void updateData();
 
-   static std::string getChunkType() { return std::string("Keyboard");}
+   // process the current x-events
+   // Called repetatively by the controlLoop
+   int sample();
+   void updateData();
+   static std::string getChunkType();
 
    // returns the number of times the key was pressed during the
    // last frame, so you can put this in an if to check if was
    // pressed at all, or if you are doing processing based on this
    // catch the actual number..
-   int isKeyPressed(int Key)
-   {  return m_keys[Key];}
+   int isKeyPressed( int Key );
+   virtual int keyPressed( int keyId );
+   virtual bool modifierOnly( int modKey );
 
-   virtual int keyPressed(int keyId)
-   { return isKeyPressed(keyId); }
+protected:
+   virtual void processEvent( UINT message, UINT wParam, LONG lParam ){}
+   void lockMouse();
+   void unlockMouse();
+   friend LONG APIENTRY MenuWndProc( HWND hWnd, UINT message, UINT wParam, LONG lParam );
 
-   virtual bool modifierOnly(int modKey)
-   { return ((onlyModifier(modKey) == 1) ? true : false); }
-
-   void createWindowWin32 ();
-   void updKeys(  UINT message,  UINT wParam, LONG lParam);
-
-public:
    HINSTANCE   m_hInst;
    HWND        m_hWnd;
+   void createWindowWin32();
+   void updKeys( UINT message,  UINT wParam, LONG lParam );
 
-private:
+   //: Handle any events in the system
+   // Copies m_keys to m_curKeys
+   void handleEvents();
+
    /** @name Private functions for processing input data */
    /* Private functions for processing input data */
-     //@{
-   int onlyModifier(int);
-
+   //@{
+   int onlyModifier( int );
+   //@}
 
    /** @name Windows utility functions */
    //@{
-   int VKKeyToKey(int vkKey);
-   char* checkArgs(char* look_for);
+   int VKKeyToKey( int vkKey );
+   char* checkArgs( char* look_for );
 
-   BOOL MenuInit (HINSTANCE hInstance);
+   BOOL MenuInit( HINSTANCE hInstance );
    //@}
 
-
-   int      m_screen, m_x, m_y;
+   bool         mWeOwnTheWindow;       // True if this class owns the window (is reposible for opening, closing, and event processing)                      
+   int          m_screen, m_x, m_y;    // screen id, x_origin, y_origin
    unsigned int m_width,m_height;
 
-   // Use m_keys to know how many times each key was KEYDOWN during the loop
-   // m_realkeys is the current up/down state of each key
-   // m_framekeys is a buffer of the keys that have been pressed
-   // since the last updateData.  It creates a non-thread accurate buffer
-   int m_keys[256];        //: Last frames state, set by the updateData
-   int m_framekeys[256];   //: The keyboard state during a frame of update, without KeyUp events included
-   int m_realkeys[256];    //: The real keyboard state, all events processed
+      /* Keyboard state holders */
+   // NOTE: This driver does not use the normal triple buffering mechanism.
+   // Instead, it just uses a modified double buffering system.
+   int      m_keys[256];         // (0,*): The num key presses during an UpdateData (ie. How many keypress events)
+   int      m_curKeys[256];      // (0,*): Copy of m_keys that the user reads from between updates
+   int      m_realkeys[256];     // (0,1): The real keyboard state, all events processed (ie. what is the key now)
+
+   vpr::Mutex  mKeysLock;           // Must hold this lock when accessing m_keys
+   bool     mExitFlag;           // Should we exit
+
+   lockState   mLockState;       // The current state of locking
+   int         mLockStoredKey;   // The key that was pressed down
+   int         mLockToggleKey;   // The key that toggles the locking
 
    float m_mouse_sensitivity;
-
-   int oldx,oldy,newx,newy;  // Mousemove variables
+   int   mSleepTimeMS;            // Amount of time to sleep in milliseconds between updates
+   int   mPrevX, mPrevY;          // Previous mouse location
+   bool  mControlLoopDone;
 };
 
 };
