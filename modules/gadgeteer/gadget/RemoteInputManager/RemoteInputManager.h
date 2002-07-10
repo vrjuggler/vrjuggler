ba294d4f29f5dc31664b2c71d36ee31efb1e2611
Aron Bierbaum <aronbierbaum@gmail.com>
2002-07-10 19:45:12 +0000

    Fixing some warning errors and debug levels.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9647 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/gadget/RemoteInputManager/RemoteInputManager.h
+++ modules/gadgeteer/gadget/RemoteInputManager/RemoteInputManager.h
@@ -66,7 +66,7 @@ namespace gadget{
    class GADGET_CLASS_API RemoteInputManager
    {
    protected:
-      int                           mIncomingConnections;
+      unsigned                      mIncomingConnections;
       int                           mNumMachines;
       ClusterSync                   mClusterSync;
       bool                          mListenWasInitialized;
