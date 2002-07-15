9b402cdfeb91c76094efd546691218707e87800b
Patrick Hartling <patrick.hartling@gmail.com>
2002-07-15 21:07:26 +0000

    Fixed many problems with the documentation generated by Doxygen simply by using punctuation correctly.  Doxygen relies quite heavily on good use of periods.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@9763 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrj/Draw/Pf/PfApp.h
+++ juggler/modules/vrjuggler/vrj/Draw/Pf/PfApp.h
@@ -48,12 +48,11 @@ namespace vrj
    class Kernel;
 
 /** Encapulates an actual Performer application.
-*
-* This class defines the class that Performer
-*  application classes should be derived from.  The interface
-*  given is the interface that the System expects in order to
-*  interface with the application.
-*/
+ *
+ * This class defines the class that Performer application classes should be
+ * derived from.  The interface given is the interface that the System expects
+ * in order to interface with the application.
+ */
 class PfApp : public App
 {
 public:
@@ -64,65 +63,72 @@ public:
 
    PfApp() {;}
 
-   /** Initialize the scene graph
-   * Called after pfInit & pfConfig but before apiInit
-   */
+   /**
+    * Initializes the scene graph.
+    * Called after pfInit and pfConfig but before apiInit.
+    */
    virtual void initScene() = 0;
 
-   /** Called between pfInit and pfConfig
-   * This function allows the user application to do any processing that needs
-   * to happen before performer forks its processes off but after
-   * pfInit()
-   * @note: Using this function leads to applications that are
-   *         not "switchable"
-   */
+   /**
+    * Called between pfInit and pfConfig.
+    * This function allows the user application to do any processing that needs
+    * to happen before performer forks its processes off but after
+    * pfInit().
+    * @note Using this function leads to applications that are not "switchable".
+    */
    virtual void preForkInit() {;}
 
-   /** Function called in application process for each active channel each frame
-   * Called immediately before draw (pfFrame())
-   * XXX: Should maybe only call this for one "master" channel each frame
-   */
+   /**
+    * Function called in application process for each active channel each frame.
+    * Called immediately before draw (pfFrame()).
+    * XXX: Should maybe only call this for one "master" channel each frame.
+    */
    virtual void appChanFunc(pfChannel* chan) {;}
 
-   /** Return the current scene graph
-   * This function must be defined so that the performer draw manager
-   * can get a scene graph to draw.
-   * @note Do NOT put model loading and manipulation in this function
-   *       instead, put that type of thing in initScene()
-   * @note Make sure that the node returned is NOT a pfScene.
-   *       If it is, then lighting will NOT work.
-   */
+   /**
+    * Returns the current scene graph.
+    * This function must be defined so that the Performer Draw Manager
+    * can get a scene graph to draw.
+    * @note Do NOT put model loading and manipulation in this function
+    *       instead, put that type of thing in initScene().
+    * @note Make sure that the node returned is NOT a pfScene.
+    *       If it is, then lighting will NOT work.
+    */
    virtual pfGroup* getScene() = 0;
 
-   /** Init a pWin
-   * Called by the pf draw manager as soon as the pwin
-   * is opened.
-   */
+   /**
+    * Initializes a pWin.
+    * Called by the Performer Draw Manager as soon as the pwin
+    * is opened.
+    */
    virtual void configPWin(pfPipeWindow* pWin)
    {;}
 
-   /** Return the needed parameters for the performer framebuffer
-   * NOTE: Stereo, doublebuffer, depth buffer, and rgba are all requested by default
-   */
+   /**
+    * Returns the needed parameters for the performer framebuffer.
+    * @note Stereo, doublebuffer, depth buffer, and rgba are all requested by
+    *       default.
+    */
    virtual std::vector<int> getFrameBufferAttrs()
    { return std::vector<int>();}
 
-   /** Function called in the channel draw function to do the actual drawing
-   *
-   * For most programs the default behavior of this function is correct.
-   * It simply does the following: <br>
-   *    chan->clear(); <br>
-   *    pfDraw();      <br>
-   *
-   * Advanced users may want to overide this behavior for advanced rendering effects
-   * such as over-lays, or multi-pass rendering.
-   * See the Performer man pages about overiding the draw traversal function.
-   * This function is the draw traversal function but with the projections
-   * set correctly for the given display and eye
-   *
-   * @pre chan is ready to draw
-   * @post Channel should be drawn
-   */
+   /**
+    * Function called in the channel draw function to do the actual drawing.
+    *
+    * For most programs the default behavior of this function is correct.
+    * It simply does the following: <br>
+    *    chan->clear(); <br>
+    *    pfDraw();      <br>
+    *
+    * Advanced users may want to overide this behavior for advanced rendering
+    * effects such as over-lays, or multi-pass rendering.
+    * See the Performer man pages about overiding the draw traversal function.
+    * This function is the draw traversal function but with the projections
+    * set correctly for the given display and eye.
+    *
+    * @pre chan is ready to draw.
+    * @post Channel should be drawn.
+    */
    virtual void drawChan(pfChannel* chan, void* chandata)
    {
       vprDEBUG_BEGIN(vrjDBG_DRAW_MGR, 4) << "--- drawChan: Entered ---.\n" << vprDEBUG_FLUSH;
@@ -132,20 +138,22 @@ public:
       this->postDrawChan(chan,chandata);
    }
 
-   /** Function called by the DEFAULT drawChan function before clearing the channel
-   * and drawing the next frame (pfFrame())
-   */
+   /**
+    * Function called by the DEFAULT drawChan function before clearing the
+    * channel and drawing the next frame (pfFrame()).
+    */
    virtual void preDrawChan(pfChannel* chan, void* chandata){;}
 
-   /** Function called by the DEFAULT drawChan function after clearing the channel
-   * and drawing the next frame (pfFrame())
-   */
+   /**
+    * Function called by the DEFAULT drawChan function after clearing the
+    * channel and drawing the next frame (pfFrame()).
+    */
    virtual void postDrawChan(pfChannel* chan, void* chandata){;}
 
 public: // -- Factory Function -- //
-   /** Get the DrawManager to use
-   * Returns the Performer draw manager
-   */
+   /**
+    * Gets the Draw Manager to use. Returns the Performer Draw Manager.
+    */
    virtual DrawManager* getDrawManager()
    { return PfDrawManager::instance(); }
 
