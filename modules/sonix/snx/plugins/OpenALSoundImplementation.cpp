d00a13148b29c4ea56a098ce70e168050a07a810
Kevin Meinert <subatomic@users.sourceforge.net>
2002-07-19 21:26:59 +0000

    output formatting
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9857 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/sonix/snx/plugins/OpenALSoundImplementation.cpp
+++ modules/sonix/snx/plugins/OpenALSoundImplementation.cpp
@@ -508,7 +508,7 @@ void OpenALSoundImplementation::bind( const std::string& alias )
          // open the file as readonly binary
          if (!snxFileIO::fileExists( soundInfo.filename.c_str() )) 
          {
-            std::cerr<<"[snx]OpenAL| file doesn't exist: "<<soundInfo.filename<<"\n" << std::flush;
+            std::cerr<<"[snx]OpenAL| file '"<<soundInfo.filename<<"' doesn't exist\n" << std::flush;
             break;
          }
 
