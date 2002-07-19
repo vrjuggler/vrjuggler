03b139734ec76a8025850a36202ea4947ef8d9a0
Kevin Meinert <subatomic@users.sourceforge.net>
2002-07-19 20:50:51 +0000

    sonix singleton is a pointer, not reference.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9855 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Sound/pf/pfSoundNode.cpp
+++ modules/vrjuggler/vrj/Sound/pf/pfSoundNode.cpp
@@ -86,7 +86,7 @@ int pfSoundNode::app(pfTraverser *trav)
       // set my sound's position.
       pfVec3 pf_soundPosition = coord.xyz;
       gmtl::Vec3f soundPosition = vrj::GetVjVec( pf_soundPosition );
-      sonix::instance().setPosition( mSound, soundPosition[0], soundPosition[1], soundPosition[2] );
+      sonix::instance()->setPosition( mSound, soundPosition[0], soundPosition[1], soundPosition[2] );
       
       // Engine's update should be called by the app's frame process,
       // or in juggler's manager (not both, of course)...
@@ -98,7 +98,7 @@ int pfSoundNode::app(pfTraverser *trav)
    {
       // redundant (fixme), but make sure it's 0.0f,0.0f,0.0f
       // this makes the sound the same as the observer.
-      sonix::instance().setPosition( mSound, 0.0f, 0.0f, 0.0f );
+      sonix::instance()->setPosition( mSound, 0.0f, 0.0f, 0.0f );
    }
 
    return pfDCS::app( trav );  // call the parent class's app()
