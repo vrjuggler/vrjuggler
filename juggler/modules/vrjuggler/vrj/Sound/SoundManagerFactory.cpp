ea76d2640ee6c440fa26e671e4f1d64283ee5afe
Kevin Meinert <subatomic@users.sourceforge.net>
2002-07-19 20:16:44 +0000

    remove USE_SONIX juggler depends on sonix now.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@9846 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrj/Sound/SoundManagerFactory.cpp
+++ juggler/modules/vrjuggler/vrj/Sound/SoundManagerFactory.cpp
@@ -32,12 +32,7 @@
 
 #include <vrj/vrjConfig.h>
 
-#ifdef USE_SONIX
 #include <vrj/Sound/SoundManagerSonix.h>
-#else
-#include <vrj/Sound/SoundManager.h>
-#endif
-
 #include <vrj/Sound/SoundManagerFactory.h>
 
 namespace vrj
@@ -45,13 +40,8 @@ namespace vrj
 
    SoundManager& SoundManagerFactory::get()
    {
-#     ifdef USE_SONIX
-         static vrj::SoundManagerSonix temp;
-         return temp;
-#     else
-         static SoundManager temp;
-         return temp;
-#     endif
+      static vrj::SoundManagerSonix temp;
+      return temp;
    }
 
 };
