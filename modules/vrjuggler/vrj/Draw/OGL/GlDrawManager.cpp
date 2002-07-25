700b36acb68c487333fa7986d301aa8477687a12
Allen Bierbaum <abierbaum@gmail.com>
2002-07-25 07:54:35 +0000

    Add call to buffer swap lock.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@9955 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/OGL/GlDrawManager.cpp
+++ modules/vrjuggler/vrj/Draw/OGL/GlDrawManager.cpp
@@ -178,6 +178,12 @@ void GlDrawManager::drawAllPipes()
    for(pipeNum=0; pipeNum<pipes.size(); pipeNum++)
       pipes[pipeNum]->completeRender();
 
+   // Barrier for Cluster
+   //vprDEBUG(vprDBG_ALL,1) <<  "BARRIER: Going to sleep for: " << num << std::endl << vprDEBUG_FLUSH; 
+   gadget::InputManager::instance()->getRemoteInputManager()->createBarrier();
+   vprDEBUG(vprDBG_ALL,1) <<  "BARRIER: IS DONE" << std::endl << vprDEBUG_FLUSH; 
+
+
    // SWAP
       // Start swapping all the pipes
    for(pipeNum=0; pipeNum<pipes.size(); pipeNum++)
