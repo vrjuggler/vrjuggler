5f4a9e5dc115f3dde289f151a7c90cfd3f7c8ae2
Patrick Hartling <patrick.hartling@gmail.com>
2002-09-04 14:44:29 +0000

    Copyright update.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@10527 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/config/org/vrjuggler/jccl/config/ChunkIOException.java
+++ juggler/modules/jackal/config/org/vrjuggler/jccl/config/ChunkIOException.java
@@ -1,6 +1,6 @@
 /*************** <auto-copyright.pl BEGIN do not edit this line> **************
  *
- * VR Juggler is (C) Copyright 1998, 1999, 2000 by Iowa State University
+ * VR Juggler is (C) Copyright 1998-2002 by Iowa State University
  *
  * Original Authors:
  *   Allen Bierbaum, Christopher Just,
