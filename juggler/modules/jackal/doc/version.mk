c0a4835b023902104abfb27da1e5e6d19948f56f
Patrick Hartling <patrick.hartling@gmail.com>
2002-09-25 15:04:21 +0000

    JCCL is at version 0.9 now.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@10798 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/doc/version.mk
+++ juggler/modules/jackal/doc/version.mk
@@ -1,4 +1,4 @@
 # This file can be included by a Doxygen configuration file to set the
 # project version number and by makefiles to accomplish the same.
 
-PROJECT_NUMBER = 0.8
+PROJECT_NUMBER = 0.9
