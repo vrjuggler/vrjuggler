15798cb7f42e86c78efe9240e7520aa83ae34e3e
Allen Bierbaum <abierbaum@gmail.com>
2002-10-10 14:43:14 +0000

    TrackdAPI additions.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@10946 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/drivers/VRCO/TrackdAPI/TrackdAPISensor.h
+++ modules/gadgeteer/drivers/VRCO/TrackdAPI/TrackdAPISensor.h
@@ -111,6 +111,25 @@ public:
     getChunkType () 
     { return std::string("TrackdAPISensor"); }
 
+    /**
+     * Invokes the global scope delete operator.  This is required for proper
+     * releasing of memory in DLLs on Win32.
+     */
+    void operator delete(void* p)
+    {
+       ::operator delete(p);
+    }
+
+ protected:
+    /**
+     * Deletes this object.  This is an implementation of the pure virtual
+     * gadget::Input::destroy() method.
+     */
+    virtual void destroy()
+    {
+       delete this;
+    }
+
 private:
     TrackerReader*              mTrackerReader;    /**< The tracker sensor to read data from */
     std::vector<PositionData>   mCurSensorValues; // The current (up-to-date) values    
