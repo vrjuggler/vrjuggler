17e2816ee094f24c0f92694070c3838e3eff6807
Allen Bierbaum <abierbaum@gmail.com>
2002-10-10 17:58:16 +0000

    Added User class.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@10974 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/samples/OpenSG/advanced/OpenSGViewer/User.cpp
@@ -0,0 +1,43 @@
+#include <gmtl/Matrix.h>
+#include <gmtl/MatrixOps.h>
+#include <gmtl/Vec.h>
+#include <gmtl/Generate.h>
+#include <gmtl/Output.h>
+
+#include <ViewPlatform.h>
+
+void User::update(ViewPlatform* vp)
+{
+   //mViewPlatform.update(this);   // Update the view platform
+
+   // --- UPDATE USER POSITION --- //
+   gmtl::Matrix44f vw_M_plat = vp->getTransform_virtMplat();
+   gmtl::Matrix44f head_pos_world;  // Head position in real world (platform) coords
+   head_pos_world = *(getHeadPos()->getData());
+
+   M_vwMuser = vw_M_plat * head_pos_world;   
+}
+
+void User::init()
+{
+   mHeadInterface.init("VJHead");
+   mWandInterface.init("VJWand");
+
+   gadget::DigitalInterface* btn0 = new gadget::DigitalInterface;
+   btn0->init("VJButton0");
+   gadget::DigitalInterface* btn1 = new gadget::DigitalInterface;
+   btn1->init("VJButton1");
+   gadget::DigitalInterface* btn2 = new gadget::DigitalInterface;
+   btn2->init("VJButton2");
+   gadget::DigitalInterface* btn3 = new gadget::DigitalInterface;
+   btn3->init("VJButton3");
+   gadget::DigitalInterface* btn4 = new gadget::DigitalInterface;
+   btn4->init("VJButton4");
+   
+   mButtonInterfaces.push_back(btn0);
+   mButtonInterfaces.push_back(btn1);
+   mButtonInterfaces.push_back(btn2);
+   mButtonInterfaces.push_back(btn3);
+   mButtonInterfaces.push_back(btn4);
+}
+
---
+++ modules/vrjuggler/samples/OpenSG/advanced/OpenSGViewer/User.h
@@ -0,0 +1,71 @@
+#ifndef USER_H
+#define USER_H
+
+#include <vpr/vpr.h>
+#include <gmtl/Matrix.h>
+
+class ViewPlatform;
+
+/** Representation and holder for User related data.
+*
+*
+* Coordinate systems used
+* 
+* The following describes the coordinate systems and representations used
+* in the code for User, Platform, and navigation
+*
+* Coord systems
+* - V - Virtual World (vw): The coordinate system that all the data objects are in.
+*                      This system is used to represent the "world" that we create.
+* - P - Platform (plat): The platform is the "magic carpet" that you are traveling through
+*                 the virtual world on.  Navigation methods move this carpet
+* - W - World (rw): This is the "real world".  This actually corresponds exactly to the
+*              platform since the platform is the VR device you are "in"
+*/
+class User
+{
+public:
+
+   /** Initialize the user */
+   void init();
+
+   /**
+   * Get the positon of the user in the virtual world
+   */
+   gmtl::Matrix44f getUserPosition_vw()
+   { return M_vwMuser; }
+
+   /**
+   * Get the xformation from world space to local user space   
+   */
+   gmtl::Matrix44f getXform_vwMrw()
+   { return mViewPlatform.getTransform_virtMplat(); }
+
+   /** User interaction stuff */   
+   gadget::PositionInterface& getHeadPos()
+   { return mHeadInterface; }
+   gadget::PositionInterface& getWandPos()
+   { return mWandInterface; }
+   gadget::DigitalInterface& getButton(unsigned buttonNum)
+   { return *(mButtonInterfaces[buttonNum]); }
+
+public:
+   /**
+   * Update the user state
+   */
+   void update(ViewPlatform* vp);
+   
+protected:
+    gmtl::Matrix44f  M_vwMuser;        /**< Position of user in virtual world */
+    //gmtl::Matrix44f  mUserMworld;      /**< Matrix to convert for world coords to virtual world coords */
+
+protected:
+   /** User interface stuff */
+   gadget::PositionInterface              mHeadInterface;
+   gadget::PositionInterface              mWandInterface;      /**< The wand position interface */
+   std::vector<gadget::DigitalInterface*> mButtonInterfaces;   /**< Button interfaces */
+};
+
+
+#endif //USER_H
+
