a536e9843af07531b4c8aba50f4de92693f790b5
Patrick Hartling <patrick.hartling@gmail.com>
2002-10-13 20:21:09 +0000

    Use vprDEBUG instead of std::cout to print the current string value.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11032 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/test/CxxClient/StringObserverImpl.cpp
+++ modules/tweek/test/CxxClient/StringObserverImpl.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <vpr/Util/Debug.h>
 #include <StringObserverImpl.h>
 
 
@@ -6,6 +6,8 @@ void StringObserverImpl::update()
 {
    char* cur_value = mSubject->getValue();
 
-   std::cout << "Current string value is now '" << cur_value << "'\n";
+   vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL)
+      << "Current string value is now '" << cur_value << "'\n"
+      << vprDEBUG_FLUSH;
    delete cur_value;
 }
