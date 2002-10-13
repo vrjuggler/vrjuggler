9152842954a84b03e41589ec2371d02db2fe9dcd
Patrick Hartling <patrick.hartling@gmail.com>
2002-10-13 19:10:45 +0000

    A test application for the C++ client interface.  This can also serve as an example of how to use the Tweek C++ client interface.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11028 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/tweek/test/CxxClient/StringSubjectImpl.h
@@ -0,0 +1,65 @@
+#ifndef _STRING_SUBJECT_IMPL_H_
+#define _STRING_SUBJECT_IMPL_H_
+
+#include <tweek/tweekConfig.h>
+
+#include <string>
+
+#include <tweek/CORBA/SubjectImpl.h>
+#include <tweek/CORBA/Observer.h>
+#include <StringSubject.h>
+
+
+namespace CxxClientTest
+{
+
+/**
+ * This class is an extension to the base Tweek SubjectImpl class.  It uses
+ * multiple inheritance with that class and with the generated CORBA class
+ * corresponding to the IDL for StringSubject.
+ */
+class StringSubjectImpl : public POA_CxxClientTest::StringSubject,
+                          public tweek::SubjectImpl
+{
+public:
+   StringSubjectImpl()
+      : tweek::SubjectImpl(), mValue("")
+   {
+      /* Do nothing. */ ;
+   }
+
+   virtual ~StringSubjectImpl()
+   {
+      /* Do nothing. */ ;
+   }
+
+   /**
+    * Sets this subject's internal value.
+    */
+   virtual void setValue(const char* value);
+
+   /**
+    * Returns this subject's internal value.
+    */
+   virtual char* getValue();
+
+   /**
+    * This overriding method is needed so that the correct type is returned
+    * when the _this() method is invoked.  Without this method, an object of
+    * type tweek::Subject_ptr would be returned.
+    *
+    * XXX: It may be possible to remove this requirement in the future.
+    */
+   CxxClientTest::StringSubject_ptr _this()
+   {
+      return POA_CxxClientTest::StringSubject::_this();
+   }
+
+private:
+   std::string mValue;   /**< Our value */
+};
+
+} // End of CxxClientTest namespace
+
+
+#endif /* _STRING_SUBJECT_IMPL_H_ */
