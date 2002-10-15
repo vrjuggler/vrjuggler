601f667d1771ffd1cdff2842437c12395d961002
Josh Brown <browner10@users.sourceforge.net>
2002-10-15 22:34:50 +0000

    Here are the files that I forgot last time (sorry) :(
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11066 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/sonix/snx/Util/Debug.h
@@ -0,0 +1,12 @@
+#ifndef _SNX_DEBUG_H_
+#define _SNX_DEBUG_H_
+
+//  #define VJ_DEBUG  Defined in vjConfig.h
+#include <vpr/Util/Debug.h>
+#include <stdlib.h>
+#include <vpr/vpr.h>
+
+// snx categories
+const vpr::DebugCategory snxDBG("863c7fc3-1997-51e4-0772-724cd577d2a5", "SNX_DBG", "snx:");  
+
+#endif
