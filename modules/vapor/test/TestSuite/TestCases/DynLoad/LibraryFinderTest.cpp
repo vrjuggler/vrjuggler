7a88ff6e2b4a802010c771955e81941d9f952252
Patrick Hartling <patrick.hartling@gmail.com>
2002-10-29 23:24:18 +0000

    New feature:	Added a new class (vpr::LibraryFinder) that searches a 		given directory looking for shared libraries.  The search 		happens using something like a pattern match based on a 		user-defined library extension (usually .so or .dll).
    Bumped version to 0.19.3.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11199 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/test/TestSuite/TestCases/DynLoad/LibraryFinderTest.cpp
@@ -0,0 +1,80 @@
+#include <string>
+
+#include <vpr/DynLoad/LibraryFinder.h>
+#include <vpr/System.h>
+
+#include <LibraryFinderTest.h>
+
+
+namespace vprTest
+{
+
+static const std::string C_MOD("libcmod.so");
+
+LibraryFinderTest::LibraryFinderTest()
+   : CppUnit::TestCase(), mModuleDir(MODULE_DIR)
+{
+   ;
+}
+
+LibraryFinderTest::LibraryFinderTest(std::string name)
+   : CppUnit::TestCase(name), mModuleDir(MODULE_DIR)
+{
+   ;
+}
+
+void LibraryFinderTest::setUp()
+{
+   std::string c_lib_path(MODULE_DIR);
+   c_lib_path += "/" + C_MOD;
+   mCModuleName = c_lib_path;
+
+   std::string cxx_lib_path(MODULE_DIR);
+   cxx_lib_path += "/libcxxmod.so";
+   mCxxModuleName = cxx_lib_path;
+}
+
+void LibraryFinderTest::scanTest()
+{
+   vpr::LibraryFinder finder1(mModuleDir, std::string(".so"), false);
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 0 && "Should not have any libraries yet");
+
+   finder1.rescan();
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 2 && "Wrong number of libraries found");
+
+   finder1.setLibraryDirectory(std::string("bogusDir"));
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 0 && "Should not have found any libraries");
+
+   finder1.setLibraryDirectory(mModuleDir);
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 2 && "Wrong number of libraries found");
+
+   finder1.setLibraryExtension(".bad");
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 0 && "Should not have found any libraries");
+
+   finder1.setLibraryExtension(".so");
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 2 && "Wrong number of libraries found");
+
+   finder1.setDirAndExt("bogus", ".bad");
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 0 && "Should not have found any libraries");
+
+   finder1.setDirAndExt(mModuleDir, ".so");
+   CPPUNIT_ASSERT(finder1.getLibraries().size() == 2 && "Wrong number of libraries found");
+
+   vpr::LibraryFinder finder2(mModuleDir, std::string(".so"));
+   CPPUNIT_ASSERT(finder2.getLibraries().size() == 2 && "Wrong number of libraries found");
+}
+
+void LibraryFinderTest::scanAndLoadTest()
+{
+   vpr::LibraryFinder finder(mModuleDir, ".so");
+
+   vpr::LibraryFinder::LibraryList libs;
+   libs = finder.getLibraries();
+   CPPUNIT_ASSERT(libs.size() == 2 && "Wrong number of libraries found");
+
+   vpr::ReturnStatus status;
+   status = libs[0]->load();
+   CPPUNIT_ASSERT(status.success() && "Load failed");
+}
+
+}
