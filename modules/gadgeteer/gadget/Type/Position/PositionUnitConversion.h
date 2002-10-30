29081091312525de97958f94a8b189c0eb09a286
Allen Bierbaum <abierbaum@gmail.com>
2002-10-30 20:00:48 +0000

    Added new file of conversion factors.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11225 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Type/Position/PositionUnitConversion.h
@@ -0,0 +1,16 @@
+#ifndef GADGET_UNITS_H_
+#define GADGET_UNITS_H_
+
+namespace gadget
+{
+   namespace PositionUnitConversion
+   {
+   const float ConvertToFeet = 0.3048;
+   const float ConvertToInches = 0.0254
+   const float ConvertToMeters = 1.0f;
+   const float ConvertToCentimeters = 0.01f;
+   }
+}
+
+#endif
+
