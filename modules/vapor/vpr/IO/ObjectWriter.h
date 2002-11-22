dad8a0b1662233cb2b492f16e44d3e8730fd5949
Allen Bierbaum <abierbaum@gmail.com>
2002-11-22 23:15:26 +0000

    New feature:	GUID is now serializable Refactor:	Removed some include's that caused cycles.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11434 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/IO/ObjectWriter.h
+++ modules/vapor/vpr/IO/ObjectWriter.h
@@ -43,8 +43,8 @@
 #define _VPR_OBJECT_WRITER_H
 
 #include <vpr/vprConfig.h>
-#include <vpr/System.h>
-#include <vpr/Util/Debug.h>
+//#include <vpr/System.h>
+//#include <vpr/Util/Debug.h>
 
 #include <vector>
 
