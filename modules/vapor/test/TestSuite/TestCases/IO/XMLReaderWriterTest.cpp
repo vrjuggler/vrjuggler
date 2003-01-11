b8cac5ad2940d46dcf6a76f5aa214f20e4eaa749
Allen Bierbaum <abierbaum@gmail.com>
2003-01-11 23:55:12 +0000

    Added testing of the XML reader.
    WARNING: For now this is generating an unhandled exception that I need to track down.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11621 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/test/TestSuite/TestCases/IO/XMLReaderWriterTest.cpp
+++ modules/vapor/test/TestSuite/TestCases/IO/XMLReaderWriterTest.cpp
@@ -19,25 +19,50 @@ void XMLReaderWriterTest::testBasicWriteRead()
    const double      data_double(1.2211975);
    const std::string data_string("test string");
    const bool        data_bool(true);
-
+   const std::string long_string("X", 1500);
+      
    std::vector<vpr::Uint8> data_buffer;
    vpr::XMLObjectWriter xml_writer;
    
-   xml_writer.beginTag("FirstLevel");
-      xml_writer.beginAttribute("uint8");
-         xml_writer.writeUint8(data_uint8);
-      xml_writer.endAttribute();
-      xml_writer.writeUint16(data_uint16);
-      xml_writer.writeUint32(data_uint32);
-      xml_writer.writeUint64(data_uint64);
-      xml_writer.beginTag("LargeNumberLevel");
-         xml_writer.writeFloat(data_float);
-         xml_writer.writeDouble(data_double);
-         xml_writer.writeUint8(data_string.length());
+   // Things to test
+   // - strings in cdata
+   // - string in attribute
+   // - multiple non-string in attribute
+   // - non string in cdata
+   // - Multiple cdata areas for a node
+   // - Very long string (> 1024chars)
+   try
+   {
+      xml_writer.beginTag("FirstLevel");
+         xml_writer.beginAttribute("uint8");
+            xml_writer.writeUint8(data_uint8);
+            xml_writer.writeUint16(data_uint16);
+         xml_writer.endAttribute();
+         xml_writer.writeUint32(data_uint32);
+         xml_writer.writeUint64(data_uint64);
+         xml_writer.beginTag("LargeNumberLevel");
+            xml_writer.writeFloat(data_float);
+            xml_writer.beginAttribute("String Attrib");
+               xml_writer.writeString(data_string);
+            xml_writer.endAttribute();
+            xml_writer.writeDouble(data_double);         
+         xml_writer.endTag();
+         xml_writer.writeString(data_string);
+         xml_writer.writeBool(data_bool);
+         xml_writer.beginTag("DataLevel");
+            //xml_writer.writeString(long_string);
+         xml_writer.endTag();
       xml_writer.endTag();
-      xml_writer.writeString(data_string);
-      xml_writer.writeBool(data_bool);
-   xml_writer.endTag();
+   }
+   catch (cppdom::Error e)
+   {
+      std::cerr << "Error [building]: " << e.getString() << std::endl;
+      if (e.getInfo().size())
+      {
+         std::cerr << "Info: " << e.getInfo() << std::endl;
+      }           
+   }
+
 
    vpr::Uint8 read_uint8;
    vpr::Uint16 read_uint16;
@@ -47,25 +72,61 @@ void XMLReaderWriterTest::testBasicWriteRead()
    double      read_double;
    vpr::Uint8  read_str_len;
    std::string read_string;
+   std::string read_string_attrib;
+   std::string read_long_string;
    bool        read_bool;
 
-   std::cout << "------ Current data -----\n";
-   xml_writer.getRootNode()->save(std::cout);
-   std::cout << "------ End data -----\n";
+   try
+   {
+      std::cout << "------ Current data -----\n";
+      xml_writer.getRootNode()->save(std::cout);
+      std::cout << "------ End data -----\n";
 
-   /*
-   vpr::ObjectReader* reader;
-   reader = new vpr::BufferObjectReader(&data_buffer);
+      data_buffer = xml_writer.getData();
+   }
+   catch (cppdom::Error e)
+   {
+      std::cerr << "Error [output]: " << e.getString() << std::endl;
+      if (e.getInfo().size())
+      {
+         std::cerr << "Info: " << e.getInfo() << std::endl;
+      }           
+   }
+      
+   vpr::XMLObjectReader xml_reader(data_buffer);
+   
+   try
+   {
+      xml_reader.beginTag("FirstLevel");
+         xml_writer.beginAttribute("uint8");
+            xml_reader.readUint8(read_uint8);
+            xml_reader.readUint16(read_uint16);
+         xml_reader.endAttribute();
+         xml_reader.readUint32(read_uint32);
+         xml_reader.readUint64(read_uint64);
+         xml_writer.beginTag("LargeNumberLevel");
+            xml_reader.readFloat(read_float);
+            xml_reader.beginAttribute("String Attrib");
+               xml_reader.readString(read_string_attrib, read_str_len);
+            xml_reader.endAttribute();
+            xml_reader.readDouble(read_double);
+         xml_reader.endTag();
+         xml_reader.readString(read_string, read_str_len);
+         xml_reader.readBool(read_bool);
+         xml_reader.beginTag("DataLevel");
+            //xml_reader.readString(read_long_string, 0);
+         xml_reader.endTag();
+      xml_reader.endTag();
+   }
+   catch (cppdom::Error e)
+   {
+     std::cerr << "Error [reading]: " << e.getString() << std::endl;
+     if (e.getInfo().size())
+     {
+        std::cerr << "Info: " << e.getInfo() << std::endl;
+     }           
+   }
 
-   reader->readUint8(read_uint8);
-   reader->readUint16(read_uint16);
-   reader->readUint32(read_uint32);
-   reader->readUint64(read_uint64);
-   reader->readFloat(read_float);
-   reader->readDouble(read_double);
-   reader->readUint8(read_str_len);
-   reader->readString(read_string, read_str_len);
-   reader->readBool(read_bool);
 
    CPPUNIT_ASSERT(data_uint8 == read_uint8);
    CPPUNIT_ASSERT(data_uint16 == read_uint16);
@@ -74,8 +135,9 @@ void XMLReaderWriterTest::testBasicWriteRead()
    CPPUNIT_ASSERT(data_float == read_float);
    CPPUNIT_ASSERT(data_double == read_double);
    CPPUNIT_ASSERT(data_string == read_string);
+   CPPUNIT_ASSERT(data_string == read_string_attrib);
    CPPUNIT_ASSERT(data_bool == read_bool);
-   */
+   CPPUNIT_ASSERT(long_string == read_long_string);
 }
 
 
