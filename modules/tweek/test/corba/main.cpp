761433b1dd1180edb73082eeed18b23f92df19e4
Patrick Hartling <patrick.hartling@gmail.com>
2003-01-14 02:55:17 +0000

    Use vpr::System::msleep().  This is a pre-emptive change in preparation for the removal of vpr::Thread::msleep() (and friends) in VPR.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11660 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/test/corba/main.cpp
+++ modules/tweek/test/corba/main.cpp
@@ -44,7 +44,7 @@ int main (int argc, char* argv[])
             }
             else
             {
-               vpr::Thread::msleep(100);
+               vpr::System::msleep(100);
             }
          }
       }
