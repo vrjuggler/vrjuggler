ef6f0f1f853a8b438bae73de9d790a108251f2a5
Allen Bierbaum <abierbaum@gmail.com>
2003-01-14 18:01:30 +0000

    - Moved XMLObjectWriter method impls to .cpp file. - Refactored beginTag method to make a little clearer and easier to debug
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@11672 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vapor/vpr/IO/XMLObjectReader.h
+++ juggler/modules/vapor/vpr/IO/XMLObjectReader.h
@@ -64,17 +64,9 @@ namespace vpr
 class XMLObjectReader : public ObjectReader
 {
 public:
-   XMLObjectReader(std::vector<vpr::Uint8> data)
-      : mCurSource(CdataSource)
-   {
-      initCppDomTree(data);
-   }
+   XMLObjectReader(std::vector<vpr::Uint8> data);
 
-   XMLObjectReader(cppdom::NodePtr rootNode)
-      : mCurSource(CdataSource)
-   {
-      mRootNode = rootNode;
-   }
+   XMLObjectReader(cppdom::NodePtr rootNode);
 
    /** @name Tag and attribute handling */
    //@{
@@ -126,7 +118,7 @@ protected:
      CdataSource /**< We are currently targetting cdata */
     };
 
-    /** Helper to write the data to the current string */
+    /** Helper to read the data from the current string */
     template<class T>
     vpr::ReturnStatus readValueStringRep(T& val)
     {
@@ -149,30 +141,15 @@ protected:
    /** State to keep at each level of recursion */
    struct NodeState
    {
-      NodeState(cppdom::Node* cur_node=NULL)
-         : node(cur_node)
-      {
-         nextChild = node->getChildren().begin();     // Initialize to the first child
-         cdataSource.str(node->getFullCdata());
-      }
+      NodeState(cppdom::Node* cur_node=NULL);
 
-      NodeState(const NodeState& rhs)
-      {
-         node = rhs.node;
-         nextChild = rhs.nextChild;
-         cdataSource.str(rhs.cdataSource.str());
-      }
+      NodeState(const NodeState& rhs);
 
-      NodeState& operator=(const NodeState& rhs)
-      {
-         node = rhs.node;
-         nextChild = rhs.nextChild;
-         cdataSource.str(rhs.cdataSource.str());
-         return *this;
-      }
+      NodeState& operator=(const NodeState& rhs);
 
       cppdom::Node*              node;          /**< The node that this state is for */
-      cppdom::NodeList::iterator nextChild;      /**< Index of the current child we are examining */
+      cppdom::NodeList::iterator nextChild_i;      /**< Index of the current child we are examining */
+      cppdom::NodeList::iterator endChild_i;      /**< The end of the child list */
       std::stringstream          cdataSource;   /**< The current cdata input source stream */
    };
 
@@ -185,167 +162,6 @@ protected:
    std::stringstream       mAttribSource;    /**< Source of attribute data */
    CurSource               mCurSource;       /**< The current source of reading data */
 };
-
-/** Initialize the members based on a serialized version of something in the data buffer */
-inline void XMLObjectReader::initCppDomTree(std::vector<vpr::Uint8> data)
-{
-   std::string xml_data(data.begin(), data.end());
-   std::stringstream xml_stream(xml_data);
-   cppdom::ContextPtr cxt(new cppdom::Context);
-   cppdom::NodePtr new_node = cppdom::NodePtr(new cppdom::Node(cxt));
-   new_node->load(xml_stream, cxt);
-
-   mRootNode = new_node;
-   mCurNodeStack.clear();
-}
-
-/** Starting a new tag
-* Get the local cdata into the current data source
-*/
-inline  vpr::ReturnStatus XMLObjectReader::beginTag(std::string tagName)
-{
-   // Find the correct child
-   if(mCurNodeStack.empty())  // We should enter the root
-   {
-      mCurNodeStack.push_back(NodeState(mRootNode.get()));
-   }
-   else                      // Find the next child and push it on
-   {
-      vprASSERT(mCurNodeStack.back().nextChild != mCurNodeStack.back().node->getChildren().end() && "Past last child");
-
-      // Make sure that we get to child of type node.
-      // While next child is not of type node skip over cdata...
-      while( (*(mCurNodeStack.back().nextChild))->getType() != cppdom::xml_nt_node)
-      {
-         mCurNodeStack.back().nextChild++;
-         vprASSERT(mCurNodeStack.back().nextChild != mCurNodeStack.back().node->getChildren().end() && "Past last child");
-      }
-
-      cppdom::NodePtr next_child = *(mCurNodeStack.back().nextChild);      // Get the next child
-      mCurNodeStack.back().nextChild++;                                    // Step to the next child for next time
-      mCurNodeStack.push_back( NodeState(next_child.get()));               // Add the child to the stack
-   }
-
-   std::string cur_node_name = mCurNodeStack.back().node->getName();
-   vprASSERT(tagName == cur_node_name && "Reading back in incorrect order");
-   return vpr::ReturnStatus::Succeed;
-}
-
-/** Ends the most recently named tag.
-* Just pop of the cur node state information
-*/
-inline vpr::ReturnStatus XMLObjectReader::endTag()
-{
-   vprASSERT(!mCurNodeStack.empty());
-   mCurNodeStack.pop_back();
-   return vpr::ReturnStatus::Succeed;
-}
-
-/** Starts an attribute of the name attributeName
-* Get the attribute content and set the attribute source with that content
-*/
-inline vpr::ReturnStatus XMLObjectReader::beginAttribute(std::string attributeName)
-{
-   std::string attrib_content = mCurNodeStack.back().node->getAttribute(attributeName).getString();
-   mAttribSource.str(attrib_content);
-   mCurSource = AttribSource;             // Get content from attribute now
-   return vpr::ReturnStatus::Succeed;
-}
-
-/** Ends the most recently named attribute */
-inline vpr::ReturnStatus XMLObjectReader::endAttribute()
-{
-   mAttribSource.str("");     // Clear the attrib content (not required, but helps point out bugs)
-   mCurSource = CdataSource;     // Set back to getting data from cdata
-   return vpr::ReturnStatus::Succeed;
-}
-//@}
-
-
-inline vpr::Uint8 XMLObjectReader::readUint8()
-{
-   // Read a uint16 so that it does not treat it as a single char
-   vpr::Uint16 temp_data;
-   readValueStringRep(temp_data);
-   return vpr::Uint8(temp_data);
-}
-
-inline vpr::Uint16 XMLObjectReader::readUint16()
-{
-   vpr::Uint16 val;
-   readValueStringRep(val);
-   return val;
-}
-
-inline vpr::Uint32 XMLObjectReader::readUint32()
-{
-   vpr::Uint32 val;
-   readValueStringRep(val);
-   return val;
-}
-
-inline vpr::Uint64 XMLObjectReader::readUint64()
-{
-   vpr::Uint64 val;
-   readValueStringRep(val);
-   return val;
-}
-
-inline float XMLObjectReader::readFloat()
-{
-   float val;
-   readValueStringRep(val);
-   return val;
-}
-
-inline double XMLObjectReader::readDouble()
-{
-   double val;
-   readValueStringRep(val);
-   return val;
-}
-
-
-inline std::string XMLObjectReader::readString(unsigned len)
-{
-   std::string ret_val("");
-
-   if(AttribSource == mCurSource)   // Just return a copy of the attribute source
-   {
-      ret_val = mAttribSource.str();
-   }
-   else  // Cdata, extract the "'s around the string
-   {
-      char char_buffer[1024];    // Buffer for writing the final value
-      char temp_char('0');
-
-      // Get the first "
-      while('"' != temp_char)
-      {
-         mCurNodeStack.back().cdataSource.get(temp_char);
-         vprASSERT(('>' != temp_char) && ('<' != temp_char) && "String delimiter not found");
-      }
-
-      // Get the contents of the string
-      bool done_reading(false);
-      while(!done_reading)
-      {
-         mCurNodeStack.back().cdataSource.get(char_buffer,1024, '"');      // Copy over to target until we have second "
-         ret_val += char_buffer;                                           // append onto end
-         done_reading = (mCurNodeStack.back().cdataSource.peek() == '"');     // Done reading if got to a "
-      }
-      mCurNodeStack.back().cdataSource.ignore(1);                    // Ignore that " as well
-   }
-
-   return ret_val;
-}
-
-inline bool XMLObjectReader::readBool()
-{
-   bool val;
-   readValueStringRep(val);
-   return val;
-}
 
 } // namespace vpr
 
