ae123fb24dd235f06d5b59d33569e9fc887eef0e
Patrick Hartling <patrick.hartling@gmail.com>
2003-01-18 14:31:07 +0000

    Added copyright.  Fixed Doxygen problems.  (Periods are important!!)
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11752 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/Util/AttributeMapBase.h
+++ modules/vapor/vpr/Util/AttributeMapBase.h
@@ -1,3 +1,44 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2002 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
 #ifndef _VPR_ATTRIBUTE_MAP_BASE_H
 #define _VPR_ATTRIBUTE_MAP_BASE_H
 
@@ -21,7 +62,7 @@ class AttributeMapBase
 {
 public:
    /** Set an attribute.
-   * Set the attribute of name to value of val
+   * Set the attribute of name to value of val.
    */
    template<typename TYPE>
    void setAttrib(std::string name, TYPE val)
@@ -29,10 +70,10 @@ public:
       mAttribMap[name] = val;
    }
 
-   /** Get an attribute value
+   /** Get an attribute value.
    *
    * If not previously set, then return the value
-   * of a default constructed instance of the given type
+   * of a default constructed instance of the given type.
    */
    template<typename TYPE>
    TYPE getAttrib(std::string name)
@@ -53,8 +94,8 @@ public:
       return ret_val;
    }
 
-   /** Does the named attribute exist
-   * @return true if it exists
+   /** Does the named attribute exist.
+   * @return true if it exists.
    */
    bool attribExists(std::string name)
    {
