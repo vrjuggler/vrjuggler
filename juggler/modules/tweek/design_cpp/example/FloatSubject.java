664ae20b369aa771cdaeac4130f708c3a013ed4a
Patrick Hartling <patrick.hartling@gmail.com>
2003-01-20 21:25:09 +0000

    Fixed some unhappiness with Together and Java packages.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@11781 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/tweek/design_cpp/example/FloatSubject.java
@@ -0,0 +1,11 @@
+package example;
+
+/**
+ *	Generated from IDL definition of interface "FloatSubject"
+ *	@author JacORB IDL compiler 
+ */
+
+public interface FloatSubject
+	extends FloatSubjectOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity, tweek.Subject
+{
+}
