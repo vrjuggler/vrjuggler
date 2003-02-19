3995388f0e847ea7bf07ca4c290740cf41f608d8
Ben Scott <nonchocoboy@users.sourceforge.net>
2003-02-19 16:04:01 +0000

    Wand app no longer sucks.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@11990 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/test/Draw/OGL/wand/wandApp.h
+++ modules/vrjuggler/test/Draw/OGL/wand/wandApp.h
@@ -29,187 +29,78 @@
  * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
-
 #ifndef _WAND_APP_
 #define _WAND_APP_
 
-#include <vrj/vrjConfig.h>
-
-#include <iostream>
-#include <iomanip>
-
-#include <vrj/Draw/OGL/GlApp.h>
-
+#include <gadget/InputLogger.h>
 #include <gadget/Type/PositionInterface.h>
 #include <gadget/Type/DigitalInterface.h>
-
-#include <gadget/InputManager.h>
-#include <gadget/InputLogger.h>
-
-#include <gmtl/Matrix.h>
+#include <vrj/Draw/OGL/GlApp.h>
 #include <gmtl/Vec.h>
-#include <gmtl/Coord.h>
-#include <gmtl/Generate.h>
-
+#include <gmtl/Matrix.h>
 
 namespace vrjTest
 {
 
-// Utility function to draw a cube
-void drawbox( GLdouble x0, GLdouble x1, GLdouble y0, GLdouble y1,
-                GLdouble z0, GLdouble z1, GLenum type );
-
-//----------------------------------------------------
-//: Demonstration OpenGL application class
-//
-// This application simply renders a box on the wand
-//----------------------------------------------------
+/**
+ * Demonstration OpenGL application class
+ *
+ * This application simply renders a box on the wand with the axes for the
+ * wand's coordinate space.
+ */
 class wandApp : public vrj::GlApp
 {
 public:
-   // Constructor.
-   wandApp(vrj::Kernel* kern)
-      : vrj::GlApp(kern)         // Initialize base class
-   {;}
+   /** Constructor. */
+   wandApp(vrj::Kernel* kernel);
 
-   // Destructor.
-   virtual ~wandApp (void) {
-      /* Do nothing. */ ;
-   }
+   /** Destructor. */
+   ~wandApp();
 
-   // Execute any initialization needed before the API is started<BR><BR>
-   // Initialize VR Juggler device interfaces here.
-   //
-   // This is called once before OGL is initialized
-   virtual void init()
-   {
-      //std::cout << "---------- App:init() ---------------" << std::endl;
-      // Initialize devices
-      mWand.init("VJWand");
-      mHead.init("VJHead");
-      mButton0.init("VJButton0");
-      mButton1.init("VJButton1");
-      mButton2.init("VJButton2");
-      mButton3.init("VJButton3");
-      mButton4.init("VJButton4");
-      mButton5.init("VJButton5");
+   /**
+    * Execute any initialization needed before the API is started<BR><BR>
+    * Initialize VR Juggler device interfaces here.
+    *
+    * This is called once before OGL is initialized
+    */
+   void init();
 
-      mLoggerPlayButton.init("LoggerPlayButton");
-   }
+   /**
+    * Function that is called immediately after a new OGL context is created.
+    * This is called for every display window that is opened.  Initialize GL
+    * state here. Also used to create context specific information<BR>
+    *
+    * This is called once for each display (twice for each stereo display)
+    */
+   void contextInit();
 
-   // Execute any initialization needed after API is started
-   //  but before the drawManager starts the drawing loops.<BR><BR>
-   //
-   // This is called once after OGL is initialized
-   virtual void apiInit()
-   {;}
+   /**
+    * Function called after tracker update but before start of drawing<BR><BR>
+    * Called once before every frame.  Do calculations and state modifications
+    * here.
+    */
+   void preFrame();
 
-   // Function that is called immediately after a new OGL context is created.
-   // This is called for every display window that is opened.  Initialize GL
-   // state here. Also used to create context specific information<BR>
-   //
-   // This is called once for each display (twice for each stereo display)
-   virtual void contextInit()
-   {
-      // Initialize the GL state information. (lights, shading, etc)
-      initGLState();
-   }
+   void bufferPreDraw();
 
-   // ----- Drawing Loop Functions ------
-   //
-   //  The drawing loop will look similar to this:
-   //
-   //  while (drawing)
-   //  {
-   //        preFrame();
-   //       draw();
-   //        intraFrame();      // Drawing is happening while here
-   //       sync();
-   //        postFrame();      // Drawing is now done
-   //
-   //       UpdateTrackers();
-   //  }
-   //------------------------------------
-
-   // Function called after tracker update but before start of drawing<BR><BR>
-   // Called once before every frame.  Do calculations and state modifications
-   // here.
-   virtual void preFrame()
-   {
-      // Put your pre frame computations here.
-
-      /*
-	  std::cout  << "Wand Buttons:"
-                 << " 0:" << mButton0->getData()
-                 << " 1:" << mButton1->getData()
-                 << " 2:" << mButton2->getData()
-                 << " 3:" << mButton3->getData()
-                 << " 4:" << mButton4->getData()
-                 << " 5:" << mButton5->getData() << std::endl;
-		*/
-      //mHeadHistory.push_back( gmtl::makeTrans<gmtl::Vec3f>(*(mHead->getData())) );
-
-      // Check logger play button
-      if(mLoggerPlayButton->getData() == gadget::Digital::TOGGLE_ON)
-      {
-         std::cout << "\n\n------ Log Play Button hit ----\n" << std::flush;
-         gadget::InputManager* input_mgr = gadget::InputManager::instance();
-         gadget::InputLoggerPtr logger = input_mgr->getInputLogger();
-
-         vprASSERT(logger.get() != NULL);
-         logger->load("test_logging.xml");
-         logger->play();
-      }
-
-   }
-
-   virtual void bufferPreDraw();
-
-   // Function to draw the scene.  Put OpenGL draw functions here.
-   //
-   // PRE: OpenGL state has correct transformation and buffer selected
-   // POST: The current scene has been drawn<BR><BR>
-   //
-   // called [once * number of displays] per frame
-   virtual void draw()
-   {
-      //call your openGL drawing code here.
-      myDraw();
-   }
-
-   // Function called after drawing has been triggered but BEFORE it
-   // completes<BR><BR>
-   //
-   // called once during each frame
-   virtual void intraFrame()
-   {
-      // Put your intra frame computations here.
-   }
-
-   // Function called before updating trackers but after the frame is
-   // drawn<BR><BR>
-   //
-   // Called once after every frame.  Do calculations here.
-   virtual void postFrame()
-   {
-      // Put your post frame computations here.
-   }
+   /**
+    * Function to draw the scene.  Put OpenGL draw functions here.
+    *
+    * PRE: OpenGL state has correct transformation and buffer selected
+    * POST: The current scene has been drawn<BR><BR>
+    *
+    * called [once * number of displays] per frame
+    */
+   void draw();
 
 private:
-   //------------------------------------------------
-   // Draw the scene.  A box on the end of the wand.
-   //------------------------------------------------
-   void myDraw();
    void initGLState();
 
-   void drawCube()
-   {
-      drawbox(-0.5, 0.5, -0.5, 0.5, -0.5, 0.5, GL_QUADS);
-   }
+   void drawCube();
 
-public:
-   gadget::PositionInterface    mWand;      // the Wand
-   gadget::PositionInterface    mHead;      // the head
+private:
+   gadget::PositionInterface    mWand;
+   gadget::PositionInterface    mHead;
    gadget::DigitalInterface     mButton0;
    gadget::DigitalInterface     mButton1;
    gadget::DigitalInterface     mButton2;
@@ -217,12 +108,12 @@ public:
    gadget::DigitalInterface     mButton4;
    gadget::DigitalInterface     mButton5;
 
-   gadget::DigitalInterface     mLoggerPlayButton;   // Playback log file when pressed
+   /** Playback log file when pressed. */
+   gadget::DigitalInterface     mLoggerPlayButton;
 
    std::vector<gmtl::Vec3f>     mHeadHistory;
 };
 
-};
-
+}
 
 #endif
