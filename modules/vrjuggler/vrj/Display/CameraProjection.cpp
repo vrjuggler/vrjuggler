e60865bd3e9837ad91d9a7ccc61e17e96d9db122
Patrick Hartling <patrick.hartling@gmail.com>
2003-03-02 00:38:49 +0000

    Added missing copyright header.  Include vrj/vrjConfig.h.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@12121 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Display/CameraProjection.cpp
+++ modules/vrjuggler/vrj/Display/CameraProjection.cpp
@@ -1,4 +1,36 @@
-#include <vrj/Display/CameraProjection.h>
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2002 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <vrj/vrjConfig.h>
 
 #include <jccl/Config/ConfigChunk.h>
 
@@ -6,6 +38,7 @@
 #include <gmtl/Generate.h>
 
 #include <vrj/Display/Display.h>
+#include <vrj/Display/CameraProjection.h>
 
 
 namespace vrj
@@ -60,13 +93,12 @@ void CameraProjection::calcViewMatrix(gmtl::Matrix44f& cameraPos, const float sc
 std::ostream& CameraProjection::outStream(std::ostream& out,
                                           const unsigned int indentLevel)
 {
-   const int pad_width_dot(20 - indentLevel);
+//   const int pad_width_dot(20 - indentLevel);
    out.setf(std::ios::left);
 
    const std::string indent_text(indentLevel, ' ');
 
-   out << indent_text << std::setw(pad_width_dot)
-       << "Type " << " vrj::CameraProjection\n";
+   out << indent_text << "vrj::CameraProjection:\n";
 
    return Projection::outStream(out, indentLevel);
 }
