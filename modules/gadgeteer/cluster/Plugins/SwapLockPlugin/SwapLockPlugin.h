7091fa9e8ac231c1c3cf23efa012355027504293
Aron Bierbaum <aronbierbaum@gmail.com>
2003-03-03 04:51:52 +0000

    Added SwapLockPluing files.
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@12135 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/cluster/Plugins/SwapLockPlugin/SwapLockPlugin.h
@@ -0,0 +1,109 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2002 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _CLUSTER_SWAP_LOCK_PLUGIN_H
+#define _CLUSTER_SWAP_LOCK_PLUGIN_H
+
+#include <vpr/Util/Singleton.h>
+
+#include <gadget/gadgetConfig.h>
+
+// Must implement the Abstract Base Class in order to be a manager used on the ClusterNetwork
+#include <cluster/ClusterPlugin.h>
+
+#include <jccl/Config/ConfigChunk.h>
+#include <jccl/Config/ConfigChunkPtr.h>
+#include <jccl/RTRC/ConfigChunkHandler.h>
+
+#include <map>
+
+namespace cluster
+{
+   class ClusterBarrier;
+
+class GADGET_CLASS_API SwapLockPlugin : public cluster::ClusterPlugin
+{
+   vprSingletonHeader( SwapLockPlugin );
+
+public:
+   SwapLockPlugin();
+   virtual ~SwapLockPlugin();
+   
+   virtual void preDraw();
+   virtual void postPostFrame();
+   virtual bool isPluginReady();
+   
+   bool createBarrier();
+
+   virtual std::string getManagerName()
+   {
+      return(std::string("SwapLockPlugin"));
+   }
+//   bool recognizeSwapLockPluginConfig(jccl::ConfigChunkPtr chunk);
+   
+   /** Add the pending chunk to the configuration.
+    *  PRE: configCanHandle (chunk) == true.
+    *  @return true iff chunk was successfully added to configuration.
+    */
+   bool configAdd(jccl::ConfigChunkPtr chunk);
+   
+   /** Remove the pending chunk from the current configuration.
+    *  PRE: configCanHandle (chunk) == true.
+    *  @return true iff the chunk (and any objects it represented)
+    *          were successfully removed.
+    */
+   bool configRemove(jccl::ConfigChunkPtr chunk);
+
+   /** Checks if this handler can process chunk.
+    *  Typically, an implementation of handler will check the chunk's
+    *  description name/token to decide if it knows how to deal with
+    *  it.
+    *  @return true iff this handler can process chunk.
+    */
+   bool configCanHandle(jccl::ConfigChunkPtr chunk);
+
+   bool recognizeSwapLockPluginConfig(jccl::ConfigChunkPtr chunk);
+   
+private:      
+   /**
+    * Returns the string representation of the chunk type used for the SwapLockPlugin
+    */   
+   static std::string getChunkType() { return std::string( "SwapLockPlugin" ); }
+
+private:
+   ClusterBarrier*   mBarrier;
+   int               mFrameNumber;
+};
+
+} // end namespace
+
+#endif
