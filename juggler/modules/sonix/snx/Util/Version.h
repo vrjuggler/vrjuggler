cb0b3efac69c877865b65c9c30f473b2839bb00f
Patrick Hartling <patrick.hartling@gmail.com>
2003-03-05 16:32:48 +0000

    Blanket copyright update.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@12175 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/sonix/snx/Util/Version.h
+++ juggler/modules/sonix/snx/Util/Version.h
@@ -1,6 +1,21 @@
+/****************** <SNX heading BEGIN do not edit this line> *****************
+ *
+ * sonix
+ *
+ * Original Authors:
+ *   Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <SNX heading END do not edit this line> ******************/
+
 /*************** <auto-copyright.pl BEGIN do not edit this line> **************
  *
- * VR Juggler is (C) Copyright 1998-2002 by Iowa State University
+ * VR Juggler is (C) Copyright 1998-2003 by Iowa State University
  *
  * Original Authors:
  *   Allen Bierbaum, Christopher Just,
@@ -21,12 +36,6 @@
  * License along with this library; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- *
- * -----------------------------------------------------------------
- * File:          $RCSfile$
- * Date modified: $Date$
- * Version:       $Revision$
- * -----------------------------------------------------------------
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
