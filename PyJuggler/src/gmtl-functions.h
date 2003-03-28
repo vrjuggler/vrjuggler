73b20babcf8cf8d740ede35a3655dc2f4a166bfe
Patrick Hartling <patrick.hartling@gmail.com>
2003-03-28 04:08:28 +0000

    The contents of this file help Pyste generate wrappers for various instantiations of functions defined in gmtl/MatrixOps.h.  Further additions will be made for gmtl/QuatOps.h and gmtl/VecOps.h.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@12307 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ PyJuggler/src/gmtl-functions.h
@@ -0,0 +1,113 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * PyJuggler is (C) Copyright 2002, 2003 by Patrick Hartling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _PYJUGGLER_GMTL_FUNCTIONS_H_
+#define _PYJUGGLER_GMTL_FUNCTIONS_H_
+
+#include <gmtl/Matrix.h>
+#include <gmtl/MatrixOps.h>
+
+namespace gmtl
+{
+// gmtl::Matrix<> functions ===================================================
+   template<> gmtl::Matrix44f& identity(gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& identity(gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& zero(gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& zero(gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& mult(gmtl::Matrix44f&, const gmtl::Matrix44f&,
+                                    const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& mult(gmtl::Matrix33f&, const gmtl::Matrix33f&,
+                                    const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& sub(gmtl::Matrix44f&, const gmtl::Matrix44f&,
+                                   const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& sub(gmtl::Matrix33f&, const gmtl::Matrix33f&,
+                                   const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& add(gmtl::Matrix44f&, const gmtl::Matrix44f&,
+                                   const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& add(gmtl::Matrix33f&, const gmtl::Matrix33f&,
+                                   const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& postMult(gmtl::Matrix44f&,
+                                        const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& postMult(gmtl::Matrix33f&,
+                                        const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& preMult(gmtl::Matrix44f&,
+                                       const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& preMult(gmtl::Matrix33f&,
+                                       const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& mult(gmtl::Matrix44f&, const gmtl::Matrix44f&,
+                                    float);
+   template<> gmtl::Matrix33f& mult(gmtl::Matrix33f&, const gmtl::Matrix33f&,
+                                    float);
+
+   template<> gmtl::Matrix44f& mult(gmtl::Matrix44f&, float);
+   template<> gmtl::Matrix33f& mult(gmtl::Matrix33f&, float);
+
+   template<> gmtl::Matrix44f& transpose(gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& transpose(gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& transpose(gmtl::Matrix44f&,
+                                         const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& transpose(gmtl::Matrix33f&,
+                                         const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& invertFull(gmtl::Matrix44f&,
+                                          const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& invertFull(gmtl::Matrix33f&,
+                                          const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& invert(gmtl::Matrix44f&, const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& invert(gmtl::Matrix33f&, const gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f& invert(gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f& invert(gmtl::Matrix33f&);
+
+   template<> gmtl::Matrix44f operator*(const gmtl::Matrix44f&,
+                                        const gmtl::Matrix44f&);
+   template<> gmtl::Matrix33f operator*(const gmtl::Matrix33f&,
+                                        const gmtl::Matrix33f&);
+
+   template<> bool operator==(const gmtl::Matrix44f&, const gmtl::Matrix44f&);
+   template<> bool operator==(const gmtl::Matrix33f&, const gmtl::Matrix33f&);
+
+   template<> bool operator!=(const gmtl::Matrix44f&, const gmtl::Matrix44f&);
+   template<> bool operator!=(const gmtl::Matrix33f&, const gmtl::Matrix33f&);
+
+   template<> bool isEqual(const gmtl::Matrix44f&, const gmtl::Matrix44f&,
+                           const float&);
+   template<> bool isEqual(const gmtl::Matrix33f&, const gmtl::Matrix33f&,
+                           const float&);
+// =================================================== gmtl::Matrix<> functions
+}
+
+#endif /* _PYJUGGLER_GMTL_FUNCTIONS_H_ */
