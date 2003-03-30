24e5e59df436783264321d174483813fde76bc69
Patrick Hartling <patrick.hartling@gmail.com>
2003-03-30 18:41:30 +0000

    Some tests of the Python interface to GMTL.  This is pretty pathetic at this point, but it demonstrates uses of some common GMTL constructs.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@12322 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ PyJuggler/test/gmtl-test.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+# ************** <auto-copyright.pl BEGIN do not edit this line> **************
+#
+# PyJuggler is (C) Copyright 2002, 2003 by Patrick Hartling
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# -----------------------------------------------------------------
+# File:          $RCSfile$
+# Date modified: $Date$
+# Version:       $Revision$
+# -----------------------------------------------------------------
+#
+# *************** <auto-copyright.pl END do not edit this line> ***************
+
+import gmtl
+
+mat1 = gmtl.Matrix44f()
+mat2 = gmtl.Matrix44f()
+
+gmtl.zero(mat1)
+gmtl.identity(mat1)
+
+mat3 = gmtl.Matrix44f()
+gmtl.mult(mat3, mat1, mat2)
+
+gmtl.sub(mat3, mat2, mat1)
+gmtl.add(mat3, mat2, mat1)
+
+gmtl.preMult(mat3, mat1)
+gmtl.postMult(mat3, mat1)
+
+gmtl.mult(mat3, mat1, 555.5)
+
+mat3 = gmtl.transpose(mat1)
+gmtl.transpose(mat3, mat1)
+
+gmtl.invertFull(mat3, mat1)
+
+mat3 = gmtl.invert(mat1)
+gmtl.invert(mat3, mat1)
+
+gmtl.identity(mat1)
+gmtl.zero(mat2)
+
+if not gmtl.isEqual(mat1, mat2):
+   print "gmtl.isEqual(mat1, mat2) pass"
+else:
+   print "gmtl.isEqual(mat1, mat2) fail"
+
+if gmtl.isEqual(mat1, mat1):
+   print "gmtl.isEqual(mat1, mat2) pass"
+else:
+   print "gmtl.isEqual(mat1, mat2) fail"
+
+vec1 = gmtl.Vec3f()
+vec2 = gmtl.Vec3f()
+vec3 = gmtl.Vec3f()
+
+vec1.set(1, 2, 3)
+vec2.set(4, 5, 6)
+vec3.set(7, 8, 9)
+
+dot_prod = gmtl.dot(vec1, vec2)
+print "Dot product = ", dot_prod
+
+print "Length of vec1 = ", gmtl.length(vec1)
+print "Squared length of vec1 = ", gmtl.lengthSquared(vec1)
+
+vec3_orig_length = gmtl.length(vec3)
+vec3_old_length = gmtl.normalize(vec3)
+
+if vec3_orig_length == vec3_old_length:
+   print "gmtl.normalize(vec3) return pass"
+else:
+   print "gmtl.normalize(vec3) return fail"
+
+if not gmtl.isNormalized(vec1):
+   print "gmtl.isNormalized(vec1) pass"
+else:
+   print "gmtl.isNormalized(vec1) fail"
+
+if gmtl.isNormalized(vec3):
+   print "gmtl.isNormalized(vec3) pass"
+else:
+   print "gmtl.isNormalized(vec3) fail"
+
+if gmtl.length(vec3) == 1.0:
+   print "gmtl.length(vec3) pass"
+else:
+   print "gmtl.length(vec3) fail"
+   print "Normalized length of vec3 = ", gmtl.length(vec3)
+
+vec4 = gmtl.cross(vec1, vec2)
+
+euler1 = gmtl.EulerAngleXYZf(90.0, 0.0, 0.0)
+euler2 = gmtl.EulerAngleXYZf(0.0, 90.0, 0.0)
+
+if not gmtl.isEqual(euler1, euler2):
+   print "gmtl.isEqual(euler1, euler2) pass"
+else:
+   print "gmtl.isEqual(euler1, euler2) fail"
+
+rot_mat   = gmtl.makeRotMatrix44(euler1)
+mat_angle = gmtl.makeRotEulerAngleXYZ(rot_mat)
+
+# XXX: This is questionable...
+if gmtl.isEqual(euler1, mat_angle):
+   print "gmtl.makeRot() pass"
+else:
+   print "gmtl.makeRot() fail"
+
+gmtl.identity(mat1)
+mat3 = gmtl.makeInverse(mat1)
+
+if gmtl.isEqual(mat1, mat3):
+   print "gmtl.makeInverse() pass (bad test!)"
+else:
+   print "gmtl.makeInverse() fail"
