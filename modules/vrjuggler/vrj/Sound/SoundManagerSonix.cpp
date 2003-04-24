445e39906e23bca8cd7100c7d443d1d27c55f3cb
Patrick Hartling <patrick.hartling@gmail.com>
2003-04-24 19:04:55 +0000

    Added a missing GMTL #include.  I don't know how this slipped by for so long...
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@12445 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Sound/SoundManagerSonix.cpp
+++ modules/vrjuggler/vrj/Sound/SoundManagerSonix.cpp
@@ -30,11 +30,12 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
+#include <vrj/vrjConfig.h>
 #include <gmtl/Matrix.h>
+#include <gmtl/Generate.h>
 #include <vpr/Util/FileUtils.h> // for replaceEnvVars...
 #include <snx/sonix.h>
 #include <jccl/Config/ConfigChunk.h>
-#include <vrj/vrjConfig.h>
 #include <vrj/Sound/SoundManagerSonix.h>
 
 namespace vrj
