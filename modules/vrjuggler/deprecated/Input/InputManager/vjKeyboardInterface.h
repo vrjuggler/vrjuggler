f259d71f9f17709493c8a79dc4ca95e3f6821b8e
Patrick Hartling <patrick.hartling@gmail.com>
2003-05-05 14:46:44 +0000

    Oops, I forgot to commit these changes last week when gadget::Keyboard* was renamed.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@12527 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/deprecated/Input/InputManager/vjKeyboardInterface.h
+++ modules/vrjuggler/deprecated/Input/InputManager/vjKeyboardInterface.h
@@ -1,10 +1,10 @@
 #ifndef COMPATIBILITY_HEADER_VJ_KEYBOARD_INTERFACE
 #define COMPATIBILITY_HEADER_VJ_KEYBOARD_INTERFACE
 
-#include <gadget/Type/KeyboardInterface.h>
+#include <gadget/Type/EventWindowInterface.h>
 #include <deprecated/Input/InputManager/vjKeyboardProxy.h>
 #include <deprecated/Input/vjInput/vjKeyboard.h>
 
-typedef gadget::KeyboardInterface vjKeyboardInterface;
+typedef gadget::EventWindowInterface vjKeyboardInterface;
 
 #endif
