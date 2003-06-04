0aa9f472989e0dd03f23b8623aa9bae3a301a0b9
Johnathan Gurley <daren@users.sourceforge.net>
2003-06-04 03:31:57 +0000

    Bug fixed: The vrjconfig.bat file is now more robust.  It will test for the existance of both the VJ_BASE_DIR and the TWEEK_BASE_DIR environment variables. It will also test to see if these directories actually exist.  Then it will run the Java code. SF Bug#: 748205
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@12694 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/vrjconfig.bat
+++ modules/vrjuggler/vrjconfig/vrjconfig.bat
@@ -1,1 +1,27 @@
-java -DTWEEK_BASE_DIR=%VJ_BASE_DIR% -DVJ_BASE_DIR=%VJ_BASE_DIR% -Djava.security.policy=%TWEEK_BASE_DIR%/bin/java.security.policy.txt -cp %TWEEK_BASE_DIR%/bin/jdom.jar;%TWEEK_BASE_DIR%/bin/xerces.jar;%TWEEK_BASE_DIR%/bin/Tweek.jar;%TWEEK_BASE_DIR%/bin/TweekBeans.jar;%TWEEK_BASE_DIR%/bin/TweekEvents.jar;%TWEEK_BASE_DIR%/bin/TweekNet.jar;%TWEEK_BASE_DIR%/bin/TweekBeanDelivery.jar;%TWEEK_BASE_DIR%/bin/TweekServices.jar;%TWEEK_BASE_DIR%/bin/kunststoff-mod.jar;%TWEEK_BASE_DIR%/bin/metouia.jar org.vrjuggler.tweek.Tweek --defaultbean=VRJConfig %1 %2 %3 %4 %5 %6 %7 %8 %9
+@ECHO OFF
+IF NOT "%VJ_BASE_DIR%" == "" GOTO ELSE1
+ECHO [ERR] VJ_BASE_DIR unset; please set the environment variable VJ_BASE_DIR to
+ECHO [ERR] point to your VR Juggler installation directory.  For more information, 
+ECHO [ERR] please see INSTALL.html
+GOTO ERREXIT
+:ELSE1
+IF EXIST "%VJ_BASE_DIR%" GOTO ELSE2
+ECHO [ERR] The VJ_BASE_DIR %VJ_BASE_DIR% does not appear to exist.  Please 
+ECHO [ERR] check the path and try again.
+GOTO ERREXIT
+:ELSE2
+IF NOT "%TWEEK_BASE_DIR%"=="" GOTO ELSE3
+ECHO [ERR] TWEEK_BASE_DIR unset; please set the environment variable 
+ECHO [ERR] TWEEK_BASE_DIR to point to your Tweek installation.  For more 
+ECHO [ERR] information, please see INSTALL.html
+GOTO ERREXIT
+:ELSE3
+IF EXIST "%TWEEK_BASE_DIR%" GOTO RUNJAVA
+ECHO [ERR] The TWEEK_BASE_DIR %TWEEK_BASE_DIR does not appear to exist.  Please 
+ECHO [ERR] check the path and try again.
+GOTO ERREXIT
+:RUNJAVA
+java -DTWEEK_BASE_DIR=%TWEEK_BASE_DIR% -DVJ_BASE_DIR=%VJ_BASE_DIR% -Djava.security.policy=%TWEEK_BASE_DIR%/bin/java.security.policy.txt -cp %TWEEK_BASE_DIR%/bin/jdom.jar;%TWEEK_BASE_DIR%/bin/xerces.jar;%TWEEK_BASE_DIR%/bin/Tweek.jar;%TWEEK_BASE_DIR%/bin/TweekBeans.jar;%TWEEK_BASE_DIR%/bin/TweekEvents.jar;%TWEEK_BASE_DIR%/bin/TweekNet.jar;%TWEEK_BASE_DIR%/bin/TweekBeanDelivery.jar;%TWEEK_BASE_DIR%/bin/TweekServices.jar;%TWEEK_BASE_DIR%/bin/kunststoff-mod.jar;%TWEEK_BASE_DIR%/bin/metouia.jar org.vrjuggler.tweek.Tweek --defaultbean=VRJConfig %1 %2 %3 %4 %5 %6 %7 %8 %9
+EXIT
+:ERREXIT
+ECHO [ERR] VRJConfig exiting due to previous errors.
