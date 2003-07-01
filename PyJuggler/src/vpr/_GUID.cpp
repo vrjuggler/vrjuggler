0bd1121e392e2690eb09550a7fbdb891481e6f14
Patrick Hartling <patrick.hartling@gmail.com>
2003-07-01 01:28:13 +0000

    Take a major step forward in getting PyJuggler working on Windows.  It turns out that Visual C++ can't handle the intense use of templates when there are a bunch of boost::python::class_<> declarations in a single file, and the recommendation on the Boost.Python FAQ is to split the source into multiple files (or to upgrade the compiler).
    This code has been regenerated with the latest version of Pyste from the
    Boost CVS repository, and I have done my best to migrate all of my
    customizations from the "monolithic" .cpp into the multi-file scenario we
    have now.  I have tested the Python and C++ example applications as well
    as PySim from the VR Juggler Toolbox.  To the best of my knowledge, all
    functionality has been retained, and the code does compile on Windows.
    Getting it to run would be the next step.  :)
    
    Prior to this change, the source tree was tagged with the symbol
    PRE_FILE_EXPLOSION.  After this commit, the tree will be tagged again with
    the symbol POST_FILE_EXPLOSION.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@12985 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/src/vpr/_GUID.cpp
+++ PyJuggler/src/vpr/_GUID.cpp
@@ -25,17 +25,16 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
-
 // Includes ====================================================================
 #include <boost/python.hpp>
 #include <vpr/Util/GUID.h>
-#include <vpr/Util/ReturnStatus.h>
-#include <vpr/Util/Interval.h>
 
 // Using =======================================================================
 using namespace boost::python;
 
 // Declarations ================================================================
+
+
 namespace  {
 
 
@@ -105,88 +104,8 @@ struct vpr_GUID_Wrapper: vpr::GUID
 
 
 // Module ======================================================================
-BOOST_PYTHON_MODULE(vpr)
+void _Export_GUID()
 {
-    scope* vpr_Interval_scope = new scope(
-    class_< vpr::Interval >("Interval", init<  >())
-        .def(init< const vpr::Interval & >())
-        .def(init< const vpr::Uint64, const vpr::Interval::Unit >())
-        .def("set", &vpr::Interval::set)
-        .def("setf", &vpr::Interval::setf)
-        .def("setd", &vpr::Interval::setd)
-        .def("setNow", &vpr::Interval::setNow)
-        .def("setNowReal", &vpr::Interval::setNowReal)
-        .def("sec", (void (vpr::Interval::*)(const vpr::Uint64) )&vpr::Interval::sec)
-        .def("sec", (vpr::Uint64 (vpr::Interval::*)() const)&vpr::Interval::sec)
-        .def("secf", (void (vpr::Interval::*)(const float) )&vpr::Interval::secf)
-        .def("secf", (float (vpr::Interval::*)() const)&vpr::Interval::secf)
-        .def("secd", (void (vpr::Interval::*)(const double) )&vpr::Interval::secd)
-        .def("secd", (double (vpr::Interval::*)() const)&vpr::Interval::secd)
-        .def("msec", (void (vpr::Interval::*)(const vpr::Uint64) )&vpr::Interval::msec)
-        .def("msec", (vpr::Uint64 (vpr::Interval::*)() const)&vpr::Interval::msec)
-        .def("msecf", (void (vpr::Interval::*)(const float) )&vpr::Interval::msecf)
-        .def("msecf", (float (vpr::Interval::*)() const)&vpr::Interval::msecf)
-        .def("msecd", (void (vpr::Interval::*)(const double) )&vpr::Interval::msecd)
-        .def("msecd", (double (vpr::Interval::*)() const)&vpr::Interval::msecd)
-        .def("usec", (void (vpr::Interval::*)(const vpr::Uint64) )&vpr::Interval::usec)
-        .def("usec", (vpr::Uint64 (vpr::Interval::*)() const)&vpr::Interval::usec)
-        .def("usecf", (void (vpr::Interval::*)(const float) )&vpr::Interval::usecf)
-        .def("usecf", (float (vpr::Interval::*)() const)&vpr::Interval::usecf)
-        .def("usecd", (void (vpr::Interval::*)(const double) )&vpr::Interval::usecd)
-        .def("usecd", (double (vpr::Interval::*)() const)&vpr::Interval::usecd)
-        .def("getBaseVal", &vpr::Interval::getBaseVal)
-        .def("now", &vpr::Interval::now)
-        .staticmethod("now")
-        .def( self == self )
-        .def( self != self )
-        .def( self < self )
-        .def( self <= self )
-        .def( self > self )
-        .def( self += self )
-        .def( self + self )
-        .def( self - self )
-    );
-    vpr_Interval_scope->attr("NoWait") = vpr::Interval::NoWait;
-    vpr_Interval_scope->attr("NoTimeout") = vpr::Interval::NoTimeout;
-    vpr_Interval_scope->attr("HalfPeriod") = vpr::Interval::HalfPeriod;
-
-    enum_< vpr::Interval::Unit >("Unit")
-        .value("Msec", vpr::Interval::Msec)
-        .value("Base", vpr::Interval::Base)
-        .value("Sec", vpr::Interval::Sec)
-        .value("Usec", vpr::Interval::Usec)
-    ;
-
-    delete vpr_Interval_scope;
-
-    scope* vpr_ReturnStatus_scope = new scope(
-    class_< vpr::ReturnStatus >("ReturnStatus", init<  >())
-        .def(init< const vpr::ReturnStatus & >())
-        .def(init< const vpr::ReturnStatus::Code & >())
-        .def("copy", &vpr::ReturnStatus::copy)
-        .def("setCode", &vpr::ReturnStatus::setCode)
-        .def("success", &vpr::ReturnStatus::success)
-        .def("failure", &vpr::ReturnStatus::failure)
-        .def("wouldBlock", &vpr::ReturnStatus::wouldBlock)
-        .def("inProgress", &vpr::ReturnStatus::inProgress)
-        .def("timeout", &vpr::ReturnStatus::timeout)
-        .def( self == self )
-        .def( self == other< vpr::ReturnStatus::Code >() )
-        .def( self != self )
-        .def( self != other< vpr::ReturnStatus::Code >() )
-    );
-
-    enum_< vpr::ReturnStatus::Code >("Code")
-        .value("WouldBlock", vpr::ReturnStatus::WouldBlock)
-        .value("NotConnected", vpr::ReturnStatus::NotConnected)
-        .value("Succeed", vpr::ReturnStatus::Succeed)
-        .value("Timeout", vpr::ReturnStatus::Timeout)
-        .value("Fail", vpr::ReturnStatus::Fail)
-        .value("InProgress", vpr::ReturnStatus::InProgress)
-    ;
-
-    delete vpr_ReturnStatus_scope;
-
     scope* vpr_GUID_scope = new scope(
     class_< vpr::GUID, vpr_GUID_Wrapper >("GUID", init<  >())
         .def(init< const vpr::GUID::GenerateTag >())
@@ -195,11 +114,11 @@ BOOST_PYTHON_MODULE(vpr)
         .def(init< const std::basic_string<char,std::char_traits<char>,std::allocator<char> > & >())
         .def(init< const vpr::GUID &, const std::basic_string<char,std::char_traits<char>,std::allocator<char> > & >())
         .def(init< const vpr::GUID & >())
+        .def("writeObject", &vpr::GUID::writeObject, &vpr_GUID_Wrapper::default_writeObject)
+        .def("readObject", &vpr::GUID::readObject, &vpr_GUID_Wrapper::default_readObject)
         .def("toString", &vpr::GUID::toString)
         .def("generate", (void (vpr::GUID::*)() )&vpr::GUID::generate)
         .def("generate", (void (vpr::GUID::*)(const vpr::GUID &, const std::basic_string<char,std::char_traits<char>,std::allocator<char> > &) )&vpr::GUID::generate)
-        .def("writeObject", &vpr::GUID::writeObject, &vpr_GUID_Wrapper::default_writeObject)
-        .def("readObject", &vpr::GUID::readObject, &vpr_GUID_Wrapper::default_readObject)
         .def(self_ns::str(self))
         .def( self == self )
         .def( self != self )
