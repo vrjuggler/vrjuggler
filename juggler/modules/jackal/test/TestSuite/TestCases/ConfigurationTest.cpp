6f0bbe2c9dc1d7774ad46b5bd0eb49067bab8b0c
Patrick Hartling <patrick.hartling@gmail.com>
2003-08-08 16:45:51 +0000

    This test case works again.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13278 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/test/TestSuite/TestCases/ConfigurationTest.cpp
+++ juggler/modules/jackal/test/TestSuite/TestCases/ConfigurationTest.cpp
@@ -14,16 +14,13 @@ void ConfigurationTest::testLoad()
    bool status;
 
    // Load the test configuration file
-   jccl::Configuration chunkdb;
-   status = jccl::ElementFactory::instance()->loadDefs(file_path + "ConfigurationTest/ConfigurationTest.jdef");
-   CPPUNIT_ASSERT(status && "Failed to load definition file");
+   jccl::Configuration cfg;
+   CPPUNIT_ASSERT(cfg.vec().empty() && "The configuration should be empty");
 
-   CPPUNIT_ASSERT(chunkdb.vec().empty() && "The DB should be empty");
-
-   status = chunkdb.load(file_path + "ConfigurationTest/ConfigurationTest_start.jconf");
+   status = cfg.load(file_path + "cfg/ConfigurationTest_start.jconf");
    CPPUNIT_ASSERT(status && "Failed to load initial config file");
 
-   CPPUNIT_ASSERT(! chunkdb.vec().empty() && "The DB should not be empty");
+   CPPUNIT_ASSERT(! cfg.vec().empty() && "The configuration should not be empty");
 }
 
 /*
@@ -33,34 +30,33 @@ void ConfigurationTest::testDependencySort()
    bool status;
 
    // Construct the known sorted list of dependencies.
-   std::vector<std::string> chunk_names;
-   chunk_names.push_back(std::string("DependentChunk1"));
-   chunk_names.push_back(std::string("DependentChunk2"));
-   chunk_names.push_back(std::string("Test Chunk 1"));
-   chunk_names.push_back(std::string("External Test Chunk 1"));
-   chunk_names.push_back(std::string("DependentChunk3"));
-   chunk_names.push_back(std::string("Embedded Chunk Ptr Test"));
+   std::vector<std::string> elt_names;
+   elt_names.push_back(std::string("Dependent Element 1"));
+   elt_names.push_back(std::string("Dependent Element 2"));
+   elt_names.push_back(std::string("Test Element 1"));
+   elt_names.push_back(std::string("External Test Element 1"));
+   elt_names.push_back(std::string("Dependent Element 3"));
+   elt_names.push_back(std::string("Child Element Ptr Test"));
 
    // Load the test configuration file
-   jccl::Configuration chunkdb_start, chunkdb_dep;
-   status = jccl::ElementFactory::instance()->loadDefs(file_path + "ConfigurationTest/ConfigurationTest.jdef");
+   jccl::Configuration cfg_start, cfg_dep;
    CPPUNIT_ASSERT(status && "Failed to load definition file");
 
-   status = chunkdb_start.load(file_path + "ConfigurationTest/ConfigurationTest_start.jconf");
+   status = cfg_start.load(file_path + "cfg/ConfigurationTest_start.jconf");
    CPPUNIT_ASSERT(status && "Failed to load initial config file");
 
-   status = chunkdb_dep.load(file_path + "ConfigurationTest/ConfigurationTest.jconf");
+   status = cfg_dep.load(file_path + "cfg/ConfigurationTest.jconf");
    CPPUNIT_ASSERT(status && "Failed to load config file with dependencies");
 
-   status = chunkdb_dep.dependencySort(&chunkdb_start);
+   status = cfg_dep.dependencySort(&cfg_start);
    CPPUNIT_ASSERT(status && "Dependency sort failed");
 
    // This is pretty gross...
    std::vector<jccl::ConfigElementPtr>::iterator i;
    int j = 0;
-   for ( i = chunkdb_dep.vec().begin(); i != chunkdb_dep.vec().end(); ++i, ++j )
+   for ( i = cfg_dep.vec().begin(); i != cfg_dep.vec().end(); ++i, ++j )
    {
-      CPPUNIT_ASSERT((*i)->getName() == chunk_names[j] && "Incorrect dependency sort");
+      CPPUNIT_ASSERT((*i)->getName() == cfg_names[j] && "Incorrect dependency sort");
    }
 }
 
@@ -70,17 +66,16 @@ void ConfigurationTest::testDependencySortFailure()
    bool status;
 
    // Load the test configuration file
-   jccl::Configuration chunkdb;
-   status = jccl::ElementFactory::instance()->loadDefs(file_path + "ConfigurationTest/ConfigurationTest.jdef");
+   jccl::Configuration cfg;
    CPPUNIT_ASSERT(status && "Failed to load definition file");
 
-   status = chunkdb.load(file_path + "ConfigurationTest/ConfigurationTest.jconf");
+   status = cfg.load(file_path + "cfg/ConfigurationTest.jconf");
    CPPUNIT_ASSERT(status && "Failed to load config file with dependencies");
 
-   // This dependency sort should fail because chunkdb_dep depends on chunks
-   // in chunkdb_start.  Since it's suppoesd to fail, ignore warnings/errors
+   // This dependency sort should fail because cfg_dep depends on elements
+   // in cfg_start.  Since it's suppoesd to fail, ignore warnings/errors
    // printed to the screen and focus on the result of the assertion.
-   status = chunkdb.dependencySort(NULL);
+   status = cfg.dependencySort(NULL);
    CPPUNIT_ASSERT(! status && "Dependency sort should have failed");
 }
 */
@@ -91,18 +86,16 @@ void ConfigurationTest::testClear()
    bool status;
 
    // Load the test configuration file
-   jccl::Configuration chunkdb;
-   status = jccl::ElementFactory::instance()->loadDefs(file_path + "ConfigurationTest/ConfigurationTest.jdef");
-   CPPUNIT_ASSERT(status && "Failed to load definition file");
+   jccl::Configuration cfg;
 
-   CPPUNIT_ASSERT(chunkdb.vec().empty() && "The DB should be empty");
+   CPPUNIT_ASSERT(cfg.vec().empty() && "The configuration should be empty");
 
-   status = chunkdb.load(file_path + "ConfigurationTest/ConfigurationTest_start.jconf");
+   status = cfg.load(file_path + "cfg/ConfigurationTest_start.jconf");
    CPPUNIT_ASSERT(status && "Failed to load initial config file");
 
-   CPPUNIT_ASSERT(! chunkdb.vec().empty() && "The DB should not be empty");
-   chunkdb.vec().clear();
-   CPPUNIT_ASSERT(chunkdb.vec().empty() && "The DB now should be empty");
+   CPPUNIT_ASSERT(! cfg.vec().empty() && "The configuration should not be empty");
+   cfg.vec().clear();
+   CPPUNIT_ASSERT(cfg.vec().empty() && "The configuration now should be empty");
 }
 
 }
