6e19147dd928dc0cf29e5ffd44cc207b04b0f525
Patrick Hartling <patrick.hartling@gmail.com>
2003-08-08 21:22:18 +0000

    Reformatted to conform to our coding standard.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@13305 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/gadget/Devices/Open/VRPN/Vrpn.cpp
+++ modules/gadgeteer/gadget/Devices/Open/VRPN/Vrpn.cpp
@@ -34,24 +34,24 @@
 // Vrpn
 //
 // Purpose:
-//	VR Juggler VRPN
+//  VR Juggler VRPN
 //
 // Author:
-//	Jason Jerald   8-26-02
+//  Jason Jerald   8-26-02
 // Revisions:
-//      Ported to to 1.1 (DR2) and sgi platform Anthony Steed, 10-4-02. 
+//      Ported to to 1.1 (DR2) and sgi platform Anthony Steed, 10-4-02.
 //      Simplified to one class to deal with threading issues.
 //
 // ToDo:
 //   Should we fetch the room->tracker_base and tracker->hand transforms
-//    from the VRPN config system, or always set them in VRJuggler. Or just 
+//    from the VRPN config system, or always set them in VRJuggler. Or just
 //    set room->juggler in VRJuggler?
 //
-//   Analog and other VRPN devices. 
+//   Analog and other VRPN devices.
 //
 //   Tidy up variables, there is some redundancy
 //
-//   Consider doing pre-rotate in posiiton proxy tracker offset. Though 
+//   Consider doing pre-rotate in posiiton proxy tracker offset. Though
 //    note there are no handedness changes in position proxy.
 //======================================================
 
@@ -76,8 +76,8 @@
 #include <gadget/Devices/Open/VRPN/Vrpn.h>
 
 #define VRPN_DEBUG 0
-				// 1 == reporting
-				// 2 == misc
+// 1 == reporting
+// 2 == misc
 
 
 void initDevice(gadget::InputManager* inputMgr)
@@ -87,225 +87,225 @@ void initDevice(gadget::InputManager* inputMgr)
 
 namespace gadget
 {
-    
-  void staticHandleTracker(void *userdata, vrpn_TRACKERCB t)
-  {
-    
+
+void staticHandleTracker(void *userdata, vrpn_TRACKERCB t)
+{
+
 #if (VRPN_DEBUG&1)
-    std::cout << "HandleTracker" << std::endl;
+   std::cout << "HandleTracker" << std::endl;
 #endif
 
-    gadget::Vrpn *this_ptr = static_cast<gadget::Vrpn*>(userdata);
-    this_ptr->handleTracker(t);
-  }
-  
-  void staticHandleButton(void *userdata, vrpn_BUTTONCB b)
-  {
+   gadget::Vrpn *this_ptr = static_cast<gadget::Vrpn*>(userdata);
+   this_ptr->handleTracker(t);
+}
+
+void staticHandleButton(void *userdata, vrpn_BUTTONCB b)
+{
 #if (VRPN_DEBUG&1)
-    std::cout << "HandleButton" << std::endl;
+   std::cout << "HandleButton" << std::endl;
 #endif
-    gadget::Vrpn *this_ptr = static_cast<gadget::Vrpn*>(userdata);
-    this_ptr->handleButton(b);
-  }
+   gadget::Vrpn *this_ptr = static_cast<gadget::Vrpn*>(userdata);
+   this_ptr->handleButton(b);
+}
 
-  std::string Vrpn::getElementType()
-  {
-     return "vrpn";
-  }
+std::string Vrpn::getElementType()
+{
+   return "vrpn";
+}
 
-  bool Vrpn::config(jccl::ConfigElementPtr e)
-  {
-    if(! (Input::config(e) && Position::config(e)))
-    {
+bool Vrpn::config(jccl::ConfigElementPtr e)
+{
+   if ( ! (Input::config(e) && Position::config(e)) )
+   {
       return false;
-    }
+   }
 
-    std::cerr << "***************** config ******* " << std::endl;
+   std::cerr << "***************** config ******* " << std::endl;
 
-    // tracking
-    mTrackerServer = e->getProperty<std::string>("tracker_server");
-    if(mTrackerServer == std::string(""))
-      {
-	std::cerr << "vrpn tracker server name not set!!!!!!" << std::endl;
-      }
-    else
-      {
-	std::cerr << "vrpn tracker server name set to " << mTrackerServer << std::endl;
-      }
+   // tracking
+   mTrackerServer = e->getProperty<std::string>("tracker_server");
+   if ( mTrackerServer == std::string("") )
+   {
+      std::cerr << "vrpn tracker server name not set!!!!!!" << std::endl;
+   }
+   else
+   {
+      std::cerr << "vrpn tracker server name set to " << mTrackerServer << std::endl;
+   }
 
-    mTrackerNumber = e->getProperty<int>("tracker_count");
+   mTrackerNumber = e->getProperty<int>("tracker_count");
 
-    // button
-    mButtonServer = e->getProperty<std::string >("button_server");
-    if(mButtonServer == std::string(""))
-      {
-	std::cerr << "vrpn button server name not set!!!!!!" << std::endl;
-      }
-    else
-      {
-	std::cerr << "vrpn button server name set to " << mButtonServer << std::endl;
-      }
-    
-    mButtonNumber = e->getProperty<int>("button_count");
+   // button
+   mButtonServer = e->getProperty<std::string >("button_server");
+   if ( mButtonServer == std::string("") )
+   {
+      std::cerr << "vrpn button server name not set!!!!!!" << std::endl;
+   }
+   else
+   {
+      std::cerr << "vrpn button server name set to " << mButtonServer << std::endl;
+   }
 
-    return true;
-  }
+   mButtonNumber = e->getProperty<int>("button_count");
 
-  Vrpn::~Vrpn()
-  {
-    stopSampling();
-  }
+   return true;
+}
+
+Vrpn::~Vrpn()
+{
+   stopSampling();
+}
+
+int Vrpn::startSampling()
+{
+   int status(0);
 
 
-  int Vrpn::startSampling()
-  {
-    int status(0);
-    
-    
-    std::cout << "Going to create fine\n";
-    if ( NULL == mReadThread ) {
+   std::cout << "Going to create fine\n";
+   if ( NULL == mReadThread )
+   {
       vpr::ThreadMemberFunctor<Vrpn>* read_func =
-	new vpr::ThreadMemberFunctor<Vrpn>(this,
-					   &Vrpn::readLoop,
-					   NULL);
-      mReadThread = new vpr::Thread(read_func); 
-      if ( mReadThread->valid() ) {
-	status = 1; // thread creation succeded
-	std::cout << "Created fine\n";
-      }
-      else {
-	std::cout << "Not fine\n";
-      }
-    } 
-    else {
-	std::cout << "Not null\n";
-    }
-    
-    mPositions.resize(mTrackerNumber);
-    mQuats.resize(mTrackerNumber);
-    mButtons.resize(mButtonNumber);
-    
-    mCurPositions.resize(mTrackerNumber);
-    mCurButtons.resize(mButtonNumber);
-    return status;
-  }
-
-
-  void Vrpn::readLoop(void *nullParam)
-  {
-    boost::ignore_unused_variable_warning(nullParam);
-
-    vrpn_Tracker_Remote *tracker;
-    vrpn_Button_Remote *button;
-    
-    tracker = new vrpn_Tracker_Remote(mTrackerServer.c_str());
-    button = new vrpn_Button_Remote(mButtonServer.c_str());
-    tracker->register_change_handler((void *) this, staticHandleTracker);
-    button->register_change_handler((void *) this, staticHandleButton);
-    
-    // loop through  and keep sampling
-    while(1)
+         new vpr::ThreadMemberFunctor<Vrpn>(this, &Vrpn::readLoop, NULL);
+      mReadThread = new vpr::Thread(read_func);
+      if ( mReadThread->valid() )
       {
-	tracker->mainloop();
-	button->mainloop();
-	sample();
+         status = 1; // thread creation succeded
+         std::cout << "Created fine\n";
       }
-  }
+      else
+      {
+         std::cout << "Not fine\n";
+      }
+   }
+   else
+   {
+      std::cout << "Not null\n";
+   }
 
-  void Vrpn::handleTracker(vrpn_TRACKERCB t)
-  {	
-    if (t.sensor > mTrackerNumber) {
+   mPositions.resize(mTrackerNumber);
+   mQuats.resize(mTrackerNumber);
+   mButtons.resize(mButtonNumber);
+
+   mCurPositions.resize(mTrackerNumber);
+   mCurButtons.resize(mButtonNumber);
+   return status;
+}
+
+void Vrpn::readLoop(void *nullParam)
+{
+   boost::ignore_unused_variable_warning(nullParam);
+
+   vrpn_Tracker_Remote *tracker;
+   vrpn_Button_Remote *button;
+
+   tracker = new vrpn_Tracker_Remote(mTrackerServer.c_str());
+   button = new vrpn_Button_Remote(mButtonServer.c_str());
+   tracker->register_change_handler((void *) this, staticHandleTracker);
+   button->register_change_handler((void *) this, staticHandleButton);
+
+   // loop through  and keep sampling
+   while ( 1 )
+   {
+      tracker->mainloop();
+      button->mainloop();
+      sample();
+   }
+}
+
+void Vrpn::handleTracker(vrpn_TRACKERCB t)
+{
+   if ( t.sensor > mTrackerNumber )
+   {
       vprDEBUG(vprDBG_ALL,vprDBG_CONFIG_LVL)
-	<< "Vrpn: tracker " << t.sensor 
-	<< " out of declared range ("<<mPositions.size()<<")"<<std::endl
-	<< vprDEBUG_FLUSH;
-      mPositions.resize(t.sensor);    
-      mQuats.resize(t.sensor);    
-    }
-  
+         << "Vrpn: tracker " << t.sensor
+         << " out of declared range ("<<mPositions.size()<<")"<<std::endl
+         << vprDEBUG_FLUSH;
+      mPositions.resize(t.sensor);
+      mQuats.resize(t.sensor);
+   }
+
 #if (VRPN_DEBUG&1)
-    std::cout << "Tracker #"<<t.sensor<< " quat " <<
+   std::cout << "Tracker #"<<t.sensor<< " quat " <<
       mQuats[t.sensor][0] << " " <<
       mQuats[t.sensor][1] << " " <<
       mQuats[t.sensor][2] << " " <<
       mQuats[t.sensor][3] << " " << std::endl;
-  
+
 #endif
-  
-    mQuats[t.sensor][0] = t.quat[0];
-    mQuats[t.sensor][1] = t.quat[1];
-    mQuats[t.sensor][2] = t.quat[2];
-    mQuats[t.sensor][3] = t.quat[3];
-  
-    mPositions[t.sensor][0] = t.pos[0];
-    mPositions[t.sensor][1] = t.pos[1];
-    mPositions[t.sensor][2] = t.pos[2];
-  }
-  
-  void Vrpn::handleButton(vrpn_BUTTONCB b)
-  {
-    if (b.button > mButtonNumber) {
+
+   mQuats[t.sensor][0] = t.quat[0];
+   mQuats[t.sensor][1] = t.quat[1];
+   mQuats[t.sensor][2] = t.quat[2];
+   mQuats[t.sensor][3] = t.quat[3];
+
+   mPositions[t.sensor][0] = t.pos[0];
+   mPositions[t.sensor][1] = t.pos[1];
+   mPositions[t.sensor][2] = t.pos[2];
+}
+
+void Vrpn::handleButton(vrpn_BUTTONCB b)
+{
+   if ( b.button > mButtonNumber )
+   {
       vprDEBUG(vprDBG_ALL,vprDBG_CONFIG_LVL)
-	<< "Vrpn: button " << b.button 
-	<< " out of declared range ("<<mButtons.size()<<")"<<std::endl
-	<< vprDEBUG_FLUSH;
-      mButtons.resize(b.button);    
-    }
+      << "Vrpn: button " << b.button
+         << " out of declared range ("<<mButtons.size()<<")"<<std::endl
+         << vprDEBUG_FLUSH;
+      mButtons.resize(b.button);
+   }
 #if (VRPN_DEBUG&1)
-    std::cout << "Button #"<<b.button<< " state " <<
+   std::cout << "Button #"<<b.button<< " state " <<
       b.state << " " << std::endl;
-  
+
 #endif
-    mButtons[b.button] = b.state;
-  }
+   mButtons[b.button] = b.state;
+}
 
+int Vrpn::sample()
+{
+   for ( int i=0;i<mTrackerNumber;i++ )
+   {
+      mCurPositions[i].setPosition(getSensorPos(i));
+      mCurPositions[i].setTime();
+   }
 
-  int Vrpn::sample()
-  {
-      for(int i=0;i<mTrackerNumber;i++)
-      {
-         mCurPositions[i].setPosition(getSensorPos(i));
-         mCurPositions[i].setTime();
-      }
-    
-    for (int i=0;i<mButtonNumber;i++)
-      {
-	mCurButtons[i] = getDigitalData(i);
-	mCurButtons[i].setTime();
-      }
-    
-    // Update the data buffer
-    addPositionSample(mCurPositions);
-    addDigitalSample(mCurButtons);
+   for ( int i=0;i<mButtonNumber;i++ )
+   {
+      mCurButtons[i] = getDigitalData(i);
+      mCurButtons[i].setTime();
+   }
 
-    return 1;
-  }
+   // Update the data buffer
+   addPositionSample(mCurPositions);
+   addDigitalSample(mCurButtons);
 
+   return 1;
+}
 
-  int Vrpn::stopSampling()
-  {
-    return 1;
-  }
-  
-  void Vrpn::updateData()
-  {
-    // Swap it
-    swapPositionBuffers();
-    swapDigitalBuffers();
+int Vrpn::stopSampling()
+{
+   return 1;
+}
 
-  }
+void Vrpn::updateData()
+{
+   // Swap it
+   swapPositionBuffers();
+   swapDigitalBuffers();
 
-  gmtl::Matrix44f Vrpn::getSensorPos(int d)
-  {
-    gmtl::Matrix44f ret_val;
-    gmtl::setRot( ret_val, mQuats[d]);
-    gmtl::setTrans( ret_val, mPositions[d]);
-    return ret_val;
-  }
-  
-  gadget::DigitalData Vrpn::getDigitalData(int d)
-  {
-    return mButtons[d];
-  }
+}
+
+gmtl::Matrix44f Vrpn::getSensorPos(int d)
+{
+   gmtl::Matrix44f ret_val;
+   gmtl::setRot( ret_val, mQuats[d]);
+   gmtl::setTrans( ret_val, mPositions[d]);
+   return ret_val;
+}
+
+gadget::DigitalData Vrpn::getDigitalData(int d)
+{
+   return mButtons[d];
+}
 
 } // End of gadget namespace
