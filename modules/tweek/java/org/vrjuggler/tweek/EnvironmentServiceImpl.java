d3516e06c8468a477173ad717dae20abade58481
Patrick Hartling <patrick.hartling@gmail.com>
2003-08-16 20:36:41 +0000

    Refactor:	The built-in Service Beans are now accessed via proxy 		objects that greatly simplify the process of acquiring the 		Service Bean reference.  This is modeled after Ben's work 		in the JCCL config Beans--specifically the ConfigBroker.
    Bumped version to 0.15 due to changes in the EnvironmentService interface.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@13373 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/java/org/vrjuggler/tweek/EnvironmentServiceImpl.java
+++ modules/tweek/java/org/vrjuggler/tweek/EnvironmentServiceImpl.java
@@ -34,30 +34,47 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
-package org.vrjuggler.tweek.services;
+package org.vrjuggler.tweek;
 
+import java.util.ArrayList;
+import org.vrjuggler.tweek.beans.BeanAttributes;
+import org.vrjuggler.tweek.beans.ServiceBean;
+import org.vrjuggler.tweek.services.EnvironmentService;
 
 /**
- * The Environment Service interface.  This is the interface that must be
- * implemented in order to get access to the Tweek Environment Service.
+ * The Environment Service Bean. This service will expand environment variables
+ * in a string.
  *
  * @since 1.0
  */
-public interface EnvironmentService
+class EnvironmentServiceImpl
+   extends ServiceBean
+   implements java.io.Serializable, EnvironmentService
 {
+   public EnvironmentServiceImpl(BeanAttributes attrs)
+   {
+      super(attrs);
+   }
+
    /**
     * Stores the command-line arguments passed to the application when it was
     * started.
     */
-   public void setCommandLineArgs(String[] args);
+   public void setCommandLineArgs(String[] args)
+   {
+      commandLineArgs = args;
+   }
 
    /**
     * Returns the array of command-line arguments.
     */
-   public String[] getCommandLineArgs();
+   public String[] getCommandLineArgs()
+   {
+      return commandLineArgs;
+   }
 
    /**
-    * Expands environment variables in the input string.  Environment variables
+    * Expands environment variables in the input string. Environment variables
     * may be specified using either $(variable_name) or ${variable_name}
     * everything between the parentheses or braces will be considered the name
     * of the variables.
@@ -67,7 +84,56 @@ public interface EnvironmentService
     *
     * @return  the input string with the variables replaced by their values.
     */
-   public String expandEnvVars(String inputStr);
+   public String expandEnvVars(String inputStr)
+   {
+      String new_str = inputStr;
+      int index, end_index;
+      String env_var, value;
+
+      while ( (index = new_str.indexOf("$")) != -1 )
+      {
+         if ( inputStr.substring(index, index + 2).equals("$(") )
+         {
+            end_index = inputStr.indexOf(")");
+         }
+         else if ( inputStr.substring(index, index + 2).equals("${") )
+         {
+            end_index = inputStr.indexOf("}");
+         }
+         else
+         {
+            continue;
+         }
+
+         env_var  = inputStr.substring(index + 2, end_index);
+
+         // Treat $HOME as a special case since we can actually get its value
+         // from the JVM without any aggrivating hacks.
+         if ( env_var.equals("HOME") )
+         {
+            value = getUserHome();
+         }
+         else
+         {
+            value = System.getProperty(env_var);
+         }
+
+         if ( value != null )
+         {
+            new_str = inputStr.substring(0, index) + value +
+                      inputStr.substring(end_index + 1);
+         }
+         else
+         {
+            System.err.println("WARNING: Environment variable " + env_var +
+                               " has no value");
+            new_str = inputStr.substring(0, index) +
+                      inputStr.substring(end_index + 1);
+         }
+      }
+
+      return new_str;
+   }
 
    /**
     * Gets the user's HOME environment variable using the JVM instead of the
@@ -75,5 +141,10 @@ public interface EnvironmentService
     *
     * @return  the path to the user's home directory
     */
-   public String getUserHome();
+   public String getUserHome()
+   {
+      return System.getProperty("user.home");
+   }
+
+   private String[] commandLineArgs = null;
 }
---
+++ modules/tweek/java/org/vrjuggler/tweek/services/GlobalPreferencesServiceProxy.java
@@ -0,0 +1,275 @@
+/***************** <Tweek heading BEGIN do not edit this line> ****************
+ * Tweek
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ ***************** <Tweek heading END do not edit this line> *****************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2003 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+package org.vrjuggler.tweek.services;
+
+import org.vrjuggler.tweek.beans.*;
+
+
+/**
+ * A proxy to the built-in Tweek Global Preferences Service Bean.  Instantiate
+ * this class to get access to the Global Preferences Service.
+ *
+ * @since 1.0
+ */
+public class GlobalPreferencesServiceProxy
+   implements GlobalPreferencesService
+{
+   /**
+    * Creates a new GlobalPreferencesService proxy to the Tweek Global
+    * Preferences Service.
+    *
+    * @throws RuntimeException if the GlobalPreferencesService Bean could not
+    *                          be found.
+    */
+   public GlobalPreferencesServiceProxy()
+      throws RuntimeException
+   {
+      TweekBean bean = BeanRegistry.instance().getBean("GlobalPreferences");
+
+      if ( null == bean )
+      {
+         throw new RuntimeException("Could not find Global Preferences Service");
+      }
+
+      if ( bean instanceof GlobalPreferencesService )
+      {
+         mPrefsService = (GlobalPreferencesService) bean;
+      }
+      else
+      {
+         throw new RuntimeException("The Bean registered as 'GlobalPreferences' is not a GlobalPreferencesService implementation.");
+      }
+   }
+
+   /**
+    * Called by the BeanRegistry singleton whenever a new bean is registered
+    * with it.
+    *
+    * @param evt     the event describing the bean that got registered
+    */
+   public void beanRegistered(BeanRegistrationEvent evt)
+   {
+      mPrefsService.beanRegistered(evt);
+   }
+
+   /**
+    * Returns a list of the basic starting directories from which the user may
+    * choose.  This does not necessarily include the user's current preference.
+    * A separate check should be done to determine how to handle the user's
+    * preferred start directory versus the default possible choices.
+    *
+    * @see getChooserStartDir()
+    * @see setChooserStartDir()
+    */
+   public java.util.List getStartDirList()
+   {
+      return mPrefsService.getStartDirList();
+   }
+
+   public void setUserLevel(int level)
+   {
+      mPrefsService.setUserLevel(level);
+   }
+
+   public int getUserLevel()
+   {
+      return mPrefsService.getUserLevel();
+   }
+
+   public void setLookAndFeel(String laf)
+   {
+      mPrefsService.setLookAndFeel(laf);
+   }
+
+   public String getLookAndFeel()
+   {
+      return mPrefsService.getLookAndFeel();
+   }
+
+   public java.util.Vector getBeanViewers()
+   {
+      return mPrefsService.getBeanViewers();
+   }
+
+   public void setBeanViewer(String v)
+   {
+      mPrefsService.setBeanViewer(v);
+   }
+
+   public String getBeanViewer()
+   {
+      return mPrefsService.getBeanViewer();
+   }
+
+   public void setWindowWidth(int width)
+   {
+      mPrefsService.setWindowWidth(width);
+   }
+
+   public int getWindowWidth()
+   {
+      return mPrefsService.getWindowWidth();
+   }
+
+   public void setWindowHeight(int height)
+   {
+      mPrefsService.setWindowHeight(height);
+   }
+
+   public int getWindowHeight()
+   {
+      return mPrefsService.getWindowHeight();
+   }
+
+   /**
+    * Sets the preferred start directory for file choosers.  It is up to the
+    * code that opens file choosers to act on this preference.  The given
+    * directory may be one of the default possible choices, or it may be a
+    * user-entered string, possibly containing one or more environment
+    * variables.
+    */
+   public void setChooserStartDir(String d)
+   {
+      mPrefsService.setChooserStartDir(d);
+   }
+
+   /**
+    * Returns the user's current preferred starting directory.  This may be
+    * one of the default list, or it may be a user-defined setting.
+    */
+   public String getChooserStartDir()
+   {
+      return mPrefsService.getChooserStartDir();
+   }
+
+   /**
+    * Sets the user's current preferred file chooser "open style".  This
+    * defines how file choosers should behave with respect to the directory
+    * they use when first opening.  It is up to the code that opens the file
+    * choosers to act on this preference.
+    */
+   public void setChooserOpenStyle(int style)
+   {
+      mPrefsService.setChooserOpenStyle(style);
+   }
+
+   public int getChooserOpenStyle()
+   {
+      return mPrefsService.getChooserOpenStyle();
+   }
+
+   /**
+    * Sets the user's preference for lazy Panel Bean instantiation.  This
+    * defines whether Panel Beans are instantiated upon discovery or upon
+    * first interaction.
+    */
+   public void setLazyPanelBeanInstantiation(boolean enabled)
+   {
+      mPrefsService.setLazyPanelBeanInstantiation(enabled);
+   }
+
+   public boolean getLazyPanelBeanInstantiation()
+   {
+      return mPrefsService.getLazyPanelBeanInstantiation();
+   }
+
+   public void setDefaultCorbaHost(String host)
+   {
+      mPrefsService.setDefaultCorbaHost(host);
+   }
+
+   public String getDefaultCorbaHost()
+   {
+      return mPrefsService.getDefaultCorbaHost();
+   }
+
+   public void setDefaultCorbaPort(int port)
+   {
+      mPrefsService.setDefaultCorbaPort(port);
+   }
+
+   public int getDefaultCorbaPort()
+   {
+      return mPrefsService.getDefaultCorbaPort();
+   }
+
+   public void setDefaultIiopVersion(String version)
+   {
+      mPrefsService.setDefaultIiopVersion(version);
+   }
+
+   public String getDefaultIiopVersion()
+   {
+      return mPrefsService.getDefaultIiopVersion();
+   }
+
+   /**
+    * Changes the default preferences file name to be the given name.
+    */
+   public void setFileName(String name)
+   {
+      mPrefsService.setFileName(name);
+   }
+
+   /**
+    * Changes the default preferences file object to use the given object.
+    */
+   public void setFile(java.io.File prefsFile)
+   {
+      mPrefsService.setFile(prefsFile);
+   }
+
+   /**
+    * Loads the user's prefernces file if one exists.  If the user has not
+    * defined a preferences file (i.e., the named preferences file does not
+    * exist), one is created.
+    */
+   public synchronized void load()
+   {
+      mPrefsService.load();
+   }
+
+   /**
+    * Saves the current prefernces document.
+    */
+   public synchronized void save()
+   {
+      mPrefsService.save();
+   }
+
+   private GlobalPreferencesService mPrefsService = null;
+}
