a054d281ca0bbf5c0e8966e75ba636af3f276cf8
Patrick Hartling <patrick.hartling@gmail.com>
2003-08-20 05:26:33 +0000

    It turns out that the official Python build for Win32 prefers that modules use the extension .pyd instead of .dll.  This handy feature prevents runtime loader clashes between a DLL and a Python extension module.  With this knowledge, I am going back to having PyJuggler split up into multiple Python module files (with the .pyd extension on Windows and .so on UNIX). I prefer this because the way I was doing things in the single file was a little questionable.  This approach is also more flexible for users who only want to load specific pieces of PyJuggler into their Python applications.
    Bumped version to 0.8.2.  As before when the code was combined into a
    single module file, there are no changes required to user code.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13389 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ PyJuggler/dist/__init__.py
@@ -0,0 +1,39 @@
+# ************** <auto-copyright.pl BEGIN do not edit this line> **************
+#
+# PyJuggler is (C) Copyright 2002, 2003 by Patrick Hartling
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# -----------------------------------------------------------------
+# File:          $RCSfile$
+# Date modified: $Date$
+# Version:       $Revision$
+# -----------------------------------------------------------------
+#
+# *************** <auto-copyright.pl END do not edit this line> ***************
+
+import string
+__all__     = ["vrj", "gadget", "jccl"]
+__build__   = 1
+__version__ = '0.8.2.%d' % __build__
+__date__    = string.join(string.split('$Date$')[1:3], ' ')
+__author__  = 'Patrick Hartling <patrick@vrjuggler.org>'
+__doc__     = '''This is PyJuggler.  For information regarding PyJuggler see:
+    http://www.vrjuggler.org/pyjuggler/
+
+For information on VR Juggler see:
+    http://www.vrjuggler.org'''
+del string
