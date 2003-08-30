e1d99e1e022f4e8b47525c2255562a0aaedb01c9
Patrick Hartling <patrick.hartling@gmail.com>
2003-08-30 15:19:42 +0000

    Do not provide the virtual function callback hooks so that a Python class can derive from vpr.GUID and override writeObject() and/or readObject(). This is not useful in general, and removing the code for allowing that capability fixes a Visual C++ 7.1 compile failure (related to anonymous namespaces).
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13429 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/src/vpr/_GUID.cpp
+++ PyJuggler/src/vpr/_GUID.cpp
@@ -32,90 +32,19 @@
 // Using =======================================================================
 using namespace boost::python;
 
-// Declarations ================================================================
-
-
-namespace  {
-
-
-struct vpr_GUID_Wrapper: vpr::GUID
-{
-    vpr_GUID_Wrapper(PyObject* self_, const vpr::GUID::GenerateTag p0):
-        vpr::GUID(p0), self(self_) {}
-
-    vpr_GUID_Wrapper(PyObject* self_):
-        vpr::GUID(), self(self_) {}
-
-    vpr_GUID_Wrapper(PyObject* self_, const vpr::GUID::StdGUID & p0):
-        vpr::GUID(p0), self(self_) {}
-
-    vpr_GUID_Wrapper(PyObject* self_, const char * p0):
-        vpr::GUID(p0), self(self_) {}
-
-    vpr_GUID_Wrapper(PyObject* self_, const std::basic_string<char,std::char_traits<char>,std::allocator<char> > & p0):
-        vpr::GUID(p0), self(self_) {}
-
-    vpr_GUID_Wrapper(PyObject* self_, const vpr::GUID & p0, const std::basic_string<char,std::char_traits<char>,std::allocator<char> > & p1):
-        vpr::GUID(p0, p1), self(self_) {}
-
-    vpr_GUID_Wrapper(PyObject* self_, const vpr::GUID & p0):
-        vpr::GUID(p0), self(self_) {}
-
-    vpr::ReturnStatus writeObject(vpr::ObjectWriter * p0) {
-        try
-        {
-            return call_method< vpr::ReturnStatus >(self, "writeObject", p0);
-        }
-        catch(error_already_set)
-        {
-            PyErr_Print();
-        }
-
-        return vpr::ReturnStatus::Fail;
-    }
-
-    vpr::ReturnStatus default_writeObject(vpr::ObjectWriter * p0) {
-        return vpr::GUID::writeObject(p0);
-    }
-
-    vpr::ReturnStatus readObject(vpr::ObjectReader * p0) {
-        try
-        {
-            return call_method< vpr::ReturnStatus >(self, "readObject", p0);
-        }
-        catch(error_already_set)
-        {
-            PyErr_Print();
-        }
-
-        return vpr::ReturnStatus::Fail;
-    }
-
-    vpr::ReturnStatus default_readObject(vpr::ObjectReader * p0) {
-        return vpr::GUID::readObject(p0);
-    }
-
-    PyObject* self;
-};
-
-
-
-}// namespace 
-
 
 // Module ======================================================================
 void _Export_GUID()
 {
     scope* vpr_GUID_scope = new scope(
-    class_< vpr::GUID, vpr_GUID_Wrapper >("GUID", init<  >())
+    class_< vpr::GUID >("GUID", init<  >())
         .def(init< const vpr::GUID::GenerateTag & >())
-        .def(init< const vpr::GUID::StdGUID & >())
         .def(init< const char * >())
         .def(init< const std::basic_string<char,std::char_traits<char>,std::allocator<char> > & >())
         .def(init< const vpr::GUID &, const std::basic_string<char,std::char_traits<char>,std::allocator<char> > & >())
         .def(init< const vpr::GUID & >())
-        .def("writeObject", &vpr::GUID::writeObject, &vpr_GUID_Wrapper::default_writeObject)
-        .def("readObject", &vpr::GUID::readObject, &vpr_GUID_Wrapper::default_readObject)
+        .def("writeObject", &vpr::GUID::writeObject)
+        .def("readObject", &vpr::GUID::readObject)
         .def("toString", &vpr::GUID::toString)
         .def("generate", (void (vpr::GUID::*)() )&vpr::GUID::generate)
         .def("generate", (void (vpr::GUID::*)(const vpr::GUID &, const std::basic_string<char,std::char_traits<char>,std::allocator<char> > &) )&vpr::GUID::generate)
