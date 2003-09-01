31b7074f53584eccd25bf531736b6c05ae9cab03
Allen Bierbaum <abierbaum@gmail.com>
2003-09-01 22:16:04 +0000

    Refactored the signal test case to print the current call stack in the signal handler.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@13447 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/test/TestSuite/TestCases/Thread/SignalTest.cpp
+++ modules/vapor/test/TestSuite/TestCases/Thread/SignalTest.cpp
@@ -6,6 +6,8 @@
 #include <TestCases/Thread/SignalTest.h>
 #include <boost/concept_check.hpp>
 
+#include <vpr/System.h>
+
 
 static RETSIGTYPE handlerSIGSEGV
 #if defined(VPR_OS_IRIX) && defined(__GNUC__)
@@ -17,8 +19,9 @@ static RETSIGTYPE handlerSIGSEGV
    boost::ignore_unused_variable_warning(signum);
 #endif
 
-//   std::cout << "\n\n\nSignal caught\n" << std::flush;
-//   throw new CppUnit::Exception("stuff");
+   std::cout << "\n\n\nSignal caught\n" << std::flush;
+   std::cout << "Cur stack:\n" << vpr::System::getCallStack() << std::endl;
+   //throw new CppUnit::Exception("stuff");
    return;
 }
 
@@ -34,11 +37,11 @@ void SignalTest::testSegFault ()
    char* bad_addr;
    vpr::ReturnStatus status;
 
-   status = vpr::SigHandler::registerHandler(SIGSEGV, segv_action);
+   status = vpr::SigHandler::registerHandler(SIGSEGV, segv_action, false);
    CPPUNIT_ASSERT(status.success() && "Handler registration failed");
 
    bad_addr = (char*) 0x1;
-   memcpy(buffer, bad_addr, sizeof(buffer));
+   memcpy(buffer, bad_addr, 1);
 
    CPPUNIT_ASSERT(false);  // Execution should not reach this point
 }
