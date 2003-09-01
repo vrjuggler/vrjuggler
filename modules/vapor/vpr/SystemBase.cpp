77fdb407c2492593db9f099415af415230112dcd
Patrick Hartling <patrick.hartling@gmail.com>
2003-09-01 02:42:01 +0000

    Moved the implementation of vpr::SystemBase::getStackTrace() into a .cpp file, mainly to get the platform-specific #ifdef's out of the header file. Besides that, there is really no point in inlining a function that will be called almost exclusively when an error/crash occurs.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@13440 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/SystemBase.cpp
@@ -0,0 +1,89 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2003 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <vpr/vprConfig.h>
+
+#if defined(VPR_OS_FreeBSD) || defined(VPR_OS_Linux)
+#include <sys/types.h>
+#include <unistd.h>
+#include <execinfo.h>
+
+#include <string>
+#include <sstream>
+#endif
+
+#include <vpr/SystemBase.h>
+
+
+namespace vpr
+{
+
+std::string SystemBase::getCallStack()
+{
+   std::string ret_stack("<Call stack not supported>");
+
+   // XXX: should come up with better test for glib
+#if defined(VPR_OS_FreeBSD) || defined(VPR_OS_Linux)
+   void* trace_syms[100];
+   size_t size;
+   char **strings;
+
+   pid_t cur_pid = getpid();
+   size = backtrace (trace_syms, 100);
+   strings = backtrace_symbols (trace_syms, size);
+
+   std::ostringstream trace_stream;
+   trace_stream << "Stack trace: thread: " << cur_pid << std::endl;
+
+   for (size_t i = 0; i < size; ++i)
+   {
+      trace_stream << "   " << strings[i] << std::endl;
+   }
+
+   free(strings);
+
+   ret_stack = trace_stream.str();
+#endif
+
+   return ret_stack;
+}
+
+}
