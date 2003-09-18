acf5f80a58728a2ea1f418aecde949246988d973
Patrick Hartling <patrick.hartling@gmail.com>
2003-09-18 21:24:24 +0000

    New feature:	The new method vpr::LibraryLoader::findAndInitDSO() will 		search for a DSO based on a platform-independent "base 		name" and a path.  The platform-dependent details are 		maintained inside vpr::LibraryLoader. Refactor:	vpr::LibraryLoader::findAndInitDSO() now takes a callable 		object (a Boost.Function instance) that it uses as a 		callback.  This is done instead of requiring that the 		initialization/factory function for a DSO have a specific 		signature.  Now, it is the responsibility of the calling 		code to handle the specfics of invoking the entry point 		into the DSO.
    The "platform-specifc" details mentioned above include the file extension
    for DSOs (.dll, .so, or .dylib) and whether a debug or an optimized version
    should be released.  A debug build of vpr::LibraryLoader will search for
    debug DSOs; an optmized build will search for optimized DSOs.  Debug versions
    are characterized by having "_d" appended to their base name.  The actual
    binary issues are the responsibility of the developer.  This is being done
    to help alleviate compatibility problems with debug/release code generated
    by Visual C++.  Note that only vpr::LibraryLoader::findAndInitDSO() makes
    use of this convention.
    
    A collection of tests has been added for vpr::LibraryLoader.
    
    Suggested by:	allenb
    Reviewed by:	allenb, aronb, browner10 (at least the initial ideas)
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13507 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/vapor/test/TestSuite/TestCases/DynLoad/LibraryLoaderTest.cpp
@@ -0,0 +1,157 @@
+#include <fstream>
+#include <string>
+
+#include <vpr/vpr.h>
+#include <vpr/DynLoad/Library.h>
+#include <vpr/DynLoad/LibraryLoader.h>
+#include <vpr/System.h>
+#include <vpr/Util/Debug.h>
+#include <TestCases/DynLoad/modules/TestInterface.h>
+
+#include <TestCases/DynLoad/LibraryLoaderTest.h>
+
+
+namespace vprTest
+{
+
+CPPUNIT_TEST_SUITE_REGISTRATION(LibraryLoaderTest);
+
+#if defined(VPR_OS_Win32)
+static const std::string MODULE1("loadermod1.dll");
+static const std::string MODULE2("loadermod2.dll");
+#elif defined(VPR_OS_Darwin)
+static const std::string MODULE1("loadermod1.dylib");
+static const std::string MODULE2("loadermod2.dylib");
+#else
+static const std::string MODULE1("loadermod1.so");
+static const std::string MODULE2("loadermod2.so");
+#endif
+
+#if defined(VPR_OS_Win32)
+static const std::string PATH_SEP("\\");
+#else
+static const std::string PATH_SEP("/");
+#endif
+
+void LibraryLoaderTest::setUp()
+{
+   const std::string base_path(MODULE_DIR);
+
+   std::string mod1_path = base_path + PATH_SEP + MODULE1;
+   mModules[0].first     = mod1_path;
+   mModules[0].second    = "sumFunc";
+
+   std::string mod2_path = base_path + PATH_SEP + MODULE2;
+   mModules[1].first     = mod2_path;
+   mModules[1].second    = "otherFunc";
+}
+
+static bool rawCallback(void* creator)
+{
+   int (*init_func)(int, int);
+   init_func = (int (*)(int, int)) creator;
+
+   int sum = (*init_func)(4, 5);
+   CPPUNIT_ASSERT(sum == 9);
+
+   return true;
+}
+
+class LoaderFunctor
+{
+public:
+   bool operator()(void* arg)
+   {
+      void (*init_func)(const char*);
+      init_func = (void (*)(const char*)) arg;
+      init_func("Hello");
+      return true;
+   }
+};
+
+void LibraryLoaderTest::loadAndInitTestRaw()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+
+   vpr::LibraryPtr dso(new vpr::Library(mModules[0].first));
+   status = loader.loadAndInitDSO(dso, mModules[0].second, rawCallback);
+   CPPUNIT_ASSERT(status.success());
+}
+
+void LibraryLoaderTest::loadAndInitTestFunctor()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+
+   LoaderFunctor functor;
+   vpr::LibraryPtr dso(new vpr::Library(mModules[1].first));
+   status = loader.loadAndInitDSO(dso, mModules[1].second, functor);
+   CPPUNIT_ASSERT(status.success());
+}
+
+void LibraryLoaderTest::findAndInitTest()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+   std::vector<std::string> path(1);
+
+   path[0] = std::string(MODULE_DIR);
+   status = loader.findAndInitDSO("loadermod1", path, mModules[0].second,
+                                  rawCallback);
+   CPPUNIT_ASSERT(status.success());
+}
+
+void LibraryLoaderTest::multiLoadTest()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+
+   vpr::LibraryPtr dso1(new vpr::Library(mModules[0].first));
+   status = loader.loadAndInitDSO(dso1, mModules[0].second, rawCallback);
+   CPPUNIT_ASSERT(status.success());
+
+   LoaderFunctor functor;
+   vpr::LibraryPtr dso2(new vpr::Library(mModules[1].first));
+   status = loader.loadAndInitDSO(dso2, mModules[1].second, functor);
+   CPPUNIT_ASSERT(status.success());
+}
+
+void LibraryLoaderTest::loadFailureTest()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+
+   vpr::Debug::instance()->disableOutput();
+      vpr::LibraryPtr dso(new vpr::Library("loadFailureTest"));
+      status = loader.loadAndInitDSO(dso, "notThere", rawCallback);
+      CPPUNIT_ASSERT(status.failure());
+   vpr::Debug::instance()->enableOutput();
+}
+
+void LibraryLoaderTest::lookupFailureTest()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+
+   vpr::Debug::instance()->disableOutput();
+      vpr::LibraryPtr dso1(new vpr::Library(mModules[0].first));
+      status = loader.loadAndInitDSO(dso1, "lookupFailureTest", rawCallback);
+      CPPUNIT_ASSERT(status.failure());
+   vpr::Debug::instance()->enableOutput();
+}
+
+void LibraryLoaderTest::findFailureTest()
+{
+   vpr::LibraryLoader loader;
+   vpr::ReturnStatus status;
+   std::vector<std::string> path(0);
+
+   vpr::Debug::instance()->disableOutput();
+      status = loader.findAndInitDSO("findFailureTest", path, "notThere",
+                                     rawCallback);
+      CPPUNIT_ASSERT(status.failure());
+   vpr::Debug::instance()->enableOutput();
+}
+
+}
