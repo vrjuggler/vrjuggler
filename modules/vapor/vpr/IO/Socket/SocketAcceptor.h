a2db2b10b20319b9de2976817ebd8f3b35228fa7
Patrick Hartling <patrick.hartling@gmail.com>
2003-11-03 20:23:48 +0000

    Export this class in the DLL on Windows.  Fixed Doxygen formatting problems.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@13740 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/IO/Socket/SocketAcceptor.h
+++ modules/vapor/vpr/IO/Socket/SocketAcceptor.h
@@ -54,7 +54,7 @@ namespace vpr
  * Socket connection acceptor factory.
  * Wraps all the nitty-gritty details of accepting a connection.
  */
-class SocketAcceptor
+class VPR_CLASS_API SocketAcceptor
 {
 public:
    /**
