10185afef79eb3fdb67580014d172dba897752a9
Patrick Hartling <patrick.hartling@gmail.com>
2003-11-07 23:02:31 +0000

    Bug fixed:	The graphical editor for the Java GUI's global preferences 		was saving the wrong path information for the user's 		preferred starting directory for file choosers.  It always 		saved the absolute path instead of using the internal 		values when they would have been appropriate.
    Bumped version to 0.16.2.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13810 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/tweek/java/org/vrjuggler/tweek/services/GlobalPreferencesServiceProxy.java
+++ juggler/modules/tweek/java/org/vrjuggler/tweek/services/GlobalPreferencesServiceProxy.java
@@ -168,10 +168,9 @@ public class GlobalPreferencesServiceProxy
    }
 
    /**
-    * Returns the user's current preferred starting directory.  This may be
-    * one of the default list, or it may be a user-defined setting.  The path
-    * itself is returned as the actual preferred path rather than the
-    * internal preference setting.
+    * Returns the user's current preferred starting directory for file
+    * choosers.  The string returned is the actual preferred path rather than
+    * the internal preference setting.
     *
     * @return A string that represents a path on the local system.  This can
     *         be passed directly to the java.io.File constructor, for example.
@@ -179,6 +178,16 @@ public class GlobalPreferencesServiceProxy
    public String getChooserStartDir()
    {
       return mPrefsService.getChooserStartDir();
+   }
+
+   /**
+    * Returns the "raw" version of the user's current preferred starting
+    * directory for file choosers.  This may be one of the default list, or it
+    * may be a user-defined string (an actual path).
+    */
+   public String getRawChooserStartDir()
+   {
+      return mPrefsService.getRawChooserStartDir();
    }
 
    /**
