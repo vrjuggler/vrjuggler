d47c2ccfd9dadce0116aea1c7eca936d5e538612
Patrick Hartling <patrick.hartling@gmail.com>
2003-11-17 20:34:11 +0000

    Refactor:	This plug-in is now initialized with a configuration 		element.  The init() and enable() methods are stubbed out, 		and the CORBA stuff is only enabled when a configuration 		element of type corba_remote_reconfig is passed to us.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13878 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/plugins/corba_rtrc/CorbaRemoteReconfig.cpp
+++ juggler/modules/jackal/plugins/corba_rtrc/CorbaRemoteReconfig.cpp
@@ -33,12 +33,15 @@
 #include <jccl/Plugins/PluginConfig.h>
 
 #include <stdlib.h>
+#include <boost/concept_check.hpp>
 #include <vpr/vpr.h>
 #include <vpr/Util/Debug.h>
 
 #include <jccl/Util/Debug.h>
+#include <jccl/RTRC/ConfigManager.h>
 #include <corba_rtrc/RemoteReconfigSubjectImpl.h>
 #include <corba_rtrc/CorbaRemoteReconfig.h>
+
 
 jccl::RemoteReconfig* initPlugin(jccl::ConfigManager* configMgr)
 {
@@ -55,31 +58,122 @@ CorbaRemoteReconfig::CorbaRemoteReconfig(jccl::ConfigManager* configMgr)
    , mEnabled(false)
    , mInterfaceName("CorbaRemoteReconfig")
 {
-   /* Do nothing. */ ;
+   mConfigManager->addConfigElementHandler(this);
 }
 
 CorbaRemoteReconfig::~CorbaRemoteReconfig()
 {
+   mConfigManager->removeConfigElementHandler(this);
    mConfigManager = NULL;
+   stopCorba();
+}
 
-   //Clean up interface by disconnecting first
-   if (mInterface != NULL)
+bool CorbaRemoteReconfig::configCanHandle(jccl::ConfigElementPtr element)
+{
+   return element->getID() == std::string("corba_remote_reconfig");
+}
+
+bool CorbaRemoteReconfig::configAdd(jccl::ConfigElementPtr element)
+{
+   // If the ORB is already running, we need to shut it down first.  One big
+   // reason for doing this is to release the resources (memory and so on)
+   // allocated previously.
+   if ( mInterface != NULL || mCorbaManager != NULL )
+   {
+      vprDEBUG(jcclDBG_PLUGIN, vprDBG_STATE_LVL)
+         << "[CorbaRemoteReconfig::configAdd()] Attempting to shut down "
+         << "existing CORBA instance.\n" << vprDEBUG_FLUSH;
+
+      // stopCorba() will call disable() if we are still in the enabled state.
+      stopCorba();
+   }
+
+   const std::string ns_host =
+      element->getProperty<std::string>("naming_service_host");
+   const vpr::Uint16 ns_port =
+      element->getProperty<vpr::Uint16>("naming_service_port");
+   const std::string iiop_version =
+      element->getProperty<std::string>("iiop_version");
+
+   // We'll ignore the return value for now.  startCorba() prints out enough
+   // warning information on its own if something goes wrong.
+   this->startCorba(ns_host, ns_port, iiop_version);
+
+   return true;
+}
+
+bool CorbaRemoteReconfig::configRemove(jccl::ConfigElementPtr element)
+{
+   boost::ignore_unused_variable_warning(element);
+
+   if ( isEnabled() )
    {
       disable();
-      delete mInterface;
-      mInterface = NULL;
    }
 
-   // Clean up the corba manager and delete it.
-   if (mCorbaManager != NULL)
-   {
-      // CORBA Manager has its own destructor cleanup.
-      delete mCorbaManager;
-      mCorbaManager = NULL;
-   }
+   return true;
 }
 
 vpr::ReturnStatus CorbaRemoteReconfig::init()
+{
+   return vpr::ReturnStatus();
+}
+
+vpr::ReturnStatus CorbaRemoteReconfig::enable()
+{
+   return vpr::ReturnStatus();
+}
+
+bool CorbaRemoteReconfig::isEnabled() const
+{
+   return mEnabled;
+}
+
+void CorbaRemoteReconfig::disable()
+{
+   //Make sure the corba manager has been initialized
+   if ( mInterface == NULL || mCorbaManager == NULL || ! mEnabled )
+   {
+      vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
+         << clrOutBOLD(clrYELLOW, "WARNING:")
+         << " [CorbaRemoteReconfig::disable()] Cannot disable interface "
+         << "until it has been initialized!\n" << vprDEBUG_FLUSH;
+
+      return;
+   }
+
+   try
+   {
+      // Unregister the subject (note that observers must handle this
+      // disconnection).
+      vpr::ReturnStatus status;
+
+      // Attempt to un-register the subject.
+      status = mCorbaManager->getSubjectManager()->unregisterSubject(mInterfaceName.c_str());
+
+      if ( ! status.success() )
+      {
+         vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
+            << clrOutBOLD(clrYELLOW, "WARNING:")
+            << " [CorbaRemoteReconfig::disable()] Could not unregister subject."
+            << std::endl << vprDEBUG_FLUSH;
+      }
+   }
+   catch (...)
+   {
+      vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
+         << clrOutBOLD(clrYELLOW, "WARNING:")
+         << " [CorbaRemoteReconfig::disable()] Caught an exception while "
+         << "trying to unregister subject.\n" << vprDEBUG_FLUSH;
+   }
+
+   mCorbaManager->shutdown();
+   mEnabled = false;
+}
+
+vpr::ReturnStatus CorbaRemoteReconfig::startCorba(const std::string& nsHost,
+                                                  const vpr::Uint16 nsPort,
+                                                  const std::string& iiopVer)
 {
    // Create new CORBA Manager and initialize it.
    mCorbaManager = new tweek::CorbaManager;
@@ -91,7 +185,8 @@ vpr::ReturnStatus CorbaRemoteReconfig::init()
       int dummy_int(0);
 
       // Attempt to initialize the CORBA Manager.
-      status = mCorbaManager->init("corba_rtrc", dummy_int, NULL);
+      status = mCorbaManager->init("corba_rtrc", dummy_int, NULL, nsHost,
+                                   nsPort, iiopVer);
 
       // Test to see if init succeeded.
       if ( status.success() )
@@ -110,6 +205,26 @@ vpr::ReturnStatus CorbaRemoteReconfig::init()
 
                // Create an intstance of our subject.
                mInterface = new RemoteReconfigSubjectImpl;
+
+               // Register the subject with the CORBA Manager.
+               try
+               {
+                  // Attempt to register the subject.
+                  mCorbaManager->getSubjectManager()->registerSubject(mInterface, mInterfaceName.c_str());
+                  mEnabled = true;
+               }
+               catch(...)
+               {
+                  vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
+                     << clrOutBOLD(clrRED, "ERROR:")
+                     << " [CorbaRemoteReconfig::startCorba()] Caught an "
+                     << "exception while trying to register subject.\n"
+                     << vprDEBUG_FLUSH;
+                  vprDEBUG_NEXT(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
+                     << "Disabling CORBA remote run-time reconfiguration.\n"
+                     << vprDEBUG_FLUSH;
+                  status.setCode(vpr::ReturnStatus::Fail);
+               }
             }
             else
             {
@@ -132,8 +247,9 @@ vpr::ReturnStatus CorbaRemoteReconfig::init()
    catch(...)
    {
       vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-         << clrOutBOLD(clrRED, "ERROR:") << " [CorbaRemoteReconfig::init()]: "
-         << err_msg << std::endl << vprDEBUG_FLUSH;
+         << clrOutBOLD(clrRED, "ERROR:")
+         << " [CorbaRemoteReconfig::startCorba()] " << err_msg << std::endl
+         << vprDEBUG_FLUSH;
       vprDEBUG_NEXT(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
          << "Disabling CORBA remote run-time reconfiguration.\n"
          << vprDEBUG_FLUSH;
@@ -146,89 +262,27 @@ vpr::ReturnStatus CorbaRemoteReconfig::init()
    return status;
 }
 
-vpr::ReturnStatus CorbaRemoteReconfig::enable()
+void CorbaRemoteReconfig::stopCorba()
 {
-   vpr::ReturnStatus status;
-
-   //Make sure the corba manager has been initialized
-   if ( mInterface == NULL || mCorbaManager == NULL )
+   //Clean up interface by disconnecting first
+   if (mInterface != NULL)
    {
-      vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-         << "CorbaRemoteReconfig::enable: Cannot enable interface until it has been initialized\n"
-         << vprDEBUG_FLUSH;
-
-      status.setCode(vpr::ReturnStatus::Fail);
-   }
-   else
-   {
-      // Register the subject with the CORBA Manager.
-      try
+      if ( isEnabled() )
       {
-         // Attempt to register the subject.
-         mCorbaManager->getSubjectManager()->registerSubject(mInterface, mInterfaceName.c_str());
-         mEnabled = true;
+         disable();
       }
-      catch(...)
-      {
-         vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-            << clrOutBOLD(clrRED, "ERROR:")
-            << " [CorbaRemoteReconfig::enable()]: Caught an exception while "
-            << "trying to register subject.\n" << vprDEBUG_FLUSH;
-         vprDEBUG_NEXT(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-            << "Disabling CORBA remote run-time reconfiguration.\n"
-            << vprDEBUG_FLUSH;
-         status.setCode(vpr::ReturnStatus::Fail);
-      }
+
+      delete mInterface;
+      mInterface = NULL;
    }
 
-   return status;
-}
-
-bool CorbaRemoteReconfig::isEnabled() const
-{
-   return mEnabled;
-}
-
-void CorbaRemoteReconfig::disable()
-{
-   //Make sure the corba manager has been initialized
-   if ( mInterface == NULL || mCorbaManager == NULL || ! mEnabled )
+   // Clean up the corba manager and delete it.
+   if (mCorbaManager != NULL)
    {
-      vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-         << clrOutBOLD(clrYELLOW, "WARNING:")
-         << " [CorbaRemoteReconfig::disable()]: Cannot disable interface "
-         << "until it has been initialized!\n" << vprDEBUG_FLUSH;
-
-      return;
+      // CORBA Manager has its own destructor cleanup.
+      delete mCorbaManager;
+      mCorbaManager = NULL;
    }
-
-   try
-   {
-      // Unregister the subject (note that observers must handle this
-      // disconnection).
-      vpr::ReturnStatus status;
-
-      // Attempt to un-register the subject.
-      status = mCorbaManager->getSubjectManager()->unregisterSubject(mInterfaceName.c_str());
-
-      if ( ! status.success() )
-      {
-         vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-            << clrOutBOLD(clrYELLOW, "WARNING:")
-            << " [CorbaRemoteReconfig::disable()]: Could not unregister subject."
-            << std::endl << vprDEBUG_FLUSH;
-      }
-   }
-   catch (...)
-   {
-      vprDEBUG(jcclDBG_PLUGIN, vprDBG_WARNING_LVL)
-         << clrOutBOLD(clrYELLOW, "WARNING:")
-         << " [CorbaRemoteReconfig::disable()]: Caught an exception while "
-         << "trying to unregister subject.\n" << vprDEBUG_FLUSH;
-   }
-
-   mCorbaManager->shutdown();
-   mEnabled = false;
 }
 
 } // End of jccl namespace
