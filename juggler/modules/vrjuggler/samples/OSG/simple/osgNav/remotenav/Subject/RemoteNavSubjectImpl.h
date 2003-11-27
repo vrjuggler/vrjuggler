4f21c053fe66b6691ac596c818af7c9af31d4aac
Allen Bierbaum <abierbaum@gmail.com>
2003-11-27 07:07:50 +0000

    Commiting Subject and facade changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@13920 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/samples/OSG/simple/osgNav/remotenav/Subject/RemoteNavSubjectImpl.h
+++ juggler/modules/vrjuggler/samples/OSG/simple/osgNav/remotenav/Subject/RemoteNavSubjectImpl.h
@@ -1,37 +1,56 @@
 #ifndef _REMOTE_NAV_SUBJECT_IMPL_H_
 #define _REMOTE_NAV_SUBJECT_IMPL_H_
 
-#include <tweek/CORBA/Subject.h>
+#include <tweek/CORBA/SubjectImpl.h>
+#include <nav.h>
+#include <RemoteNavSubject.h>
 
 class RemoteNavSubjectImpl
-   : POA_vrj::RemoteNavSubject
-   , tweek::SubjectImpl
+   : public POA_vrj::RemoteNavSubject
+   , public tweek::SubjectImpl
 {
 public:
-   RemoteNavSubjectImpl();
+   RemoteNavSubjectImpl(OsgNavigater* nav) : mNavigater(nav)
+   {;}
 
    virtual ~RemoteNavSubjectImpl()
    {;}
 
-   void setPosition(CORBA::Float xPos, CORBA::Float yPos, CORBA::Float zPos);
+   void setPosition(CORBA::Float xPos, CORBA::Float yPos, CORBA::Float zPos)
+   {;}
 
-   void getPosition(CORBA::Float& xPos, CORBA::Float& yPos, CORBA::Float& zPos);
+   void getPosition(CORBA::Float& xPos, CORBA::Float& yPos, CORBA::Float& zPos)
+   {;}
 
    void getOrientation(CORBA::Float& heading, CORBA::Float& pitch,
-                       CORBA::Float& roll);
+                       CORBA::Float& roll)
+   {;}
 
    void setVelocity(CORBA::Float xDelta, CORBA::Float yDelta,
-                    CORBA::Float zDelta);
+                    CORBA::Float zDelta)
+   {
+      mNavigater->setVelocity(gmtl::Vec3f(xDelta, yDelta, zDelta));
+   }
 
    void setRotationalVelocity(CORBA::Float headingDelta,
-                              CORBA::Float pitchDelta, CORBA::Float rollDelta);
+                              CORBA::Float pitchDelta, CORBA::Float rollDelta)
+   {
+      gmtl::EulerAngleXYZf euler( headingDelta, pitchDelta, rollDelta );
+      gmtl::Matrix44f real = gmtl::makeRot<gmtl::Matrix44f>( euler ); 
+      mNavigater->setRotationalVelocity(real);
+   }
 
-   void setWalkMode(CORBA::Boolean walk);
+   void setWalkMode(CORBA::Boolean walk)
+   {
+   }
 
    vrj::RemoteNavSubject_ptr _this()
    {
-      return POA_vrj::RemoveNavSubject::_this();
+      return POA_vrj::RemoteNavSubject::_this();
    }
+
+private:
+   OsgNavigater* mNavigater;
 };
 
 
