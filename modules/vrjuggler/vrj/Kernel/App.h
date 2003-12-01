195f0d9f5c8c19e7fb35e9b39e27007917d4b585
Patrick Hartling <patrick.hartling@gmail.com>
2003-12-01 23:03:56 +0000

    Fixed loads of Doxygen formatting errors.  Why is it so hard for people to end sentences with punctuation?!?  Doxygen depends on that sort of thing. Fixed coding standard violations while I was here, and removed a line of code that was commented out years ago.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@13981 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Kernel/App.h
+++ modules/vrjuggler/vrj/Kernel/App.h
@@ -66,7 +66,7 @@ class Kernel;
  *  the application programmer uses to interface with VR Juggler.
  *
  *  The control loop will look similar to this: <br> <br>
- *  NOTE: One time through the loop is a Juggler Frame <br>
+ *  @note One time through the loop is a Juggler Frame <br>
  *
  *  while (drawing)           <br>
  *  {                         <br>
@@ -82,13 +82,15 @@ class Kernel;
 class VJ_CLASS_API App : public jccl::ConfigElementHandler
 {
 public:
-   /** Constructor
-    * @param kern - The Kernel that is active.
-    *    So application has easy access to kernel
+   /**
+    * Constructor.
+    * @param kern The Kernel that is active (so application has easy access to
+    *             the kernel).
+    *    
     */
    App(Kernel* kern);
 
-   /** Just call App(Kernel::instance()) */
+   /** Just call App(Kernel::instance()). */
    App();
 
    virtual ~App()
@@ -98,67 +100,74 @@ public:
 
 public:
 
-   /** Application init function
-    * Execute any initialization needed before the API is started
-    * @note: Derived classes MUST call base class version of this method
+   /**
+    * Application initialization function.
+    * Execute any initialization needed before the API is started.
+    * @note Derived classes MUST call base class version of this method.
     */
    virtual void init()
    {;}
 
-   /** Application API init function
+   /**
+    * Application API initialization function.
     * Execute any initialization needed <b>after</b> API is started
-    *  but before the drawManager starts the drawing loops.
+    * but before the drawManager starts the drawing loops.
     */
    virtual void apiInit()
    {;}
 
-   /** Execute any final cleanup needed for the application
-    */
+   /** Executes any final cleanup needed for the application. */
    virtual void exit()
    {;}
 
-   /** Function called before juggler frame starts.
-    * Called after tracker update but before start of a new frame
+   /**
+    * Function called before juggler frame starts.
+    * Called after tracker update but before start of a new frame.
     */
    virtual void preFrame()
    {;}
 
-   /** Function called <b>during</b> the application's drawing time
-    */
+   /** Function called <b>during</b> the application's drawing time. */
    virtual void intraFrame()
    {;}
 
-   /** Function called before updating trackers but after the frame is complete
+   /**
+    * Function called before updating trackers but after the frame is complete.
     */
    virtual void postFrame()
    {;}
 
-
-   /** Reset the application
+   /**
+    * Resets the application.
     * This is used when the system (or applications) would like the application
     * to reset to the initial state that it started in.
     */
    virtual void reset() {;}
 
-   /** Does the application currently have focus
-    * If an application has focus:
-    * - The user may be attempting to interact with it, so the app should process input
-    * If not,
-    * - The user is not interating with it, so ignore all input
-    * - BUT, the user may still be viewing it, so render and update any animations, etc.
+   /**
+    * Does the application currently have focus?
+    * If an application has focus, the user may be attempting to interact with
+    * it, so the app should process input.  If not, the user is not interating
+    * with it, so ignore all input, but the user may still be viewing it, so
+    * render and update any animations, etc.
     *
-    * This is akin to the way a user can only interact with a GUI window that has focus
-    * (ie.The mouse is over the window)
+    * This is akin to the way a user can only interact with a GUI window that
+    * has focus (i.e., the mouse is over the window).
     */
-   bool haveFocus() { return mHaveFocus;}
+   bool haveFocus()
+   {
+      return mHaveFocus;
+   }
 
-   /** Called when the focus state changes
-    */
+   /** Called when the focus state changes. */
    virtual void focusChanged()
    {;}
 
-   /** Sets the focus state
-    * POST: If state has changed, then calls focusChanged
+   /**
+    * Sets the focus state.
+    * @post If state has changed, then calls focusChanged().
+    *
+    * @see focusChanged()
     */
    void setFocus(bool newState)
    {
@@ -169,14 +178,15 @@ public:
       }
    }
 
-   /** Return scale scale factor to get from Juggler units (meters) to application units.
-   * Internally VR Juggler stores and processes all position values
-   * in meters.  The scale factor returned by this method is
-   * used by VR Juggler to scale the OpenGL drawing state from meters to
-   * whatever local units the application wants to use
-   *
-   * Example: to use feet as local app unit, return 3.28;
-   */
+   /**
+    * Returns scale scale factor to get from Juggler units (meters) to
+    * application units.  Internally VR Juggler stores and processes all
+    * position values in meters.  The scale factor returned by this method is
+    * used by VR Juggler to scale the OpenGL drawing state from meters to
+    * whatever local units the application wants to use.
+    *
+    * Example: to use feet as local app unit, return 3.28;
+    */
    virtual float getDrawScaleFactor()
    {
       return gadget::PositionUnitConversion::ConvertToFeet;
@@ -184,30 +194,34 @@ public:
 
 
 public:  // --- Default config handlers: (inherited from jccl::ConfigElementHandler) --- //
-   // Default to not handling anything
+   /** Default to handling nothing. */
    virtual bool configCanHandle(jccl::ConfigElementPtr element)
    {
       boost::ignore_unused_variable_warning(element);
       return false;
    }
 
-   /** Are any application dependencies satisfied
+   /**
+    * Are any application dependencies satisfied?
     * If the application requires anything special of the system for successful
-    * initialization, check it here.
-    * If retval == false, then the application will not be started yet
-    *    retval == true, application will be allowed to enter the system
+    * initialization, check it here.  If the return value is false, then the
+    * application will not be started yet.  If the return value is true,
+    * application will be allowed to enter the system.
     */
    virtual bool depSatisfied()
-   { return true; }
+   {
+      return true;
+   }
 
 protected:
-   /// @note Inherited from jccl::ConfigElementHandler.
+   /** @note Inherited from jccl::ConfigElementHandler. */
    virtual bool configAdd(jccl::ConfigElementPtr element)
    {
      boost::ignore_unused_variable_warning(element);
      vprASSERT(false);  return false;
    }
-   /// @note Inherited from jccl::ConfigElementHandler.
+
+   /** @note Inherited from jccl::ConfigElementHandler. */
    virtual bool configRemove(jccl::ConfigElementPtr element)
    {
       boost::ignore_unused_variable_warning(element);
@@ -215,20 +229,21 @@ protected:
    }
 
 public:
-   //vjAPI       api;        // Used to signal which API this application works with
-   Kernel*     mKernel;     // The library kernel (here for convienence)
-   bool        mHaveFocus;
+   Kernel* mKernel;     // The library kernel (here for convienence)
+   bool    mHaveFocus;
 
 public:  // --- Factory functions --- //
-   /** Get the DrawManager to use
-    * @NOTE Each derived app MUST implement this function
+   /**
+    * Gets the Draw Manager to use.
+    * @note Each derived app MUST implement this function.
     */
-   virtual DrawManager*    getDrawManager() = 0;
+   virtual DrawManager* getDrawManager() = 0;
 
-   /** Get the SoundManager to use
-    * @NOTE Each derived app could implement this function if needed
+   /**
+    * Get the SoundManager to use.
+    * @note Each derived app could implement this function if needed.
     */
-   virtual SoundManager*    getSoundManager()
+   virtual SoundManager* getSoundManager()
    {
       return &SoundManagerFactory::get();
    }
