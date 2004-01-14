d5950eefd98fdb2fcb97956aff561d48f8c3fae1
Aron Bierbaum <aronbierbaum@gmail.com>
2004-01-14 23:41:38 +0000

    PropertySheets should be created using a factory. Also PropertySheets should listen for ConfigElementEvents instead of hard coding the behavior.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14145 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/editors/org/vrjuggler/jccl/editors/VarListPanel.java
+++ modules/jackal/editors/org/vrjuggler/jccl/editors/VarListPanel.java
@@ -8,17 +8,20 @@ import info.clearthought.layout.*;
 //TODO: Remove these import
 import org.vrjuggler.jccl.config.ConfigElement;
 import org.vrjuggler.jccl.config.PropertyDefinition;
+import org.vrjuggler.jccl.config.event.*;
 
-public class VarListPanel extends ExpandablePanel
+public class VarListPanel extends ExpandablePanel implements ConfigElementListener
 {       
    public VarListPanel(ConfigElement elm, PropertyDefinition prop_def, Color color)
    {
       super(color);
-
-      // XXX: Move this out to its own class soon.
+   
+      elm.addConfigElementListener(this);
+            
       // Create the new property list which is actually just a variable list of
       // property values.
-      PropertySheet new_sheet = new PropertySheet(elm, prop_def, getNextColor());
+      PropertySheet new_sheet = PropertySheetFactory.instance().makeVarSheet(elm, prop_def, getNextColor());
+
       mComponent = new_sheet;
       new_sheet.setBorder(BorderFactory.createEtchedBorder());
 
@@ -29,5 +32,37 @@ public class VarListPanel extends ExpandablePanel
       JLabel name = new JLabel(title);
       TableLayoutConstraints c2 = new TableLayoutConstraints(2, 0, 2, 0, TableLayout.LEFT, TableLayout.TOP);
       add(name, c2);
+   }
+
+   public void nameChanged(ConfigElementEvent evt)
+   {
+      System.out.println("Name changed...");
+   }
+   public void propertyValueChanged(ConfigElementEvent evt)
+   {
+      System.out.println("Property value changed...");
+   }
+   public void propertyValueAdded(ConfigElementEvent evt)
+   {
+      System.out.println("Property value added...");
+      ConfigElement elm = (ConfigElement)evt.getSource();
+      PropertyDefinition prop_def =
+         elm.getDefinition().getPropertyDefinition(evt.getProperty());
+      if(ConfigElement.class == prop_def.getType())
+      {
+         // Use the PropertySheetFactory to add a new embeddedElement.
+         PropertySheetFactory.instance().addEmbeddedElement((PropertySheet)mComponent, elm, evt.getValue(), prop_def, 2);
+      }
+      else
+      {
+         String label = prop_def.getPropertyValueDefinition(0).getLabel();
+         PropertySheetFactory.instance().addNormalEditor((PropertySheet)mComponent, elm, 
+                                                         evt.getValue(), prop_def, label, 2,
+                                                         elm.getPropertyValueCount(prop_def.getToken()) - 1);
+      }
+   }
+   public void propertyValueRemoved(ConfigElementEvent evt)
+   {
+      System.out.println("Property value removed...");
    }
 }
