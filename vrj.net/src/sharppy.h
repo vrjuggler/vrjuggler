548e8c0c7fa6391e8d8977a3ae069c802af8a3d9
Patrick Hartling <patrick.hartling@gmail.com>
2004-01-15 03:32:08 +0000

    Added a Sharppy-generated file that I overlooked in the initial VRJ.NET import.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@14148 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ vrj.net/src/sharppy.h
@@ -0,0 +1,39 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VRJ.NET is (C) Copyright 2004 by Patrick Hartling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _SHARPPY_H_
+#define _SHARPPY_H_
+
+#ifdef _MSC_VER
+#define SHARPPY_API __declspec(dllexport)
+#define SHARPPY_CLASS_API __declspec(dllexport)
+#else
+#define SHARPPY_API
+#define SHARPPY_CLASS_API
+#endif
+
+#endif /* _SHARPPY_H_ */
