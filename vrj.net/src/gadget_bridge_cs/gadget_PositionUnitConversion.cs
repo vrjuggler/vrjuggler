8f1f9b29a4efac0ccfa7934f212ea4bf0b82f224
Patrick Hartling <patrick.hartling@gmail.com>
2004-02-19 23:29:07 +0000

    Added the constants needed for calling methods on objects related to positional devices that can do unit conversions.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@14352 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ vrj.net/src/gadget_bridge_cs/gadget_PositionUnitConversion.cs
@@ -0,0 +1,45 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VRJ.NET is (C) Copyright 2004 by Patrick Hartling
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+// Generated from Revision: 1.2 of RCSfile: free_types_cs.tmpl,v
+using System;
+using System.Runtime.InteropServices;
+
+namespace gadget
+{
+
+public sealed abstract class PositionUnitConversion
+{
+   const float ConvertToFeet = 3.2799999713897705078125e+0f;
+   const float ConvertToInches = 3.9369998931884765625e+1f;
+   const float ConvertToMeters = 1.0e+0f;
+   const float ConvertToCentimeters = 1.0e+2f;
+
+}
+
+
+} // namespace gadget
