190b5c255c83b9eb362a84a84e6a98eb3b73f394
Patrick Hartling <patrick.hartling@gmail.com>
2004-02-27 15:06:06 +0000

    Silence GCC 3.3 warnings that have been bugging me for a long, long time.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@14409 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/drivers/Open/Tweek/TweekAnalogSubjectImpl.h
+++ juggler/modules/gadgeteer/drivers/Open/Tweek/TweekAnalogSubjectImpl.h
@@ -88,7 +88,13 @@ private:
    // exported public symbols.  This causes problems because copying
    // vpr::Mutex objects is not allowed.
    TweekAnalogSubjectImpl(const TweekAnalogSubjectImpl& subj)
-      : POA_gadget::TweekAnalogSubject(subj), tweek::SubjectImpl(subj)
+      : omniServant(subj)
+      , tweek::_impl_Subject(subj)
+      , gadget::_impl_TweekAnalogSubject(subj)
+      , PortableServer::ServantBase(subj)
+      , POA_tweek::Subject(subj)
+      , POA_gadget::TweekAnalogSubject(subj)
+      , tweek::SubjectImpl(subj)
    {
       /* Do nothing. */ ;
    }
