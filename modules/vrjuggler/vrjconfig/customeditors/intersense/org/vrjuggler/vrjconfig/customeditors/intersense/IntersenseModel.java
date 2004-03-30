088a477d8b95f868930a37e83a145d2f295d5369
Aron Bierbaum <aronbierbaum@gmail.com>
2004-03-30 22:13:04 +0000

    Adding the rest of the Intersense custom editor.
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14562 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/vrjconfig/customeditors/intersense/org/vrjuggler/vrjconfig/customeditors/intersense/IntersenseModel.java
@@ -0,0 +1,393 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2003 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+package org.vrjuggler.vrjconfig.customeditors.intersense;
+
+import java.util.*;
+import javax.swing.event.EventListenerList;
+import org.vrjuggler.jccl.config.*;
+import org.vrjuggler.jccl.config.event.*;
+import org.vrjuggler.jccl.editors.*;
+
+/**
+ * Keeps track of all configuration information for an Intersense tracker.
+ *
+ * @see ConfigListener
+ * @see ConfigElementListener
+ */
+public class IntersenseModel implements ConfigListener, ConfigElementListener
+{
+   /** ConfigElement for Intersense tracker. */
+   private ConfigElement   mISenseConfigElement = null;
+
+   /** ConfigContext used by Intersense tracker. */
+   private ConfigContext   mISenseConfigContext = null;
+  
+   /** List of ConfigElements, one for each Intersense station. */
+   private List            mStationElements     = new ArrayList();
+
+   /** List of Intersense station models. */
+   private List            mStationModels       = new ArrayList();
+   
+   /** Location of the Intersense API library supplied by Intersense. */
+   private String          mDriverLocation      = null;
+
+   /** Name of serial port tracker is connected to. */
+   private String          mPortName            = null;
+
+   /** Baud for communication with tracker. */
+   private Integer         mBaud                = null;
+
+   /** PositionFilter that translates and rotates position input into VR Juggler coordinate space. */
+   private ConfigElement   mPositionFilter      = null;
+
+   /** Wether verbose output is enabled for the Intersense API library. */
+   private Boolean         mVerboseEnabled      = null;
+
+   /** The minimum float value for analog input. */
+   private Float           mRangeMin            = null;
+   
+   /** Listeners interested in this Intersense model. */
+   private EventListenerList listenerList = new EventListenerList();
+
+   /**
+    * Returns the location of the Intersense API library supplied by Intersense.
+    */
+   public String getDriverLocation()
+   {
+      return mDriverLocation;
+   }
+   
+   /**
+    * Specify the location of the Intersense API library supplied by Intersense.
+    *
+    * @param location full path to Intersense API library.
+    */
+   public void setDriverLocation(String location)
+   {
+      mDriverLocation = location;
+   }
+
+   /**
+    * Returns the name of the serial port the tracker is connected to.
+    */
+   public String getPortName()
+   {
+      return mPortName;
+   }
+   
+   /**
+    * Specifies the name of the serial port that the tracker is connected to.
+    *
+    * @param name serial port the tracker is connected to.
+    */
+   public void setPortName(String name)
+   {
+      mPortName = name;
+   }
+
+   /**
+    * Returns the baud at which communication with tracker takes place.
+    */
+   public Integer getBaud()
+   {
+      return mBaud;
+   }
+
+   /**
+    * Specifies the baud at which communication with the tracker will take place.
+    * 
+    * @param baud speed of communication with tracker
+    */
+   public void setBaud(Integer baud)
+   {
+      mBaud = baud;
+   }
+
+   /**
+    * Returns a list of all station models associated with this Intersense device.
+    */
+   public List getStationModels()
+   {
+      return mStationModels;
+   }
+
+   /**
+    * Constructor.
+    *
+    * @param elm        ConfigElement for this Intersense device.
+    * @param context    ConfigContext associated with this Intersense device.
+    */
+   public IntersenseModel(ConfigElement elm, ConfigContext context)
+   {
+      // Save references for later use.
+      mISenseConfigElement = elm;
+      mISenseConfigContext = context;
+      
+      // Add a listener to update this model if more config elements are added to context.
+      getBroker().addConfigListener(this);
+
+      System.out.println("================================");
+      System.out.println("Parsing Intersense ConfigElement");
+      System.out.println("================================");
+
+      // Get general information about tracker.
+      mBaud = (Integer)mISenseConfigElement.getProperty("baud",0);
+      mPortName = (String)mISenseConfigElement.getProperty("port",0);
+      mDriverLocation = (String)mISenseConfigElement.getProperty("driver",0);
+      
+      // Get a list of all embedded station elements.
+      java.util.List stations = elm.getPropertyValues("stations");
+      for(Iterator itr = stations.iterator() ; itr.hasNext() ; )
+      {
+         Object value = itr.next();
+         if(value instanceof ConfigElement)
+         {
+            ConfigElement station_elm = (ConfigElement)value;
+            System.out.println("Embedded Element: " + station_elm.getName());
+            mStationElements.add(station_elm);
+            
+            // Create a new StationModel for each embedded element.
+            StationModel station = new StationModel(elm, station_elm, mISenseConfigContext);
+            mStationModels.add(station);
+
+            // Make this StationModel listen to us for events.
+            this.addIntersenseModelListener(station.getProxyModel());
+            
+         }
+      }
+
+      // We must listen to all proxy elements to ensure that we are informed if
+      // they start pointing at this device.
+      List all_elms = getBroker().getElementsIncludingEmbedded(mISenseConfigContext);
+      for (Iterator itr = all_elms.iterator(); itr.hasNext(); )
+      {
+         ConfigElement elt = (ConfigElement)itr.next();
+         String token = elt.getDefinition().getToken();
+         if (token.equals("digital_proxy") || token.equals("analog_proxy") || token.equals("position_proxy"))
+         {
+            elt.addConfigElementListener(this);
+         }
+      }
+   }
+
+   /**
+    * ConfigElementListener interface to catch all changes in existing proxies by
+    * calling a helper method, elementChanged.
+    *
+    * @see ConfigElementListener
+    * @see #elementChanged
+    */
+   public void nameChanged(ConfigElementEvent evt)
+   {
+      elementChanged(evt);
+   }
+   public void propertyValueChanged(ConfigElementEvent evt)
+   {
+      elementChanged(evt);
+   }
+   public void propertyValueAdded(ConfigElementEvent evt)
+   {
+      elementChanged(evt);
+   }
+   public void propertyValueRemoved(ConfigElementEvent evt)
+   {
+      elementChanged(evt);
+   }
+
+   /**
+    * ConfigListener interface to catch all added ConfigElements.
+    * 
+    * @see ConfigListener
+    */
+   public void configElementAdded(ConfigEvent evt)
+   {
+      // - If the element is a proxy.
+      //   - If new new element points at us we must inform all listeners.
+      //   - We must start listening to see if it every starts pointing at us.
+      ConfigElement elm = evt.getElement();
+      if(elm.getDefinition().getParents().contains("proxy"))
+      {
+         // We must start listening to this proxy now.
+         elm.addConfigElementListener(this);
+         
+         String device_name = ((ConfigElementPointer)elm.getProperty("device", 0)).getTarget();
+         String our_name = mISenseConfigElement.getName();
+         // If the proxy pointed at us inform all listeners.
+         if(device_name.equals(our_name))
+         {
+            String token = elm.getDefinition().getToken();
+            int unit = ((Integer)elm.getProperty("unit", 0)).intValue();
+            fireProxyAdded(elm, token, unit);
+         }
+      }
+   }
+   
+   /**
+    * ConfigListener interface to catch removed added ConfigElements.
+    * 
+    * @see ConfigListener
+    */
+   public void configElementRemoved(ConfigEvent evt)
+   {
+      // - If element is a proxy
+      //   - If it pointed at us inform all listeners.
+      ConfigElement elm = evt.getElement();
+      if(elm.getDefinition().getParents().contains("proxy"))
+      {
+         String device_name = ((ConfigElementPointer)elm.getProperty("device", 0)).getTarget();
+         String our_name = mISenseConfigElement.getName();
+         // If the proxy pointed at us inform all listeners.
+         if(device_name.equals(our_name))
+         {
+            String token = elm.getDefinition().getToken();
+            int unit = ((Integer)elm.getProperty("unit", 0)).intValue();
+            fireProxyRemoved(elm, token, unit);
+         }
+      }
+   }
+
+   /**
+    * Helper function that is used to process all changes to existing proxies.
+    */
+   private void elementChanged(ConfigElementEvent evt)
+   { 
+      // - If element is a proxy
+      //   - Remove it from the tree.
+      //     - If device and unit match add it again.
+      ConfigElement elm = (ConfigElement)evt.getSource();
+      String token = elm.getDefinition().getToken();
+      if(token.equals("position_proxy") ||
+         token.equals("digital_proxy") ||
+         token.equals("analog_proxy"))
+      {
+         String our_name = mISenseConfigElement.getName();
+         
+         if(evt.getProperty() == null)
+         {
+            return;
+         }
+         
+         ConfigElementPointer ep = (ConfigElementPointer)elm.getProperty("device", 0);
+         String device = ep.getTarget();
+
+         int unit = ((Integer)elm.getProperty("unit", 0)).intValue();
+
+         // Remove the node if it exists.
+         fireProxyRemoved(elm, elm.getDefinition().getToken(), unit);
+
+         // Add the node if it points at this device.
+         if(device.equals(our_name))
+         {
+            fireProxyAdded(elm, elm.getDefinition().getToken(), unit);
+         }
+      }
+   }
+
+
+   /**
+    * Adds the given listener to be notified when this Intersense model changes.
+    */
+   public void addIntersenseModelListener(IntersenseModelListener listener)
+   {
+      listenerList.add(IntersenseModelListener.class, listener);
+   }
+
+   /**
+    * Removes the given listener that was registered to be notified when this
+    * Intersense model changes.
+    */
+   public void removeIntersenseModelListener(IntersenseModelListener listener)
+   {
+      listenerList.remove(IntersenseModelListener.class, listener);
+   }
+
+   /**
+    * Notifies listeners that a new element has been created that points at us.
+    */
+   protected void fireProxyAdded(ConfigElement elm, String token, int unit)
+   {
+      IntersenseModelEvent evt = null;
+      Object[] listeners = listenerList.getListenerList();
+      for (int i=listeners.length-2; i>=0; i-=2)
+      {
+         if (listeners[i] == IntersenseModelListener.class)
+         {
+            if (evt == null)
+            {
+               evt = new IntersenseModelEvent(this, elm, token, unit);
+            }
+            ((IntersenseModelListener)listeners[i+1]).proxyAdded(evt);
+         }
+      }
+   }
+   
+   /**
+    * Notifies listeners that an element that points at us is being removed.
+    */
+   protected void fireProxyRemoved(ConfigElement elm, String token, int unit)
+   {
+      IntersenseModelEvent evt = null;
+      Object[] listeners = listenerList.getListenerList();
+      for (int i=listeners.length-2; i>=0; i-=2)
+      {
+         if (listeners[i] == IntersenseModelListener.class)
+         {
+            if (evt == null)
+            {
+               evt = new IntersenseModelEvent(this, elm, token, unit);
+            }
+            ((IntersenseModelListener)listeners[i+1]).proxyRemoved(evt);
+         }
+      }
+   }
+
+   /** Reference to the ConfigBroker used in this object. */
+   private ConfigBroker mBroker = null;
+   
+   /**
+    * Gets a handle to the configuration broker.
+    */
+   private ConfigBroker getBroker()
+   {
+      if (mBroker == null)
+      {
+         synchronized (this)
+         {
+            if (mBroker == null)
+            {
+               mBroker = new ConfigBrokerProxy();
+            }
+         }
+      }
+      return mBroker;
+   }
+}
