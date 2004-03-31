3fa9dd0f153c7572f97bc896339a4b0c40ce2137
Patrick Hartling <patrick.hartling@gmail.com>
2004-03-31 20:22:08 +0000

    Tell the JVM about the environment variable JCCL_DEFINITION_PATH.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14566 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/vrjconfig.bat
+++ modules/vrjuggler/vrjconfig/vrjconfig.bat
@@ -50,7 +50,7 @@ ECHO [ERR] The TWEEK_BASE_DIR %TWEEK_BASE_DIR does not appear to exist.  Please
 ECHO [ERR] check the path and try again.
 GOTO ERREXIT
 :RUNJAVA
-java -DTWEEK_BASE_DIR=%TWEEK_BASE_DIR% -DVJ_BASE_DIR=%VJ_BASE_DIR% -Djava.security.policy=%TWEEK_BASE_DIR%/bin/java.security.policy.txt -cp %TWEEK_BASE_DIR%/bin/jdom.jar;%TWEEK_BASE_DIR%/bin/xerces.jar;%TWEEK_BASE_DIR%/bin/Tweek.jar;%TWEEK_BASE_DIR%/bin/TweekBeans.jar;%TWEEK_BASE_DIR%/bin/TweekEvents.jar;%TWEEK_BASE_DIR%/bin/TweekNet.jar;%TWEEK_BASE_DIR%/bin/TweekBeanDelivery.jar;%TWEEK_BASE_DIR%/bin/TweekServices.jar;%TWEEK_BASE_DIR%/bin/kunststoff-mod.jar;%TWEEK_BASE_DIR%/bin/metouia.jar org.vrjuggler.tweek.Tweek --defaultbean=VRJConfig %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -DTWEEK_BASE_DIR=%TWEEK_BASE_DIR% -DVJ_BASE_DIR=%VJ_BASE_DIR% -DJCCL_DEFINITION_PATH=%JCCL_DEFINITION_PATH% -Djava.security.policy=%TWEEK_BASE_DIR%/bin/java.security.policy.txt -cp %TWEEK_BASE_DIR%/bin/jdom.jar;%TWEEK_BASE_DIR%/bin/xerces.jar;%TWEEK_BASE_DIR%/bin/Tweek.jar;%TWEEK_BASE_DIR%/bin/TweekBeans.jar;%TWEEK_BASE_DIR%/bin/TweekEvents.jar;%TWEEK_BASE_DIR%/bin/TweekNet.jar;%TWEEK_BASE_DIR%/bin/TweekBeanDelivery.jar;%TWEEK_BASE_DIR%/bin/TweekServices.jar;%TWEEK_BASE_DIR%/bin/kunststoff-mod.jar;%TWEEK_BASE_DIR%/bin/metouia.jar org.vrjuggler.tweek.Tweek --defaultbean=VRJConfig %1 %2 %3 %4 %5 %6 %7 %8 %9
 EXIT
 :ERREXIT
 ECHO [ERR] VRJConfig exiting due to previous errors.
