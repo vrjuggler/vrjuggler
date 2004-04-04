916b144c7e43240d226041c0bc61cab8df33473d
Patrick Hartling <patrick.hartling@gmail.com>
2004-04-04 17:27:56 +0000

    Take steps to elimiate more compiler warnings about redefined symbols that Autoconf adds (which we neither want nor care about).
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14601 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/vprConfig.h
+++ modules/vapor/vpr/vprConfig.h
@@ -51,10 +51,20 @@
  * ----------------------------------------------------------------------------
  */
 
+/*
+ * Protect against redefinition of symbols added by Autoconf 2.5x in
+ * vprDefines.h.
+ */
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_VERSION
+
 /* This should always be included first. */
 #include <vpr/vprDefines.h>
 
-/* Get rid of symbols added by Autoconf 2.5x. */
+/* Get rid of symbols added by Autoconf 2.5x in vprDefines.h. */
 #undef PACKAGE_BUGREPORT
 #undef PACKAGE_NAME
 #undef PACKAGE_STRING
