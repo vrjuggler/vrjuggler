69e54fad5dd1528d140924a7b9668b20fbaf5f85
Patrick Hartling <patrick.hartling@gmail.com>
2004-04-05 20:12:49 +0000

    Updated for the jccl::ConfigManager syntactic and semantic changes.  Some of the unit tests now do more than I would like, but it is unavoidable if the Config Manager's pending list is not directly accessible (which I think is a good thing).  For the time being, the RemovePending() method has been commented out because it's too complicated to update the test for jccl::ConfigManager::removePending() at this stage.  Ultimately, I suspect that that method is going to be removed, so I am really unmotivated to update the test case for it right now.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@14620 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/test/TestSuite/TestCases/ConfigManagerTest.cpp
+++ juggler/modules/jackal/test/TestSuite/TestCases/ConfigManagerTest.cpp
@@ -74,96 +74,41 @@ void ConfigManagerTest::tearDown()
    vpr::Debug::instance()->enableOutput();
 }
 
-void ConfigManagerTest::AddPendingAdds()
-{
-   bool status;
-
-   jccl::Configuration cfg;
-   status = cfg.load(sFilePath + "cfg/ConfigManagerTest1.jconf");
-   CPPUNIT_ASSERT(status);
-
-   jccl::ConfigManager::instance()->addPendingAdds(&cfg);
-   CPPUNIT_ASSERT(! jccl::ConfigManager::instance()->isPendingStale());
-
-   std::list<jccl::ConfigManager::PendingElement>::iterator i;
-   std::list<jccl::ConfigManager::PendingElement>::iterator pending_end;
-
-   jccl::ConfigManager::instance()->lockPending();
-   {
-      i           = jccl::ConfigManager::instance()->getPendingBegin();
-      pending_end = jccl::ConfigManager::instance()->getPendingEnd();
-      CPPUNIT_ASSERT(i != pending_end);
-   }
-   jccl::ConfigManager::instance()->unlockPending();
-
-   status = jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_1");
-   CPPUNIT_ASSERT(status);
-
-   status = jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_bogus");
-   CPPUNIT_ASSERT(! status);
-
-   status = jccl::ConfigManager::instance()->isElementTypeInActiveList("element_type_1");
-   CPPUNIT_ASSERT(! status);
-}
-
-void ConfigManagerTest::AddPendingRemoves()
-{
-   bool status;
-
-   jccl::Configuration cfg;
-   status = cfg.load(sFilePath + "cfg/ConfigManagerTest1.jconf");
-   CPPUNIT_ASSERT(status);
-
-   jccl::ConfigManager::instance()->addPendingRemoves(&cfg);
-   int num = jccl::ConfigManager::instance()->getNumPending();
-   CPPUNIT_ASSERT(num == 3);
-   CPPUNIT_ASSERT(! jccl::ConfigManager::instance()->isPendingStale());
-
-   jccl::ConfigManager::instance()->lockPending();
-   {
-      std::list<jccl::ConfigManager::PendingElement>::iterator i =
-         jccl::ConfigManager::instance()->getPendingBegin();
-      std::list<jccl::ConfigManager::PendingElement>::iterator pending_end =
-         jccl::ConfigManager::instance()->getPendingEnd();
-      CPPUNIT_ASSERT(i != pending_end);
-   }
-   jccl::ConfigManager::instance()->unlockPending();
-
-   status = jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_1");
-   CPPUNIT_ASSERT(status);
-
-   status = jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_bogus");
-   CPPUNIT_ASSERT(! status);
-
-   status = jccl::ConfigManager::instance()->isElementTypeInActiveList("element_type_1");
-   CPPUNIT_ASSERT(! status);
-}
-
+// This tests the following methods in jccl::ConfigManager:
+//    - addConfigElementHandler()
+//    - addConfigurationAdditions()
+//    - attemptReconfiguration()
+//    - removeConfigurationAdditions()
 void ConfigManagerTest::AddConfigElementHandler()
 {
+   // Load a configuration file that has only one element and no
+   // dependencies on other elements.
    jccl::Configuration cfg;
    bool status = cfg.load(sFilePath + "cfg/ConfigManagerTest2.jconf");
    CPPUNIT_ASSERT(status);
 
    ElementType2Handler e2_handler;
    jccl::ConfigManager::instance()->addConfigElementHandler(&e2_handler);
-   jccl::ConfigManager::instance()->addPendingAdds(&cfg);
+   jccl::ConfigManager::instance()->addConfigurationAdditions(&cfg);
    int num = jccl::ConfigManager::instance()->getNumPending();
+   CPPUNIT_ASSERT(num == 0);
+   num = jccl::ConfigManager::instance()->getNumIncoming();
    CPPUNIT_ASSERT(num == 1);
-
-   status =
-      jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_2");
-   CPPUNIT_ASSERT(status);
 
    status = jccl::ConfigManager::instance()->isActiveEmpty();
    CPPUNIT_ASSERT(status);
 
+   int count;
+
    jccl::ConfigManager::instance()->lockPending();
    {
-      int count = e2_handler.configProcessPending();
-      CPPUNIT_ASSERT(count == 1);
+      count = e2_handler.configProcessPending();
+      CPPUNIT_ASSERT(count == 0);
    }
    jccl::ConfigManager::instance()->unlockPending();
+
+   count = jccl::ConfigManager::instance()->attemptReconfiguration();
+   CPPUNIT_ASSERT(count == 1);
 
    status = jccl::ConfigManager::instance()->isActiveEmpty();
    CPPUNIT_ASSERT(! status);
@@ -172,63 +117,137 @@ void ConfigManagerTest::AddConfigElementHandler()
       jccl::ConfigManager::instance()->isElementInActiveList("External Element");
    CPPUNIT_ASSERT(status);
 
-   status =
-      jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_2");
+   jccl::ConfigManager::instance()->removeConfigElementHandler(&e2_handler);
+}
+
+void ConfigManagerTest::AddConfigurationRemovals()
+{
+   bool status;
+
+   jccl::Configuration cfg;
+   status = cfg.load(sFilePath + "cfg/ConfigManagerTest2.jconf");
+   CPPUNIT_ASSERT(status);
+
+   ElementType2Handler e2_handler;
+   jccl::ConfigManager::instance()->addConfigElementHandler(&e2_handler);
+   jccl::ConfigManager::instance()->addConfigurationRemovals(&cfg);
+   int num = jccl::ConfigManager::instance()->getNumPending();
+   CPPUNIT_ASSERT(num == 0);
+   num = jccl::ConfigManager::instance()->getNumIncoming();
+   CPPUNIT_ASSERT(num == 1);
+   CPPUNIT_ASSERT(! jccl::ConfigManager::instance()->isPendingStale());
+
+   num = jccl::ConfigManager::instance()->attemptReconfiguration();
+   CPPUNIT_ASSERT(num == 1);
+
+   status = jccl::ConfigManager::instance()->isElementTypeInActiveList("element_type_1");
    CPPUNIT_ASSERT(! status);
 
    jccl::ConfigManager::instance()->removeConfigElementHandler(&e2_handler);
 }
 
-void ConfigManagerTest::RemovePending()
+void ConfigManagerTest::GetElementNamed()
 {
+   // Load a configuration file that has only one element and no
+   // dependencies on other elements.
    jccl::Configuration cfg;
    bool status = cfg.load(sFilePath + "cfg/ConfigManagerTest2.jconf");
    CPPUNIT_ASSERT(status);
 
-   jccl::ConfigManager::instance()->addPendingAdds(&cfg);
+   ElementType2Handler e2_handler;
+   jccl::ConfigManager::instance()->addConfigElementHandler(&e2_handler);
+   jccl::ConfigManager::instance()->addConfigurationAdditions(&cfg);
    int num = jccl::ConfigManager::instance()->getNumPending();
+   CPPUNIT_ASSERT(num == 0);
+   num = jccl::ConfigManager::instance()->getNumIncoming();
    CPPUNIT_ASSERT(num == 1);
 
-   jccl::ConfigManager::instance()->lockPending();
-   {
-      std::list<jccl::ConfigManager::PendingElement>::iterator i =
-         jccl::ConfigManager::instance()->getPendingBegin();
-      std::list<jccl::ConfigManager::PendingElement>::iterator pending_end =
-         jccl::ConfigManager::instance()->getPendingEnd();
+   status = jccl::ConfigManager::instance()->isActiveEmpty();
+   CPPUNIT_ASSERT(status);
 
-      for ( ; i != pending_end; )
-      {
-         std::list<jccl::ConfigManager::PendingElement>::iterator j = i;
-         i++;
-         jccl::ConfigManager::instance()->removePending(j);
-      }
-   }
-   jccl::ConfigManager::instance()->unlockPending();
+   int count = jccl::ConfigManager::instance()->attemptReconfiguration();
+   CPPUNIT_ASSERT(count == 1);
 
-   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 0);
+   status = jccl::ConfigManager::instance()->isActiveEmpty();
+   CPPUNIT_ASSERT(! status);
 
-   jccl::ConfigManager::instance()->addPendingRemoves(&cfg);
-   num = jccl::ConfigManager::instance()->getNumPending();
-   CPPUNIT_ASSERT(num == 1);
+   const std::string element_name("External Element");
+   status =
+      jccl::ConfigManager::instance()->isElementInActiveList(element_name);
+   CPPUNIT_ASSERT(status);
 
-   jccl::ConfigManager::instance()->lockPending();
-   {
-      std::list<jccl::ConfigManager::PendingElement>::iterator i =
-         jccl::ConfigManager::instance()->getPendingBegin();
-      std::list<jccl::ConfigManager::PendingElement>::iterator pending_end =
-         jccl::ConfigManager::instance()->getPendingEnd();
+   jccl::ConfigElementPtr mgr_elt =
+      jccl::ConfigManager::instance()->getElementNamed(element_name);
+   CPPUNIT_ASSERT(mgr_elt.get() != NULL);
 
-      for ( ; i != pending_end; )
-      {
-         std::list<jccl::ConfigManager::PendingElement>::iterator j = i;
-         i++;
-         jccl::ConfigManager::instance()->removePending(j);
-      }
-   }
-   jccl::ConfigManager::instance()->unlockPending();
+   jccl::ConfigElementPtr cfg_elt = cfg.get(element_name);
+   CPPUNIT_ASSERT(mgr_elt == cfg_elt);
 
-   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 0);
+   jccl::ConfigManager::instance()->removeConfigElementHandler(&e2_handler);
 }
+
+/*
+void ConfigManagerTest::RemovePending()
+{
+   jccl::Configuration cfg;
+   bool status = cfg.load(sFilePath + "cfg/ConfigManagerTest1.jconf");
+   CPPUNIT_ASSERT(status);
+
+   ElementType1Handler e1_handler;
+   ElementType2Handler e2_handler;
+   jccl::ConfigManager::instance()->addConfigElementHandler(&e1_handler);
+   jccl::ConfigManager::instance()->addConfigElementHandler(&e2_handler);
+   jccl::ConfigManager::instance()->addConfigurationAdditions(&cfg);
+   int num = jccl::ConfigManager::instance()->getNumPending();
+   CPPUNIT_ASSERT(num == 0);
+
+   num = jccl::ConfigManager::instance()->attemptReconfiguration();
+   CPPUNIT_ASSERT(num == 1);
+
+   jccl::ConfigManager::instance()->lockPending();
+   {
+      std::list<jccl::ConfigManager::PendingElement>::iterator i =
+         jccl::ConfigManager::instance()->getPendingBegin();
+      std::list<jccl::ConfigManager::PendingElement>::iterator pending_end =
+         jccl::ConfigManager::instance()->getPendingEnd();
+
+      for ( ; i != pending_end; )
+      {
+         std::list<jccl::ConfigManager::PendingElement>::iterator j = i;
+         i++;
+         jccl::ConfigManager::instance()->removePending(j);
+      }
+   }
+   jccl::ConfigManager::instance()->unlockPending();
+
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 0);
+
+   jccl::ConfigManager::instance()->addConfigurationRemovals(&cfg);
+   num = jccl::ConfigManager::instance()->getNumPending();
+   CPPUNIT_ASSERT(num == 0);
+
+   jccl::ConfigManager::instance()->lockPending();
+   {
+      std::list<jccl::ConfigManager::PendingElement>::iterator i =
+         jccl::ConfigManager::instance()->getPendingBegin();
+      std::list<jccl::ConfigManager::PendingElement>::iterator pending_end =
+         jccl::ConfigManager::instance()->getPendingEnd();
+
+      for ( ; i != pending_end; )
+      {
+         std::list<jccl::ConfigManager::PendingElement>::iterator j = i;
+         i++;
+         jccl::ConfigManager::instance()->removePending(j);
+      }
+   }
+   jccl::ConfigManager::instance()->unlockPending();
+
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 0);
+
+   jccl::ConfigManager::instance()->removeConfigElementHandler(&e1_handler);
+   jccl::ConfigManager::instance()->removeConfigElementHandler(&e2_handler);
+}
+*/
 
 void ConfigManagerTest::AttemptReconfiguration()
 {
@@ -240,6 +259,7 @@ void ConfigManagerTest::AttemptReconfiguration()
 
    CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isActiveEmpty());
    CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 0);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumIncoming() == 0);
 
    jccl::ConfigManager::instance()->addConfigElementHandler(&e1_handler);
 
@@ -247,14 +267,25 @@ void ConfigManagerTest::AttemptReconfiguration()
    status = cfg1.load(sFilePath + "cfg/ConfigManagerTest1.jconf");
    CPPUNIT_ASSERT(status);
 
-   jccl::ConfigManager::instance()->addPendingAdds(&cfg1);
-   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 3);
+   // This adds two elements of type element_type_1 and one element of type
+   // element_type_2.
+   jccl::ConfigManager::instance()->addConfigurationAdditions(&cfg1);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isActiveEmpty());
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 0);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumIncoming() == 3);
 
-   // Nothing will be processed this first time because there are no handlers
-   // for element_type_2, and the only element_type_1 instance depends on an
-   // unloaded element_type_2 instance.
+   // Make the first pass at reconfiguration.  This will not put anything
+   // into the active list because there is no handler for element_type_2,
+   // and the element_type_1 elements will have unmet dependencies.
    processed = jccl::ConfigManager::instance()->attemptReconfiguration();
    CPPUNIT_ASSERT(processed == 0);
+
+   // There will still be three elements pending: one of element_type_2 (no
+   // handler registered yet) and two of element_type_1 (missing
+   // dependencies).
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 3);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumIncoming() == 0);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isActiveEmpty());
 
    // Now, load the second configuration file that will allow dependency
    // resolutoin of the pending element_type_1 instance.
@@ -265,8 +296,9 @@ void ConfigManagerTest::AttemptReconfiguration()
    jccl::ConfigManager::instance()->addConfigElementHandler(&e2_handler);
 
    // Hand the second configuration off to the Config Manager.
-   jccl::ConfigManager::instance()->addPendingAdds(&cfg2);
-   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 4);
+   jccl::ConfigManager::instance()->addConfigurationAdditions(&cfg2);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumIncoming() == 1);
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 3);
 
    // Make the next reconfiguration pass.  This will add the elements named
    // "Element2" and "External Element" to the active list.
@@ -274,15 +306,16 @@ void ConfigManagerTest::AttemptReconfiguration()
    CPPUNIT_ASSERT(processed == 2);
 
    // At this point, all of the following must be true:
-   //    - There are still two elements in the pending list.
-   //    - Both elements are of type element_type_1.
+   //    - The incoming list is empty.
+   //    - There are still two elements in the pending list, both of type
+   //      element_type_1.
    //    - The active list is no longer empty.
    //    - The active list contains an element of type element_type_2 named
    //      "External Element" and another named "Element2".
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumIncoming() == 0);
    CPPUNIT_ASSERT(jccl::ConfigManager::instance()->getNumPending() == 2);
-   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_1"));
-   CPPUNIT_ASSERT(! jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_2"));
    CPPUNIT_ASSERT(! jccl::ConfigManager::instance()->isActiveEmpty());
+   CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isElementTypeInPendingList("element_type_1"));
    CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isElementTypeInActiveList("element_type_2"));
    CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isElementInActiveList("External Element"));
    CPPUNIT_ASSERT(jccl::ConfigManager::instance()->isElementInActiveList("Element2"));
