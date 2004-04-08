52308c832b70553e51cbf019e31ad75180569955
Patrick Hartling <patrick.hartling@gmail.com>
2004-04-08 06:32:12 +0000

    Do not call EXIT to exit this batch file.  That exits the shell, and that can be annoying.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14634 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/tweek/java/tweek.bat
+++ modules/tweek/java/tweek.bat
@@ -45,6 +45,7 @@ GOTO ERREXIT
 set TWEEK_CLASSPATH=%TWEEK_BASE_DIR%/bin/jdom.jar;%TWEEK_BASE_DIR%/bin/xerces.jar;%TWEEK_BASE_DIR%/bin/Tweek.jar;%TWEEK_BASE_DIR%/bin/TweekBeans.jar;%TWEEK_BASE_DIR%/bin/TweekEvents.jar;%TWEEK_BASE_DIR%/bin/TweekNet.jar;%TWEEK_BASE_DIR%/bin/TweekBeanDelivery.jar;%TWEEK_BASE_DIR%/bin/TweekServices.jar;%TWEEK_BASE_DIR%/bin/kunststoff-mod.jar;%TWEEK_BASE_DIR%/bin/metouia.jar;%TWEEK_BASE_DIR%/bin/looks.jar
 
 java -DTWEEK_BASE_DIR=%TWEEK_BASE_DIR% -Djava.security.policy=%TWEEK_BASE_DIR%/bin/java.security.policy.txt -cp %TWEEK_CLASSPATH% org.vrjuggler.tweek.Tweek %1 %2 %3 %4 %5 %6 %7 %8 %9
-EXIT
+GOTO DONE
 :ERREXIT
 ECHO [ERR] Tweek exiting due to previous errors.
+:DONE
