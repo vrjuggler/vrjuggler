dc12afcd0c39c51552c124545d03431090b1da14
Daniel Shipton <daniel.shipton.oss@gmail.com>
2004-04-12 20:53:39 +0000

    Added sample history
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14710 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/Perf/ProfileNode.cpp
+++ modules/vapor/vpr/Perf/ProfileNode.cpp
@@ -5,7 +5,6 @@
 namespace vpr
 {
 
-
 ProfileNode::ProfileNode( const char * name, ProfileNode * parent ) :
 	mName( name ),
 	mTotalCalls( 0 ),
@@ -15,17 +14,32 @@ ProfileNode::ProfileNode( const char * name, ProfileNode * parent ) :
 	mChild( NULL ),
 	mSibling( NULL )
 {
+   mHistorySize = 1;
+   mHistory.resize(mHistorySize);
    mStartTime.secf(0);
 	reset();
 }
 
+ProfileNode::ProfileNode( const char * name, ProfileNode * parent, const unsigned int queue_size) :
+	mName( name ),
+	mTotalCalls( 0 ),
+	mTotalTime( 0 ),
+	mRecursionCounter( 0 ),
+	mParent( parent ),
+	mChild( NULL ),
+	mSibling( NULL )
+{
+   mHistorySize = queue_size;
+   mHistory.resize(mHistorySize);
+   mStartTime.secf(0);
+	reset();
+}
 
 ProfileNode::~ProfileNode( void )
 {
 	delete mChild;
 	delete mSibling;
 }
-
 
 ProfileNode* ProfileNode::getSubNode( const char* name )
 {
@@ -45,6 +59,42 @@ ProfileNode* ProfileNode::getSubNode( const char* name )
 	return node;
 }
 
+ProfileNode* ProfileNode::getSubNode( const char* name, const unsigned int queue_size )
+{
+	// Try to find this sub node
+	ProfileNode* child = mChild;
+	while ( child ) {
+		if ( child->mName == name ) {
+			return child;
+		}
+		child = child->mSibling;
+	}
+
+	// We didn't find it, so add it
+	ProfileNode* node = new ProfileNode( name, this, queue_size);
+	node->mSibling = mChild;
+	mChild = node;
+	return node;
+}
+
+void ProfileNode::printTree(ProfileNode* node)
+{
+   if(node == NULL) return;
+
+   std::cout << getName() << " total calls: " << getTotalCalls()
+          << " total time: " << getTotalTime() << " ave: " 
+          << getTotalCalls() /getTotalTime() << " history: ";
+
+   NodeHistoryRange p = getNodeHistoryRange();
+      for(; p.first != p.second; p.first++)
+         { std::cout << *(p.first) << " "; }
+
+      std::cout << std::endl;
+
+   printTree(node->getChild());
+   printTree(node->getSibling());
+}
+   
 
 void	ProfileNode::reset( void )
 {
@@ -59,7 +109,6 @@ void	ProfileNode::reset( void )
 	}
 }
 
-
 void	ProfileNode::call( void )
 {
 	mTotalCalls++;
@@ -68,20 +117,18 @@ void	ProfileNode::call( void )
 	}
 }
 
-
 bool	ProfileNode::Return( void )
 {
 	if ( --mRecursionCounter == 0 && mTotalCalls != 0 ) { 
       vpr::Interval time;
 		profileGetTicks(&time);
 		time-=mStartTime;
-		mTotalTime += (float)(time.secf() / profileGetTickRate().secf());
+      mHistory.push_front((float)(time.secf() / profileGetTickRate().secf()));
+      mHistory.resize(mHistorySize);
+		mTotalTime += mHistory.front();
 	}
 	return ( mRecursionCounter == 0 );
 }
-
-
-
 
 ProfileNode	   ProfileManager::mRoot( "Root", NULL );
 ProfileNode*	ProfileManager::mCurrentNode = &ProfileManager::mRoot;
