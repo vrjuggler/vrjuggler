589398be6549760f439461d96aa756d730e638c1
Patrick Hartling <patrick.hartling@gmail.com>
2004-04-20 22:43:25 +0000

    Fix coding standard violations.  Changed the return value from main() to be 0 since that is the usual value to be returned on successful completion.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14786 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/OpenSG/simple/OpenSGNav/app.cpp
+++ modules/vrjuggler/samples/OpenSG/simple/OpenSGNav/app.cpp
@@ -35,25 +35,28 @@
 
 int main(int argc, char* argv[])
 {
-    vrj::Kernel* kernel = vrj::Kernel::instance();
-    OpenSGNav* application = new OpenSGNav(kernel);
+   vrj::Kernel* kernel = vrj::Kernel::instance();
+   OpenSGNav* application = new OpenSGNav(kernel);
 
-    if (argc <= 2)
-    {
-      std::cout << "\n\nUsage: " << argv[0] << " modelname vjconfigfile[0] vjconfigfile[1] ... vjconfigfile[n]\n" << std::flush;
+   if (argc <= 2)
+   {
+      std::cout << "\n\nUsage: " << argv[0]
+                << " modelname vjconfigfile[0] vjconfigfile[1] ... vjconfigfile[n]\n"
+                << std::flush;
       exit(1);
-    }
+   }
 
-    application->setModelFileName(std::string(argv[1]));
+   application->setModelFileName(std::string(argv[1]));
 
-    // Load any config files specified on the command line
-    for(int i=2;i<argc;i++)
-        kernel->loadConfigFile(argv[i]);
+   // Load any config files specified on the command line
+   for ( int i = 2; i < argc; ++i )
+   {
+      kernel->loadConfigFile(argv[i]);
+   }
     
-    kernel->start();
-    kernel->setApplication(application);
-    kernel->waitForKernelStop();
+   kernel->start();
+   kernel->setApplication(application);
+   kernel->waitForKernelStop();
 
-    return 1;
+   return 0;
 }
-
