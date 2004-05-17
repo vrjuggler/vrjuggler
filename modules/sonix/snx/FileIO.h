86f5def3e859e4665446114f1481695f36cc24e9
Patrick Hartling <patrick.hartling@gmail.com>
2004-05-17 20:43:51 +0000

    Attempt to decrease the sloppiness of this file.  Changed comments to Doxygen style.  Export a few more helper functions in the Sonix DLL on Windows.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14970 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/sonix/snx/FileIO.h
+++ modules/sonix/snx/FileIO.h
@@ -38,10 +38,6 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
-
-
-
-
 //////////////////////////////////////////////////////////////////////////////
 //
 //                         -=     CFileIO class     =-
@@ -70,46 +66,48 @@
 //
 ///////////////// <auto-copyright END do not edit this line> ///////////////////
 
-//////////////////////////////////////////////////////////////////////////////
-#ifndef CFILEIO_INCLUDED
-#define CFILEIO_INCLUDED
+#ifndef _SNX_FILE_IO_H_
+#define _SNX_FILE_IO_H_
 
 #include <snx/snxConfig.h>
 
+#include <stdio.h>
 #include <string>
 #include <vector>
 #include <fstream>
-#include <stdio.h> // for FILE
 
 #include <snx/Endian.h>
 
+
 namespace snxFileIO
 {
-   //: true - 
-   SNX_API(bool) fileExists( const char* const name );
+   SNX_API(bool) fileExists(const char* name);
 
-   int fileSize( const char* const filename );
+   SNX_API(int) fileSize(const char* filename);
 
-   void fileLoad( const char* const filename, std::vector<unsigned char>& data );
+   SNX_API(void) fileLoad(const char* filename,
+                          std::vector<unsigned char>& data);
 
    enum Endianness
    {
       LITTLE, BIG
    };
 
-   //: Read
-   // this function is designed to work with little endian files, such as TGA and BMP.
-   // this will return data that is correctly translated for your system architecture.
-   // i.e. reading a little endian BMP on a big endian mips system, 
-   //      returns (translated) bigendian data
-   // i.e. reading a little endian TGA on a little endian intel system, 
-   //      returns (untranslated) littleendian data
+   /**
+    * Read.  This function is designed to work with little endian files, such
+    * as TGA and BMP.  This will return data that is correctly translated for
+    * your system architecture.  In other words, reading a little endian BMP
+    * on a big endian mips system returns (translated) bigendian data.  For
+    * example, reading a little endian TGA on a little endian intel system,
+    * returns (untranslated) littleendian data.
+    */
    template<class typeT>
-   inline int ReadData( Endianness fileByteOrdering, FILE* fp, typeT& data )
+   inline int ReadData(Endianness fileByteOrdering, FILE* fp, typeT& data)
    {
       int size = ::fread( &data, sizeof(typeT), 1, fp );
 
-      // if we're not on a little endian machine (intel is little endian) then reverse the bytes.
+      // if we're not on a little endian machine (intel is little endian) then
+      // reverse the bytes.
       if (fileByteOrdering == snxFileIO::LITTLE && vpr::System::isBigEndian() ||
          fileByteOrdering == snxFileIO::BIG && vpr::System::isLittleEndian())
       {
@@ -119,47 +117,64 @@ namespace snxFileIO
       return size;
    }
 
-   //: Write
-   // this function is designed to work with little endian files, such as TGA and BMP.
+   /**
+    * Write.  This function is designed to work with little endian files,
+    * such as TGA and BMP.
+    */
    template<class typeT>
-   inline int WriteData( Endianness fileByteOrdering, FILE* fp, const typeT& data )
+   inline int WriteData(Endianness fileByteOrdering, FILE* fp,
+                        const typeT& data)
    {
       typeT tempData = data;
 
-      // if we're not on a little endian machine (i.e. intel is little endian, mips is big) 
-      // then reverse the bytes.
-      if (fileByteOrdering == LITTLE && vpr::System::isBigEndian() ||
-         fileByteOrdering == BIG && vpr::System::isLittleEndian())
+      // if we're not on a little endian machine (i.e. intel is little endian,
+      // mips is big), then reverse the bytes.
+      if ( fileByteOrdering == LITTLE && vpr::System::isBigEndian() ||
+           fileByteOrdering == BIG && vpr::System::isLittleEndian() )
       {
          snxEndian::byteReverse( tempData );
       }
-         
+
       int size = ::fwrite( &tempData, 1, sizeof(typeT), fp );
 
       return size;
    }
 
-   // Read one byte of data from the file stream
-   int ReadByte( FILE *fp, unsigned char& value );
+   /** Reads one byte of data from the file stream. */
+   int ReadByte(FILE *fp, unsigned char& value);
 
-   // Read one short of data from the file stream
-   // NOTE: these enhance readability and type checking, try to use them over the generic ReadData function
-   int ReadShort( Endianness fileByteOrdering, FILE* fp, unsigned short& value );
+   /**
+    * Reads one short of data from the file stream.
+    * @note These enhance readability and type checking; try to use them over
+    *       the generic ReadData function.
+    */
+   int ReadShort(Endianness fileByteOrdering, FILE* fp, unsigned short& value);
 
-   // Read one long of data from the file stream
-   // NOTE: these enhance readability and type checking, try to use them over the generic ReadData function
-   int ReadLong( Endianness fileByteOrdering, FILE *fp, unsigned long& value );
+   /**
+    * Reads one long of data from the file stream.
+    * @note These enhance readability and type checking; try to use them over
+    *       the generic ReadData function.
+    */
+   int ReadLong(Endianness fileByteOrdering, FILE *fp, unsigned long& value);
 
-   // Write one byte of data to the file stream
-   int WriteByte( FILE *fp, const unsigned char& value );
+   /** Writes one byte of data to the file stream. */
+   int WriteByte(FILE *fp, const unsigned char& value);
 
-   // Write one short of data to the file stream
-   // NOTE: these enhance readability and type checking, try to use them over the generic WriteData function
-   int WriteShort( Endianness fileByteOrdering, FILE* fp, const unsigned short& value );
+   /**
+    * Writes one short of data to the file stream.
+    * @note These enhance readability and type checking; try to use them over
+    *       the generic WriteData function.
+    */
+   int WriteShort(Endianness fileByteOrdering, FILE* fp,
+                  const unsigned short& value);
 
-   // Write one long of data to the file stream
-   // NOTE: these enhance readability and type checking, try to use them over the generic WriteData function
-   int WriteLong( Endianness fileByteOrdering, FILE *fp, const unsigned long& value );
+   /**
+    * Writes one long of data to the file stream.
+    * @note These enhance readability and type checking; try to use them over
+    *       the generic WriteData function.
+    */
+   int WriteLong(Endianness fileByteOrdering, FILE *fp,
+                 const unsigned long& value);
 
    void getLine( std::ifstream& f, std::string& text  );
 
