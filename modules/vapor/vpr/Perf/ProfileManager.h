6edd14e8d67c4a67ef122dd805732766bb9a476c
Daniel Shipton <daniel.shipton.oss@gmail.com>
2004-05-20 20:23:28 +0000

    code cleanup. no functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@14999 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/Perf/ProfileManager.h
+++ modules/vapor/vpr/Perf/ProfileManager.h
@@ -18,93 +18,113 @@ namespace vpr
 /*
  * The Manager for the Profile system
  */
-class VPR_CLASS_API ProfileManager {
-public:
+   class VPR_CLASS_API ProfileManager
+   {
+   public:
 
-   /**
-   * Steps one level deeper into the tree, if a child already exists with the specified name
-   * then it accumulates the profiling; otherwise a new child node is added to the profile tree.
-   * INPUT: name - name of this profiling record
-   * WARNINGS:
-   * The string used is assumed to be a static string; pointer compares are used throughout
-   * the profiling code for efficiency.
-   */
-	static	void						startProfile( const char * name );
+      /**
+      * Steps one level deeper into the tree, if a child already exists with the specified name
+      * then it accumulates the profiling; otherwise a new child node is added to the profile tree.
+      * INPUT: name - name of this profiling record
+      * WARNINGS:
+      * The string used is assumed to be a static string; pointer compares are used throughout
+      * the profiling code for efficiency.
+      */
+      static   void                 startProfile( const char * name );
 
-	static	void						startProfile( const char * name, const unsigned int queue_size);
+      static   void                 startProfile( const char * name, const unsigned int queue_size);
 
-   /**
-    * Stop timing and record the results.
-    */
-	static	void						stopProfile( void );
+      /**
+       * Stop timing and record the results.
+       */
+      static   void                 stopProfile( void );
 
-   /**
-    * Reset the contents of the profiling system
-    * 
-    * @post Everything is reset except tree structure. Timing data is reset.
-    */
-	static	void						reset( void );
+      /**
+       * Reset the contents of the profiling system
+       * 
+       * @post Everything is reset except tree structure. Timing data is reset.
+       */
+      static   void                 reset( void );
 
-   /**
-    * Increment the frame counter
-    *
-    * @post Frame counter incremented by one.
-    */
-	static	void						incrementFrameCounter( void );
+      /**
+       * Increment the frame counter
+       *
+       * @post Frame counter incremented by one.
+       */
+      static   void                 incrementFrameCounter( void );
 
-   /**
-    * @return Returns the number of frames since reset
-    */
-	static	int						getFrameCountSinceReset( void )	{ return mFrameCounter; }
+      /**
+       * @return Returns the number of frames since reset
+       */
+      static   int                  getFrameCountSinceReset( void )
+      {
+         return mFrameCounter;
+      }
 
-   /**
-    * @return Returns the elapsed time since last reset
-    */
-	static	float						getTimeSinceReset(void);
+      /**
+       * @return Returns the elapsed time since last reset
+       */
+      static   float                getTimeSinceReset(void);
 
-   /**
-    * @return Returns a new Iterator that is set to the root.
-    */
-	static	ProfileIterator*	   getIterator( void ){ return new ProfileIterator(&mRoot); }
+      /**
+       * @return Returns a new Iterator that is set to the root.
+       */
+      static   ProfileIterator*     getIterator( void )
+      {
+         return new ProfileIterator(&mRoot);
+      }
 
-	static	ProfileNode*	      getRootNode( void ){ return &mRoot; }
+      static   ProfileNode*         getRootNode( void )
+      {
+         return &mRoot;
+      }
 
-   static   void                 printTree(void ) { if(mRoot.getChild() != NULL) {mRoot.printTree(mRoot.getChild());} }
+      static   void                 printTree(void )
+      {
+         if ( mRoot.getChild() != NULL )
+         {
+            mRoot.printTree(mRoot.getChild());
+         }
+      }
 
-   /**
-    * @post Iterator has been deleted
-    */
-	static	void						releaseIterator( ProfileIterator* iterator ) { delete iterator; }
+      /**
+       * @post Iterator has been deleted
+       */
+      static   void                 releaseIterator( ProfileIterator* iterator )
+      {
+         delete iterator;
+      }
 
-private:
-	static	ProfileNode			   mRoot;
-	static	ProfileNode*			mCurrentNode;
-	static	int						mFrameCounter;
-	static	vpr::Interval*			mResetTime;
-};
+   private:
+      static   ProfileNode          mRoot;
+      static   ProfileNode*         mCurrentNode;
+      static   int                  mFrameCounter;
+      static   vpr::Interval*       mResetTime;
+   };
 
 
 /*
  * ProfileSampleClass is a simple way to profile a function's scope
  * Use the PROFILE macro at the start of scope to time
  */
-class	ProfileSample {
-public:
-	ProfileSample( const char * name )
-	{ 
-		ProfileManager::startProfile( name ); 
-	}
+   class ProfileSample
+   {
+   public:
+      ProfileSample( const char * name )
+      {
+         ProfileManager::startProfile( name ); 
+      }
 
-	ProfileSample( const char * name, const unsigned int queue_size)
-	{ 
-		ProfileManager::startProfile( name, queue_size); 
-	}
+      ProfileSample( const char * name, const unsigned int queue_size)
+      {
+         ProfileManager::startProfile( name, queue_size); 
+      }
 
-	~ProfileSample( void )					
-	{ 
-		ProfileManager::stopProfile(); 
-	}
-};
+      ~ProfileSample( void )              
+      {
+         ProfileManager::stopProfile(); 
+      }
+   };
 
 #if defined(DEBUG) || defined(_DEBUG)
 #define	VPR_PROFILE( name )			vpr::ProfileSample __profile( name )
