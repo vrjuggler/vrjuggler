2ed71a271a2eaf9d053b21a260caded76a48e282
Daniel Shipton <daniel.shipton.oss@gmail.com>
2004-05-26 21:11:01 +0000

    Added a standalone version of the driver.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15073 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/gadgeteer/drivers/USDigital/SerialEncoder/SerialEncoderStandalone.h
+++ juggler/modules/gadgeteer/drivers/USDigital/SerialEncoder/SerialEncoderStandalone.h
@@ -30,73 +30,46 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
-#ifndef _GADGET_USDIGITAL_SERIAL_ENCODER_H
-#define _GADGET_USDIGITAL_SERIAL_ENCODER_H
+#ifndef _GADGET_USDIGITAL_SERIAL_ENCODER_STANDALONE_H
+#define _GADGET_USDIGITAL_SERIAL_ENCODER_STANDALONE_H
 
-#include <gadget/Devices/DriverConfig.h>
 #include <vpr/Thread/Thread.h>
 #include <vpr/Sync/Guard.h>
 
-#include <gadget/Type/Input.h>
-#include <gadget/Type/Position.h>
-#include <gadget/Type/DeviceConstructor.h>
-#include <jccl/Config/ConfigElementPtr.h>
-
-#include <gadget/Type/PositionInterface.h>
-#include <gadget/Type/InputMixer.h>
-
-#include "SerialEncoderStandalone.h"
 #include "SEIBus.h"
+#include <gmtl/gmtl.h>
+#include <gmtl/Generate.h>
 
-namespace gadget
-{
-   class InputManager;
-}
-
-extern "C" GADGET_DRIVER_API(void) initDevice(gadget::InputManager* inputMgr);
-
-class SerialEncoder:public gadget::InputMixer<gadget::Input,gadget::Position>
+class SerialEncoderStandalone
 {
 public:
-   SerialEncoder();
-   virtual ~SerialEncoder();
+   SerialEncoderStandalone();
 
-   static std::string getElementType()
+   void setPort(const std::string& port)
    {
-      return std::string("serial_encoder");
+      mPortStr = port;
+   }
+   void setBaudRate(const int& baud_rate)
+   {
+      mBaudRate = baud_rate;
+   }
+   const std::string getPort()
+   {
+      return mPortStr;
+   }
+   int getBaudRate()
+   {
+      return mBaudRate;
    }
 
-   bool config(jccl::ConfigElementPtr e);
-
-   bool startSampling();
-   bool sample();
-   bool stopSampling();
-
-   void updateData()
-   {
-      swapPositionBuffers();
-   }
-
-   void threadedSampleFunction(void* classPointer);
-
-   void operator delete(void* p)
-   {
-      ::operator delete(p);
-   }
-
-protected:
-   virtual void destroy()
-   {
-      delete this;
-   }
+   bool initializeDevice();
+   gmtl::Matrix44f getSample();
 
 private:
-   vpr::ThreadMemberFunctor<SerialEncoder>* mThreadFunctor;
-   vpr::Thread* mSampleThread;
-   bool  mExitFlag;
+   bool  mActive;
    std::string mPortStr;
    long mBaudRate;
-   SerialEncoderStandalone* mSerialEncoder;
+   SEIBus mBus;
 };
 
 #endif
