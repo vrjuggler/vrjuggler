77d6ae18e641a880abbffd4061a66b793d4b826f
David Kabala <dkabala@users.sourceforge.net>
2004-06-10 21:58:07 +0000

    New feature:	Added a new InputData Type that is a std::string as its type. Refactor: Bug fixed: SF Bug#:
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15118 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/gadgeteer/gadget/Type/StringData.h
@@ -0,0 +1,93 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2003 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _GADGET_STRING_DATA_H_
+#define _GADGET_STRING_DATA_H_
+
+#include <stdlib.h>
+#include <string>
+
+#include <gadget/Type/InputData.h>
+
+namespace gadget
+{
+
+/** InputData subclass for string data. */
+class StringData : public InputData
+{
+public:
+   /** Constructor. */
+   StringData()
+      : InputData()
+      , mStringData("")
+   {
+      ;
+   }
+
+   StringData(const std::string s)
+      : InputData()
+      , mStringData(s)
+   {
+      ;
+   }
+
+   std::string getString() const
+   {
+      return mStringData;
+   }
+
+   void setString(const std::string s)
+   {
+      mStringData = s;
+   }
+
+   StringData& operator= (const StringData& pd)
+   {
+      InputData::copy (pd);
+      mStringData = pd.mStringData;
+      return *this;
+   }
+
+   StringData& operator= (const std::string data)
+   {
+      mStringData = data;
+      return *this;
+   }
+
+protected:
+   std::string mStringData;
+}; // class StringData
+
+} // namespace gadget
+
+
+#endif /* _GADGET_STRING_DATA_H_ */
