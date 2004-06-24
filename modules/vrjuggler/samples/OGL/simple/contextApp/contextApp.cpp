3f0019f4aa607d4aeb1636c72fd5abb1b15a462a
Patrick Hartling <patrick.hartling@gmail.com>
2004-06-24 21:59:23 +0000

    Match up the code here with what is shown in VR Juggler tutorial slides. Merged Revision 1.14 to silence compiler warnings.
    git-svn-id: https://vrjuggler.googlecode.com/svn/branches/VRJ_2_0_ALPHA_4_BRANCH/juggler@15126 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/OGL/simple/contextApp/contextApp.cpp
+++ modules/vrjuggler/samples/OGL/simple/contextApp/contextApp.cpp
@@ -71,8 +71,7 @@ void contextApp::draw()
    glMatrixMode(GL_MODELVIEW);
 
    // -- Get Wand matrix --- //
-   Matrix44f wand_matrix;
-   wand_matrix = mWand->getData();
+   Matrix44f wand_pos = mWand->getData();
 
    // --- Create a color for the wand --- //
    float wand_color[3];
@@ -82,7 +81,7 @@ void contextApp::draw()
 
       // --- Draw the box --- //
       glPushMatrix();
-         glMultMatrixf(wand_matrix.mData);    // Push the wand offset matrix on the stack
+         glMultMatrixf(wand_pos.mData);    // Push the wand offset matrix on the stack
          glColor3fv(wand_color);
          glCallList(*mCubeDlId);
       glPopMatrix();
@@ -91,7 +90,7 @@ void contextApp::draw()
       glLineWidth(5.0f);
       glDisable(GL_LIGHTING);
       glPushMatrix();
-         glMultMatrixf(wand_matrix.mData);    // Goto wand position
+         glMultMatrixf(wand_pos.mData);    // Goto wand position
          drawAxis();
       glPopMatrix();
       glEnable(GL_LIGHTING);
@@ -103,8 +102,8 @@ void contextApp::contextInit()
    // --- Allocate context specific data --- //
    (*mCubeDlId) = glGenLists(1);
 
-   glNewList((*mCubeDlId), GL_COMPILE);
-      glScalef(0.50f, 0.50f, 0.50f);
+   glNewList(*mCubeDlId, GL_COMPILE);
+      glScalef(0.5f, 0.5f, 0.5f);
       drawCube();
    glEndList();
 
@@ -120,8 +119,8 @@ void contextApp::initGLState()
    GLfloat light0_specular[] = { 1.0f,  1.0f,  1.0f,  1.0f};
    GLfloat light0_position[] = {0.0f, 0.75f, 0.75f, 0.0f};
 
-   GLfloat mat_ambient[] = { 0.7, 0.7,  0.7,  1.0};
-   GLfloat mat_diffuse[] = { 1.0,  0.5,  0.8,  1.0};
+   GLfloat mat_ambient[] = { 0.7f, 0.7f,  0.7f, 1.0f };
+   GLfloat mat_diffuse[] = { 1.0f,  0.5f,  0.8f, 1.0f };
    GLfloat mat_specular[] = { 1.0,  1.0,  1.0,  1.0};
    GLfloat mat_shininess[] = { 50.0};
 //   GLfloat mat_emission[] = { 1.0,  1.0,  1.0,  1.0};
