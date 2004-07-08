77b489aa2131a847eb1f219d3003f8b85d7dd343
Patrick Hartling <patrick.hartling@gmail.com>
2004-07-08 23:36:11 +0000

    Added a VR Juggler application written in VB.NET.  This is the same application as the C# and Managed C++ versions of SimpleApp, but it is written in VB.NET.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15229 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ vrj.net/examples/VB.NET/SimpleApp/AssemblyInfo.vb
@@ -0,0 +1,59 @@
+'************** <auto-copyright.pl BEGIN do not edit this line> *************
+'
+' VRJ.NET is (C) Copyright 2004 by Patrick Hartling
+'
+' This library is free software; you can redistribute it and/or
+' modify it under the terms of the GNU Library General Public
+' License as published by the Free Software Foundation; either
+' version 2 of the License, or (at your option) any later version.
+'
+' This library is distributed in the hope that it will be useful,
+' but WITHOUT ANY WARRANTY; without even the implied warranty of
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+' Library General Public License for more details.
+'
+' You should have received a copy of the GNU Library General Public
+' License along with this library; if not, write to the
+' Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+' Boston, MA 02111-1307, USA.
+'
+' -----------------------------------------------------------------
+' File:          $RCSfile$
+' Date modified: $Date$
+' Version:       $Revision$
+' -----------------------------------------------------------------
+'
+'************** <auto-copyright.pl END do not edit this line> ***************
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("")> 
+<Assembly: AssemblyCopyright("")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: CLSCompliant(True)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("98E6ED83-3CC9-498D-A3A2-F391C111B912")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.*")> 
