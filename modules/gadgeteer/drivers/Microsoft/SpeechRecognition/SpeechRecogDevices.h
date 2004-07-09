113cb53cbf6787bb9b66dd080aa96ee597deb9f4
David Kabala <dkabala@users.sourceforge.net>
2004-07-09 21:45:04 +0000

    New feature:	MSFTSpeechRecogDigital and MSFTSpeechRecogString are the device wrappers and inherit                from SpeechRecogDigital and SpeechRecogString respectively.
                   MSFTSpeechServerManager is the driver for comunicating with the Microsoft Speech Server.
                   When created MSFTSpeechServerManager registers a callback function witht the Speech Server
                   and creates a new thread for updating with the SPeech Server.
    Refactor:
    Bug fixed:
    SF Bug#:
    
    Submitted by:
    Reviewed by:
    Approved by:
    Obtained from:
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@15237 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/drivers/Microsoft/SpeechRecognition/SpeechRecogDevices.h
@@ -0,0 +1,9 @@
+
+#ifndef _GADGET_SPEECHRECOG_DEVICES_H_
+#define _GADGET_SPEECHRECOG_DEVICES_H_
+
+#include <gadget/Devices/DriverConfig.h>
+
+extern "C" GADGET_DRIVER_API(void) initDevice(gadget::InputManager* inputMgr);
+
+#endif
