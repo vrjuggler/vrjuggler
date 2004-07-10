43bacff36798853cedd3aacaa8d2d5d9eba20677
Patrick Hartling <patrick.hartling@gmail.com>
2004-07-10 15:49:18 +0000

    Wrapped a long line introduced in the last revision.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15248 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/buildwin32.py
+++ juggler/buildwin32.py
@@ -885,8 +885,8 @@ def installGadgeteerDrivers(prefix):
    srcroot = os.path.join(juggler_dir, 'vc7', 'Gadgeteer')
 
    drivers = ['DataGlove', 'Fastrak', 'Flock', 'IBox', 'IntersenseAPI',
-              'IS900', 'MotionStar', 'MSFTSpeechRecognition', 'PinchGlove', 'SerialEncoder',
-              'TrackdAPI', 'VRPN']
+              'IS900', 'MotionStar', 'MSFTSpeechRecognition', 'PinchGlove',
+              'SerialEncoder', 'TrackdAPI', 'VRPN']
 
    for d in drivers:
       srcdir = os.path.join(srcroot, d)
