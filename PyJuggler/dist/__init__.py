2ce1ce82f343434ef43e00bdb15d191f05813f3d
Patrick Hartling <patrick.hartling@gmail.com>
2004-07-14 14:08:55 +0000

    Roll the version to 0.9.2 for another release.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15294 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/dist/__init__.py
+++ PyJuggler/dist/__init__.py
@@ -28,7 +28,7 @@
 import string
 __all__     = ["vrj", "gadget", "jccl", "vpr"]
 __build__   = 1
-__version__ = '0.9.1.%d' % __build__
+__version__ = '0.9.2.%d' % __build__
 __date__    = string.join(string.split('$Date$')[1:3], ' ')
 __author__  = 'Patrick Hartling <patrick@vrjuggler.org>'
 __doc__     = '''This is PyJuggler.  For information regarding PyJuggler see:
