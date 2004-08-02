622280b14eed4edf5f1b022b76244b6e3a130db5
Allen Bierbaum <abierbaum@gmail.com>
2004-08-02 15:36:06 +0000

    Added test case for Profile code.  Doesn't do much yet, but it is there to make sure it compiles.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@15405 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/test/TestSuite/TestCases/Perf/PerfTest.cpp
@@ -0,0 +1,63 @@
+#include <vpr/vpr.h>
+#include <vpr/Util/GUID.h>
+//#include <vpr/Util/GUIDFactory.h>
+#include <vpr/Util/Interval.h>
+
+#include <vpr/Util/Debug.h>
+
+#include <TestCases/Perf/PerfTest.h>
+#include <vpr/Perf/ProfileManager.h>      // Get all the profile stuff
+
+
+namespace vprTest
+{
+
+CPPUNIT_TEST_SUITE_REGISTRATION( PerfTest );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( PerfMetricTest, MySuites::metric() );
+
+void PerfTest::testConstructTree ()
+{
+   vpr::ProfileManager::startProfile("First");
+   vpr::ProfileManager::startProfile("First-one");
+   vpr::ProfileManager::stopProfile();
+   vpr::ProfileManager::stopProfile();
+
+   vpr::ProfileNode* root_node = vpr::ProfileManager::getRootNode();
+   vpr::ProfileNode* first_node = root_node->getChild();
+   vpr::ProfileNode* first_one_node = first_node->getChild();
+
+   CPPUNIT_ASSERT(first_node->getParent() == root_node);
+   CPPUNIT_ASSERT(first_one_node->getParent() == first_node);
+}
+
+void PerfTest::testReset ()
+{
+
+   //CPPUNIT_ASSERT(false);
+}
+
+
+// ------------------ Perf Metric --------------------- //
+void PerfMetricTest::testTreeOverhead()
+{
+   const vpr::Uint32 iters(1000);
+   vpr::Uint32 loops = iters;
+   vpr::GUID guid1;
+
+   CPPUNIT_METRIC_START_TIMING();
+
+   while(loops--)
+   {
+     vpr::ProfileManager::startProfile("MyFirst");
+     vpr::ProfileManager::startProfile("MySecond");
+     vpr::ProfileManager::stopProfile();
+     vpr::ProfileManager::startProfile("MySecondAgain");
+     vpr::ProfileManager::stopProfile();
+     vpr::ProfileManager::stopProfile();
+   }
+
+   CPPUNIT_METRIC_STOP_TIMING();
+   CPPUNIT_ASSERT_METRIC_TIMING_LE("PerfTest/TreeOverhead", iters, 0.05f, 0.1f);
+}
+
+} // End of vprTest namespace
