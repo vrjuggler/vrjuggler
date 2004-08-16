54b4547928529fa04c44a6c980150416142afed0
Patrick Hartling <patrick.hartling@gmail.com>
2004-08-16 19:12:19 +0000

    Shorten the copyright header to follow the Boost style.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15541 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- vrj.net/src/gmtl-Xforms.h
+++ vrj.net/src/gmtl-Xforms.h
@@ -1,29 +1,10 @@
-/*************** <auto-copyright.pl BEGIN do not edit this line> **************
- *
- * VRJ.NET is (C) Copyright 2004 by Patrick Hartling
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- * -----------------------------------------------------------------
- * File:          $RCSfile$
- * Date modified: $Date$
- * Version:       $Revision$
- * -----------------------------------------------------------------
- *
- *************** <auto-copyright.pl END do not edit this line> ***************/
+// VRJ.NET is (C) Copyright 2004 by Patrick Hartling
+// Distributed under the GNU Lesser General Public License 2.1.  (See
+// accompanying file COPYING.txt or http://www.gnu.org/copyleft/lesser.txt)
+
+// File:          $RCSfile$
+// Date modified: $Date$
+// Version:       $Revision$
 
 #ifndef _VRJ_NET_XFORMS_H_
 #define _VRJ_NET_XFORMS_H_
