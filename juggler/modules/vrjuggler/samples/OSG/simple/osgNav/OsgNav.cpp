14aa56bd1bde31d9e507b59724a0ed8ddf396089
Patrick Hartling <patrick.hartling@gmail.com>
2004-08-20 01:45:31 +0000

    The depth buffer is not supposed to be cleared in bufferPreDraw() for any application type.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@15556 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/samples/OSG/simple/osgNav/OsgNav.cpp
+++ juggler/modules/vrjuggler/samples/OSG/simple/osgNav/OsgNav.cpp
@@ -122,7 +122,7 @@ void OsgNav::preFrame()
 void OsgNav::bufferPreDraw()
 {
    glClearColor(0.0, 0.0, 0.0, 0.0);
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void OsgNav::myInit()
