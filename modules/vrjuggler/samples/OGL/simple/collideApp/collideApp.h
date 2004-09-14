5a3449f7ddba5c764a0b52749efaa154eaf0e7e9
Patrick Hartling <patrick.hartling@gmail.com>
2004-09-14 13:59:35 +0000

    Fixed some coding standard violations (there are still many left).  Part of this involves a massive clean-up of whitespace.  There are no functional changes included with this revision.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@15638 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/OGL/simple/collideApp/collideApp.h
+++ modules/vrjuggler/samples/OGL/simple/collideApp/collideApp.h
@@ -78,46 +78,46 @@ static const float RADIUS = 0.25;
 // This class holds all information for our spheres
 class Ball
 {
-   public:
-      Ball(Spheref s, Vec3f vel) : mSphere(s), mVel(vel)
-      {;}
+public:
+   Ball(Spheref s, Vec3f vel) : mSphere(s), mVel(vel)
+   {;}
       
-      void init()
-      {
-         // Create a GLU Quadric object
-         mSphereObj = gluNewQuadric();
-      }
+   void init()
+   {
+      // Create a GLU Quadric object
+      mSphereObj = gluNewQuadric();
+   }
       
-      void draw()
-      {
-         // Draw a Sphere
-         glPushMatrix();
+   void draw()
+   {
+      // Draw a Sphere
+      glPushMatrix();
                   
-         glColor3f(c1, c2, c3);
-         glTranslatef(mSphere.getCenter()[0],
-                      mSphere.getCenter()[1],
-                      mSphere.getCenter()[2]);
-         gluSphere(mSphereObj, RADIUS, 16, 16);
-         glPopMatrix();
-      }
+      glColor3f(c1, c2, c3);
+      glTranslatef(mSphere.getCenter()[0],
+                   mSphere.getCenter()[1],
+                   mSphere.getCenter()[2]);
+      gluSphere(mSphereObj, RADIUS, 16, 16);
+      glPopMatrix();
+   }
 
-      void update(const float delta)
-      {
-         Vec3f pos = mSphere.getCenter();
-         pos = pos + (mVel * delta);
-         mSphere.setCenter(pos);
-      }
+   void update(const float delta)
+   {
+      Vec3f pos = mSphere.getCenter();
+      pos = pos + (mVel * delta);
+      mSphere.setCenter(pos);
+   }
    
-      Spheref 	        mSphere;        // Sphere object
-      Vec3f 	        mVel;           // Velocity vector of Ball
-      GLUquadricObj*    mSphereObj;     
-      double            c1, c2, c3;     // Assigns Color
-      bool              hCheck;         // keeps track of collision
+   Spheref 	  mSphere;        // Sphere object
+   Vec3f 	  mVel;           // Velocity vector of Ball
+   GLUquadricObj* mSphereObj;     
+   double         c1, c2, c3;     // Assigns Color
+   bool           hCheck;         // keeps track of collision
 };
 //*****************************************************************
 
 
-/*
+/**
  * Simple OGL Demonstration application
  *
  * Various new features of VR Juggler are demonstrated here
@@ -125,158 +125,165 @@ class Ball
  */
 class collideApp : public GlApp
 {
-   public:
-      collideApp()
-      {;}
+public:
+   collideApp()
+   {;}
 
-      virtual ~collideApp (void) {;}
+   virtual ~collideApp()
+   {;}
 
-   public:
-      // initialization
-      virtual void init()
-      {
-         // initialize input devices
-         cout << "App:init()" << endl;
-         mWand.init("VJWand");
-         mHead.init("VJHead");
-         mButton0.init("VJButton0");
-         mButton1.init("VJButton1");
+public:
+   // initialization
+   virtual void init()
+   {
+      // initialize input devices
+      cout << "App:init()" << endl;
+      mWand.init("VJWand");
+      mHead.init("VJHead");
+      mButton0.init("VJButton0");
+      mButton1.init("VJButton1");
       
-         // initialize 10 balls
-         for (int i = 0; i < 10; i++)
-         {
-            // random positioning of Spheres
-            Point3f  p((i % 5) + (i % 4) - 2.0, (i % 2) + (i % 3) + 1.0, (i % 3) + (i % 2) + 1.0);
+      // initialize 10 balls
+      for (int i = 0; i < 10; ++i)
+      {
+         // random positioning of Spheres
+         Point3f  p((i % 5) + (i % 4) - 2.0, (i % 2) + (i % 3) + 1.0, (i % 3) + (i % 2) + 1.0);
 
-            Spheref s(p, RADIUS);
-            Vec3f v( 0.1 * (i % 4) + 0.1, -0.1 * (i % 3) + 0.1, 0.1 * (i % 2) + 0.1);
+         Spheref s(p, RADIUS);
+         Vec3f v( 0.1 * (i % 4) + 0.1, -0.1 * (i % 3) + 0.1, 0.1 * (i % 2) + 0.1);
 
-            // a new ball
-            Ball* temp = new Ball(s, v);
+         // a new ball
+         Ball* temp = new Ball(s, v);
 
-            std::cout << "Adding ball at: " << p << std::endl;
+         std::cout << "Adding ball at: " << p << std::endl;
             
-            // assigns the color of the ball
-            unsigned int r1 = rand() % 10;
-            unsigned int r2 = rand() % 10;
-            unsigned int r3 = rand() % 10;
-            temp->c1 = (double) r1 / 10;
-            temp->c2 = (double) r2 / 10;
-            temp->c3 = (double) r3 / 10;
+         // assigns the color of the ball
+         unsigned int r1 = rand() % 10;
+         unsigned int r2 = rand() % 10;
+         unsigned int r3 = rand() % 10;
+         temp->c1 = (double) r1 / 10;
+         temp->c2 = (double) r2 / 10;
+         temp->c3 = (double) r3 / 10;
 
-            temp->hCheck = false;
+         temp->hCheck = false;
 
-            mBalls.push_back(temp);
-	    temp->init();
-         }
-      
-         // initializing 6 planes
-         p1 = Planef(Vec3f (0.0, 1.0, 0.0), Point3f (0.0, 0.0, 0.0));   // bottom
-         p2 = Planef(Vec3f (0.0, 0.0, -1.0), Point3f (0.0, 5.0, 5.0));  // front
-         p3 = Planef(Vec3f (0.0, 0.0, 1.0), Point3f (0.0, 5.0, -5.0));  // back
-         p4 = Planef(Vec3f (1.0, 0.0, 0.0), Point3f (-5.0, 5.0, 0.0));  // left
-         p5 = Planef(Vec3f (-1.0, 0.0, 0.0), Point3f (5.0, 5.0, 0.0));  // right
-         p6 = Planef(Vec3f (0.0, -1.0, 0.0), Point3f (0.0, 10.0, 0.0)); // top
-         pc1 = false;
-         pc2 = false;
-         pc3 = false;
-         pc4 = false;
-         pc5 = false;
-         pc6 = false;
-         // bottom wiring
-         for (int a = -5; a < 6; a++)
-         {
-            for (int b = -5; b < 6; b++)
-            {
-               m1.push_back(Point3f( (float) a, 0.0f, (float) b));
-            }
-         }
-         // front wiring
-         for (int a = -5; a < 6; a++)
-         {
-            for (int b = 0; b < 11; b++)
-            {
-               m2.push_back(Point3f( (float) a, (float) b, 5.0f));
-            }
-         }
-         // back wiring
-         for (int a = -5; a < 6; a++)
-         {
-            for (int b = 0; b < 11; b++)
-            {
-               m3.push_back(Point3f( (float) a, (float) b, -5.0f));
-            }
-         }
-         // left wiring
-         for (int a = -5; a < 6; a++)
-         {
-            for (int b = 0; b < 11; b++)
-            {
-               m4.push_back(Point3f( -5.0f, (float) b, (float) a));
-            }
-         }
-         // right wiring
-         for (int a = -5; a < 6; a++)
-         {
-            for (int b = 0; b < 11; b++)
-            {
-               m5.push_back(Point3f( 5.0f, (float) b, (float) a));
-            }
-         }
-         // top wiring
-         for (int a = -5; a < 6; a++)
-         {
-            for (int b = -5; b < 6; b++)
-            {
-               m6.push_back(Point3f( (float) a, 10.0f, (float) b));
-            }
-         }
+         mBalls.push_back(temp);
+         temp->init();
       }
-
-      virtual void apiInit()
-      {;}
-
-   public:
-      virtual void bufferPreDraw();
-
-      // Sphere against Sphere collision 
-      template<class DATA_TYPE> void testBallCollision(Sphere<DATA_TYPE>& s1, Vec<DATA_TYPE, 3>& v1, Sphere<DATA_TYPE>& s2, Vec<DATA_TYPE, 3>& v2, DATA_TYPE delta, DATA_TYPE first, DATA_TYPE second, bool& hit);
-
-      // Sphere against Plane collision
-      template<class DATA_TYPE> void testPlaneCollision(Plane<DATA_TYPE>& plane, Sphere<DATA_TYPE>& sp, Vec<DATA_TYPE, 3>& vel, DATA_TYPE delta, bool& hit);
-   
-      // Increase number of balls
-      virtual void increase();
-       	 
-      // Decrease number of balls
-      virtual void decrease();
-   
-      virtual void preFrame();
-      virtual void intraFrame() {;}
-      virtual void postFrame() {;}
-
-   public:
-      virtual void contextInit()
+      
+      // initializing 6 planes
+      p1 = Planef(Vec3f (0.0, 1.0, 0.0), Point3f (0.0, 0.0, 0.0));   // bottom
+      p2 = Planef(Vec3f (0.0, 0.0, -1.0), Point3f (0.0, 5.0, 5.0));  // front
+      p3 = Planef(Vec3f (0.0, 0.0, 1.0), Point3f (0.0, 5.0, -5.0));  // back
+      p4 = Planef(Vec3f (1.0, 0.0, 0.0), Point3f (-5.0, 5.0, 0.0));  // left
+      p5 = Planef(Vec3f (-1.0, 0.0, 0.0), Point3f (5.0, 5.0, 0.0));  // right
+      p6 = Planef(Vec3f (0.0, -1.0, 0.0), Point3f (0.0, 10.0, 0.0)); // top
+      pc1 = false;
+      pc2 = false;
+      pc3 = false;
+      pc4 = false;
+      pc5 = false;
+      pc6 = false;
+      // bottom wiring
+      for (int a = -5; a < 6; ++a)
       {
-         initGLState();
+         for (int b = -5; b < 6; ++b)
+         {
+            m1.push_back(Point3f( (float) a, 0.0f, (float) b));
+         }
       }
-      
-      virtual void draw();
+      // front wiring
+      for (int a = -5; a < 6; ++a)
+      {
+         for (int b = 0; b < 11; ++b)
+         {
+            m2.push_back(Point3f( (float) a, (float) b, 5.0f));
+         }
+      }
+      // back wiring
+      for (int a = -5; a < 6; ++a)
+      {
+         for (int b = 0; b < 11; ++b)
+         {
+            m3.push_back(Point3f( (float) a, (float) b, -5.0f));
+         }
+      }
+      // left wiring
+      for (int a = -5; a < 6; ++a)
+      {
+         for (int b = 0; b < 11; ++b)
+         {
+            m4.push_back(Point3f( -5.0f, (float) b, (float) a));
+         }
+      }
+      // right wiring
+      for (int a = -5; a < 6; ++a)
+      {
+         for (int b = 0; b < 11; ++b)
+         {
+            m5.push_back(Point3f( 5.0f, (float) b, (float) a));
+         }
+      }
+      // top wiring
+      for (int a = -5; a < 6; ++a)
+      {
+         for (int b = -5; b < 6; ++b)
+         {
+            m6.push_back(Point3f( (float) a, 10.0f, (float) b));
+         }
+      }
+   }
 
-   private:
-      void initGLState();
+   virtual void apiInit()
+   {;}
 
-   public:
-      vector<Ball*> mBalls;
-      Planef p1,p2,p3,p4,p5,p6;
-      vector<Point3f> m1,m2,m3,m4,m5,m6;
-      bool pc1,pc2,pc3,pc4,pc5,pc6;
-      float delta;                      // rate of application/demo
+public:
+   virtual void bufferPreDraw();
+
+   // Sphere against Sphere collision 
+   template<class DATA_TYPE>
+   void testBallCollision(Sphere<DATA_TYPE>& s1, Vec<DATA_TYPE, 3>& v1,
+                          Sphere<DATA_TYPE>& s2, Vec<DATA_TYPE, 3>& v2,
+                          DATA_TYPE delta, DATA_TYPE first, DATA_TYPE second,
+                          bool& hit);
+
+   // Sphere against Plane collision
+   template<class DATA_TYPE>
+   void testPlaneCollision(Plane<DATA_TYPE>& plane, Sphere<DATA_TYPE>& sp,
+                           Vec<DATA_TYPE, 3>& vel, DATA_TYPE delta, bool& hit);
    
-      gadget::PositionInterface mWand;
-      gadget::PositionInterface mHead;
-      gadget::DigitalInterface mButton0;
-      gadget::DigitalInterface mButton1;
+   // Increase number of balls
+   virtual void increase();
+       	 
+   // Decrease number of balls
+   virtual void decrease();
+   
+   virtual void preFrame();
+   virtual void intraFrame() {;}
+   virtual void postFrame() {;}
+
+public:
+   virtual void contextInit()
+   {
+      initGLState();
+   }
+      
+   virtual void draw();
+
+private:
+   void initGLState();
+
+public:
+   vector<Ball*> mBalls;
+   Planef p1,p2,p3,p4,p5,p6;
+   vector<Point3f> m1,m2,m3,m4,m5,m6;
+   bool pc1,pc2,pc3,pc4,pc5,pc6;
+   float delta;                      // rate of application/demo
+   
+   gadget::PositionInterface mWand;
+   gadget::PositionInterface mHead;
+   gadget::DigitalInterface mButton0;
+   gadget::DigitalInterface mButton1;
 };
 
 #endif
