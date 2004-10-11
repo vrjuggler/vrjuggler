8deaef0ffc5e835f613992db3fe840335c5b8a9a
Patrick Hartling <patrick.hartling@gmail.com>
2004-10-11 14:56:55 +0000

    Refactor:	vpr::LibraryLoader is a collection of helper functions, and 		it should be written as such.  Now, all its methods are 		static, and it makes no attempt to manage any DSOs that get 		loaded.  Furthermore, the methods have been given new names 		that accurately reflect what they do.  The notion of an 		"initialization function" has been replaced by one of an 		entry point function.
    Bumped version to 0.40.0.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@15767 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/md/POSIX/DynLoad/LibraryUNIX.h
+++ modules/vapor/vpr/md/POSIX/DynLoad/LibraryUNIX.h
@@ -75,7 +75,9 @@ public:
     *             name of the library (e.g., "mylib.dll" or "libmylib.so").
     *             The name may be an absolute path or a relative path.
     */
-   LibraryUNIX(const std::string& name) : mName(name), mLibrary(NULL)
+   LibraryUNIX(const std::string& name)
+      : mName(name)
+      , mLibrary(NULL)
    {
       ;
    }
@@ -89,7 +91,9 @@ public:
     *
     * @see findSymbolAndLibrary, load
     */
-   LibraryUNIX() : mName(""), mLibrary(NULL)
+   LibraryUNIX()
+      : mName("")
+      , mLibrary(NULL)
    {
       ;
    }
@@ -97,7 +101,9 @@ public:
    /**
     * Copy constructor.
     */
-   LibraryUNIX(const LibraryUNIX& lib) : mName(""), mLibrary(NULL)
+   LibraryUNIX(const LibraryUNIX& lib)
+      : mName("")
+      , mLibrary(NULL)
    {
       copy(lib);
    }
@@ -148,6 +154,17 @@ public:
     * @post The library is unloaded.
     */
    vpr::ReturnStatus unload();
+
+   /**
+    * Returns whether this library has been loaded from local storage.
+    *
+    * @return true is returned if this library has been loaded from local
+    *         storage, false otherwise.
+    */
+   bool isLoaded() const
+   {
+      return mLibrary != NULL;
+   }
 
    //@{
    /**
