fb589022098788110a0ddf590127c13d49ba7a89
Patrick Hartling <patrick.hartling@gmail.com>
2004-10-12 21:54:39 +0000

    Merged changes from HEAD:
    Revision 1.7:
       Update the layout when property values are added, removed, or otherwise
       changed.
    
    Revision 1.8:
       Added missing copyright header.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/branches/VRJ_2_0_ALPHA_4_BRANCH/juggler@15814 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/editors/org/vrjuggler/jccl/editors/VarListPanel.java
+++ modules/jackal/editors/org/vrjuggler/jccl/editors/VarListPanel.java
@@ -1,3 +1,34 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2003 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
 package org.vrjuggler.jccl.editors;
 
 import java.awt.Color;
@@ -61,18 +92,28 @@ public class VarListPanel extends ExpandablePanel implements ConfigElementListen
                                                          evt.getValue(), prop_def, label, 2,
                                                          elm.getPropertyValueCount(prop_def.getToken()) - 1);
       }
+
+      revalidate();
+      repaint();
    }
    
    public void nameChanged(ConfigElementEvent evt)
    {
-      // Do Nothing...
+      revalidate();
+      repaint();
    }
+
    public void propertyValueChanged(ConfigElementEvent evt)
    { 
-      // Do Nothing...
+      revalidate();
+      repaint();
    }
+
    public void propertyValueRemoved(ConfigElementEvent evt)
    {
       System.out.println("Property value removed...");
+
+      revalidate();
+      repaint();
    }
 }
