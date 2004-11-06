90420673a2b080be0070cb5d0dd0aac70db8269b
Patrick Hartling <patrick.hartling@gmail.com>
2004-11-06 16:24:08 +0000

    Added docstrings.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16085 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/src/jccl/_ConfigElement.cpp
+++ PyJuggler/src/jccl/_ConfigElement.cpp
@@ -18,22 +18,86 @@ using namespace boost::python;
 void _Export_ConfigElement()
 {
     scope* jccl_ConfigElement_scope = new scope(
-    class_< jccl::ConfigElement >("ConfigElement", init<  >())
+    class_< jccl::ConfigElement >("ConfigElement",
+         "A unit of configuration information.\n"
+         "References a config element DOM node and a definition for the given\n"
+         "configuration element type."
+         ,
+         init<  >(
+            "__init__()\n"
+            "Default constructor.\n\n"
+            "__init__(def)\n"
+            "Constructs a ConfigElement matching the given definition.\n"
+            "Arguments:\n"
+            "def -- A ConfigDefinition object that refers to a valid\n"
+            "       ConfigDefinition.\n\n"
+            "__init__(elt)\n"
+            "Copy constructor."
+         )
+        )
         .def(init< jccl::ConfigDefinitionPtr >())
         .def(init< const jccl::ConfigElement& >())
         .def("isValid", &jccl::ConfigElement::isValid)
         .def("assertValid", &jccl::ConfigElement::assertValid)
-        .def("initFromNode", &jccl::ConfigElement::initFromNode)
-        .def("getChildElement", &jccl::ConfigElement::getChildElement)
-        .def("getNum", &jccl::ConfigElement::getNum)
-        .def("getName", &jccl::ConfigElement::getName)
-        .def("getFullName", &jccl::ConfigElement::getFullName)
-        .def("getVersion", &jccl::ConfigElement::getVersion)
-        .def("getID", &jccl::ConfigElement::getID)
-        .def("setProperty", (bool (jccl::ConfigElement::*)(const std::string&, const int, bool) )&jccl::ConfigElement::setProperty)
+        .def("initFromNode", &jccl::ConfigElement::initFromNode,
+             "initFromNode(elementNode) -> Boolean\n"
+             "Initializes this element from an XML node.\n"
+             "Constructs the element to reference the given element node.")
+        .def("getChildElement", &jccl::ConfigElement::getChildElement,
+             "getChildElement(path) -> ConfigElement\n"
+             "Gets a child element from a property of self.\n"
+             "Arguments:\n"
+             "path -- The complete name of a child element in self.\n"
+             "        The form is, for example, \"property_token/instance_name\".\n"
+             "NOTE: This uses the name attribute of the elements.\n"
+             "Returns:\n"
+             "A ConfigElement pointing to the child element.  This may be\n"
+             "a NULL ConfigElement if the child element name was not found.\n"
+             "Check before use.")
+        .def("getNum", &jccl::ConfigElement::getNum,
+             "getNum(property) -> int\n"
+             "Returns the number of values for the specified property.\n"
+             "Arguments:\n"
+             "property -- The token of a property in this element.\n"
+             "Returns:\n"
+             "The number of values that exist for the given property which\n"
+             "may be zero.")
+        .def("getName", &jccl::ConfigElement::getName,
+             "getName() -> string\n"
+             "Returns the instance name of this ConfigElement.")
+        .def("getFullName", &jccl::ConfigElement::getFullName,
+             "getFullName() -> string\n"
+             "Returns the fully qualified, unique name of this element.\n"
+             "This will be different from getName() when this element is a\n"
+             "child of another element.  In that case, the name will be\n"
+             "based on the element hierarchy and the property token.  The\n"
+             "format in that case will be\n"
+             "\"element name 0/proeprty_token_0/element name 1/property_token_1/...\"")
+        .def("getVersion", &jccl::ConfigElement::getVersion,
+             "getVersion() -> int\n"
+             "Returns the version number of the definition used by this\n"
+             "ConfigElement.")
+        .def("getID", &jccl::ConfigElement::getID,
+             "getID() -> string\n"
+             "Returns the string that identifies self's ConfigDefinition.")
+        .def("setProperty",
+             (bool (jccl::ConfigElement::*)(const std::string&, const int, bool) )&jccl::ConfigElement::setProperty)
         .def("setProperty", (bool (jccl::ConfigElement::*)(const std::string&, int, jccl::ConfigElementPtr) )&jccl::ConfigElement::setProperty)
-        .def("getElementPtrDependencies", &jccl::ConfigElement::getElementPtrDependencies)
-        .def("getChildElements", &jccl::ConfigElement::getChildElements)
+        .def("getElementPtrDependencies",
+             &jccl::ConfigElement::getElementPtrDependencies,
+             "getElementPtrDependencies() -> string list\n"
+             "Returns a list of self's dependencies.\n"
+             "Dependencies are any config element objects by an \"Element\n"
+             "Pointer\" proeprty of self (or any element embedded in self).\n"
+             "Returns:\n"
+             "A list of the names of all config elements referenced by self,\n"
+             "which can be used for dependency checking.")
+        .def("getChildElements", &jccl::ConfigElement::getChildElements,
+             "getChildElements() -> ConfigElement list\n"
+             "Returns a list of self's child (embedded) elements.\n"
+             "Returns:\n"
+             "A list of ConfigElement objects that are the config elements\n"
+             "embedded in self.")
         .def("getNode", &jccl::ConfigElement::getNode)
         .def(self_ns::str(self))
         .def( self == self )
--- PyJuggler/src/jccl/_ConfigElementHandler.cpp
+++ PyJuggler/src/jccl/_ConfigElementHandler.cpp
@@ -92,10 +92,80 @@ struct jccl_ConfigElementHandler_Wrapper: jccl::ConfigElementHandler
 // Module ======================================================================
 void _Export_ConfigElementHandler()
 {
-    class_< jccl::ConfigElementHandler, boost::noncopyable, pyj::jccl_ConfigElementHandler_Wrapper >("ConfigElementHandler", init<  >())
-        .def("configCanHandle", pure_virtual(&jccl::ConfigElementHandler::configCanHandle))
-        .def("configProcessPending", &jccl::ConfigElementHandler::configProcessPending, &pyj::jccl_ConfigElementHandler_Wrapper::default_configProcessPending)
-        .def("configAdd", pure_virtual(&jccl::ConfigElementHandler::configAdd))
-        .def("configRemove", pure_virtual(&jccl::ConfigElementHandler::configRemove))
+    class_< jccl::ConfigElementHandler, boost::noncopyable, pyj::jccl_ConfigElementHandler_Wrapper >("ConfigElementHandler",
+         "Abstract base class for all classes that can handle ConfigElement\n"
+         "objects.  Any class supporting this interface can be dynamically\n"
+         "reconfigured by the JCCL Configuration Manager.\n\n"
+         "The idea is that a subclass overrides configCanHandle() to\n"
+         "recognize those elements types that the derived class cares about.\n"
+         "Then override configAdd() and configRemove() to process instances\n"
+         "of those elements.\n\n"
+         "The actual work of checking the list of pending add and remove\n"
+         "requests and passing them to the methods of this interface is done\n"
+         "by configProcessPending().  This can also be overriden to provide\n"
+         "special behavior, but this is strongly discouraged.  The default\n"
+         "implementation should be sufficient for almost any conceivable\n"
+         "dynamic reconfiguration need."
+         ,
+         init<  >(
+         )
+        )
+        .def("configCanHandle",
+             pure_virtual(&jccl::ConfigElementHandler::configCanHandle),
+             "configCanHandle(element) -> Boolean\n"
+             "Checks if this handler can process the given config element.\n"
+             "Typically, an implementation of this method will check the\n"
+             "element's definition ID to decide if it knows how to deal with\n"
+             "the element.\n"
+             "Arguments:\n"
+             "element -- The current config element that is ready to be\n"
+             "           processed.\n"
+             "Returns:\n"
+             "True is returned if this handler can process the given element."
+         )
+        .def("configProcessPending",
+             &jccl::ConfigElementHandler::configProcessPending,
+             &pyj::jccl_ConfigElementHandler_Wrapper::default_configProcessPending,
+             "configProcessPending() -> int\n"
+             "Processes any pending reconfiguration requests that we know\n"
+             "how to deal with.\n\n"
+             "The default implementation does the following for each item in\n"
+             "the pending list:\n\n"
+             "   for each pending item p in the pending list do\n"
+             "      if this->configCanHandle(p) AND p's dependencides are met\n"
+             "         retval = configAdd or configRemove (p)\n"
+             "         if retval == true\n"
+             "            remove request from pending\n"
+             "            add or remove p.element from active\n\n"
+             "ConfigManager's pending list MUST be locked before this\n"
+             "function is called.  Typically, configProcessPending() will be\n"
+             "called by ConfigManager.attemptReconfiguration(), which takes\n"
+             "care of this automatically."
+         )
+        .def("configAdd",
+             pure_virtual(&jccl::ConfigElementHandler::configAdd),
+             "configAdd(element) -> Boolean\n"
+             "Adds the pending element to the configuration.\n"
+             "Pre-condition:\n"
+             "configCanHandle(element) == True\n"
+             "Arguments:\n"
+             "element -- A newly added ConfigElement object to be processed.\n"
+             "Returns:\n"
+             "True is returned if and only if the given element was\n"
+             "processed successfully."
+         )
+        .def("configRemove",
+             pure_virtual(&jccl::ConfigElementHandler::configRemove),
+             "configRemove(element) -> Boolean\n"
+             "Removes the pending element from the configuration.\n"
+             "Pre-condition:\n"
+             "configCanHandle(element) == True\n"
+             "Arguments:\n"
+             "element -- A newly removed ConfigElement object to be\n"
+             "           processed.\n"
+             "Returns:\n"
+             "True is returned if and only if the given element was\n"
+             "processed successfully."
+         )
     ;
 }
