c0ed38df1a06102927897704335bd2fe2e354080
Patrick Hartling <patrick.hartling@gmail.com>
2004-11-10 15:22:08 +0000

    Whitespace and formatting clean-up.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16118 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/plugins/ApplicationDataManager/UserData.h
+++ modules/gadgeteer/plugins/ApplicationDataManager/UserData.h
@@ -52,15 +52,15 @@ namespace cluster
 /**
  * If MyType all ready derives from vpr::SerializableObject you can
  * use the following:
- * 
+ *
  *                cluster::UserData<MyType> mMyData;
- *                
- * - Otherwise you can use the SerializableObjectMixin and templete 
+ *
+ * - Otherwise you can use the SerializableObjectMixin and templete
  * specializations to add the readObject() and writeObject() functions
  * to use for your existing datatype.
- * 
+ *
  *      cluster::UserData<vpr::SerializableObjectMixin<MyType> > mMyData;
- *      
+ *
  *      template<class MyType>
  *      vpr::ReturnStatus
  *      vpr::SerializableObjectMixin<MyType>::writeObject(vpr::ObjectWriter* writer)
@@ -83,32 +83,33 @@ template<class BASE>
 class AppDataMixin : public BASE, public ApplicationData
 {
 public:
-   AppDataMixin(const vpr::GUID& id, const std::string& host_name) : ApplicationData(id, host_name)
+   AppDataMixin(const vpr::GUID& id, const std::string& host_name)
+      : ApplicationData(id, host_name)
    {;}
-   
+
    virtual vpr::ReturnStatus writeObject(vpr::ObjectWriter* writer)
    {
       return BASE::writeObject(writer);
    }
-   
+
    virtual vpr::ReturnStatus readObject(vpr::ObjectReader* reader)
    {
       return BASE::readObject(reader);
    }
-      
+
 #ifndef _MSC_VER
 private:
    // Make sure that we are not already deriving from ApplicationData
-   BOOST_STATIC_ASSERT(::boost::type_traits::ice_not< 
-                        (::boost::is_base_and_derived<cluster::ApplicationData,BASE>::value) 
+   BOOST_STATIC_ASSERT(::boost::type_traits::ice_not<
+                        (::boost::is_base_and_derived<cluster::ApplicationData,BASE>::value)
                         >::value);
-   
+
    // Make sure that we are deriving from a SerializableObject class
    BOOST_STATIC_ASSERT((::boost::is_base_and_derived<vpr::SerializableObject,BASE>::value));
 #endif
 };
 
-/** 
+/**
  * Allows the user to have a copy constructor
  * and also not have to know the exact type
  */
@@ -117,24 +118,24 @@ class UserData
 {
 public:
    typedef TYPE Type;
-   typedef boost::shared_ptr< AppDataMixin<TYPE> > AppDataPtr; 
-   
+   typedef boost::shared_ptr< AppDataMixin<TYPE> > AppDataPtr;
+
    UserData()
    {;}
-   
+
    /**
     * Copy Constructor
     */
    UserData(const UserData& userdata) : mAppData(userdata.mAppData)
    {;}
-   
+
    UserData& operator=(const UserData& userdata)
    {
       if (this == &userdata)
       {
          return *this;
       }
-      
+
       mAppData = AppDataPtr(userdata.mAppData);
       return *this;
    }
@@ -143,7 +144,7 @@ public:
     * Initialize the ApplicationData object.
     *
     * @param id         unique id associated with this ApplicationData object.
-    * @param host_name  network address of cluster node responsible for 
+    * @param host_name  network address of cluster node responsible for
     *                   updating the ApplicationData object
     */
    void init(const vpr::GUID& id,
@@ -151,33 +152,39 @@ public:
    {
       mAppData = AppDataPtr(new AppDataMixin<TYPE>(id, host_name));
    }
-   
+
    virtual ~UserData()
-   {}
-  
+   {;}
+
    /**
-    * Returns wether this cluster node is responsible for updating 
+    * Returns wether this cluster node is responsible for updating
     * this ApplicationData object.
     */
    bool isLocal()
    {
       return(mAppData->isLocal());
    }
-   
+
    TYPE* operator->()
-   { return mAppData.operator->(); }
-   
+   {
+      return mAppData.operator->();
+   }
+
    TYPE& operator*()
-   { return mAppData.operator*(); }
+   {
+      return mAppData.operator*();
+   }
+
 #ifndef _MSC_VER
-private:      
+private:
    // Make sure that we are wrapping a type that is derived from SerializableObject.
    BOOST_STATIC_ASSERT((::boost::is_base_and_derived<vpr::SerializableObject,TYPE>::value));
 #endif
+
 private:
    AppDataPtr mAppData;
 };
-   
+
 } // end namespace cluster
 
 #endif
