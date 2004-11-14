37e6a57dd6768b856e63b644ef8d444fe9a82a8e
Patrick Hartling <patrick.hartling@gmail.com>
2004-11-14 18:31:16 +0000

    Added docstrings.  The PyJuggler API is now fully documented.  Whew!
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16180 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/src/osgapp/_OsgApp.cpp
+++ PyJuggler/src/osgapp/_OsgApp.cpp
@@ -552,35 +552,264 @@ struct vrj_OsgApp_Wrapper: vrj::OsgApp
 // Module ======================================================================
 void _Export_OsgApp()
 {
-    class_< vrj::OsgApp, bases< vrj::GlApp >, boost::noncopyable, pyj::vrj_OsgApp_Wrapper >("OsgApp", init<  >())
+    class_< vrj::OsgApp, bases< vrj::GlApp >, boost::noncopyable, pyj::vrj_OsgApp_Wrapper >("OsgApp",
+         "vrj.OsgApp encapsulates an Open Scene Graph (OSG) application.\n"
+         "This defines the base class from which OSG-based application\n"
+         "objects should be derived.  It makes use of the OpenGL Draw\n"
+         "Manager.\n\n"
+         "To use OSG with Python and this class, use PyOSG.  PyOSG is also\n"
+         "written using Boost.Python, so instances of PyOSG objects can be\n"
+         "passed into methods of this class without compatibility problems.\n\n"
+         "See: vrj.GlApp"
+         ,
+         init<  >(
+            "__init__()\n"
+            "Default constructor.  Uses vrj.Kernel.instance().\n\n"
+            "__init__(kernel)\n"
+            "Arguments:\n"
+            "kernel -- The vrj.Kernel instance that is active (so the\n"
+            "          application has easy access to the kernel)."
+         )
+        )
         .def(init< vrj::Kernel* >())
         .def_readwrite("mKernel", &vrj::App::mKernel)
         .def_readwrite("mHaveFocus", &vrj::App::mHaveFocus)
-        .def("initScene", pure_virtual(&vrj::OsgApp::initScene))
-        .def("getScene", pure_virtual(&vrj::OsgApp::getScene), return_internal_reference< 1 >())
-//        .def("configSceneView", &vrj::OsgApp::configSceneView, &pyj::vrj_OsgApp_Wrapper::default_configSceneView)
-        .def("draw", (void (vrj::OsgApp::*)() )&vrj::OsgApp::draw, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_draw)
-        .def("init", (void (vrj::OsgApp::*)() )&vrj::OsgApp::init, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_init)
-        .def("contextInit", (void (vrj::OsgApp::*)() )&vrj::OsgApp::contextInit, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextInit)
-        .def("contextClose", (void (vrj::OsgApp::*)() )&vrj::OsgApp::contextClose, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextClose)
-        .def("contextPreDraw", (void (vrj::OsgApp::*)() )&vrj::OsgApp::contextPreDraw, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextPreDraw)
-        .def("bufferPreDraw", (void (vrj::OsgApp::*)() )&vrj::OsgApp::bufferPreDraw, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_bufferPreDraw)
-        .def("pipePreDraw", (void (vrj::OsgApp::*)() )&vrj::OsgApp::pipePreDraw, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_pipePreDraw)
-        .def("contextPostDraw", (void (vrj::GlApp::*)() )&vrj::GlApp::contextPostDraw, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextPostDraw)
-        .def("apiInit", (void (vrj::App::*)() )&vrj::App::apiInit, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_apiInit)
-        .def("exit", (void (vrj::App::*)() )&vrj::App::exit, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_exit)
-        .def("preFrame", (void (vrj::App::*)() )&vrj::App::preFrame, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_preFrame)
-        .def("latePreFrame", (void (vrj::App::*)() )&vrj::App::latePreFrame, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_latePreFrame)
-        .def("intraFrame", (void (vrj::App::*)() )&vrj::App::intraFrame, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_intraFrame)
-        .def("postFrame", (void (vrj::App::*)() )&vrj::App::postFrame, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_postFrame)
-        .def("reset", (void (vrj::App::*)() )&vrj::App::reset, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_reset)
-        .def("focusChanged", (void (vrj::App::*)() )&vrj::App::focusChanged, (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_focusChanged)
-        .def("getDrawScaleFactor", (float (vrj::App::*)() )&vrj::App::getDrawScaleFactor, (float (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_getDrawScaleFactor)
-        .def("configCanHandle", (bool (vrj::App::*)(jccl::ConfigElementPtr) )&vrj::App::configCanHandle, (bool (pyj::vrj_OsgApp_Wrapper::*)(jccl::ConfigElementPtr))&pyj::vrj_OsgApp_Wrapper::default_configCanHandle)
-        .def("depSatisfied", (bool (vrj::App::*)() )&vrj::App::depSatisfied, (bool (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_depSatisfied)
-        .def("configProcessPending", (int (jccl::ConfigElementHandler::*)() )&jccl::ConfigElementHandler::configProcessPending, (int (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_configProcessPending)
-        .def("haveFocus", &vrj::App::haveFocus)
-        .def("setFocus", &vrj::App::setFocus)
+        .def("initScene", pure_virtual(&vrj::OsgApp::initScene),
+             "initScene()\n"
+             "Initializes the scene.  Called by init() to initialize the\n"
+             "scene."
+         )
+        .def("getScene", pure_virtual(&vrj::OsgApp::getScene),
+             return_internal_reference< 1 >(), 
+             "getScene() -> osg.Group object\n"
+             "Gets the root fo the scene to render.  Called each frame to\n"
+             "get teh current scene to render."
+         )
+/*
+        .def("configSceneView", &vrj::OsgApp::configSceneView,
+             &pyj::vrj_OsgApp_Wrapper::default_configSceneView,
+             "configSceneView(newSceneVeiwer)\n"
+             "Configures newly created scene viewers.  This is called\n"
+             "immediately after a new scene viewer is created for a context.\n"
+             "This is the place to configure application background colors\n"
+             "and other viewer-specific information.\n\n"
+             "Arguments:\n"
+             "newSceneViewer -- An osgUtil.SceneView object that is the\n"
+             "                  newly created scene view."
+             )
+*/
+        .def("draw", (void (vrj::OsgApp::*)() )&vrj::OsgApp::draw,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_draw,
+             "draw()\n"
+             "Function to draw the scene.  Override this method with great\n"
+             "create.  All the logic to handle multi-pipe rendering and\n"
+             "other VR Juggler features happens here.\n\n"
+             "Pre-conditions:\n"
+             "OpenGL state has correct transformation and buffer selected.\n\n"
+             "Post-conditions:\n"
+             "The current sceen has been drawn."
+             )
+        .def("init", (void (vrj::OsgApp::*)() )&vrj::OsgApp::init,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_init,
+             "init()\n"
+             "Application initialization function.  Execute any\n"
+             "initialization needed before the graphics API is started.  If\n"
+             "this method is overridden, it must be called by the overriding\n"
+             "version.  This calls initScene(), which is used to set up this\n"
+             "application object's scene graph.\n\n"
+             "Note:\n"
+             "Derived classes MUST call the base class version of this\n"
+             "method."
+         )
+        .def("contextInit",
+             (void (vrj::OsgApp::*)() )&vrj::OsgApp::contextInit,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextInit,
+             "contextInit()\n"
+             "Function that is called immediately after a new context is\n"
+             "created.  Use this function to create context-specific data\n"
+             "structures such as display lists and texture objects that are\n"
+             "known to be required when the context is created.\n\n"
+             "Pre-condition:\n"
+             "The OpenGL context has been set to the new context.\n\n"
+             "Post-condition:\n"
+             "The application has completed context-specific initialization."
+         )
+        .def("contextClose",
+             (void (vrj::OsgApp::*)() )&vrj::OsgApp::contextClose,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextClose,
+             "contextClose()\n"
+             "Function that is called immediately before a context is closed.\n"
+             "Use this function to clean up any context-specific data\n"
+             "structures."
+         )
+        .def("contextPreDraw",
+             (void (vrj::OsgApp::*)() )&vrj::OsgApp::contextPreDraw,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextPreDraw,
+             "contextPreDraw()\n"
+             "Function that is called upon entry into the context before\n"
+             "rendering begins.  This can be used to allocate\n"
+             "context-specific data dynamically.\n\n"
+             "Pre-conditions:\n"
+             "The OpenGL context has been set to the context for drawing.\n\n"
+             "Post-conditins:\n"
+             "The application object has executed any commands that need to\n"
+             "be executed only once per context, per frame.\n\n"
+             "This function can be used for things that need to happen every\n"
+             "frame but only once per context."
+         )
+        .def("bufferPreDraw",
+             (void (vrj::OsgApp::*)() )&vrj::OsgApp::bufferPreDraw,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_bufferPreDraw,
+             "bufferPreDraw()\n"
+             "Function that is called once for each frame buffer of an\n"
+             "OpenGL context.  This function is executed after contextInit()\n"
+             "(if neede) but before contextPreDraw().  It is called once\n"
+             "per frame buffer (see note below).\n\n"
+             "Pre-conditions:\n"
+             "The OpenGL context has been set to the context for drawing.\n\n"
+             "Post-conditions:\n"
+             "The application object has executed any commands that need to\n"
+             "be executed once per context, per buffer, per frame.\n\n"
+             "Note:\n"
+             "This function is designed to be used when some task must be\n"
+             "performed only once per frame buffer (i.e., once for the left\n"
+             "tbuffer, once for the right buffer).  For example, the OpenGL\n"
+             "clear color should be defined and glClear(GL_COLOR_BUFFER_BIT)\n"
+             "should be called in this method."
+         )
+        .def("pipePreDraw",
+             (void (vrj::OsgApp::*)() )&vrj::OsgApp::pipePreDraw,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_pipePreDraw,
+             "pipePreDraw()\n"
+             "Function that is called at the beginning of the drawing of eacn\n"
+             "pipe.\n\n"
+             "Pre-conditions:\n"
+             "The library is preparing to render all windows on a given\n"
+             "pipe.\n\n"
+             "Post-conditions:\n"
+             "Any pre-pipe user calls have been done.\n\n"
+             "Note:\n"
+             "Currently the OpenGL context is not set when this function is\n"
+             "called.  This is a TEST function.  USE AT YOUR OWN RISK!"
+         )
+        .def("contextPostDraw",
+             (void (vrj::GlApp::*)() )&vrj::GlApp::contextPostDraw,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_contextPostDraw,
+             "contextPostDraw()\n"
+             "Function that is called upon exit of the context after\n"
+             "rendering\n\n"
+             "Pre-conditions:\n"
+             "The OpenGL context has been set to the context for drawing."
+         )
+        .def("apiInit", (void (vrj::App::*)() )&vrj::App::apiInit,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_apiInit,
+             "apiInit()\n"
+             "Application graphics API initialization function.  Execute any\n"
+             "initialization needed after the graphics API is started but\n"
+             "before the Draw Manager starts the rendering loop(s)."
+         )
+        .def("exit", (void (vrj::App::*)() )&vrj::App::exit,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_exit,
+             "exit()\n"
+             "Executes any final clean-up needed for the application before\n"
+             "exiting."
+         )
+        .def("preFrame", (void (vrj::App::*)() )&vrj::App::preFrame,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_preFrame,
+             "preFrame()\n"
+             "Function called before the Juggler frame starts.  This is\n"
+             "called after input device updates but before the start of a\n"
+             "new frame."
+         )
+        .def("latePreFrame", (void (vrj::App::*)() )&vrj::App::latePreFrame,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_latePreFrame,
+             "latePreFrame()\n"
+             "Function called after preFrame() and application-specific data\n"
+             "synchronization (in a cluster conifguration) but before the\n"
+             "start of a new frame.\n\n"
+             "Note:\n"
+             "This is required because we cannot update data during the\n"
+             "rendering process since it might be using multiple threads."
+         )
+        .def("intraFrame", (void (vrj::App::*)() )&vrj::App::intraFrame,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_intraFrame,
+             "intraFrame()\n"
+             "Function called during the application's drawing time."
+         )
+        .def("postFrame", (void (vrj::App::*)() )&vrj::App::postFrame,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_postFrame,
+             "postFrame()\n"
+             "Function alled before updating input devices but after the\n"
+             "frame is complete."
+         )
+        .def("reset", (void (vrj::App::*)() )&vrj::App::reset,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_reset,
+             "reset()\n"
+             "Resets the application.  This is used when the kernel (or\n"
+             "applications would like this application to reset to its\n"
+             "initial state."
+         )
+        .def("focusChanged", (void (vrj::App::*)() )&vrj::App::focusChanged,
+             (void (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_focusChanged,
+             "focusChanged()\n"
+             "Called when the focus state changes."
+         )
+        .def("getDrawScaleFactor",
+             (float (vrj::App::*)() )&vrj::App::getDrawScaleFactor,
+             (float (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_getDrawScaleFactor,
+             "getDrawScaleFactor() -> float\n"
+             "Returns the scale factor to convert from Juggler units\n"
+             "(meters) to application units.  Internally, VR Juggler stores\n"
+             "and processes all position values in meterrs.  The scale\n"
+             "factor returned by this method is used by VR Juggler to scale\n"
+             "the rendering state from meters to whatever units this\n"
+             "application wants to use.  For example, to render in feet,\n"
+             "return 3.28 (gadget.PositionUnitConversion.ConvertToFeet)."
+         )
+        .def("configCanHandle",
+             (bool (vrj::App::*)(jccl::ConfigElementPtr) )&vrj::App::configCanHandle,
+             (bool (pyj::vrj_OsgApp_Wrapper::*)(jccl::ConfigElementPtr))&pyj::vrj_OsgApp_Wrapper::default_configCanHandle,
+             "configCanHandle(element) -> Boolean\n"
+             "Defaults to handling nothing.\n\n"
+             "Arguments:\n"
+             "element -- An instance of jccl.ConfigElement."
+         )
+        .def("depSatisfied", (bool (vrj::App::*)() )&vrj::App::depSatisfied,
+             (bool (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_depSatisfied,
+             "depSatisfied() -> Boolean\n"
+             "Are any application dependencies satisified?  If this\n"
+             "application requires anything special of the system for\n"
+             "successful initialization, check it here.  If the return value\n"
+             "is False, then this application will not be started yet.  If\n"
+             "the return value is True, this application will be allowed to\n"
+             "enter the system."
+         )
+        .def("configProcessPending",
+             (int (jccl::ConfigElementHandler::*)() )&jccl::ConfigElementHandler::configProcessPending,
+             (int (pyj::vrj_OsgApp_Wrapper::*)())&pyj::vrj_OsgApp_Wrapper::default_configProcessPending,
+             "configProcessPending() -> int\n"
+             "Inherited from jccl.ConfigElementHandler and not overridden."
+         )
+        .def("haveFocus", &vrj::App::haveFocus,
+             "haveFocus() -> Boolean\n"
+             "Does this application currently have focus?  If an application\n"
+             "has focus, the user may be attempting to interact with it, so\n"
+             "the application should process input.  If not, the user is\n"
+             "not interacting with it, so ignore all input.  However, the\n"
+             "user may still be viewing the application, so render, update\n"
+             "animations, etc.  This is akin to the way that a user can only\n"
+             "interact with a GUI window that has focus."
+         )
+        .def("setFocus", &vrj::App::setFocus,
+             "setFocus(newState)\n"
+             "Sets the focus state.\n\n"
+             "Post-condition:\n"
+             "If the focus state has changed, then focusChanged() is\n"
+             "called.\n\n"
+             "Arguments:\n"
+             "newState -- A Boolean value indicating whether this application\n"
+             "            now has focus."
+         )
     ;
 
 }
--- PyJuggler/src/osgapp/__osgapp.cpp
+++ PyJuggler/src/osgapp/__osgapp.cpp
@@ -15,5 +15,17 @@ void _Export_OsgApp();
 // Module ======================================================================
 BOOST_PYTHON_MODULE(__osgapp)
 {
+    boost::python::scope().attr("__doc__") =
+       "PyJuggler.__osgapp is an extension module written in Boost.Python\n"
+       "that exposes the C++ class vrj::OsgApp.  Upon successful loading,\n"
+       "the symbols of this module are imported into the PyJuggler.vrj\n"
+       "namespace for use in the authoring of VR Juggler application\n"
+       "objects based on Open Scene Graph.  To accomplish this, PyOSG must\n"
+       "be used together with vrj.OsgApp.\n\n"
+       "Refer to the VR Juggler 2.0 C++ documentation for general usage\n"
+       "details:\n"
+       "   http://www.vrjuggler.org/vrjuggler/docs.php"
+    ;
+
     _Export_OsgApp();
 }
