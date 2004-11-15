055242755a789302420581e434ed644c3554261e
Patrick Hartling <patrick.hartling@gmail.com>
2004-11-15 23:39:52 +0000

    Replaced the interesting code with a more brute-force approach that actually works (through a lot of code duplication).
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16196 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/src/container_conversions.h
+++ PyJuggler/src/container_conversions.h
@@ -16,7 +16,7 @@ namespace pyj
 
 // This is adapted from scitbx::boost_python::container_conversions::to_tuple,
 // the source for which can be found at http://cctbx.sourceforge.net/
-template<typename ContainerType, bool hasCopy = true>
+template<typename ContainerType>
 struct to_tuple
 {
    static PyObject* convert(ContainerType const& a)
@@ -25,34 +25,45 @@ struct to_tuple
       typedef typename ContainerType::const_iterator const_iter;
       for ( const_iter p = a.begin(); p != a.end(); ++p )
       {
-         if ( hasCopy )
-         {
-            result.append(boost::python::object(*p));
-         }
-         else
-         {
-            result.append(boost::python::object(boost::python::ptr(*p)));
-         }
+         result.append(boost::python::object(*p));
+      }
+      return boost::python::incref(boost::python::tuple(result).ptr());
+   }
+};
+
+template<typename ContainerType>
+struct to_tuple_noncopyable
+{
+   static PyObject* convert(ContainerType const& a)
+   {
+      boost::python::list result;
+      typedef typename ContainerType::const_iterator const_iter;
+      for ( const_iter p = a.begin(); p != a.end(); ++p )
+      {
+         result.append(boost::python::object(boost::python::ptr(*p)));
       }
       return boost::python::incref(boost::python::tuple(result).ptr());
    }
 };
 
 template<typename T>
-struct std_vector_to_tuple
+struct std_vector_copyable_to_tuple
 {
-   // Holy functional programming, Batman!  This uses the Boost Metaprogramming
-   // Library.
-   std_vector_to_tuple()
+   std_vector_copyable_to_tuple()
    {
       boost::python::to_python_converter<
-         std::vector<T>,
-         to_tuple<
-            std::vector<T>,
-            boost::has_trivial_copy<
-               typename boost::remove_pointer<T>::type
-            >::value
-         >
+         std::vector<T>, to_tuple< std::vector<T> >
+      >();
+   }
+};
+
+template<typename T>
+struct std_vector_noncopyable_to_tuple
+{
+   std_vector_noncopyable_to_tuple()
+   {
+      boost::python::to_python_converter<
+         std::vector<T>, to_tuple_noncopyable< std::vector<T> >
       >();
    }
 };
