c43db996a01a3665fc716d1e4f96ac518aba7454
Patrick Hartling <patrick.hartling@gmail.com>
2004-11-18 05:41:54 +0000

    Note that the interface of this class should be considered alpha and that it may change in a release of Gadgeteer after the 1.0 series (such as 1.2).
    Suggested by:	allenb
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16225 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/gadget/Type/StringProxy.h
+++ modules/gadgeteer/gadget/Type/StringProxy.h
@@ -51,6 +51,13 @@ namespace gadget
  * devices that only return a single sub-device's amount of data (one
  * std::string).
  *
+ * @note This interface should be considered as being in an "alpha" state.
+ *       String-oriented device types are very new to Gadgeteer, and they
+ *       have not have the same amount of time to mature as the other
+ *       device types (which have been around since VR Juggler 1.0).  This
+ *       interface may change in a future release of Gadgeteer (such as
+ *       Version 1.2).
+ *
  * @see gagdet::String
  */
 class GADGET_CLASS_API StringProxy : public TypedProxy<String>
