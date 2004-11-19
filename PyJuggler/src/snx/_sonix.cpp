eba54464842410b0dcda92d626169a0ca1fe67fd
Patrick Hartling <patrick.hartling@gmail.com>
2004-11-19 05:23:33 +0000

    Updated to Sonix 0.5.0.  Bumped version to 0.16.1.  This does not change anything in the interfaces defined in the PyJuggler.snx extension module.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16242 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- PyJuggler/src/snx/_sonix.cpp
+++ PyJuggler/src/snx/_sonix.cpp
@@ -19,21 +19,21 @@ using namespace boost::python;
 // Declarations ================================================================
 namespace pyj {
 
-struct sonix_Wrapper: sonix
+struct snx_sonix_Wrapper: snx::sonix
 {
-    sonix_Wrapper(PyObject* self_, const sonix& p0):
-        sonix(p0), self(self_) {}
+    snx_sonix_Wrapper(PyObject* self_, const snx::sonix& p0):
+        snx::sonix(p0), self(self_) {}
 
     void trigger(const std::string& p0, const int& p1) {
         call_method< void >(self, "trigger", p0, p1);
     }
 
     void default_trigger_1(const std::string& p0) {
-        sonix::trigger(p0);
+        snx::sonix::trigger(p0);
     }
 
     void default_trigger_2(const std::string& p0, const int& p1) {
-        sonix::trigger(p0, p1);
+        snx::sonix::trigger(p0, p1);
     }
 
     bool isPlaying(const std::string& p0) {
@@ -41,7 +41,7 @@ struct sonix_Wrapper: sonix
     }
 
     bool default_isPlaying(const std::string& p0) {
-        return sonix::isPlaying(p0);
+        return snx::sonix::isPlaying(p0);
     }
 
     void setRetriggerable(const std::string& p0, bool p1) {
@@ -49,7 +49,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setRetriggerable(const std::string& p0, bool p1) {
-        sonix::setRetriggerable(p0, p1);
+        snx::sonix::setRetriggerable(p0, p1);
     }
 
     bool isRetriggerable(const std::string& p0) {
@@ -57,7 +57,7 @@ struct sonix_Wrapper: sonix
     }
 
     bool default_isRetriggerable(const std::string& p0) {
-        return sonix::isRetriggerable(p0);
+        return snx::sonix::isRetriggerable(p0);
     }
 
     void stop(const std::string& p0) {
@@ -65,7 +65,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_stop(const std::string& p0) {
-        sonix::stop(p0);
+        snx::sonix::stop(p0);
     }
 
     void pause(const std::string& p0) {
@@ -73,7 +73,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_pause(const std::string& p0) {
-        sonix::pause(p0);
+        snx::sonix::pause(p0);
     }
 
     void unpause(const std::string& p0) {
@@ -81,7 +81,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_unpause(const std::string& p0) {
-        sonix::unpause(p0);
+        snx::sonix::unpause(p0);
     }
 
     bool isPaused(const std::string& p0) {
@@ -89,7 +89,7 @@ struct sonix_Wrapper: sonix
     }
 
     bool default_isPaused(const std::string& p0) {
-        return sonix::isPaused(p0);
+        return snx::sonix::isPaused(p0);
     }
 
     void setAmbient(const std::string& p0, bool p1) {
@@ -97,11 +97,11 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setAmbient_1(const std::string& p0) {
-        sonix::setAmbient(p0);
+        snx::sonix::setAmbient(p0);
     }
 
     void default_setAmbient_2(const std::string& p0, bool p1) {
-        sonix::setAmbient(p0, p1);
+        snx::sonix::setAmbient(p0, p1);
     }
 
     bool isAmbient(const std::string& p0) {
@@ -109,7 +109,7 @@ struct sonix_Wrapper: sonix
     }
 
     bool default_isAmbient(const std::string& p0) {
-        return sonix::isAmbient(p0);
+        return snx::sonix::isAmbient(p0);
     }
 
     void setPitchBend(const std::string& p0, float p1) {
@@ -117,7 +117,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setPitchBend(const std::string& p0, float p1) {
-        sonix::setPitchBend(p0, p1);
+        snx::sonix::setPitchBend(p0, p1);
     }
 
     void setVolume(const std::string& p0, float p1) {
@@ -125,7 +125,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setVolume(const std::string& p0, float p1) {
-        sonix::setVolume(p0, p1);
+        snx::sonix::setVolume(p0, p1);
     }
 
     void setCutoff(const std::string& p0, float p1) {
@@ -133,7 +133,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setCutoff(const std::string& p0, float p1) {
-        sonix::setCutoff(p0, p1);
+        snx::sonix::setCutoff(p0, p1);
     }
 
     void setPosition(const std::string& p0, const float& p1, const float& p2, const float& p3) {
@@ -141,7 +141,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setPosition(const std::string& p0, const float& p1, const float& p2, const float& p3) {
-        sonix::setPosition(p0, p1, p2, p3);
+        snx::sonix::setPosition(p0, p1, p2, p3);
     }
 
     void getPosition(const std::string& p0, float& p1, float& p2, float& p3) {
@@ -149,7 +149,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_getPosition(const std::string& p0, float& p1, float& p2, float& p3) {
-        sonix::getPosition(p0, p1, p2, p3);
+        snx::sonix::getPosition(p0, p1, p2, p3);
     }
 
     void setListenerPosition(const gmtl::Matrix44f& p0) {
@@ -157,7 +157,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_setListenerPosition(const gmtl::Matrix44f& p0) {
-        sonix::setListenerPosition(p0);
+        snx::sonix::setListenerPosition(p0);
     }
 
     void getListenerPosition(gmtl::Matrix44f& p0) {
@@ -165,7 +165,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_getListenerPosition(gmtl::Matrix44f& p0) {
-        sonix::getListenerPosition(p0);
+        snx::sonix::getListenerPosition(p0);
     }
 
     void changeAPI(const std::string& p0) {
@@ -173,7 +173,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_changeAPI(const std::string& p0) {
-        sonix::changeAPI(p0);
+        snx::sonix::changeAPI(p0);
     }
 
     void configure(const snx::SoundAPIInfo& p0) {
@@ -181,7 +181,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_configure(const snx::SoundAPIInfo& p0) {
-        sonix::configure(p0);
+        snx::sonix::configure(p0);
     }
 
     void configure(const std::string& p0, const snx::SoundInfo& p1) {
@@ -189,7 +189,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_configure(const std::string& p0, const snx::SoundInfo& p1) {
-        sonix::configure(p0, p1);
+        snx::sonix::configure(p0, p1);
     }
 
     void remove(const std::string p0) {
@@ -197,7 +197,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_remove(const std::string p0) {
-        sonix::remove(p0);
+        snx::sonix::remove(p0);
     }
 
     void step(const float& p0) {
@@ -205,7 +205,7 @@ struct sonix_Wrapper: sonix
     }
 
     void default_step(const float& p0) {
-        sonix::step(p0);
+        snx::sonix::step(p0);
     }
 
     PyObject* self;
@@ -218,32 +218,32 @@ struct sonix_Wrapper: sonix
 // Module ======================================================================
 void _Export_sonix()
 {
-    class_< sonix, pyj::sonix_Wrapper, boost::noncopyable >("sonix", no_init)
-        .def("trigger", &sonix::trigger, &pyj::sonix_Wrapper::default_trigger_2)
-        .def("trigger", &pyj::sonix_Wrapper::default_trigger_1)
-        .def("isPlaying", &sonix::isPlaying, &pyj::sonix_Wrapper::default_isPlaying)
-        .def("setRetriggerable", &sonix::setRetriggerable, &pyj::sonix_Wrapper::default_setRetriggerable)
-        .def("isRetriggerable", &sonix::isRetriggerable, &pyj::sonix_Wrapper::default_isRetriggerable)
-        .def("stop", &sonix::stop, &pyj::sonix_Wrapper::default_stop)
-        .def("pause", &sonix::pause, &pyj::sonix_Wrapper::default_pause)
-        .def("unpause", &sonix::unpause, &pyj::sonix_Wrapper::default_unpause)
-        .def("isPaused", &sonix::isPaused, &pyj::sonix_Wrapper::default_isPaused)
-        .def("setAmbient", &sonix::setAmbient, &pyj::sonix_Wrapper::default_setAmbient_2)
-        .def("setAmbient", &pyj::sonix_Wrapper::default_setAmbient_1)
-        .def("isAmbient", &sonix::isAmbient, &pyj::sonix_Wrapper::default_isAmbient)
-        .def("setPitchBend", &sonix::setPitchBend, &pyj::sonix_Wrapper::default_setPitchBend)
-        .def("setVolume", &sonix::setVolume, &pyj::sonix_Wrapper::default_setVolume)
-        .def("setCutoff", &sonix::setCutoff, &pyj::sonix_Wrapper::default_setCutoff)
-        .def("setPosition", &sonix::setPosition, &pyj::sonix_Wrapper::default_setPosition)
-        .def("getPosition", &sonix::getPosition, &pyj::sonix_Wrapper::default_getPosition)
-        .def("setListenerPosition", &sonix::setListenerPosition, &pyj::sonix_Wrapper::default_setListenerPosition)
-        .def("getListenerPosition", &sonix::getListenerPosition, &pyj::sonix_Wrapper::default_getListenerPosition)
-        .def("changeAPI", &sonix::changeAPI, &pyj::sonix_Wrapper::default_changeAPI)
-        .def("configure", (void (sonix::*)(const snx::SoundAPIInfo&) )&sonix::configure, (void (pyj::sonix_Wrapper::*)(const snx::SoundAPIInfo&))&pyj::sonix_Wrapper::default_configure)
-        .def("configure", (void (sonix::*)(const std::string&, const snx::SoundInfo&) )&sonix::configure, (void (pyj::sonix_Wrapper::*)(const std::string&, const snx::SoundInfo&))&pyj::sonix_Wrapper::default_configure)
-        .def("remove", &sonix::remove, &pyj::sonix_Wrapper::default_remove)
-        .def("step", &sonix::step, &pyj::sonix_Wrapper::default_step)
-        .def("instance", &sonix::instance, return_value_policy< reference_existing_object >())
+    class_< snx::sonix, pyj::snx_sonix_Wrapper, boost::noncopyable >("sonix", no_init)
+        .def("trigger", &snx::sonix::trigger, &pyj::snx_sonix_Wrapper::default_trigger_2)
+        .def("trigger", &pyj::snx_sonix_Wrapper::default_trigger_1)
+        .def("isPlaying", &snx::sonix::isPlaying, &pyj::snx_sonix_Wrapper::default_isPlaying)
+        .def("setRetriggerable", &snx::sonix::setRetriggerable, &pyj::snx_sonix_Wrapper::default_setRetriggerable)
+        .def("isRetriggerable", &snx::sonix::isRetriggerable, &pyj::snx_sonix_Wrapper::default_isRetriggerable)
+        .def("stop", &snx::sonix::stop, &pyj::snx_sonix_Wrapper::default_stop)
+        .def("pause", &snx::sonix::pause, &pyj::snx_sonix_Wrapper::default_pause)
+        .def("unpause", &snx::sonix::unpause, &pyj::snx_sonix_Wrapper::default_unpause)
+        .def("isPaused", &snx::sonix::isPaused, &pyj::snx_sonix_Wrapper::default_isPaused)
+        .def("setAmbient", &snx::sonix::setAmbient, &pyj::snx_sonix_Wrapper::default_setAmbient_2)
+        .def("setAmbient", &pyj::snx_sonix_Wrapper::default_setAmbient_1)
+        .def("isAmbient", &snx::sonix::isAmbient, &pyj::snx_sonix_Wrapper::default_isAmbient)
+        .def("setPitchBend", &snx::sonix::setPitchBend, &pyj::snx_sonix_Wrapper::default_setPitchBend)
+        .def("setVolume", &snx::sonix::setVolume, &pyj::snx_sonix_Wrapper::default_setVolume)
+        .def("setCutoff", &snx::sonix::setCutoff, &pyj::snx_sonix_Wrapper::default_setCutoff)
+        .def("setPosition", &snx::sonix::setPosition, &pyj::snx_sonix_Wrapper::default_setPosition)
+        .def("getPosition", &snx::sonix::getPosition, &pyj::snx_sonix_Wrapper::default_getPosition)
+        .def("setListenerPosition", &snx::sonix::setListenerPosition, &pyj::snx_sonix_Wrapper::default_setListenerPosition)
+        .def("getListenerPosition", &snx::sonix::getListenerPosition, &pyj::snx_sonix_Wrapper::default_getListenerPosition)
+        .def("changeAPI", &snx::sonix::changeAPI, &pyj::snx_sonix_Wrapper::default_changeAPI)
+        .def("configure", (void (snx::sonix::*)(const snx::SoundAPIInfo&) )&snx::sonix::configure, (void (pyj::snx_sonix_Wrapper::*)(const snx::SoundAPIInfo&))&pyj::snx_sonix_Wrapper::default_configure)
+        .def("configure", (void (snx::sonix::*)(const std::string&, const snx::SoundInfo&) )&snx::sonix::configure, (void (pyj::snx_sonix_Wrapper::*)(const std::string&, const snx::SoundInfo&))&pyj::snx_sonix_Wrapper::default_configure)
+        .def("remove", &snx::sonix::remove, &pyj::snx_sonix_Wrapper::default_remove)
+        .def("step", &snx::sonix::step, &pyj::snx_sonix_Wrapper::default_step)
+        .def("instance", &snx::sonix::instance, return_value_policy< reference_existing_object >())
         .staticmethod("instance")
     ;
 
