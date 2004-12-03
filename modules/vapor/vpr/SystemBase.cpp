c6f9bbee5a56acd2cc280db95ead4432d62d5101
Patrick Hartling <patrick.hartling@gmail.com>
2004-12-03 00:01:01 +0000

    Fixed coding standard violations and wrapped long lines.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16307 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/SystemBase.cpp
+++ modules/vapor/vpr/SystemBase.cpp
@@ -67,7 +67,6 @@ namespace
 std::string demangleTraceString(char* traceLine)
 {
 #ifdef USE_CXA_DEMANGLE
-
    // Try to extract the mangled name from the line (if it exists)
    // and replace it with a demangled version of the name.
    // Example: build.linux/stuff/classfile(_ZN4vpr11Someing33methodEv+0xd3) [0x80cfa29]
@@ -78,12 +77,16 @@ std::string demangleTraceString(char* traceLine)
    unsigned start(std::string::npos), end(std::string::npos);
    start = trace_line.find("(_");
    if(std::string::npos != start)
-   { end = trace_line.find_first_of("+)",start); }
+   {
+      end = trace_line.find_first_of("+)",start);
+   }
+
    if(std::string::npos != end)
    {
       mangled_name.assign(trace_line, start+1, end-start-1);
       int status;
-      char* demangled_buf = abi::__cxa_demangle(mangled_name.c_str(), NULL, NULL, &status);
+      char* demangled_buf = abi::__cxa_demangle(mangled_name.c_str(), NULL,
+                                                NULL, &status);
       if(0==status)
       {
          demangled_name = std::string(demangled_buf);
@@ -93,19 +96,24 @@ std::string demangleTraceString(char* traceLine)
       }
       else if(-1==status)
       {
-         //std::cerr << "vpr::SystemBase::dmangleTraceString: A memory allocation failiure occurred.\n";
+//         std::cerr << "vpr::SystemBase::dmangleTraceString: "
+//                   << "A memory allocation failiure occurred.\n";
       }
       else if(-2 == status)
       {
-         //std::cerr << "vpr::SystemBase::demangleTraceString: mangled_name is not a valid name under the C++ ABI mangling rules.\n";
+//         std::cerr << "vpr::SystemBase::demangleTraceString: mangled_name "
+//                   << "is not a valid name under the C++ ABI mangling '
+//                   << "rules.\n";
       }
       else if(-3 == status)
       {
-         //std::cerr << "vpr::SystemBase::demangleTraceString: One of the arguments is invalid.\n";
+//         std::cerr << "vpr::SystemBase::demangleTraceString: One of the "
+//                   << "arguments is invalid.\n";
       }
    }
 
-   //trace_line += std::string(" mangle:") + mangled_name + std::string(" demangled:") + demangled_name;
+//   trace_line += std::string(" mangle:") + mangled_name +
+//                 std::string(" demangled:") + demangled_name;
    return trace_line;
 
 #else
@@ -136,7 +144,8 @@ std::string SystemBase::getCallStack()
 
    for (size_t i = 0; i < size; ++i)
    {
-      trace_stream << "   " << i << ":" << demangleTraceString(strings[i]) << std::endl;
+      trace_stream << "   " << i << ":" << demangleTraceString(strings[i])
+                   << std::endl;
    }
 
    free(strings);
