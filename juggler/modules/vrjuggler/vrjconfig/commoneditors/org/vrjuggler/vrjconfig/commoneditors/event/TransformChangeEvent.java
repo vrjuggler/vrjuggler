086cb643bc6eeb41cd2e8da75983669454a72b13
Patrick Hartling <patrick.hartling@gmail.com>
2005-01-09 20:11:42 +0000

    First pass at implementing an editor for position transform filter config elements.  There is still a lot of work to be done on this, mostly relating to widget layout and rendering.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16542 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ juggler/modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/event/TransformChangeEvent.java
@@ -0,0 +1,68 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+package org.vrjuggler.vrjconfig.commoneditors.event;
+
+import java.util.EventObject;
+
+
+public class TransformChangeEvent
+   extends EventObject
+{
+   public TransformChangeEvent(Object source, double xRot, double yRot,
+                               double zRot)
+   {
+      super(source);
+      this.xRot = xRot;
+      this.yRot = yRot;
+      this.zRot = zRot;
+   }
+
+   public double getXRot()
+   {
+      return xRot;
+   }
+
+   public double getYRot()
+   {
+      return yRot;
+   }
+
+   public double getZRot()
+   {
+      return zRot;
+   }
+
+   private double xRot;
+   private double yRot;
+   private double zRot;
+}
