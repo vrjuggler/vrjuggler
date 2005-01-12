06d3ed8f148163949b75c7a4c89f3eefcc2c40ed
Patrick Hartling <patrick.hartling@gmail.com>
2005-01-12 23:06:21 +0000

    New feature:	Added a new Swing panel that provides an interface for 		editing any device proxy type.  Currently, editors exist 		for analog, digital, and position proxies.  The analog and 		digital proxies are the trivial cases.  The position proxy 		editor makes use of CoordinateFrameEditor and 		TransformTranslationEditor.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16581 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/ProxyTypeEditor.java
@@ -0,0 +1,10 @@
+package org.vrjuggler.vrjconfig.commoneditors;
+
+import org.vrjuggler.jccl.config.ConfigElement;
+import org.vrjuggler.jccl.config.ConfigContext;
+
+
+public interface ProxyTypeEditor
+{
+   public void setConfig(ConfigContext ctx, ConfigElement elt);
+}
