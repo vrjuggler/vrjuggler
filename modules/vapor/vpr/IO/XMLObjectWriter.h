a0b3fa44e1f00a309ead4a3e272cb9c305cd46ac
Patrick Hartling <patrick.hartling@gmail.com>
2005-01-18 04:49:31 +0000

    De-inline the method implementations.  This is being done for three reasons:
       1. There is no performance gain to be had from making virtual methods
          inline.
       2. If a polymorphic type is not compiled into the shared library where
          it is defined, there can be problems later with code linking against
          the shared library and using the polymorphic type.
       3. VPR already has CppDOM symbols compiled in, so there is no reason to
          have any of these methods be inlined in this manner.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16636 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vapor/vpr/IO/XMLObjectWriter.h
+++ modules/vapor/vpr/IO/XMLObjectWriter.h
@@ -43,13 +43,13 @@
 #define _VPR_XML_OBJECT_WRITER_H
 
 #include <vpr/vprConfig.h>
+
 #include <vector>
+#include <string>
+#include <cppdom/cppdom.h>
 
 #include <vpr/IO/ObjectWriter.h>
-#include <vpr/Util/Assert.h>
-#include <cppdom/cppdom.h>
-#include <string>
-#include <sstream>
+
 
 namespace vpr
 {
@@ -166,158 +166,7 @@ protected:
    //@}
 };
 
-inline std::vector<vpr::Uint8> XMLObjectWriter::getData()
-{
-   vprASSERT(mRootNode.get() != NULL);
-   std::stringstream oss;
-   mRootNode->save(oss, 0);
-   std::string str_rep = oss.str();
-   std::vector<vpr::Uint8> ret_buffer(str_rep.begin(), str_rep.end());
-   return ret_buffer;
-}
-
-// Starts a new section/element of name tagName.
-// When mCurNode is Null, then we need to allocated a new node in its place.
-// And also check the Root node to set it to (this is the first call to
-// beginTag).
-inline vpr::ReturnStatus XMLObjectWriter::beginTag(const std::string& tagName)
-{
-   cppdom::NodePtr new_node;
-
-   if(mCurNode == NULL)    // First node created, so initialize everything
-   {
-      cppdom::ContextPtr cxt(new cppdom::Context);
-      new_node = cppdom::NodePtr(new cppdom::Node(tagName, cxt));
-      vprASSERT(mRootNode.get() == NULL && "Tried to root the writer twice");
-      mRootNode = new_node;
-      mCurNode = new_node.get();
-   }
-   else                          // Create new child of current node
-   {
-      // Check for pending cdata to write
-      // - If there is some, then write it out
-      if(!mCurCData.empty())
-      {
-         mCurNode->setCdata(mCurCData);
-         mCurCData.clear();
-      }
-
-      // Add new child node
-      new_node = cppdom::NodePtr(new cppdom::Node(tagName, mRootNode->getContext()));
-      mCurNode->addChild(new_node);
-      mCurNode = new_node.get();
-   }
-   return vpr::ReturnStatus::Succeed;
-}
-
-// Ends the most recently named tag.
-// Close off the current node and set current to its parent.
-inline vpr::ReturnStatus XMLObjectWriter::endTag()
-{
-   vprASSERT(mCurNode != NULL);
-   //vprASSERT(mCurNode->getParent() != NULL);
-
-   // Write out the cdata
-   if(!mCurCData.empty())
-   {
-      mCurNode->setCdata(mCurCData);
-      mCurCData.clear();
-   }
-
-   cppdom::Node* cur_parent = mCurNode->getParent();     // Get cur node's parent node
-   mCurNode = cur_parent;                                // Set to new current node
-
-   // If it had some cdata, then get it to initialize the current cdata
-   // so we can write more data to it
-   if(NULL != mCurNode)
-   {
-      mCurCData = mCurNode->getCdata();
-   }
-
-   return vpr::ReturnStatus::Succeed;
-}
-
-// Starts an attribute of the name attributeName.
-inline vpr::ReturnStatus XMLObjectWriter::beginAttribute(const std::string& attributeName)
-{
-   // Make sure that we have not called beginAttribute without an endAttribute
-   vprASSERT(mCurAttribName.empty() && "Didn't close previous attribute");
-   vprASSERT(mCurAttribData.empty() && "There should not be any attribute data now.  It should have been flushed");   
-
-   mCurAttribName = attributeName;
-   mCurTarget = AttribTarget;
-   return vpr::ReturnStatus::Succeed;
-}
-
-// Ends the most recently named attribute.
-inline vpr::ReturnStatus XMLObjectWriter::endAttribute()
-{
-   vprASSERT(AttribTarget == mCurTarget);
-
-   mCurNode->setAttribute(mCurAttribName, mCurAttribData);
-   //std::cout << "Setting attrib:[" << mCurAttribName << "] value:[" << mCurAttribData << "]\n";
-
-   mCurAttribName.clear();
-   mCurAttribData.clear();
-
-   mCurTarget = CdataTarget;
-
-   return vpr::ReturnStatus::Succeed;
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeUint8(vpr::Uint8 val)
-{
-   // Cast to uint16 so it doesn't get written as a char
-   return writeValueStringRep(vpr::Uint16(val));
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeUint16(vpr::Uint16 val)
-{
-   return writeValueStringRep(val);
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeUint32(vpr::Uint32 val)
-{
-   return writeValueStringRep(val);
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeUint64(vpr::Uint64 val)
-{
-   return writeValueStringRep(val);
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeFloat(float val)
-{
-   return writeValueStringRep(val);
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeDouble(double val)
-{
-   return writeValueStringRep(val);
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeString(std::string val)
-{
-   if(AttribTarget == mCurTarget)
-   {
-      vprASSERT(mCurAttribData.empty() && "Can't add string to non-empty attribute");
-      mCurAttribData = val;
-   }
-   else
-   {
-      if(!mCurCData.empty())  // Add spacing
-         mCurCData += ' ';
-      mCurCData += '"' + val + '"';    // Add string in quotes
-   }
-
-   return vpr::ReturnStatus::Succeed;
-}
-
-inline vpr::ReturnStatus XMLObjectWriter::writeBool(bool val)
-{
-   return writeValueStringRep(val);
-}
-
 } // namespace vpr
+
 
 #endif
