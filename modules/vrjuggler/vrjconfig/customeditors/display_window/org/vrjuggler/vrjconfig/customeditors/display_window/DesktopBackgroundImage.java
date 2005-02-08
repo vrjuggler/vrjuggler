75ef07d503b9996e04dd02f0da91f9a116bc7120
Patrick Hartling <patrick.hartling@gmail.com>
2005-02-08 01:24:21 +0000

    Fixed indentation.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16763 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/customeditors/display_window/org/vrjuggler/vrjconfig/customeditors/display_window/DesktopBackgroundImage.java
+++ modules/vrjuggler/vrjconfig/customeditors/display_window/org/vrjuggler/vrjconfig/customeditors/display_window/DesktopBackgroundImage.java
@@ -98,29 +98,29 @@ public class DesktopBackgroundImage extends JComponent
 
    protected void drawBar(Graphics g, int x, int y, int w, int h)
    {
-           Color oldColor = g.getColor();
+      Color oldColor = g.getColor();
 
-           g.translate(x, y);
+      g.translate(x, y);
 
-           Color highlightOuterColor = getBackground().brighter().brighter();
-           Color highlightInnerColor = getBackground().brighter();
-           Color shadowOuterColor = getBackground().darker().darker();
-           Color shadowInnerColor = getBackground().darker();
+      Color highlightOuterColor = getBackground().brighter().brighter();
+      Color highlightInnerColor = getBackground().brighter();
+      Color shadowOuterColor = getBackground().darker().darker();
+      Color shadowInnerColor = getBackground().darker();
 
-           g.setColor(shadowInnerColor);
-           g.drawLine(1, 0, w-1, 0);
+      g.setColor(shadowInnerColor);
+      g.drawLine(1, 0, w-1, 0);
 
-           g.setColor(shadowOuterColor);
-           g.drawLine(2, 1, w-2, 1);
+      g.setColor(shadowOuterColor);
+      g.drawLine(2, 1, w-2, 1);
 
-           g.setColor(highlightOuterColor);
-           g.drawLine(0, -1, w-1, -1);
+      g.setColor(highlightOuterColor);
+      g.drawLine(0, -1, w-1, -1);
 
-           g.setColor(highlightInnerColor);
-           g.drawLine(2, -2, w-2, -2);
+      g.setColor(highlightInnerColor);
+      g.drawLine(2, -2, w-2, -2);
 
-           g.translate(-x, -y);
-           g.setColor(oldColor);
+      g.translate(-x, -y);
+      g.setColor(oldColor);
    }
 
    protected void paintComponent(Graphics g)
