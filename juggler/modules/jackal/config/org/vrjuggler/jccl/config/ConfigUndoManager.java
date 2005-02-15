17ebf972071965d2e7fffb96e0bab86a2a73bf12
Patrick Hartling <patrick.hartling@gmail.com>
2005-02-15 22:09:08 +0000

    New feature:	Added methods to org.vrjuggler.jccl.config.ConfigUndoManager 		that provide support for automamted multiple undo/redo 		operations.
    Bumped version to 0.92.3.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16851 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/jackal/config/org/vrjuggler/jccl/config/ConfigUndoManager.java
+++ juggler/modules/jackal/config/org/vrjuggler/jccl/config/ConfigUndoManager.java
@@ -62,12 +62,87 @@ public class ConfigUndoManager extends UndoManager
       super.undo();
       mUndoInProgress = false;
    }
-   
+
+   /**
+    * Undoes every edit posted since the last save.
+    *
+    * @since 0.92.3
+    */
+   public synchronized void undoAll() throws CannotUndoException
+   {
+      undoUntil(0);
+   }
+
+   /**
+    * Undoes every edit up until the specified save point.  The "save point"
+    * is a value of the edit offset since the last save queried sometime prior
+    * to invoking this method.  This is the inverse operation of redoUntil().
+    *
+    * @param savePoint  the "save point" to which the configuration state will
+    *                   be reset
+    *
+    * @see #getEditOffsetFromSave()
+    * @see #redoUntil(int)
+    *
+    * @since 0.92.3
+    */
+   public synchronized void undoUntil(int savePoint) throws CannotUndoException
+   {
+      mUndoInProgress = true;
+      while ( mEditOffsetFromSave > savePoint )
+      {
+         --mEditOffsetFromSave;
+         super.undo();
+      }
+      mUndoInProgress = false;
+   }
+
    public synchronized void redo() throws CannotUndoException
    {
       mUndoInProgress = true;
       ++mEditOffsetFromSave;
       super.redo();
+      mUndoInProgress = false;
+   }
+
+   /**
+    * Re-applies every edit that has been undone.
+    *
+    * @since 0.92.3
+    */
+   public synchronized void redoAll() throws CannotUndoException
+   {
+      mUndoInProgress = true;
+      while ( canRedo() )
+      {
+         ++mEditOffsetFromSave;
+         super.redo();
+      }
+      mUndoInProgress = false;
+   }
+
+   /**
+    * Re-applies every undone edit up until the specified save point.  The
+    * "save point" is a value of the edit offset since the last save queried
+    * sometime prior to invoking this method.  This is the inverse operation of
+    * undoUntil().
+    *
+    * @param savePoint  the "save point" to which the configuration state will
+    *                   be reset
+    *
+    * @see #getEditOffsetFromSave()
+    * @see #undoUntil(int)
+    *
+    * @since 0.92.3
+    */
+   public synchronized void redoUntil(int savePoint) throws CannotUndoException
+   {
+      mUndoInProgress = true;
+      while ( mEditOffsetFromSave < savePoint && canRedo() )
+      {
+         ++mEditOffsetFromSave;
+         super.redo();
+      }
       mUndoInProgress = false;
    }
 
@@ -81,6 +156,21 @@ public class ConfigUndoManager extends UndoManager
       return ( 0 != mEditOffsetFromSave );
    }
 
+   /**
+    * Returns the current edit offset since the last time a save operation
+    * was performed.  This can be used in conjunction with undoUntil() and
+    * redoUntil().
+    *
+    * @see #undoUntil(int)
+    * @see #redoUntil(int)
+    *
+    * @since 0.92.3
+    */
+   public int getEditOffsetFromSave()
+   {
+      return mEditOffsetFromSave;
+   }
+
    private boolean mUndoInProgress = false;
-   int mEditOffsetFromSave = 0;
+   private int mEditOffsetFromSave = 0;
 }
