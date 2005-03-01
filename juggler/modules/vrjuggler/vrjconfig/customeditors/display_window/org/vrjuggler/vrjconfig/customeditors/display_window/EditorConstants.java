6770f5109c6ed0ee4e6db6926ab1627b0c71a684
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-01 21:30:28 +0000

    Shift constants related to config element types and property names from org.vrjuggler.vrjconfig.customeditors.display_window.EditorConstants to org.vrjuggler.vrjconfig.commoneditors.EditorConstants.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@16941 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrjconfig/customeditors/display_window/org/vrjuggler/vrjconfig/customeditors/display_window/EditorConstants.java
+++ juggler/modules/vrjuggler/vrjconfig/customeditors/display_window/org/vrjuggler/vrjconfig/customeditors/display_window/EditorConstants.java
@@ -34,61 +34,10 @@ package org.vrjuggler.vrjconfig.customeditors.display_window;
 
 
 interface EditorConstants
+   extends org.vrjuggler.vrjconfig.commoneditors.EditorConstants
 {
    static final String RESOURCE_BASE =
       "org/vrjuggler/vrjconfig/customeditors/display_window";
 
    static final String IMAGE_BASE = RESOURCE_BASE + "/images";
-
-   static final String ALIAS_TYPE = "alias";
-
-   static final String ANALOG_PROXY_TYPE = "analog_proxy";
-
-   static final String DIGITAL_PROXY_TYPE = "digital_proxy";
-
-   static final String DISPLAY_WINDOW_TYPE = "display_window";
-
-   static final String INPUT_AREA_TYPE = "input_area";
-
-   static final String SIMULATOR_VIEWPORT_TYPE = "simulator_viewport";
-
-   static final String SURFACE_VIEWPORT_TYPE = "surface_viewport";
-
-   static final String INPUT_WINDOW_TYPE = "input_window";
-
-   static final String KEYBOARD_MOUSE_TYPE = "keyboard_mouse_device";
-
-   static final String KEYBOARD_MOUSE_PROXY_TYPE = "keyboard_mouse_proxy";
-
-   static final String OPENGL_FRAME_BUFFER_TYPE = "opengl_frame_buffer_config";
-
-   static final String POSITION_PROXY_TYPE = "position_proxy";
-
-   static final String PROXY_TYPE = "proxy";
-
-   static final String SIM_ANALOG_DEVICE_TYPE = "simulated_analog_device";
-
-   static final String SIM_DIGITAL_DEVICE_TYPE = "simulated_digital_device";
-
-   static final String SIM_POS_DEVICE_TYPE = "simulated_positional_device";
-
-   static final String DEVICE_PROPERTY = "device";
-
-   static final String KEYBOARD_MOUSE_PROXY_PTR_PROPERTY =
-      "keyboard_mouse_proxy";
-
-   static final String KEYBOARD_MOUSE_PTR_PROPERTY =
-      "keyboard_mouse_device_name";
-
-   static final String LOCK_KEY_PROPERTY = "lock_key";
-
-   static final String ORIGIN_PROPERTY = "origin";
-
-   static final String PROXY_PROPERTY = "proxy";
-
-   static final String START_LOCKED_PROPERTY = "start_locked";
-
-   static final String SLEEP_TIME_PROPERTY = "sleep_time";
-
-   static final String SIZE_PROPERTY = "size";
 }
