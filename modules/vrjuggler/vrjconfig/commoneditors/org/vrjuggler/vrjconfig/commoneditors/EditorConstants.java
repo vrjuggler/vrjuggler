892419142eda192307b9a10ae86cd4a57f261e87
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-01 21:30:28 +0000

    Shift constants related to config element types and property names from org.vrjuggler.vrjconfig.customeditors.display_window.EditorConstants to org.vrjuggler.vrjconfig.commoneditors.EditorConstants.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16941 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/EditorConstants.java
+++ modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/EditorConstants.java
@@ -32,28 +32,121 @@
 
 package org.vrjuggler.vrjconfig.commoneditors;
 
+
+/**
+ * String constants used for looking up config elements, config definitions,
+ * and properties.
+ */
 public interface EditorConstants
 {
-   public static final String ANALOG_PROXY_TYPE  = "analog_proxy";
+   // Config element types (definition tokens).
 
+   /** Config element type alias. */
+   public static final String ALIAS_TYPE = "alias";
+
+   /** Config element type analog_proxy. */
+   public static final String ANALOG_PROXY_TYPE = "analog_proxy";
+
+   /** Config element type digital_proxy. */
    public static final String DIGITAL_PROXY_TYPE = "digital_proxy";
 
-   public static final String POSITION_PROXY_TYPE   = "position_proxy";
+   /** Config element type display_window. */
+   public static final String DISPLAY_WINDOW_TYPE = "display_window";
 
+   /** Config element type input_area. */
+   public static final String INPUT_AREA_TYPE = "input_area";
+
+   /** Config element type input_window. */
+   public static final String INPUT_WINDOW_TYPE = "input_window";
+
+   /** Config element type keyboard_mouse_device. */
+   public static final String KEYBOARD_MOUSE_TYPE = "keyboard_mouse_device";
+
+   /** Config element type keyboard_mouse_proxy. */
+   public static final String KEYBOARD_MOUSE_PROXY_TYPE =
+      "keyboard_mouse_proxy";
+
+   /** Config element type opengl_frame_buffer_config. */
+   public static final String OPENGL_FRAME_BUFFER_TYPE =
+      "opengl_frame_buffer_config";
+
+   /** Config element type position_proxy. */
+   public static final String POSITION_PROXY_TYPE = "position_proxy";
+
+   /** Config element type proxy. */
+   public static final String PROXY_TYPE = "proxy";
+
+   /** Config element type simulated_analog_device. */
+   public static final String SIM_ANALOG_DEVICE_TYPE =
+      "simulated_analog_device";
+
+   /** Config element type simulated_digital_device. */
+   public static final String SIM_DIGITAL_DEVICE_TYPE =
+      "simulated_digital_device";
+
+   /** Config element type simulated_positional_device. */
+   public static final String SIM_POS_DEVICE_TYPE =
+      "simulated_positional_device";
+
+   /** Config element type simulator_viewport. */
+   public static final String SIMULATOR_VIEWPORT_TYPE = "simulator_viewport";
+
+   /** Config element type surface_viewport. */
+   public static final String SURFACE_VIEWPORT_TYPE = "surface_viewport";
+
+   // Properties.
+
+   /** Property type custom_scale. */
+   public static final String CUSTOM_SCALE_PROPERTY = "custom_scale";
+
+   /** Property type device. */
+   public static final String DEVICE_PROPERTY = "device";
+
+   /** Property type device_units. */
+   public static final String DEVICE_UNITS_PROPERTY = "device_units";
+
+   /** Property type keyboard_mouse_proxy. */
+   public static final String KEYBOARD_MOUSE_PROXY_PTR_PROPERTY =
+      "keyboard_mouse_proxy";
+
+   /** Property type keyboard_mouse_device_name. */
+   public static final String KEYBOARD_MOUSE_PTR_PROPERTY =
+      "keyboard_mouse_device_name";
+
+   /** Property type lock_key. */
+   public static final String LOCK_KEY_PROPERTY = "lock_key";
+
+   /** Property type origin. */
+   public static final String ORIGIN_PROPERTY = "origin";
+
+   /** Property type position_filters. */
+   public static final String POSITION_FILTERS_PROPERTY = "position_filters";
+
+   /** Property type position_transform_filter. */
    public static final String POSITION_TRANSFORM_FILTER_TYPE =
       "position_transform_filter";
 
-   public static final String POSITION_FILTERS_PROPERTY = "position_filters";
+   /** Property type proxy. */
+   public static final String PROXY_PROPERTY = "proxy";
 
-   public static final String CUSTOM_SCALE_PROPERTY = "custom_scale";
+   /** Property type start_locked. */
+   public static final String START_LOCKED_PROPERTY = "start_locked";
 
-   public static final String DEVICE_UNITS_PROPERTY = "device_units";
+   /** Property type sleep_time. */
+   public static final String SLEEP_TIME_PROPERTY = "sleep_time";
 
+   /** Property type size. */
+   public static final String SIZE_PROPERTY = "size";
+
+   /** Property type pre_rotation. */
    public static final String PRE_ROTATION_PROPERTY = "pre_rotation";
 
-   public static final String POST_ROTATION_PROPERTY = "post_rotation";
-
+   /** Property type pre_translation. */
    public static final String PRE_TRANSLATION_PROPERTY = "pre_translate";
 
+   /** Property type post_rotation. */
+   public static final String POST_ROTATION_PROPERTY = "post_rotation";
+
+   /** Property type post_translation. */
    public static final String POST_TRANSLATION_PROPERTY = "post_translate";
 }
