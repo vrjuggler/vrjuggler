6c27e695001057c3a218987e4c05b820b023b45e
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-05 23:01:03 +0000

    Split this file up to match the way that it is documented in the /Device Driver Authoring Guide/.  This also clarifies to users what parts would go in the header file and what would go in the implementation file.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@16972 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/samples/tutorials/device.drivers/buttondevice.cpp
+++ modules/gadgeteer/samples/tutorials/device.drivers/buttondevice.cpp
@@ -33,79 +33,17 @@
 #include <gadget/Devices/DriverConfig.h>
 
 #include <vector>
-#include <string>
 
 #include <vpr/vpr.h>
-#include <vpr/Thread/Thread.h>
 #include <vpr/System.h>
 
+#include <jccl/Config/ConfigElement.h>
 #include <gadget/gadgetParam.h>
 #include <gadget/InputManager.h>
-#include <gadget/Type/Input.h>
-#include <gadget/Type/Digital.h>
-#include <gadget/Type/InputMixer.h>
 #include <gadget/Type/DeviceConstructor.h>
 
+#include "buttondevice.h"
 
-using namespace gadget;
-
-/** The device driver class. */
-class ButtonDevice : public InputMixer<Input, Digital>
-{
-public:
-   ButtonDevice()
-      : mSampleThread(NULL)
-      , mRunning(false)
-   {
-      /* Do nothing. */ ;
-   }
-
-   virtual ~ButtonDevice()
-   {
-      if ( mRunning )
-      {
-         this->stopSampling();
-      }
-   }
-
-   virtual bool config(jccl::ConfigElementPtr e);
-   virtual void updateData();
-
-   virtual bool startSampling();
-   virtual bool sample();
-   virtual bool stopSampling();
-
-   static std::string getElementType();
-
-   /**
-    * Invokes the global scope delete operator.  This is required for proper
-    * releasing of memory in DLLs on Win32.
-    */
-   void operator delete(void* p)
-   {
-      ::operator delete(p);
-   }
-
-protected:
-   /**
-    * Deletes this object.  This is an implementation of the pure virtual
-    * gadget::Input::destroy() method.
-    */
-   virtual void destroy()
-   {
-      delete this;
-   }
-
-private:
-   static void   threadedSampleFunction( void* classPointer );
-   int           mDigitalData;
-   vpr::Thread*  mSampleThread;
-   bool          mRunning;
-
-   // configuration data set by config()
-   std::string   mPort;
-   int           mBaud;
-};
 
 /** Entry point function for the device driver plug-in. */
 extern "C" GADGET_DRIVER_EXPORT(vpr::Uint32) getGadgeteerVersion()
@@ -222,3 +160,4 @@ void ButtonDevice::threadedSampleFunction(void* classPointer)
      vpr::System::sleep(1); //specify some time here, so you don't waste CPU cycles
    }
 }
+
