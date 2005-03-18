1fb8a06f875938c5cb2036c7089576476546eddf
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-18 21:37:21 +0000

    Rewrote setSource() and setTarget() to achieve the following:
       1. They now work correctly, though the logic within the methods is still
          a little messy.
       2. They now do something (i.e., update the proxy to reflect changes in
          the pointer).
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17058 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/devicegraph/ProxyPointerEdge.java
+++ modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/devicegraph/ProxyPointerEdge.java
@@ -42,6 +42,15 @@ import org.vrjuggler.jccl.config.ConfigElementPointer;
 import org.vrjuggler.jccl.config.PropertyDefinition;
 
 
+/**
+ * A special-purpose edge that is used for the connection between a proxy
+ * (source) and a device (target).  The validity of source/target connections
+ * through the user interface is verified by <code>DeviceGraphModel</code>,
+ * but sources and targets can still be set programatically using the
+ * <code>org.jgraph.graph.Edge</code> interface.
+ *
+ * @see DeviceGraphModel
+ */
 public class ProxyPointerEdge
    extends DefaultEdge
    implements org.vrjuggler.vrjconfig.commoneditors.EditorConstants
@@ -90,56 +99,100 @@ public class ProxyPointerEdge
       return dev_elt;
    }
 
+   /**
+    * Sets the source for this edge.  If the target is the port of a vertex
+    * containing a <code>DeviceInfo</code> object, then the source must refer
+    * to a port of a vertex that holds a <code>ProxyInfo</code> object.  The
+    * <code>ConfigElement</code> of the target must be a type that the source
+    * proxy is allowed to point to.
+    *
+    * @see DeviceGraphModel#acceptsSource(Object,Object)
+    *
+    * @throws InvalidArgumentException
+    *   thrown if the given source port (when not null) is for a vertex not
+    *   contaning a <code>ProxyInfo</code> object or if the target (when not
+    *   null) is not for a vertex containing a <code>DeviceInfo</code> object.
+    */
    public void setSource(Object port)
    {
-      if ( port == null )
+      // If there is no change to the source, we cannot do anything.
+      if ( getSource() == port )
       {
          super.setSource(port);
       }
-      else if ( port instanceof DefaultPort )
+      // Otherwise, we need to perform updates on all proxies involved.
+      else
       {
-         DefaultPort def_port = (DefaultPort) port;
-         DefaultGraphCell parent = (DefaultGraphCell) def_port.getParent();
-
-         // The new source is a graph cell holding a ConfigElement for a
-         // proxy.  This means that the target for this edge should be a
-         // graph cell holding a ConfigElement for a device.
-         if ( parent.getUserObject() instanceof ProxyInfo )
+         try
          {
-            Object target_obj = getTarget();
+            // Save the old source for later.  This may or may not be null.
+            DefaultPort old_source = (DefaultPort) getSource();
 
-            // If the target has not been set yet, there is nothing to do.
-            // We just set the source that was provided.
-            if ( target_obj != null )
+            DefaultPort proxy_port = (DefaultPort) port;
+            DefaultGraphCell proxy_cell =
+               (DefaultGraphCell) proxy_port.getParent();
+
+            // The new source is a graph cell holding a ProxyInfo object for a
+            // proxy.  This means that the target for this edge should be a
+            // graph cell holding a DeviceInfo for a device.
+            if ( ! (proxy_cell.getUserObject() instanceof ProxyInfo) )
             {
-               super.setSource(port);
+               throw new IllegalArgumentException(
+                  "Invalid type for parent user object -> " +
+                  proxy_cell.getUserObject().getClass()
+               );
+            }
+
+            ProxyInfo proxy_info = (ProxyInfo) proxy_cell.getUserObject();
+            ConfigElement proxy_elt = proxy_info.getElement();
+
+            // At this point, we have verified the correctness of the source,
+            // so we can set port to be our new source.
+            super.setSource(port);
+
+            // If the old source for this edge is non-null, then we need to
+            // update the old proxy so that it points at nothing.
+            if ( old_source != null )
+            {
+               DefaultGraphCell old_proxy_cell =
+                  (DefaultGraphCell) old_source.getParent();
+               ProxyInfo old_proxy_info =
+                  (ProxyInfo) old_proxy_cell.getUserObject();
+               ConfigElement old_proxy_elt = old_proxy_info.getElement();
+               old_proxy_elt.setProperty(DEVICE_PROPERTY, 0, "",
+                                         old_proxy_info.getContext());
+            }
+
+            // Get the device (target) information.
+            Object dev_port_obj = getTarget();
+
+            // If the device is null, then we need to set our proxy's
+            // DEVICE_PROPERTY to point to nothing.
+            if ( dev_port_obj == null )
+            {
+               proxy_elt.setProperty(DEVICE_PROPERTY, 0, "",
+                                     proxy_info.getContext());
             }
             else
             {
-               DefaultPort target = (DefaultPort) target_obj;
-               DefaultGraphCell target_parent =
-                  (DefaultGraphCell) target.getParent();
+               DefaultPort dev_port = (DefaultPort) dev_port_obj;
+               DefaultGraphCell dev_cell =
+                  (DefaultGraphCell) dev_port.getParent();
 
-               if ( ! (target_parent.getUserObject() instanceof DeviceInfo) )
+               if ( ! (dev_cell.getUserObject() instanceof DeviceInfo) )
                {
-                  throw new IllegalArgumentException("Invalid type for " +
-                                                     "target's parent user " +
-                                                     "object -> " +
-                                                     target_parent.getUserObject().getClass());
+                  throw new IllegalArgumentException(
+                     "Invalid type for target's parent user object -> " +
+                     dev_cell.getUserObject().getClass()
+                  );
                }
 
-               // At this point, we have verified the correctness of the
-               // source, so we can set port to be our new source.
-               super.setSource(port);
-
-               DeviceInfo dev_info = (DeviceInfo) target_parent.getUserObject();
-               ProxyInfo  proxy_info = (ProxyInfo) parent.getUserObject();
-
-               ConfigElement proxy_elt = proxy_info.getElement();
-               ConfigElement dev_elt   = dev_info.getElement();
+               DeviceInfo dev_info   = (DeviceInfo) dev_cell.getUserObject();
+               ConfigElement dev_elt = dev_info.getElement();
 
                ConfigElementPointer dev_ptr =
-                  (ConfigElementPointer) proxy_elt.getProperty(DEVICE_PROPERTY, 0);
+                  (ConfigElementPointer) proxy_elt.getProperty(DEVICE_PROPERTY,
+                                                               0);
 
                // If the property value is null or it has a target other than
                // dev_elt, update it.  Otherwise, we just leave it as is.
@@ -151,106 +204,104 @@ public class ProxyPointerEdge
                }
             }
          }
-         else
+         catch (ClassCastException ex)
          {
-            throw new IllegalArgumentException("Invalid type for parent " +
-                                               "user object -> " +
-                                               parent.getUserObject().getClass());
+            throw new IllegalArgumentException("Invalid type for port: " +
+                                               port.getClass());
          }
-      }
-      else
-      {
-         throw new IllegalArgumentException("Invalid type for source port: " +
-                                            port.getClass());
       }
    }
 
    /**
-    * Sets the target for this edge.  If the source is the port of a proxy
-    * ConfigElement, then the target must refer to a port of a graph cell that
-    * holds a ConfigElement reference.  The port of that cell corresponds with
-    * an input source for the ConfigElement.  The ConfigElement of the target
-    * must be a type that the source proxy is allowed to point to.
+    * Sets the target for this edge.  If the source is the port of a vertex
+    * containing a <code>ProxyInfo</code> object, then the target must refer
+    * to a port of a vertex that holds a <code>DeviceInfo</code> object.  The
+    * port of that cell corresponds with an input source for the device.  The
+    * <code>ConfigElement</code> of the target must be a type that the source
+    * proxy is allowed to point to.
+    *
+    * @see DeviceGraphModel#acceptsTarget(Object,Object)
+    *
+    * @throws InvalidArgumentException
+    *   thrown if the given target port (when not null) is for a vertex not
+    *   contaning a <code>DeviceInfo</code> object or if the source (when not
+    *   null) is not for a vertex containing a <code>ProxyInfo</code> object.
     */
    public void setTarget(Object port)
    {
-      if ( port == null )
+      // If there is no change to the source, we cannot do anything.
+      if ( getTarget() == port )
       {
          super.setTarget(port);
       }
-      else if ( port instanceof DefaultPort )
+      // Otherwise, we need to perform updates on all proxies involved.
+      else
       {
-         DefaultPort def_port = (DefaultPort) port;
-         DefaultGraphCell parent = (DefaultGraphCell) def_port.getParent();
-
-         // The new target is a graph cell holding a ConfigElement for a
-         // device.  This means that the source for this edge should be a
-         // graph cell holding a ConfigElement for a proxy.
-         if ( parent.getUserObject() instanceof DeviceInfo )
+         try
          {
-            Object source_obj = getSource();
+            DefaultPort dev_port = (DefaultPort) port;
 
-            // If the source has not been set yet, there is nothing to do.
-            // We just set the target that was provided.
-            if ( source_obj == null )
-            {
-               super.setTarget(port);
-            }
-            else
-            {
-               DefaultPort source = (DefaultPort) source_obj;
-               DefaultGraphCell source_parent =
-                  (DefaultGraphCell) source.getParent();
+            // Get the proxy (source) information.
+            Object proxy_port_obj = getSource();
 
-               if ( ! (source_parent.getUserObject() instanceof ProxyInfo) )
+            if ( proxy_port_obj != null )
+            {
+               DefaultPort proxy_port = (DefaultPort) proxy_port_obj;
+               DefaultGraphCell proxy_cell =
+                  (DefaultGraphCell) proxy_port.getParent();
+
+               if ( ! (proxy_cell.getUserObject() instanceof ProxyInfo) )
                {
-                  throw new IllegalArgumentException("Invalid type for " +
-                                                     "source's parent user " +
-                                                     "object -> " +
-                                                     source_parent.getUserObject().getClass());
+                  throw new IllegalArgumentException(
+                     "Invalid type for source's parent user object -> " +
+                     proxy_cell.getUserObject().getClass()
+                  );
                }
 
-               // At this point, we have verified the correctness of the target,
-               // so we can set port to be our new target.
-               super.setTarget(port);
-
-               DeviceInfo dev_info   = (DeviceInfo) parent.getUserObject();
-               ProxyInfo  proxy_info = (ProxyInfo) source_parent.getUserObject();
-
+               ProxyInfo proxy_info    = (ProxyInfo) proxy_cell.getUserObject();
                ConfigElement proxy_elt = proxy_info.getElement();
-               ConfigElement dev_elt   = dev_info.getElement();
 
-               ConfigElementPointer dev_ptr =
-                  (ConfigElementPointer) proxy_elt.getProperty(DEVICE_PROPERTY, 0);
-
-               // If the property value is null or it has a target other than
-               // dev_elt, update it.  Otherwise, we just leave it as is.
-               if ( dev_ptr == null ||
-                    ! dev_ptr.getTarget().equals(dev_elt.getName()) )
+               if ( dev_port == null )
                {
+                  proxy_elt.setProperty(DEVICE_PROPERTY, 0, "",
+                                        proxy_info.getContext());
+               }
+               else
+               {
+                  DefaultGraphCell dev_cell =
+                     (DefaultGraphCell) dev_port.getParent();
+
+                  // The new target is a graph cell holding a DeviceInfo object
+                  // for a device.  This means that the source for this edge
+                  // should be a graph cell holding a ProxyInfo object for a
+                  // proxy.
+                  if ( ! (dev_cell.getUserObject() instanceof DeviceInfo) )
+                  {
+                     throw new IllegalArgumentException(
+                        "Invalid type for parent user object -> " +
+                        dev_cell.getUserObject().getClass()
+                     );
+                  }
+
+                  DeviceInfo dev_info   = (DeviceInfo) dev_cell.getUserObject();
+                  ConfigElement dev_elt = dev_info.getElement();
                   proxy_elt.setProperty(DEVICE_PROPERTY, 0, dev_elt.getName(),
+                                        proxy_info.getContext());
+                  proxy_elt.setProperty(UNIT_PROPERTY, 0,
+                                        dev_port.getUserObject(),
                                         proxy_info.getContext());
                }
             }
-         }
-/*
-         else if ( parent.getUsreObject() instanceof ProxyInfo )
-         {
-            ProxyInfo dev_info = (ProxyInfo) parent.getUserObject();
+
+            // At this point, we have verified the correctness of the target,
+            // so we can set port to be our new target.
             super.setTarget(port);
          }
-*/
-         else
+         catch (ClassCastException ex)
          {
-            throw new IllegalArgumentException("Invalid type for parent " +
-                                               "user object -> " +
-                                               parent.getUserObject().getClass());
+            throw new IllegalArgumentException("Invalid type for port: " +
+                                               port.getClass());
          }
-      }
-      else
-      {
-         throw new IllegalArgumentException("Invalid type for target port: " +
-                                            port.getClass());
       }
    }
 }
