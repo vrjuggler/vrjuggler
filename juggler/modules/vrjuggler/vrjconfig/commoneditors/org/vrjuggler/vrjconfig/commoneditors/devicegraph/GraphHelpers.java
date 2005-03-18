c8230211552c7639de01f10613a04fda8fe0de90
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-18 21:56:24 +0000

    Added a whole bunch of new helper functions intended to ensure correct creation of graph cells for use within an instance of org.vrjuggler.vrjconfig.commoneditors.DeviceGraph.  As always happens when I make helper functions, I have gone overboard with the number of functions in an effort to handle more cases than are probably necessary.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@17060 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/devicegraph/GraphHelpers.java
+++ juggler/modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/devicegraph/GraphHelpers.java
@@ -35,17 +35,33 @@ package org.vrjuggler.vrjconfig.commoneditors.devicegraph;
 import java.awt.Dimension;
 import java.awt.geom.Rectangle2D;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.jgraph.JGraph;
 import org.jgraph.graph.AttributeMap;
 import org.jgraph.graph.CellView;
+import org.jgraph.graph.ConnectionSet;
+import org.jgraph.graph.DefaultEdge;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.DefaultPort;
+import org.jgraph.graph.GraphCell;
 import org.jgraph.graph.GraphConstants;
+
+import org.vrjuggler.jccl.config.ConfigContext;
+import org.vrjuggler.jccl.config.ConfigDefinition;
+import org.vrjuggler.jccl.config.ConfigElement;
+
+import org.vrjuggler.vrjconfig.commoneditors.DeviceGraph;
+import org.vrjuggler.vrjconfig.commoneditors.EditorConstants;
 
 
 /**
  * A collection of helper functions that can be helpful in extending JGraph.
  */
 public abstract class GraphHelpers
+   implements EditorConstants
 {
    /**
     * Changes the size of the given <code>CellView</code> reference to be the
@@ -103,5 +119,296 @@ public abstract class GraphHelpers
       }
 
       return edited;
+   }
+
+   /**
+    * Creates a new graph cell for the given config element that is used to
+    * configure an input device.  The parameters to use for creating the
+    * appropriate <code>DefaultGraphCell</code> (with its ports) are
+    * determined based on the type of the config element passed in.  This
+    * method is intended to be used for ensuring correct and easy graph cell
+    * creation for config elements used to configure devices.  However, the
+    * config element type must be known <i>a priori</i> in order for this
+    * method to be able to do its job.
+    *
+    * @param devElt             the config element for the device being
+    *                           represented by the vertex cell to be created
+    * @param context            the config context where <code>devElt</code>
+    *                           exists
+    * @param attributes         the attribute map where the vertex cell's
+    *                           default attribute map will be stored by this
+    *                           function
+    *
+    * @throws IllegalArgumentException
+    *   thrown when the type of <code>devElt</code> is unknown to this method,
+    *   thus preventing correct creation of a <code>DefaultGraphCell</code>
+    *   instance.
+    */
+   public static DefaultGraphCell createDeviceCell(ConfigElement devElt,
+                                                   ConfigContext context,
+                                                   Map attributes)
+   {
+      ConfigDefinition def = devElt.getDefinition();
+      DefaultGraphCell cell = null;
+
+      // XXX: Come up with a better system for setting the x and y values.
+      int x = 120, y = 20;
+
+      if ( def.getToken().equals(SIM_POS_DEVICE_TYPE) )
+      {
+         cell = createDeviceCell(devElt, context, 1, attributes, x, y, true);
+      }
+      else if ( def.getToken().equals(SIM_DIGITAL_DEVICE_TYPE) )
+      {
+         cell = createDeviceCell(devElt, context, KEY_PAIR_PROPERTY,
+                                 attributes, x, y, false);
+      }
+      else
+      {
+         throw new IllegalArgumentException("Unexpected definition " +
+                                            def.getToken());
+      }
+
+      return cell;
+   }
+
+   /**
+    * Creates a new graph cell for the given config element that is used to
+    * configure an input device, which has exactly one input source (unit)
+    * at all times.  The numbmer of units indicates how many ports the new
+    * graph cell should have initially.  The initial attribute map used for
+    * the graph cell is created using
+    * <code>org.vrjuggler.vrjconfig.commoneditors.DeviceGraph.createDeviceAttributes()</code>,
+    * and it is stored in the given attribute map.
+    *
+    * @param devElt             the config element for the device being
+    *                           represented by the vertex cell to be created
+    * @param context            the config context where <code>devElt</code>
+    *                           exists
+    * @param attributes         the attribute map where the vertex cell's
+    *                           default attribute map will be stored by this
+    *                           function
+    * @param x                  the X coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param y                  the Y coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param autoSize           flag indicating whether the new cell should be
+    *                           auto-sized
+    *
+    * @see org.vrjuggler.vrjconfig.commoneditors.DeviceGraph#createDeviceAttributes(int,int,boolean)
+    * @see #createDeviceCell(ConfigElement,ConfigContext,int,Map,int,int,boolean)
+    */
+   public static DefaultGraphCell createDeviceCell(ConfigElement devElt,
+                                                   ConfigContext context,
+                                                   Map attributes, int x,
+                                                   int y, boolean autoSize)
+   {
+      return createDeviceCell(devElt, context, 1, attributes, x, y, autoSize);
+   }
+
+   /**
+    * Creates a new graph cell for the given config element that is used to
+    * configure an input device.  The number of units is determined based on
+    * the value of <code>unitPropName</code>.  If it is non-null, then
+    * <code>devElt</code> is queried for the number of values of thet named
+    * property, indicating that the number of values is variable.  If
+    * <code>unitPropName</code> is null, then the number of units is set to 1
+    * and is treated as non-variable.  The numbmer of units indicates how many
+    * ports the new graph cell should have initially.  The initial attribute
+    * map used for the graph cell is created using
+    * <code>org.vrjuggler.vrjconfig.commoneditors.DeviceGraph.createDeviceAttributes()</code>,
+    * and it is stored in the given attribute map.
+    *
+    * @param devElt             the config element for the device being
+    *                           represented by the vertex cell to be created
+    * @param context            the config context where <code>devElt</code>
+    *                           exists
+    * @param unitPropName       the name of the property that indicates the
+    *                           number of available units for the represented
+    *                           device
+    * @param attributes         the attribute map where the vertex cell's
+    *                           default attribute map will be stored by this
+    *                           function
+    * @param x                  the X coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param y                  the Y coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param autoSize           flag indicating whether the new cell should be
+    *                           auto-sized
+    *
+    * @see org.vrjuggler.vrjconfig.commoneditors.DeviceGraph#createDeviceAttributes(int,int,boolean)
+    */
+   public static DefaultGraphCell createDeviceCell(ConfigElement devElt,
+                                                   ConfigContext context,
+                                                   String unitPropName,
+                                                   Map attributes, int x,
+                                                   int y, boolean autoSize)
+   {
+      DefaultGraphCell dev_cell =
+         new DefaultGraphCell(new DeviceInfo(devElt, context, unitPropName));
+
+      // If unitPropName is null, that means that the number of units for the
+      // device being represented by dev_cell is exactly 1 at all times.
+      // Otherwise, we get the current number of units by querying devElt.
+      int num_units =
+         (unitPropName == null) ? 1
+                                : devElt.getPropertyValueCount(unitPropName);
+
+      addDevicePorts(dev_cell, num_units);
+      attributes.put(dev_cell, DeviceGraph.createDeviceAttributes(x, y,
+                                                                  autoSize));
+
+      return dev_cell;
+   }
+
+   /**
+    * Creates a new graph cell for the given config element that is used to
+    * configure an input device.  The number of units (specified by the
+    * <code>numUnits</code> parameter) indicates the fixed number of ports
+    * the new graph cell has.  The initial attribute map used for the graph
+    * cell is created using
+    * <code>org.vrjuggler.vrjconfig.commoneditors.DeviceGraph.createDeviceAttributes()</code>,
+    * and it is stored in the given attribute map.
+    *
+    * @param devElt             the config element for the device being
+    *                           represented by the vertex cell to be created
+    * @param context            the config context where <code>devElt</code>
+    *                           exists
+    * @param numUnits           the fixed number of units this device has
+    * @param attributes         the attribute map where the vertex cell's
+    *                           default attribute map will be stored by this
+    *                           function
+    * @param x                  the X coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param y                  the Y coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param autoSize           flag indicating whether the new cell should be
+    *                           auto-sized
+    *
+    * @see org.vrjuggler.vrjconfig.commoneditors.DeviceGraph#createDeviceAttributes(int,int,boolean)
+    */
+   public static DefaultGraphCell createDeviceCell(ConfigElement devElt,
+                                                   ConfigContext context,
+                                                   int numUnits,
+                                                   Map attributes, int x,
+                                                   int y, boolean autoSize)
+   {
+      // Create a DeviceInfo object with a fixed number of units.
+      DefaultGraphCell dev_cell =
+         new DefaultGraphCell(new DeviceInfo(devElt, context));
+      addDevicePorts(dev_cell, numUnits);
+      attributes.put(dev_cell, DeviceGraph.createDeviceAttributes(x, y,
+                                                                  autoSize));
+
+      return dev_cell;
+   }
+
+   /**
+    * Creates a graph cell for a device proxy that has a single port as a
+    * child.  The initial attribute map used for the graph cell is created
+    * using
+    * <code>org.vrjuggler.vrjconfig.commoneditors.DeviceGraph.createProxyAttributes()</code>,
+    * and it is stored in the given attribute map.
+    *
+    * @param proxyElt           the config element for the device proxy being
+    *                           represented by the vertex cell to be created
+    * @param context            the config context where <code>proxyElt</code>
+    *                           exists
+    * @param aliases            all the aliases (zero or more) that exist for
+    *                           <code>proxyElt</code>
+    * @param attributes         the attribute map where the vertex cell's
+    *                           default attribute map will be stored by this
+    *                           function
+    * @param x                  the X coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param y                  the Y coordinate for the initial positionaing
+    *                           of the new vertex
+    * @param autoSize           flag indicating whether the new cell should be
+    *                           auto-sized
+    *
+    * @see org.vrjuggler.vrjconfig.commoneditors.DeviceGraph#createProxyAttributes(int,int,boolean)
+    */
+   public static DefaultGraphCell createProxyCell(ConfigElement proxyElt,
+                                                  ConfigContext context,
+                                                  List aliases,
+                                                  Map attributes, int x,
+                                                  int y, boolean autoSize)
+   {
+      DefaultGraphCell proxy_cell =
+         new DefaultGraphCell(new ProxyInfo(proxyElt, context, aliases));
+      proxy_cell.add(new DefaultPort());
+      attributes.put(proxy_cell,
+                     DeviceGraph.createProxyAttributes(x, y, autoSize));
+
+      return proxy_cell;
+   }
+
+   /**
+    * Creates a connection between <code>proxyCell</code> and
+    * <code>deviceCell</code> using the appropriate port in
+    * <code>deviceCell</code>.  The port to use is determined by the user
+    * object contained in <code>proxyCell</code>, which must be of type
+    * <code>ProxyInfo</code>.  The proxy cell's user object will contain the
+    * <code>ConfigElement</code> for the proxy, which in turn provides a unit
+    * number for the device to which the proxy should refer.
+    *
+    * @throws NoSuchPortException thrown If the two cells cannot be connected
+    *                             because no appropriate port exists in
+    *                             <code>deviceCell</code>
+    *
+    * @see ProxyInfo
+    * @see #createProxyCell(ConfigElement,ConfigContext,List,Map,int,int,boolean)
+    */
+   public static DefaultEdge connectProxyToDevice(DefaultGraphCell proxyCell,
+                                                  DefaultGraphCell deviceCell,
+                                                  ConnectionSet cs,
+                                                  Map attributes)
+      throws NoSuchPortException
+   {
+      ProxyPointerEdge edge = null;
+      ProxyInfo proxy_info = (ProxyInfo) proxyCell.getUserObject();
+      Object unit_prop = proxy_info.getElement().getProperty(UNIT_PROPERTY, 0);
+
+      // Look for the child of deviceCell that is a DefaultPort whose user
+      // object matches the UNIT_PROPERTY value of proxyCell's ConfigElement.
+      // If none is found, we cannot make a connection.
+      for ( Iterator c = deviceCell.getChildren().iterator(); c.hasNext(); )
+      {
+         Object child = c.next();
+
+         if ( child instanceof DefaultPort )
+         {
+            DefaultPort child_port = (DefaultPort) child;
+
+            if ( child_port.getUserObject().equals(unit_prop) )
+            {
+               DefaultPort proxy_port = (DefaultPort) proxyCell.getFirstChild();
+               edge = new ProxyPointerEdge("");
+               cs.connect(edge, proxy_port, child_port);
+               attributes.put(edge, DeviceGraph.createProxyLineStyle());
+               break;
+            }
+         }
+      }
+
+      if ( edge == null )
+      {
+         throw new NoSuchPortException("Could not find port with unit value " +
+                                       unit_prop);
+      }
+
+      return edge;
+   }
+
+   /**
+    * Adds the given number of ports to the given graph cell as children.
+    * The ports are all instances of <code>org.jgraph.graph.DefaultPort</code>.
+    */
+   private static void addDevicePorts(DefaultGraphCell cell, int numUnits)
+   {
+      for ( int i = 0; i < numUnits; ++i )
+      {
+         cell.add(new DefaultPort(new Integer(i)));
+      }
    }
 }
