6916d88fea53c8cf0a4aeb47852c39fd33ba8ef8
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-25 20:54:43 +0000

    Silenced JavaDoc warnings resulting from copying and pasting a typo.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17152 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/devicegraph/DeviceInfo.java
+++ modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/devicegraph/DeviceInfo.java
@@ -312,7 +312,7 @@ public class DeviceInfo
     *
     * @param unitType   the type (as defined in <code>UnitConstants</code> of
     *                   the device unit to be added)
-    * @param untiNumber the index of the unit to remove
+    * @param unitNumber the index of the unit to remove
     *
     * @see UnitConstants
     */
