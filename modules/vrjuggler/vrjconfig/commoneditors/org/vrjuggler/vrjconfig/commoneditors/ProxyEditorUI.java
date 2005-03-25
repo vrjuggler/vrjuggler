20a8af0525a62cda976f8023f77374089083324a
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-25 15:08:06 +0000

    Attempt to use an ancestor lookup for dialog box positioning that will work when this component is contained within a JFrame or a JDialog.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17144 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/ProxyEditorUI.java
+++ modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/ProxyEditorUI.java
@@ -37,11 +37,11 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Cursor;
-import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -714,8 +714,8 @@ public class ProxyEditorUI
       ConfigDefinitionChooser chooser = new ConfigDefinitionChooser();
       chooser.setDefinitions(mAllowedDefs);
 
-      Frame parent = (Frame) SwingUtilities.getAncestorOfClass(Frame.class,
-                                                               this);
+      Window parent = (Window) SwingUtilities.getAncestorOfClass(Window.class,
+                                                                 this);
 
       if ( chooser.showDialog(parent) == ConfigDefinitionChooser.APPROVE_OPTION )
       {
