1794f11c4f8fb71183fc9d684593db0a8c509b0e
Patrick Hartling <patrick.hartling@gmail.com>
2005-03-28 21:45:31 +0000

    Document how instances of this class are created and what the various methods return.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17169 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/jackal/config/org/vrjuggler/jccl/config/event/ConfigElementEvent.java
+++ modules/jackal/config/org/vrjuggler/jccl/config/event/ConfigElementEvent.java
@@ -36,13 +36,24 @@ import java.util.EventObject;
 import org.vrjuggler.jccl.config.ConfigElement;
 
 
+/**
+ * The event type fired by instances of
+ * <code>org.vrjuggler.jccl.config.ConfigElement</code> when the name or a
+ * property of the source config elment changes in some way.
+ *
+ * @see org.vrjuggler.jccl.config.ConfigElement
+ * @see ConfigElementListener
+ */
 public class ConfigElementEvent
    extends EventObject
 {
    /**
     * Creates a new ConfigElement event from the given source eelemnt relating
     * to the given property, the index into that property, and the value of
-    * such property.
+    * such property.  For cases of config element name changes, property value
+    * removals, and property value chagnes, <code>value</code> represents the 
+    * old value.  For property value additions, <code>value</code> is a
+    * reference to the newly added value.
     */
    public ConfigElementEvent(ConfigElement srcElt, String property, int index,
                              Object value)
@@ -53,16 +64,35 @@ public class ConfigElementEvent
       this.value = value;
    }
 
+   /**
+    * Returns the name of the property in the source config element that
+    * changed or null if it was the name of the source config element that
+    * changed.
+    */
    public String getProperty()
    {
       return property;
    }
 
+   /**
+    * Returns the value index (0 or greater) of the property that changed.  If
+    * 0 is returned, this may be an indication that it was the name of the
+    * source config element that changed.
+    *
+    * @see #getProperty()
+    */
    public int getIndex()
    {
       return index;
    }
 
+   /**
+    * Gets the value associated with this event.  For the case of config
+    * element name changes, the old name of the config element is returned.
+    * For the cases of property value removals and property value changes, a
+    * reference to the old value is returned.  For property value additions, a
+    * reference to the newly added value is returned.
+    */
    public Object getValue()
    {
       return value;
