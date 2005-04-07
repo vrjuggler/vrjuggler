891ddbc3d6ac7bb646fa4d6d0438c0f731493a07
Aron Bierbaum <aronbierbaum@gmail.com>
2005-04-07 01:52:42 +0000

    SF Bug#: [1167555] Keyboard/mouse input in Performer windows broken on X11
    Keyboard/Mouse input has been refactored to not use any of Performer's
    utility classes. This eliminates the painful multiprocess nightmare.
    The downfall is that input events are currently handled in the
    APP_CULL_DRAW process. At some point we should consider putting this
    in its own thread.
    
    It was a tough game, but I pulled through in the end.
    Performer  75
    Aron       77 <
    
    Performer input on Win32 needs to be addressed next.
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@17242 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/modules/vrjuggler/vrj/Draw/Pf/PfInputHandler.cpp
+++ juggler/modules/vrjuggler/vrj/Draw/Pf/PfInputHandler.cpp
@@ -30,27 +30,148 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
+#define PF_C_API 1
+
+#include <vrj/Util/Debug.h>
 #include <vrj/Draw/Pf/PfInputHandler.h>
 
 namespace vrj
 {
 
-PfInputHandler::PfInputHandler(pfPipeWindow* pWin, const std::string& displayName)
+PfInputHandler::PfInputHandler(pfPipeWindow* pipeWindow, const std::string& displayName)
 {
+   mName = displayName;
+   mPipeWindow = pipeWindow;
+   mPipe = pfGetPWinPipe(mPipeWindow);
 #ifdef VPR_OS_Win32
-   mWinHandle = pWin->getWSWindow();
+   mWinHandle = pipeWindow->getWSWindow();
+   //mWinHandle = pfGetPWinWSWindow(pipeWindow);
 #else
    // Get the XWindow from that we are going to recieve events from.
-   mXWindow = pWin->getWSWindow();
+   mXWindow = pfGetPWinWSWindow(mPipeWindow);
+#endif
 
-   // Create a new display connection to the X server.
-   mXDisplay = XOpenDisplay( displayName.c_str());
+   openConnection();
+}
+
+void PfInputHandler::openConnection()
+{
+#ifdef VPR_OS_Win32
+   HWND hwnd;
+
+   // Store pointer to old WndProcs
+   hwnd = pfGetWinWSWindow(pfGetPWinSelect(mPipeWindow));
+   if(hwnd == NULL)
+   {
+      displayWarningAboutNULLHWND("openWin32Input","Window");
+      initializedWins = 0;
+   }
+   else
+   {
+      //mWinHandle = hwnd;
+      mOldWndProc = SetWindowLong(hwnd, GWL_WNDPROC, (LONG) collectWin32InputWrapper);
+   }
+#else
+   static Atom wm_protocols, wm_delete_window;
+   
+   if (!mPipe)
+   {
+      vprDEBUG(vprDBG_ERROR, vprDBG_CRITICAL_LVL)
+         << clrOutNORM(clrRED, "ERROR:")
+         << "pfInputHandler::openConnection() NULL mPipe!"
+         << std::endl << vprDEBUG_FLUSH;
+      return;
+   }
+
+   const char* str = pfGetPipeWSConnectionName(mPipe);
+
+   mXDisplay = pfOpenWSConnection(str, false);
+   if (NULL == mXDisplay)
+   {
+      vprDEBUG(vprDBG_ERROR, vprDBG_CRITICAL_LVL)
+         << clrOutNORM(clrRED, "ERROR:")
+         << "pfInputHandler::openConnection() Can't open display '" << str
+         << "'\n" << vprDEBUG_FLUSH;
+      return;
+   }
+
+   // XSync in order to ensure that window is initialized
+   // for this connection.
+   XSync(mXDisplay, false);
+
+   static long event_mask = (FocusChangeMask | ExposureMask |
+                             VisibilityChangeMask | /*StructureNotifyMask |*/
+                             KeyPressMask | KeyReleaseMask | 
+                             ButtonPressMask | ButtonReleaseMask |
+                             PointerMotionMask);
+
+   wm_protocols = XInternAtom(mXDisplay, "WM_PROTOCOLS", 1);
+   wm_delete_window = XInternAtom(mXDisplay, "WM_DELETE_WINDOW", 1);
+
+   XSetWMProtocols(mXDisplay, mXWindow, &wm_delete_window, 1);
+   XSelectInput(mXDisplay, mXWindow, event_mask);
+   XMapWindow(mXDisplay, mXWindow);
+   XSync(mXDisplay,false);
 #endif
 }
 
 #ifdef VPR_OS_Win32
-void PfInputHandler::handlePerformerEvent(MSG message)
+LRESULT CALLBACK eventCallback(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 {
+   int i;
+
+   collectWin32Input(hwnd,uMsg,wParam,lParam);
+
+   if(hwnd == mWinHandle)
+   {
+      return CallWindowProc(oldWndProc, hwnd, uMsg, wParam, lParam);
+   }
+
+   // Invoke the default window handler if none found.
+   return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+#endif
+
+#ifdef VPR_OS_Win32
+void pfuCollectWin32EventStream(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+{
+   HWND parentWindow = hwnd;
+   LPTSTR tmpName[101];
+   pfuWin32Event event;
+
+   event.hwnd = hwnd; /* or should this too point to the parentWindow, if any? */
+   event.uMsg = uMsg;
+   event.wParam = wParam;
+   event.lParam = lParam;
+   event.time = pfGetTime();
+
+   /* we want the inWin member of the pfuMouse to be == to the
+   * HWND for the pipe window regardless of whether or not
+   * the event occurred in the child or parent of the pipe win
+   * In theory this variable is just a bool but it's used in
+   * some sample programs to check the window id of the pipe
+   * window. gui.c works with just a bool but ...
+   */
+   if(GetClassName(hwnd,tmpName,100) > 0)
+   {
+      if(!strcmp(tmpName,"pfChildWNDCLASS"))
+      {
+         parentWindow = (HWND)GetWindowLong(hwnd,GWL_HWNDPARENT);
+      }
+      if(parentWindow == NULL)
+      {
+         parentWindow = hwnd; /* and spew error message ... */
+      }
+   }
+   else
+   {
+      vprDEBUG(vprDBG_ERROR, vprDBG_CRITICAL_LVL)
+         << clrOutNORM(clrRED, "ERROR:")
+         << " Unable to determine class name of win: " << std::hex << hwnd 
+         << std::dec << std::endl << vprDEBUG_FLUSH;
+   }
+
+
    // If we have a valid KeyboardMouseDevice, process
    // all keyboard/mouse events
    if ( NULL != mKeyboardMouseDevice )
@@ -60,16 +181,52 @@ void PfInputHandler::handlePerformerEvent(MSG message)
    }
 }
 #else
-void PfInputHandler::handlePerformerEvent(::XEvent& event)
+
+void PfInputHandler::handleEvents()
 {
-   // If we have a valid KeyboardMouseDevice, process
-   // all keyboard/mouse events
-   if ( NULL != mKeyboardMouseDevice )
-   {
-      // Forward events on to subclass. The magic of inheritance :)
-      handleEvent(event);
-   }
-}
+   XEvent		event;
+
+   /*
+   // In Linux, XEventsQueued with QueuedAlready results in an
+   // extreme amount of latency in SP mode since the event queue
+   // only ever reports one event per call.  It's necessary to
+   // change this to re-query the X server for more events after
+   // the first one is pulled from the queue.  However this results
+   // in a server roundtrip (in Linux) and probably causes a performance
+   // hit.  So only being defined for LINUX
+   */
+#ifdef __linux__
+   while (XPending(mXDisplay)) 
+#else
+   while (XEventsQueued(mXDisplay, QueuedAlready)) 
 #endif
+   {
+      XNextEvent(mXDisplay, &event);
+      
+      switch (event.type) 
+      {
+      case ClientMessage:
+         //if ((event.xclient.message_type == wm_protocols) &&
+         //    (event.xclient.data.l[0] == wm_delete_window))
+         {
+            //events->devQ[events->numDevs] = PFUDEV_WINQUIT;
+            //events->devVal[events->numDevs] = 1;
+            //events->devCount[PFUDEV_WINQUIT] += 1;
+            //events->numDevs++;
+         } 
+         break;
+      default:
+         // If we have a valid KeyboardMouseDevice, process
+         // all keyboard/mouse events
+         if ( NULL != mKeyboardMouseDevice )
+         {
+            // Forward events on to subclass. The magic of inheritance :)
+            handleEvent(event);
+         }
+         break;
+      }
+   }
+#endif /* VPR_OS_Win32 */
+}
 
 } // End of vrj namespace
