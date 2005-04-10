b7850d69debb5c9a381af5c35370c931be0e77f1
Patrick Hartling <patrick.hartling@gmail.com>
2005-04-10 17:56:47 +0000

    Protect against spaces in paths.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk@17290 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- juggler/external/JacORB/idl.bat
+++ juggler/external/JacORB/idl.bat
@@ -1,2 +1,2 @@
 @echo off
-%JAVA_HOME%\bin\java -classpath %JACORB_PATH%\idl.jar;%JACORB_PATH%\logkit-1.2.jar org.jacorb.idl.parser %*
+"%JAVA_HOME%\bin\java" -classpath "%JACORB_PATH%\idl.jar;%JACORB_PATH%\logkit-1.2.jar" org.jacorb.idl.parser %*
