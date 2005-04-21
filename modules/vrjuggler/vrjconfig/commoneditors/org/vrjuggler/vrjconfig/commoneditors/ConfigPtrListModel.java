1d78fc81a25aae2c3864778b638287cdaba30d88
Aron Bierbaum <aronbierbaum@gmail.com>
2005-04-21 00:56:58 +0000

    Adding ListModel and ComboBoxModel implementations that can display all ConfigElements of a given type in a ConfigContext. These models can be used to select a ConfigElement for a ConfigElementPointer to point at. In most cases we would use an instance of a PropertyEditorPanel, but in some cases we are not actually editing a ConfigElement, as with the case for setting the cluster_node that a display_window(screen) should be located on.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17389 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/vrjconfig/commoneditors/org/vrjuggler/vrjconfig/commoneditors/ConfigPtrListModel.java
@@ -0,0 +1,106 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+package org.vrjuggler.vrjconfig.commoneditors;
+
+import java.util.*;
+import javax.swing.*;
+import org.vrjuggler.jccl.config.*;
+import org.vrjuggler.jccl.config.event.ConfigEvent;
+import org.vrjuggler.jccl.config.event.ConfigListener;
+
+public class ConfigPtrListModel
+   extends DefaultListModel
+   implements ConfigListener
+{
+   ConfigBroker mBroker = null;
+   ConfigContext mContext = null;
+   List mElementTypes = new ArrayList();
+
+   public ConfigPtrListModel(ConfigContext context)
+   {
+      mBroker = new ConfigBrokerProxy();
+      mContext = context;
+
+      update();
+   }
+
+   public void addElementType(String element_type)
+   {
+      mElementTypes.add(element_type);
+      update();
+   }
+
+   public void removeElementType(String element_type)
+   {
+      mElementTypes.remove(element_type);
+      update();
+   }
+
+   public void removeAllElementTypes()
+   {
+      mElementTypes.clear();
+      update();
+   }
+
+   public void configElementAdded(ConfigEvent evt)
+   {
+      System.out.println("XXX: Element Added");
+      update();
+      fireIntervalAdded(evt.getSource(), 0, getSize());
+   }
+
+   public void configElementRemoved(ConfigEvent evt)
+   {
+      System.out.println("XXX: Element Removed");
+      update();
+      fireIntervalRemoved(evt.getSource(), 0, getSize());
+   }
+
+   public void update()
+   {
+      this.removeAllElements();
+      List matches = new ArrayList();
+      List elts = mBroker.getElements(mContext);
+
+      for(Iterator i = mElementTypes.iterator() ; i.hasNext() ;)
+      {
+         String temp_type = (String)i.next();
+         matches.addAll(ConfigUtilities.getElementsWithDefinition(elts, temp_type));
+      }
+
+      for(int i = 0; i < matches.size() ; i++)
+      {
+         this.addElement(((ConfigElement)matches.get(i)).getName());
+      }
+   }
+}
