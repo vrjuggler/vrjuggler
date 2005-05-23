e4a6d796b2eca28a54629a78fc4438457c2e5c87
Patrick Hartling <patrick.hartling@gmail.com>
2005-05-23 01:11:33 +0000

    Fixed coding standard violations.  Removed trailing whitespace.  Wrapped long lines.  No functional changes.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17554 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/Pf/advanced/nav/velocityNav.h
+++ modules/vrjuggler/samples/Pf/advanced/nav/velocityNav.h
@@ -62,19 +62,43 @@ public:
    void setNavPosControl(std::string wand_dev);
 
    // Specify the combination of keys necessary to trigger the given state
-   void setAccelForwardActionCombo(std::vector<ActionState> combo) { mAccelForwardCombo = combo; }
-   void setBrakeActionCombo(std::vector<ActionState> combo) { mBrakeCombo = combo; }
-   void setStopActionCombo(std::vector<ActionState> combo) { mStopCombo = combo; }
-   void setRotationActionCombo(std::vector<ActionState> combo) { mRotateCombo = combo; }
-   void setResetActionCombo(std::vector<ActionState> combo) { mResetCombo = combo; }
+   void setAccelForwardActionCombo(std::vector<ActionState> combo)
+   {
+      mAccelForwardCombo = combo;
+   }
+
+   void setBrakeActionCombo(std::vector<ActionState> combo)
+   {
+      mBrakeCombo = combo;
+   }
+
+   void setStopActionCombo(std::vector<ActionState> combo)
+   {
+      mStopCombo = combo;
+   }
+
+   void setRotationActionCombo(std::vector<ActionState> combo)
+   {
+      mRotateCombo = combo;
+   }
+
+   void setResetActionCombo(std::vector<ActionState> combo)
+   {
+      mResetCombo = combo;
+   }
 
    // pass in velocity to add per second
-   void setAccel( const float& vel_per_sec ) { mAcceleration = vel_per_sec; }
+   void setAccel(const float& vel_per_sec)
+   {
+      mAcceleration = vel_per_sec;
+   }
 
    // set the highest velocity this nav can achieve.
    // default is 35
-   void setMaxVelocity( const float& velocity = 50.0f )
-   { mMaxVelocity = velocity; }
+   void setMaxVelocity(const float& velocity = 50.0f)
+   {
+      mMaxVelocity = velocity;
+   }
 
    //: arbitrary acceleration in any direction.
    //  nice for straefing, rising or diving. :)
@@ -84,22 +108,51 @@ public:
    //: zero all velocity
    void stop();
 
-   // TODO: add other directions to accelerate. (since it's hard coded to 0,0,-x in updateInteraction)
-   bool acceleratingForward() { return mAcceleratingForward;}
-   bool braking() { return mBraking; }
-   bool rotating() { return mRotating;}
-   bool stopping() { return mStopping;}
-   bool resetting() { return mResetting;}
+   // TODO: add other directions to accelerate. (since it's hard coded to
+   // 0,0,-x in updateInteraction)
+   bool acceleratingForward()
+   {
+      return mAcceleratingForward;
+   }
+
+   bool braking()
+   {
+      return mBraking;
+   }
+
+   bool rotating()
+   {
+      return mRotating;
+   }
+
+   bool stopping()
+   {
+      return mStopping;
+   }
+
+   bool resetting()
+   {
+      return mResetting;
+   }
 
    //: damping [0...1], where 0 stops immediately, .99 brakes very slowly
    //  default is no damping (1.0f)
    void setDamping( const float& damping = 1.0f );
 
    void setMode(navMode new_mode)
-   { mMode = new_mode; }
+   {
+      mMode = new_mode;
+   }
 
-   virtual gmtl::Vec3f getVelocity() const { return mVelocity; }
-   virtual float  getSpeed() const { return gmtl::length(mVelocity); }
+   virtual gmtl::Vec3f getVelocity() const
+   {
+      return mVelocity;
+   }
+
+   virtual float  getSpeed() const
+   {
+      return gmtl::length(mVelocity);
+   }
 
    // resets the navigator's matrix to the origin (set by setOrigin)
    virtual void reset();
@@ -107,14 +160,17 @@ public:
 protected:
    // set the rotational acceleration matrix
    // only rotational components of this matrix are used.
-   void setRotationalAcceleration( const gmtl::Matrix44f& mat )
-   { mRotationalAcceleration = mat;}
+   void setRotationalAcceleration(const gmtl::Matrix44f& mat)
+   {
+      mRotationalAcceleration = mat;
+   }
 
    // Rotate the environment
    void scaled_rotate(gmtl::Matrix44f rot_mat);
 
    // util func... used to convert gravity from meters...
-   inline void meters2feet( const gmtl::Vec3f& gravityInMeters, gmtl::Vec3f& gravityInFeet )
+   void meters2feet(const gmtl::Vec3f& gravityInMeters,
+                    gmtl::Vec3f& gravityInFeet)
    {
       float meters_to_feet = (100.0f / 0.3048f) * 0.01f;
       //float convtofeet = (meters / .3048 * 100.0f) * .01;
@@ -126,7 +182,10 @@ protected:
       METERS, FEET
    };
 
-   void setUnits( Units units = FEET ) { mUnits = units; }
+   void setUnits(Units units = FEET)
+   {
+      mUnits = units;
+   }
 
 private:
    gmtl::Vec3f mVelocity;
