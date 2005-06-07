24872403e3e85f3cb2e9eaa0a333ef8a56d18310
Patrick Hartling <patrick.hartling@gmail.com>
2005-06-07 02:42:26 +0000

    Update version numbers for the Beta 3 release.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17603 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- vc7/Sonix/snx/snxDefines.h
+++ vc7/Sonix/snx/snxDefines.h
@@ -70,12 +70,12 @@
 #define PACKAGE_NAME "Sonix"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Sonix 0.92"
+#define PACKAGE_STRING "Sonix 0.93"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "sonix"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.92"
+#define PACKAGE_VERSION "0.93"
 
 #endif
