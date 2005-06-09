5ceeb122287ac8ba424e6cf56b7e8a97cb02550d
Daniel Shipton <daniel.shipton.oss@gmail.com>
2005-06-09 06:18:54 +0000

    Get this checked in. A sample for grabbing within OSG. Grab math is still off for some reason...but need some version control before I hose it up real bad and change something for the worse.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17635 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/samples/OSG/advanced/OSGNavGrab/OsgNavGrab.cpp
@@ -0,0 +1,275 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <OsgNavGrab.h>
+
+#include <osg/Math>
+#include <osg/Geode>
+#include <osg/Material>
+#include <osg/Vec3>
+#include <osgUtil/Optimizer>
+#include <osgDB/ReadFile>
+
+#include <gmtl/Vec.h>
+#include <gmtl/Coord.h>
+#include <gmtl/Xforms.h>
+#include <gmtl/Math.h>
+
+OsgNavGrab::OsgNavGrab(vrj::Kernel* kern, int& argc, char** argv) : vrj::OsgApp(kern)
+{
+   mFileToLoad = std::string("");
+}
+
+void OsgNavGrab::latePreFrame()
+{
+   gmtl::Matrix44f world_transform;
+   gmtl::invertFull(world_transform, mNavigator.getCurPos());
+   // Update the scene graph
+   osg::Matrix osg_current_matrix;
+   osg_current_matrix.set(world_transform.getData());
+   mNavTrans->setMatrix(osg_current_matrix);
+}
+
+void OsgNavGrab::preFrame()
+{
+   //vprDEBUG(vprDBG_ALL, vprDBG_CRITICAL_LVL) << "------- preFrame ------\n" << vprDEBUG_FLUSH;
+   
+   vpr::Interval cur_time = mWand->getTimeStamp();
+   vpr::Interval diff_time(cur_time-mLastPreFrameTime);
+   if (mLastPreFrameTime.getBaseVal() >= cur_time.getBaseVal())
+   {  diff_time.secf(0.0f); }
+
+   float time_delta = diff_time.secf();
+
+   mLastPreFrameTime = cur_time;
+
+
+   // Get wand data
+   gmtl::Matrix44f wandMatrix = mWand->getData();
+
+   // If we are pressing button 1 then translate in the direction the wand is
+   // pointing.
+   if ( mButton0->getData() == gadget::Digital::ON )
+   {
+      gmtl::Vec3f direction;
+      gmtl::Vec3f Zdir = gmtl::Vec3f(0.0f, 0.0f, -10.0f);
+      gmtl::xform(direction, wandMatrix, Zdir);
+
+      mNavigator.setVelocity(direction);
+   }  // Make sure to reset the velocity when we stop pressing the button.
+   else if ( mButton0->getData() == gadget::Digital::TOGGLE_OFF)
+   {
+      mNavigator.setVelocity(gmtl::Vec3f(0.0, 0.0, 0.0));
+   }
+
+   // If we are pressing button 2 then rotate in the direction the wand is
+   // pointing.
+   if ( mButton2->getData() == gadget::Digital::ON )
+   {
+      gmtl::EulerAngleXYZf euler( 0.0f, gmtl::makeYRot(mWand->getData()), 0.0f );// Only allow Yaw (rot y)
+      gmtl::Matrix44f rot_mat = gmtl::makeRot<gmtl::Matrix44f>( euler );
+      mNavigator.setRotationalVelocity(rot_mat);
+   } // Make sure to reset the rotational velocity when we stop pressing the button.
+   else if(mButton2->getData() == gadget::Digital::TOGGLE_OFF)
+   {
+      mNavigator.setRotationalVelocity(gmtl::Matrix44f());
+   }
+
+   // Get the wand matrix in the units of this application.
+   const gmtl::Matrix44f wand_mat(mWand->getData(getDrawScaleFactor()));
+   updateGrabbing(wand_mat);
+   // Update the navigation using the time delta between
+   mNavigator.update(time_delta);
+   
+}
+
+void OsgNavGrab::bufferPreDraw()
+{
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+   glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void OsgNavGrab::initScene()
+{
+   // Initialize devices
+   const std::string wand("VJWand");
+   const std::string vjhead("VJHead");
+   const std::string but0("VJButton0");
+   const std::string but1("VJButton1");
+   const std::string but2("VJButton2");
+   const std::string but3("VJButton3");
+   const std::string but4("VJButton4");
+   const std::string but5("VJButton5");
+
+   mWand.init(wand);
+   mHead.init(vjhead);
+   mButton0.init(but0);
+   mButton1.init(but1);
+   mButton2.init(but2);
+   mButton3.init(but3);
+   mButton4.init(but4);
+   mButton5.init(but5);
+
+   myInit();
+
+}
+
+OsgNavGrab::GrabObject* OsgNavGrab::makeGrabbable(osg::Node* model,
+                                                        osg::MatrixTransform* modelPos)
+{
+   return new GrabObject(model, modelPos, modelPos->getMatrix());
+}
+
+
+void OsgNavGrab::updateGrabbing(const gmtl::Matrix44f& wandMatrix)
+{
+   // The navigation matrix is w_M_vw, so invert it for use here.
+   const osg::Matrix& nav_mat(mNavTrans->getMatrix());
+   osg::Matrix vw_M_w;
+   nav_mat.inverse(vw_M_w);
+
+   // Transform the wand matrix from real world coordinates into virtual world
+   // coordinates.
+   osg::Matrix osg_wandMatrix(
+            wandMatrix[0][0],wandMatrix[0][1],wandMatrix[0][2],wandMatrix[0][3],
+            wandMatrix[1][0],wandMatrix[1][1],wandMatrix[1][2],wandMatrix[1][3],
+            wandMatrix[2][0],wandMatrix[2][1],wandMatrix[2][2],wandMatrix[2][3],
+            wandMatrix[3][0],wandMatrix[3][1],wandMatrix[3][2],wandMatrix[3][3]);
+   const osg::Matrix wand_matrix = vw_M_w * osg_wandMatrix;
+
+   const osg::Vec3 wand_point(wand_matrix.getTrans());
+
+   // Only perform the intersection testing when we are not already grabbing
+   // an object.
+   if ( mGrabbedObj == NULL )
+   {
+      GrabObject* intersect_obj(NULL);
+
+      // Find the first object--if any--in mObjects with which the wand
+      // intersects.
+      for ( std::vector<GrabObject*>::iterator o = mObjects.begin();
+            o != mObjects.end();
+            ++o )
+      {
+         const osg::BoundingSphere& bbox = (*o)->xformNode->getBound();
+
+         if ( bbox.contains(wand_point) )
+         {
+            intersect_obj = *o;
+            break;
+         }
+      }
+
+      // If the intersected object changed since the last frame, we need to
+      // update things.
+      if ( intersect_obj != mIntersectedObj )
+      {
+         mIntersectedObj = intersect_obj;
+      }
+   }
+
+   // Enable grabbing only when no other object is currently grabbed, when
+   // the wand button is intersecting an object, and when button 1 is pressed.
+   if ( mIntersectedObj != NULL && mGrabbedObj == NULL &&
+        mButton1->getData() == gadget::Digital::ON )
+   {
+      mGrabbedObj = mIntersectedObj;
+   }
+   // We cannot be grabbing anything unless button 1 is pressed.
+   else if ( mButton1->getData() != gadget::Digital::ON )
+   {
+      mGrabbedObj = NULL;
+   }
+
+   // If mGrabbedObj is non-NULL, then we are grabbing the object pointed at
+   // by mGrabbedObj.
+   if ( mGrabbedObj != NULL )
+   {
+      osg::Matrix new_xlate = mGrabbedObj->xformCore->getMatrix(); // could be a bug... ?take old and apply...or
+                             // just apply
+      new_xlate.makeTranslate(wand_point);
+      mGrabbedObj->xformCore->setMatrix(new_xlate);
+
+   }
+}
+
+
+void OsgNavGrab::myInit()
+{
+   //
+   //          /-- mNoNav
+   // mRootNode
+   //         \-- mNavTrans -- mModelTrans -- mModel
+
+   //The top level nodes of the tree
+   mRootNode = new osg::Group();
+   mNoNav    = new osg::Group();
+   mNavTrans = new osg::MatrixTransform();
+
+
+   mNavigator.init();
+
+   mRootNode->addChild( mNoNav );
+   mRootNode->addChild( mNavTrans );
+
+   //Load the model
+   std::cout << "Attempting to load file: " << mFileToLoad << "... ";
+   mModel = osgDB::readNodeFile(mFileToLoad);
+   std::cout << "done." << std::endl;
+
+   // Transform node for the model
+   mModelTrans  = new osg::MatrixTransform();
+   //This can be used if the model orientation needs to change
+   mModelTrans->preMult( osg::Matrix::rotate( gmtl::Math::deg2Rad( -90.0f ), 1.0f, 0.0f, 0.0f) );
+
+
+   if(NULL == mModel)
+   {
+      std::cout << "ERROR: Could not load file: " << mFileToLoad << std::endl;
+   }
+   else
+   {
+      // Add model to the transform
+      mModelTrans->addChild(mModel);
+   }
+   mObjects.push_back(makeGrabbable(mModel, mModelTrans));
+   
+   // Add the transform to the tree
+   mNavTrans->addChild( mModelTrans );
+
+   // run optimization over the scene graph
+   osgUtil::Optimizer optimizer;
+   optimizer.optimize(mRootNode);
+
+
+}
+
---
+++ modules/vrjuggler/samples/OSG/advanced/OSGNavGrab/OsgNavGrab.h
@@ -0,0 +1,223 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2005 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date$
+ * Version:       $Revision$
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _OSG_NAV_GRAB
+#define _OSG_NAV_GRAB
+
+#include <vrj/vrjConfig.h>
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+#include <vrj/Draw/OGL/GlApp.h>
+#include <gadget/Type/Position/PositionUnitConversion.h>
+
+#include <gadget/Type/PositionInterface.h>
+#include <gadget/Type/AnalogInterface.h>
+#include <gadget/Type/DigitalInterface.h>
+
+//OSG  includes
+#include <osg/Matrix>
+#include <osg/Transform>
+#include <osg/MatrixTransform>
+
+#include <osgUtil/SceneView>
+
+#include "nav.h"
+
+#include <vrj/Draw/OSG/OsgApp.h>
+
+/**
+ * Demonstration Open Scene Graph application class
+ */
+class OsgNavGrab : public vrj::OsgApp
+{
+public:
+   OsgNavGrab(vrj::Kernel* kern, int& argc, char** argv);
+
+   virtual ~OsgNavGrab()
+   {
+      /* Do nothing. */ ;
+   }
+
+   // Execute any initialization needed before the API is started<BR><BR>
+   //
+   // This is called once before OGL is initialized
+   virtual void initScene();
+
+   void myInit();
+
+   virtual osg::Group* getScene()
+   {
+      return mRootNode;
+   }
+
+   virtual void configSceneView(osgUtil::SceneView* newSceneViewer)
+   {
+      vrj::OsgApp::configSceneView(newSceneViewer);
+
+      newSceneViewer->getLight()->setAmbient(osg::Vec4(0.3f,0.3f,0.3f,1.0f));
+      newSceneViewer->getLight()->setDiffuse(osg::Vec4(0.9f,0.9f,0.9f,1.0f));
+      newSceneViewer->getLight()->setSpecular(osg::Vec4(1.0f,1.0f,1.0f,1.0f));
+   }
+
+   void bufferPreDraw();
+
+   // ----- Drawing Loop Functions ------
+   //
+   //  The drawing loop will look similar to this:
+   //
+   //  while (drawing)
+   //  {
+   //        preFrame();
+   //       <Application Data Syncronization>
+   //        latePreFrame();
+   //       draw();
+   //        intraFrame();     // Drawing is happening while here
+   //       sync();
+   //        postFrame();      // Drawing is now done
+   //
+   //       UpdateTrackers();
+   //  }
+   //------------------------------------
+
+   //: Function called after tracker update but before start of drawing<BR><BR>
+   //
+   // called once before every frame.
+   virtual void preFrame();
+
+   // Function called after ApplicationData syncronization but before draw()
+   virtual void latePreFrame();
+   
+   //: Function called after drawing has been triggered but BEFORE it completes<BR><BR>
+   //
+   // called once during each frame
+   virtual void intraFrame()
+   {
+      // Put your intra frame computations here.
+   }
+
+   //: Function called before updating trackers but after the frame is drawn<BR><BR>
+   //
+   // called once after every frame
+   virtual void postFrame()
+   {
+      // Put your post frame computations here.
+   }
+
+   void setModelFileName(std::string filename)
+   {
+      mFileToLoad = filename;
+   }
+
+   /**
+    * Returns the scale factor for this application.  For this sample, we
+    * are handling everything in feet.
+    */
+   virtual float getDrawScaleFactor()
+   {
+      return gadget::PositionUnitConversion::ConvertToFeet;
+   }
+
+private:
+   osg::Group*             mRootNode;
+   osg::Group*             mNoNav;
+   osg::MatrixTransform*   mNavTrans;
+   osg::MatrixTransform*   mModelTrans;
+   osg::Node*              mModel;
+
+   OsgNavigator  mNavigator;       /** Navigation class */
+
+   std::string mFileToLoad;
+
+   vpr::Interval           mLastPreFrameTime;   /**< Time of the start of the last preframe */
+
+   struct GrabObject
+   {
+      GrabObject(osg::Node* node, osg::MatrixTransform* xform,
+            const osg::Matrix& pos)
+         : xformNode(node)
+         , xformCore(xform)
+         , homePos(pos)
+      {
+         ;
+      }
+
+      /** The transform node (parent) for the grabbable object. */
+      osg::Node* xformNode;
+
+      /** The transform core for the grabbable object. */
+      osg::MatrixTransform* xformCore;
+
+      /**
+      * The original transformation for the grabbable object.  This is used
+      * for resetting the scene.
+      */
+      const osg::Matrix homePos;
+   };
+
+   /**
+    * Creates a GrabObject instance for the given model that is to be located
+    * initially at the given position.
+    *
+    * @param model      The model (i.e., geometric object) that will be
+    *                   grabbable.
+    * @param modelPos   The starting position for the given model.
+    *
+    * @return A new GrabObject instance.  It is the responsibility of the
+    *         caller to release the memory.
+    */
+   GrabObject* makeGrabbable(osg::Node* model, osg::MatrixTransform* modelPos);
+
+   void updateGrabbing(const gmtl::Matrix44f& wandMatrix);
+
+   /** @name Grabbed object management */
+   //@{
+   std::vector<GrabObject*> mObjects;
+   GrabObject*              mIntersectedObj;
+   GrabObject*              mGrabbedObj;
+   //@}
+
+public:
+   gadget::PositionInterface  mWand;     // the Wand
+   gadget::PositionInterface  mHead;     // the head
+   gadget::DigitalInterface   mButton0;
+   gadget::DigitalInterface   mButton1;
+   gadget::DigitalInterface   mButton2;
+   gadget::DigitalInterface   mButton3;
+   gadget::DigitalInterface   mButton4;
+   gadget::DigitalInterface   mButton5;
+};
+
+
+#endif
