cbd7a020e4156be29de4618a92e9c20b96a70d99
Patrick Hartling <patrick.hartling@gmail.com>
2005-06-10 21:25:25 +0000

    When running on Linux/amd64, look in lib64/vrjuggler/plugins for plug-ins.
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17668 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Performance/PerformanceMediator.cpp
+++ modules/vrjuggler/vrj/Performance/PerformanceMediator.cpp
@@ -144,9 +144,10 @@ namespace vrj
          return;
       }
 
-#if defined(_ABIN32)
+#if defined(VPR_OS_IRIX) && defined(_ABIN32)
       const std::string bit_suffix("32");
-#elif defined(_ABI64)
+#elif defined(VPR_OS_IRIX) && defined(_ABI64) || \
+      defined(VPR_OS_Linux) && defined(__x86_64__)
       const std::string bit_suffix("64");
 #else
       const std::string bit_suffix("");
