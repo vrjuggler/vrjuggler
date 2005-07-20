c64d1530a058e5d0d98c424eeed09bfa1afa2974
Patrick Hartling <patrick.hartling@gmail.com>
2005-07-20 16:21:44 +0000

    MFH: Cleaned up formatting and added a call to pfExit() at the end of      main().
    The specific revisions merged are as follows:
    
       pfNav.cpp: 1.11, 1.12
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/branches/VRJ_RELENG_2_0_BRANCH/juggler@17900 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/samples/Pf/advanced/pfNav/pfNav.cpp
+++ modules/vrjuggler/samples/Pf/advanced/pfNav/pfNav.cpp
@@ -41,78 +41,74 @@
 
 void usage(char** argv)
 {
-   std::cout<<"_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n"<<std::flush;
-   std::cout<<"\n"<<std::flush;
-   std::cout<<"Usage:\n";
-   std::cout<<"      "<<argv[0]<<" modelfile.flt bump_thresh vjconfigfile[0] vjconfigfile[1] ... vjconfigfile[n] (for plib's SL library)\n";
-   std::cout<<"_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n"<<std::flush;
+   std::cout << "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/\n\n"
+             << "Usage:\n"
+             << "      " << argv[0] << " modelfile.flt bump_thresh "
+             << "vjconfigfile[0] vjconfigfile[1] ... vjconfigfile[n]\n"
+             << "_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/"
+             << std::endl;
 }
-
-// The app class we want to use
-// We are required to have a default constructor
-// so create a new class whose constructor configures the simplePfNavApp
-//
-class myPfNavApp : public simplePfNavApp
-{
-public:
-   myPfNavApp()
-   {
-   }
-
-   // Empty virtual destructor.
-   virtual ~myPfNavApp (void)
-   {
-      /* Do nothing. */ ;
-   }
-};
 
 int main(int argc, char* argv[])
 {
    vrj::Kernel* kernel = vrj::Kernel::instance(); // Declare a new Kernel
-   simplePfNavApp* application = new simplePfNavApp();  // Delcare an instance of my application
 
-   std::string file_path( "" );
-   const float dcs_scale( 1.0f );
-   const gmtl::Vec3f dcs_trans( 0.0f, 0.0f, 0.0f );
+   // Delcare an instance of my application
+   simplePfNavApp* application = new simplePfNavApp();
+
+   std::string file_path("");
+   const float dcs_scale(1.0f);
+   const gmtl::Vec3f dcs_trans(0.0f, 0.0f, 0.0f);
    const gmtl::Vec3f dcs_rot(0.0f, 0.0f, 0.0f);
-   const gmtl::Vec3f initial_pos( 0.0f, 0.0f, 0.0f );
+   const gmtl::Vec3f initial_pos(0.0f, 0.0f, 0.0f);
 
-   if (argc < 2)
+   if ( argc < 2 )
    {
-      usage( argv );
-      std::cout<<"\n\n[ERROR!!!] you must supply a model database (then config files)\n\n"<<std::flush;
+      usage(argv);
+      std::cout << "\n\n[ERROR!!!] you must supply a model database followed "
+                << "by VR Juggler config files." << std::endl;
       return 1;
    }
    
    std::string model_filename = argv[1];
-   std::cout<<"Will load: "<<model_filename.c_str()<<"\n"<<std::flush;
+   std::cout << "Will load: " << model_filename << std::endl;
 
-   if (argc < 3)
+   if ( argc < 3 )
    {
-      std::cout<<"\n\n[ERROR!!!] you must supply config files after the model file...\n\n"<<std::flush;
+      usage(argv);
+      std::cout << "\n\n[ERROR!!!] you must supply VR Juggler config files "
+                << "after the model file." << std::endl;
       return 2;
    }
 
-   std::cout<<"==========\n"<<std::flush;
+   std::cout << "==========" << std::endl;
    for ( int i = 2; i < argc; ++i )
    {
-      std::cout<<"Loading Config File: "<<argv[i]<<"\n"<<std::flush;
-      kernel->loadConfigFile( argv[i] );
+      std::cout << "Loading Config File: " << argv[i] << std::endl;
+      kernel->loadConfigFile(argv[i]);
    }
-   std::cout<<"===========\n"<<std::flush;
 
-    kernel->start();
+   std::cout << "===========" << std::endl;
 
-    // Configure that application
-    application->addModel( simplePfNavApp::Model(model_filename,model_filename,dcs_scale,dcs_trans, dcs_rot,true));
-    application->setFilePath( file_path );
-    application->setInitialNavPos( initial_pos );
+   kernel->start();
 
-    kernel->setApplication( application );    // Set up the kernel
+   // Configure that application
+   application->addModel(simplePfNavApp::Model(model_filename, model_filename,
+                                               dcs_scale, dcs_trans, dcs_rot,
+                                               true));
+   application->setFilePath(file_path);
+   application->setInitialNavPos(initial_pos);
+
+   kernel->setApplication(application);    // Set up the kernel
 
    kernel->waitForKernelStop();
 
    delete application;
+
+   // Tell Performer to clean up its resources.  A side effect of this call
+   // is that the standard library exit() function will be called, so nothing
+   // after this call to pfExit() will be executed.
+   pfExit();
 
    return 0;
 }
