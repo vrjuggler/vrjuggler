2ebf00a9e7fc649d87bfeae4e9f315d6d0bcef0f
Patrick Hartling <patrick.hartling@gmail.com>
2005-08-10 18:10:44 +0000

    When running the JVM, specify the full path to the java.exe file instead of relying on it being in the user's path. Too many times recently, I have run into situations where there was a java.exe in my path that was no the one that I wanted to be using.
    Now that the JVM launching works the same way as it does in the UNIX case,
    we also validate the use of the environment variable used to identify the
    Java installation (%JAVA_HOME% or %JDK_HOME%).
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/trunk/juggler@17931 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrjconfig/vrjconfig.bat
+++ modules/vrjuggler/vrjconfig/vrjconfig.bat
@@ -30,6 +30,21 @@ rem Version:       $Revision$
 rem -----------------------------------------------------------------
 rem
 rem ************** <auto-copyright.pl END do not edit this line> **************
+IF NOT "%JAVA_HOME%" == "" GOTO TEST_JAVA_EXIST
+IF "%JDK_HOME%" == "" GOTO JAVA_ERR
+ECHO [NOTE] Setting JAVA_HOME to "%JDK_HOME%"
+set JAVA_HOME=%JDK_HOME%
+GOTO TEST_JAVA_EXIST
+:JAVA_ERR
+ECHO [ERR] Neither JAVA_HOME nor JDK_HOME is set.  One of these environment
+ECHO [ERR] variables must be set to a valid Java 1.4 or newer installation
+ECHO [ERR] directory to run VRJConfig.
+GOTO ERREXIT
+:TEST_JAVA_EXIST
+IF EXIST "%JAVA_HOME%\bin\java.exe" GOTO TEST_VJ_BASE
+ECHO [ERR] %JAVA_HOME%\bin\java.exe does not exist, so VRJConfig cannot be
+ECHO [ERR] started.
+:TEST_VJ_BASE
 IF NOT "%VJ_BASE_DIR%" == "" GOTO TEST_VJ_EXIST
 ECHO [ERR] VJ_BASE_DIR unset; please set the environment variable VJ_BASE_DIR to
 ECHO [ERR] point to your VR Juggler installation directory.  For more information,
@@ -62,7 +77,7 @@ GOTO ERREXIT
 IF NOT "%JCCL_DEFINITION_PATH%" == "" GOTO RUNJAVA
 set jccl_def_arg=-DJCCL_DEFINITION_PATH="%JCCL_DEFINITION_PATH%"
 :RUNJAVA
-java -Dsun.java2d.opengl=True -DTWEEK_BASE_DIR="%TWEEK_BASE_DIR%" -DJCCL_BASE_DIR="%JCCL_BASE_DIR%" -DVJ_BASE_DIR="%VJ_BASE_DIR%" %jccl_def_arg% -Djava.library.path="%TWEEK_BASE_DIR%\share\tweek\java\%PROCESSOR_ARCHITECTURE%" -Djava.security.policy="%TWEEK_BASE_DIR%\share\tweek\java\java.security.policy.txt" -cp "%TWEEK_BASE_DIR%\share\tweek\java\jaxen-core.jar;%TWEEK_BASE_DIR%\share\tweek\java\jaxen-jdom.jar;%TWEEK_BASE_DIR%\share\tweek\java\jdom.jar;%TWEEK_BASE_DIR%\share\tweek\java\saxpath.jar;%TWEEK_BASE_DIR%\share\tweek\java\xalan.jar;%TWEEK_BASE_DIR%\share\tweek\java\xerces.jar;%TWEEK_BASE_DIR%\share\tweek\java\xml-apis.jar;%TWEEK_BASE_DIR%\share\tweek\java\Tweek.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekBeans.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekEvents.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekNet.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekBeanDelivery.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekServices.jar;%TWEEK_BASE_DIR%\share\tweek\java\kunststoff-mod.jar;%TWEEK_BASE_DIR%\share\tweek\java\liquidlnf.jar;%TWEEK_BASE_DIR%\share\tweek\java\metouia.jar;%TWEEK_BASE_DIR%\share\tweek\java\looks.jar" org.vrjuggler.tweek.Tweek --beanpath="%JCCL_BASE_DIR%\share\jccl\beans" --beanpath="%VJ_BASE_DIR%\share\vrjuggler\beans" --defaultbean="Configuration Editor" %*
+"%JAVA_HOME%\bin\java" -Dsun.java2d.opengl=True -DTWEEK_BASE_DIR="%TWEEK_BASE_DIR%" -DJCCL_BASE_DIR="%JCCL_BASE_DIR%" -DVJ_BASE_DIR="%VJ_BASE_DIR%" %jccl_def_arg% -Djava.library.path="%TWEEK_BASE_DIR%\share\tweek\java\%PROCESSOR_ARCHITECTURE%" -Djava.security.policy="%TWEEK_BASE_DIR%\share\tweek\java\java.security.policy.txt" -cp "%TWEEK_BASE_DIR%\share\tweek\java\jaxen-core.jar;%TWEEK_BASE_DIR%\share\tweek\java\jaxen-jdom.jar;%TWEEK_BASE_DIR%\share\tweek\java\jdom.jar;%TWEEK_BASE_DIR%\share\tweek\java\saxpath.jar;%TWEEK_BASE_DIR%\share\tweek\java\xalan.jar;%TWEEK_BASE_DIR%\share\tweek\java\xerces.jar;%TWEEK_BASE_DIR%\share\tweek\java\xml-apis.jar;%TWEEK_BASE_DIR%\share\tweek\java\Tweek.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekBeans.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekEvents.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekNet.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekBeanDelivery.jar;%TWEEK_BASE_DIR%\share\tweek\java\TweekServices.jar;%TWEEK_BASE_DIR%\share\tweek\java\kunststoff-mod.jar;%TWEEK_BASE_DIR%\share\tweek\java\liquidlnf.jar;%TWEEK_BASE_DIR%\share\tweek\java\metouia.jar;%TWEEK_BASE_DIR%\share\tweek\java\looks.jar" org.vrjuggler.tweek.Tweek --beanpath="%JCCL_BASE_DIR%\share\jccl\beans" --beanpath="%VJ_BASE_DIR%\share\vrjuggler\beans" --defaultbean="Configuration Editor" %*
 GOTO DONE
 :ERREXIT
 ECHO [ERR] VRJConfig exiting due to previous errors.
