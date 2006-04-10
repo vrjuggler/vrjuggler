76395bc6c1a9c209941e38dd4aeb49ab878a1caa
Aron Bierbaum <aronbierbaum@gmail.com>
2006-04-10 20:07:24 +0000

    - Remove commands that are not needed.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@18482 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/gadgeteer/tools/serial/pinchtest/Command.py
+++ modules/gadgeteer/tools/serial/pinchtest/Command.py
@@ -1,30 +1,5 @@
 #!/bin/env/python
 
-#Angles = 0x57
-#ChangeValue = 0x50
-#ExamineValue = 0x4F
-#FbbReset = 0x2F
-#Hemisphere = 0x4C
-#Matrix = 0x58
-#NextTransmitter = 0x30
-#Point = 0x42
-#Position = 0x56
-#PositionAngles = 0x59
-#PositionMatrix = 0x5A
-#PositionQuaternion = 0x5D
-#Quaternion = 0x5C
-#ReportRate1 = 0x51
-#ReportRate2 = 0x52
-#ReportRate8 = 0x53
-#ReportRate32 = 0x54
-#ToFbbNormal = 0xF0
-#ToFbbExpanded = 0xE0
-#ToFbbSuperExpanded = 0xA0
-#Run = 0x46
-#Sleep = 0x47
-#Stream = 0x40
-#StreamStop = 0x3F
-
 START_BYTE_NTS = 0x80
 START_BYTE_TS = 0x81
 START_BYTE = 0x82
