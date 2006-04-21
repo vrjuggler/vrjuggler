68851ed803f5ef12f03a1e7bf880c4856dee33c9
Patrick Hartling <patrick.hartling@gmail.com>
2006-04-21 15:08:35 +0000

    MF20 [rev 18706]: Added ClusterTestMode.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@18716 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/test/Draw/OGL/WallTest/ClusterTestMode.h
@@ -0,0 +1,63 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile$
+ * Date modified: $Date: 2006-02-08 13:44:09 -0600 (Wed, 08 Feb 2006) $
+ * Version:       $Revision: 18378 $
+ * -----------------------------------------------------------------
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _STEREO_EYE_TEST_MODE_H_
+#define _STEREO_EYE_TEST_MODE_H_
+
+#include <TestMode.h>
+#include <Text.h>
+#include <vrj/Draw/OGL/GlContextData.h>
+
+
+class WallTest;
+
+class ClusterTestMode : public TestMode
+{
+public:
+   ClusterTestMode()
+      : mSleepTime(0.25f)
+   {;}
+   virtual std::string getName()
+   {
+      return std::string("ClusterTestMode");
+   }
+   
+   virtual void update(WallTest* wallTest);
+
+   virtual void draw(WallTest* wallTest);
+
+   vrj::GlContextData<Text>   mText;         /**< Context specific copy of text. */
+   float mSleepTime;
+};
+
+
+#endif
