06815833653a00141c38923669f7da7a8e8da02b
Patrick Hartling <patrick.hartling@gmail.com>
2006-12-07 04:32:11 +0000

    Added some helper code for printing out Windows error messages.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@19578 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/md/WIN32/Util/ErrorImplWin32.cpp
@@ -0,0 +1,79 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <vpr/vprConfig.h>
+
+#include <vpr/md/WiN32/Util/ErrorImplWin32.h>
+
+
+namespace vpr
+{
+
+std::string ErrorImplWin32::getCurrentErrorMsg()
+{
+   char* msg_buffer(NULL);
+
+   DWORD error_code = GetLastError();
+   FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                 NULL, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                 (LPTSTR) &msg_buffer, 0, NULL);
+
+   std::string err_str;
+
+   if ( NULL != msg_buffer )
+   {
+      err_str = msg_buffer;
+      LocalFree(msg_buffer);
+   }
+
+   return err_str;
+}
+
+void ErrorImplWin32::outputCurrentError(std::ostream& out,
+                                        const std::string& prefix)
+{
+   const std::string err_str(getCurrentErrorMsg());
+
+   out << "Error (WIN32): " << prefix << " (" << errno;
+
+   if ( ! err_str.empty() )
+   {
+      out << ", " << err_str;
+   }
+
+   out << ")" << std::endl;
+}
+
+}
---
+++ modules/vapor/vpr/md/WIN32/Util/ErrorImplWin32.h
@@ -0,0 +1,84 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2006 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VPR_ERROR_IMPL_WIN32_H_
+#define _VPR_ERROR_IMPL_WIN32_H_
+
+#include <vpr/vprConfig.h>
+#include <iostream>
+#include <string>
+#include <vpr/Util/ErrorBase.h>
+
+
+namespace vpr
+{
+
+/** \class ErrorImplWin32 ErrorImplWin32.h vpr/Util/Error.h
+ *
+ * Win32 implementation of cross-platform error reporting class.
+ *
+ * @since 1.1.36
+ */
+class VPR_CLASS_API ErrorImplWin32 : public ErrorBase
+{
+public:
+   static std::string getCurrentErrorMsg();
+
+   static void outputCurrentError(std::ostream& out,
+                                  const std::string& prefix);
+
+   static ErrorType getCurrentError()
+   {
+      return NoError;
+   }
+
+protected:
+/*
+   static int convertErrorVprToNspr(ErrorType mask)
+   {
+      return 0;
+   }
+
+   static ErrorType convertErrorNsprToVpr(int mask)
+   {
+      return NoError;
+   }
+*/
+};
+
+} // End of vpr namespace
+
+
+#endif  /* _VPR_ERROR_IMPL_WIN32_H_ */
