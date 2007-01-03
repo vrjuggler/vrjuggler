28446b11bffcc7bfd52cdfd4db37e9dd61d2e38c
Patrick Hartling <patrick.hartling@gmail.com>
2007-01-03 19:31:41 +0000

    It's Direct3D_Draw_Manager, not DirectX_Draw_Manager.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@19743 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- build_windows.py
+++ build_windows.py
@@ -1467,7 +1467,7 @@ def installVRJuggler(prefix, buildDir):
    srcroot = os.path.join(buildDir, 'VRJuggler', 'Performer_Draw_Manager')
    installLibs(srcroot, destdir)
 
-   srcroot = os.path.join(buildDir, 'VRJuggler', 'DirectX_Draw_Manager')
+   srcroot = os.path.join(buildDir, 'VRJuggler', 'Direct3D_Draw_Manager')
    installLibs(srcroot, destdir)
 
    destdir = os.path.join(prefix, 'lib', 'flagpoll')
