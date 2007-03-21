fd82028ba0d008c8779663d5a3362ec1cb255953
Patrick Hartling <patrick.hartling@gmail.com>
2007-03-21 12:54:34 +0000

    Ensure that the polymorphic type vrj::DrawSimInterface is compiled into the correct shared library. I still need to update the Windows build for the addition of DrawSimInterface.cpp.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@19872 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/DrawSimInterface.cpp
+++ modules/vrjuggler/vrj/Draw/DrawSimInterface.cpp
@@ -24,44 +24,24 @@
  *
  *************** <auto-copyright.pl END do not edit this line> ***************/
 
-#ifndef _VRJ_DRAW_SIM_INTERFACE_H_
-#define _VRJ_DRAW_SIM_INTERFACE_H_
-
 #include <vrj/vrjConfig.h>
 
-#include <jccl/Config/ConfigElementPtr.h>
+#include <vrj/Draw/DrawSimInterface.h>
 
 
 namespace vrj
 {
-   class Projection;
-   class SimViewport;
 
-   /** \class DrawSimInterface DrawSimInterface.h vrj/Draw/DrawSimInterface.h
-    *
-    * Base class for all simulator interface across all Draw Manager types.
-    */
-   class DrawSimInterface
-   {
-   public:
-      virtual ~DrawSimInterface();
-
-      /**
-       * Configures the simulator interface.
-       *
-       * @pre element is a valid configuration element.
-       * @post It should be configured.
-       */
-      virtual bool config(jccl::ConfigElementPtr element) = 0;
-
-      /** Called as part of the viewports updateProjection call. */
-      virtual void updateProjectionData(const float positionScale,
-                                        Projection* leftProj,
-                                        Projection* rightProj);
-
-      virtual void initialize(SimViewport* simVp) = 0;
-   };
+DrawSimInterface::~DrawSimInterface()
+{
+   /* Do nothing. */ ;
 }
 
+/** Called as part of the viewports updateProjection call. */
+void DrawSimInterface::updateProjectionData(const float, Projection*,
+                                            Projection*)
+{
+   /* Do nothing. */ ;
+}
 
-#endif
+}
