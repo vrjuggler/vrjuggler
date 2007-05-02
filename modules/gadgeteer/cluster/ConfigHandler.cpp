7d139e115ef4590ec78e402cd788716a2e4f0eae
Aron Bierbaum <aronbierbaum@gmail.com>
2007-05-02 22:09:25 +0000

    Commit ConfigHandler that I forgot in last revision.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@20112 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/cluster/ConfigHandler.cpp
@@ -0,0 +1,83 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <gadget/gadgetConfig.h>
+#include <boost/concept_check.hpp>
+#include <jccl/Config/Configuration.h>
+#include <jccl/RTRC/ConfigManager.h>
+#include <cluster/ConfigHandler.h>
+#include <cluster/Packets/ConfigPacket.h>
+
+namespace cluster
+{
+
+ConfigHandler::ConfigHandler()
+{;}
+
+ConfigHandler::~ConfigHandler()
+{;}
+
+ConfigHandlerPtr ConfigHandler::create()
+{
+   return ConfigHandlerPtr(new ConfigHandler());
+}
+
+void ConfigHandler::recoverFromLostNode( gadget::NodePtr node )
+{
+   boost::ignore_unused_variable_warning(node);
+
+   /*
+   vpr::Guard<vpr::Mutex> guard( mPluginsLock );
+
+   for ( plugin_list_t::iterator itr = mPlugins.begin(); itr != mPlugins.end(); itr++ )
+   {
+      (*itr)->recoverFromLostNode( node );
+   }
+   */
+}
+
+void ConfigHandler::handlePacket(cluster::PacketPtr packet, gadget::NodePtr node)
+{
+   boost::ignore_unused_variable_warning(node);
+
+   vprASSERT(Header::CONFIG_PACKET == packet->getPacketType() && "Not a config packet.");
+   cluster::ConfigPacketPtr cfg_pkt = boost::dynamic_pointer_cast<cluster::ConfigPacket>(packet);
+   vprASSERT(NULL != cfg_pkt && "Failed to cast ConfigPacket.");
+
+   jccl::Configuration incoming_config;
+   //Loading from an istream
+   std::istringstream config_input(cfg_pkt->getConfig());
+   config_input >> incoming_config;
+
+   vprDEBUG( gadgetDBG_RIM, vprDBG_CONFIG_LVL )
+      << clrOutBOLD( clrCYAN, "[ConfigHandler] " )
+      << "Got configuration packet."
+      << std::endl << vprDEBUG_FLUSH;
+
+   jccl::ConfigManager::instance()->addConfigurationAdditions(&incoming_config);
+}
+
+} // End of cluster namespace
---
+++ modules/gadgeteer/cluster/ConfigHandler.h
@@ -0,0 +1,89 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _GADGET_CONFIG_HANDLER_H_
+#define _GADGET_CONFIG_HANDLER_H_
+
+#include <boost/noncopyable.hpp>
+
+#include <gadget/gadgetConfig.h>
+#include <gadget/PacketHandler.h>
+#include <gadget/Node.h>
+#include <cluster/ConfigHandlerPtr.h>
+
+namespace cluster
+{
+
+/** \class ConfigHandler ConfigHandler.h gadget/ConfigHandler.h
+ *
+ * Packet handler interface used for network communication.
+ */
+class GADGET_CLASS_API ConfigHandler
+   : public gadget::PacketHandler
+   , boost::noncopyable
+{
+protected:
+   ConfigHandler();
+
+public:
+   virtual ~ConfigHandler();
+
+   /**
+    * Creates a ConfigHandler instance and returns it wrapped in a
+    * ConfigHandlerPtr object.
+    *
+    * @since 1.3.7
+    */
+   static ConfigHandlerPtr create();
+
+   /**
+    * Get the GUID associated with this handler.
+    */
+   virtual vpr::GUID getHandlerGUID()
+   {
+      return vpr::GUID("f3ea94e2-82fc-43f6-a57f-474d3fd1d6eb");
+   }
+   
+   virtual std::string getHandlerName()
+   {
+      return std::string("Config Handler");
+   }
+
+   /**
+    * Handle a incoming packet.
+    */
+   virtual void handlePacket(cluster::PacketPtr packet, gadget::NodePtr node);
+   
+   /**
+    * Virtual function used to inform all handlers that the network
+    * has lost its connection to the given Node.
+    */
+   virtual void recoverFromLostNode(gadget::NodePtr lostNode);
+};
+
+} // end namespace gadget
+
+#endif /*_GADGET_CONFIG_HANDLER_H_*/
