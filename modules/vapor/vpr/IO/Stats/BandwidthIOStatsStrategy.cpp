05a854c801cda6210e21bd78d8c4f400513d3ea4
Patrick Hartling <patrick.hartling@gmail.com>
2007-05-23 15:13:44 +0000

    MFT r20257: Moved the implementation of virtual functions in             vpr::BandwidthIOStatsStrategy into a .cpp file. This ensures that             this polymorphic type is compiled into the correct shared library.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/branches/2.2@20266 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vapor/vpr/IO/Stats/BandwidthIOStatsStrategy.cpp
@@ -0,0 +1,75 @@
+/****************** <VPR heading BEGIN do not edit this line> *****************
+ *
+ * VR Juggler Portable Runtime
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Patrick Hartling, Kevin Meinert, Carolina Cruz-Neira
+ *
+ ****************** <VPR heading END do not edit this line> ******************/
+
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2007 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <vpr/vprConfig.h>
+
+#include <vpr/IO/Stats/BandwidthIOStatsStrategy.h>
+
+
+namespace vpr
+{
+
+BandwidthIOStatsStrategy::BandwidthIOStatsStrategy()
+{
+   /* Do nothing. */ ;
+}
+
+BandwidthIOStatsStrategy::~BandwidthIOStatsStrategy()
+{
+   /* Do nothing. */ ;
+}
+
+void BandwidthIOStatsStrategy::read_s(void*, const vpr::Uint32,
+                                      vpr::Uint32& bytesRead,
+                                      const vpr::Interval&)
+{
+   mReadStats.addSample(bytesRead);
+}
+
+void BandwidthIOStatsStrategy::readn_s(void*, const vpr::Uint32,
+                                       vpr::Uint32& bytesRead,
+                                       const vpr::Interval&)
+{
+   mReadStats.addSample(bytesRead);
+}
+
+void BandwidthIOStatsStrategy::write_s(const void*, const vpr::Uint32,
+                                       vpr::Uint32& bytesWritten,
+                                       const vpr::Interval&)
+{
+   mWriteStats.addSample(bytesWritten);
+}
+
+} //namespace vpr
