1d4ec3103d31a9aac2eaf2487d7c6aa09774d450
Patrick Hartling <patrick.hartling@gmail.com>
2007-08-09 20:52:12 +0000

    MF22 r20654: Don't hard code the use of VJHead. Instead, retrieve the              position proxy from the first configured vrj::User instance in              the kernel.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/branches/2.0@20656 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/OSG/OsgApp.h
+++ modules/vrjuggler/vrj/Draw/OSG/OsgApp.h
@@ -38,13 +38,14 @@
 #include <vpr/Sync/Mutex.h>
 #include <vpr/Sync/Guard.h>
 
-#include <gadget/Type/PositionInterface.h>
-
 #include <vrj/Draw/OGL/GlApp.h>
 #include <vrj/Draw/OGL/GlContextData.h>
 #include <vrj/Draw/OGL/GlWindow.h>
 
 #include <vrj/Display/CameraProjection.h>
+
+#include <vrj/Kernel/Kernel.h>
+#include <vrj/Kernel/User.h>
 
 #include <osg/Version>
 #include <osg/Vec3>
@@ -168,8 +169,6 @@ public:
 
       GlApp::init();
 
-      mHead.init("VJHead");
-
       //Create the scene
       this->initScene();
    }
@@ -269,7 +268,9 @@ protected:
       // Update the frame stamp with information from this frame.
       mFrameStamp->setFrameNumber(mFrameNumber);
 
-      const double head_time(mHead->getTimeStamp().secd());
+      const double head_time(
+         mKernel->getUsers()[0]->getHeadPosProxy()->getTimeStamp().secd()
+      );
       mFrameStamp->setReferenceTime(head_time);
 #if OSG_VERSION_MAJOR == 1 && OSG_VERSION_MINOR > 2 || OSG_MAJOR_VERSION >= 2
       // This is available in OSG 1.9 and newer.
@@ -295,7 +296,6 @@ private:
    osg::ref_ptr<osg::FrameStamp> mFrameStamp;
 
    int mFrameNumber;
-   gadget::PositionInterface mHead;
    vpr::Mutex mSceneViewLock;
 };
 
