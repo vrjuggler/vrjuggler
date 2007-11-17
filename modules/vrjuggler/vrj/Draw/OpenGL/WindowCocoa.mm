67b4e1be638b0569b9a5fa262e0733a92fa4e4cc
Patrick Hartling <patrick.hartling@gmail.com>
2007-11-17 21:10:26 +0000

    Assign a value to gadget::InputAreaCocoa::mMainView when we have the NSView object being used for the window.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@20920 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/OpenGL/WindowCocoa.mm
+++ modules/vrjuggler/vrj/Draw/OpenGL/WindowCocoa.mm
@@ -209,6 +209,7 @@ bool WindowCocoa::open()
                                             glWindow:this
                                                 screen:screen
                                          inputHandling:handle_input];
+      mMainView = mGlView;
 
       // mCocoaWindow retains a refernece to mGlView, meaning that the
       // reference count on it is now two. We release our reference to mGlView
@@ -274,8 +275,10 @@ bool WindowCocoa::open()
          << exception_name << ": " << exception_reason << std::endl
          << vprDEBUG_FLUSH;
 
+      // We do not need to release mMainView here because it is the same as
+      // mGlView, and we did not retain an extra reference to it when
+      // mMainView was asssigned.
       [mGlView release];
-      [mMainView release];
       [mCocoaWindow release];
       [mRenderLock release];
 
