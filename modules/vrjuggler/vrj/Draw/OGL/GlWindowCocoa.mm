15017e5d07419be80f1a60708ac58fe814b8ed49
Patrick Hartling <patrick.hartling@gmail.com>
2007-11-27 16:01:41 +0000

    MFT r20944: Protect against (mis-configured) screen identifiers that are less             than 0.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/branches/2.2@20945 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- modules/vrjuggler/vrj/Draw/OGL/GlWindowCocoa.mm
+++ modules/vrjuggler/vrj/Draw/OGL/GlWindowCocoa.mm
@@ -356,7 +356,7 @@ void GlWindowCocoa::configWindow(vrj::DisplayPtr displayWindow)
       mScreen = disp_sys_elt->getProperty<unsigned int>("pipes", screen_num);
 
       NSArray* screens = [NSScreen screens];
-      if ( mScreen >= [screens count] )
+      if ( mScreen < 0 || mScreen >= [screens count] )
       {
          mScreen = 0;
       }
