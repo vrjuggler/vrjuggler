c2edcb035cd09891b9d4891198cf26c00133eac4
Patrick Hartling <patrick.hartling@gmail.com>
2008-04-18 22:25:57 +0000

    Added a new position filter that allows the user to specify the full pre- and post-transformation matrices. Bumped the version to 1.3.20.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@21070 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Filter/Position/FullPositionXformFilter.cpp
@@ -0,0 +1,128 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2008 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#include <gadget/gadgetConfig.h>
+
+#include <gmtl/Matrix.h>
+#include <gmtl/MatrixOps.h>
+#include <gmtl/Output.h>
+
+#include <jccl/Config/ConfigElement.h>
+
+#include <gadget/Filter/Position/PositionFilter.h>
+#include <gadget/Filter/Position/PositionFilterFactory.h>
+#include <gadget/Filter/Position/FullPositionXformFilter.h>
+
+
+namespace gadget
+{
+
+// Register this filter.
+GADGET_REGISTER_POSFILTER_CREATOR(FullPositionXformFilter);
+
+FullPositionXformFilter::FullPositionXformFilter()
+{
+   /* Do nothing. */ ;
+}
+
+/** Destructor. */
+FullPositionXformFilter::~FullPositionXformFilter()
+{
+   /* Do nothing. */ ;
+}
+
+std::string FullPositionXformFilter::getElementType()
+{
+   return "full_position_transform_filter";
+}
+
+bool FullPositionXformFilter::config(jccl::ConfigElementPtr e)
+{
+   vprDEBUG_OutputGuard(vprDBG_ALL, vprDBG_VERB_LVL,
+                        std::string("[FullPositionXformFilter::config()]") +
+                           e->getFullName() + std::string(":") +
+                           e->getID() + std::string("\n"),
+                        std::string("[PositionXformFilter::config9)] Done.\n"));
+
+   vprASSERT(e->getID() == FullPositionXformFilter::getElementType());
+
+   // These are the transforms from the base tracker coord system
+   mPreXform[0][0] = e->getProperty<float>("pre_xform", 0);
+   mPreXform[0][1] = e->getProperty<float>("pre_xform", 1);
+   mPreXform[0][2] = e->getProperty<float>("pre_xform", 2);
+   mPreXform[0][3] = e->getProperty<float>("pre_xform", 3);
+   mPreXform[1][0] = e->getProperty<float>("pre_xform", 4);
+   mPreXform[1][1] = e->getProperty<float>("pre_xform", 5);
+   mPreXform[1][2] = e->getProperty<float>("pre_xform", 6);
+   mPreXform[1][3] = e->getProperty<float>("pre_xform", 7);
+   mPreXform[2][0] = e->getProperty<float>("pre_xform", 8);
+   mPreXform[2][1] = e->getProperty<float>("pre_xform", 9);
+   mPreXform[2][2] = e->getProperty<float>("pre_xform", 10);
+   mPreXform[2][3] = e->getProperty<float>("pre_xform", 11);
+   mPreXform[3][0] = e->getProperty<float>("pre_xform", 12);
+   mPreXform[3][1] = e->getProperty<float>("pre_xform", 13);
+   mPreXform[3][2] = e->getProperty<float>("pre_xform", 14);
+   mPreXform[3][3] = e->getProperty<float>("pre_xform", 15);
+
+   mPostXform[0][0] = e->getProperty<float>("post_xform", 0);
+   mPostXform[0][1] = e->getProperty<float>("post_xform", 1);
+   mPostXform[0][2] = e->getProperty<float>("post_xform", 2);
+   mPostXform[0][3] = e->getProperty<float>("post_xform", 3);
+   mPostXform[1][0] = e->getProperty<float>("post_xform", 4);
+   mPostXform[1][1] = e->getProperty<float>("post_xform", 5);
+   mPostXform[1][2] = e->getProperty<float>("post_xform", 6);
+   mPostXform[1][3] = e->getProperty<float>("post_xform", 7);
+   mPostXform[2][0] = e->getProperty<float>("post_xform", 8);
+   mPostXform[2][1] = e->getProperty<float>("post_xform", 9);
+   mPostXform[2][2] = e->getProperty<float>("post_xform", 10);
+   mPostXform[2][3] = e->getProperty<float>("post_xform", 11);
+   mPostXform[3][0] = e->getProperty<float>("post_xform", 12);
+   mPostXform[3][1] = e->getProperty<float>("post_xform", 13);
+   mPostXform[3][2] = e->getProperty<float>("post_xform", 14);
+   mPostXform[3][3] = e->getProperty<float>("post_xform", 15);
+
+   vprDEBUG(vprDBG_ALL, vprDBG_VERB_LVL)
+      << "m:preXform [T*R]:\n" << mPreXform << std::endl << vprDEBUG_FLUSH;
+   vprDEBUG(vprDBG_ALL, vprDBG_VERB_LVL)
+      << "m:postXform [T*R]:\n" << mPostXform << std::endl << vprDEBUG_FLUSH;
+
+   return true;
+}
+
+void FullPositionXformFilter::apply(std::vector<PositionData>& posSamples)
+{
+   typedef std::vector<PositionData>::iterator iter_type;
+   for ( iter_type i = posSamples.begin(); i != posSamples.end(); ++i )
+   {
+      // POST xform: cur = cur * postTrans * postRot
+      gmtl::postMult((*i).mPosData, mPostXform);
+
+      // PRE: S_world = wMs * S_sensor
+      gmtl::preMult((*i).mPosData, mPreXform);
+   }
+}
+
+} // End of gadget namespace
---
+++ modules/gadgeteer/gadget/Filter/Position/FullPositionXformFilter.h
@@ -0,0 +1,120 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2008 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _GADGET_FULL_POSITION_XFORM_FILTER_H_
+#define _GADGET_FULL_POSITION_XFORM_FILTER_H_
+
+#include <gadget/gadgetConfig.h>
+
+#include <vector>
+
+#include <gmtl/Matrix.h>
+
+#include <jccl/Config/ConfigElementPtr.h>
+#include <gadget/Filter/Position/PositionFilter.h>
+#include <gadget/Type/PositionData.h>
+
+
+namespace gadget
+{
+
+/** \class FullPositionXformFilter FullPositionXformFilter.h gadget/Filter/Position/FullPositionXformFilter.h
+ *
+ * Fully configurable position transformation filter. Users define the entire
+ * pre- and post-transformation matrices for the filter in a config element.
+ *
+ * @since 1.3.20
+ */
+class GADGET_CLASS_API FullPositionXformFilter : public PositionFilter
+{
+public:
+   /** Constructor. */
+   FullPositionXformFilter();
+
+   /** Destructor. */
+   virtual ~FullPositionXformFilter();
+
+   /**
+    * Configures this position filter.
+    *
+    * @return \c true is returned if configured correctly.
+    */
+   virtual bool config(jccl::ConfigElementPtr e);
+
+   /**
+    * Applies the position filter to the given samples.
+    *
+    * @post Each gadget::PositionData object in \p posSamples is updated with
+    *       a transformed version.
+    *
+    * @param posSamples The collection of samples to modify (in place).
+    */
+   virtual void apply(std::vector<PositionData>& posSamples);
+
+   static std::string getElementType();
+
+   /** @name Transformation Matrix Accessors */
+   //@{
+   const gmtl::Matrix44f& getPreXform() const
+   {
+      return mPreXform;
+   }
+
+   void setPreXform(const gmtl::Matrix44f& xform)
+   {
+      mPreXform = xform;
+   }
+
+   const gmtl::Matrix44f& getPostXform() const
+   {
+      return mPostXform;
+   }
+
+   void setPostXform(const gmtl::Matrix44f& xform)
+   {
+      mPostXform = xform;
+   }
+   //@}
+
+private:
+   /**
+    * Given Sensor position transform in sensor coordinate system: Sensor
+    * world - world coordindate system that the transformed sensor "ends up in"
+    * Sensor' - Modified sensor coordinate system.
+    *
+    * preXform is the transform from world to sensor (world_M_sensor).
+    * postXform is the transform from sensor to sensor' (sensor_M_newsensor).
+    *
+    * compute Sensor = preTrans*preRot*(Scale*Sensor)*postTrans*postRot
+    */
+   gmtl::Matrix44f mPreXform;    /**< Pre-transformation */
+   gmtl::Matrix44f mPostXform;   /**< Post-transformation */
+};
+
+} // End of gadget namespace
+
+
+#endif /* _GADGET_FULL_POSITION_XFORM_FILTER_H_ */
