953f55e0965f0472112cfa3be78b76524b52df56
Patrick Hartling <patrick.hartling@gmail.com>
2010-06-07 12:10:17 +0000

    Added vrj::osg::AppPager, an application base class for Open Scene Graph that knows how to interact with osgDB::DatabasePager.
    Submitted by: Lucie Lemonnier
    
    
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@21300 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/vrjuggler/vrj/Draw/OSG/AppPager.h
@@ -0,0 +1,142 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2010 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _VRJ_OSG_APP_PAGER_H_
+#define _VRJ_OSG_APP_PAGER_H_
+
+#include <vrj/Draw/OSG/App.h>
+#include <vrj/vrjConfig.h>
+#include <gadget/Type/PositionInterface.h>
+#include <osgDB/DatabasePager>
+#include <osgDB/ImagePager>
+#include <osg/FrameStamp>
+#include <osgUtil/SceneView>
+
+
+namespace vrj
+{
+
+namespace osg
+{
+
+/** \class vrj::osg::AppPager AppPager.h vrj/Draw/OSG/AppPager.h
+ *
+ * @since 2.3.20
+ */
+class AppPager : public vrj::osg::App
+{
+public:
+   AppPager(Kernel* kern = NULL)
+      : vrj::osg::App(kern)
+      , mFrameCount(0)
+   {
+   }
+
+   virtual ~AppPager()
+   {
+   }
+
+   virtual void init()
+   {
+      vrj::osg::App::init();
+      mFrameStamp = new ::osg::FrameStamp;
+      initScene();
+   }
+
+   virtual void initScene()
+   {
+      mHead.init("VJHead");
+
+      mDatabasePager = new osgDB::DatabasePager;
+      mImagePager = new osgDB::ImagePager;
+
+      mDatabasePager->registerPagedLODs(getScene()); 
+   }
+
+   virtual void latePreFrame()
+   {
+      update();
+   }
+
+   virtual void configSceneView(osgUtil::SceneView* newSceneViewer)
+   {
+      vrj::osg::App::configSceneView(newSceneViewer);
+
+      newSceneViewer->setFrameStamp(mFrameStamp);
+
+      newSceneViewer->getCullVisitor()->setDatabaseRequestHandler(mDatabasePager);
+      mDatabasePager->setCompileGLObjectsForContextID(
+         newSceneViewer->getState()->getContextID(), true
+      );
+   }
+
+   void update()
+   {
+      vrj::osg::App::update();
+      ++mFrameCount;
+      mFrameStamp->setFrameNumber(mFrameCount);
+
+      mHeadTime = mHead->getTimeStamp().secd();
+      mFrameStamp->setReferenceTime(mHeadTime);
+      mFrameStamp->setSimulationTime(mHeadTime);
+
+      mDatabasePager->updateSceneGraph(*mFrameStamp);
+      mImagePager->updateSceneGraph(*mFrameStamp);
+
+      mDatabasePager->signalBeginFrame(mFrameStamp);
+   }
+
+   virtual void contextPostDraw()
+   {
+      const double available_time(1.0 / 10.0);
+
+      if (mFrameStamp)
+      {
+         mDatabasePager->compileGLObjects(*sceneViewer->get()->getState(),
+                                          available_time);
+         sceneViewer->get()->flushDeletedGLObjects(available_time);
+      }
+
+      mDatabasePager->signalEndFrame();
+      vrj::osg::App::contextPostDraw();
+   }
+
+protected:
+   gadget::PositionInterface  mHead;     /**< the head */
+   ::osg::ref_ptr<osgDB::DatabasePager> mDatabasePager;
+   ::osg::ref_ptr<osgDB::ImagePager> mImagePager;
+   double mHeadTime;
+   ::osg::ref_ptr<FrameStamp> mFrameStamp;
+
+   int mFrameCount;
+};
+
+} // End of osg namespace
+
+} // End of vrj namespace
+
+
+#endif /* _VRJ_OSG_APP_PAGER_H_ */
