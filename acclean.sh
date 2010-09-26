af6d2708d616b8c04db8b07d1ffd454357431baf
Patrick Hartling <patrick.hartling@gmail.com>
2010-09-26 14:39:21 +0000

    Added a script that should be helpful in handling cases of Aclocal or Autoconf being updated while continuing to use the same Juggler source tree. The intent is to ensure that all cached and generated output from running these tools is removed to get a clean, fully regenerated build system the next time that make(1) is run.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@21500 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ acclean.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# ************** <auto-copyright.pl BEGIN do not edit this line> **************
+#
+# VR Juggler is (C) Copyright 1998-2010 by Iowa State University
+#
+# Original Authors:
+#   Allen Bierbaum, Christopher Just,
+#   Patrick Hartling, Kevin Meinert,
+#   Carolina Cruz-Neira, Albert Baker
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
+# *************** <auto-copyright.pl END do not edit this line> ***************
+
+# This script cleans up cached files and directories created as part of
+# running aclocal(1) and autoconf(1). It is useful when either of Aclocal
+# and/or Autoconf is updated or when trying to test multiple versions of
+# these tools.
+echo "Removing:"
+find . -name aclocal.m4 -print -exec rm -f \{} \;
+find . -name config.cache -print -exec rm -f \{} \;
+find . -type d -name autom4te.cache -print -exec rm -rf \{} \;
+
+echo "Updating modified type (to ensure regeneration):"
+find . -name configure.ac -print -exec touch \{} \;
