vrjuggler/vrjuggler

commit 25dca9bfff257c244f41d82a058bbf21850a1b9f

MFT [r21569]: Silenced aliasing warnings from GCC.
Submitted by: Carsten Neumann


git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/branches/3.0@21572 08b38cba-cd3b-11de-854e-f91c5b6e4272

modules/vapor/vpr/IO/BufferObjectReader.h
@@ -355,25 +355,32 @@ inline float BufferObjectReader::readFloat()
 {
    // We are reading the float as a 4 byte value
    BOOST_STATIC_ASSERT(sizeof(float) == 4);
+   union
+   {
+     float       floatVal;
+     vpr::Uint32 intVal;
+   } data;
 
-   vpr::Uint32 nw_val;
-   std::memcpy(&nw_val, readRaw(4), 4);
-   vpr::Uint32 h_val = vpr::System::Ntohl(nw_val);
+   std::memcpy(&data.intVal, readRaw(4), 4);
+   data.intVal = vpr::System::Ntohl(data.intVal);
 
-   return *((float*)&h_val);
+   return data.floatVal;
 }
 
 inline double BufferObjectReader::readDouble()
 {
    // We are reading the double as a 8 byte value
    BOOST_STATIC_ASSERT(sizeof(double) == 8);
+   union
+   {
+     double      doubleVal;
+     vpr::Uint64 intVal;
+   } data;
 
-   vpr::Uint64 nw_val;
-   std::memcpy(&nw_val, readRaw(8), 8);
-   vpr::Uint64 h_val = vpr::System::Ntohll(nw_val);
-   double d_val = *((double*)&h_val);
+   std::memcpy(&data.intVal, readRaw(4), 4);
+   data.intVal = vpr::System::Ntohl(data.intVal);
 
-   return d_val;
+   return data.doubleVal;
 }
 
 inline std::string BufferObjectReader::readString()

modules/vapor/vpr/IO/BufferObjectWriter.h
@@ -246,18 +246,32 @@ inline void BufferObjectWriter::writeFloat(float val)
 {
    // We are writing the float as a 4 byte value
    BOOST_STATIC_ASSERT(sizeof(float) == 4);
-   vpr::Uint32 nw_val = vpr::System::Htonl(*((vpr::Uint32*) &val));
+   union
+   {
+     float       floatVal;
+     vpr::Uint32 intVal;
+   } data;
 
-   writeRaw((vpr::Uint8*) &nw_val, 4);
+   data.floatVal = val;
+   data.intVal   = vpr::System::Htonl(data.intVal);
+
+   writeRaw((vpr::Uint8*) &data.intVal, 4);
 }
 
 inline void BufferObjectWriter::writeDouble(double val)
 {
    // We are writing the double as a 8 byte value
    BOOST_STATIC_ASSERT(sizeof(double) == 8);
-   vpr::Uint64 nw_val = vpr::System::Htonll(*((vpr::Uint64*) &val));
+   union
+   {
+     double      doubleVal;
+     vpr::Uint64 intVal;
+   } data;
 
-   writeRaw((vpr::Uint8*) &nw_val, 8);
+   data.doubleVal = val;
+   data.intVal    = vpr::System::Htonll(data.intVal);
+
+   writeRaw((vpr::Uint8*) &data.intVal, 8);
 }
 
 inline void BufferObjectWriter::writeString(std::string val)

commit a1a7f97ce8c2ff476053ceeb9af0aca956a69178

MFT [r21650]: A double is eight bytes wide.
This fixes a data serialization bug introduced in r21572.


git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/branches/3.0@21651 08b38cba-cd3b-11de-854e-f91c5b6e4272

modules/vapor/vpr/IO/BufferObjectReader.h
@@ -377,7 +377,7 @@ inline double BufferObjectReader::readDouble()
      vpr::Uint64 intVal;
    } data;
 
-   std::memcpy(&data.intVal, readRaw(4), 4);
+   std::memcpy(&data.intVal, readRaw(8), 8);
    data.intVal = vpr::System::Ntohl(data.intVal);
 
    return data.doubleVal;