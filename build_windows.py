b04b7c01062c40ab6e8b4d12d995b0b6a088a6e8
Patrick Hartling <patrick.hartling@gmail.com>
2011-10-09 13:59:53 +0000

    Added the bits for installing the Application Barrier Manager plug-in on Windows.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/branches/3.0@21799 08b38cba-cd3b-11de-854e-f91c5b6e4272

--- build_windows.py
+++ build_windows.py
@@ -1613,10 +1613,16 @@ def installGadgeteerPlugins(prefix, buildDir):
                           'ApplicationDataManager')
    installDir(srcdir, destdir, ['.h'])
 
+   destdir = os.path.join(prefix, 'include', 'plugins',
+                          'ApplicationBarrierManager')
+   srcdir  = os.path.join(gJugglerDir, 'modules', 'gadgeteer', 'plugins',
+                          'ApplicationBarrierManager')
+   installDir(srcdir, destdir, ['.h'])
+
    destdir = os.path.join(prefix, 'lib', 'gadgeteer', 'plugins')
    srcroot = os.path.join(buildDir, 'Gadgeteer')
 
-   plugins = ['ApplicationDataManager', 'RemoteInputManager']
+   plugins = ['ApplicationDataManager', 'ApplicationBarrierManager', 'RemoteInputManager']
 
    for p in plugins:
       srcdir = os.path.join(srcroot, p)
