9ca6a287e1e2b36dce68e95e91e3e1c47305f298
Patrick Hartling <patrick.hartling@gmail.com>
2011-11-26 21:21:09 +0000

    Added a replacement for gadget::DigitalMultiClickEventInterface based on the new multi-event architecture. Bumped the version to 2.1.23.
    git-svn-id: https://vrjuggler.googlecode.com/svn/juggler/trunk@21846 08b38cba-cd3b-11de-854e-f91c5b6e4272

---
+++ modules/gadgeteer/gadget/Event/DigitalClickEventInterface.h
@@ -0,0 +1,201 @@
+/*************** <auto-copyright.pl BEGIN do not edit this line> **************
+ *
+ * VR Juggler is (C) Copyright 1998-2011 by Iowa State University
+ *
+ * Original Authors:
+ *   Allen Bierbaum, Christopher Just,
+ *   Patrick Hartling, Kevin Meinert,
+ *   Carolina Cruz-Neira, Albert Baker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ *************** <auto-copyright.pl END do not edit this line> ***************/
+
+#ifndef _GADGET_DIGITAL_CLICK_EVENT_INTERFACE_H_
+#define _GADGET_DIGITAL_CLICK_EVENT_INTERFACE_H_
+
+#include <gadget/gadgetConfig.h>
+
+#include <gadget/Event/ClickEvent.h>
+#include <gadget/Event/MultiEventInterface.h>
+#include <gadget/Event/MultiEventGenerator.h>
+#include <gadget/Type/DigitalProxy.h>
+
+
+namespace gadget
+{
+
+/** \class DigitalClickEventInterface DigitalClickEventInterface.h gadget/Event/DigitalClickEventInterface.h
+ *
+ * A specialized digital event interface type for detecting click events from
+ * a digital device. For a given instantiation, events for 1 to \c ClickCount
+ * clicks are generated. The callback signature is the same as for
+ * gadget::DigitalEventInterface.
+ *
+ * This is a multi-event interface, and callbacks are classified based on
+ * event tags. The tags for this type are generated automatically based on the
+ * \c ClickCount template parameter. The tag type is gadget::event::click_tag,
+ * a type instantiated based on the number of clicks in the event. Thus, if
+ * \c ClickCount is 2, then there are two event tags that can be used:
+ * gadget::event::click_tag<1> and gadget::event::click_tag<2>. There are
+ * typedefs for these two types because they are so common:
+ * gadget::event::single_click_tag and gadget::event::double_click_tag.
+ *
+ * Event tags are used to correlate event tags to callbacks. For example, if
+ * an application class the following member:
+ *
+ * \code
+ * gadget::DigitalClickEventInterface<3> mClickIface;
+ * \endcode
+ *
+ * it effectively declares interest in single, double-, and/or triple-click
+ * events from a gadget::DigitalProxy. The callbacks are added as follows:
+ *
+ * \code
+ * void singleClickCallback(const gadget::DigitalState::State)
+ * {
+ *    // Handle a single click event...
+ * }
+ *
+ * void doubleClickCallback(const gadget::DigitalState::State)
+ * {
+ *    // Handle a double-click event...
+ * }
+ *
+ * void tripleClickCallback(const gadget::DigitalState::State)
+ * {
+ *    // Handle a triple-click event...
+ * }
+ *
+ * void MyApp::init()
+ * {
+ *    mClickIface.init("VJDigital0");
+ *
+ *    using namespace gadget::event;
+ *    mClickIface.addCallback<single_click_tag>(singleClickCallback);
+ *    mClickIface.addCallback<double_click_tag>(doubleClickCallback);
+ *    mClickIface.addCallback<click_tag<3> >(tripleClickCallback);
+ * }
+ * \endcode
+ *
+ * Receipt of a double-click event is preceded by delivery of two single click
+ * events. Whether the single click events are of interest is up to the
+ * user-level code.
+ *
+ * @tparam ClickCount    The maximum number of clicks that can be present in a
+ *                       click event. For example, a value of 2 means that
+ *                       single and double-click events will be generated. A
+ *                       value of 3 means that single, double-, and
+ *                       triple-click events will be generated.
+ * @tparam CollectionTag A tag specifyiing which event(s) will be collected by
+ *                       the event generator created by this object. This must
+ *                       be a valid collection tag in order for the code to
+ *                       compile. This template paramter is optional, and it
+ *                       defaults to gadget::event::all_events_tag.
+ * @tparam GenerationTag A tag specifying how events will be emitted by the
+ *                       event generator created by this object. This must be
+ *                       a valid generation tag in order for the code to
+ *                       compile. This template paramter is optional, and it
+ *                       defaults to gadget::event::immediate_tag.
+ *
+ * @since 2.1.23
+ */
+template<unsigned int ClickCount
+       , typename CollectionTag = event::all_events_tag
+       , typename GenerationTag = event::immediate_tag>
+class DigitalClickEventInterface
+   : public MultiEventInterface<
+                 DigitalProxy
+               , MultiEventGenerator<
+                      DigitalProxy
+                    , typename event::ClickMaker<ClickCount>::type
+                    , CollectionTag
+                    , GenerationTag
+                 >
+            >
+{
+public:
+   typedef typename DigitalClickEventInterface::event_interface_ base_type;
+   typedef typename base_type::proxy_ptr_type proxy_ptr_type;
+
+   /**
+    * Constructor.
+    *
+    * @param clickTime The maximum time (measured in milliseconds) allowed
+    *                  between sequential clicks for the click events to be
+    *                  treated as a single multi-click event. This parameter
+    *                  is optional, and it defaults to 300 milliseconds.
+    *
+    * @see setClickTime()
+    */
+   DigitalClickEventInterface(const unsigned long clickTime = 300)
+      : mClickTime(clickTime)
+   {
+      /* Do nothing. */ ;
+   }
+
+   virtual ~DigitalClickEventInterface()
+   {
+      /* Do nothing. */ ;
+   }
+
+   /**
+    * Changes the maximum click time to be the given value.
+    *
+    * @pre An event generator for this event interface has not been created
+    *      yet. Setting this value after the event generator has been created
+    *      will not influence the event creation.
+    *
+    * @param clickTime The maximum time (measured in milliseconds) allowed
+    *                  between sequential clicks for the click events to be
+    *                  treated as a single multi-click event.
+    */
+   void setClickTime(const unsigned long clickTime)
+   {
+      mClickTime = clickTime;
+   }
+
+protected:
+   typedef typename base_type::generator_type     generator_type;
+   typedef typename base_type::generator_ptr_type generator_ptr_type;
+
+   EventGeneratorPtr createEventGenerator(const proxy_ptr_type& proxy)
+   {
+      EventGeneratorPtr base_generator(
+         this->base_type::createEventGenerator(proxy)
+      );
+
+      // Downcast to our specific generator type so that we can set the
+      // maximum click time that the newly created event generator will use.
+      generator_ptr_type generator(
+         boost::dynamic_pointer_cast<generator_type>(base_generator)
+      );
+
+      typedef event::detail::ClickTimeSetter<generator_ptr_type> cts_type;
+      cts_type setter(generator, mClickTime);
+      boost::mpl::for_each<typename base_type::event_tags>(setter);
+
+      return base_generator;
+   }
+
+private:
+   unsigned long mClickTime;    /**< Maximum click time (milliseconds) */
+};
+
+}
+
+
+#endif /* _GADGET_DIGITAL_CLICK_EVENT_INTERFACE_H_ */
