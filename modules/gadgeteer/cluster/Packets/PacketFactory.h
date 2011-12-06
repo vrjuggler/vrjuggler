20098a1ba68c92c3d153a8990e41977a1d7a211b
Ryan Pavlik <rpavlik@iastate.edu>
2011-12-06 16:59:30 -0600

    PacketFactory.h: Fix two-stage lookup issue.
    Found by Clang.

--- modules/gadgeteer/cluster/Packets/PacketFactory.h
+++ modules/gadgeteer/cluster/Packets/PacketFactory.h
@@ -128,7 +128,7 @@ public:
          return (i->second)();
       }
 
-      return onUnknownType(id);     // Calls template method from FactoryErrorPolicy<>
+      return FactoryErrorPolicy::onUnknownType(id);     // Calls template method from FactoryErrorPolicy<>
    }
 
 protected:
