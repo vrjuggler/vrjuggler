6a57dbb7dc0f8f05a2b7e94d585f41400b306c67
Carsten Neumann <carsten_neumann@gmx.net>
2016-01-22 16:27:31 -0600

    Remove dead function declarations

--- modules/gadgeteer/drivers/Open/Trackd/trackdmem.h
+++ modules/gadgeteer/drivers/Open/Trackd/trackdmem.h
@@ -78,17 +78,4 @@ trackd_controller_button(ControllerConnection* c, int buttonNum);
 float
 trackd_controller_valuator(ControllerConnection* c, int valuatorNum);
 
-#if 0
-void * trackd_attach_tracker_mem(int shmKey);
-void trackd_release_tracker_mem(void *mem);
-int trackd_num_sensors(void *mem);
-CAVE_SENSOR_ST * trackd_sensor(void *mem,int sensorNum);
-void * trackd_attach_controller_mem(int shmKey);
-void trackd_release_controller_mem(void *mem);
-int trackd_num_buttons(void *mem);
-int trackd_num_valuators(void *mem);
-int trackd_button(void *mem,int buttonNum);
-float trackd_valuator(void *mem,int valuatorNum);
-#endif
-
 #endif
